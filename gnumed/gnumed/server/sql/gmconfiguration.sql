b61b071d3d785c9929950e788d50b9d000ae0640
hherb <hherb>
2001-10-26 14:04:48 +0000

    No limitation any more as to which and how many services can be distributed

--- gnumed/gnumed/server/sql/gmconfiguration.sql
+++ gnumed/gnumed/server/sql/gmconfiguration.sql
@@ -1,4 +1,5 @@
 -- structure of configuration database for gnumed
+-- neccessary to allow for distributed servers
 -- Copyrigth by Dr. Horst Herb
 -- This is free software in the sense of the General Public License (GPL)
 -- For details regarding GPL licensing see http://gnu.org
@@ -9,7 +10,7 @@
 
 CREATE TABLE db (
     id SERIAL PRIMARY KEY,
-    name CHAR(25),
+    name CHAR(35),
     port INT DEFAULT 5432,
     host VARCHAR(255)DEFAULT 'localhost'
 );
@@ -28,25 +29,42 @@ COMMENT ON COLUMN db.host IS
 
 --=====================================================================
 
+CREATE TABLE distributed_db (
+	id SERIAL PRIMARY KEY,
+	name char(35)
+);
+
+COMMENT ON TABLE distributed_db IS
+'Enumerates all possibly available distributed servers. Naming needs approval by gnumed administrators!';
+
+-- !I18N note to translators: do NOT change these values !!!
+INSERT INTO distributed_db(name) values('default');
+INSERT INTO distributed_db(name) values('transactions');
+INSERT INTO distributed_db(name) values('demographica');
+INSERT INTO distributed_db(name) values('geographica');
+INSERT INTO distributed_db(name) values('pharmaceutica');
+INSERT INTO distributed_db(name) values('pathologica');
+INSERT INTO distributed_db(name) values('radiologica');
+INSERT INTO distributed_db(name) values('blobs');
+INSERT INTO distributed_db(name) values('medicalhx');
+INSERT INTO distributed_db(name) values('progressnotes');
+INSERT INTO distributed_db(name) values('educativa');
+INSERT INTO distributed_db(name) values('reference');
+INSERT INTO distributed_db(name) values('modules');
+
+
+
+
 CREATE TABLE config (
     id SERIAL PRIMARY KEY,
     profile CHAR(25) DEFAULT 'default',
     username CHAR(25) DEFAULT CURRENT_USER,
+    ddb INT REFERENCES distributed_db DEFAULT NULL,
+    db INT REFERENCES db,
     crypt_pwd varchar(255) DEFAULT NULL,
     crypt_algo varchar(255) DEFAULT 'RIJNDAEL',
     pwd_hash varchar(255) DEFAULT NULL,
-    hash_algo varchar(255) DEFAULT 'RIPEMD160',
-    demographica INT REFERENCES db DEFAULT NULL,
-    geographica INT REFERENCES db DEFAULT NULL,
-    ref_drug INT REFERENCES db DEFAULT NULL,
-    ref_pateducation INT REFERENCES db DEFAULT NULL,
-    ref_travelmedicine INT REFERENCES db DEFAULT NULL,
-    ref_vaccination INT REFERENCES db DEFAULT NULL,
-    transactions INT REFERENCES db DEFAULT NULL,
-    progressnotes INT REFERENCES db DEFAULT NULL,
-    imaging INT REFERENCES db DEFAULT NULL,
-    pathresults INT REFERENCES db DEFAULT NULL,
-    blobs INT REFERENCES db DEFAULT NULL
+    hash_algo varchar(255) DEFAULT 'RIPEMD160'
 );
 
 COMMENT ON TABLE config IS
@@ -57,6 +75,12 @@ COMMENT ON COLUMN config.profile IS
 
 COMMENT ON COLUMN config.username IS
 'user name as used within the gnumed system';
+
+COMMENT ON COLUMN config.ddb IS
+'reference to one of the allowed distrbuted servers';
+
+COMMENT ON COLUMN config.db IS
+'reference to the implementation details of the distributed server';
 
 COMMENT ON COLUMN config.crypt_pwd IS
 'password for user and database, encrypted';
@@ -73,20 +97,6 @@ COMMENT ON COLUMN config.hash_algo IS
 COMMENT ON COLUMN config.profile IS
 'one user may have different configuration profiles depending on role, need and location';
 
-COMMENT ON COLUMN config.demographica IS
-'demographic information database server';
-
-COMMENT ON COLUMN config.geographica IS
-'geographic information database server';
-
-COMMENT ON COLUMN config.ref_drug IS
-'drug related information database server';
-
-COMMENT ON COLUMN config.ref_pateducation IS
-'patient education information related database server';
-
-COMMENT ON COLUMN config.transactions IS
-'database server responsible for allocating transaction IDs';
 
 
 
