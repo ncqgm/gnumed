a22ec4b5fd5ef6782e3e5f1fb958117d635ec4a8
Karsten Hilbert <karsten.hilbert@gmx.net>
2002-03-09 09:57:54 +0000

    - added a bit of logging

--- gnumed/gnumed/client/wxpython/gnumed.py
+++ gnumed/gnumed/client/wxpython/gnumed.py
@@ -15,14 +15,26 @@
 ############################################################################
 
 import sys, os
+import gmLog
 
 if __name__ == "__main__":
 	"launch the gnumed wx GUI client "
-	appPath = ''
+	appPath = os.path.split(sys.argv[0])[0]
+
+	# log file
+	aFile = gmLog.LogTargetFile (os.path.basename(sys.argv[0]) + ".log", "a", gmLog.lInfo)
+	gmLog.gmDefLog.AddTarget(aFile)
+
+	# console is Good(tm)
+	aLogTarget = gmLog.LogTargetConsole(gmLog.lInfo)
+	gmLog.gmDefLog.AddTarget(aLogTarget)
+
+	gmLog.gmDefLog.Log(gmLog.lInfo, 'Starting up as main module.')
+	gmLog.gmDefLog.Log(gmLog.lInfo, "appPath = " + appPath)
+
 	try:
 		#change into our working directory
 		#this does NOT affect the cdw in the shell from where gnumed is started!
-		appPath = os.path.split(sys.argv[0])[0]
 		os.chdir(appPath)
 	except:
 		pass
@@ -30,9 +42,10 @@ if __name__ == "__main__":
 	try:
 		import gmGuiMain
 	except ImportError:
+		gmLog.gmDefLog.Log(gmLog.lPanic, "Cannot load gmGuiMain")
+		exc = sys.exc_info()
+		gmLog.gmDefLog.LogException ("Exception caught !", exc)
 		sys.exit("CRITICAL ERROR: Can't find module gmGuiMain! - Program halted")
 
 	#run gnumed!
 	gmGuiMain.main()
-
-
