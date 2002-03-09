b56d95b24f95f543130b57e84395c2ccf724c1bc
hherb <hherb>
2002-03-09 05:50:21 +0000

    New exception for use of pure virtual functions added

--- gnumed/gnumed/client/wxpython/gmExceptions.py
+++ gnumed/gnumed/client/wxpython/gmExceptions.py
@@ -16,24 +16,33 @@
 
 
 class ConnectionError(Exception):
-    #raised whenever the database backend connection fails
-    def __init__(self, errmsg):
-	self.errmsg=errmsg
+	#raised whenever the database backend connection fails
+	def __init__(self, errmsg):
+		self.errmsg=errmsg
 
-    def __str__(self):
-	return errmsg
+	def __str__(self):
+		return errmsg
 
-	
+
 
 class NoGuiError(Exception):
-    #raised whenever the database backend connection fails
-    def __init__(self, errmsg):
-	self.errmsg=errmsg
+	#raised whenever the database backend connection fails
+	def __init__(self, errmsg):
+		self.errmsg=errmsg
 
-    def __str__(self):
-	return errmsg
+	def __str__(self):
+		return errmsg
 
 
+class PureVirtualFunction(Exception):
+	#raised whenever the database backend connection fails
+	def __init__(self, errmsg=None):
+		if errmsg is not None:
+			self.errmsg=errmsg
+	else:
+		self.errmsg="Attempt to call a pure virtual function!")
 
+	def __str__(self):
+		return errmsg
 
 
