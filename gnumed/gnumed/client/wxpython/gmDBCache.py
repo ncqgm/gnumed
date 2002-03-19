49d11dcb8a4cfee3875a3aca8dd169efbbc0c296
hherb <hherb>
2002-03-19 09:19:57 +0000

    buffer is now shared at subclass level

--- gnumed/gnumed/client/wxpython/gmDBCache.py
+++ gnumed/gnumed/client/wxpython/gmDBCache.py
@@ -17,28 +17,41 @@
 
 # standard Python modules
 import copy
-
 # GNUmed modules
 import gmLog
+
+class DBcache:
+
+	def __init__(self):
+		self.buffer = None
+		self.id = None
+		self.db = None
+		self.querystr = None
+		self.lazy = 1	#default: set lazy on
+		self.expiry = -1	#default: buffer never expires
+		self.lastquery = None	#timestamp of last executed query
+		self.notify = {}
+		self.attributes = []
+
+
+
+
 
 class CachedDBObject:
 
 	#static private variables:
-	__buffer = None
-	__id = None
-	__db = None
-	__querystr = None
-	__lazy = 1	#default: set lazy on
-	__expiry = -1	#default: buffer never expires
-	__lastquery = None	#timestamp of last executed query
-	__notify = {}
-	__attributes = []
+	dbcache = DBcache()
 
 	def __init__(self, id=None, db=None, querystr=None, lazy=None, expiry=None):
 
+		#derived classes MUST replace CachedDBObject with their own classname!
+		#execstr =
+		#print execstr
+		exec("self.cache = %s.dbcache" % self.__class__.__name__)
+
 		if db is not None:
-			if CachedDBObject.__db is not db:
-				CachedDBObject.__db = db
+			if self.cache.db is not db:
+				self.cache.db = db
 				self.reset
 
 		if id is not None:
@@ -48,57 +61,57 @@ class CachedDBObject:
 			self.setQueryStr(querystr)
 
 		if expiry is not None:
-			CachedDBObject.__expiry = expiry
+			self.cache.expiry = expiry
 
-		if not lazy and self.__buffer is not None:
+		if not lazy and self.cache.buffer is not None:
 			self.__query()
 
-		_myLog.Log(gmLog.lData, "Instantiated. ID='" + str(self.__id) +
-					"' DB='" + str(self.__db) +
-					"' query='" + str(self.__querystr) +
-					"' lazy='" + str(self.__lazy) +
-					"' expiry='" + str(self.__expiry) + "'")
+		_myLog.Log(gmLog.lData, "Instantiated. ID='" + str(self.cache.id) +
+					"' DB='" + str(self.cache.db) +
+					"' query='" + str(self.cache.querystr) +
+					"' lazy='" + str(self.cache.lazy) +
+					"' expiry='" + str(self.cache.expiry) + "'")
 
 	def reset(self):
 		"force a re-query of buffer on next data access attempt"
-		CachedDBObject.__buffer = None
-		CachedDBObject.__lastquery = None
+		self.cache.buffer = None
+		self.cache.lastquery = None
 
 	def setQueryStr(self, querystr):
-		if CachedDBObject.__querystr != querystr :
-			CachedDBObject.__querystr = querystr
+		if self.cache.querystr != querystr :
+			self.cache.querystr = querystr
 			self.reset
 			_myLog.Log(gmLog.lData, "changing query to SQL>>>" + str(querystr) + "<<<SQL")
 
 	def getQueryStr(self):
-		return copy.copy(CachedDBObject.__querystr)
+		return copy.copy(self.cache.querystr)
 
 	def setId(self, id):
-		if CachedDBObject.__id != id:
-			CachedDBObject.__id = id
+		if self.cache.id != id:
+			self.cache.id = id
 			self.reset()
 
 	def getId(self):
 		"get the ID of the current object"
-		return copy.copy(CachedDBObject.__id)
+		return self.cache.id
 
 
 	def get(self, id=None, copy=1):
 		"""returns the buffer. If id is not None and not in cache,
 		the backend will be queried."""
 		if id is not None:
-			if CachedDBObject.__id != id:
-				CachedDBObject.__id = id
+			if self.cache.id != id:
+				self.cache.id = id
 				self.reset()
-		if CachedDBObject.__buffer is None:
+		if self.cache.buffer is None:
 			self.__query()
 		if copy:
 			try:
-				return copy.copy(CachedDBObject.__buffer)
+				return copy.deepcopy(self.cache.buffer)
 			except:
-				return CachedDBObject.__buffer
+				return self.cache.buffer
 		else:
-			return CachedDBObject.__buffer
+			return self.cache.buffer
 
 
 
@@ -113,36 +126,47 @@ class CachedDBObject:
 		'callback' must not return anything"""
 		if callback is None:
 			try:
-				del(CachedDBObject.__notify[who])
+				del(self.cache.notify[who])
 			except:
 				pass
 		else:
-			CachedDBObject.__notify[who] = callback
+			self.cache.notify[who] = callback
 
 
 	def notify(self):
 		"""forces execution of all registered callback functions
-
 		This function will be called whenever the buffer changes
 		"""
-		for caller in CachedDBObject.__notify.keys():
-			CachedDBObject.__notify[caller](caller, CachedDBObject.__id)
+		for caller in self.cache.notify.keys():
+			self.cache.notify[caller](caller, self.cache.id)
+
 
 	def attributes(self):
-		"""returns row attributes ('column/field names')"""
-		return CachedDBObject.__attributes
+		"returns row attributes ('field names')"
+		return self.cache.attributes
+
+	def pprint(self):
+		buf = self.get(copy=0)
+		#labels = self.attributes()
+		pstr=''
+		for row in buf:
+			for attr in row:
+				pstr =  "%s %s | " % (pstr, attr)
+			pstr = pstr + "\n"
+		return pstr
 
 
 	def __query(self):
-		assert (CachedDBObject.__id is not None)
-		assert (CachedDBObject.__db is not None)
-		assert (CachedDBObject.__querystr is not None)
-		cursor = CachedDBObject.__db.cursor()
-		#print "Executing %s\n" % CachedDBObject.__querystr
-		_myLog.Log(gmLog.lData, "Executing " + str(CachedDBObject.__querystr))
-		cursor.execute(CachedDBObject.__querystr)
-		CachedDBObject.__buffer = cursor.fetchall()
-		CachedDBObject.__attributes = gmPG.fieldNames(cursor)
+		#assert (self.__cache.__id is not None)
+		assert (self.cache.db is not None)
+		assert (self.cache.querystr is not None)
+		cursor = self.cache.db.cursor()
+		#<DEBUG>
+		print "Executing %s\n" % self.cache.querystr
+		#</DEBUG>
+		cursor.execute(self.cache.querystr)
+		self.cache.buffer = cursor.fetchall()
+		self.cache.attributes = gmPG.fieldNames(cursor)
 		#let everybody know that our buffer has changed
 		self.notify()
 
@@ -152,6 +176,19 @@ class CachedDBObject:
 _myLog = gmLog.gmDefLog
 
 if __name__ == "__main__":
+
+
+	class tables(CachedDBObject):
+		dbcache = DBcache()
+		def __init__(self, db):
+			CachedDBObject.__init__(self, id=-1, db=db, querystr="select * from pg_tables where tablename not like 'pg_%'")
+
+	class dbs(CachedDBObject):
+		dbcache = DBcache()
+		def __init__(self, db):
+			CachedDBObject.__init__(self, id=-1, db=db, querystr="select datname from pg_database")
+
+
 
 	aHandle = gmLog.LogTargetConsole(gmLog.lData)
 	_myLog.AddTarget(aHandle)
@@ -164,8 +201,12 @@ if __name__ == "__main__":
 	import gmPG
 	conn = gmPG.ConnectionPool()
 	db = conn.GetConnection('default')
+
 	databases = CachedDBObject(db=db, id=-1, querystr="select * from pg_database")
 	databases.notify_me("main", callback)
+	tablelist = tables(db)
+	dbl = dbs(db)
+
 	dblist = databases.get()
 	print "List of available databases:"
 	print "-----------------------------------------------------"
@@ -173,3 +214,18 @@ if __name__ == "__main__":
 		for a in d:
 			print str(a) + "\t",
 		print "\n"
+
+	print "List of tables in curent database:"
+	print "-----------------------------------------------------"
+	print tablelist.pprint()
+
+	print "List of databases (names only):"
+	print "-----------------------------------------------------"
+	print dbl.pprint()
+
+	print "List of databases (all attributes again):"
+	print "-----------------------------------------------------"
+	print databases.pprint()
+
+
+
