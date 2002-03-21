7c1fae94d8a083d06af062609790c7018c1e0ed6
Karsten Hilbert <karsten.hilbert@gmx.net>
2002-03-21 09:10:56 +0000

    - first draft of script to disable excess debugging

---
+++ gnumed/gnumed/client/python-tools/debugging_off.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+###############################################################
+# This is to disable excess debugging in production releases.
+#
+# theory of operation:
+#
+# debugging comments that should be off in a release build
+# must be fenced off by the two tags #<DEBUG> and #</DEBUG>
+#
+# example:
+#
+#	do.something()
+#
+#	#<DEBUG>
+#	log.log(everything)
+#	shout.at(user)
+#	#</DEBUG>
+#
+#	do.something_else(now)
+#
+#--------------------------------------------------------------
+# @author: Karsten Hilbert
+# @copyright: author
+# @license: GPL (details at http://www.gnu.org)
+# @Date: $Date: 2002-03-21 09:10:56 $
+# @version $Revision: 1.1 $ $Date: 2002-03-21 09:10:56 $ $Author: ncq $
+###############################################################
+
+import string, sys, fileinput
+
+if len(sys.argv) < 2:
+    print "Usage: debugging_off.py <a_python_script> <a_python_script> ..."
+    sys.exit(1)
+
+print "Removing excess debugging from", sys.argv[1:]
+
+for line in fileinput.input(inplace=1, backup='.bak1'):
+
+    line = string.replace(line, '\015','')
+    line = string.replace(line, '\012','')
+    tmp = string.lstrip(line)
+
+    # find start of tagged debugging
+    if string.find(tmp, "#<DEBUG>", 0, 8) == 0:
+	print '"""' + line
+
+    # find end of tagged debugging
+    elif string.find(tmp, "#</DEBUG>", 0, 9) == 0:
+	print line + '"""'
+
+    else:
+	print line
