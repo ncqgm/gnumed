4854cccd1b93840d64aa5369b9881bfefe3551a8
hherb <hherb>
2002-03-31 09:05:52 +0000

    Conveniece container for cached access of address related data

---
+++ gnumed/gnumed/client/wxpython/gmCachedAddress.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+#############################################################################
+#
+# gmCachedAddress - data broker for a person's address
+# ---------------------------------------------------------------------------
+#
+# @author: Dr. Horst Herb
+# @copyright: author
+# @license: GPL (details at http://www.gnu.org)
+# @dependencies: nil
+# @change log:
+#	10.03.2002 hherb first draft, largely untested
+#
+# @TODO: Almost everything
+############################################################################
+
+import gmDBCache, gmPG
+
+class AddressCache(gmDBCache.DBcache):
+	pass
+
+class CachedAddress(gmDBCache.CachedDBObject):
+	__dbcache = AddressCache()
+
+	def __init__(self, db=None):
+		#reference our class hierarchy level "singleton" cache
+		self.cache = CachedAddress.__dbcache
+		#make sure we allocte the default database connection
+		#in case no connection has been passed as parameter
+		if db is None and self.cache.db is None:
+			conn = gmPG.ConnectionPool()
+			self.cache.db = conn.GetConnection('geographica')
+
+		gmDBCache.CachedDBObject.__init__(self, self.cache, id=-1, db=db,
+		                          querystr="select * from v_basic_address where id = %d")
+
+	def get(self, id=None, by_reference=0, refresh_only=0):
+		#allow only searches when the id is available
+		assert(self.cache.id is not None or id is not None)
+		return gmDBCache.CachedDBObject.get(self, id, by_reference, refresh_only)
+
+	def dictresult(self, id=None):
+		if id is not None:
+			self.get(id, refresh_only=1)
+		try:
+			return gmDBCache.CachedDBObject.dictresult(self)[0]
+		except:
+			return None
+
+
+
+if __name__ == "__main__":
+	p = CachedAddress()
+	(data,) = p.get(id=1)
+	for a in data:
+		print a
+	print p.dictresult()
+
