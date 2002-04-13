3b15ee90aea673d8bc6c0aee8333a788681dc515
sjtan <sjtan>
2002-04-13 12:48:23 +0000

    altered allergy view to conform to phx_event display

--- gnumed/gnumed/test-area/clin_summary/sql/allergy_view.sql
+++ gnumed/gnumed/test-area/clin_summary/sql/allergy_view.sql
@@ -1,3 +1,3 @@
 drop view allergy_view;
- create view allergy_view as select  to_char(started, 'DD-MON-YYYY') as date, drug, description, patient as patient_id, clin_id from allergy;  
+ create view allergy_view as select  to_char(started, 'DD-MON-YYYY') as date,  drug ,extra,   patient as patient_id, clin_id from allergy;  
 
