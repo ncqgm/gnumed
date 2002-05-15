0e2885693f7492540293b533141cbf7b320b1345
ihaywood <ihaywood>
2002-05-15 11:45:20 +0000

    Working on loadable plugins

--- gnumed/gnumed/client/wxpython/gmPlugin.py
+++ gnumed/gnumed/client/wxpython/gmPlugin.py
@@ -14,9 +14,25 @@
 # @TODO: Almost everything
 ############################################################################
 
-import gmExceptions 
+import gmExceptions, os, inspect
+from gmLog import gmDefLog
 
 class gmPlugin:
+	# these are class variables which provide clues to the
+	# configuration tool to make sure the set of plugins installed
+	# are valid (plugins have the plugins they rely on installed
+	# first, no conflict in installation occurs)
+	"""Requirements for the plugin.
+	This is  a list of the plugin classes, an instance of which
+	must be installed before this plugin can be installed. Thus, a
+	hierarchy of requirements is formed, like Debian packages.
+	"""
+	requires = []
+
+	"""Set to true if only one instance of this class may be installed"""
+	single = 0
+
+	
 	"base class for all gnumed plugins"
 	def __init__(self, name):
 		self.__name = name
@@ -30,19 +46,101 @@ class gmPlugin:
 	def unregister(self):
 		raise gmExceptions.PureVirtualFunction()
 
-
-
-class wxGuiPlugin(gmPlugin):
+class wxBasePlugin (gmPlugin):
+	
 	"base class for all plugins providing wxPython widgets"
-	def __init__(self, name, guibroker=None, callbackbroker=None, dbbroker=None):
+	def __init__(self, name, guibroker=None, callbackbroker=None,
+	dbbroker=None, params=None):
 		gmPlugin.__init__(self, name)
 		self.__gb = guibroker
 		self.__cb = callbackbroker
 		self.__db = dbbroker
 
-	def getMainWidget(self):
-		raise gmExceptions.PureVirtualFunction()
 
+"Layout manager creating base windows and placing other plugins"
+class wxLayoutManager (wxBasePlugin):
+	"""Only one layout manager can be installed"""
+	single = 1
+
+
+	def __init__(self, name, guibroker=None, callbackbroker=None,
+	dbbroker=None, params=None):
+		wxBasePlugin.__init__ (self, name, guibroker,
+	callbackbroker, dbbroker, params)
+
+	def register ():
+		__gb['layoutmanager'] = self
+
+	"called by wxGuiPlugin.register to add plugin"
+	def AddPlugin (self, plugin):
+		gmExceptions.PureVirtualFunction ()
+
+"Base class for gui elements"
+class wxGuiPlugin (wxBasePlugin):
+	requires = [wxLayoutManager]
+
+	def __init__(self, name, guibroker=None, callbackbroker=None,
+	dbbroker=None, params=None):
+		wxBasePlugin.__init__ (self, name, guibroker,
+	callbackbroker, dbbroker, params)
+
+	def register ():
+		self.__gb['layoutmanager'].AddPlugin (self)
+
+	"Returns the wxPanel for display by the layout manager "
+	def getMainWidget (self):
+		gmExceptions.PureVirtualFunction ()
+
+	"""Returns one of gmPP_SIDE (a small window on the
+	side), gmPP_MAIN (the main window), gmPP_POPUP) a popup window)"""
+	def PositionPreference (self):
+		gmExceptions.PureVirtualFunction ()
+
+	"Returns wxConfiguration for configuration window"
+	def GetConfiguration (self):
+		return None
+
+	"Returns a wxIcon to represent this widget on a toolbar"
+	def GetIcon (self):
+		return None
+
+"""Class representing contents of a configuration box for a plugin. Does
+not have OK and cancel buttons"""
+class wxPluginConfiguration (wxPanel):
+	"""Returns config string when OK pressed. Widget destroyed
+	for cancel"""
+	def GetNewConfigString (self):
+		gmExceptions.PureVirtualFunction ()
+
+"A cryptographic algorithm"
+class gmCrypto (gmPlugin):
+	def __init__ (self, name):
+		gmPlugin.__init__(self, name)
+	
+	def encrypt (self, plaintext, key):
+		gmExceptions.PureVirtualFunction ()
+
+	def decrypt (self, cyphertext, key):
+		gmExceptions.PureVirtualFunction ()
+		
+"A plugin which uses crypto algorithms"
+class gmCryptoUser ():
+	def LoadAlgorithms ():
+		self.algorithm = {}
+		for file in os.listdir ('crypto'):
+			if file[-3:] == '.py':
+				# found python file!
+				try:
+					crypto_name = file[:-3]
+					aPlugin = __import__('crypto.' + crypto_name)
+					for plugin_class in inspect.getmembers (aPlugin, inspect.isclass):
+						if issubclass (plugin_class, gmCrypto):
+							self.algorithm[crypto_name] = plugin_class (crypto_name)
+			
+				except:
+					gmDefLog (lErr, "can't load file: %s" % file)
+				
+			
 
 if __name__ == "__main__":
 
---
+++ gnumed/gnumed/server/sql/gmmodule.sql
@@ -0,0 +1,48 @@
+-- project: GNUMed
+-- database: module
+-- purpose: module information and implementation 
+-- author: hherb
+-- copyright: Dr. Horst Herb, horst@hherb.com
+-- license: GPL (details at http://gnu.org)
+-- version: 0.4
+-- changelog:
+-- 15.5.02:  (ihaywood) first version
+
+
+
+-- different clients
+CREATE TABLE client (
+       id SERIAL,
+       name VARCHAR (20)
+);
+
+
+-- modules of the client, including reference source and object code
+CREATE TABLE modules (
+       id SERIAL,
+       client INTEGER REFERENCES client (id),
+       filename VARCHAR (256) NOT NULL,
+       modulename VARCHAR (256),
+       -- name of the module for internal loading, so the file
+       -- plugin/Foo.py would be plugin.Foo. Only an issue for 
+       -- Python and Java languages.
+       version VARCHAR (10) NOT NULL,
+       date DATE DEFAULT now (),
+       source TEXT,
+       object TEXT
+       -- code to be downloaded and executed
+       -- NULL here means delete the file
+);
+
+-- configuration table for the plugins
+CREATE plugin (
+       id SERIAL,
+       profile CHAR(25) DEFAULT 'default',
+       username CHAR(25) DEFAULT CURRENT_USER,
+       file INTEGER REFERENCES modules (id),
+       loadorder INTEGER NOT NULL, -- set by configuration module, 
+				   -- required modules MUST precede
+				   -- this module, configs job to check this
+       param TEXT -- parameters in the URL format: foo=bar&a=b
+);
+       
