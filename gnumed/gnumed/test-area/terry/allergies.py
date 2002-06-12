84f862c72e099af14a840d16c4f69dcaf217defb
ihaywood <ihaywood>
2002-06-12 01:44:59 +0000

    *** empty log message ***

--- gnumed/gnumed/test-area/terry/allergies.py
+++ gnumed/gnumed/test-area/terry/allergies.py
@@ -29,7 +29,14 @@ import gmGuiElement_HeadingCaptionPanel        #panel class to display top headi
 import gmGuiElement_DividerCaptionPanel        #panel class to display sub-headings or divider headings 
 import gmGuiElement_AlertCaptionPanel          #panel to hold flashing alert messages
 ID_ALLERGYLIST = wxNewId()
-
+#------------------------------------
+#Dummy data to simulate allergy items
+#------------------------------------
+allergydata = {
+1 : ("penicillin","Allergy", "definate","amoxycillin trihydrate","anaphylaxis"),
+2 : ("macrolides","Sensitivity","definate", "erythromycin ethyl succinate", "nausea and vomiting"),
+3 : ( "celecoxib","Allergy","definate","celecoxib", "allergic drug rash"),
+}
 class MyFrame(wxFrame):
      def __init__(self,parent,ID,title,position,size,style):
           wxFrame.__init__(self,parent,ID,title,
@@ -46,10 +53,10 @@ class MyFrame(wxFrame):
           self.dummypanel = wxPanel(self,-1,wxDefaultPosition,wxDefaultSize,0)
           self.messagetxt = wxTextCtrl(self,-1,
                         "Hi Guys, this is a prototype Allergy Panel. Comments please to rterry@gnumed.net..\n\n"
-		        "The information in the lists and text boxes below is completely dummy stuff.\n\n"
-		          "When Karsten and Ian finish the wxWordWheel.py then I'll be\n\n"
-		          "able to finish designing the editing area!!!! \n\n"
-                                      "which by rights, should occupy the space this text box does \n\n"
+		        "The information in the lists and text boxes below is completely dummy stuff.\n"
+		          "When Karsten and Ian finish the wxWordWheel.py then I'll be\n"
+		          "able to finish designing the editing area!!!! \n"
+                                      "which by rights, should occupy the space this text box does \n\N"
      		 	  "View this screen about 1/2 the size of your monitor width. Bye for now...\n\n",  
                                        size = (200,100), style=wxTE_MULTILINE)
           
@@ -57,7 +64,7 @@ class MyFrame(wxFrame):
           #-----------------------------------------------
           #add the divider headings below the editing area
           #-----------------------------------------------
-          self.drug_subheading = gmGuiElement_DividerCaptionPanel.DividerCaptionPanel(self,-1,"Drug and Reaction")
+          self.drug_subheading = gmGuiElement_DividerCaptionPanel.DividerCaptionPanel(self,-1,"Allergy and Sensitivity - Summary")
           self.sizer_divider_drug_generic = wxBoxSizer(wxHORIZONTAL) 
           self.sizer_divider_drug_generic.Add(self.drug_subheading,1, wxEXPAND)
           #--------------------------------------------------------------------------------------                                                                               
@@ -69,18 +76,39 @@ class MyFrame(wxFrame):
           # const wxValidator& validator = wxDefaultValidator, const wxString& name = "listCtrl")
 	  #
           #--------------------------------------------------------------------------------------
-          #self.allergy_list  = wxListCtrl(self,ID_ALLERGYLIST, wxDefaultPosition, wxDefaultSize, style = 0)
-	  #--------------------------------------------------------------------------
-	  # OR: Add a simple listcontrol under that for recall items and insert dummy data
-	  #--------------------------------------------------------------------------
-	  allergysamplelist =['Allergy        -Definate-  Celecoxib   Generic specific Uricaria',
-		       "Sensitivity    -Definate-   Macrolides   Class reaction    vomiting"]
-
-	  self.allergy_list = wxListBox(self,-1,wxDefaultPosition, wxDefaultSize, allergysamplelist,
-                            wxLB_SINGLE)
-          #list_allergy.SetForegroundColour(wxColor(255,0,0))
-	  self.allergy_list.SetFont(wxFont(14,wxSWISS, wxNORMAL, wxNORMAL, false, 'xselfont'))
-          #--------------------------------------------------------------------------------------
+       	  self.list_allergy = wxListCtrl(self, ID_ALLERGYLIST,  wxDefaultPosition, wxDefaultSize,wxLC_REPORT|wxLC_NO_HEADER|wxSUNKEN_BORDER)
+          #self.list_allergy.SetForegroundColour(wxColor(131,129,131))	
+	  self.list_allergy.SetFont(wxFont(12,wxSWISS, wxNORMAL, wxNORMAL, false, 'xselfont'))
+          #----------------------------------------	  
+          # add some dummy data to the allergy list
+	  self.list_allergy.InsertColumn(0, "Type")
+	  self.list_allergy.InsertColumn(1, "Status")
+	  self.list_allergy.InsertColumn(2, "Class")
+	  self.list_allergy.InsertColumn(3, "Generic")
+	  self.list_allergy.InsertColumn(4, "Reaction")
+     
+	  #-------------------------------------------------------------
+	  #loop through the scriptdata array and add to the list control
+	  #note the different syntax for the first coloum of each row
+	  #i.e. here > self.list_allergy.InsertStringItem(x, data[0])!!
+	  #-------------------------------------------------------------
+	  items = allergydata.items()
+	  for x in range(len(items)):
+	      key, data = items[x]
+	      print items[x]
+	      #print x, data[0],data[1],data[2]
+	      self.list_allergy.InsertStringItem(x, data[0])
+	      self.list_allergy.SetStringItem(x, 1, data[1])
+	      self.list_allergy.SetStringItem(x, 2, data[2])
+	      self.list_allergy.SetStringItem(x, 3, data[3])
+	      self.list_allergy.SetStringItem(x, 4, data[4])
+	      self.list_allergy.SetItemData(x, key)
+	  self.list_allergy.SetColumnWidth(0, wxLIST_AUTOSIZE)
+          self.list_allergy.SetColumnWidth(1, wxLIST_AUTOSIZE)
+	  self.list_allergy.SetColumnWidth(2, wxLIST_AUTOSIZE)
+	  self.list_allergy.SetColumnWidth(3, wxLIST_AUTOSIZE)
+	  self.list_allergy.SetColumnWidth(4, wxLIST_AUTOSIZE)
+	  #--------------------------------------------------------------------------------------
           #add a richtext control or a wxTextCtrl multiline to display the class text information
           #e.g. would contain say information re the penicillins
           #--------------------------------------------------------------------------------------
@@ -90,7 +118,7 @@ class MyFrame(wxFrame):
  
                                         
                                           size=(200, 100), style=wxTE_MULTILINE)
-	  self.classtxt.SetFont(wxFont(14,wxSWISS,wxNORMAL,wxNORMAL,false,'xselfont'))
+	  self.classtxt.SetFont(wxFont(12,wxSWISS,wxNORMAL,wxNORMAL,false,'xselfont'))
           #----------------------------------------
           #add an alert caption panel to the bottom
           #----------------------------------------
@@ -104,7 +132,7 @@ class MyFrame(wxFrame):
           self.mainsizer.Add(self.messagetxt,4,wxEXPAND)
           self.mainsizer.Add(self.dummypanel2,1,wxEXPAND)
           self.mainsizer.Add(self.sizer_divider_drug_generic,0,wxEXPAND)
-          self.mainsizer.Add(self.allergy_list,4,wxEXPAND)
+          self.mainsizer.Add(self.list_allergy,4,wxEXPAND)
           self.mainsizer.Add(self.classtext_subheading,0,wxEXPAND)
           self.mainsizer.Add(self.classtxt,4,wxEXPAND)
           self.mainsizer.Add(self.alertpanel,0,wxEXPAND)
@@ -119,7 +147,7 @@ class MyFrame(wxFrame):
 class App(wxApp):
      def OnInit(self):
          #frame = MyFrame(NULL,-1,"Allergies")
-         frame = MyFrame(NULL, -1, "gnuMEdGP_PreAlphaGUI__AllergyPanel_V0.0.1", 
+         frame = MyFrame(NULL, -1, "gnuMEdGP_PreAlphaGUI__AllergyPanel_V0.0.2", 
 		                           wxDefaultPosition, size = wxSize(600,500),
 		                           style= wxDEFAULT_FRAME_STYLE|wxNO_FULL_REPAINT_ON_RESIZE)
          frame.Show (true)
