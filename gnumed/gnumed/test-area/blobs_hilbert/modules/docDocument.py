b423908a2ddbd580417fbeb28bf037afd5729450
Karsten Hilbert <karsten.hilbert@gmx.net>
2002-06-15 17:42:59 +0000

    - changed tempfile prefix from "@$pid" to "doc-"

--- gnumed/gnumed/test-area/blobs_hilbert/modules/docDocument.py
+++ gnumed/gnumed/test-area/blobs_hilbert/modules/docDocument.py
@@ -6,7 +6,7 @@
 """
 #=======================================================================================
 __author__ = "Karsten Hilbert <Karsten.Hilbert@gmx.net>"
-__version__ = "$Revision: 1.1 $"
+__version__ = "$Revision: 1.2 $"
 
 import os.path, fileinput, string, types, sys, tempfile, os
 import gmLog
@@ -224,6 +224,7 @@ class cDocument:
 
 	# if None -> use tempfile module default, else use that path as base directory for temp files
 	tempfile.tempdir = aTempDir
+	tempfile.template = "doc-"
 
 	# start our transaction (done implicitely by defining a cursor)
 	cursor = aConn.cursor()
@@ -517,25 +518,3 @@ class cPatientDocumentList:
 
 	return tmp.getMetaData()
 #----------------------------------------------------------------------------
-def guess_mimetype(aFileName = None):
-    """Guess mime type of arbitrary file.
-
-    Perhaps this should be moved elsewhere (another module).
-    """
-    # sanity check
-    if aFileName == None:
-	__log__.Log(gmLog.lErr, "Cannot guess mimetypes if I don't have any file to guess on.")
-	return None
-
-    # this only works on POSIX with 'file' installed (which is standard, however)
-    if os.name == 'posix':
-	aPipe = os.popen("file -i -b %s" % aFileName)
-	mime_type = string.replace(aPipe.readline(), "\n", "")
-	aPipe.close()
-    # sorry, no mime types on lesser operating systems ...
-    # please send me your improvements
-    else:
-	__log__.Log(gmLog.lWarn, "No mime type detection available on this system. Please send your suggestions.")
-	mime_type = 'application/octet-stream'
-
-    return mime_type
