bf7afcf1d89b4b7f2663823536d6c5f276f6e619
ihaywood <ihaywood>
2002-06-22 07:28:07 +0000

    plugin architecture. Works with Richards client

--- gnumed/gnumed/client/wxpython/gmPlugin.py
+++ gnumed/gnumed/client/wxpython/gmPlugin.py
@@ -15,7 +15,7 @@
 ############################################################################
 
 import gmExceptions, os, inspect
-from gmLog import gmDefLog
+import gmLog
 from wxPython.wx import *
 
 class gmPlugin:
@@ -23,27 +23,34 @@ class gmPlugin:
 	# configuration tool to make sure the set of plugins installed
 	# are valid (plugins have the plugins they rely on installed
 	# first, no conflict in installation occurs)
-	"""Requirements for the plugin.
-	This is  a list of the plugin classes, an instance of which
-	must be installed before this plugin can be installed. Thus, a
-	hierarchy of requirements is formed, like Debian packages.
-	"""
-	requires = []
-
-	"""Set to true if only one instance of this class may be installed"""
-	single = 0
 
 	
 	"base class for all gnumed plugins"
-	def __init__(self, name):
-		self.__name = name
 
 	def name(self):
-		return self.__name
-		
+		return None
 
+	def provides ():
+		"""
+		Returns a list of services that the plugin provides
+		"""
+		return []
+	
+	def requires ():
+		"""
+		Requires a list of services that must be registered
+		before this plugin is registered. The configuration
+		tool must check these and make sure the load order
+		satisfies the plugins' requirements
+		"""
+		return []
 
-	def register(self, parentwidget=None):
+	def description ():
+		"""
+		Returns a brief description of the plugin
+		"""
+
+	def register(self):
 		raise gmExceptions.PureVirtualFunction()
 
 	def unregister(self):
@@ -51,110 +58,53 @@ class gmPlugin:
 
 class wxBasePlugin (gmPlugin):
 	
-	"base class for all plugins providing wxPython widgets"
-	def __init__(self, name, guibroker=None, callbackbroker=None,
+	"base class for all plugins providing wxPython widgets."
+	# NOTE: I anticipate that all plugins will in fact be derived
+	# from this class. Without the brokers a plugin is useless (IH)
+	def __init__(self, guibroker=None, callbackbroker=None,
 	dbbroker=None, params=None):
-		gmPlugin.__init__(self, name)
-		self.__gb = guibroker
-		self.__cb = callbackbroker
-		self.__db = dbbroker
+		self.gb = guibroker
+		self.cb = callbackbroker
+		self.db = dbbroker
 
-	def register(self):
-		raise gmExceptions.PureVirtualFunction()
-
-
-"Layout manager creating base windows and placing other plugins"
-class wxLayoutManager (wxBasePlugin):
-	"""Only one layout manager can be installed"""
-	single = 1
-
-
-	def __init__(self, name, guibroker=None, callbackbroker=None,
-	dbbroker=None, params=None):
-		wxBasePlugin.__init__ (self, name, guibroker,
-	callbackbroker, dbbroker, params)
-
-	def register ():
-		__gb['layoutmanager'] = self
-
-	"called by wxGuiPlugin.register to add plugin"
-	def AddPlugin (self, plugin):
-		gmExceptions.PureVirtualFunction ()
-
-"Base class for gui elements"
-class wxGuiPlugin (wxBasePlugin):
-	requires = [wxLayoutManager]
-
-	def __init__(self, name, guibroker=None, callbackbroker=None,
-	dbbroker=None, params=None):
-		wxBasePlugin.__init__ (self, name, guibroker,
-	callbackbroker, dbbroker, params)
-
-	def register ():
-		self.__gb['layoutmanager'].AddPlugin (self)
-
-	"Returns the wxPanel for display by the layout manager "
-	def getMainWidget (self):
-		gmExceptions.PureVirtualFunction ()
-
-	"""Returns one of gmPP_SIDE (a small window on the
-	side), gmPP_MAIN (the main window), gmPP_POPUP) a popup window)"""
-	def PositionPreference (self):
-		gmExceptions.PureVirtualFunction ()
-
-	"Returns wxConfiguration for configuration window"
-	def GetConfiguration (self):
+	def GetConfigPanel (self):
+		"""
+		Returns an instance of wxPluginConfig
+		for the configuration tool
+		"""
 		return None
 
-	"Returns a wxIcon to represent this widget on a toolbar"
-	def GetIcon (self):
-		return None
-
-"""Class representing contents of a configuration box for a plugin. Does
-not have OK and cancel buttons"""
-class wxPluginConfiguration (wxPanel):
-	"""Returns config string when OK pressed. Widget destroyed
-	for cancel"""
-	def GetNewConfigString (self):
-		gmExceptions.PureVirtualFunction ()
-
-"A cryptographic algorithm"
-class gmCrypto (gmPlugin):
-	def __init__ (self, name):
-		gmPlugin.__init__(self, name)
-
-	def encrypt (self, plaintext, key):
-		gmExceptions.PureVirtualFunction ()
-
-	def decrypt (self, cyphertext, key):
-		gmExceptions.PureVirtualFunction ()
-
-"A plugin which uses crypto algorithms"
-class gmCryptoUser:
-	def LoadAlgorithms ():
-		self.algorithm = {}
-		for file in os.listdir ('crypto'):
-			if file[-3:] == '.py':
-				# found python file!
-				try:
-					crypto_name = file[:-3]
-					aPlugin = __import__('crypto.' + crypto_name)
-					for plugin_class in inspect.getmembers (aPlugin, inspect.isclass):
-						if issubclass (plugin_class, gmCrypto):
-							self.algorithm[crypto_name] = plugin_class (crypto_name)
-
-				except:
-					gmDefLog (lErr, "can't load file: %s" % file)
-
-
-
-if __name__ == "__main__":
-
-	plugin = gmPlugin("A plugin")
-	print "Plugin installed: ", plugin.name()
-
-	print "This should throw an exception:"
-	plugin.register()
+def LoadPluginSet (set, guibroker, dbbroker, defaults):
+	"""
+	Loads a set of plugins.
+	set is a string specifying the subdirectory in which to
+	find the plugins.
+	There will be a general 'gui' directory for large GUI
+	components: prescritions, etc., then several others for more
+	specific types: export/import filters, crypto algorithms
+	guibroker, dbbroker are broker objects provided
+	defaults are the default set of plugins to be loaded
+	(TODO: get plugin list from gmconfiguration for this user).
+	"""
+	toload = defaults
+	# talk to database here instead
+	dict = {}
+	for name in toload:
+		try:
+			set_module = __import__ ("%s.%s" % (set, name))
+			plugin_module = set_module.__dict__[name]
+			plugin_class = plugin_module.__dict__[name]
+			if issubclass (plugin_class, wxBasePlugin):
+				plugin = plugin_class (guibroker = guibroker, dbbroker = dbbroker)
+				plugin.register ()
+				dict[plugin.name ()] = plugin
+				gmLog.gmDefLog.Log (gmLog.lInfo, "registing plugin %s" % plugin.name ())
+			else:
+				gmLog.gmDefLog.Log (lPanic, "class %s is not a subclass of wxBasePlugin" % name)
+		except:
+			gmLog.gmDefLog.Log (gmLog.lPanic,
+				  "cannot load module %s.%s" % (set, name))
+	guibroker['modules.%s' % set] = dict # keep a record of plugins	
 
 
 #####################################################################
