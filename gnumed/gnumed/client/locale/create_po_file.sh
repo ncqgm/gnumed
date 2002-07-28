99d5315cc5e0be7ba4e23d25381afa00c4ea7352
Karsten Hilbert <karsten.hilbert@gmx.net>
2002-07-28 14:53:11 +0000

    - force *.po instead of *.pot

--- gnumed/gnumed/client/locale/create_po_file.sh
+++ gnumed/gnumed/client/locale/create_po_file.sh
@@ -2,9 +2,9 @@
 
 # this tool can be used to generate a *.po file from all the gnumed client source
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/locale/Attic/create_po_file.sh,v $
-# $Revision: 1.1 $
+# $Revision: 1.2 $
 
 # where to look for files
 BASE="../"
 
-find $BASE -name '*.py' -print0 | xargs -0 pygettext.py -v -d "gnumed" "-"
+find $BASE -name '*.py' -print0 | xargs -0 pygettext.py -v -o gnumed.po "-"
