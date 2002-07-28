6429473a05a4ad10a7444afd0616ec882719ae80
Karsten Hilbert <karsten.hilbert@gmx.net>
2002-07-28 11:33:29 +0000

    - use this to create gnumed.po for translators - initial release

---
+++ gnumed/gnumed/client/locale/create_po_file.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# this tool can be used to generate a *.po file from all the gnumed client source
+# $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/locale/Attic/create_po_file.sh,v $
+# $Revision: 1.1 $
+
+# where to look for files
+BASE="../"
+
+find $BASE -name '*.py' -print0 | xargs -0 pygettext.py -v -d "gnumed" "-"
