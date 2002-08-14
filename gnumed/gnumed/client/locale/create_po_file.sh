65912a43252d9b99b55c4a9c64a69481f7720be5
Karsten Hilbert <karsten.hilbert@gmx.net>
2002-08-14 09:38:30 +0000

    - follow symlinks, too

--- gnumed/gnumed/client/locale/create_po_file.sh
+++ gnumed/gnumed/client/locale/create_po_file.sh
@@ -3,7 +3,7 @@
 # this tool can be used to generate a *.po file from all the gnumed client source
 
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/locale/Attic/create_po_file.sh,v $
-# $Revision: 1.3 $
+# $Revision: 1.4 $
 
 # where to look for files
 BASE="$1"
@@ -11,4 +11,4 @@ BASE="$1"
 PONAME="$2"
 
 echo "harvesting python source files in ${BASE} into the file ${2}.po"
-find ${BASE} -name '*.py' -print0 | xargs -0 pygettext.py -v -o ${2}.po "-"
+find ${BASE} -follow -name '*.py' -print0 | xargs -0 pygettext.py -v -o ${2}.po "-"
