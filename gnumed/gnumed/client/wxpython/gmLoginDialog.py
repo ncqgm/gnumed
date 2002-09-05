2d7207d7a853d4ab864e31429c667ccf43461aae
hinnef <hinnef>
2002-09-05 22:18:20 +0000

    new: advanced options hidden in popup window

--- gnumed/gnumed/client/wxpython/gmLoginDialog.py
+++ gnumed/gnumed/client/wxpython/gmLoginDialog.py
@@ -26,7 +26,7 @@ It features combo boxes which "remember" any number of previously entered settin
 
 from wxPython.wx import *
 import os.path
-import gmLoginInfo, gmGuiMain, gmGuiBroker, gmCfg
+import gmLoginInfo, gmGuiMain, gmGuiBroker, gmCfg, time
 _cfg = gmCfg.gmDefCfgFile
 
 #############################################################################
@@ -173,6 +173,24 @@ class LoginPanel(wxPanel):
 		except:
 			self.topsizer.Add(wxStaticText (self, -1, "Cannot find image" + bitmap, style=wxALIGN_CENTRE), 0, wxGROW|wxALIGN_CENTER_VERTICAL|wxALL, 10)
 
+    	    	# add some infos on top of the login prompts
+		self.infobox = wxStaticBox( self, -1,'')
+		self.infoboxsizer = wxStaticBoxSizer( self.infobox, wxVERTICAL )
+    	    	
+		# current date
+    	    	date = time.strftime("%d.%m.%Y")
+		self.infoboxsizer.Add(wxStaticText (self, -1, _("Date: ") + date, style=wxALIGN_LEFT), 0,wxGROW|wxALIGN_CENTRE_VERTICAL|wxALL,10)
+
+    	    	# workplace
+    	    	workplace  = "Desktop Dr.Jekyll"
+		self.infoboxsizer.Add(wxStaticText (self, -1, _("Workplace: ") + workplace, style=wxALIGN_LEFT), 0, wxGROW|wxALIGN_CENTRE_VERTICAL|wxALL, 10)
+
+    	    	#  who to contact in case of problems with gnumed (local service provider etc.)
+       	    	contactinfo  = "0815-HELPME"
+		self.infoboxsizer.Add(wxStaticText (self, -1, _("Contact: ") + contactinfo, style=wxALIGN_LEFT), 0, wxGROW|wxALIGN_CENTRE_VERTICAL|wxALL, 10)
+    	    	self.infoboxsizer.Fit(self)
+
+    	    	# start getting login parameters
 		self.paramsbox = wxStaticBox( self, -1, _("Login Parameters"))
 		self.paramsboxsizer = wxStaticBoxSizer( self.paramsbox, wxVERTICAL )
 
@@ -199,34 +217,6 @@ class LoginPanel(wxPanel):
 		self.pwdentry = wxTextCtrl( self, 1, '', wxDefaultPosition, wxSize(80,-1), wxTE_PASSWORD )
 		self.pboxgrid.AddWindow( self.pwdentry, 0, wxGROW|wxALIGN_CENTER_VERTICAL|wxALL, 5 )
 
-		#DATABASE COMBO
-		label = wxStaticText( self, -1, _("database"), wxDefaultPosition, wxDefaultSize, 0 )
-		self.pboxgrid.AddWindow( label, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5 )
-		self.dbcombo = wxComboBox( self, -1, self.loginparams.databaselist[0], wxDefaultPosition, wxSize(100,-1),
-			self.loginparams.databaselist , wxCB_DROPDOWN )
-		self.pboxgrid.AddWindow( self.dbcombo, 0, wxGROW|wxALIGN_CENTER_VERTICAL|wxALL, 5 )
-
-		#HOST NAME / IP NUMBER COMBO
-		label = wxStaticText( self, -1, _("host"), wxDefaultPosition, wxDefaultSize, 0 )
-		self.pboxgrid.AddWindow( label, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5 )
-		self.hostcombo = wxComboBox( self, -1, self.loginparams.hostlist[0], wxDefaultPosition, wxSize(100,-1),
-			self.loginparams.hostlist , wxCB_DROPDOWN )
-		self.pboxgrid.AddWindow( self.hostcombo, 0, wxGROW|wxALIGN_CENTER_VERTICAL|wxALL, 5 )
-
-		#PORT NUMBER COMBO
-		label = wxStaticText( self, -1, _("port"), wxDefaultPosition, wxDefaultSize, 0 )
-		self.pboxgrid.AddWindow( label, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5 )
-		self.portcombo = wxComboBox( self, -1, self.loginparams.portlist[0], wxDefaultPosition, wxSize(100,-1),
-			self.loginparams.portlist , wxCB_DROPDOWN )
-		self.pboxgrid.AddWindow( self.portcombo, 0, wxGROW|wxALIGN_CENTER_VERTICAL|wxALL, 5 )
-
-		#DATABASE BACKEND OPTIONS COMBO
-		label = wxStaticText( self, -1, _("backend options"), wxDefaultPosition, wxDefaultSize, 0 )
-		self.pboxgrid.AddWindow( label, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5 )
-		self.beoptioncombo= wxComboBox( self, -1, self.loginparams.backendoptionlist[0], wxDefaultPosition, wxSize(100,-1),
-			self.loginparams.backendoptionlist , wxCB_DROPDOWN )
-		self.pboxgrid.AddWindow( self.beoptioncombo, 0, wxGROW|wxALIGN_CENTER_VERTICAL|wxALL, 5 )
-
 		self.buttonsizer = wxBoxSizer( wxHORIZONTAL )
 
 		ID_BUTTON_LOGIN = wxNewId()
@@ -234,9 +224,11 @@ class LoginPanel(wxPanel):
 		button.SetDefault()
 		self.buttonsizer.AddWindow( button, 0, wxALIGN_CENTRE|wxALL, 5 )
 
-		ID_BUTTON_SAVECONF = wxNewId()
-		button = wxButton( self, ID_BUTTON_SAVECONF, _("&Save conf."), wxDefaultPosition, wxDefaultSize, 0 )
-		button.SetToolTip( wxToolTip(_("Save current entries in configuration file")) )
+		self.buttonsizer.AddSpacer( 10, 20, 0, wxALIGN_CENTRE|wxALL, 5 )
+
+		ID_BUTTON_OPTIONS = wxNewId()
+		button = wxButton( self, ID_BUTTON_OPTIONS, _("&Options"), wxDefaultPosition, wxDefaultSize, 0 )
+		button.SetToolTip( wxToolTip(_("Set advanced options like database, host, port etc.")) )
 		self.buttonsizer.AddWindow( button, 0, wxALIGN_CENTRE|wxALL, 5 )
 
 		self.buttonsizer.AddSpacer( 10, 20, 0, wxALIGN_CENTRE|wxALL, 5 )
@@ -252,6 +244,7 @@ class LoginPanel(wxPanel):
 		self.buttonsizer.AddWindow( button, 0, wxALIGN_CENTRE|wxALL, 5 )
 
 		self.paramsboxsizer.AddSizer(self.pboxgrid, 1, wxGROW|wxALL, 10)
+		self.topsizer.AddSizer(self.infoboxsizer, 0,wxGROW|wxALL, 10)
 		self.topsizer.AddSizer(self.paramsboxsizer, 1, wxGROW|wxALL, 10)
 		self.topsizer.AddSizer( self.buttonsizer, 0, wxGROW|wxALIGN_CENTER_VERTICAL|wxALL, 5 )
 
@@ -262,7 +255,7 @@ class LoginPanel(wxPanel):
 			self.topsizer.SetSizeHints( parent )
 
 		EVT_BUTTON(self, ID_BUTTON_HELP, self.OnHelp)
-		EVT_BUTTON(self, ID_BUTTON_SAVECONF, self.OnSaveConfiguration)
+		EVT_BUTTON(self, ID_BUTTON_OPTIONS, self.OnOptions)
 		EVT_BUTTON(self, ID_BUTTON_LOGIN, self.OnLogin)
 		EVT_BUTTON(self, ID_BUTTON_CANCEL, self.OnCancel)
 
@@ -295,45 +288,22 @@ class LoginPanel(wxPanel):
 		if not self.loginparams.backendoptionlist:
 			self.loginparams.backendoptionlist = ['']
 
-#		conf = wxFileConfig(self.confname, style=wxCONFIG_USE_LOCAL_FILE)
-#		self.loginparams.userlist = StringToList(conf.Read("/login/user"))
-#		self.loginparams.password = ''
-#		self.loginparams.databaselist = StringToList(conf.Read("login/database"))
-#		self.loginparams.hostlist = StringToList(conf.Read("/login/host"))
-#		self.loginparams.portlist = StringToList(conf.Read("/login/port"))
-#		self.loginparams.backendoptionlist = StringToList(conf.Read("/login/backendoption"))
 
 #############################################################################
 # Save all settings to the configuration file
 #############################################################################
 
-#	def SaveSettingsFor(self, conf, key, setting):
-		"""Append items not already existing in the configuration file
-		and make sure that changes already committed by other processes
-		using the same configuration file don't get overwritten"""
-
-#		conf.Write(key, AppendSettings(conf.Read(key), setting, self.maxitems))
-
 	def SaveSettings(self):
 		"""Save parameter settings to standard configuration file"""
 
 		_cfg.set('backend', 'logins', ComboBoxItems(self.usercombo))
-		_cfg.set('backend', 'databases', ComboBoxItems(self.dbcombo))
-		_cfg.set('backend', 'hosts', ComboBoxItems(self.hostcombo))
-		_cfg.set('backend', 'ports', ComboBoxItems(self.portcombo))
-		_cfg.set('backend', 'options', ComboBoxItems(self.beoptioncombo))
+		_cfg.set('backend', 'databases', self.loginparams.databaselist)
+		_cfg.set('backend', 'hosts', self.loginparams.hostlist)
+		_cfg.set('backend', 'ports', self.loginparams.portlist)
+		_cfg.set('backend', 'options', self.loginparams.backendoptionlist)
 
 		_cfg.store()
 
-		# set the location of the configuration file in a platform independent way
-#		conf = wxFileConfig(self.confname, style=wxCONFIG_USE_LOCAL_FILE)
-#		self.SaveSettingsFor(conf, "/login/user", ListToString(ComboBoxItems(self.usercombo)))
-#		self.SaveSettingsFor(conf, "/login/database", ListToString(ComboBoxItems(self.dbcombo)))
-#		self.SaveSettingsFor(conf, "/login/host", ListToString(ComboBoxItems(self.hostcombo)))
-#		self.SaveSettingsFor(conf, "/login/port", ListToString(ComboBoxItems(self.portcombo)))
-#		self.SaveSettingsFor(conf, "/login/backendoption", ListToString(ComboBoxItems(self.beoptioncombo)))
-		# make sure changes are written to disk immediately
-#		conf.Flush()
 
 #############################################################################
 # Retrieve current settings from user interface widgets
@@ -344,10 +314,7 @@ class LoginPanel(wxPanel):
 		if not self.cancelled:
 			self.loginparams.userlist = ComboBoxItems(self.usercombo)
 			self.loginparams.password = self.GetPassword()
-			self.loginparams.databaselist = ComboBoxItems(self.dbcombo)
-			self.loginparams.hostlist = ComboBoxItems(self.hostcombo)
-			self.loginparams.portlist = ComboBoxItems(self.portcombo)
-			self.loginparams.backendoptionlist = ComboBoxItems(self.beoptioncombo)
+    	    	    	# database, host, port and backend lists are updated by OptionWindow and should be up to date
 			return self.loginparams
 		else:
 			return None
@@ -383,28 +350,30 @@ class LoginPanel(wxPanel):
 		self.pwdentry.SetValue(pwd)
 
 	def GetDatabase(self):
-		return self.dbcombo.GetValue()
+		return self.loginparams.databaselist[0]
 
 	def SetDatabase(self, db):
-		self.dbcombo.SetValue(db)
+		self.loginparams.databaselist[0]
 
 	def GetHost(self):
-		return self.hostcombo.GetValue()
+		return self.loginparams.hostlist[0]
 
 	def SetHost(self, host):
-		self.dbcombo.SetValue(host)
+		self.loginparams.hostlist[0]
 
 	def GetBackendOptions(self):
-		return self.beoptioncombo.GetValue()
+		return self.loginparams.backendoptionlist[0]
 
 	def SetBackendOptions(self, opt):
-		self.beoptioncombo.SetValue(opt)
+		self.loginparams.backendoptionlist[0]
 
 	def GetPort(self):
-		return self.portcombo.GetValue()
+		return self.loginparams.portlist[0]
 
 	def SetPort(self, port):
-		self.portcombo.SetValue(port)
+		self.loginparams.portlist[0]
+
+
 
 #############################################################################
 # GUI action functions from here on
@@ -413,20 +382,26 @@ class LoginPanel(wxPanel):
 	def OnHelp(self, event):
 		wxMessageBox(_("Sorry, not implemented yet!"))
 
-
-	def OnSaveConfiguration(self, event):
-		self.SaveSettings()
-
+	def OnOptions(self, event):
+		self.optionwindow = OptionWindow(self,wxNewId(),loginparams=self.loginparams)
+    	    	self.optionwindow.ShowModal() 
+		if not self.optionwindow.panel.cancelled: 
+		    self.loginparams.databaselist = ComboBoxItems(self.optionwindow.panel.dbcombo)
+		    self.loginparams.hostlist = ComboBoxItems(self.optionwindow.panel.hostcombo)
+		    self.loginparams.portlist = ComboBoxItems(self.optionwindow.panel.portcombo)
+		    self.loginparams.backendoptionlist = ComboBoxItems(self.optionwindow.panel.beoptioncombo)
+		
 	def OnLogin(self, event):
 		self.cancelled = false
 		self.parent.Close()
-
 
 	def OnCancel(self, event):
 		self.cancelled = true
 		self.parent.Close()
 
-
+########################################################################################
+# LoginDialog - window holding LoginPanel
+########################################################################################
 
 class LoginDialog(wxDialog):
 	def __init__(self, parent, id, title=_("Login")):
@@ -435,6 +410,141 @@ class LoginDialog(wxDialog):
 		self.Fit () # needed for Windoze.
 		self.Centre()
 
+
+
+########################################################################################
+#  Advanced options window
+########################################################################################
+
+class OptionWindow(wxDialog):
+	def __init__(self, parent, id=wxNewId(), title=_("Advanced login options"),loginparams=None):
+		wxDialog.__init__(self, parent, id, title)
+		self.panel = OptionPanel(self, -1, isDialog=1,loginparams=loginparams)
+		self.Fit () # needed for Windoze.
+		self.Centre()
+    	    	
+
+class OptionPanel(wxPanel):
+	"GUI panel class that gets interactively advanced login parameters"
+
+	# constructor parameters:
+	#	loginparams: to override default login parameters 
+	#	             see class "LoginParameters" in this module
+	#	isDialog: if this panel is the main panel of a dialog, the panel will
+	#	          resize the dialog automatically to display everything neatly
+	#	          if isDialog is set to true
+	#	maxitems: maximum number of items "remembered" in the comboboxes
+
+	def __init__(self, parent, id, pos = wxPyDefaultPosition, size = wxPyDefaultSize, style = wxTAB_TRAVERSAL,
+		loginparams = None,
+		isDialog = 0,
+		maxitems = 8):
+
+		wxPanel.__init__(self, parent, id, pos, size, style)
+		self.parent = parent
+
+		#true if dialog was cancelled by user
+		self.cancelled = false
+
+		#true if this panel is displayed within a dialog (will resize the dialog automatically then)
+		self.isDialog = isDialog
+
+		#the number of items we allow to be stored in the comboboxes "history"
+		self.maxitems = maxitems
+
+		#login parameter settings should have been initialized by the parent window
+		self.loginparams = loginparams
+
+		self.topsizer = wxBoxSizer(wxVERTICAL)
+
+		self.paramsbox = wxStaticBox( self, -1, _("Advanced Login Parameters"))
+		self.paramsboxsizer = wxStaticBoxSizer( self.paramsbox, wxVERTICAL )
+
+		self.pboxgrid = wxFlexGridSizer( 4, 2, 5, 5 )
+		self.pboxgrid.AddGrowableCol( 1 )
+
+		#DATABASE COMBO
+		label = wxStaticText( self, -1, _("database"), wxDefaultPosition, wxDefaultSize, 0 )
+		self.pboxgrid.AddWindow( label, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5 )
+		self.dbcombo = wxComboBox( self, -1, self.loginparams.databaselist[0], wxDefaultPosition, wxSize(100,-1),
+			self.loginparams.databaselist , wxCB_DROPDOWN )
+		self.pboxgrid.AddWindow( self.dbcombo, 0, wxGROW|wxALIGN_CENTER_VERTICAL|wxALL, 5 )
+
+		#HOST NAME / IP NUMBER COMBO
+		label = wxStaticText( self, -1, _("host"), wxDefaultPosition, wxDefaultSize, 0 )
+		self.pboxgrid.AddWindow( label, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5 )
+		self.hostcombo = wxComboBox( self, -1, self.loginparams.hostlist[0], wxDefaultPosition, wxSize(100,-1),
+			self.loginparams.hostlist , wxCB_DROPDOWN )
+		self.pboxgrid.AddWindow( self.hostcombo, 0, wxGROW|wxALIGN_CENTER_VERTICAL|wxALL, 5 )
+
+		#PORT NUMBER COMBO
+		label = wxStaticText( self, -1, _("port"), wxDefaultPosition, wxDefaultSize, 0 )
+		self.pboxgrid.AddWindow( label, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5 )
+		self.portcombo = wxComboBox( self, -1, self.loginparams.portlist[0], wxDefaultPosition, wxSize(100,-1),
+			self.loginparams.portlist , wxCB_DROPDOWN )
+		self.pboxgrid.AddWindow( self.portcombo, 0, wxGROW|wxALIGN_CENTER_VERTICAL|wxALL, 5 )
+
+		#DATABASE BACKEND OPTIONS COMBO
+		label = wxStaticText( self, -1, _("backend options"), wxDefaultPosition, wxDefaultSize, 0 )
+		self.pboxgrid.AddWindow( label, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5 )
+		self.beoptioncombo= wxComboBox( self, -1, self.loginparams.backendoptionlist[0], wxDefaultPosition, wxSize(100,-1),
+			self.loginparams.backendoptionlist , wxCB_DROPDOWN )
+		self.pboxgrid.AddWindow( self.beoptioncombo, 0, wxGROW|wxALIGN_CENTER_VERTICAL|wxALL, 5 )
+
+		self.buttonsizer = wxBoxSizer( wxHORIZONTAL )
+
+		ID_BUTTON_OK = wxNewId()
+		button = wxButton( self, ID_BUTTON_OK, _("&Ok"), wxDefaultPosition, wxDefaultSize, 0 )
+		button.SetDefault()
+		self.buttonsizer.AddWindow( button, 0, wxALIGN_CENTRE|wxALL, 5 )
+
+		self.buttonsizer.AddSpacer( 10, 20, 0, wxALIGN_CENTRE|wxALL, 5 )
+
+		ID_BUTTON_HELP = wxNewId()
+		button = wxButton( self, ID_BUTTON_HELP, _("&Help"), wxDefaultPosition, wxDefaultSize, 0 )
+		self.buttonsizer.AddWindow( button, 0, wxALIGN_CENTRE|wxALL, 5 )
+
+		self.buttonsizer.AddSpacer( 10, 20, 1, wxALIGN_CENTRE|wxALL, 5 )
+
+		ID_BUTTON_CANCEL = wxNewId()
+		button = wxButton( self, ID_BUTTON_CANCEL, _("&Cancel"), wxDefaultPosition, wxDefaultSize, 0 )
+		self.buttonsizer.AddWindow( button, 0, wxALIGN_CENTRE|wxALL, 5 )
+
+		self.paramsboxsizer.AddSizer(self.pboxgrid, 1, wxGROW|wxALL, 10)
+		self.topsizer.AddSizer(self.paramsboxsizer, 1, wxGROW|wxALL, 10)
+		self.topsizer.AddSizer( self.buttonsizer, 0, wxGROW|wxALIGN_CENTER_VERTICAL|wxALL, 5 )
+
+		self.SetAutoLayout( true )
+		self.SetSizer( self.topsizer)
+		self.topsizer.Fit( self )
+		if self.isDialog:
+			self.topsizer.SetSizeHints( parent )
+
+		EVT_BUTTON(self, ID_BUTTON_HELP, self.OnHelp)
+		EVT_BUTTON(self, ID_BUTTON_OK, self.OnOK)
+		EVT_BUTTON(self, ID_BUTTON_CANCEL, self.OnCancel)
+
+
+#############################################################################
+# GUI action functions from here on
+#############################################################################
+
+    	def OnCancel(self, event):
+		self.cancelled = true
+		self.parent.Close()
+    	    	
+	def OnHelp(self, event):
+		wxMessageBox(_("\
+Advanced backend options: \n\
+\n\
+Database: name of GnuMed database\n\
+Host: name of database server\n\
+Port: port number of database server\n\
+Backend options: options passed through unparsed to the backend\n"))
+		
+	def OnOK(self, event):
+		self.cancelled = false
+		self.parent.Close()
 
 
 #############################################################################
