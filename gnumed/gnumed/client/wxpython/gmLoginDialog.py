3539870ddfb34e21485747196b09a4e497a07f0d
rterry <rterry>
2002-09-06 13:55:26 +0000

    simplified gui whilst retaining relevant information

--- gnumed/gnumed/client/wxpython/gmLoginDialog.py
+++ gnumed/gnumed/client/wxpython/gmLoginDialog.py
@@ -16,8 +16,19 @@
 #	24.10.2001 hherb persistent changes across multiple processes enabled
 #	25.10.2001 hherb more flexible configuration options, more commenting
 #	07.02.2002 hherb saved parameters now showing corectly in combo boxes
+#       06.09.2002 rterry simplified gui
+#                  -duplication and visual clutter removed
+#                   removed duplication of information in display
+#                   login gone from title bar , occurs once only now
+#                   visually highlighted in dark blue
+#                   login button title now 'ok' to conform to usual defaults of 
+#                   ok, cancel in most operating systems
+#                   reference to help removed (have help button for that)
+#                   date removed - can't see use of this in login - clutters
 #
 # @TODO:
+#FIXME:rterry:Has anyone noticed that if one closes the dialog it logs on anyway!
+#             whereas it should cancel like the cancel button does
 ############################################################################
 
 """gmLoginDialog - This module provides a login dialog to GNUMed
@@ -172,28 +183,40 @@ class LoginPanel(wxPanel):
 			self.topsizer.Add(bmp, 0, wxGROW|wxALIGN_CENTER_VERTICAL|wxALL, 10)
 		except:
 			self.topsizer.Add(wxStaticText (self, -1, "Cannot find image" + bitmap, style=wxALIGN_CENTRE), 0, wxGROW|wxALIGN_CENTER_VERTICAL|wxALL, 10)
-
-    	    	# add some infos on top of the login prompts
-		self.infobox = wxStaticBox( self, -1,'')
-		self.infoboxsizer = wxStaticBoxSizer( self.infobox, wxVERTICAL )
+		#---------------------------------------------------------------------------------------	
+                #Old code: commented out rterry 06.09.2002
+		#I don't think this is needed:
+		#  a) Date not relevant to login screen
+		#  b) help button exists at the bottom anyway
+		#  c) my version consolidates the site (here Desktop Dr.Jekyll, into the login heading)
+		#-------------------------------------------------------------------------------------- 
+    	    	#### add some infos on top of the login prompts
+		####self.infobox = wxStaticBox( self, -1,'')
+		####self.infoboxsizer = wxStaticBoxSizer( self.infobox, wxVERTICAL )
     	    	
-		# current date
-    	    	date = time.strftime("%d.%m.%Y")
-		self.infoboxsizer.Add(wxStaticText (self, -1, _("Date: ") + date, style=wxALIGN_LEFT), 0,wxGROW|wxALIGN_CENTRE_VERTICAL|wxALL,10)
+		#### current date
+    	    	####date = time.strftime("%d.%m.%Y")
+		####self.infoboxsizer.Add(wxStaticText (self, -1, _("Date: ") + date, style=wxALIGN_LEFT), 0,wxGROW|wxALIGN_CENTRE_VERTICAL|wxALL,10)
 
-    	    	# workplace
-    	    	workplace  = "Desktop Dr.Jekyll"
-		self.infoboxsizer.Add(wxStaticText (self, -1, _("Workplace: ") + workplace, style=wxALIGN_LEFT), 0, wxGROW|wxALIGN_CENTRE_VERTICAL|wxALL, 10)
+    	    	#### workplace
+    	    	####workplace  = "Desktop Dr.Jekyll"
+		####self.infoboxsizer.Add(wxStaticText (self, -1, _("Workplace: ") + workplace, style=wxALIGN_LEFT), 0, wxGROW|wxALIGN_CENTRE_VERTICAL|wxALL, 10)
 
-    	    	#  who to contact in case of problems with gnumed (local service provider etc.)
-       	    	contactinfo  = "0815-HELPME"
-		self.infoboxsizer.Add(wxStaticText (self, -1, _("Contact: ") + contactinfo, style=wxALIGN_LEFT), 0, wxGROW|wxALIGN_CENTRE_VERTICAL|wxALL, 10)
-    	    	self.infoboxsizer.Fit(self)
-
+    	    	####  who to contact in case of problems with gnumed (local service provider etc.)
+       	    	####contactinfo  = "0815-HELPME"
+		####self.infoboxsizer.Add(wxStaticText (self, -1, _("Contact: ") + contactinfo, style=wxALIGN_LEFT), 0, wxGROW|wxALIGN_CENTRE_VERTICAL|wxALL, 10)
+    	    	####self.infoboxsizer.Fit(self)
+		#-----------------------------------------------------------
+                #FIXME - This needs to be got from somewhere, not fixed text
+		#why doesn't this align in the centre
+		#------------------------------------
+                login_location =  _(" Login - Consulting Room 1 ")
+		
     	    	# start getting login parameters
-		self.paramsbox = wxStaticBox( self, -1, _("Login Parameters"))
+		self.paramsbox = wxStaticBox( self, -1, login_location, style=wxALIGN_CENTRE_HORIZONTAL)
 		self.paramsboxsizer = wxStaticBoxSizer( self.paramsbox, wxVERTICAL )
-
+		self.paramsbox.SetForegroundColour(wxColour(35, 35, 142))
+		self.paramsbox.SetFont(wxFont(12,wxSWISS,wxBOLD,wxBOLD,false,''))
 		self.pboxgrid = wxFlexGridSizer( 4, 2, 5, 5 )
 		self.pboxgrid.AddGrowableCol( 1 )
 
@@ -216,37 +239,81 @@ class LoginPanel(wxPanel):
 		self.pboxgrid.AddWindow( label, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5 )
 		self.pwdentry = wxTextCtrl( self, 1, '', wxDefaultPosition, wxSize(80,-1), wxTE_PASSWORD )
 		self.pboxgrid.AddWindow( self.pwdentry, 0, wxGROW|wxALIGN_CENTER_VERTICAL|wxALL, 5 )
+		#-----------------------------------------------
+		#old button code - commented out rterry 06Sept02
+		#-----------------------------------------------
+		####self.buttonsizer = wxBoxSizer( wxHORIZONTAL )
+		####ID_BUTTON_LOGIN = wxNewId()
+		####button = wxButton( self, ID_BUTTON_LOGIN, _("&Login"), wxDefaultPosition, wxDefaultSize, 0 )
+		####button.SetDefault()
+		####self.buttonsizer.AddWindow( button, 0, wxALIGN_CENTRE|wxALL, 5 )
 
-		self.buttonsizer = wxBoxSizer( wxHORIZONTAL )
+		####self.buttonsizer.AddSpacer( 10, 20, 0, wxALIGN_CENTRE|wxALL, 5 )
 
+		####ID_BUTTON_OPTIONS = wxNewId()
+		####button = wxButton( self, ID_BUTTON_OPTIONS, _("&Options"), wxDefaultPosition, wxDefaultSize, 0 )
+		####button.SetToolTip( wxToolTip(_("Set advanced options like database, host, port etc.")) )
+		####self.buttonsizer.AddWindow( button, 0, wxALIGN_CENTRE|wxALL, 5 )
+
+		####self.buttonsizer.AddSpacer( 10, 20, 0, wxALIGN_CENTRE|wxALL, 5 )
+		
+		####ID_BUTTON_HELP = wxNewId()
+		####button = wxButton( self, ID_BUTTON_HELP, _("&Help"), wxDefaultPosition, wxDefaultSize, 0 )
+		####self.buttonsizer.AddWindow( button, 0, wxALIGN_CENTRE|wxALL, 5 )
+
+		####self.buttonsizer.AddSpacer( 10, 20, 1, wxALIGN_CENTRE|wxALL, 5 )
+
+		####ID_BUTTON_CANCEL = wxNewId()
+		####button = wxButton( self, ID_BUTTON_CANCEL, _("&Cancel"), wxDefaultPosition, wxDefaultSize, 0 )
+		#----------------------------------------------------------------------
+		#new button code inserted rterry 06Sept02
+		#button order re-arraged to make it consistant with usual dialog format
+		#in most operating systems ie  btns ok and cancel are standard and 
+		#in that order
+		#ie Order is now Options, help, ok and cancel
+		#The order of creation is the tab order
+		#login-ok button automatically is the default when tabbing (or <enter>)
+		#from password
+		#this eliminates the heavy border when you use the default 
+		#?is the default word needed for any other reason?
+		#----------------------------------------------------------------------
+		self.button_gridsizer = wxGridSizer(1,4,0,0)
+		#---------------------
+		#3:create login ok button
+		#---------------------
 		ID_BUTTON_LOGIN = wxNewId()
-		button = wxButton( self, ID_BUTTON_LOGIN, _("&Login"), wxDefaultPosition, wxDefaultSize, 0 )
-		button.SetDefault()
-		self.buttonsizer.AddWindow( button, 0, wxALIGN_CENTRE|wxALL, 5 )
-
-		self.buttonsizer.AddSpacer( 10, 20, 0, wxALIGN_CENTRE|wxALL, 5 )
-
-		ID_BUTTON_OPTIONS = wxNewId()
-		button = wxButton( self, ID_BUTTON_OPTIONS, _("&Options"), wxDefaultPosition, wxDefaultSize, 0 )
-		button.SetToolTip( wxToolTip(_("Set advanced options like database, host, port etc.")) )
-		self.buttonsizer.AddWindow( button, 0, wxALIGN_CENTRE|wxALL, 5 )
-
-		self.buttonsizer.AddSpacer( 10, 20, 0, wxALIGN_CENTRE|wxALL, 5 )
-
-		ID_BUTTON_HELP = wxNewId()
-		button = wxButton( self, ID_BUTTON_HELP, _("&Help"), wxDefaultPosition, wxDefaultSize, 0 )
-		self.buttonsizer.AddWindow( button, 0, wxALIGN_CENTRE|wxALL, 5 )
-
-		self.buttonsizer.AddSpacer( 10, 20, 1, wxALIGN_CENTRE|wxALL, 5 )
-
+		button_login_ok = wxButton( self, ID_BUTTON_LOGIN, _("&Ok"), wxDefaultPosition, wxDefaultSize, 0 )
+		button_login_ok.SetToolTip( wxToolTip(_("Proceed with login.")) )
+		#---------------------
+		#3:create cancel button
+		#---------------------
 		ID_BUTTON_CANCEL = wxNewId()
-		button = wxButton( self, ID_BUTTON_CANCEL, _("&Cancel"), wxDefaultPosition, wxDefaultSize, 0 )
-		self.buttonsizer.AddWindow( button, 0, wxALIGN_CENTRE|wxALL, 5 )
-
+		button_cancel = wxButton( self, ID_BUTTON_CANCEL, _("&Cancel"), wxDefaultPosition, wxDefaultSize, 0 )
+		button_cancel.SetToolTip( wxToolTip(_("Cancel Login.")) )
+		#-------------------
+		#1:create option button
+		#-------------------
+		ID_BUTTON_OPTIONS = wxNewId()
+		button_option = wxButton( self, ID_BUTTON_OPTIONS, _("&Options"), wxDefaultPosition, wxDefaultSize, 0 )
+		button_option.SetToolTip( wxToolTip(_("Set advanced options like database, host, port etc.")) )
+		#---------------------
+		#2:create Help button
+		#---------------------
+		ID_BUTTON_HELP = wxNewId()
+		button_help = wxButton( self, ID_BUTTON_HELP, _("&Help"), wxDefaultPosition, wxDefaultSize, 0 )
+		button_help.SetToolTip( wxToolTip(_("Help for login screen")) )
+		#----------------------------
+		#Add buttons to the gridsizer
+		#----------------------------
+		self.button_gridsizer.Add (button_option,0,wxEXPAND|wxALL,5)
+		self.button_gridsizer.Add (button_help,0,wxEXPAND|wxALL,5)
+		self.button_gridsizer.Add (button_login_ok,0,wxEXPAND|wxALL,5)
+		self.button_gridsizer.Add (button_cancel,0,wxEXPAND|wxALL,5)
+               
 		self.paramsboxsizer.AddSizer(self.pboxgrid, 1, wxGROW|wxALL, 10)
-		self.topsizer.AddSizer(self.infoboxsizer, 0,wxGROW|wxALL, 10)
 		self.topsizer.AddSizer(self.paramsboxsizer, 1, wxGROW|wxALL, 10)
-		self.topsizer.AddSizer( self.buttonsizer, 0, wxGROW|wxALIGN_CENTER_VERTICAL|wxALL, 5 )
+		#old code:self.topsizer.AddSizer(self.infoboxsizer, 0,wxGROW|wxALL, 10)
+	        self.topsizer.AddSizer( self.button_gridsizer, 0, wxGROW|wxALIGN_CENTER_VERTICAL|wxALL, 5 )
 
 		self.SetAutoLayout( true )
 		self.SetSizer( self.topsizer)
@@ -404,7 +471,7 @@ class LoginPanel(wxPanel):
 ########################################################################################
 
 class LoginDialog(wxDialog):
-	def __init__(self, parent, id, title=_("Login")):
+	def __init__(self, parent, id, title=_("")):
 		wxDialog.__init__(self, parent, id, title)
 		self.panel = LoginPanel(self, -1, isDialog=1)
 		self.Fit () # needed for Windoze.
@@ -492,7 +559,7 @@ class OptionPanel(wxPanel):
 		self.pboxgrid.AddWindow( self.beoptioncombo, 0, wxGROW|wxALIGN_CENTER_VERTICAL|wxALL, 5 )
 
 		self.buttonsizer = wxBoxSizer( wxHORIZONTAL )
-
+		
 		ID_BUTTON_OK = wxNewId()
 		button = wxButton( self, ID_BUTTON_OK, _("&Ok"), wxDefaultPosition, wxDefaultSize, 0 )
 		button.SetDefault()
@@ -554,7 +621,7 @@ Backend options: options passed through unparsed to the backend\n"))
 if __name__ == '__main__':
 	import gmI18N
 
-	app = wxPyWidgetTester(size = (400, 500))
+	app = wxPyWidgetTester(size = (300,400))
 	#show the login panel in a main window
 	app.SetWidget(LoginPanel, -1)
 	#and pop the login dialog up modally
