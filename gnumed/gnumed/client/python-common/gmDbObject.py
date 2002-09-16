ed80c3fa55ed2ea2d07d3bd005d97b5bca2ec6b4
Karsten Hilbert <karsten.hilbert@gmx.net>
2002-09-16 10:44:08 +0000

    - add logging as requested

--- gnumed/gnumed/client/python-common/gmDbObject.py
+++ gnumed/gnumed/client/python-common/gmDbObject.py
@@ -17,13 +17,17 @@
 ############################################################################
 
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/python-common/Attic/gmDbObject.py,v $
-__version__ = "$Revision: 1.2 $"
+__version__ = "$Revision: 1.3 $"
 __author__  = "H. Herb <hherb@gnumed.net>"
 
-
+import sys
+import gmLog
+_log = gmLog.gmDefLog
+_log.SetAllLogLevels(gmLog.lData)
+#============================================================
 class DBObject:
 	"""High level DB-API based base class for all gnumed database objects"""
-	
+
 	def __init__(self, dbbroker, service='default', select_query=None):
 		"""dbbroker: database broker as in gmPG.py
 		service: name of the gnumed database service the data is based on
@@ -34,8 +38,7 @@ class DBObject:
 		self._qInsert = None
 		self._qUpdate = None
 		self._qDelete = None
-		
-		
+	#----------------------------
 	def SetSelectQuery(self, query):
 		"""Definition of the query string used to select the row(s)
 		associated with this database object. Parameters (for example for
@@ -43,16 +46,14 @@ class DBObject:
 		 'dictionary parameter style', that is as '%(dictionary key)s'
 		All parameters have to be passed as strings, but no quoting!!!"""
 		self._qSelect = query
-	
-			
+	#----------------------------
 	def SetInsertQuery(self, query):
 		"""Definition of the query that would insert a row into this
 		database object. All parameters must
 		be set in 'dictionary parameter style', that is as '%(dictionary key)s'
 		All parameters have to be passed as strings, but no quoting!!!"""
 		self._qInsert = query
-	
-			
+	#----------------------------
 	def SetUpdateQuery(self, query):
 		"""Definition of the query neccessary to update the associated row.
 		The string must have a '%(primarykey)s' parameter which is representing the 
@@ -60,22 +61,22 @@ class DBObject:
 		be set in 'dictionary parameter style', that is as '%(dictionary key)s'
 		All parameters have to be passed as strings, but no quoting!!!"""
 		self._qUpdate = query
-	
-			
+	#----------------------------
 	def SetDeleteQuery(self, query):
 		"""Definition of the query neccessary to delete the associated row.
 		The string must have a '%(primarykey)s' parameter which is representing the 
 		primary key attribute of the row to delete"""
 		self._qDelete = query
-		
-		
-	def LogError(self, msg, map):
+	#----------------------------
+	def __log_error(self, msg, aMap = None):
 		"""Please replace with gnumed logging functins s.a.p."""
-		print msg
-		for key in map.keys():
-			print "%s = %s", str(key), str(map[key])
-	
-			
+		_log.Log(gmLog.lErr, msg)
+		if aMap != None:
+			_log.Log(gmLog.lData, "--------------------------------")
+			for key in aMap.keys():
+				_log.Log(gmLog.lData, "%s = %s" % (str(key), str(aMap[key])))
+			_log.Log(gmLog.lData, "--------------------------------")
+	#----------------------------
 	def Select(self, map=None, maxfetch=0, listonly=0):
 		"""Executes the select query and returns a list of
 		PgResultSets (see pyPgSQL documentation)
@@ -86,7 +87,7 @@ class DBObject:
 		of rows is expected as result by returning a simple list
 		of lists instead of a list of PgResultSets"""
 		if self._qSelect is None:
-			self.LogError("Error: select query not set yet!")
+			self.__log_error("Error: select query not set yet!")
 			return None
 		result = []
 		try:
@@ -94,7 +95,8 @@ class DBObject:
 			try:
 				con = self._db.GetConnection(self.__service)
 			except:
-				self.LogError(_("Failed to connect to backend in DBObject.Select()"), map)
+				exc = sys.exc_info()
+				_log.LogException("Failed to connect to backend.", exc, fatal=1)
 				return None
 			cursor = con.cursor()
 			try:
@@ -103,96 +105,116 @@ class DBObject:
 				else:
 					cursor.execute(self._qSelect)
 			except:
-				self.LogError(_("Failed to execute query in DBObject.Select()"), map)
+				exc = sys.exc_info()
+				_log.LogException("Query failed: >>>%s<<<" % map, exc, fatal=1)
 				return None
 			if maxfetch>0:
 				result =  cursor.fetchmany(maxfetch)
 			else:
 				result = cursor.fetchall()
-				print result
+				#<DEBUG>
+				_log.Log(gmLog.lData, "result: %s" % result)
+				#print result
+				#</DEBUG>
 		finally:
 			self._db.SetFetchReturnsList(0)
 		if result is None:
 			result = []
 		return result
-		
-		
+	#----------------------------
 	def Insert(self, map):
 		"""insert a row with attributes as listed in the dictionary 'map'.
-		Returns the OID is succesful, otherwise returns 'None'"""
+		Returns the OID if succesful, otherwise returns 'None'"""
 		oid = None
 		if self._qInsert is None:
-			self.LogError("Error: insert query not set yet!")
+			self.__log_error("Error: insert query not set yet!")
 			return None
 		try:
 			con = self._db.GetConnection(self.__service, readonly=0)
 		except:
-			self.LogError(_("Failed to connect to backend in DBObject.Select()"), map)
+			exc = sys.exc_info()
+			_log.LogException("Failed to connect to backend.", exc, fatal=1)
 			return None
 		cursor = con.cursor()
 		try:
 			cursor.execute(self._qInsert, map)
 		except:
-			self.LogError(_("Failed to execute query in DBObject.Insert()"), map)
+			exc = sys.exc_info()
+			_log.LogException("Query failed: >>>%s<<<" % map, exc, fatal=1)
 			return None
 		oid = cursor.oidValue
 		con.commit()
 		return oid
-		
-	
-	def Update(self, map):
+	#----------------------------
+	def Update(self, map = None):
 		"""update a row with attributes as listed in the dictionary "map".
 		'map' dictionary  MUST contain the key 'primarykey' with the
 		value set to the primay key of the row to be updated
 		Returns 'None' if failed, the primary key if success """
 		if self._qUpdate is None:
-			self.LogError("Error: update query not set yet!")
+			self.__log_error("update query not set yet!")
 			return None
+		if not map.has_key('primarykey'):
+			self.__log_error("no primary key in value map [%s]" % map)
+			return None
+
 		try:
 			con = self._db.GetConnection(self.__service, readonly=0)
 		except:
-			self.LogError(_("Failed to connect to backend in DBObject.Select()"), map)
+			exc = sys.exc_info()
+			_log.LogException("Failed to connect to backend.", exc, fatal=1)
 			return None
 		cursor = con.cursor()
 		try:
 			cursor.execute(self._qUpdate,map)
 		except:
-			self.LogError(_("Failed to execute query in DBObject.Insert()"), map)
+			exc = sys.exc_info()
+			_log.LogException("Query failed: >>>%s<<<" % map, exc, fatal=1)
 			return None
 		con.commit()
 		return primarykey
-	
-	
+	#----------------------------
 	def Delete(self, map):
 		"""deletes a row as determined by the delete query string.
 		'map' dictionary  MUST contain the key 'primarykey' with the
 		value set to the primay key of the row to be deleted
 		Returns 'None' if failed, the primary key if success """
 		if self._qDelete is None:
-			self.LogError("Error: delete query not set yet!")
+			self.__log_error("delete query not set yet!")
+			return None
+		if not map.has_key('primarykey'):
+			self.__log_error("no primary key in value map [%s]" % map)
 			return None
 		try:
 			con = self._db.GetConnection(self.__service, readonly=0)
 		except:
-			self.LogError(_("Failed to connect to backend in DBObject.Select()"), map)
+			exc = sys.exc_info()
+			_log.LogException("Failed to connect to backend.", exc, fatal=1)
 			return None
 		cursor = con.cursor()
 		try:
 			cursor.execute(self._qDelete, map)
 		except:
-			self.LogError(_("Failed to execute query in DBObject.Delete()"), map)
+			exc = sys.exc_info()
+			_log.LogException("Failed to connect to backend.", exc, fatal=1)
 			return None
 		con.commit()
 		return primarykey
-		
-
+#============================================================
+# main
+#============================================================
 if __name__ == "__main__":
 	import gmPG
-	db=gmPG.ConnectionPool()
+	db = gmPG.ConnectionPool()
 	dbo = DBObject(db, select_query = "select * from pg_tables where tablename not like 'pg_%'")
 	rows = dbo.Select()
 	for row in rows:
 		print ""
 		for key in row.keys():
-			print "%s=%s," % (key, str(row[key])) ,
-		
+			print "%s=%s," % (key, str(row[key])),
+
+#============================================================
+# $Log: gmDbObject.py,v $
+# Revision 1.3  2002-09-16 10:44:08  ncq
+# - add logging as requested
+#
