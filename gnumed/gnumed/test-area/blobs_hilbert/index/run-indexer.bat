e1e97ee842cf44cdc9bedba24604e8b348817560
Karsten Hilbert <karsten.hilbert@gmx.net>
2002-09-17 00:08:52 +0000

    - added comment as to when to delete the XDT file with the patient data

--- gnumed/gnumed/test-area/blobs_hilbert/index/run-indexer.bat
+++ gnumed/gnumed/test-area/blobs_hilbert/index/run-indexer.bat
@@ -2,3 +2,6 @@ REM set your preferred language here
 REM set LANG=de_DE@euro
 
 c:\Python22\python.exe .\index-med_docs.py --conf-file=.\gnumed-archiv.conf --text-domain=gnumed-archiv
+
+REM Delete the XDT file with the patient data
+REM del c:\....\patient.dat
