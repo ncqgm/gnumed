0b0b417709165b7795be64d3f6a441f79ceb5104
Karsten Hilbert <karsten.hilbert@gmx.net>
2002-10-08 12:17:15 +0000

    - switch over to sizers-based version

--- gnumed/gnumed/test-area/blobs_hilbert/scan/run-scanner.bat
+++ gnumed/gnumed/test-area/blobs_hilbert/scan/run-scanner.bat
@@ -1,5 +1,9 @@
 @echo off
+REM =================================
+REM $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/test-area/blobs_hilbert/scan/Attic/run-scanner.bat,v $
+REM $Revision: 1.3 $
+REM =================================
 REM set your preferred language here
 set LANG=de_DE@euro
 
-c:\Python22\python.exe .\scan-med_docs.py --conf-file=.\gnumed-archiv.conf --text-domain=gnumed-archiv
+c:\Python22\python.exe .\gmScanMedDocs.py --conf-file=.\gnumed-archiv.conf --text-domain=gnumed-archiv
--- gnumed/gnumed/test-area/blobs_hilbert/scan/run-scanner.sh
+++ gnumed/gnumed/test-area/blobs_hilbert/scan/run-scanner.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/test-area/blobs_hilbert/scan/Attic/run-scanner.sh,v $
-# $Revision: 1.1 $
+# $Revision: 1.2 $
 
 # this is a wrapper for the indexer
 # set your command line arguments etc. here
@@ -10,6 +10,6 @@
 #LANG = "de_DE@euro"
 
 # if you need to set a special base directory for some reason
-#SCAN-MED_DOCS_DIR = ""
+#GMSCANMEDDOCS_DIR = ""
 
-python ./scan-med_docs.py --conf-file=/home/ncq/.gnumed/gnumed-archiv.conf --text-domain=gnumed-archiv
+python ./gmScanMedDocs.py --conf-file=/home/ncq/.gnumed/gnumed-archiv.conf --text-domain=gnumed-archiv
