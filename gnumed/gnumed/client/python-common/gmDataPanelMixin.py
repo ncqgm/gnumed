4d56425ada113fcf12e35aa1cbf4217894b25718
hherb <hherb>
2002-10-13 11:49:50 +0000

    provides a uniform means of data sensitivity to a GUI panel

---
+++ gnumed/gnumed/client/python-common/gmDataPanelMixin.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+#############################################################################
+#
+# gmDataPanelMixin - Provides a uniform means of data sensitivity to a GUI panel
+# ---------------------------------------------------------------------------
+#
+# @author: Dr. Horst Herb
+# @copyright: author
+# @license: GPL (details at http://www.gnu.org)
+# @dependencies: nil
+############################################################################
+
+# $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/python-common/Attic/gmDataPanelMixin.py,v $
+__version__ = "$Revision: 1.1 $"
+__author__ = "H.Herb <hherb@gnumed.net>"
+# $Log: gmDataPanelMixin.py,v $
+# Revision 1.1  2002-10-13 11:49:50  hherb
+# provides a uniform means of data sensitivity to a GUI panel
+#
+
+"""Provides a uniform means of data sensitivity to a GUI panel"""
+
+class DataPanelMixin :
+
+	def __init__(self, dbbroker):
+		self._dbbroker = dbbroker	# a gmPG database broker object
+		self._dirty = 0		# true if the data has been modified
+		self._loaded = 0	# true if the panel has been filled with data loaded from the backend
+		self._data = {}		# a dictionary containing the data displayed in this panel;
+					# key words are the column names returned by the query/queries
+
+	
+	def OnDefault(self):
+		"""Virtual function: has to put all widgets into their default state"""
+		pass
+	
+	def OnLoadData(self, **kwds):
+		"""Virtual function: loads data from the backend and updates the
+		correlating widgets. 'kwds' is a dictionary argument for
+		arbitrary query arguments like primary keys"""
+		self._loaded = 1
+		
+	def OnSaveData(self, **kwds):
+		"""Virtual function: saves the data from this panel to the backend;
+		if not 'self._loaded', it is inserted, else if 'self._dirty is true, updated"""
+		pass
+		
+	def OnUndo(self):
+		"""restores the data as loaded from the backend if 'self._loaded is true,
+		else retores the default"""
+		pass
