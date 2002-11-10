4f0b9bdf24b535e849b7f3a5182f01fc86c00640
Karsten Hilbert <karsten.hilbert@gmx.net>
2002-11-10 13:54:14 +0000

    - log database creation - encoding set to LATIN1

--- gnumed/gnumed/test-area/gmDrug/setup-amis.sh
+++ gnumed/gnumed/test-area/gmDrug/setup-amis.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/test-area/gmDrug/setup-amis.sh,v $
-# $Revision: 1.1 $
+# $Revision: 1.2 $
 
 DB="amis"
 host="localhost"
 user="gm-dbowner"
 
 dropdb $DB
-createdb $DB
-psql -a -E -h $host -d $DB -U $user -f amis-create_tables.sql > create-amis.log
-psql -a -E -h $host -d $DB -U $user -f amis-import_data.sql >> create-amis.log
+createdb -e -h $host -U $user -E LATIN1 $DB "AMIS - German drug data" > setup-amis.log
+psql -a -E -h $host -d $DB -U $user -f amis-create_tables.sql >> setup-amis.log
+psql -a -E -h $host -d $DB -U $user -f amis-import_data.sql >> setup-amis.log
 
 #--------------------------------------------
 # $Log: setup-amis.sh,v $
-# Revision 1.1  2002-11-10 13:46:37  ncq
+# Revision 1.2  2002-11-10 13:54:14  ncq
+# - log database creation
+# - encoding set to LATIN1
+#
+# Revision 1.1  2002/11/10 13:46:37  ncq
 # - first version
 #
