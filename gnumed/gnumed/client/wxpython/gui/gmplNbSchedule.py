4bda48f97042926c30e0596f5c0347945ccee894
hherb <hherb>
2002-11-12 22:58:14 +0000

    widget for patient appointments schedule as a notebook plugin

---
+++ gnumed/gnumed/client/wxpython/gui/gmplNbSchedule.py
@@ -0,0 +1,26 @@
+# a simple wrapper for the cryptowidget
+from wxPython.wx import *
+import gmPlugin
+import gmScheduleAllDoctorsPnl
+
+class gmplNbSchedule(gmPlugin.wxNotebookPlugin):
+    """
+    Plugin to encapsulate a patient scheduling system
+    """
+
+    def name (self):
+        return 'Appointments'
+
+    def MenuInfo (self):
+        return ('view', '&Appointments')
+
+    def GetWidget (self, parent):
+        try:
+            pnl = gmScheduleAllDoctorsPnl.ScheduleAllDoctorsPnl(parent)
+        except:
+            print "Failed to load schedule panel"
+	    return None
+        return pnl
+
+
+
