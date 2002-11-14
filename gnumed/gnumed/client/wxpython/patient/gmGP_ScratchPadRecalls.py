43631e699af8c4d4cd8d44e02ab9068f98e69d80
hherb <hherb>
2002-11-14 05:23:21 +0000

    rewritten to a large degree; now connecting to database and reading the current patient's recalls and scratch pad info from the backend

--- gnumed/gnumed/client/wxpython/patient/gmGP_ScratchPadRecalls.py
+++ gnumed/gnumed/client/wxpython/patient/gmGP_ScratchPadRecalls.py
@@ -1,88 +1,129 @@
 from wxPython.wx import *
-import gmPlugin, gmShadow, gmConf, gmLog
+import gmPlugin, gmShadow, gmConf, gmLog, gmDispatcher, gmSignals, gmPG
 
-scratchpaddata = {
-1 : ("01/12/2001", "check BP next visit"),
-2 : ("01/12/2001", "daughter requests dementia screen"),
-}
-recalldata = {
-1 : ("Annual Checkup", "due in 1 month"),
-2 : ("PAP smear", "overdue 6 months"),
-}
+scratchpaddata = {}
+recalldata = {}
+
+query_scratchpad = "select id, timestamp, text, author from scratchpad where id_identity=%s"
+query_recalls = "select id, timestamp, reason from recalls where id_identity=%s"
+
 class ScratchPadRecalls(wxPanel):
-    def __init__(self, parent,id):
-	wxPanel.__init__(self,parent,id,wxDefaultPosition,wxDefaultSize,style = wxRAISED_BORDER)
-	#add a label which is the heading for the text data entry 'Scratchpad' 	
-	scratchpad_lbl = wxStaticText(self,-1, _("Scratch Pad"),style = wxALIGN_CENTRE) #add static text control for the capion
-	scratchpad_lbl.SetForegroundColour(wxColor(0,0,131))               #set caption text colour 
-	scratchpad_lbl.SetFont(wxFont(12,wxSWISS,wxBOLD,wxBOLD,false,''))
-	#Add a text control under that
-	scratchpad_txt = wxTextCtrl(self,-1,"",wxDefaultPosition,wxDefaultSize,0)
-	#Add a label for the recalls/reviews list
-	recalls_lbl = wxStaticText(self,-1, _("Recalls/Reviews"),style = wxALIGN_CENTRE) #add static text control for the capion
-	recalls_lbl.SetForegroundColour(wxColor(0,0,131))               #set caption text colour 
-	recalls_lbl.SetFont(wxFont(12,wxSWISS,wxBOLD,wxBOLD,false,''))
+	def __init__(self, parent,id=-1):
+		self.patientID=None
+		wxPanel.__init__(self,parent,id,wxDefaultPosition,wxDefaultSize,style = wxRAISED_BORDER)
+		self.parent=parent
+		self.create_widgets()
+		self.layout_widgets()
+		self.register_interests()
+		self._con = gmPG.ConnectionPool()
 
-        #------------------------------------------------------------------------------
-	#Add a simple listcontrol under that for scratchpad items and insert dummy data
-	#------------------------------------------------------------------------------
-	list_scratchpad = wxListCtrl(self, -1,  wxDefaultPosition, wxDefaultSize,wxLC_REPORT|wxLC_NO_HEADER|wxSUNKEN_BORDER)
-	list_scratchpad.SetForegroundColour(wxColor(255,0,0))
-	list_scratchpad.InsertColumn(0, "Logged")
-	list_scratchpad.InsertColumn(1, "", wxLIST_FORMAT_LEFT)
-	#-------------------------------------------------------------
-	#loop through the scriptdata array and add to the list control
-	#note the different syntax for the first coloum of each row
-	#i.e. here > self.List_Script.InsertStringItem(x, data[0])!!
-	#-------------------------------------------------------------
-	items = scratchpaddata.items()
-	for x in range(len(items)):
-            key, data = items[x]
-	    gmLog.gmDefLog.Log (gmLog.lData, items[x])
-	    #print x, data[0],data[1]
-	    list_scratchpad.InsertStringItem(x, data[0])
-            list_scratchpad.SetStringItem(x, 1, data[1])
-            list_scratchpad.SetItemData(x, key)
-	list_scratchpad.SetColumnWidth(0, wxLIST_AUTOSIZE)
-        list_scratchpad.SetColumnWidth(1, 200)
-	#--------------------------------------------------------------------------
-	#Add a simple listcontrol under that for recall items and insert dummy data
-	#--------------------------------------------------------------------------
-	list_recalls = wxListCtrl(self, -1,  wxDefaultPosition, wxDefaultSize,wxLC_REPORT|wxLC_NO_HEADER|wxSUNKEN_BORDER)
-	list_recalls.SetForegroundColour(wxColor(255,0,0))
-	list_recalls.InsertColumn(0, _("Recall or Review"))
-	list_recalls.InsertColumn(1, _("Status"), wxLIST_FORMAT_LEFT)
-	
-	#-------------------------------------------------------------
-	#loop through the scriptdata array and add to the list control
-	#note the different syntax for the first coloum of each row
-	#i.e. here > self.List_Script.InsertStringItem(x, data[0])!!
-	#-------------------------------------------------------------
-	items = recalldata.items()
-	for x in range(len(items)):
-            key, data = items[x]
-            #<DEBUG>
-	    gmLog.gmDefLog.Log (gmLog.lData,  items[x])
-            #</DEBUG>
-	    #print x, data[0],data[1]
-	    list_recalls.InsertStringItem(x, data[0])
-            list_recalls.SetStringItem(x, 1, data[1])
-            list_recalls.SetItemData(x, key)
-	list_recalls.SetColumnWidth(0, wxLIST_AUTOSIZE)
-        list_recalls.SetColumnWidth(1, 200)
 
-        sizer= wxBoxSizer(wxVERTICAL)
-	sizer.Add(scratchpad_lbl,0,wxEXPAND)
-	sizer.Add(scratchpad_txt,0,wxEXPAND)
-	#sizer.Add(10,10,0,wxEXPAND)
-        sizer.Add(list_scratchpad,30,wxEXPAND) 
-	sizer.Add(recalls_lbl,0, wxEXPAND)
-	#sizer.Add(5,5,0,wxEXPAND)
-	sizer.Add(list_recalls,70,wxEXPAND)
-	self.SetSizer(sizer)  #set the sizer 
-	sizer.Fit(self)             #set to minimum size as calculated by sizer
-        self.SetAutoLayout(true)                 #tell frame to use the sizer
-        self.Show(true) 
+
+	def create_widgets(self):
+		self.lbl_fgcolour = wxColor(0,0,131)
+		self.list_fgcolour = wxColor(255,0,0)
+		self.lbl_font = wxFont(12,wxSWISS,wxBOLD,wxBOLD,false,'')
+		#add a label which is the heading for the text data entry 'Scratchpad'
+		self.scratchpad_lbl = wxStaticText(self,-1, _("Scratch Pad"),style = wxALIGN_CENTRE) #add static text control for the capion
+		self.scratchpad_lbl.SetForegroundColour(self.lbl_fgcolour)               #set caption text colour
+		self.scratchpad_lbl.SetFont(self.lbl_font)
+		#Add a text control under that
+		self.scratchpad_txt = wxTextCtrl(self,-1,"",wxDefaultPosition,wxDefaultSize,0)
+		#Add a label for the recalls/reviews list
+		self.recalls_lbl = wxStaticText(self,-1, _("Recalls/Reviews"),style = wxALIGN_CENTRE) #add static text control for the capion
+		self.recalls_lbl.SetForegroundColour(self.lbl_fgcolour)               #set caption text colour
+		self.recalls_lbl.SetFont(self.lbl_font)
+
+		#------------------------------------------------------------------------------
+		#Add a simple listcontrol under that for scratchpad items
+		#------------------------------------------------------------------------------
+		self.list_scratchpad = wxListCtrl(self, -1,  wxDefaultPosition, wxDefaultSize,wxLC_REPORT|wxLC_NO_HEADER|wxSUNKEN_BORDER)
+		self.list_scratchpad.SetForegroundColour(self.list_fgcolour)
+		self.list_scratchpad.InsertColumn(0, _("Logged"))
+		self.list_scratchpad.InsertColumn(1, "", wxLIST_FORMAT_LEFT)
+
+		#--------------------------------------------------------------------------
+		#Add a simple listcontrol under that for recall items
+		#--------------------------------------------------------------------------
+		self.list_recalls = wxListCtrl(self, -1,  wxDefaultPosition, wxDefaultSize,wxLC_REPORT|wxLC_NO_HEADER|wxSUNKEN_BORDER)
+		self.list_recalls.SetForegroundColour(self.list_fgcolour)
+		self.list_recalls.InsertColumn(0, _("Recall or Review"))
+		self.list_recalls.InsertColumn(1, _("Status"), wxLIST_FORMAT_LEFT)
+
+	def layout_widgets(self):
+		self.sizer= wxBoxSizer(wxVERTICAL)
+		self.sizer.Add(self.scratchpad_lbl,0,wxEXPAND)
+		self.sizer.Add(self.scratchpad_txt,0,wxEXPAND)
+		#sizer.Add(10,10,0,wxEXPAND)
+		self.sizer.Add(self.list_scratchpad,30,wxEXPAND)
+		self.sizer.Add(self.recalls_lbl,0, wxEXPAND)
+		#sizer.Add(5,5,0,wxEXPAND)
+		self.sizer.Add(self.list_recalls,70,wxEXPAND)
+		self.SetSizer(self.sizer)  #set the sizer
+		self.sizer.Fit(self)             #set to minimum size as calculated by sizer
+		self.SetAutoLayout(true)                 #tell frame to use the sizer
+		self.Show(true)
+
+	def register_interests(self):
+		gmDispatcher.connect(self.OnPatientID, gmSignals.patient_selected())
+
+
+	def UpdateRecalls(self, patid):
+		self.list_scratchpad.DeleteAllItems()
+		if patid is None:
+			return
+		db = self._con.GetConnection('clinical')
+		cur = db.cursor()
+		cur.execute(query_recalls % str(patid))
+		fetched = cur.fetchall()
+		for index in range(len(fetched)):
+			row=fetched[index]
+			id=row[0]
+			#date=row[1].strftime("%d.%m.%y")
+			date=str(row[1])[:10]
+			text=row[2]
+			self.list_recalls.InsertStringItem(index, date )
+			self.list_recalls.SetStringItem(index, 1, text)
+			self.list_recalls.SetItemData(index, id)
+		self.list_recalls.SetColumnWidth(0, wxLIST_AUTOSIZE)
+		self.list_recalls.SetColumnWidth(1, 200)
+
+
+	def UpdateScratchpad(self, patid):
+		self.list_scratchpad.DeleteAllItems()
+		self.scratchpad_txt.SetValue("")
+		if patid is None:
+			return
+		db = self._con.GetConnection('clinical')
+		cur = db.cursor()
+		cur.execute(query_scratchpad % str(patid))
+		fetched = cur.fetchall()
+		for index in range(len(fetched)):
+			row=fetched[index]
+			id=row[0]
+			#date=row[1].strftime("%d.%m.%y")
+			date=str(row[1])[:10]
+			reason=row[2]
+			self.list_scratchpad.InsertStringItem(index, date)
+			self.list_scratchpad.SetStringItem(index, 1, reason)
+			self.list_scratchpad.SetItemData(index, id)
+		self.list_scratchpad.SetColumnWidth(0, wxLIST_AUTOSIZE)
+		self.list_scratchpad.SetColumnWidth(1, 200)
+
+
+	def OnPatientID(self, **kwargs):
+		"must be executed when the current patient changes. Updates all widgets accordingly"
+		if kwargs is None:
+			#new patient, blank widgets
+			self.UpdateRecalls(None)
+			self.UpdateSCratchpad(None)
+			return
+
+		kwds = kwargs['kwds']
+		patid = kwds['ID']
+		self.UpdateRecalls(patid)
+		self.UpdateScratchpad(patid)
+
 
 
 
