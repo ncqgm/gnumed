8eb6295ebee7d7d305e7687c18d4635b9161449c
Karsten Hilbert <karsten.hilbert@gmx.net>
2002-11-20 12:56:47 +0000

    - standard log file location

--- gnumed/gnumed/test-area/blobs_hilbert/index/run-indexer.sh
+++ gnumed/gnumed/test-area/blobs_hilbert/index/run-indexer.sh
@@ -9,4 +9,16 @@
 # if you need to set a special base directory for some reason
 #INDEX-MED_DOCS_DIR = ""
 
-python ./index-med_docs.py --conf-file=/home/ncq/.gnumed/gnumed-archiv.conf --text-domain=gnumed-archiv
+python ./index-med_docs.py \
+	--conf-file=/home/ncq/.gnumed/gnumed-archiv.conf \
+	--text-domain=gnumed-archiv \
+	--log-file=/var/log/gnumed/archive-index.log
+
+#============================================================
+# $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/test-area/blobs_hilbert/index/Attic/run-indexer.sh,v $
+# $Revision: 1.2 $
+
+# $Log: run-indexer.sh,v $
+# Revision 1.2  2002-11-20 12:55:13  ncq
+# - standard log file location
+#
--- gnumed/gnumed/test-area/blobs_hilbert/scan/run-scanner.sh
+++ gnumed/gnumed/test-area/blobs_hilbert/scan/run-scanner.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/test-area/blobs_hilbert/scan/Attic/run-scanner.sh,v $
-# $Revision: 1.3 $
-
 # - this is a wrapper for the scanner,
 # - set your command line arguments etc. here
 
@@ -12,4 +9,16 @@
 # if you need to set a special base directory for some reason
 #GMSCANMEDDOCS_DIR = ""
 
-python ./gmScanMedDocs.py --conf-file=/home/ncq/.gnumed/gnumed-archiv.conf --text-domain=gnumed-archiv
+python ./gmScanMedDocs.py \
+	--conf-file=/home/ncq/.gnumed/gnumed-archiv.conf \
+	--text-domain=gnumed-archiv \
+	--log-file=/var/log/gnumed/archive-scan.log
+
+#=================================================================
+# $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/test-area/blobs_hilbert/scan/Attic/run-scanner.sh,v $
+# $Revision: 1.4 $
+
+# $Log: run-scanner.sh,v $
+# Revision 1.4  2002-11-20 12:56:47  ncq
+# - standard log file location
+#
