2d726ba6d641aadc30d83e349d7fc61331ce45d6
Karsten Hilbert <karsten.hilbert@gmx.net>
2002-11-30 11:10:42 +0000

    - log cvs update output for later reference

--- gnumed/update_tree.sh
+++ gnumed/update_tree.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/update_tree.sh,v $
-# $Revision: 1.1 $
+# $Revision: 1.2 $
 
 echo "cleaning out debris"
 find ./ -name '*.pyc' -exec rm -v '{}' ';'
 echo "updating python client source from CVS"
-cvs -z9 update
+cvs -z9 update | tee update.log
