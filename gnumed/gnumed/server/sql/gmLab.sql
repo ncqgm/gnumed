3299486c1e17754d29896ab87601146a5d9ebd78
Karsten Hilbert <karsten.hilbert@gmx.net>
2002-12-22 01:24:25 +0000

    - some changes suggested by Christof Meigen

--- gnumed/gnumed/server/sql/gmLab.sql
+++ gnumed/gnumed/server/sql/gmLab.sql
@@ -1,9 +1,9 @@
--- lab related tables
+-- GnuMed lab related tables
 
 -- author: Karsten Hilbert <Karsten.Hilbert@gmx.net>
 -- license: GPL
 -- $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/server/sql/Attic/gmLab.sql,v $
--- $Revision: 1.4 $
+-- $Revision: 1.5 $
 -- ===================================================================
 -- force terminate + exit(3) on errors if non-interactive
 \set ON_ERROR_STOP 1
@@ -19,6 +19,11 @@ CREATE TABLE episode_dummy (
 	dummy varchar(10)
 );
 
+create table dummy_clinical_transaction (
+	id serial primary key
+);
+
+-- ====================================
 CREATE TABLE lab (
 	id serial primary key,
 	practice_id INTEGER REFERENCES practice_dummy,
@@ -46,7 +51,7 @@ COMMENT ON COLUMN lab_test.long_name IS 'long name unmistakingly identifying thi
 -- =============================================
 CREATE TABLE lab_unit (
 	id serial primary key,
-	name CHARACTER VARYING(20)
+	name varchar(20)
 ) INHERITS (audit_identity);
 
 -- ====================================
@@ -61,53 +66,73 @@ CREATE TABLE lab_specific_test (
 	comment CHARACTER VARYING(60)
 ) INHERITS (audit_identity);
 
-COMMENT ON TABLE lab_specific_test IS 'description of a test as provided by a specific lab, this table can be built either from an ELV (Elektronisches LeistungsVerzeichnis = electronic methods directory) provided by the labs or dynamically during import of lab results, or manually of course';
-COMMENT ON COLUMN lab_specific_test.lab_id IS 'link to lab record';
-COMMENT ON COLUMN lab_specific_test.test_id IS 'link to test in our practice';
-COMMENT ON COLUMN lab_specific_test.short_name IS 'short name as provided by a specific lab';
-COMMENT ON COLUMN lab_specific_test.long_name IS 'long name as provided by a specific lab';
+COMMENT ON TABLE lab_specific_test IS
+	'description of a test as provided by a specific lab,
+	 this table can be built either from an ELV (Elektronisches LeistungsVerzeichnis = electronic methods directory)
+	 provided by the labs or dynamically during import of lab results, or manually of course';
+COMMENT ON COLUMN lab_specific_test.lab_id IS
+	'link to lab record';
+COMMENT ON COLUMN lab_specific_test.test_id IS
+	'link to test in our practice';
+COMMENT ON COLUMN lab_specific_test.short_name IS
+	'short name as provided by a specific lab';
+COMMENT ON COLUMN lab_specific_test.long_name IS
+	'long name as provided by a specific lab';
 
 -- ====================================
 CREATE TABLE lab_result (
 	pat_id INTEGER REFERENCES identity,
-	episode_id INTEGER REFERENCES episode_dummy default 0,
+	id_clinical_transaction INTEGER REFERENCES dummy_clinical_transaction,
+
 	test_id INTEGER REFERENCES lab_test,
 	lab_id INTEGER REFERENCES lab,
 	sample_id CHARACTER VARYING(25) NOT NULL default 'not available',
-	sample_date DATE NOT NULL,
-	result_date DATE NOT NULL,
+	result_date timestamp with timezone NOT NULL,
 	value CHARACTER(15) NOT NULL,
-	unit CHARACTER(10) NOT NULL REFERENCES lab_unit,
-	abnormal_tag CHARACTER(5),
+	unit varchar(20) NOT NULL REFERENCES lab_unit (name),
+
+	lab_abnormal_tag character(5),
 	lab_comment TEXT,
-	comment TEXT
+
+	doc_abnormal_flag boolean,
+	doc_comment TEXT
 ) INHERITS (audit_identity);
 -- episode 0 == 'unattached'
 
 COMMENT ON TABLE lab_result is 'one specific instance of lab_test';
 
 COMMENT ON COLUMN lab_result.pat_id IS 'the patient this test belongs to';
-COMMENT ON COLUMN lab_result.episode_id IS 'the episode of care this lab result belongs to';
+COMMENT ON COLUMN lab_result.id_clinical_transaction IS 'the clinical transaction this lab result belongs to';
 COMMENT ON COLUMN lab_result.test_id IS 'link to test in our practice';
 COMMENT ON COLUMN lab_result.lab_id IS 'the lab which provided this result';
 
 COMMENT ON COLUMN lab_result.sample_id IS 'ID this sample had in the lab or when sent to the lab';
-COMMENT ON COLUMN lab_result.sample_date IS 'date/time sample was taken';
 COMMENT ON COLUMN lab_result.result_date IS 'date/time result was produced';
+-- date/time sample was taken is in clinical_transaction->date
 
-COMMENT ON COLUMN lab_result.unit IS 'if we normalize this into it s own field we may later on be able to leverage it for clinical calculations';
-COMMENT ON COLUMN lab_result.abnormal_tag IS 'tag attached by the lab whether value is considered pathological';
-COMMENT ON COLUMN lab_result.lab_comment IS 'verbal evaluation of result by lab staff (doc, technician or comment such as "contaminated sample"';
-COMMENT ON COLUMN lab_result.comment IS 'what WE think about this result';
+COMMENT ON COLUMN lab_result.unit IS
+	'if we normalize this into it"s own field we may later on be able to leverage it for clinical calculations';
+COMMENT ON COLUMN lab_result.lab_abnormal_tag IS
+	'tag attached by the lab whether value is considered pathological';
+COMMENT ON COLUMN lab_result.lab_comment IS
+	'verbal evaluation of result by lab staff (doc, technician or comment such as "contaminated sample"';
+
+COMMENT ON COLUMN lab_result.doc_abnormal_flag IS
+	'whether the treating doctor considers this value clinically relevantly abnormal for this patient at this point in time';
+COMMENT ON COLUMN lab_result.doc_comment IS
+	'what the doc thinks about this result';
 
 -- =============================================
 -- do simple schema revision tracking
 \i gmSchemaRevision.sql
-INSERT INTO schema_revision (filename, version) VALUES('$RCSfile: gmLab.sql,v $', '$Revision: 1.4 $');
+INSERT INTO schema_revision (filename, version) VALUES('$RCSfile: gmLab.sql,v $', '$Revision: 1.5 $');
 
 -- =============================================
 -- $Log: gmLab.sql,v $
--- Revision 1.4  2002-12-01 13:53:09  ncq
+-- Revision 1.5  2002-12-22 01:24:25  ncq
+-- - some changes suggested by Christof Meigen
+--
+-- Revision 1.4  2002/12/01 13:53:09  ncq
 -- - missing ; at end of schema tracking line
 --
 -- Revision 1.3  2002/11/16 01:08:03  ncq
