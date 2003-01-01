b238f8c442383c597cc65d57d14f76b98251b956
Karsten Hilbert <karsten.hilbert@gmx.net>
2003-01-01 17:42:59 +0000

    - changed for new i18n

--- gnumed/gnumed/server/sql/country.specific/de/german-doc_types.sql
+++ gnumed/gnumed/server/sql/country.specific/de/german-doc_types.sql
@@ -1,41 +1,53 @@
 -- $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/server/sql/country.specific/de/Attic/german-doc_types.sql,v $
--- $Revision: 1.4 $
+-- $Revision: 1.5 $
 
 -- part of GnuMed
 -- GPL
 
--- doc types useful in Germany
+-- doc types translated into German
 -- run this _after_ gmBlobs.sql !
+-- ===================================================================
+-- do fixed string i18n()ing
+\i gmI18N.sql
+
 -- =============================================
 -- force terminate + exit(3) on errors if non-interactive
 \set ON_ERROR_STOP 1
 
 -- =============================================
-INSERT INTO doc_type(id, name) values(100, 'Entlassung Chirurgie');
-INSERT INTO doc_type(id, name) values(101, 'Entlassung Interne');
-INSERT INTO doc_type(id, name) values(102, 'Entlassung Psychatrie');
-INSERT INTO doc_type(id, name) values(103, 'Entlassung Reha');
-INSERT INTO doc_type(id, name) values(104, 'Entlassung Orthopädie');
+INSERT INTO i18n_translations(lang, orig, trans) values('de_DE', 'discharge summary internal', 'Entlassung Interne');
+INSERT INTO i18n_translations(lang, orig, trans) values('de_DE', 'discharge summary surgical', 'Entlassung Chirurgie');
+INSERT INTO i18n_translations(lang, orig, trans) values('de_DE', 'discharge summary psychiatric', 'Entlassung Psychiatrie');
+INSERT INTO i18n_translations(lang, orig, trans) values('de_DE', 'discharge summary neurological', 'Entlassung Neuro');
+INSERT INTO i18n_translations(lang, orig, trans) values('de_DE', 'discharge summary orthopaedic', 'Entlassung Ortho');
+INSERT INTO i18n_translations(lang, orig, trans) values('de_DE', 'discharge summary other', 'Entlassung ');
+--INSERT into _doc_type(name) values(103, 'Entlassung Reha');
 
-INSERT INTO doc_type(id, name) values(105, 'Arztbrief Chirurgie');
-INSERT INTO doc_type(id, name) values(106, 'Arztbrief Orthopädie');
-INSERT INTO doc_type(id, name) values(107, 'Arztbrief Innere');
-INSERT INTO doc_type(id, name) values(108, 'Arztbrief Neurologie');
-INSERT INTO doc_type(id, name) values(109, 'Arztbrief Psychotherapie');
-INSERT INTO doc_type(id, name) values(110, 'Arztbrief Radiologie');
-INSERT INTO doc_type(id, name) values(111, 'Arztbrief Umweltmedizin');
-INSERT INTO doc_type(id, name) values(112, 'Arztbrief Mikrobiologie');
-INSERT INTO doc_type(id, name) values(113, 'Labor');
-INSERT INTO doc_type(id, name) values(114, 'Befund Röntgen');
-INSERT INTO doc_type(id, name) values(115, 'Arztbrief Kardiologie');
+INSERT INTO i18n_translations(lang, orig, trans) values('de_DE', 'referral report internal', 'Arztbrief Innere');
+INSERT INTO i18n_translations(lang, orig, trans) values('de_DE', 'referral report surgical', 'Arztbrief Chirurgie');
+INSERT INTO i18n_translations(lang, orig, trans) values('de_DE', 'referral report ENT', 'Arztbrief HNO');
+INSERT INTO i18n_translations(lang, orig, trans) values('de_DE', 'referral report eye', 'Arztbrief Augen');
+INSERT INTO i18n_translations(lang, orig, trans) values('de_DE', 'referral report urology', 'Arztbrief Uro');
+INSERT INTO i18n_translations(lang, orig, trans) values('de_DE', 'referral report orthopaedic', 'Arztbrief Ortho');
+INSERT INTO i18n_translations(lang, orig, trans) values('de_DE', 'referral report neuro', 'Arztbrief Neuro');
+INSERT INTO i18n_translations(lang, orig, trans) values('de_DE', 'referral report radiology', 'Arztbrief Röntgen');
+INSERT INTO i18n_translations(lang, orig, trans) values('de_DE', 'referral report other', 'sonstiger Arztbrief');
+INSERT INTO i18n_translations(lang, orig, trans) values('de_DE', 'referral report cardiology', 'Arztbrief Kardio');
+INSERT INTO i18n_translations(lang, orig, trans) values('de_DE', 'referral report psychotherapy', 'Arztbrief Psychotherapie');
+--INSERT into _doc_type(name) values('Arztbrief Umweltmedizin');
+--INSERT into _doc_type(name) values('Arztbrief Mikrobiologie');
+--INSERT into _doc_type(name) values('Labor');
 
 -- do simple revision tracking
 \i gmSchemaRevision.sql
-INSERT INTO schema_revision (filename, version) VALUES('$RCSfile: german-doc_types.sql,v $', '$Revision: 1.4 $');
+INSERT INTO schema_revision (filename, version) VALUES('$RCSfile: german-doc_types.sql,v $', '$Revision: 1.5 $');
 
 -- =============================================
 -- $Log: german-doc_types.sql,v $
--- Revision 1.4  2002-12-26 15:52:28  ncq
+-- Revision 1.5  2003-01-01 17:42:59  ncq
+-- - changed for new i18n
+--
+-- Revision 1.4  2002/12/26 15:52:28  ncq
 -- - add two types
 --
 -- Revision 1.3  2002/12/01 13:53:09  ncq
