3a7dd7dc25f603c0b97584b9c762cd717e770011
Karsten Hilbert <karsten.hilbert@gmx.net>
2003-01-19 15:30:32 +0000

    - a few test users

---
+++ gnumed/gnumed/server/sql/gmTestAccounts.sql
@@ -0,0 +1,59 @@
+-- GnuMed database test accounts
+
+-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+-- !! NEVER EVER use those accounts/passwords in a  !!
+-- !! production database. As they are in CVS know- !!
+-- !! ledge about them is PUBLIC and thus extremely !!
+-- !! insecure. DO NOT USE FOR PRODUCTION.          !!
+-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+-- $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/server/sql/gmTestAccounts.sql,v $
+-- $Id: gmTestAccounts.sql,v 1.1 2003-01-19 15:30:32 ncq Exp $
+-- GPL
+-- author: Karsten.Hilbert@gmx.net
+-- ===================================================
+CREATE USER "test-doc"
+	WITH PASSWORD 'test-doc'
+	IN GROUP "gm-doctors, gm-public"
+	VALID UNTIL (CURRENT_DATE + interval '4 months')
+;
+CREATE USER "_test-doc"
+	WITH PASSWORD 'test-doc'
+	IN GROUP "gm-doctors, _gm-doctors, gm-public"
+	VALID UNTIL (CURRENT_DATE + interval '4 months')
+;
+
+-- ===================================================
+CREATE USER "test-nurse"
+	WITH PASSWORD 'test-nurse'
+	IN GROUP "gm-staff_medical, gm-public"
+	VALID UNTIL (CURRENT_DATE + interval '4 months')
+;
+CREATE USER "_test-nurse"
+	WITH PASSWORD 'test-nurse'
+	IN GROUP "gm-staff_medical, _gm-staff_medical, gm-public"
+	VALID UNTIL (CURRENT_DATE + interval '4 months')
+;
+
+-- ===================================================
+CREATE USER "test-secretary"
+	WITH PASSWORD 'test-secretary'
+	IN GROUP "gm-staff_office, gm-public"
+	VALID UNTIL (CURRENT_DATE + interval '4 months')
+;
+CREATE USER "_test-secretary"
+	WITH PASSWORD 'test-secretary'
+	IN GROUP "gm-staff_office, _gm-staff_office, gm-public"
+	VALID UNTIL (CURRENT_DATE + interval '4 months')
+;
+
+-- ===================================================
+-- do simple schema revision tracking
+\i gmSchemaRevision.sql
+INSERT INTO schema_revision (filename, version) VALUES('$RCSfile: gmTestAccounts.sql,v $', '$Revision: 1.1 $');
+
+-- ===================================================
+-- $Log: gmTestAccounts.sql,v $
+-- Revision 1.1  2003-01-19 15:30:32  ncq
+-- - a few test users
+--
