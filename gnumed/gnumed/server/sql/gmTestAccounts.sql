e218f9c218ca4a0bbd53282ae488ab36c632a7d9
Karsten Hilbert <karsten.hilbert@gmx.net>
2003-01-20 12:11:46 +0000

    - typo

--- gnumed/gnumed/server/sql/gmTestAccounts.sql
+++ gnumed/gnumed/server/sql/gmTestAccounts.sql
@@ -8,9 +8,13 @@
 -- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 -- $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/server/sql/gmTestAccounts.sql,v $
--- $Id: gmTestAccounts.sql,v 1.1 2003-01-19 15:30:32 ncq Exp $
+-- $Id: gmTestAccounts.sql,v 1.2 2003-01-20 12:11:46 ncq Exp $
 -- GPL
 -- author: Karsten.Hilbert@gmx.net
+-- ===================================================================
+-- force terminate + exit(3) on errors if non-interactive
+\unset ON_ERROR_STOP
+
 -- ===================================================
 CREATE USER "test-doc"
 	WITH PASSWORD 'test-doc'
@@ -50,10 +54,15 @@ CREATE USER "_test-secretary"
 -- ===================================================
 -- do simple schema revision tracking
 \i gmSchemaRevision.sql
-INSERT INTO schema_revision (filename, version) VALUES('$RCSfile: gmTestAccounts.sql,v $', '$Revision: 1.1 $');
+INSERT INTO schema_revision (filename, version) VALUES('$RCSfile: gmTestAccounts.sql,v $', '$Revision: 1.2 $');
+
+\set ON_ERROR_STOP 1
 
 -- ===================================================
 -- $Log: gmTestAccounts.sql,v $
--- Revision 1.1  2003-01-19 15:30:32  ncq
+-- Revision 1.2  2003-01-20 12:11:46  ncq
+-- - typo
+--
+-- Revision 1.1  2003/01/19 15:30:32  ncq
 -- - a few test users
 --
