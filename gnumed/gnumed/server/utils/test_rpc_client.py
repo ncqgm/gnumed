7deedd6d8712342a606789680ea8578886b78cdb
sjtan <sjtan>
2003-01-30 20:55:30 +0000

    using fork to load test. Need to delete from enum_confidentiality_level  otherwise the number of items retrieved slows things down (I think , its the xml marshalling that slows things down) .  What's the story about connection pooling - is it internal to postgres tcp interface?

--- gnumed/gnumed/server/utils/test_rpc_client.py
+++ gnumed/gnumed/server/utils/test_rpc_client.py
@@ -1,12 +1,16 @@
-"""This is a test client for server generated from gmclinical.sql with analyse_sql3.py"""
-
-
 from xmlrpclib import *
 import time
+import os
 s = Server( "http://localhost.localdomain:9000")
+for x in xrange(1, 100):
+	f = os.fork()
+	if f in (-1, 0):
+		break
 
 #print "fields are ", s.get_description('enum_confidentiality_level')
+
 r = s.describe_fields_enum_confidentiality_level()
+
 print "fields are", r
 s.create_enum_confidentiality_level("mid level" + time.strftime('%T'))
 
