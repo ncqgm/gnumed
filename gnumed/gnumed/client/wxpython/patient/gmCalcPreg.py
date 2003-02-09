c8b95377ccce89cb98e6f5345eb11fc9cdab97e4
michaelb <michaelb>
2003-02-09 05:13:52 +0000

    initialize due date (using LNMP = current date), clean-up of code assoc. with icon

--- gnumed/gnumed/client/wxpython/patient/gmCalcPreg.py
+++ gnumed/gnumed/client/wxpython/patient/gmCalcPreg.py
@@ -7,8 +7,8 @@
 # 11/7/02: inital version
 #====================================================================
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/wxpython/patient/Attic/gmCalcPreg.py,v $
-# $Id: gmCalcPreg.py,v 1.13 2003-02-03 16:32:34 ncq Exp $
-__version__ = "$Revision: 1.13 $"
+# $Id: gmCalcPreg.py,v 1.14 2003-02-09 05:13:52 michaelb Exp $
+__version__ = "$Revision: 1.14 $"
 __author__ = "I.Haywood, M.Bonert"
 
 from wxPython.wx import *
@@ -29,11 +29,8 @@ GESTATION = 24192000
 WEEK = 604800
 DAY = 86400
 #====================================================================
-class PregnancyFrame (wxFrame):
-	"""
-	Frame class to show dates.
-	"""
-	__icons = {"""icon_Preg_calc""":
+
+_icons = {"""icon_Preg_calc""":
 'x\xdaMP1\x0e\x830\x0c\xdcy\x85\xa5\x0et\xb2`h\x95\xb9H\xac\x0c,^\x11c\x91\
 \xdc\xffO\xbd\xb3C\xc0\xb1\x02w\xf1]\xec<\x8f\xdf\xd8\xad\xfd\xf8\x16\xe4K\
 \xc6\xbe\xdb\xd6\xded\x97\xcf\xb1\xed\xdf@\x0e\xf4\x98\x06\xae\xc0J\\\x06\
@@ -42,14 +39,20 @@ class PregnancyFrame (wxFrame):
 \xdcje\xa8\xa3\x94\xfaS\xeeI\xe4mv\xde\xae\xd9\xd2\x02\xcb[\xf3\x9ar\xf56Q\
 \xb0\x11\xe4\xeec\xfa\xe9\x9c$\xa7`\x03No|\xda\xd3]\xe1|:\xfd\x03\xab\xf8h\
 \xbf' }
+
+class PregnancyFrame (wxFrame):
+	"""
+	Frame class to show dates.
+	"""
+
 	def __init__ (self, parent):
 		wxFrame.__init__(self, parent, -1, _("Pregnancy Calculator"))
 
 		icon = wxEmptyIcon()
-		icon.CopyFromBitmap(self.getBitmap())
+		icon.CopyFromBitmap(wxBitmapFromXPMData(cPickle.loads(zlib.decompress( _icons[_("""icon_Preg_calc""")] ))) )
 		self.SetIcon(icon)
 
-		# create the text label 'LNMP' 
+		# create the text label 'LNMP'
 		self.dyntxt = wxStaticText (self, -1, _('LNMP'),(5,5))
 
 		vbox = wxBoxSizer (wxVERTICAL)
@@ -65,10 +68,11 @@ class PregnancyFrame (wxFrame):
 		self.gest_day_ctrl = wxSpinCtrl (self, ID_DAY, value = "0", min = 0, max = 6)
 		hbox.Add (self.gest_day_ctrl, 1, wxALIGN_CENTRE, 15)
 		vbox.Add (hbox, 0, wxALL, 10)
-		
+
 		vbox.Add (wxStaticText (self, -1, _('Due date')), 0, wxALL, 5)
 		self.due_cal = wxCalendarCtrl (self, ID_DUE)
 		vbox.Add (self.due_cal, 0, wxALL, 10)
+		self.OnCalcByLNMP(self)		# initialize due date for LNMP = current date
 
 		self.SetSizer (vbox)
 		self.SetAutoLayout (1)
@@ -79,13 +83,6 @@ class PregnancyFrame (wxFrame):
 		EVT_SPINCTRL (self.gest_day_ctrl, ID_DAY, self.OnCalcByGest)
 		EVT_CLOSE (self, self.OnClose )
 		self.Show(1)
-
-
-	def getBitmap (self):
-		# used for:
-		# (1) defintion the window icon
-		# (2) defintion of button used to activate the 'Pregnancy calculator'
-		return wxBitmapFromXPMData(cPickle.loads(zlib.decompress( self.__icons[_("""icon_Preg_calc""")] )))
 
 	def OnClose (self, event):
 		self.Destroy ()
@@ -155,7 +152,7 @@ else:
 
 	ID_CalcPreg_BUTTON = wxNewId()
 
-	class gmCalcPreg (gmPlugin.wxBasePlugin, PregnancyFrame): # Inherit 'PregnancyFrame' so 'getBitmap()' is available
+	class gmCalcPreg (gmPlugin.wxBasePlugin):
 		def name (self):
 			return 'Pregnancy Calculator'
 		#---------------------
@@ -167,7 +164,7 @@ else:
 			self.tool = wxToolBar (self.tb, -1, style=wxTB_HORIZONTAL|wxNO_BORDER|wxTB_FLAT)
 			self.tool.AddTool(
 				ID_CalcPreg_BUTTON,
-				self.getBitmap(),
+				wxBitmapFromXPMData(cPickle.loads(zlib.decompress( _icons[_("""icon_Preg_calc""")] ))),
 				shortHelpString = _("Pregnancy Calculator")
 			)
 			self.tb.AddWidgetRightBottom (self.tool)
@@ -183,7 +180,10 @@ else:
 
 #=====================================================================
 # $Log: gmCalcPreg.py,v $
-# Revision 1.13  2003-02-03 16:32:34  ncq
+# Revision 1.14  2003-02-09 05:13:52  michaelb
+# initialize due date (using LNMP = current date), clean-up of code assoc. with icon
+#
+# Revision 1.13  2003/02/03 16:32:34  ncq
 # - cleaned up handling of self.dyntxt vs. self.handle4dyntxt
 #
 # Revision 1.12  2003/01/24 05:28:15  michaelb
