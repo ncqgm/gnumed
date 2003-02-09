a87f8a6b040c624e325c12ef7a93be708f0836d2
michaelb <michaelb>
2003-02-09 21:12:39 +0000

    removed trickery to get motion, made easy to edit list of names

--- gnumed/gnumed/client/wxpython/gmAbout.py
+++ gnumed/gnumed/client/wxpython/gmAbout.py
@@ -6,7 +6,7 @@
 # Changelog:
 # 30/01/03: inital version
 #====================================================================
-__version__ = "$Revision: 1.2 $"
+__version__ = "$Revision: 1.3 $"
 __author__ = "M.Bonert"
 
 from wxPython.wx import *
@@ -23,24 +23,47 @@ class ScrollTxtWin (wxWindow):
 	Scrolling Text!
 	"""
 
-	__scroll_ctr=-101
+	# control parameters
+	__scroll_speed=.3 	# pixels/milliseconds (?)
+	__delay=500		# milliseconds
+	name_list=['Dr Gerardo Arnaez','Dr Hilmar Berger', 'Michael Bonert', 'Dr Elizabeth Dodd', \
+'Engelbert Gruber', 'Ian Haywood', 'Dr Richard Terry', 'Thierry Michel', 'Andreas Tille' ]
+
+	# initializations
+	__scroll_ctr=-230
+	__name_ctr=1
+	__delay_ctr=1
 
 	def __init__ (self, parent):
-		#pass
 		wxWindow.__init__(self, parent, -1, size=(230,20), style=wxSUNKEN_BORDER)
 		self.SetBackgroundColour(wxColour(255, 255, 255))
+		self.__delay_ctr_reset=self.__delay*self.__scroll_speed
 
-		self.changing_txt=wxStaticText(self, -1, "Hilmar Berger", size=(240,20))
+		self.moving_txt=wxStaticText(self, -1, "", size=(230,20), style=wxALIGN_CENTRE | wxST_NO_AUTORESIZE)
+		self.moving_txt.SetLabel(self.name_list[0])
 
 		EVT_TIMER(self, -1, self.OnTimer)
 		self.timer = wxTimer(self, -1)
-		self.timer.Start(10)
+		#self.timer.Start(self.__scroll_speed)
+		self.timer.Start(1./self.__scroll_speed)
 
 	def OnTimer(self, evt):
-		self.changing_txt.MoveXY(self.__scroll_ctr, 0)
-		self.__scroll_ctr=self.__scroll_ctr+1
-		if(self.__scroll_ctr>253):
-			self.__scroll_ctr=0
+		if(self.__scroll_ctr>-2 and self.__delay_ctr<self.__delay_ctr_reset):
+			# pause at centre
+			self.__delay_ctr=self.__delay_ctr+1
+		else:
+			self.__scroll_ctr=self.__scroll_ctr+1
+			self.moving_txt.MoveXY(self.__scroll_ctr, 0)
+		if(self.__scroll_ctr>230):
+			# reset counters
+			self.__scroll_ctr=-230
+			self.__delay_ctr=1
+
+			# get next name in dict.
+			self.moving_txt.SetLabel(self.name_list[self.__name_ctr])
+			self.__name_ctr=self.__name_ctr+1
+			if(self.__name_ctr>len(self.name_list)-1):
+				self.__name_ctr=0
 
 class AboutFrame (wxFrame):
 	"""
@@ -54,8 +77,6 @@ class AboutFrame (wxFrame):
 \xcf\xf8ye\xd0\x00\x90\x0etH \x84\x80B\xaa\x8a\x88\x85\xc4(U\x9d$\xfeR;\xc5J\
 \xa6\x01\xbbt9\xceR\xc8\x81e_$\x98\xb9\x9c\xa9\x8d,y\xa9t\xc8\xcf\x152\xe0x\
 \xe9$\xf5\x07\x95\x0cD\x95t:\xb1\x92\xae\x9cI\xa8~\x84\x1f\xe0\xa3ec'
-
-
 
 	def __init__(self, parent, ID, title, pos=wxDefaultPosition, size=wxDefaultSize, style=wxDEFAULT_FRAME_STYLE):
 		wxFrame.__init__(self, parent, ID, title, pos, size, style)
@@ -92,8 +113,7 @@ class AboutFrame (wxFrame):
  		self.Layout()
 
 	def OnClose (self, event):
-		###self.timer.Stop ()
-		#ScrollTxtWin.timer.Stop()   # FIX THIS?
+		self.win.timer.Stop ()
 		self.Destroy ()
 
 
