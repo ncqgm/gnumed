443fb78916738419b13b58fd230600cb0e527aa2
sjtan <sjtan>
2003-02-11 12:16:48 +0000

    a little more linked into gmGuiMain at 2 places, on initialization , and on closing. Closing signals handler_loader to try and save any persistent objects, as gmDemographics as an example. I tried shelve and it gave my an insecure string pickle error which I couldn't fix, so I'm manually using cPickle for the example. Periodic saving in another thread could use this persistence method to reload a user interface after unexpected errors, windows freezing etc. Can also use this persistence method to implement client caching for high availability disconnected operation : e.g. all patient objects are loaded onto a client when appointments have been made, and updated by client server polling. If network partition ( breakdown occurs) users can still operate on the local cache of patient objects , based on the appointments.

---
+++ gnumed/gnumed/client/wxpython/handler_loader.py
@@ -0,0 +1,71 @@
+"""loads handler onto widgets stored in widget map of guiBroker."""
+
+import gmGuiBroker 
+import EditAreaHandler
+from handler_gmSelectPersonImpl import *
+from handler_patient import *
+import cPickle 
+
+def configure_handlers():
+	gb = gmGuiBroker.GuiBroker()
+	gb['DlgSelectPerson_handler'] = gmSelectPerson_handler_impl()
+	handler = gmDemographics_handler(None)
+	try :
+		model = cPickle.load(file('gmDemographics', 'r'))
+	except:
+		model = {}
+		
+	handler.set_model( model) 
+
+	gb['models'] = {}
+	gb['models']['gmDemographics'] = model
+	gb['PatientsPanel_handler'] = handler
+
+def save_models():
+	gb = gmGuiBroker.GuiBroker()
+	
+	cPickle.dump(gb['models']['gmDemographics'] ,  file('gmDemographics', 'w'))
+
+def load_widget(widget, model):
+	if model <> None:
+		wkeys = widget.__dict__.keys()
+		for k in model.keys():
+			if model[k] <> None:
+				pass
+				# code to compare the widget names
+				
+	
+
+def load_handlers():
+	gb = gmGuiBroker.GuiBroker()
+	if gb.has_key('widgets'):
+		map = gb['widgets']
+		for k, widget  in map.items():
+			print "widget found = ",k, widget
+			if widget.__dict__.has_key('editarea'):
+				#all set to put in hooks
+				section = widget.editarea.rightside.section
+				section_name =  EditAreaHandler.section_num_map[section]
+				print "section = %d, or section name=%s\n" % (section, section_name) 
+
+
+				# get a registered handler, and use it's cloning method create_handler to clone another handler for this editarea.rightside
+				gb = gmGuiBroker.GuiBroker()
+				
+				if  gb.has_key( section_name ):
+					gb[section_name].create_handler(widget.editarea.rightside) 	
+					print "**** HANDLER IS IN ! (but it does nothing)"
+					continue
+				# remove this later, into main directory code
+
+			handler_name = widget.__class__.__name__ + "_handler" 
+			if gb.has_key( handler_name ):
+				handler = gb[handler_name]
+				handler.create_handler(widget)
+				load_widget(widget, handler.model)
+			continue
+
+def main():
+	configure_handlers()
+	load_handlers()	
+
