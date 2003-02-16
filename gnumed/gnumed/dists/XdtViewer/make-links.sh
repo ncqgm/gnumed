2753b45a8b5e684f1fceed24e1fb60b49cd35533
Karsten Hilbert <karsten.hilbert@gmx.net>
2003-02-16 14:07:03 +0000

    - add link to translation

--- gnumed/gnumed/dists/XdtViewer/make-links.sh
+++ gnumed/gnumed/dists/XdtViewer/make-links.sh
@@ -2,7 +2,7 @@
 
 #========================================================
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/dists/XdtViewer/Attic/make-links.sh,v $
-# $Id: make-links.sh,v 1.1 2003-02-15 17:14:32 ncq Exp $
+# $Id: make-links.sh,v 1.2 2003-02-16 14:07:03 ncq Exp $
 
 echo "making links"
 ln -vfs ../../client/wxpython/gui/gmXdtViewer.py gmXdtViewer.py
@@ -13,9 +13,17 @@ ln -vfs ../../../client/python-common/gmI18N.py modules/gmI18N.py
 ln -vfs ../../../client/python-common/gmLog.py modules/gmLog.py
 ln -vfs ../../../client/business/gmXdtMappings.py modules/gmXdtMappings.py
 
+mkdir locale
+mkdir locale/de_DE@euro
+mkdir locale/de_DE@euro/LC_MESSAGES
+ln -vfs ../../../../../client/locale/de-gnumed.mo locale/de_DE@euro/LC_MESSAGES/gnumed.mo
+
 echo "done"
 #========================================================
 # $Log: make-links.sh,v $
-# Revision 1.1  2003-02-15 17:14:32  ncq
+# Revision 1.2  2003-02-16 14:07:03  ncq
+# - add link to translation
+#
+# Revision 1.1  2003/02/15 17:14:32  ncq
 # - first check in
 #
