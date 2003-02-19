97baed5a78421dcbaf3de5dbf77a10dcebd182b0
Karsten Hilbert <karsten.hilbert@gmx.net>
2003-02-19 15:43:17 +0000

    - add locale

--- gnumed/gnumed/dists/XdtViewer/make-dist.sh
+++ gnumed/gnumed/dists/XdtViewer/make-dist.sh
@@ -2,14 +2,17 @@
 
 #========================================================
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/dists/XdtViewer/Attic/make-dist.sh,v $
-# $Id: make-dist.sh,v 1.1 2003-02-15 17:14:32 ncq Exp $
+# $Id: make-dist.sh,v 1.2 2003-02-19 15:43:17 ncq Exp $
 
 echo "making tgz"
-tar -cvzhf gnumed-XdtViewer.tgz gmXdtViewer.py run-xdt-viewer.sh modules
+tar -cvzhf gnumed-XdtViewer.tgz gmXdtViewer.py run-xdt-viewer.sh modules locale
 echo "done"
 
 #========================================================
 # $Log: make-dist.sh,v $
-# Revision 1.1  2003-02-15 17:14:32  ncq
+# Revision 1.2  2003-02-19 15:43:17  ncq
+# - add locale
+#
+# Revision 1.1  2003/02/15 17:14:32  ncq
 # - first check in
 #
