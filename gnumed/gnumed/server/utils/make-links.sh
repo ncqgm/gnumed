48b5afff0b4767009d07d3e97e288eb5d94ae75f
Karsten Hilbert <karsten.hilbert@gmx.net>
2003-02-21 14:59:00 +0000

    - create links modules and sql

---
+++ gnumed/gnumed/server/utils/make-links.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/server/utils/Attic/make-links.sh,v $
+# $Id: make-links.sh,v 1.1 2003-02-21 14:59:00 ncq Exp $
+
+echo "setting up links"
+
+ln -vfs ../../client/python-common modules
+ln -vfs ../sql sql
