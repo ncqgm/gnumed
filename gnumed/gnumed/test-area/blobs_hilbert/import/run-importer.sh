4bb30927f378623633b27f72e487f30df95c5781
Karsten Hilbert <karsten.hilbert@gmx.net>
2003-02-23 20:39:12 +0000

    - eating your own dog-food makes you find lots of inconsistencies

--- gnumed/gnumed/test-area/blobs_hilbert/import/run-importer.sh
+++ gnumed/gnumed/test-area/blobs_hilbert/import/run-importer.sh
@@ -9,15 +9,24 @@
 # if you need to set a special base directory for some reason
 #IMPORT-MED_DOCS_DIR = ""
 
-python ./import-med_docs.py \
-	--conf-file=/etc/gnumed/gnumed-archive.conf \
-	--log-file=/var/log/gnumed/archive-import.log
+LOG=/var/log/gnumed/archive-import.log
+CFG=/etc/gnumed/gnumed-archive.conf
+
+echo > $LOG
+cd /usr/bin
+import-med_docs.py \
+ --conf-file=$CFG \
+ --log-file=$LOG
+cd -
 
 #============================================================
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/test-area/blobs_hilbert/import/Attic/run-importer.sh,v $
-# $Revision: 1.1 $
+# $Revision: 1.2 $
 
 # $Log: run-importer.sh,v $
-# Revision 1.1  2002-12-26 15:54:19  ncq
+# Revision 1.2  2003-02-23 20:39:12  ncq
+# - eating your own dog-food makes you find lots of inconsistencies
+#
+# Revision 1.1  2002/12/26 15:54:19  ncq
 # - initial checkin
 #
