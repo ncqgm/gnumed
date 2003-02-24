b2180fb0cf9e6d0d7c3ab0f7585d93e81ffab570
Karsten Hilbert <karsten.hilbert@gmx.net>
2003-02-24 23:10:21 +0000

    - darn it, we do need modules on the server !! - I should also check whether bootstrap* fails ...

--- gnumed/gnumed/test-area/blobs_hilbert/server/install.sh
+++ gnumed/gnumed/test-area/blobs_hilbert/server/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/test-area/blobs_hilbert/server/Attic/install.sh,v $
-# $Revision: 1.3 $
+# $Revision: 1.4 $
 
 echo "this must be run as root because we will put some links in /usr/bin/ and other places"
 
@@ -9,10 +9,14 @@ echo "this must be run as root because we will put some links in /usr/bin/ and o
 groupadd gnumed
 
 # boostrap database
+# FIXME: check returned result: 1 == failed
 ./bootstrap-gm_db_system.py --conf-file=bootstrap-archive.conf
 
 # install binaries
 install -v -g gnumed -m 0750 -b import-med_docs.py run-importer.sh /usr/bin/
+
+# FIXME: install modules
+#install -v -g gnumed -m 0750 -d /
 
 # install message catalog
 echo "no need for message catalog installation"
@@ -41,7 +45,11 @@ echo "you should configure your system in /etc/gnumed/gnumed-archive.conf"
 
 #=============================================================
 # $Log: install.sh,v $
-# Revision 1.3  2003-02-02 14:08:49  ncq
+# Revision 1.4  2003-02-24 23:10:21  ncq
+# - darn it, we do need modules on the server !!
+# - I should also check whether bootstrap* fails ...
+#
+# Revision 1.3  2003/02/02 14:08:49  ncq
 # - updated install
 # - new bootstrap config file
 #
