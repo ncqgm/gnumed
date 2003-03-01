2680f557caea8a01ebb8bad360f8b0db765e8425
Karsten Hilbert <karsten.hilbert@gmx.net>
2003-03-01 15:07:39 +0000

    - rework

--- gnumed/gnumed/Archive/server/dropBlops.sql
+++ gnumed/gnumed/Archive/server/dropBlops.sql
@@ -4,7 +4,19 @@
 -- author: Karsten Hilbert <Karsten.Hilbert@gmx.net>
 
 -- $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/Archive/server/Attic/dropBlops.sql,v $
--- $Revision: 1.1 $ $Date: 2003-03-01 15:01:10 $ $Author: ncq $
+-- $Revision: 1.2 $ $Date: 2003-03-01 15:07:39 $ $Author: ncq $
 
-drop table doc_type, doc_med, doc_med_external_ref, doc_obj, doc_desc;
-drop sequence doc_type_id_seq, doc_med_id_seq;
+drop table
+	doc_type,
+	doc_med,
+	doc_med_external_ref,
+	doc_obj,
+	doc_desc
+;
+
+drop sequence
+	doc_type_id_seq,
+	doc_med_id_seq,
+	dob_obj_id_seq,
+	doc_desc_id_seq
+;
