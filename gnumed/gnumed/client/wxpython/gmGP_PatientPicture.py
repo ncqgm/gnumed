eebac6936c15dee9a71e9b5d9b9ebaf64dcffc6c
Karsten Hilbert <karsten.hilbert@gmx.net>
2003-03-29 13:43:30 +0000

    - make standalone work, CVS keywords, general cleanup - change from wxPanel to wxStaticBitmap; load PNG, BMP, GIP automagically - alleviate sizer hell

--- gnumed/gnumed/client/wxpython/gmGP_PatientPicture.py
+++ gnumed/gnumed/client/wxpython/gmGP_PatientPicture.py
@@ -1,31 +1,52 @@
-from wxPython.wx import *
-import gmGuiBroker
-import os.path
 #--------------------------------
 #embryonic gmGP_PatientPicture.py
-#--------------------------------
+############################################################################
+# $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/wxpython/Attic/gmGP_PatientPicture.py,v $
+# $Id: gmGP_PatientPicture.py,v 1.2 2003-03-29 13:43:30 ncq Exp $
+__version__ = "$Revision: 1.2 $"
+__author__  = "R.Terry <rterry@gnumed.net>,\
+			   I.Haywood <i.haywood@ugrad.unimelb.edu.au>,\
+			   K.Hilbert <Karsten.Hilbert@gmx.net>"
 
-class PatientPicture(wxPanel):
-    def __init__(self, parent, id):
-    	wxPanel.__init__(self, parent, id, wxDefaultPosition, wxDefaultSize, wxSUNKEN_BORDER )	
-	
-	self.sizer = wxBoxSizer(wxVERTICAL)
-	#AT A BITMAP FOR PATIENT PICTURE
-	wxImage_AddHandler(wxPNGHandler())
-        bitmap = os.path.join (gmGuiBroker.GuiBroker ()['gnumed_dir'], 'bitmaps', 'any_body2.png')
-	png = wxImage(bitmap, wxBITMAP_TYPE_PNG).ConvertToBitmap()
-	bmp = wxStaticBitmap(self, -1, png, wxPoint(0, 0), wxSize(png.GetWidth(), png.GetHeight()))
-	self.sizer.Add(bmp, 0, wxGROW|wxALIGN_CENTER_VERTICAL) # |wxALL, 10)
-        self.SetSizer(self.sizer)  #set the sizer 
-	self.sizer.Fit(self)             #set to minimum size as calculated by sizer
-        self.SetAutoLayout(true)                 #tell frame to use the sizer
-        self.Show(true)
-	
+import sys, os.path
+if __name__ != "__main__":
+	import gmGuiBroker
 
-     
+from wxPython.wx import *
+#====================================================
+class cPatientPicture (wxStaticBitmap):
+	"""A patient picture control ready for display.
+	"""
+	def __init__(self, parent, id):
+
+		# just in case
+		wxImage_AddHandler(wxPNGHandler())
+		pic_name = os.path.join(gmGuiBroker.GuiBroker()['gnumed_dir'], 'bitmaps', 'any_body2.png')
+		img_data = wxImage(pic_name, wxBITMAP_TYPE_ANY)
+		bmp_data = wxBitmapFromImage(img = img_data)
+		del img_data
+		wxStaticBitmap.__init__(
+			self,
+			parent,
+			id,
+			bmp_data,
+			wxPoint(0, 0),
+			wxSize(bmp_data.GetWidth(), bmp_data.GetHeight())
+		)
+
+		# FIXME: register interest in patient change so we can load pic
+
+#====================================================
+# main
+#----------------------------------------------------
 if __name__ == "__main__":
 	app = wxPyWidgetTester(size = (200, 200))
-	app.SetWidget(PatientPicture, -1)
+	app.SetWidget(cPatientPicture, -1)
 	app.MainLoop()
-           
- 
+#====================================================
+# $Log: gmGP_PatientPicture.py,v $
+# Revision 1.2  2003-03-29 13:43:30  ncq
+# - make standalone work, CVS keywords, general cleanup
+# - change from wxPanel to wxStaticBitmap; load PNG, BMP, GIP automagically
+# - alleviate sizer hell
+#
