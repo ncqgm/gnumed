4239d4ad6b8a48028cddbf15922d27376f8a5586
Karsten Hilbert <karsten.hilbert@gmx.net>
2003-04-04 20:43:01 +0000

    - install new patient search widget - rework to be a more invariant top pane less dependant on gmDemographics - file should be renamed to gmTopPane.py

--- gnumed/gnumed/client/wxpython/gmGP_Toolbar.py
+++ gnumed/gnumed/client/wxpython/gmGP_Toolbar.py
@@ -2,23 +2,38 @@
 # GPL
 
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/wxpython/Attic/gmGP_Toolbar.py,v $
-__version__ = "$Revision: 1.12 $"
+__version__ = "$Revision: 1.13 $"
 __author__  = "R.Terry <rterry@gnumed.net>, I.Haywood <i.haywood@ugrad.unimelb.edu.au>"
 #===========================================================
-import sys, os.path
+import sys, os.path, cPickle, zlib
 if __name__ == "__main__":
 	sys.path.append(os.path.join('..', 'python-common'))
 
 import gmLog, gmGuiBroker
-import gmGP_PatientPicture
+import gmGP_PatientPicture, gmPatientSelector
 
 from wxPython.wx import *
 
+ID_BTN_pat_demographics = wxNewId()
+
 # FIXME: need a better name here !
 bg_col = wxColour(214,214,214)
+fg_col = wxColour(0,0,131)
+col_brightred = wxColour(255,0,0)
 #===========================================================
 class cMainTopPanel(wxPanel):
-	def __init__(self, parent,id):
+
+	__icons = {
+"""icon_binoculars_form""": 'x\xdam\x8e\xb1\n\xc4 \x0c\x86\xf7>\x85p\x83\x07\x01\xb9.\xa7\xb3\x16W\x87.]\
+K\xc7+x\xef?]L\xa2\xb5r!D\xbe\x9f/\xc1\xe7\xf9\x9d\xa7U\xcfo\x85\x8dCO\xfb\
+\xaaA\x1d\xca\x9f\xfb\xf1!RH\x8f\x17\x96u\xc4\xa9\xb0u6\x08\x9b\xc2\x8b[\xc2\
+\xc2\x9c\x0bG\x17Cd\xde\n{\xe7\x83wr\xef*\x83\xc5\xe1\xa6Z_\xe1_3\xb7\xea\
+\xc3\x94\xa4\x07\x13\x00h\xdcL\xc8\x90\x12\x8e\xd1\xa4\xeaM\xa0\x94\xf7\x9bI\
+\x92\xa8\xf5\x9f$\x19\xd69\xc43rp\x08\xb3\xac\xe7!4\xf5\xed\xd7M}kx+\x0c\xcd\
+\x0fE\x94aS'
+}
+
+	def __init__(self, parent, id):
 
 		gb = gmGuiBroker.GuiBroker ()
 		wxPanel.__init__(self, parent, id, wxDefaultPosition, wxDefaultSize, wxRAISED_BORDER)
@@ -31,22 +46,73 @@ class cMainTopPanel(wxPanel):
 		# |         | toolbar              |
 		# `--------------------------------'
 
-		# create patient picture
-		self.patient_picture = gmGP_PatientPicture.cPatientPicture(self, -1)
-		gb['main.patient_picture'] = self.patient_picture
-
 		# create toolbars
-		# - top (content is added elsewhere ...)
-		self.szr_top_tb = wxBoxSizer (wxHORIZONTAL)
-		# - bottom (holds most of the buttons)
-		self.szr_bottom_tb = wxBoxSizer (wxHORIZONTAL)
-		self.bottomline = wxPanel(self, -1)
-		self.szr_bottom_tb.Add (self.bottomline, 1, wxGROW)
+		# - top row
+		# .--------------------------------------.
+		# | details | patient  | age | allergies |
+		# | button  | selector |     |           |
+		# `--------------------------------------'
+		self.szr_top_row = wxBoxSizer (wxHORIZONTAL)
+
+		#  - details button
+		bmp = wxBitmapFromXPMData(cPickle.loads(zlib.decompress(self.__icons[_("icon_binoculars_form")])))
+		self.btn_pat_demographics = wxBitmapButton (
+			parent = self,
+			id = ID_BTN_pat_demographics,
+			bitmap = bmp,
+			style = wxBU_AUTODRAW
+		)
+		self.btn_pat_demographics.SetToolTip(wxToolTip(_("display patient demographics")))
+		self.szr_top_row.Add (self.btn_pat_demographics, 0, wxEXPAND)
+		EVT_BUTTON(self, ID_BTN_pat_demographics, self.__on_display_demographics)
+
+#		self.tool_patient_search = self.tb_patient_search.AddTool (
+#			ID_BUTTONFINDPATIENT,
+#			getToolbar_FindPatientBitmap(),
+#			shortHelpString = _("Patient Details")
+#		)
+#		EVT_TOOL (self.tb_patient_search, ID_BUTTONFINDPATIENT, self.OnTool)
+
+		#  - patient selector
+		self.patient_selector = gmPatientSelector.cPatientSelector(self, -1)
+		self.patient_selector.SetFont(wxFont(12, wxSWISS, wxNORMAL, wxBOLD, false, ''))
+		self.szr_top_row.Add (self.patient_selector, 5, wxEXPAND, 3)
+		#  - age
+		self.lbl_age = wxStaticText (self, -1, _("Age"), style = wxALIGN_CENTER_VERTICAL)
+		self.lbl_age.SetFont (wxFont(12,wxSWISS,wxNORMAL,wxBOLD,false,''))
+#		self.lbl_age.SetForegroundColour (fg_col)
+		self.txt_age = wxTextCtrl (self, -1, "", size = (40,-1), style = wxTE_READONLY)
+		self.txt_age.SetFont (wxFont(12, wxSWISS, wxNORMAL, wxBOLD, false, ''))
+		self.txt_age.SetBackgroundColour(bg_col)
+		self.szr_top_row.Add (self.lbl_age, 0, wxEXPAND | wxALIGN_CENTER_VERTICAL | wxALL, 3)
+		self.szr_top_row.Add (self.txt_age, 0, wxEXPAND | wxALL, 3)
+		#  - allergies
+		self.lbl_allergies = wxStaticText (self, -1, _("Allergies"), style = wxALIGN_CENTER_VERTICAL)
+		self.lbl_allergies.SetFont(wxFont(12,wxSWISS,wxNORMAL,wxBOLD,false,''))
+		self.lbl_allergies.SetBackgroundColour(bg_col)
+		self.lbl_allergies.SetForegroundColour(col_brightred)
+		self.txt_allergies = wxTextCtrl (self, -1, "", style = wxTE_READONLY)
+		self.txt_allergies.SetFont(wxFont(12,wxSWISS,wxNORMAL, wxBOLD,false,''))
+		self.txt_allergies.SetBackgroundColour(bg_col)
+		self.txt_allergies.SetForegroundColour (col_brightred)
+		self.szr_top_row.Add (self.lbl_allergies, 0, wxEXPAND | wxALIGN_CENTER_VERTICAL | wxALL, 3)
+		self.szr_top_row.Add (self.txt_allergies, 6, wxEXPAND | wxALL, 3)
+
+		# - bottom row
+		# (holds most of the buttons)
+		self.szr_bottom_row = wxBoxSizer (wxHORIZONTAL)
+		self.pnl_bottom_row = wxPanel(self, -1)
+		self.szr_bottom_row.Add (self.pnl_bottom_row, 1, wxGROW)
+
 		# - stack them atop each other
 		self.szr_toolbars = wxBoxSizer(wxVERTICAL)
 		self.szr_toolbars.Add(1, 3, 0, wxEXPAND)	# ??? (IMHO: space is at too much of a premium for such padding)
-		self.szr_toolbars.Add(self.szr_top_tb, 1, wxEXPAND)
-		self.szr_toolbars.Add(self.szr_bottom_tb, 1, wxEXPAND | wxALL, 2)
+		self.szr_toolbars.Add(self.szr_top_row, 1, wxEXPAND)
+		self.szr_toolbars.Add(self.szr_bottom_row, 1, wxEXPAND | wxALL, 2)
+
+		# create patient picture
+		self.patient_picture = gmGP_PatientPicture.cPatientPicture(self, -1)
+		gb['main.patient_picture'] = self.patient_picture
 
 		# create main sizer
 		self.szr_main = wxBoxSizer(wxHORIZONTAL)
@@ -63,20 +129,27 @@ class cMainTopPanel(wxPanel):
 		self.SetAutoLayout(true)		#tell frame to use the sizer
 		self.Show(true)
 	#-------------------------------------------------------
+	# event handlers
+	#-------------------------------------------------------
+	def __on_display_demographics(self, evt):
+		print "display patient demographic window now"
+	#-------------------------------------------------------
+	# remote layout handling
+	#-------------------------------------------------------
 	def AddWidgetRightBottom (self, widget):
 		"""Insert a widget on the right-hand side of the bottom toolbar.
 		"""
-		self.szr_bottom_tb.Add(widget, 0, wxRIGHT, 0)
+		self.szr_bottom_row.Add(widget, 0, wxRIGHT, 0)
 	#-------------------------------------------------------
 	def AddWidgetLeftBottom (self, widget):
 		"""Insert a widget on the left-hand side of the bottom toolbar.
 		"""
-		self.szr_bottom_tb.Prepend(widget, 0, wxALL, 0)
+		self.szr_bottom_row.Prepend(widget, 0, wxALL, 0)
 	#-------------------------------------------------------
 	def AddWidgetTopLine (self, widget, proportion = 0, flag = wxEXPAND, border = 0):
 		"""Inserts a widget onto the top line.
 		"""
-		self.szr_top_tb.Add(widget, proportion, flag, border)
+		self.szr_top_row.Add(widget, proportion, flag, border)
 	#-------------------------------------------------------
 	def AddBar (self, key):
 		"""Creates and returns a new empty toolbar, referenced by key.
@@ -86,9 +159,9 @@ class cMainTopPanel(wxPanel):
 		display the toolbar with the notebook
 		"""
 		self.subbars[key] = wxToolBar (
-			self.bottomline,
+			self.pnl_bottom_row,
 			-1,
-			size = self.bottomline.GetClientSize(),
+			size = self.pnl_bottom_row.GetClientSize(),
 			style=wxTB_HORIZONTAL | wxNO_BORDER | wxTB_FLAT
 		)
 
@@ -115,7 +188,7 @@ class cMainTopPanel(wxPanel):
 		#import pdb
 		#pdb.set_trace ()
 		sz = wxSize (tw, th)
-		self.bottomline.SetSize(sz)
+		self.pnl_bottom_row.SetSize(sz)
 		for i in self.subbars.values():
 			i.SetSize (sz)
 		self.szr_main.Layout()
@@ -153,7 +226,12 @@ if __name__ == "__main__":
 	app.MainLoop()
 #===========================================================
 # $Log: gmGP_Toolbar.py,v $
-# Revision 1.12  2003-03-29 18:26:04  ncq
+# Revision 1.13  2003-04-04 20:43:01  ncq
+# - install new patient search widget
+# - rework to be a more invariant top pane less dependant on gmDemographics
+# - file should be renamed to gmTopPane.py
+#
+# Revision 1.12  2003/03/29 18:26:04  ncq
 # - allow proportion/flag/border in AddWidgetTopLine()
 #
 # Revision 1.11  2003/03/29 13:46:44  ncq
