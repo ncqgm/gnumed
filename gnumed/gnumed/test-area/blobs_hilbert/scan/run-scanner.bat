d33c8934ba4546575d82a61a5c37327a062d916c
Karsten Hilbert <karsten.hilbert@gmx.net>
2003-04-13 13:45:22 +0000

    - moved to gnumed/Archive/

--- gnumed/gnumed/test-area/blobs_hilbert/scan/run-scanner.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM =================================
-REM $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/test-area/blobs_hilbert/scan/Attic/run-scanner.bat,v $
-REM $Revision: 1.4 $
-REM =================================
-REM set your preferred language here
-set LANG=de_DE@euro
-
-c:\Python22\python.exe .\gmScanMedDocs.py --conf-file=.\gnumed-archive.conf --text-domain=gnumed-archive
