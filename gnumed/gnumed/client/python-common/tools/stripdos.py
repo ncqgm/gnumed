c3c0b3cc2e3ccd018d7a7703bad55501f200f0a1
Karsten Hilbert <karsten.hilbert@gmx.net>
2003-04-17 20:49:27 +0000

    - TMP.TXT -> tmp.unix

--- gnumed/gnumed/client/python-common/tools/stripdos.py
+++ gnumed/gnumed/client/python-common/tools/stripdos.py
@@ -2,14 +2,22 @@
 
 # license GPL
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/python-common/tools/Attic/stripdos.py,v $
-__version__ = "$Revision: 1.1 $"
+__version__ = "$Revision: 1.2 $"
 
-import sys, string, glob
+import sys, string, glob, os.path
+
 files = glob.glob("*.TXT")
+files.extend(glob.glob("*.txt"))
+files.extend(glob.glob("*.Txt"))
+
 for file in files:
 	f = open(file)
-	print "Processing file", file
-	fo = open(file+'.unix', "w")
+	new_name = string.lower(os.path.splitext(os.path.basename(file))[0]) + '.unix'
+	if os.path.exists(new_name):
+		print "will not overwrite existing file [%s -> %s]" % (file, new_name)
+		continue
+	print "converting [%s -> %s]" % (file, new_name)
+	fo = open(new_name, "wb")
 	lines = f.readlines()
 	f.close()
 	for line in lines:
@@ -18,6 +26,9 @@ for file in files:
 	fo.close()
 #=====================================================
 # $Log: stripdos.py,v $
-# Revision 1.1  2002-10-01 10:41:24  ncq
+# Revision 1.2  2003-04-17 20:49:27  ncq
+# - TMP.TXT -> tmp.unix
+#
+# Revision 1.1  2002/10/01 10:41:24  ncq
 # - as per Richard's request
 #
