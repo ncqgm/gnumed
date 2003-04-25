12bc6cdb422f7d07e57ea4dbcbb8bbc1eaf4b30f
Karsten Hilbert <karsten.hilbert@gmx.net>
2003-04-25 13:00:43 +0000

    - more cleanup/renaming on the way to more goodness, eventually

--- gnumed/gnumed/client/python-common/gmBackendListener.py
+++ gnumed/gnumed/client/python-common/gmBackendListener.py
@@ -8,7 +8,7 @@ NOTE !  This is specific to the DB adapter pyPgSQL and
 """
 #=====================================================================
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/python-common/Attic/gmBackendListener.py,v $
-__version__ = "$Revision: 1.7 $"
+__version__ = "$Revision: 1.8 $"
 __author__ = "H. Herb <hherb@gnumed.net>"
 
 import sys, time, threading, select
@@ -19,18 +19,17 @@ _log = gmLog.gmDefLog
 class BackendListener:
 	def __init__(self, service, database, user, password, host='localhost', port=5432, poll_interval = 3):
 		#when self._quit is true, the thread is stopped
-		self._quit=0
+		self._quit = 0
 		# remember what signals we are listening for; no need to listen twice to the same signal
-		self._signals = []
+		self._intercepted_signals = []
 		# remember what service we are representing
 		self._service = service
 		#check for messages every 'poll_interval' seconds
 		self._poll_interval = poll_interval
 		#is the thread runnning already?
-		self._thread_running=0
+		self._thread_running = 0
 		#connect to the backend
-		self._cnx = self.Connect(database, user, password, host, port)
-
+		self._conn = self.__connect(database, user, password, host, port)
 	#-------------------------------
 	#def __del__(self):
 	#	self.__quit=1
@@ -38,11 +37,30 @@ class BackendListener:
 	#	time.sleep(self._poll_interval+2)
 
 	#-------------------------------
-	def Stop(self):
-		self._quit=1
-
+	# public API
 	#-------------------------------
-	def Connect(self, database, user, password, host='localhost', port=5432):
+	def register_callback(self, aSignal, aCallback):
+		if not self._thread_running:
+			self.__start_thread()
+		# don't listen twice to the same signal
+		# FIXME: but we DO want to connect another callback !!
+		if aSignal in self._intercepted_signals:
+			return
+		#tell the backend to notify us on this signal
+		res = self._conn.query('LISTEN %s' % signal)
+		if res.resultStatus != libpq.COMMAND_OK:
+			raise libpq.Error, "ERROR: command failed"
+
+		self._intercepted_signals.append(aSignal)
+		# connect the signal with the callback function
+		gmDispatcher.connect(receiver = aCallback, signal = aSignal)
+	#-------------------------------
+	def tell_thread_to_stop(self):
+		self._quit = 1
+	#-------------------------------
+	# internal helpers
+	#-------------------------------
+	def __connect(self, database, user, password, host='localhost', port=5432):
 		cnx=None
 		try:
 			constr="dbname='%s' user='%s' password='%s' host='%s' port=%d" % (database, user, password, host, port)
@@ -52,54 +70,33 @@ class BackendListener:
 			exc = sys.exc_info()
 			_log.LogException("Connection to database '%s' failed: %s" % (database, msg), exc, fatal=0)
 		return cnx
-
 	#-------------------------------
-	def ListeningThread(self):
-		if self._cnx is None:
+	def __start_thread(self):
+		if self._conn is None:
 			_log.Log(gmLog.lErr, "Can't start thread. No connection to backend available.")
 			return None
 		sys.stdout.flush()
-		t = threading.Thread(target=self.Listen)
-		self._thread_running=1
+		t = threading.Thread(target = self._process_signals)
+		self._thread_running = 1
 		t.start()
-
 	#-------------------------------
-	def Listen(self):
+	def _process_signals(self):
 		while 1:
-			ready_sockets = select.select([self._cnx.socket], [], [], 1.0)[0]
+			ready_sockets = select.select([self._conn.socket], [], [], 1.0)[0]
 			if len(ready_sockets):
-				self._cnx.consumeInput()
-				note = self._cnx.notifies()
+				self._conn.consumeInput()
+				note = self._conn.notifies()
 				while note:
 					sys.stdout.flush()
-					#print '+'
-					gmDispatcher.send(note.relname, sender=self._service)
-					note = self._cnx.notifies()
+					gmDispatcher.send(signal = note.relname, sender = self._service)
 					if self._quit:
 						break
+					note = self._conn.notifies()
 			else:
 				time.sleep(self._poll_interval)
 			if self._quit:
 				self._thread_running=0
 				break
-
-	#-------------------------------
-	def RegisterCallback(self, callback, signal):
-		#start the listener thread if not already started
-		if not self._thread_running:
-			self.ListeningThread()
-		#don't try to listen twice to the same signal
-		if signal in self._signals:
-			return
-		#remember that we are listening to this signal
-		self._signals.append(signal)
-		#tell the backend to notify us on this signal
-		res = self._cnx.query('LISTEN %s' % signal)
-		if res.resultStatus != libpq.COMMAND_OK:
-			raise libpq.Error, "ERROR: command failed"
-		#connect the signal with the callback function
-		gmDispatcher.connect(callback, signal)
-
 #=====================================================================
 # main
 #=====================================================================
@@ -149,7 +146,7 @@ if __name__ == "__main__":
 	print "This should trigger our backend listening callback."
 	print "You can also try to stop the demo with Ctrl-C!"
 	listener = BackendListener(service='default', database='gnumed', user='gnumed', password='')
-	listener.RegisterCallback(OnPatientModified, 'patient_changed')
+	listener.register_callback('patient_changed', OnPatientModified)
 
 	try:
 		counter = 0
@@ -169,14 +166,17 @@ if __name__ == "__main__":
 		print "With backend thread, it took", t_threaded, "seconds"
 		print "Difference:", t_threaded-t_nothreads
 
-		listener.Stop()
+		listener.tell_thread_to_stop()
 	except KeyboardInterrupt:
 		print "cancelled by user"
 		listener.Stop()
 
 #=====================================================================
 # $Log: gmBackendListener.py,v $
-# Revision 1.7  2003-02-07 14:22:35  ncq
+# Revision 1.8  2003-04-25 13:00:43  ncq
+# - more cleanup/renaming on the way to more goodness, eventually
+#
+# Revision 1.7  2003/02/07 14:22:35  ncq
 # - whitespace fix
 #
 # Revision 1.6  2003/01/16 14:45:03  ncq
