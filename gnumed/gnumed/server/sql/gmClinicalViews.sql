5a2fafefbf94d48a11e650c9f6f311a874d615c2
Karsten Hilbert <karsten.hilbert@gmx.net>
2003-04-29 12:34:54 +0000

    - added more views + grants

--- gnumed/gnumed/server/sql/gmClinicalViews.sql
+++ gnumed/gnumed/server/sql/gmClinicalViews.sql
@@ -5,7 +5,7 @@
 -- license: GPL (details at http://gnu.org)
 
 -- $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/server/sql/gmClinicalViews.sql,v $
--- $Id: gmClinicalViews.sql,v 1.2 2003-04-28 21:39:49 ncq Exp $
+-- $Id: gmClinicalViews.sql,v 1.3 2003-04-29 12:34:54 ncq Exp $
 
 -- ===================================================================
 -- do fixed string i18n()ing
@@ -46,22 +46,59 @@ where
 ;
 
 -- =============================================
+\unset ON_ERROR_STOP
+drop view v_patient_episodes;
+\set ON_ERROR_STOP 1
+
+create view v_patient_episodes as
+select
+	chi.id_patient as id_patient,
+	cep.id as id_episode
+from
+	clin_health_issue chi, clin_episode cep
+where
+	cep.id_health_issue = chi.id
+;
+
+-- =============================================
+\unset ON_ERROR_STOP
+drop view v_patient_transactions;
+\set ON_ERROR_STOP 1
+
+create view v_patient_transactions as
+select
+	vpep.id_patient as id_patient,
+	tx.id as id_transaction
+from
+	clin_transaction tx, v_patient_episodes vpep
+where
+	vpep.id_episode = tx.id_episode
+;
+
+-- =============================================
 GRANT SELECT ON
-	"v_i18n_allergy"
+	"v_i18n_allergy",
+	"v_patient_episodes",
+	"v_patient_transactions"
 TO GROUP "gm-doctors";
 
 GRANT SELECT, INSERT, UPDATE, DELETE ON
-	"v_i18n_allergy"
+	"v_i18n_allergy",
+	"v_patient_episodes",
+	"v_patient_transactions"
 TO GROUP "_gm-doctors";
 
 -- =============================================
 -- do simple schema revision tracking
 \i gmSchemaRevision.sql
-INSERT INTO gm_schema_revision (filename, version) VALUES('$RCSfile: gmClinicalViews.sql,v $', '$Revision: 1.2 $');
+INSERT INTO gm_schema_revision (filename, version) VALUES('$RCSfile: gmClinicalViews.sql,v $', '$Revision: 1.3 $');
 
 -- =============================================
 -- $Log: gmClinicalViews.sql,v $
--- Revision 1.2  2003-04-28 21:39:49  ncq
+-- Revision 1.3  2003-04-29 12:34:54  ncq
+-- - added more views + grants
+--
+-- Revision 1.2  2003/04/28 21:39:49  ncq
 -- - cleanups and GRANTs
 --
 -- Revision 1.1  2003/04/28 20:40:48  ncq
