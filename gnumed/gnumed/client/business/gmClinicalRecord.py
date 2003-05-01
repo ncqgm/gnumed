92a95bcaec05ca79149a3457cd7961fabe401841
Karsten Hilbert <karsten.hilbert@gmx.net>
2003-05-01 14:59:24 +0000

    - listen on allergy add/delete in backend, invalidate cache and notify frontend - "allergies", "allergy names" getters

--- gnumed/gnumed/client/business/gmClinicalRecord.py
+++ gnumed/gnumed/client/business/gmClinicalRecord.py
@@ -7,8 +7,8 @@ license: GPL
 """
 #============================================================
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/business/gmClinicalRecord.py,v $
-# $Id: gmClinicalRecord.py,v 1.1 2003-04-29 12:33:20 ncq Exp $
-__version__ = "$Revision: 1.1 $"
+# $Id: gmClinicalRecord.py,v 1.2 2003-05-01 14:59:24 ncq Exp $
+__version__ = "$Revision: 1.2 $"
 __author__ = "K.Hilbert <Karsten.Hilbert@gmx.net>"
 
 # access our modules
@@ -23,8 +23,7 @@ if __name__ == "__main__":
 	_log.SetAllLogLevels(gmLog.lData)
 _log.Log(gmLog.lData, __version__)
 
-import gmExceptions, gmPG
-#, gmSignals, gmDispatcher
+import gmExceptions, gmPG, gmSignals, gmDispatcher
 
 # 3rd party
 #import mx.DateTime as mxDateTime
@@ -49,13 +48,16 @@ class gmClinicalRecord:
 		if not self._pkey_exists():
 			raise gmExceptions.ConstructorError, "No patient with ID [%s] in database." % aPKey
 
+		self.__db_cache = {}
+
 		# register backend notification interests ...
-		#if not self._register_interests():
-			#raise gmExceptions.ConstructorError, "Cannot register patient modification interests."
+		if not self._register_interests():
+			raise gmExceptions.ConstructorError, "cannot register signal interests"
 
 		_log.Log(gmLog.lData, 'Instantiated clinical record for patient [%s].' % self.id_patient)
 	#--------------------------------------------------------
 	def __del__(self):
+		self._backend.Unlisten(service = 'historica', signal = gmSignals.allergy_add_del_db(), callback = self._allergy_added_deleted)
 		if self.__dict__.has_key('_backend'):
 			self._backend.ReleaseConnection('historica')
 	#--------------------------------------------------------
@@ -98,12 +100,37 @@ class gmClinicalRecord:
 	#--------------------------------------------------------
 	def _register_interests(self):
 		# backend
-		pass
-#		self._backend.Listen(
-#			service = 'historica',
-#			signal = '"%s.%s"' % (gmSignals.patient_modified(), self.id_patient),
-#			callback = self._patient_modified
-#		)
+		if not self._backend.Listen(service = 'historica', signal = gmSignals.allergy_add_del_db(), callback = self._allergy_added_deleted):
+			return None
+		return 1
+	#--------------------------------------------------------
+	def _allergy_added_deleted(self):
+		curs = self._defconn_ro.cursor()
+		# did number of allergies change for our patient ?
+		cmd = "select count(id) from v_i18n_patient_allergies where id_patient='%s';" % self.id_patient
+		if not gmPG.run_query(curs, cmd):
+			curs.close()
+			_log.Log(gmLog.lData, 'cannot check for added/deleted allergies, assuming addition/deletion did occurr')
+			# error: invalidate cache
+			del self.__db_cache['allergies']
+			# and tell others
+			gmDispatcher.send(signal = gmSignals.allergy_updated, sender = self.__class__.__name__)
+			return 1
+		result = curs.fetchone()
+		curs.close()
+		# not cached yet
+		try:
+			nr_cached_allergies = len(self.__db_cache['allergies'])
+		except KeyError:
+			gmDispatcher.send(signal = gmSignals.allergy_updated, sender = self.__class__.__name__)
+			return 1
+		# no change for our patient ...
+		if result == nr_cached_allergies:
+			return 1
+		# else invalidate cache
+		del self.__db_cache['allergies']
+		gmDispatcher.send(signal = gmSignals.allergy_updated, sender = self.__class__.__name__)
+		return 1
 	#--------------------------------------------------------
 #	def _patient_modified(self):
 		# uh, oh, cache may have been modified ...
@@ -129,12 +156,50 @@ class gmClinicalRecord:
 	def _get_patient_ID(self):
 		return self.id_patient
 	#--------------------------------------------------------
+	def _get_allergies(self):
+		"""Return rows in v_i18n_allergy for this patient"""
+		try:
+			return self.__db_cache['allergies']
+		except:
+			pass
+		curs = self._defconn_ro.cursor()
+		cmd = "select * from v_i18n_patient_allergies where id_patient='%s';" % self.id_patient
+		if not gmPG.run_query(curs, cmd):
+			curs.close()
+			_log.Log(gmLog.lErr, 'cannot load allergies for patient [%s]' % self.id_patient)
+			return None
+		rows = curs.fetchall()
+		curs.close()
+		self.__db_cache['allergies'] = rows
+		return self.__db_cache['allergies']
+	#--------------------------------------------------------
+	def _get_allergy_names(self):
+		data = []
+		try:
+			self.__db_cache['allergies']
+		except KeyError:
+			if not self._get_allergies():
+				return data
+		for allergy in self.__db_cache['allergies']:
+			tmp = {}
+			tmp['id'] = allergy['id']
+			if allergy['allergene'] not in [None, '']:
+				tmp['name'] = allergy['allergene']
+			else:
+				tmp['name'] = allergy['substance']
+			data.append(tmp)
+		return data
+	#--------------------------------------------------------
 	def _get_allergies_list(self):
 		"""Return list of IDs in v_i18n_allergy for this patient."""
-		allergies_list = []
+		try:
+			return self.__db_cache['allergy IDs']
+		except KeyError:
+			pass
+		self.__db_cache['allergy IDs'] = []
 		transactions = string.join(self['clinical transaction IDs'], ',')
 		if transactions == '':
-			return allergies_list
+			return self.__db_cache['allergy IDs']
 		curs = self._defconn_ro.cursor()
 		cmd = "select id from v_i18n_allergy where id_clin_transaction in (%s);" % transactions
 		if not gmPG.run_query(curs, cmd):
@@ -144,8 +209,8 @@ class gmClinicalRecord:
 		rows = curs.fetchall()
 		curs.close()
 		for row in rows:
-			allergies_list.extend(row)
-		return allergies_list
+			self.__db_cache['allergy IDs'].extend(row)
+		return self.__db_cache['allergy IDs']
 	#--------------------------------------------------------
 	def _get_clinical_transactions_list(self):
 		curs = self._defconn_ro.cursor()
@@ -164,18 +229,26 @@ class gmClinicalRecord:
 	# set up handler map
 	_get_handler['patient ID'] = _get_patient_ID
 #	_get_handler['allergies'] = _get_allergies
-	_get_handler['allergy IDs'] = _get_allergies_list
+#	_get_handler['allergy IDs'] = _get_allergies_list
+	_get_handler['allergy names'] = _get_allergy_names
+	_get_handler['allergies'] = _get_allergies
 	_get_handler['clinical transaction IDs'] = _get_clinical_transactions_list
 #============================================================
 # main
 #------------------------------------------------------------
 if __name__ == "__main__":
 	record = gmClinicalRecord(aPKey = 1)
-	print "clinical transaction IDs:", record['clinical transaction IDs']
-	print "allergy IDs:", record['allergy IDs']
+#	print "clinical transaction IDs:", record['clinical transaction IDs']
+#	print "allergy IDs:", record['allergy IDs']
+	while 1:
+		pass
 	del record
 #============================================================
 # $Log: gmClinicalRecord.py,v $
-# Revision 1.1  2003-04-29 12:33:20  ncq
+# Revision 1.2  2003-05-01 14:59:24  ncq
+# - listen on allergy add/delete in backend, invalidate cache and notify frontend
+# - "allergies", "allergy names" getters
+#
+# Revision 1.1  2003/04/29 12:33:20  ncq
 # - first draft
 #
