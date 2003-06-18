8de58b81f256ee7b04f59ede06f00063e2c8551f
Karsten Hilbert <karsten.hilbert@gmx.net>
2003-06-18 08:57:10 +0000

    - shell wrapper

---
+++ gnumed/gnumed/client/wxpython/gnumed.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/wxpython/Attic/gnumed.sh,v $
+# $Revision: 1.1 $
+
+# start kvkd
+# FIXME: needs logic to prevent more than one kvkd from running
+
+# only set this if you really know what you are doing
+#export GNUMED_DIR=/foo/bar
+
+# maybe force some locale setting here
+
+# start GnuMed
+python gnumed.py --debug
