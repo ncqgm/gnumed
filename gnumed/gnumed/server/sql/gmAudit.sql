9bd36e1b805985c5fae05128f6296538a5900df5
Karsten Hilbert <karsten.hilbert@gmx.net>
2003-06-29 15:22:50 +0000

    - split audit_fields off of audit_mark, they really   are two separate if related things

--- gnumed/gnumed/server/sql/gmAudit.sql
+++ gnumed/gnumed/server/sql/gmAudit.sql
@@ -1,7 +1,7 @@
 -- GnuMed auditing functionality
 -- ===================================================================
 -- $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/server/sql/gmAudit.sql,v $
--- $Revision: 1.4 $
+-- $Revision: 1.5 $
 -- license: GPL
 -- author: Karsten Hilbert
 
@@ -11,20 +11,30 @@
 
 -- ===================================================================
 create table audit_mark (
-	pk_audit serial primary key,
-	row_version integer not null default 0,
-	modify_when timestamp with time zone not null default CURRENT_TIMESTAMP,
-	modify_by name not null default CURRENT_USER
+	audit_this_table boolean default true check(audit_this_table in (true, false))
 );
 
 comment on table audit_mark is
 	'All tables that need standard auditing must inherit from this table.
 	 Marks tables for automatic audit trigger generation';
-comment on column audit_mark.row_version is
+comment on column audit_mark.audit_this_table is
+	'just a dummy field for "create table"';
+
+-- ===================================================================
+create table audit_fields (
+	pk_audit serial primary key,
+	row_version integer not null default 0,
+	modified_when timestamp with time zone not null default CURRENT_TIMESTAMP,
+	modified_by name not null default CURRENT_USER
+);
+
+comment on table audit_fields is
+	'this table holds all the fields needed for auditing';
+comment on column audit_fields.row_version is
 	'the version of the row; mainly just a count';
-comment on COLUMN audit_mark.modify_when is
+comment on COLUMN audit_fields.modified_when is
 	'when has this row been committed (created/modified)';
-comment on COLUMN audit_mark.modify_by is
+comment on COLUMN audit_fields.modified_by is
 	'by whom has this row been committed (created/modified)';
 
 -- ===================================================================
@@ -61,18 +71,23 @@ comment on column audit_trail.audit_by is
 -- ===================================================================
 grant SELECT, UPDATE, INSERT, DELETE on
 	"audit_mark",
-	"audit_mark_pk_audit_seq",
+	"audit_fields",
+	"audit_fields_pk_audit_seq",
 	"audit_trail",
 	"audit_trail_pk_audit_seq"
 to group "_gm-doctors";
 
 -- ===================================================================
 -- do simple schema revision tracking
-INSERT INTO gm_schema_revision (filename, version) VALUES('$RCSfile: gmAudit.sql,v $', '$Revision: 1.4 $');
+INSERT INTO gm_schema_revision (filename, version) VALUES('$RCSfile: gmAudit.sql,v $', '$Revision: 1.5 $');
 
 -- ===================================================================
 -- $Log: gmAudit.sql,v $
--- Revision 1.4  2003-05-22 12:55:19  ncq
+-- Revision 1.5  2003-06-29 15:22:50  ncq
+-- - split audit_fields off of audit_mark, they really
+--   are two separate if related things
+--
+-- Revision 1.4  2003/05/22 12:55:19  ncq
 -- - table audit_log -> audit_trail
 --
 -- Revision 1.3  2003/05/13 14:40:54  ncq
