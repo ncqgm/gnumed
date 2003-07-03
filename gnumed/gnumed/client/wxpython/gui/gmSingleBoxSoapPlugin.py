1c4bb5b162b279af02702789494aedd0fef58abd
Karsten Hilbert <karsten.hilbert@gmx.net>
2003-07-03 15:25:41 +0000

    - removed unneccary iports

--- gnumed/gnumed/client/wxpython/gui/gmSingleBoxSoapPlugin.py
+++ gnumed/gnumed/client/wxpython/gui/gmSingleBoxSoapPlugin.py
@@ -1,18 +1,14 @@
 """GnuMed single box SOAP notes plugin.
 """
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/wxpython/gui/Attic/gmSingleBoxSoapPlugin.py,v $
-__version__ = "$Revision: 1.3 $"
+__version__ = "$Revision: 1.4 $"
 __author__ = "Karsten Hilbert <Karsten.Hilbert@gmx.net>"
 #================================================================
-#import os.path, sys, os, re
-
-import gmLog, gmPG, gmTmpPatient, gmPlugin, gmCfg
+import gmLog, gmPlugin
 from gmSingleBoxSOAP import gmSingleBoxSOAPPanel
-from gmExceptions import ConstructorError
 
 _log = gmLog.gmDefLog
 _log.Log(gmLog.lData, __version__)
-_cfg = gmCfg.gmDefCfgFile
 
 from wxPython.wx import *
 #================================================================
@@ -41,10 +37,6 @@ class gmSingleBoxSoapPlugin(gmPlugin.wxNotebookPlugin):
 # MAIN
 #----------------------------------------------------------------
 if __name__ == '__main__':
-	if _cfg is None:
-		_log.Log(gmLog.lErr, "Cannot run without config file.")
-		sys.exit("Cannot run without config file.")
-
 	# catch all remaining exceptions
 	try:
 		application = wxPyWidgetTester(size=(640,480))
@@ -57,7 +49,10 @@ if __name__ == '__main__':
 
 #================================================================
 # $Log: gmSingleBoxSoapPlugin.py,v $
-# Revision 1.3  2003-06-29 15:21:22  ncq
+# Revision 1.4  2003-07-03 15:25:41  ncq
+# - removed unneccary iports
+#
+# Revision 1.3  2003/06/29 15:21:22  ncq
 # - add can_receive_focus() on patient not selected
 #
 # Revision 1.2  2003/06/26 21:41:51  ncq
