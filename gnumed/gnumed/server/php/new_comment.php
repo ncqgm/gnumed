6b7c599bec74b8bdacc8e41b9f917505daa79f83
ihaywood <ihaywood>
2003-07-16 12:46:10 +0000

    Removing drugref.org interface, now at savannah.nongnu.org/projects/php-drugref

--- gnumed/gnumed/server/php/new_comment.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-include ('connect.php');
-
-pg_query ("insert into audit (action, why, table_name, table_row, source) values ('v', '$why', '$table', $id, $id_info)");
-
-include ('comments.php');
--- gnumed/gnumed/server/php/new_ref.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include ('connect.php');
-
-pg_query ("insert into info_reference (description, source_category) values ('$description', '$source_category')");
-
-include ("references.php");
-
