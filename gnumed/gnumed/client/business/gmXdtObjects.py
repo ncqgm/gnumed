547c3a67d6ec6d23b69c4d2ec9ae6d8dd331d0c5
Karsten Hilbert <karsten.hilbert@gmx.net>
2003-08-27 14:58:58 +0000

    - added helpers written by shilbert for XdtViewer

--- gnumed/gnumed/client/business/gmXdtObjects.py
+++ gnumed/gnumed/client/business/gmXdtObjects.py
@@ -5,12 +5,12 @@ objects for easy access.
 """
 #==============================================================
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/business/gmXdtObjects.py,v $
-# $Id: gmXdtObjects.py,v 1.3 2003-04-19 22:56:03 ncq Exp $
-__version__ = "$Revision: 1.3 $"
-__author__ = "K.Hilbert"
+# $Id: gmXdtObjects.py,v 1.4 2003-08-27 14:58:58 ncq Exp $
+__version__ = "$Revision: 1.4 $"
+__author__ = "K.Hilbert, S.Hilbert"
 __license__ = "GPL"
 
-import os.path, sys, fileinput, string
+import os.path, sys, fileinput, string, md5, time, linecache, tempfile
 
 # access our modules
 if __name__ == "__main__":
@@ -127,7 +127,144 @@ class xdtPatient:
 		except KeyError:
 			_log.LogException('[%s] not cached in self.__data', sys.exc_info())
 			return None
+#==============================================================
+# FIXME: the following *should* get wrapped in class XdtFile ...
+#--------------------------------------------------------------
+def xdt_get_pats(aFile):
+	pat_ids = []
+	pat_names = []
+	pats = {}
+	# xDT line format: aaabbbbcccccccccccCRLF where aaa = length, bbbb = record type, cccc... = content
+	# read patient dat
+	for line in fileinput.input(aFile):
+		# remove trailing CR and/or LF
+		line = string.replace(line,'\015','')
+		line = string.replace(line,'\012','')
+		# do we care about this line ?
+		field = line[3:7]
+		# yes, if type = patient id
+		if field == '3000':
+			pat_id = line[7:]
+			if pat_id not in pat_ids:
+				pat_ids.append(pat_id)
+			continue
+		# yes, if type = patient name
+		if field == '3101':
+			pat_name = line [7:]
+			if pat_name not in pat_names:
+				pat_names.append(pat_name)
+				pats[pat_id] = pat_name
+			continue
+	fileinput.close()
 
+	_log.Log(gmLog.lData, "patients found: %s" % len(pat_ids))
+	return pats
+#==============================================================
+def get_pat_files(aFile, anID, name, patdir = None, patlst = None):
+	_log.Log(gmLog.lData, "getting files for patient [%s:%s]" % (anID, name))
+	files = patlst.get(aGroup = "%s:%s" % (ID, name), anOption = "files")
+	_log.Log(gmLog.lData, "%s => %s" % (patdir, files))
+	return [patdir, files]
+#==============================================================
+def split_xdt_file(aFile,patlst,cfg):
+	content=[]
+	lineno = []
+
+	# xDT line format: aaabbbbcccccccccccCRLF where aaa = length, bbbb = record type, cccc... = content
+
+	content = []
+	record_start_lines = []
+
+	# find record starts
+	for line in fileinput.input(aFile):
+		strippedline = string.replace(line,'\015','')
+		strippedline = string.replace(strippedline,'\012','')
+		# do we care about this line ? (records start with 8000)
+		if strippedline[3:7] == '8000':
+			record_start_lines.append(fileinput.filelineno())
+
+	# loop over patient records
+	for aline in record_start_lines:
+		# WHY +2 ?!? 
+		line = linecache.getline(aFile,aline+2)	
+		# remove trailing CR and/or LF
+		strippedline = string.replace(line,'\015','')
+		strippedline = string.replace(strippedline,'\012','')
+		# do we care about this line ?
+		field = strippedline[3:7]
+		# extract patient id
+		if field == '3000':	
+			ID = strippedline[7:]
+			line = linecache.getline(aFile,aline+3)
+			# remove trailing CR and/or LF
+			strippedline = string.replace(line,'\015','')
+			strippedline = string.replace(strippedline,'\012','')
+			# do we care about this line ?
+			field = strippedline[3:7]
+			if field == '3101':
+				name = strippedline [7:]
+			startline=aline
+			endline=record_start_lines[record_start_lines.index(aline)+1]
+			_log.Log(gmLog.lData, "reading from%s" %str(startline)+' '+str(endline) )
+			for tmp in range(startline,endline):							
+				content.append(linecache.getline(aFile,tmp))
+				_log.Log(gmLog.lData, "reading %s"%tmp )
+			hashes = check_for_previous_records(ID,name,patlst)
+			# is this new content ?
+			data_hash = md5.new()
+			map(data_hash.update, content)
+			digest = data_hash.hexdigest()
+			if digest not in hashes:
+				pat_dir = cfg.get("xdt-viewer", "export-dir")
+				file = write_xdt_pat_data(content, pat_dir)
+				add_file_to_patlst(ID, name, patlst, file, ahash)
+			content = []
+		else:
+			continue
+	# cleanup
+	fileinput.close()
+	patlst.store()
+	return 1
+#==============================================================
+def get_rand_fname(aDir):
+	# set up temp file environment for creating unique random directory
+	tempfile.tempdir = aDir
+	tempfile.template = ""
+	# create temp filename
+	tmpname = tempfile.mktemp(suffix = time.strftime(".%Y%m%d-%H%M%S", time.localtime()))
+	# extract name for dir
+	path, fname = os.path.split(tmpname)
+	return fname
+#==============================================================
+def write_xdt_pat_data(data, aDir):
+	"""write record for this patient to new file"""
+	pat_file = open(os.path.join(aDir, get_rand_fname(aDir)), "w")
+	map(pat_file.write, data)
+	pat_file.close()
+	return fname
+#==============================================================
+def check_for_previous_records(ID, name, patlst):
+	anIdentity = "%s:%s" % (ID, name)
+	hashes = []
+	# patient not listed yet
+	if anIdentity not in patlst.getGroups():
+		_log.Log(gmLog.lData, "identity not yet in list" )
+		patlst.set(aGroup = anIdentity, anOption = 'files', aValue = [], aComment = '')
+	# file already listed ?
+	file_defs = patlst.get(aGroup = anIdentity, anOption = "files")
+	for line in file_defs:
+		file, ahash = string.split(line,':')
+		hashes.append(ahash)
+
+	return hashes
+#==============================================================
+def add_file_to_patlst(ID, name, patlst, new_file, ahash):
+	anIdentity = "%s:%s" % (ID, name)
+	files = patlst.get(aGroup = anIdentity, anOption = "files")
+	for file in new_files:
+		files.append("%s:%s" % (file, ahash))
+	_log.Log(gmLog.lData, "files now there : %s" % files)
+	patlst.set(aGroup=anIdentity, anOption="files", aValue = files, aComment="")
 #==============================================================
 # main
 #--------------------------------------------------------------
@@ -149,7 +286,10 @@ if __name__ == "__main__":
 
 #==============================================================
 # $Log: gmXdtObjects.py,v $
-# Revision 1.3  2003-04-19 22:56:03  ncq
+# Revision 1.4  2003-08-27 14:58:58  ncq
+# - added helpers written by shilbert for XdtViewer
+#
+# Revision 1.3  2003/04/19 22:56:03  ncq
 # - speed up __load_data(), better encapsulate xdt file maps
 #
 # Revision 1.2  2003/02/18 02:43:16  ncq
