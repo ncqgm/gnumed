4bd38fbc60ba62457037d07d0892d46e447256d7
hinnef <hinnef>
2003-10-26 21:35:45 +0000

    -bugfixes in AddConfigParam and importDBSet

--- gnumed/gnumed/client/python-common/gmConfigCommon.py
+++ gnumed/gnumed/client/python-common/gmConfigCommon.py
@@ -15,7 +15,7 @@ License: GNU Public License
 """
 #==================================================================
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/python-common/Attic/gmConfigCommon.py,v $
-__version__ = "$Revision: 1.8 $"
+__version__ = "$Revision: 1.9 $"
 __author__ = "H.Berger,K.Hilbert"
 
 import sys, os, string,types, pickle
@@ -551,9 +551,11 @@ class ConfigDataDB(ConfigData):
 		# check if we have a cookie
 		if pNameParts[-1][0] == '_':
 			cookie = pNameParts[-1][1:]
+			option = string.join(pNameParts[:-1],".")
 		else:
 			cookie = None
-		option = string.join(pNameParts[:-1],".")
+			option = aParameterName
+			
 #		print "[%s, %s]" % (cookie, option)		
 		if option is None:
 			return None
@@ -843,6 +845,7 @@ def importDBSet(filename,aUser = None, aMachine = '__default__'):
 	groups = importFile.getGroups()
 	# every group holds one parameter description
 	# group name = parameter name
+	successfully_stored = 0
 	for paramName in groups:
 		# ignore empty parameter names
 		if paramName == "":
@@ -863,7 +866,7 @@ def importDBSet(filename,aUser = None, aMachine = '__default__'):
 			continue
 		else:
 			if paramType in ['string','numeric','str_array']:
-				paramValue = eval(paramValueStr)
+				paramValue = eval(repr(paramValueStr))
 			else:
 				paramValue = pickle.loads(paramValueStr)
 		# TODO: check if the parameter already exists with different type
@@ -877,17 +880,28 @@ def importDBSet(filename,aUser = None, aMachine = '__default__'):
 			else:
 				# same type -> store new value	
 				s=importConfigSource.setConfigData(paramName,paramValue)
+				if s is None:
+					_log.Log(gmLog.lWarn, 
+						"Cannot store config parameter [%s] to set [%s@%s]." % (paramName,aUser,aMachine))
+				else:
+					successfully_stored = successfully_stored + 1
+				
 		else:
 			# add new entry to parameter definition dictionary
 			s=importConfigSource.addConfigParam(paramName,paramType,paramValue,paramDescription)
 			if s is None:
 				_log.Log(gmLog.lWarn, 
-					"Cannot store config parameter [%s] to set [%s@%s]." % (paramName,aUser,aMachine))		
-	return len(groups)
+					"Cannot store config parameter [%s] to set [%s@%s]." % (paramName,aUser,aMachine))
+			else:
+				successfully_stored = successfully_stored + 1	
+	return successfully_stored
 
 #=============================================================
 # $Log: gmConfigCommon.py,v $
-# Revision 1.8  2003-10-26 01:38:06  ncq
+# Revision 1.9  2003-10-26 21:35:45  hinnef
+# -bugfixes in AddConfigParam and importDBSet
+#
+# Revision 1.8  2003/10/26 01:38:06  ncq
 # - gmTmpPatient -> gmPatient, cleanup
 #
 # Revision 1.7  2003/10/22 21:35:51  hinnef
