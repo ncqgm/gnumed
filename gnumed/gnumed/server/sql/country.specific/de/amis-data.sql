c253e9e7b9aaf9d4d81ba6ce174abb557f0ed713
hinnef <hinnef>
2003-10-26 22:49:54 +0000

    - added config parameter sets

--- gnumed/gnumed/server/sql/country.specific/de/amis-data.sql
+++ gnumed/gnumed/server/sql/country.specific/de/amis-data.sql
@@ -3,7 +3,7 @@
 
 -- $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/server/sql/country.specific/de/Attic/amis-data.sql,v $
 -- author: Horst Herb, Hilmar Berger, Karsten Hilbert
--- version: $Revision: 1.1 $
+-- version: $Revision: 1.2 $
 -- license: GPL
 
 -- get the correct AMIS_PATH and process the template file 
@@ -12,8 +12,14 @@
 -- run the script created from template
 \i 'amis-import_data.sql'
 
+-- set the config information in the backend
+\echo `env PYTHONPATH=../../../bootstrap/modules ../../../bootstrap/modules/tools/transferDBset.py -i ./amis-config.set`
+
 -- ===============================================
 -- $Log: amis-data.sql,v $
--- Revision 1.1  2003-10-26 18:10:06  ncq
+-- Revision 1.2  2003-10-26 22:49:54  hinnef
+-- - added config parameter sets
+--
+-- Revision 1.1  2003/10/26 18:10:06  ncq
 -- - separate schema and data better
 --
