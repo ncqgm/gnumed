02e33ca239dfcc01b398361208d8792aa6e7ac81
Karsten Hilbert <karsten.hilbert@gmx.net>
2003-10-31 22:53:27 +0000

    - started collection of test data

---
+++ gnumed/gnumed/server/sql/test-data/test_data-James_Kirk.sql
@@ -0,0 +1,105 @@
+-- Projekt GnuMed
+-- test data for James T. Kirk of Star Trek fame
+
+-- author: Karsten Hilbert <Karsten.Hilbert@gmx.net>
+-- license: GPL
+-- $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/server/sql/test-data/test_data-James_Kirk.sql,v $
+-- $Revision: 1.1 $
+-- =============================================
+-- force terminate + exit(3) on errors if non-interactive
+\set ON_ERROR_STOP 1
+
+-- =============================================
+begin;
+
+-- identity
+delete from identity where
+	gender = 'm'
+		and
+	cob = 'CA'
+		and
+	id in (select i_id from v_basic_person where firstnames='James T.' and lastnames='Kirk' and dob='1931-3-22');
+
+insert into identity (gender, dob, cob)
+values ('m', '1931-3-22', 'CA');
+
+-- name
+delete from names where
+	firstnames = 'James T.'
+		and
+	lastnames = 'Kirk';
+
+insert into names (id_identity, active, lastnames, firstnames, title)
+values (currval('identity_id_seq'), true, 'Kirk', 'James T.', 'Captain');
+
+-- health issue
+delete from clin_health_issue where
+	id_patient = (select i_id from v_basic_person where firstnames='James T.' and lastnames='Kirk' and dob='1931-3-22');
+
+insert into clin_health_issue (id_patient)
+values ((select i_id from v_basic_person where firstnames='James T.' and lastnames='Kirk' and dob='1931-3-22'));
+
+-- episode
+delete from clin_episode where
+	id in (select id_episode
+			from v_patient_episodes
+			where id_patient=(select i_id from v_basic_person where firstnames='James T.' and lastnames='Kirk' and dob='1931-3-22'));
+
+insert into clin_episode (id_health_issue, description)
+values (
+	(select id from clin_health_issue where
+		id_patient=(select i_id from v_basic_person where firstnames='James T.' and lastnames='Kirk' and dob='1931-3-22')
+			and
+		description = '__default__'),
+	'knive cut left forearm 9/2000'
+);
+
+-- encounter
+insert into clin_encounter (
+	fk_patient,
+	fk_location,
+	fk_provider,
+	fk_type,
+	description
+) values (
+	(select i_id from v_basic_person where firstnames='James T.' and lastnames='Kirk' and dob='1931-3-22'),
+	-1,
+	(select i_id from v_basic_person where firstnames='Leonard' and lastnames='McCoy' and dob='1920-1-20'),
+	(select id from _enum_encounter_type where description='in surgery'),
+	'first for this RFE'
+);
+
+insert into vaccination (
+	id_encounter,
+	id_episode,
+	narrative,
+	fk_patient,
+	fk_provider,
+	fk_vaccine,
+	fk_vacc_def,
+	date_given,
+	site,
+	batch_no
+) values (
+	currval('clin_encounter_id_seq'),
+	currval('clin_episode_id_seq'),
+	'for contaminated knife cut and previous booster > 7 years old',
+	(select i_id from v_basic_person where firstnames='James T.' and lastnames='Kirk' and dob='1931-3-22'),
+	(select i_id from v_basic_person where firstnames='Leonard' and lastnames='McCoy' and dob='1920-1-20'),
+	(select id from vaccine where trade_name='Tetasorbat SSW'),
+	(select id from vacc_def where
+		fk_indication = (select id from vacc_indication where description='tetanus')
+			and
+		is_booster = true
+	),
+	'2000-9-17',
+	'left deltoid muscle',
+	'102041A'
+);
+
+commit;
+-- =============================================
+-- $Log: test_data-James_Kirk.sql,v $
+-- Revision 1.1  2003-10-31 22:53:27  ncq
+-- - started collection of test data
+--
---
+++ gnumed/gnumed/server/sql/test-data/test_data-Leonard_McCoy.sql
@@ -0,0 +1,23 @@
+-- Projekt GnuMed
+-- test data for James T. Kirk of Star Trek fame
+
+-- author: Karsten Hilbert <Karsten.Hilbert@gmx.net>
+-- license: GPL
+-- $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/server/sql/test-data/test_data-Leonard_McCoy.sql,v $
+-- $Revision: 1.1 $
+-- =============================================
+-- force terminate + exit(3) on errors if non-interactive
+\set ON_ERROR_STOP 1
+
+-- =============================================
+insert into identity (gender, dob, cob)
+values ('m', '1920-1-20', 'US');
+
+insert into names (id_identity, active, lastnames, firstnames, title)
+values (currval('identity_id_seq'), true, 'McCoy', 'Leonard', 'Dr.');
+
+-- =============================================
+-- $Log: test_data-Leonard_McCoy.sql,v $
+-- Revision 1.1  2003-10-31 22:53:27  ncq
+-- - started collection of test data
+--
