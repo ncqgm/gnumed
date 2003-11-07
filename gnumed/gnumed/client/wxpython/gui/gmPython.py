ba6e862c770c32dce7be09acab23253d91cd8248
shilbert <shilbert>
2003-11-07 21:43:13 +0000

    - PyCrust has been renamed ; use from wx import py

--- gnumed/gnumed/client/wxpython/gui/gmPython.py
+++ gnumed/gnumed/client/wxpython/gui/gmPython.py
@@ -1,4 +1,8 @@
 # a simple wrapper for the cryptowidget
+__version__ = "$Revision: 1.6 $"
+__license__ = "GPL"
+__author__ =	"Sebastian Hilbert <Sebastian.Hilbert@gmx.net>, \
+				 someone before me :-)>"
 from wxPython.wx import *
 import gmPlugin
 
@@ -9,17 +13,23 @@ class gmPython (gmPlugin.wxNotebookPlugin):
     tab_name = _('Python Shell')
 
     def name (self):
-        return gmPython. tab_name
+        return gmPython.tab_name
 
     def MenuInfo (self):
         return ('view', _('&Python'))
 
     def GetWidget (self, parent):
-        from wxPython.lib.PyCrust import shell, version, filling
+        from wx import py
+    	#from wxPython.lib.PyCrust import shell, version, filling
         win = wxSplitterWindow(parent, -1, size=(600, 300))
-        shellWin = shell.Shell(win, -1, introText='The Amazing GnuMed Python Shell!\n')
-        fillingWin = filling.Filling(win, -1, size=(600, 180), rootIsNamespace=1)
+        shellWin = py.shell.Shell(win, -1, introText='The Amazing GnuMed Python Shell!\n')
+        fillingWin = py.filling.Filling(win, -1, size=(600, 180), rootIsNamespace=1)
         win.SplitHorizontally(shellWin, fillingWin)
         return win
 
 
+#======================================================
+# $Log: gmPython.py,v $
+# Revision 1.6  2003-11-07 21:43:13  shilbert
+# - PyCrust has been renamed ; use from wx import py
+#
