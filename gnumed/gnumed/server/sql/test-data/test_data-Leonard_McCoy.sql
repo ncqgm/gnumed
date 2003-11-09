5dd86e69ab88f2417c6ab49b28edb7b414793d49
Karsten Hilbert <karsten.hilbert@gmx.net>
2003-11-09 14:55:39 +0000

    - update comments

--- gnumed/gnumed/server/sql/test-data/test_data-Leonard_McCoy.sql
+++ gnumed/gnumed/server/sql/test-data/test_data-Leonard_McCoy.sql
@@ -1,10 +1,10 @@
 -- Projekt GnuMed
--- test data for James T. Kirk of Star Trek fame
+-- test data for Dr.Leonard McCoy of Star Trek fame
 
 -- author: Karsten Hilbert <Karsten.Hilbert@gmx.net>
 -- license: GPL
 -- $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/server/sql/test-data/test_data-Leonard_McCoy.sql,v $
--- $Revision: 1.1 $
+-- $Revision: 1.2 $
 -- =============================================
 -- force terminate + exit(3) on errors if non-interactive
 \set ON_ERROR_STOP 1
@@ -18,6 +18,9 @@ values (currval('identity_id_seq'), true, 'McCoy', 'Leonard', 'Dr.');
 
 -- =============================================
 -- $Log: test_data-Leonard_McCoy.sql,v $
--- Revision 1.1  2003-10-31 22:53:27  ncq
+-- Revision 1.2  2003-11-09 14:55:39  ncq
+-- - update comments
+--
+-- Revision 1.1  2003/10/31 22:53:27  ncq
 -- - started collection of test data
 --
