22f39e467720cad91373795cbe0d2188429949b1
Karsten Hilbert <karsten.hilbert@gmx.net>
2003-11-18 14:09:43 +0000

    - diff against tree before updating just to be sure

--- gnumed/update_tree.sh
+++ gnumed/update_tree.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/update_tree.sh,v $
-# $Revision: 1.2 $
+# $Revision: 1.3 $
 
 echo "cleaning out debris"
 find ./ -name '*.pyc' -exec rm -v '{}' ';'
+echo "diffing against CVS just to be sure"
+cvs -z9 diff | tee diff-before-update.log
 echo "updating python client source from CVS"
 cvs -z9 update | tee update.log
