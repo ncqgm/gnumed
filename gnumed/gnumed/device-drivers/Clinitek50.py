09ee2ee7cbea99e45f6e93230de4d583b688ed6a
Karsten Hilbert <karsten.hilbert@gmx.net>
2003-11-19 18:11:39 +0000

    - should be able to detect devices

--- gnumed/gnumed/device-drivers/Clinitek50.py
+++ gnumed/gnumed/device-drivers/Clinitek50.py
@@ -1,27 +1,35 @@
 """Clinitek 50 Urinalysis device driver.
 
+This device is made by Bayer Diagnostics.
+
 @license: GPL
 """
 #========================================================
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/device-drivers/Clinitek50.py,v $
-__version__ = "$Revision: 1.1 $"
+__version__ = "$Revision: 1.2 $"
 __author__ = "Karsten Hilbert <Karsten.Hilbert@gmx.net>"
 
 # stock python
 import sys, os.path
 
+if __name__ == '__main__':
+	sys.path.append('../client/python-common/')
+
 import gmLog
 _log = gmLog.gmDefLog
 _log.Log(gmLog.lInfo, __version__)
+if __name__ == '__main__':
+	_log.SetAllLogLevels(gmLog.lData)
 
-import gmExceptions, gmSerialPort
+import gmExceptions, gmSerialTools
 
 # 3rd party
-import serial
+import serial, mx.DateTime as mxDT
 
 #========================================================
 class cClinitek50:
 
+	# constant declarations
 	cmd_req_ID = chr(5)
 	cmd_NAK_packet = chr(21)
 	ETX = chr(3)
@@ -32,11 +40,14 @@ class cClinitek50:
 	dev_id = '6510'
 	known_good_dev_revs = ('  ', 'A ')
 	known_good_sw_versions = ('01.00', '01.02')
+	known_stix_types = ('MULTISTIX 8 SG',)
+	time_format = '%H%M'
 
-	def __init__(self, aPort = 0):
+	def __init__(self, aPort = 0, aDateFormat = '%y%m%d'):
 		"""Setup low level driver and check attached device."""
 		# instantiate low level hardware driver
 		try:
+			# those values are fixed according to the specs
 			self.__drv = serial.Serial (
 				aPort,
 				baudrate = 9600,
@@ -46,38 +57,26 @@ class cClinitek50:
 				timeout = 2,
 				xonxoff = 1
 			)
-		except StandardError:
+		except:
 			_log.LogException('cannot open serial port', sys.exc_info(), verbose = 1)
 			raise
 
-		self.__drv.setDTR(level = 0)
+		# must be LOW according to specs,
+		# if set to HIGH and connected the device
+		# controller will enter programming mode
+		self.__drv.setDTR(0)
 		self.__drv.flushInput()
 		self.__drv.flushOutput()
 
 		_log.Log(gmLog.lData, self.__drv)
 
+		self.__date_format = aDateFormat
+
 		if not self.__detect_device():
-			raise gmExceptions.ConstructorError
-
-
-		# check attached device
-		_log.Log(gmLog.lInfo, 'device info: "%s"' % self.getVerObsolete())
-		# verify model/firmware
-		result = self.getVer()
-		_log.Log(gmLog.lInfo, 'device info: "%s"' % result)
-		if result is None:
-			_log.Log(gmLog.lPanic, 'device info request timed out, no device/link error/cable failure')
-			raise IOError, "cannot detect device"
-		# check for known firmware strings
-		if result['data'] not in self.__known_devices:
-			_log.Log(gmLog.lWarn, 'It is not known whether the device "%s" will work properly with' % result['payload'])
-			_log.Log(gmLog.lWarn, 'this driver. Please report your mileage and the device id string')
-			_log.Log(gmLog.lWarn, 'to the authors at %s.' % __author__)
-			_log.Log(gmLog.lInfo, 'Currently known device id strings:')
-			_log.Log(gmLog.lInfo, str(self.__known_devices))
+			raise gmExceptions.ConstructorError, 'cannot detect and verify Clinitek 50 device'
 	#----------------------------------------------------
 	def __detect_device(self):
-		self.__drv.write(gmClinitek50.cmd_req_ID)
+		self.__drv.write(cClinitek50.cmd_req_ID)
 		packet = self.__get_packet()
 		if packet is None:
 			return None
@@ -87,30 +86,30 @@ class cClinitek50:
 	#----------------------------------------------------
 	def __get_packet(self):
 		# wait for ETX which is supposed to terminate all packets
-		successful, packet = gmSerialPort.wait_for_str (
+		successful, packet = gmSerialTools.wait_for_str (
 			self.__drv,
-			gmClinitek50.ETX,
-			gmClinitek50.timeout,
-			gmClinitek50.max_packet_size
+			cClinitek50.ETX,
+			cClinitek50.timeout,
+			cClinitek50.max_packet_size
 		)
 		if not successful:
 			_log.Log(gmLog.lErr, 'receiving packet from device failed')
-			_log.Log(gmLog.lData, packet, gmLog.lCooked)
 			return None
 		if not self.__verify_generic_packet_structure(packet):
 			# NAK packet
-			self.__drv.write(gmClinitek50.cmd_NAK_packet)
+			self.__drv.write(cClinitek50.cmd_NAK_packet)
 			return None
+		_log.Log(gmLog.lData, 'received generically valid packet')
 		return packet
 	#----------------------------------------------------
 	def __verify_generic_packet_structure(self, packet):
 		# does it start with STX CR LF ?
-		if packet[:3] != gmClinitek50.packet_header:
+		if packet[:3] != cClinitek50.packet_header:
 			_log.Log(gmLog.lErr, 'packet does not start with STX CR LF')
 			_log.Log(gmLog.lData, packet, gmLog.lCooked)
 			return None
 		# does it have at least 5 lines ?
-		if not len(string.split(packet, gmClinitek50.EOL)) > 4:
+		if not len(string.split(packet, cClinitek50.EOL)) > 4:
 			_log.Log(gmLog.lErr, 'packet does not have at least 5 lines')
 			_log.Log(gmLog.lData, packet, gmLog.lCooked)
 			return None
@@ -125,25 +124,46 @@ class cClinitek50:
 		return 1
 	#----------------------------------------------------
 	def __verify_detect_packet(self, packet):
-		lines = string.split(packet, gmClinitek50.EOL)
+		lines = string.split(packet, cClinitek50.EOL)
 		# product ID: 6510 = Clinitek 50
 		tmp = lines[1][:4]
-		if tmp != gmClinitek50.dev_id:
-			_log.Log(gmLog.lErr, 'device does not seem to be a Clinitek 50, product ID is [%s], expected [%s]' % (tmp, gmClinitek50.dev_id))
+		if tmp != cClinitek50.dev_id:
+			_log.Log(gmLog.lErr, 'device does not seem to be a Clinitek 50, product ID is [%s], expected [%s]' % (tmp, cClinitek50.dev_id))
 			_log.Log(gmLog.lData, lines)
 			return None
 		# product revision
 		tmp = lines[1][4:6]
-		if tmp not in gmClinitek50.known_good_dev_revs:
+		if tmp not in cClinitek50.known_good_dev_revs:
 			_log.Log(gmLog.lWarn, 'product revision [%s] untested, trying to continue anyways' % tmp)
 		# software version
-		tmp = lines[6:11]
-		if tmp not in gmClinitek50.known_good_sw_versions:
+		tmp = lines[1][6:11]
+		if tmp not in cClinitek50.known_good_sw_versions:
 			_log.Log(gmLog.lWarn, 'software version [%s] untested, trying to continue anyways' % tmp)
-
+		# date/time
+		timestamp = mxDT.strptime(lines[1][12:22], self.__date_format + cClinitek50.time_format)
+		_log.Log(gmLog.lInfo, 'device timestamp: %s' % timestamp)
+		_log.Log(gmLog.lInfo, 'system timestamp: %s' % mxDT.now())
+		age = mxDT.Age(mxDT.now(), timestamp)
+		if age.hours > 6:
+			_log.Log(gmLog.lErr, 'device time is off by %s, please correct that' % age)
+			return None
+		# language-unit profile
+		(lang, units) = string.split(lines[2], ' - ')
+		_log.Log(gmLog.lInfo, 'language: %s' % lang)
+		_log.Log(gmLog.lInfo, 'unit system: %s' % units)
+		# STIX type
+		stix_type = string.trim(line[3])
+		if not stix_type in cClinitek50.known_stix_types:
+			_log.Log(gmLog.lErr, "don't know how to handle stix of type %s" % stix_type)
+			return None
+		# seems valid
+		return 1
 #========================================================
 # main
 #========================================================
+if __name__ == '__main__':
+	# try to init device
+	dev = cClinitek50(2)
 
 #========================================================
 # docs
@@ -185,6 +205,9 @@ class cClinitek50:
 
 #========================================================
 # $Log: Clinitek50.py,v $
-# Revision 1.1  2003-11-17 01:34:55  ncq
+# Revision 1.2  2003-11-19 18:11:39  ncq
+# - should be able to detect devices
+#
+# Revision 1.1  2003/11/17 01:34:55  ncq
 # - initial checkin, non-functional
 #
