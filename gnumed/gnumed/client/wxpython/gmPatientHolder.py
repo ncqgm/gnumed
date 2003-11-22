1cdfea92784d49ba45aa1475fec63d739f5ac2e5
Karsten Hilbert <karsten.hilbert@gmx.net>
2003-11-22 14:53:06 +0000

    - coding style cleanup

--- gnumed/gnumed/client/wxpython/gmPatientHolder.py
+++ gnumed/gnumed/client/wxpython/gmPatientHolder.py
@@ -1,30 +1,19 @@
-
-
 import gmDispatcher, gmSignals
 from gmPatient import gmCurrentPatient
-import sys, traceback
+import sys
 import gmLog
-
-def _print(*kargs):
-	l = []
-	for x in kargs:
-		l.append(str(x))
-	msg = "  ".join(l)
-
-	gmLog.gmDefLog.Log( gmLog.lInfo, msg )	
 
 class PatientHolder:
 	def __init__(self):
 		gmDispatcher.connect( self._setPatientModel, gmSignals.patient_selected() )
 
 	def _setPatientModel( self, kwds = None):
-		print kwds
 		self.patient = gmCurrentPatient()
 		try:
 			self._updateUI()
 		except:
 			gmLog.gmDefLog.LogException( "updateUI problem", sys.exc_info(), verbose=1)
-	
+
 	def get_patient(self):
 		return self.patient
 
@@ -41,4 +30,4 @@ class PatientHolder:
 		return self.get_clinical_record().get_allergies_manager()
 	
 	def _updateUI(self):
-		_print("please override _updateUI() in ", self.__class__.__name__)
+		gmLog.gmDefLog.Log(gmLog.lWarn, "please override me in %s", self.__class__.__name__)
