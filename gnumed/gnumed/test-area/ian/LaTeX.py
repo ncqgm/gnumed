949bf0234f4f6d34365a77cfc6731891a3eeea11
ihaywood <ihaywood>
2003-11-25 11:16:10 +0000

    putative forms system

--- gnumed/gnumed/test-area/ian/LaTeX.py
+++ gnumed/gnumed/test-area/ian/LaTeX.py
@@ -10,22 +10,29 @@ def texify (item):
     escape a string/list properly acording to TeX markup
     """
     if type (item) is types.StringType:
-        item = item.replace ("\\", "\\backspace")
+        item = item.replace ("\\", "\\backspace ")
         item = item.replace ("&", "\\&")
         item = item.replace ("$", "\\$")
+        item = item.replace ("%", "\\%")
         item = item.replace ('"', "")
         item = item.replace ("\n", "\\\\ ")
-        if len (item).strip () == 0:
-            item = "\relax"
+        item = item.replace ("\\\\ \\\\ ", "\n\n") # this a cheat to protect double newlines
+        item = item.replace ("{", "\\{")
+        item = item.replace ("}", "\\}")
+        if len (item.strip ()) == 0:
+            item = "\\relax"
     if type (item) is types.ListType:
         # convert list of lists to LaTeX table format 
         str = ''
-        for line in item:
-            line = [texify (i) for i in line]
-            linestr = string.join (line, " & ")
-            str += linestr
-            str += ' \\\\ '
-        item = str
+        if len (item) == 0:
+            item = '\\relax'
+        else:
+            for line in item:
+                line = [texify (i) for i in line]
+                linestr = string.join (line, " & ")
+                str += linestr
+                str += ' \\\\ '
+            item = str
     # LaTeX doesn't cope with null strings. \relax is the the TeX
     # no-op instruction
     if type (item) is types.NoneType:
@@ -35,7 +42,7 @@ def texify (item):
 class LaTeX:
     def __init__ (self, file, params = {}):
         """
-        file is the filename of the LaTeX file.
+        file is the filename or python file object of the LaTeX file.
         params is a dictionary of parameters for latex forms
         parameters can be strings, lists of lists (which are mapped to latex tables) and booleans
         """
@@ -43,44 +50,25 @@ class LaTeX:
         self.params = params
         # create a 'playground' directory for LaTeX
         self.tmp = tempfile.mktemp ()
-        os.makedirs (self.tmp)
 
     def run (self):
+        os.makedirs (self.tmp)
         self.oldcwd = os.getcwd ()
-        self.stub, ext = os.path.splitext (os.path.basename (self.latex))
         os.chdir (self.tmp) # move to the playground
+        self.old_umask = os.umask (066) # set umask so other users cannot read our files
+        if type (self.latex) == types.StringType:
+            self.latex = file (self.latex) # open file if necessary
         # run latex as a co-process
-        stdin, stdout = os.popen4 ("latex %s" % self.latex, "w", 2048)
-        oldlines = []
-        line = stdout.readline ()
-        while line:
-            # script requests a field value
-            m = re.match ("^:F(.*)=$", line)
-            if m:
-                if self.params.has_key (m.group(1)):
-                    # we have this parameter, send it on stdin
-                    stdin.write (texify (self.params[m.group (1)]) + "\n")
-                else:
-                    stdin.write ("\\relax\n")
-            # script requests a flag
-            m = re.match ("^:C(.*)=$", line)
-            if m:
-                if self.params.has_key (m.group(1)) and self.params[m.group(1)]:
-                    stdin.write ("1\n")
-                else:
-                    # return false if field false or not found
-                    stdin.write ("0\n")
-            if re.match ("^!.*", line): # we have an LaTeX error
-                stdin.write ("X\n") # causes LaTeX to quit gracefully
-                print line
-                print oldlines
-                return
-            oldlines.append (line.strip ())
-            line = stdout.readline ()
+        stdin = os.popen ("latex", "w", 2048)
+        # push the parameters to the interpreter as TeX commands
+        for param in self.params.keys ():
+            stdin.write ("\\def\\%s{%s}\n" % (param, texify (self.params[param])))
+        # send the template
+        stdin.write (self.latex.read ())
+        stdin.write ('\n\\end\n')
         stdin.close ()
-        stdout.close ()
 
-    def printout (self, printer=None, job=None):
+    def print_unix (self, printer=None, job=None):
         """
         Printing on UNIX via lpr
         printer: printer passed to lpr
@@ -89,27 +77,27 @@ class LaTeX:
         # call dvips to do the printing
         if printer:
             if job:
-                os.system ("dvips %s.dvi -o ! lpr -P%s -J%s" % (self.stub, printer, job))
+                os.system ("dvips texput.dvi -o !\"lpr -P%s -J%s\"" % (printer, job))
             else:
-                os.system ("dvips %s.dvi -o ! lpr -P%s" % (self.stub, printer))
+                os.system ("dvips texput.dvi -o !\"lpr -P%s\"" % printer)
         else:
-            os.system ("dvips %s.dvi" % self.stub)
+            os.system ("dvips texput.dvi -o !lpr")
 
     def print_win (self, printer=None):
         """
         Printing on Windows. Not tested
         """
         # call dvips to make PostScript file
-        os.system ("dvips %s.dvi -o %s.ps" % (self.stub, self.stub))
+        os.system ("dvips texput.dvi -o texput.ps")
         # call GSPRINT.EXE to do the printing (http://www.cs.wisc.edu/~ghost/gsview/gsprint.htm)
         if printer:
-            os.system ("gsprint %s.ps" % self.stub)
+            os.system ("gsprint texput.ps")
         else:
-            os.system ("gsprint %s.ps -printer %s" % (self.stub, printer))
+            os.system ("gsprint texput.ps -printer %s" % printer)
 
     def xdvi (self):
         # for debugging only, previews the document
-        os.system ('xdvi %s.dvi' % self.stub)
+        os.system ('xdvi texput.dvi')
 
     def finish (self):
         # delete all files
@@ -118,68 +106,88 @@ class LaTeX:
         os.chdir (self.oldcwd)
         # delete directory
         os.rmdir (self.tmp)
+        # restore umask
+        os.umask (self.old_umask)
 
 
 # test form variables
 #<DEBUG>
 
-letter = {'to_address':"""
-Ian Haywood
+letter = {'recipient':"""Ian Haywood
 2/76 Smith St
 North Melbourne 3076""",
-          'from_address':"""
-Richard Terry
-175 Jones Rd
+          'senderaddress':"""175 Jones Rd
 Bluebridge 2675""",
-          'patient_name':"Joe Bloggs",
-          'patient_address':'12 Main Rd, North Melbourne',
-          'patient_DOB':'1/1/70',
-          'patient_gender':'M',
-          'meds_list':[
+          'sender':'Richard Terry',
+          'patientname':"Joe Bloggs",
+          'patientaddress':'12 Main Rd, North Melbourne',
+          'patientDOB':'1/1/70',
+          'patientgender':'M',
+          'medslist':[
     ['Frusemide', '80mg', 'nocte'],
     ['Atenolol', '5mg', 'mane']
     ],
-          'disease_list':[
+          'diseaselist':[
     ['Congestive cardiac failure'],
     ['Ischaemic heart disease']
     ],
-          'main_text':"""
-          Thankyou for seeing this patient. No reason in particular
-          """,
-          'salutation':'Dear Richard,',
-          'closing':'Yours sincerely,'}
+          'maintext':"""Thankyou for seeing this patient. No reason in particular
 
-form = {'to_address':"""Ian Haywood
+This is a second paragraph.""",
+          'salutation':'Dear Ian,',
+          'finish':'Yours sincerely,'}
+
+form = {'recipient':"""Ian Haywood
 2/76 Smith St
 North Melbourne 3076""",
-            'from_address':"""Richard Terry
-175 Jones Rd
+        'senderaddress':"""175 Jones Rd
 Bluebridge 2675""",
-        'patient_name':"Joe Bloggs",
-        'patient_address':'12 Main Rd, North Melbourne',
-        'patient_DOB':'1/1/70',
-        'patient_gender':'M',
+        'sender':'Richard Terry',
+        'patientname':"Joe Bloggs",
+        'patientaddress':'12 Main Rd, North Melbourne',
+        'patientDOB':'1/1/70',
+        'patientgender':'M',
         'request':'Cerebro-vascular',
-        'clinical_notes':'Some meaning clinical notes here.',
-        'Urgent':1,
-        'Veteran':1,
+        'clinicalnotes':'Some meaning clinical notes here.',
+        'urgent':1,
+        'veteran':1,
+        'fax':1,
+        'phone':1,
+        'veteran':1,
+        'pads':1,
+        'pensioner':1,
+        'routine':1,
+        'therapy':'',
+        'copyaddress':'',
         'instructions':'Nothing to eat or drink six hours prior.'
         }
 
-script = {'doctor_name':'Ian Haywood',
-          'prescriber_number':'2197115',
-          'provider_number':'2465641J',
-          'address':"""2/74 Haines St
-North Melbourne 3052""",
+script = {'doctorname':'Ian Haywood',
+          'prescribernumber':'2197115',
+          'providernumber':'2465641J',
           'drugs':[['Frusemide', '80mg', 'BD'],
                    ['Atenolol', '20mg', 'OD']
                    ],
-          'patient_name':'Joe Bloggs',
-          'patient_address':"""20 Smith St
+          'patientname':'Joe Bloggs',
+          'patientaddress':"""20 Smith St
           North Melbourne 3052""",
-          'medicare_number':'11111111'
+          'medicarenumber':'11111111',
+          'brandsubstitution':1
           }
-            
-              
+
+def test ():
+    l = LaTeX ('/home/ian/gnumed/gnumed/test-area/ian/script.tex', script)
+    l.run ()
+    l.xdvi ()
+    l.finish ()
+    l = LaTeX ('/home/ian/gnumed/gnumed/test-area/ian/letter.tex', letter)
+    l.run ()
+    l.xdvi ()
+    l.finish ()
+    l = LaTeX ('/home/ian/gnumed/gnumed/test-area/ian/terry-form.tex', form)
+    l.run ()
+    l.xdvi ()
+    l.print_unix (printer = "rlp", job = "1234557")
+    l.finish ()
 
 #</DEBUG>
