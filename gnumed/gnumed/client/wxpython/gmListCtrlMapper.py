f0ebba9a758c0fd97ff78f8029483c18293689da
Karsten Hilbert <karsten.hilbert@gmx.net>
2003-11-30 01:09:48 +0000

    - remove verbose logging - do we need this at all ?

--- gnumed/gnumed/client/wxpython/gmListCtrlMapper.py
+++ gnumed/gnumed/client/wxpython/gmListCtrlMapper.py
@@ -4,22 +4,17 @@ try:
 except:
 	sys.path.append('../python-common')
 	import gmLog
-	
+
 class gmListCtrlMapper:
 
     def __init__(self, listCtrl):
 	    self.list = listCtrl
 
     def SetData( self, map):
-	gmLog.gmDefLog.Log (gmLog.lData, "SETTING DATA FOR %s with %s" % ( str(list) ,  map ))
 	items = map.items()
 	self.list.DeleteAllItems()
 	for x in range(len(items)):
             key, data = items[x]
-            #<DEBUG>
-	    gmLog.gmDefLog.Log (gmLog.lData, items[x])
-            #</DEBUG>
-	    #print x, data[0],data[1]
 	    self.list.InsertStringItem(x, data[0])
             self.list.SetItemData(x, key)
 	    for row in range( 1, len(data)):
@@ -36,5 +31,4 @@ class gmListCtrlMapper:
 			list.append( l.GetItem(x, col).GetText() )
 		k = l.GetItemData(x)
 		map[k] = list
-	return map	
-	
+	return map
