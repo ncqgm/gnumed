fd478c48ba9f346193bcf2d70f18d1a8e4f1a29d
uid66147 <uid66147>
2003-12-29 16:37:03 +0000

    - whoami class is now a borg which automatically handles shared state - more precise naming of API - remove setUser/Machine - handle staff tables - add test code

--- gnumed/gnumed/client/python-common/gmWhoAmI.py
+++ gnumed/gnumed/client/python-common/gmWhoAmI.py
@@ -1,64 +1,118 @@
 #===================================================
-__version__ = "$Revision: 1.2 $"
+__version__ = "$Revision: 1.3 $"
 __author__ = "Hilmar.Berger@gmx.de"
 __license__ = "GPL"
-
-import gmGuiBroker
 
 import gmLog
 _log = gmLog.gmDefLog
 if __name__ == '__main__':
 	_log.SetAllLogLevels(gmLog.lData)
+_log.Log(gmLog.lData, __version__)
+
+import gmGuiBroker, gmPG, gmBorg, gmExceptions, gmCfg
+_cfg = gmCfg.gmDefCfgFile
 
 #===================================================
-class cWhoAmI:
-	"""
-	This class holds information on who and on which machine we are.
-	Right now we can only determine these information if
-	a) guiBroker is running and has 'workplace' and 'user' set or
-	b) we have set this manually via setUser() or setMachine() before
-	"""
-#	_GuiBroker = None
-	_user = None
-	_machine = None
+class cWhoAmI(gmBorg.cBorg):
+	"""Who we are and on which machine we sit.
 
+	- db_account is derived from CURRENT_USER in the database, 
+	  thus it is determined by the name used to login
+	- workplace is determined by what is set in the config file
+	- staff_* is determined by CURRENT_USER via a mapping table
+	"""
 	def __init__(self):
-		self.mGuiBroker = gmGuiBroker.GuiBroker()
-		cWhoAmI._User = None
-		cWhoAmI._mMachine = None
+		gmBorg.cBorg.__init__(self)
+	#-----------------------------------------------
+	def __get_db_account(self):
+		cmd = 'select CURRENT_USER'
+		result = gmPG.run_ro_query('default', cmd)
+		if result is None:
+			_log.Log(gmLog.lPanic, 'cannot retrieve database account name')
+			return None
+		if len(result) == 0:
+			_log.Log(gmLog.lPanic, 'cannot retrieve database account name')
+			return None
+		self._db_account = result[0][0]
+		return 1
+	#-----------------------------------------------
+	def get_db_account(self):
+		"""return db account"""
+		try:
+			return self._db_account
+		except AttributeError:
+			pass
 
-	def getUser(self):
-		"""
-		Returns: user name if set via GuiBroker or manually via SetUser()
-			 None otherwise
-		"""
-		if self.mGuiBroker.has_key('currentUser'):
-			return self.mGuiBroker['currentUser']
-		else:
-			return cWhoAmI._user
-	
-	
-	def getMachine(self):
-		"""
-		Returns: machine name if set via GuiBroker or manually via SetMachine()
-			 None otherwise
-		"""
-		if self.mGuiBroker.has_key('workplace_name'):
-			return self.mGuiBroker['workplace_name']
-		else:
-			return cWhoAmI._machine
-	
-	def setUser(self,aUser):
-		""" Set user name for this session."""
-		cWhoAmI._user = aUser
-	
-	def setMachine(self,aMachine):
-		""" Set machine name for this session."""
-		cWhoAmI._machine = aMachine 
+		if not self.__get_db_account():
+			_log.Log(gmLog.lPanic, 'cannot retrieve database account name')
+			raise ValueError, 'cannot retrieve database account name'
 
+		return self._db_account
+	#-----------------------------------------------
+	def get_workplace(self):
+		try:
+			return self._workplace
+		except AttributeError:
+			pass
+
+		self._workplace = '__default__'
+		if _cfg is None:
+			print _('No config file to read workplace name from !')
+		else:
+			tmp = _cfg.get('workplace', 'name')
+			if tmp is None:
+				print _('You should name this workplace to better identify the machine !\nTo do this set the option "name" in the group [workplace] in the config file !')
+			else:
+				self._workplace = tmp
+		return self._workplace
+	#-----------------------------------------------
+	def get_staff_ID(self):
+		try:
+			return self._staff_ID
+		except AttributeError:
+			pass
+
+		cmd = "select pk_staff from v_staff where db_user=CURRENT_USER"
+		result = gmPG.run_ro_query('personalia', cmd, None)
+		if result is None:
+			raise ValueError, _('cannot resolve db account name to primary key of staff member')
+		if len(result) == 0:
+			raise ValueError, _('no correspondig staff member for current database login')
+		self._staff_ID = result[0][0]
+		return self._staff_ID
+	#-----------------------------------------------
+	def get_staff_name(self):
+		try:
+			return self._staff_name
+		except AttributeError:
+			pass
+
+		cmd = "select title, firstnames, lastnames from v_staff where db_user=CURRENT_USER"
+		result = gmPG.run_ro_query('personalia', cmd, None)
+		if result is None:
+			raise ValueError, _('cannot get staff name')
+		if len(result) == 0:
+			raise ValueError, _('no staff name on file for current database login')
+		self._staff_name = '%s%s.%s' % (result[0][0], result[0][1][:1], result[0][2])
+		return self._staff_name
+#===================================================
+if __name__ == '__main__':
+	_ = lambda x:x
+	whoami = cWhoAmI()
+	print "workplace :", whoami.get_workplace()
+	print "db account:", whoami.get_db_account()
+	print "staff ID  :", whoami.get_staff_ID()
+	print "staff name:", whoami.get_staff_name()
 #===================================================
 # $Log: gmWhoAmI.py,v $
-# Revision 1.2  2003-11-17 10:56:37  sjtan
+# Revision 1.3  2003-12-29 16:37:03  uid66147
+# - whoami class is now a borg which automatically handles shared state
+# - more precise naming of API
+# - remove setUser/Machine
+# - handle staff tables
+# - add test code
+#
+# Revision 1.2  2003/11/17 10:56:37  sjtan
 #
 # synced and commiting.
 #
