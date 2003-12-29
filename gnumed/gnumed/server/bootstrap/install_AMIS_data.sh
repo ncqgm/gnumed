b567a313f55a3c4e3a398a8a76ca71a2a53d8c88
uid67323 <uid67323>
2003-12-29 21:09:00 +0000

    -added some informative messages

--- gnumed/gnumed/server/bootstrap/install_AMIS_data.sh
+++ gnumed/gnumed/server/bootstrap/install_AMIS_data.sh
@@ -15,10 +15,19 @@ cat $SQL_DIR/country.specific/de/amis-import_data_template.sql | sed "s%AMIS_PAT
 psql -d $GNUMED_DB -U gm-dbowner
 
 # eventually set config parameters for AMIS drug browser
+echo "Now config parameters will be set in the GnuMed database so that GnuMed will"
+echo "know which drug data should be accessed. These settings can be changed via"
+echo "the Setup plugin in GnuMed. The corresponding parameters are"
+echo "DrugReferenceBrowser.drugDBName and DrugReferenceBrowser.amis.configfile"
+echo "stored under DEFAULT_USER_DEFAULT_MACHINE."
+echo "You will have to login to the GnuMed database to write these data." 
 env PYTHONPATH=$MODULES_DIR $MODULES_DIR/tools/transferDBset.py -i ./amis-config.set
 
 # $Log: install_AMIS_data.sh,v $
-# Revision 1.2  2003-11-09 17:50:06  ncq
+# Revision 1.3  2003-12-29 21:09:00  uid67323
+# -added some informative messages
+#
+# Revision 1.2  2003/11/09 17:50:06  ncq
 # - make sure data is imported as user gm-dbowner
 #
 # Revision 1.1  2003/11/09 14:53:40  hinnef
