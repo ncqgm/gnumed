98a2d4391a3aff14d683f57a265ee7f035737232
uid66147 <uid66147>
2003-12-29 19:11:13 +0000

    - reworked public DB generation

--- gnumed/gnumed/server/bootstrap/redo-public.sh
+++ gnumed/gnumed/server/bootstrap/redo-public.sh
@@ -2,7 +2,20 @@
 
 echo "re-bootstrapping public database"
 echo "dropping old database"
-dropdb -U gm-dbowner gnumed-public
-echo "bootstrappping new database"
+dropdb -U gm-dbowner gnumed
+echo "============================"
+echo "bootstrappping core database"
 rm -rf redo-public.log
-bootstrap-gm_db_system.py --log-file=redo-public.log --conf-file=bootstrap-public_db.conf
+bootstrap-gm_db_system.py --log-file=redo-public-core.log --conf-file=bootstrap-monolithic_core.conf
+echo "========================="
+echo "adding data for locale DE"
+rm -rf redo-public-de.log
+bootstrap-gm_db_system.py --log-file=redo-public-de.log --conf-file=bootstrap-de.conf
+echo "========================="
+echo "adding data for locale AU"
+rm -rf redo-public-au.log
+bootstrap-gm_db_system.py --log-file=redo-public-au.log --conf-file=bootstrap-au.conf
+echo "================"
+echo "adding test data"
+rm -rf redo-public-test_data.log
+bootstrap-gm_db_system.py --log-file=redo-public-test_data.log --conf-file=bootstrap-test_data.conf
