3364d74c3cbbba981615e2c3ba0f7bc67fbd2242
uid66147 <uid66147>
2003-12-29 16:09:50 +0000

    - not needed anymore - whitespace fix

--- gnumed/gnumed/server/bootstrap/make-links.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/server/bootstrap/Attic/make-links.sh,v $
-# $Id: make-links.sh,v 1.2 2003-11-02 12:48:55 ncq Exp $
-
-echo "setting up links"
-
-ln -vfs ../../client/python-common modules
