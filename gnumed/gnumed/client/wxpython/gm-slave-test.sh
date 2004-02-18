23b250e75c99e7696f8f86799b5733bbeb1b0fb2
Karsten Hilbert <karsten.hilbert@gmx.net>
2004-02-18 13:44:07 +0000

    - make logfile local

--- gnumed/gnumed/client/wxpython/gm-slave-test.sh
+++ gnumed/gnumed/client/wxpython/gm-slave-test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/wxpython/Attic/gm-slave-test.sh,v $
-# $Revision: 1.1 $
+# $Revision: 1.2 $
 
 # only set this if you really know what you are doing
 #export GNUMED_DIR=/foo/bar
@@ -10,4 +10,4 @@
 #export LANG=fr
 
 # start GnuMed
-python gnumed.py --debug --slave=slave-test
+python gnumed.py --debug --log-file=./gm-slave-test.log --slave=slave-test
