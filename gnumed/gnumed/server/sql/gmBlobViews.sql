47ab4e56b46af495f89fede998a98e3c06943e9c
Karsten Hilbert <karsten.hilbert@gmx.net>
2004-03-03 15:47:32 +0000

    - collect blob views in their own file

---
+++ gnumed/gnumed/server/sql/gmBlobViews.sql
@@ -0,0 +1,100 @@
+-- BLOB views for GNUmed
+
+-- license: GPL
+-- author: Karsten Hilbert <Karsten.Hilbert@gmx.net>
+
+-- $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/server/sql/gmBlobViews.sql,v $
+-- $Revision: 1.1 $ $Date: 2004-03-03 15:47:32 $ $Author: ncq $
+
+-- ===================================================================
+-- force terminate + exit(3) on errors if non-interactive
+\set ON_ERROR_STOP 1
+
+-- =============================================
+\unset ON_ERROR_STOP
+drop view v_i18n_doc_type;
+\set ON_ERROR_STOP 1
+
+create view v_i18n_doc_type as
+	select
+		doc_type.id,
+		_(doc_type.name)
+	from
+		doc_type
+	;
+
+-- this should include: distinct on _(doc_type.name) or some such
+-- =============================================
+\unset ON_ERROR_STOP
+drop view v_obj4doc;
+\set ON_ERROR_STOP 1
+
+create view v_obj4doc as
+select
+	dm.id as pk_doc,
+	dobj.id as pk_obj,
+	dm.patient_id as pk_patient,
+	dm.type as pk_type,
+	dt.name as type,
+	_(dt.name) as l10n_type,
+	dm.comment as doc_comment,
+	dm.date as date_generated,
+	dm.ext_ref as ext_ref,
+	dobj.seq_idx as seq_idx,
+	dobj.comment as obj_comment,
+	dobj.data as object
+from
+	doc_med dm,
+	doc_obj dobj,
+	doc_type dt
+where
+	dm.type=dt.id and
+	dobj.doc_id=dm.id
+;
+
+-- =============================================
+\unset ON_ERROR_STOP
+drop view v_latest_mugshot;
+\set ON_ERROR_STOP 1
+
+create view v_latest_mugshot as
+select
+	vo4d.pk_patient as pk_patient,
+	vo4d.doc_comment as doc_comment,
+	vo4d.date_generated as date_taken,
+	vo4d.ext_ref as ext_ref,
+	vo4d.obj_comment as obj_comment,
+	vo4d.object as image,
+	dd.text as description,
+	vo4d.pk_doc as pk_doc,
+	vo4d.pk_obj as pk_obj,
+	dd.id as pk_desc
+from
+	v_obj4doc vo4d,
+	doc_desc dd
+where
+	vo4d.type='patient photo' and
+	dd.doc_id=vo4d.pk_doc and
+	vo4d.seq_idx=(
+		select max(seq_idx)
+		from v_obj4doc vo4d1
+		where vo4d1.pk_patient=vo4d.pk_patient
+	)
+;
+-- =============================================
+GRANT SELECT ON
+	v_i18n_doc_type
+	, v_obj4doc
+	, v_latest_mugshot
+TO GROUP "gm-doctors";
+
+-- =============================================
+-- do simple schema revision tracking
+delete from gm_schema_revision where filename='$RCSfile: gmBlobViews.sql,v $';
+INSERT INTO gm_schema_revision (filename, version) VALUES('$RCSfile: gmBlobViews.sql,v $', '$Revision: 1.1 $');
+
+-- =============================================
+-- $Log: gmBlobViews.sql,v $
+-- Revision 1.1  2004-03-03 15:47:32  ncq
+-- - collect blob views in their own file
+--
--- gnumed/gnumed/server/sql/gmBlobs.sql
+++ gnumed/gnumed/server/sql/gmBlobs.sql
@@ -4,52 +4,67 @@
 -- author: Karsten Hilbert <Karsten.Hilbert@gmx.net>
 
 -- $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/server/sql/gmBlobs.sql,v $
--- $Revision: 1.39 $ $Date: 2004-03-03 05:24:01 $ $Author: ihaywood $
+-- $Revision: 1.40 $ $Date: 2004-03-03 15:47:31 $ $Author: ncq $
 
 -- ===================================================================
 -- force terminate + exit(3) on errors if non-interactive
 \set ON_ERROR_STOP 1
 
 -- =============================================
-CREATE TABLE "doc_type" (
+\unset ON_ERROR_STOP
+
+create table xlnk_identity (
+	pk serial primary key,
+	xfk_identity integer unique not null,
+	pupic text unique not null,
+	data text unique default null
+) inherits (audit_fields);
+
+select add_x_db_fk_def('xlnk_identity', 'xfk_identity', 'personalia', 'identity', 'id');
+select add_table_for_audit('xlnk_identity');
+
+comment on table xlnk_identity is
+	'this is the one table with the unresolved identity(id)
+	 foreign key, all other tables in this service link to
+	 this table, depending upon circumstances one can add
+	 dblink() verification or a true FK constraint (if "personalia"
+	 is in the same database as "historica")';
+
+\set ON_ERROR_STOP 1
+-- =============================================
+CREATE TABLE doc_type (
 	id serial primary key,
 	name text unique
 );
 
 -- =============================================
-create view v_i18n_doc_type (id, name) as
-	select
-		doc_type.id,
-		_(doc_type.name)
-	from
-		doc_type
-	;
-
--- this should include: distinct on _(doc_type.name) or some such
-
--- =============================================
-CREATE TABLE "doc_med" (
-	"id" serial primary key,
-	"patient_id" integer references identity not null,
-	"type" integer references doc_type(id),
+CREATE TABLE doc_med (
+	id serial primary key,
+	patient_id integer references xlnk_identity(xfk_identity) not null,
+	type integer references doc_type(id),
 	comment text,
 	"date" timestamp with time zone default CURRENT_TIMESTAMP,
 	ext_ref text
 );
 
 COMMENT ON TABLE "doc_med" IS
-	'a medical document object possibly containing several data objects such as several pages of a paper document';
--- FIXME: this needs to be able to reference external databases
+	'a medical document object possibly containing several
+	 data objects such as several pages of a paper document';
 COMMENT ON COLUMN doc_med.patient_id IS
 	'the patient this document belongs to';
 COMMENT ON COLUMN doc_med.type IS
-	'semantic type of document (not type of file or mime type), such as >referral letter<, >discharge summary<, etc.';
+	'semantic type of document (not type of file or mime
+	 type), such as >referral letter<, >discharge summary<, etc.';
 COMMENT ON COLUMN doc_med.comment IS
-	'additional short comment such as "abdominal", "ward 3, Dr. Stein", etc.';
+	'additional short comment such as "abdominal", "ward 3,
+	 Dr. Stein", etc.';
 COMMENT ON COLUMN doc_med.date IS
-	'date of document content creation (such as exam date), NOT date of document creation or date of import; may be imprecise such as "7/99"';
+	'date of document content creation (such as exam date),
+	 NOT date of document creation or date of import; may
+	 be imprecise such as "7/99"';
 COMMENT ON COLUMN doc_med.ext_ref IS
-	'external reference string of physical document, original paper copy can be found with this';
+	'external reference string of physical document,
+	 original paper copy can be found with this';
 
 -- =============================================
 CREATE TABLE doc_obj (
@@ -60,27 +75,39 @@ CREATE TABLE doc_obj (
 	data bytea
 );
 
-COMMENT ON TABLE doc_obj IS 'possibly several of these form a medical document such as multiple scanned pages/images';
-COMMENT ON COLUMN doc_obj.seq_idx IS 'index of this object in the sequence of objects for this document';
-COMMENT ON COLUMN doc_obj.comment IS 'optional tiny comment for this object, such as "page 1"';
-COMMENT ON COLUMN doc_obj.data IS 'actual binary object data';
+COMMENT ON TABLE doc_obj IS
+	'possibly several of these form a medical document
+	 such as multiple scanned pages/images';
+COMMENT ON COLUMN doc_obj.seq_idx IS
+	'index of this object in the sequence
+	 of objects for this document';
+COMMENT ON COLUMN doc_obj.comment IS
+	'optional tiny comment for this
+	 object, such as "page 1"';
+COMMENT ON COLUMN doc_obj.data IS
+	'actual binary object data';
 
 -- =============================================
 CREATE TABLE doc_desc (
 	id serial primary key,
-	doc_id integer references doc_med(id) on delete cascade on update cascade,
+	doc_id integer
+		references doc_med(id)
+		on delete cascade
+		on update cascade,
 	"text" text
 );
 
-COMMENT ON TABLE "doc_desc" is 'A textual description of the content such as a result summary. Several of these may belong to one document object.';
+COMMENT ON TABLE doc_desc is
+	'A textual description of the content such
+	 as a result summary. Several of these may
+	 belong to one document object.';
 
 -- =============================================
 GRANT SELECT ON
-	"doc_desc",
-	"doc_obj",
-	"doc_med",
-	"doc_type",
-	"v_i18n_doc_type"
+	doc_desc,
+	doc_obj,
+	doc_med,
+	doc_type
 TO GROUP "gm-doctors";
 
 GRANT SELECT, INSERT, UPDATE, DELETE ON
@@ -90,13 +117,12 @@ GRANT SELECT, INSERT, UPDATE, DELETE ON
 	"doc_obj_id_seq",
 	"doc_med",
 	"doc_med_id_seq",
-	"doc_type",
-	"v_i18n_doc_type"
+	"doc_type"
 TO GROUP "_gm-doctors";
 
 -- =============================================
 -- do simple schema revision tracking
-INSERT INTO gm_schema_revision (filename, version) VALUES('$RCSfile: gmBlobs.sql,v $', '$Revision: 1.39 $');
+INSERT INTO gm_schema_revision (filename, version) VALUES('$RCSfile: gmBlobs.sql,v $', '$Revision: 1.40 $');
 
 -- =============================================
 -- questions:
@@ -114,7 +140,10 @@ INSERT INTO gm_schema_revision (filename, version) VALUES('$RCSfile: gmBlobs.sql
 -- - it is helpful to structure text in doc_desc to be able to identify source/content etc.
 -- =============================================
 -- $Log: gmBlobs.sql,v $
--- Revision 1.39  2004-03-03 05:24:01  ihaywood
+-- Revision 1.40  2004-03-03 15:47:31  ncq
+-- - collect blob views in their own file
+--
+-- Revision 1.39  2004/03/03 05:24:01  ihaywood
 -- patient photograph support
 --
 -- Revision 1.38  2004/01/05 00:58:27  ncq
