b53fa09afc56d9d65d4d9ede5cd22e9856456996
Karsten Hilbert <karsten.hilbert@gmx.net>
2004-03-10 00:08:31 +0000

    - add form queue handling - remove papersizes - cleanup

--- gnumed/gnumed/server/sql/gmoffice.sql
+++ gnumed/gnumed/server/sql/gmoffice.sql
@@ -1,177 +1,81 @@
--- DDL structure of 'office' tables: forms, letters, accountings, billing
+-- GnuMed office related/administrative tables
+
 -- Copyright 2002 by Dr. Horst Herb
 -- This is free software in the sense of the General Public License (GPL)
 -- For details regarding GPL licensing see http://gnu.org
---
--- usage:
---	log into psql (database gnumed OR drugs)  as administrator
---	run the script from the prompt with "\i gmoffice.sql"
---=====================================================================
 
 -- $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/server/sql/gmoffice.sql,v $
--- $Revision: 1.3 $ $Date: 2002-12-14 08:12:22 $ $Author: ihaywood $
--- ============================================================
--- $Log: gmoffice.sql,v $
--- Revision 1.3  2002-12-14 08:12:22  ihaywood
--- New prescription tables in gmclinical.sql
---
--- Revision 1.2  2002/12/06 08:50:52  ihaywood
--- SQL internationalisation, gmclinical.sql now internationalised.
---
--- Revision 1.1  2002/12/03 02:50:19  ihaywood
--- new office schema: tables for printing forms
---
---
---
+-- $Revision: 1.4 $ $Date: 2004-03-10 00:08:31 $ $Author: ncq $
 -- ===================================================================
 -- force terminate + exit(3) on errors if non-interactive
 \set ON_ERROR_STOP 1
 
-
---~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
--- Forms
---------
--- These tables describe sets of SQL queries matched to x,y positions
--- on a page, describing various forms such as prescriptions, path request 
--- slips, WorkCover forms, etc.
-
-create table papersizes
-(
-	id serial,
-	name varchar (10),
-	length float,
-	width float
+-- ===================================================================
+-- forms handling
+-- ===================================================================
+-- target classes
+create table form_target_classes (
+	pk serial primary key,
+	name text unique not null
 );
 
-comment on column papersizes.length is '(cm)';
-comment on column papersizes.width is '(cm)';
-
-
-insert into papersizes (name, length, width) values ('A0', 83.9611, 118.816);
-insert into papersizes (name, length, width) values ('A1', 59.4078, 83.9611);
-insert into papersizes (name, length, width) values ('A2', 41.9806, 59.4078);
-insert into papersizes (name, length, width) values ('A3', 29.7039, 41.9806);
-insert into papersizes (name, length, width) values ('A4', 20.9903, 29.7039);
-insert into papersizes (name, length, width) values ('A5', 14.8519, 20.9903);
-insert into papersizes (name, length, width) values ('A6', 10.4775, 14.8519);
-insert into papersizes (name, length, width) values ('A7', 7.40833, 10.4775);
-insert into papersizes (name, length, width) values ('A8', 5.22111, 7.40833);
-insert into papersizes (name, length, width) values ('A9', 3.70417, 5.22111);
-insert into papersizes (name, length, width) values ('A10', 2.61056, 3.70417);
-insert into papersizes (name, length, width) values ('B0', 100.048, 141.393);
-insert into papersizes (name, length, width) values ('B1', 70.6967 ,100.048);
-insert into papersizes (name, length, width) values ('B2',  50.0239,70.6967);
-insert into papersizes (name, length, width) values ('B3',  35.3483,50.0239);
-insert into papersizes (name, length, width) values ('B4',  25.0119,35.3483);
-insert into papersizes (name, length, width) values ('B5',  17.6742,25.0119);
-insert into papersizes (name, length, width) values ('archA', 22.86,30.48);
-insert into papersizes (name, length, width) values ('archB', 30.48,45.72);
-insert into papersizes (name, length, width) values ('archC', 45.72,60.96);
-insert into papersizes (name, length, width) values ('archD', 60.96,91.44);
-insert into papersizes (name, length, width) values ('archE', 91.44,121.92);
-insert into papersizes (name, length, width) values ('flsa',  21.59,33.02);
-insert into papersizes (name, length, width) values ('flse',  21.59,33.02);
-insert into papersizes (name, length, width) values ('halfletter',13.97,21.59);
-insert into papersizes (name, length, width) values ('note',   19.05,  25.4);
-insert into papersizes (name, length, width) values ('letter', 21.59, 27.94);
-insert into papersizes (name, length, width) values ('legal',  21.59, 35.56);
-insert into papersizes (name, length, width) values ('11x17', 27.94, 43.18);
-insert into papersizes (name, length, width) values ('ledger',43.18, 27.94);
--- paper sizes from the GostScript manpages.
-
-
-
-create table forms
-(
-	id serial,
-	iso_countrycode char (2),
-	inforce date,
-	type char check (type in ('s', 'p', 'r', 'a', 'w', 'u')),
-	name varchar (100) unique,
-	id_papersize integer references papersizes (id),
-	fontsize integer check (fontsize > 8 and fontsize < 50),
-	default boolean
+-- -------------------------------------------------------------------
+-- queue of pending forms
+create table form_queue (
+	pk serial primary key,
+	fk_form_instance integer
+		not null
+		references form_instances(pk),
+	form bytea not null,
+	fk_target_class integer
+		not null
+		references form_target_classes(pk),
+	submitted_when timestamp with time zone
+		not null
+		default CURRENT_TIMESTAMP,
+	fk_submitted_by integer not null,
+	submitted_from text not null,
+	status text not null default 'submitted'
 );
 
+-- FIXME: we need a trigger that allows UPDATEs only on "status"
+-- FIXME: we also need a notify trigger on insert/update
 
-comment on column forms.inforce is 
-'date on which this table came into force.';
-comment on table forms is 'the various forms used in practive.';
-comment on column forms.iso_countrycode is 
-'the two letter counry code of the country in which this form is used.';
-comment on column forms.default is
-'true if this form is the default in its type';
-comment on column forms.type is '
-the type of the form. This determines the variables accessible to
-the form queries (see below).
-s: script. PATIENTID, SCRIPTID
-p: pathology slip. REQUESTID: table in gmclinical, PATIENTID  
-r: radiology slip. REQUESTID: table in gmclinical, PATIENTID
-a: accounting. BEGIN, END: dates defining accounting period
-w: wage. BEGIN, END, PATIENTID: wage period and employee
-u: general user-defined form. PATIENTID is patient-id. ';
+select add_x_db_fk_def('form_queue', 'fk_submitted_by', 'personalia', 'staff', 'pk');
 
-comment on column forms.name is 
-'name of the form, usu. the issuing authority: "PBS", "WorkCover", etc.';
+comment on table form_queue is
+	'Queue table for rendered form instances. Note that
+	 the rows in this table will get deleted after processing.
+	 This is NOT an archive of form jobs.';
+comment on column form_queue.fk_form_instance is
+	'points to the unrendered source instance of the form,
+	 useful for recalling submitted jobs for changing';
+comment on column form_queue.form is
+	'the rendered form, IOW binary data such as a PDF file';
+comment on column form_queue.target_class is
+	'points to the target class';
+comment on column form_queue.submitted_when is
+	'when was this form job submitted';
+comment on column form_queue.fk_submitted_by is
+	'who of the staff submitted this form job';
+comment on column form_queue.fk_submitted_from is
+	'the workplace this form job was submitted from';
+comment on column form_queue.status is
+	'status of the form job:
+	 - submitted: ready for processing
+	 - in progress: being processed
+	 - removable: fit for removal (either cancelled, timed out or done)
+	 - halted: do not process';
 
-comment on column forms.id_papersize is
-'papersize A4, etc.';
+-- ===================================================================
 
-create table queries
-(
-	id serial,
-	name varchar (50),
-	service varchar (25),
-	query varchar (300)
-);
 
-comment on table queries is
-'SQL queries used to fill form fields';
-comment on column queries.name is
-'name of quiery to identifiy to user';
-comment on column queries.service is
-'the service on which to execute query';
-comment on column queries.query is
-'the SQL SELECT statement to execute. Variables substituted as outlined in forms.type';
-
-insert into queries (name, service, query) values ('begin', 'gmoffice', 'select BEGIN');
-insert into queries (name, service, query) values ('end', 'gmoffice', 'select END');
-insert into queries (name, service, query) values ('patient_name', 'gmidentity', 
-'select firstnames || lastnames from names where active and id_identity = PATIENTID limit 1');
-insert into queries (name, service, query) values ('patient_address', 'gmgis', 
-'select from v_home_address where id = PATIENTID');
-insert into queries (name, service, query) values ('patient_dob', 'gmidentity', 
-'select dob from identity where id = PATIENTID');
-insert into queries (name, service, query) values ('patient_age', 'gmidentity', 
-'select age (dob) from identity where id = PATIENTID');
-insert into queries (name, service, query) values ('patient_gender', 'gmidentity', 
-'select case when gender in (''m'',''tm'') then ''maennlich'' when gender in (''f'' , ''tf'') 
-then ''weiblich'' else ''unbekannt'' from identity where id = PATIENTID');
-insert into queries (name, service, query) values ('', '', '');
-
-create table formfield 
-(
-	id serial,
-	id_form integer references forms (id),
-	x integer,
-	y integer,
-	page integer,
-	id_query integer references queries (id),
-	wraparound integer
-);
-
-comment on column formfield.x is 'x-cordinate unit 720/inch, O left paper edge';
-comment on column formfield.y is 'y-cordinate unit 720/inch, o top paper edge';
-comment on column formfield.wraparound is 'wraparound for text, unit 720/inch';
-
---~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
+-- ===================================================================
 -- Embryo of billing tables
 -- This is a highly classical accounting system, with medical-specific
 -- updatable views.
 
-create table schedule 
-(
+create table schedule (
 	id serial,
 	code varchar (20),
 	name varchar (100),
@@ -179,53 +83,33 @@ create table schedule
 	description text
 );
 
-create table billing_scheme
-(
+create table billing_scheme (
 	id serial,
 	name varchar (100),
 	iso_countrycode char (2)
 );
 
-insert into billing_scheme (name, iso_countrycode) values ('Bulk-billed Medicare', 'au');
-insert into billing_scheme (name, iso_countrycode) values ('Private Billing', 'au');
-insert into billing_scheme (name, iso_countrycode) values ('Veteran''s Affairs', 'au');
-insert into billing_scheme (name, iso_countrycode) values ('WorkCover', 'au');
-
-create table prices
-(
+create table prices (
 	id_consult integer references schedule (id),
 	id_scheme integer references billing_scheme (id),
 	patient float,
 	bulkbill float
 );
- 
-comment on column prices.patient is
-'the amount of money paid by the patient';
- comment on column prices.bulkbill is
-'the amount billed directly (bulk-billed) to the payor';
 
-create table accounts
-(
+comment on column prices.patient is
+	'the amount of money paid by the patient';
+comment on column prices.bulkbill is
+	'the amount billed directly (bulk-billed) to the payor';
+
+create table accounts (
 	id serial,
 	name varchar (50),
 	extra integer
 );
 
 alter table accounts add column parent integer references accounts (id);
--- Warning: translate, but DON'T alter the order!!!
-insert into accounts (name) values ('Assets');
-insert into accounts (name) values ('Liabilities');
-insert into accounts (parent, name) values (1, 'Accounts Recievable');
-insert into accounts (parent, name) values (2, 'Accounts Payable');
-insert into accounts (parent, name) values (2, 'Capital'); 
-insert into accounts (parent, name) values (3, 'Patients');
-insert into accounts (parent, name) values (4, 'Tax');
-insert into accounts (parent, name) values (4, 'Wages'); 
-insert into accounts (parent, name) values (1, 'Cash');
-insert into accounts (parent, name) values (1, 'Inventory');
 
-create table ledger
-(
+create table ledger (
 	stamp timestamp,
 	amount float,
 	debit integer references accounts (id),
@@ -243,4 +127,23 @@ create table consults
 	id_scheme integer references billing_scheme (id)
 );
 
+-- ===================================================================
+-- do simple schema revision tracking
+delete from gm_schema_revision where filename='$RCSfile: gmoffice.sql,v $';
+INSERT INTO gm_schema_revision (filename, version) VALUES('$RCSfile: gmoffice.sql,v $', '$Revision: 1.4 $');
 
+--=====================================================================
+-- $Log: gmoffice.sql,v $
+-- Revision 1.4  2004-03-10 00:08:31  ncq
+-- - add form queue handling
+-- - remove papersizes
+-- - cleanup
+--
+-- Revision 1.3  2002/12/14 08:12:22  ihaywood
+-- New prescription tables in gmclinical.sql
+--
+-- Revision 1.2  2002/12/06 08:50:52  ihaywood
+-- SQL internationalisation, gmclinical.sql now internationalised.
+--
+-- Revision 1.1  2002/12/03 02:50:19  ihaywood
+-- new office schema: tables for printing forms
