c999bc2a9c3a369d0ccc423a78385b625e712c59
Karsten Hilbert <karsten.hilbert@gmx.net>
2004-03-18 10:29:51 +0000

    - set fk_org on EML to 99999

--- gnumed/gnumed/server/sql/test-data/test_data-USS_Enterprise.sql
+++ gnumed/gnumed/server/sql/test-data/test_data-USS_Enterprise.sql
@@ -5,7 +5,7 @@
 -- author: Karsten Hilbert <Karsten.Hilbert@gmx.net>
 -- license: GPL
 -- $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/server/sql/test-data/test_data-USS_Enterprise.sql,v $
--- $Revision: 1.1 $
+-- $Revision: 1.2 $
 -- =============================================
 -- force terminate + exit(3) on errors if non-interactive
 \set ON_ERROR_STOP 1
@@ -15,7 +15,7 @@
 insert into test_org
 	(fk_org, fk_adm_contact, fk_med_contact, internal_name, comment)
 values (
-	-1,
+	99999,
 	(select i_id from v_basic_person where firstnames='Leonard' and lastnames='Spock' and dob='1931-3-26'::timestamp),
 	(select i_id from v_basic_person where firstnames='Leonard' and lastnames='McCoy' and dob='1920-1-20'::timestamp),
 	'Enterprise Main Lab',
@@ -66,10 +66,13 @@ values (
 -- =============================================
 -- do simple schema revision tracking
 delete from gm_schema_revision where filename like '$RCSfile: test_data-USS_Enterprise.sql,v $';
-INSERT INTO gm_schema_revision (filename, version) VALUES('$RCSfile: test_data-USS_Enterprise.sql,v $', '$Revision: 1.1 $');
+INSERT INTO gm_schema_revision (filename, version) VALUES('$RCSfile: test_data-USS_Enterprise.sql,v $', '$Revision: 1.2 $');
 
 -- =============================================
 -- $Log: test_data-USS_Enterprise.sql,v $
--- Revision 1.1  2004-03-18 10:22:25  ncq
+-- Revision 1.2  2004-03-18 10:29:51  ncq
+-- - set fk_org on EML to 99999
+--
+-- Revision 1.1  2004/03/18 10:22:25  ncq
 -- - Enterprise path lab
 --
