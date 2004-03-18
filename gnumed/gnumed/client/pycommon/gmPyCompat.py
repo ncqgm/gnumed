b7b6563e42e7067db63816d03a7cfc143d36f9c8
Karsten Hilbert <karsten.hilbert@gmx.net>
2004-03-18 09:47:33 +0000

    - removed printk()

--- gnumed/gnumed/client/pycommon/gmPyCompat.py
+++ gnumed/gnumed/client/pycommon/gmPyCompat.py
@@ -5,15 +5,14 @@
 """
 #========================================================================
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/pycommon/Attic/gmPyCompat.py,v $
-# $Id: gmPyCompat.py,v 1.1 2004-03-15 15:06:28 ncq Exp $
-__version__ = "$Revision: 1.1 $"
+# $Id: gmPyCompat.py,v 1.2 2004-03-18 09:47:33 ncq Exp $
+__version__ = "$Revision: 1.2 $"
 __author__  = "K.Hilbert <Karsten.Hilbert@gmx.net>"
 
 # define dict() if < Python 2.2
 try:
 	dict(['a','b'])
 except NameError:
-	print "Defining dict()"
 	def dict(list):
 		d = {}
 		for line in list:
@@ -24,12 +23,14 @@ except NameError:
 try:
 	True
 except NameError:
-	print "defining True/False"
 	True = (1==1)
 	False = not True
 
 #========================================================================
 # $Log: gmPyCompat.py,v $
-# Revision 1.1  2004-03-15 15:06:28  ncq
+# Revision 1.2  2004-03-18 09:47:33  ncq
+# - removed printk()
+#
+# Revision 1.1  2004/03/15 15:06:28  ncq
 # - True/False and dict()
 #
