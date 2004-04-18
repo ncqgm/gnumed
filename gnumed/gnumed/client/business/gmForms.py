71a9f7a878ed8808c5c552f8742ec0a7c9bc9f1e
ihaywood <ihaywood>
2004-04-18 08:39:58 +0000

    new config options

--- gnumed/gnumed/client/business/gmForms.py
+++ gnumed/gnumed/client/business/gmForms.py
@@ -9,8 +9,8 @@ license: GPL
 """
 #============================================================
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/business/gmForms.py,v $
-# $Id: gmForms.py,v 1.12 2004-04-11 10:15:56 ncq Exp $
-__version__ = "$Revision: 1.12 $"
+# $Id: gmForms.py,v 1.13 2004-04-18 08:39:57 ihaywood Exp $
+__version__ = "$Revision: 1.13 $"
 __author__ ="Ian Haywood <ihaywood@gnu.org>"
  
 import sys, os.path, string, time, re, tempfile, cStringIO, types
@@ -21,16 +21,11 @@ if __name__ == "__main__":
 	sys.path.append('../..')
 
 # start logging
-from Gnumed.pycommon import gmLog, gmPG, gmWhoAmI
+from Gnumed.pycommon import gmLog, gmPG, gmWhoAmI, gmCfg, gmExceptions
 _log = gmLog.gmDefLog
 if __name__ == "__main__":
     _log.SetAllLogLevels(gmLog.lData)
 _log.Log(gmLog.lData, __version__)
-
-# FIXME: we make these assumptions about the assignment of form IDs on the backend.
-ID_STANDARD_REFERRAL=101
-ID_STANDARD_REFERRAL_EMAIL=102
-ID_STANDARD_SCRIPT = 201
 
 #============================================================
 class gmFormEngine:
@@ -59,6 +54,15 @@ class gmFormEngine:
         A sop to TeX which can't act as a true filter to delete temporary files
         """
         pass
+
+    def exe (self, command):
+        """
+	Executes the provided command.
+	If command cotains %F. it is substituted with the filename
+	Otherwise, the file is fed in on stdin
+	"""
+	pass
+
 
     def flags (self):
         """
@@ -95,19 +99,33 @@ class TextForm (gmFormEngine):
     
     def process (self, params, escape='@'):
         regex = "%s(\\w+)" % escape
-        result = cStringIO.StringIO ()
+        self.result = cStringIO.StringIO ()
         self.params = params
         for line in self.template.split ('\n'):
             self.index = 0
             self.start_list = 0
             self.stop_list = 0
-            result.write (re.sub (regex, lambda x: self.__subst (x.group (1)), line) + '\n')
+            self.result.write (re.sub (regex, lambda x: self.__subst (x.group (1)), line) + '\n')
             if self.start_list:
                 while not self.stop_list:
                     self.index += 1
-                    result.write (re.sub (regex, lambda x: self.__subst (x.group (1)), line) + '\n')
-        result.seek (0)
-        return result
+                    self.result.write (re.sub (regex, lambda x: self.__subst (x.group (1)), line) + '\n')
+        self.result.seek (0)
+        return self.result
+
+    def exe (self, command):
+        if "%F" in command:
+            tmp = tempfile.mktemp ()
+            f = file (tmp, "w")
+            f.write (self.result.read ())
+            f.close ()
+            command.replace ("%F", tmp)
+            os.system (command)
+            os.unlink (tmp)
+        else:
+            stdin = os.popen (command, "w", 2048)
+            stdin.write (self.result.read ())
+            stdin.close ()
 
 #============================================================
 class LaTeXForm (TextForm):
@@ -126,15 +144,16 @@ class LaTeXForm (TextForm):
             item = item.replace ("\n", "\\\\ ")
             if len (item.strip ()) == 0:
                 item = "\relax " # sometimes TeX really hates empty strings, this seems to mollify it
-		# FIXME: cover all of ISO-Latin-1 which can be expressed in TeX
+            # FIXME: cover all of ISO-Latin-1 which can be expressed in TeX
 	    if type (item) is types.UnicodeType:
 	        item = item.encode ('latin-1', 'replace')
-	    trans = {'ß':'\\ss{}', 'ä': '\\"{a}', 'Ä' :'\\"{A}', "ö ": '\\"{o}', "Ö": '\\"{O}',  "ü": '\\"{u}', "Ü": '\\"{U}',
+	    trans = {'ß':'\\ss{}', 'ä': '\\"{a}', 'Ä' :'\\"{A}', 'ö': '\\"{o}', 'Ö': '\\"{O}',  'ü': '\\"{u}', 'Ü': '\\"{U}',
 		     '\x8a':'\\v{S}', '\x8a':'\\OE{}', '\x9a':'\\v{s}', '\x9c': '\\oe{}', '\a9f':'\\"{Y}', #Microsloth extensions
 		     '\x86': '{\\dag}', '\x87': '{\\ddag}', '\xa7':'{\\S}', '\xb6': '{\\P}', '\xa9': '{\\copyright}', '\xbf': '?`',
 		     '\xc0':'\\`{A}', '\xa1': "\\'{A}", '\xa2': '\\^{A}', '\xa3':'\\~{A}', '\\xc5': '{\AA}',
+		     '\xc7':'\\c{C}', '\xc8':'\\`{E}',  
 		     '\xa1': '!`',
-		     '\xb5':'$\mu$', '\xa3': '\pounds{}'}
+		     '\xb5':'$\mu$', '\xa3': '\pounds{}', '\xa2':'cent'}
 	    for k, i in trans.items ():
                 item = item.replace (k, i)
         if type (item) is types.ListType: 
@@ -166,6 +185,13 @@ class LaTeXForm (TextForm):
         For testing purposes
         """
         os.system ("xdvi texput.dvi")
+	
+    def exe (self, command):
+        if "%F" in command:
+            command.replace ("%F", "texput.ps")
+        else:
+            command  = "%s < texput.ps" % command
+        os.system (command)
 
     def cleanup (self):
         for i in os.listdir ('.'):
@@ -190,71 +216,81 @@ def search_form (discipline = None, electronic=0):
 #------------------------------------------------------------
 def get_form (id):
     """
-    Instantiates a FormEngine based on the form ID from the backend
+    Instantiates a FormEngine based on the form ID or name from the backend
     """
-    result = gmPG.run_ro_query ('reference', 'select template, engine, flags from form_defs where pk = %s', None, id)
+    try:
+	    # it's a number: match to form ID
+	    id = int (id)
+	    cmd  = 'select template, engine, flags from form_defs where pk = %s'
+    except valueError:
+	    # it's a string, match to the form's name
+	    cmd = 'select template, engine, flags from form_defs where name_short = %s'
+    result = gmPG.run_ro_query ('reference', cmd, None, id)
+    if not result:
+	    _log.Log (gmLog.lErr, 'no such form %s' % id)
+	    raise gmExceptions.InvalidInputError ('no such form %s' % id)
     if result[0][1] == 'L':
         return LaTeXForm (result[0][0], result[0][2])
     elif result[1] == 'T':
         return TextForm (result[0][0], result[0][2])
     else:
         _log.Log (gmLog.lErr, 'no such form engine %s' % result[1])
-        return None
+        raise gmExceptions.InvalidInputError ('no such engine %s for form %s' % (result[1], id))
 #------------------------------------------------------------
 def send_referral (recipient, channel, addr, text, flags):
-	whoami = gmWhoAmI.cWhoAmI ().get_staff_identity ()
-	sender = gmDemographicRecord.cDemographicRecord_SQL (whoami)
-	patient = gmPatient.gmCurrentPatient ()
-	patient_demo = patient.get_demographic_record ()
-	params = {}
-#	params['SENDER'] = sender.getFullName ()
-	sname = sender.get_names()
-	params['SENDER'] = '%s %s %s' % (sname['title'], sname['first'], sname['last'])
-#	params['PATIENTNAME'] = patient_demo.getFullName ()
-	pname = patient_demo.get_names()
-	params['PATIENTNAME'] = '%s %s %s' % (pname['title'], pname['first'], pname['last'])
-#	params['RECIPIENT'] = recipient.getFullName ()
-	rname = recipient.get_names()
-	params['RECIPIENT'] = '%s %s %s' % (rname['title'], rname['first'], rname['last'])
-	params['DOB'] = patient_demo.getDOB ().Format ('%x')
-	params['PATIENTADDRESS'] = _("%(number)s %(street)s, %(urb)s %(postcode)s") % patient_demo.getAddresses ('home', 1)
-	params['TEXT'] = text
-	params['INCLUDEMEDS'] = flags['meds']
-	# FUTURE
-	# params['MEDLIST'] = patient_epr.getMedicationsList ()
-	params['INCLUDEPASTHX'] = flags['pasthx']
-	#F FUTURE
-	# params['PASTHXLIST'] = patient_epr.getPastHistory ()
-	
-	if channel == 'post':
-		params['RECIPIENTADDRESS'] = _('%(number)s %(street)s\n%(urb)s %(postcode)s') % addr
-		sndr_addr = sender.getAddresses ('work', 1)
-		if sndr_addr:
-			params['SENDERADDRESS'] = _('%(number)s %(street)s\n%(urb)s %(postcode)s') % sender.getAddresses('work', 1)
-		else:
-			params['SENDERADDRESS'] = _('No address')
-		form = get_form (ID_STANDARD_REFERRAL)
-		form.process (params)
-		form.xdvi ()
-		form.cleanup ()
-	if channel == 'fax':
-		params['RECIPIENTADDRESS'] = _('FAX: %s') % addr
-		sender_addr = sender.getAddresses('work', 1)
-		if sender_addr:
-			sender_addr['fax'] = sender.getCommChannel (gmDemographicRecord.FAX)
-			params['SENDERADDRESS'] = _('%(number)s %(street)s\n%(urb)s %(postcode)s\nFAX: %(fax)s' % sender_addr)
-		else:
-			params['SENDERADDRESS'] = _('No address')
-		form = get_form (ID_STANDARD_REFERRAL)
-		form.process (params)
-		form.xdvi ()
-		form.cleanup ()
-	if channel == 'email':
-		params['RECIPIENTADDRESS'] = addr
-		params['SENDERADDRESS'] = sender.getCommChannel (gmDemographicRecord.EMAIL)
-		form = get_form (ID_STANDARD_REFERRAL_EMAIL)
-		form.process (params)
-		print form.read ()
+    whoami = gmWhoAmI.cWhoAmI ()
+    sender = gmDemographicRecord.cDemographicRecord_SQL (whoami.get_staff_identity ())
+    machine = whoami.get_workplace ()
+    patient = gmPatient.gmCurrentPatient ()
+    patient_demo = patient.get_demographic_record ()
+    params = {}
+    sname = sender.get_names()
+    params['SENDER'] = '%s %s %s' % (sname['title'], sname['first'], sname['last'])
+    pname = patient_demo.get_names()
+    params['PATIENTNAME'] = '%s %s %s' % (pname['title'], pname['first'], pname['last'])
+    rname = recipient.get_names()
+    params['RECIPIENT'] = '%s %s %s' % (rname['title'], rname['first'], rname['last'])
+    params['DOB'] = patient_demo.getDOB ().Format ('%x')
+    params['PATIENTADDRESS'] = _("%(number)s %(street)s, %(urb)s %(postcode)s") % patient_demo.getAddresses ('home', 1)
+    params['TEXT'] = text
+    params['INCLUDEMEDS'] = flags['meds']
+    # FUTURE
+    # params['MEDLIST'] = patient_epr.getMedicationsList ()
+    params['INCLUDEPASTHX'] = flags['pasthx']
+    #F FUTURE
+    # params['PASTHXLIST'] = patient_epr.getPastHistory ()
+    
+    if channel == 'post':
+        params['RECIPIENTADDRESS'] = _('%(number)s %(street)s\n%(urb)s %(postcode)s') % addr
+	sndr_addr = sender.getAddresses ('work', 1)
+	if sndr_addr:
+	    params['SENDERADDRESS'] = _('%(number)s %(street)s\n%(urb)s %(postcode)s') % sender.getAddresses('work', 1)
+	else:
+	    params['SENDERADDRESS'] = _('No address')
+	form_name, set1 = gmCfg.getFirstMatchingDBSet(machine = machine, option = 'main.comms.paper_referral')
+	command, set1 = gmCfg.getFirstMatchingDBSet (machine = machine, option = 'main.comms.print')
+    if channel == 'fax':
+        params['RECIPIENTADDRESS'] = _('FAX: %s') % addr
+	sender_addr = sender.getAddresses('work', 1)
+	if sender_addr:
+	    sender_addr['fax'] = sender.getCommChannel (gmDemographicRecord.FAX)
+	    params['SENDERADDRESS'] = _('%(number)s %(street)s\n%(urb)s %(postcode)s\nFAX: %(fax)s' % sender_addr)
+        else:
+	    params['SENDERADDRESS'] = _('No address')
+	form_name, set1 = gmCfg.getFirstMatchingDBSet(machine = machine, option = 'main.comms.paper_referral')
+	command, set1 = gmCfg.getFirstMatchingDBSet (machine = machine, option = 'main.comms.fax')
+	command.replace ("%N", addr)   # substitute the %N for the number we need to fax to in the command
+    if channel == 'email':
+        params['RECIPIENTADDRESS'] = addr
+	params['SENDERADDRESS'] = sender.getCommChannel (gmDemographicRecord.EMAIL)
+	form_name, set1 = gmCfg.getFirstMatchingDBSet(machine = machine, option = 'main.comms.email_referral')
+	command, set1 = gmCfg.getFirstMatchingDBSet (machine = machine, option = 'main.comms.email')
+        command.replace ("%R", addr) # substitute recipients email address
+        command.replace ("%S", params['SENDERADDRESS']) # substitute senders email address
+    form = get_form (form_name)
+    form.process (params)
+    form.exe (command)
+    form.cleanup ()
 		
 #-------------------------------------------------------------
 def test_au():
@@ -308,7 +344,10 @@ if __name__ == '__main__':
 
 #============================================================
 # $Log: gmForms.py,v $
-# Revision 1.12  2004-04-11 10:15:56  ncq
+# Revision 1.13  2004-04-18 08:39:57  ihaywood
+# new config options
+#
+# Revision 1.12  2004/04/11 10:15:56  ncq
 # - load title in get_names() and use it superceding getFullName
 #
 # Revision 1.11  2004/04/10 01:48:31  ihaywood
