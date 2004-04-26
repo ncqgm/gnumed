3b9b57eb32a913d4600287f12c7806b13fcd5daa
Karsten Hilbert <karsten.hilbert@gmx.net>
2004-04-26 09:37:25 +0000

    - remove old logs at start

--- gnumed/gnumed/server/bootstrap/redo-public.sh
+++ gnumed/gnumed/server/bootstrap/redo-public.sh
@@ -3,22 +3,19 @@
 echo "re-bootstrapping public database"
 echo "dropping old database"
 dropdb -U postgres gnumed
+rm -rf *.log
 echo "============================"
 echo "bootstrappping core database"
-rm -rf redo-public.log
 ./bootstrap-gm_db_system.py --log-file=redo-public-core.log --conf-file=bootstrap-monolithic_core.conf
 echo "========================="
 echo "adding data for locale DE"
-rm -rf redo-public-de.log
 ./bootstrap-gm_db_system.py --log-file=redo-public-de.log --conf-file=bootstrap-de.conf
 echo "========================="
 echo "adding data for locale AU"
 cd ../sql/country.specific/au/
 make-postcode-sql.sh
 cd -
-rm -rf redo-public-au.log
 ./bootstrap-gm_db_system.py --log-file=redo-public-au.log --conf-file=bootstrap-au.conf
 echo "================"
 echo "adding test data"
-rm -rf redo-public-test_data.log
 ./bootstrap-gm_db_system.py --log-file=redo-public-test_data.log --conf-file=bootstrap-test_data.conf
