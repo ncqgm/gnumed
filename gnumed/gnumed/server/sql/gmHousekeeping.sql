083dec4bcb50e9037ef71fe63b172afa6125af12
Karsten Hilbert <karsten.hilbert@gmx.net>
2004-04-26 09:40:11 +0000

    - add context/category to todo table

--- gnumed/gnumed/server/sql/gmHousekeeping.sql
+++ gnumed/gnumed/server/sql/gmHousekeeping.sql
@@ -1,7 +1,7 @@
 -- Project: GnuMed - database housekeeping TODO tables
 -- ===================================================================
 -- $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/server/sql/gmHousekeeping.sql,v $
--- $Revision: 1.3 $
+-- $Revision: 1.4 $
 -- license: GPL
 -- author: Karsten Hilbert
 
@@ -24,8 +24,31 @@ create table housekeeping_todo (
 		default 'admin'
 		check (reported_to in ('user', 'admin')),
 	problem text not null,
-	solution text
+	solution text,
+	context text,
+	category text not null
 );
+
+comment on table housekeeping_todo is
+	'this table stores items that originate in headless processes
+	 running in the background but need to be brought to the
+	 attention of someone, say, errors that an integrity checker
+	 may find';
+comment on column housekeeping_todo.reported_by is
+	'who or what reported this condition, may be a user or software';
+comment on column housekeeping_todo.reported_to is
+	'who is this condition reported to, user or admin,
+	 used for filtering';
+comment on column housekeeping_todo.problem is
+	'a description of the reported condition';
+comment on column housekeeping_todo.solution is
+	'a proposed solution to the problem';
+comment on column housekeeping_todo.context is
+	'specific context for this condition that would
+	 make the problem field unnecessary complex and bulky';
+comment on column housekeeping_todo.category is
+	'a category for the condition, this is used
+	 for filtering, too';
 
 -- =============================================
 GRANT select, insert ON
@@ -42,11 +65,14 @@ TO GROUP "_gm-doctors";
 -- =============================================
 -- do simple schema revision tracking
 delete from gm_schema_revision where filename = '$RCSfile: gmHousekeeping.sql,v $';
-INSERT INTO gm_schema_revision (filename, version) VALUES('$RCSfile: gmHousekeeping.sql,v $', '$Revision: 1.3 $');
+INSERT INTO gm_schema_revision (filename, version) VALUES('$RCSfile: gmHousekeeping.sql,v $', '$Revision: 1.4 $');
 
 -- =============================================
 -- $Log: gmHousekeeping.sql,v $
--- Revision 1.3  2004-04-19 12:47:49  ncq
+-- Revision 1.4  2004-04-26 09:40:11  ncq
+-- - add context/category to todo table
+--
+-- Revision 1.3  2004/04/19 12:47:49  ncq
 -- - translate request_status
 -- - add housekeeping_todo.reported_to
 --
