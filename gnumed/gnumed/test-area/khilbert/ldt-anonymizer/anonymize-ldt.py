7fc384ae16634429af7be9bf242e9f377204cdd1
Karsten Hilbert <karsten.hilbert@gmx.net>
2004-06-02 00:08:16 +0000

    - tailor to Laborata Testwoman

--- gnumed/gnumed/test-area/khilbert/ldt-anonymizer/anonymize-ldt.py
+++ gnumed/gnumed/test-area/khilbert/ldt-anonymizer/anonymize-ldt.py
@@ -8,8 +8,8 @@ copyright: authors
 """
 #===============================================================
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/test-area/khilbert/ldt-anonymizer/anonymize-ldt.py,v $
-# $Id: anonymize-ldt.py,v 1.1 2004-05-13 19:44:41 ncq Exp $
-__version__ = "$Revision: 1.1 $"
+# $Id: anonymize-ldt.py,v 1.2 2004-06-02 00:08:13 ncq Exp $
+__version__ = "$Revision: 1.2 $"
 __author__ = "Karsten Hilbert <Karsten.Hilbert@gmx.net>"
 __license__ = "GPL, details at http://www.gnu.org"
 
@@ -32,8 +32,9 @@ map_sensitive_fields = {
 	'8310': 'anon: %s',
 	'8311': 'anon: lab internal sample ID',
 	'8405': 'anon: arbitrary patient info',
-	'3101': 'anon: last name',
-	'3102': 'anon: first name',
+	'3101': 'Testwoman',
+	'3102': 'Laborata',
+	'3103': '22031931',
 	# Geschlecht: 0 = unbekannt
 	'8407': '0'
 }
@@ -44,20 +45,27 @@ if len(sys.argv) < 2:
 infilename = sys.argv[1]
 outfilename = "%s.anon.txt" % infilename
 print "anonymizing [%s] into [%s]" % (infilename, outfilename)
-
 outfile = open(outfilename, "wb+")
 
 random.jumpahead(int(time.strftime('%S%M%H')))
+gender = 0
 
 for line in fileinput.input(infilename):
-	line_type = line[3:7]
-	line_data = line[7:-2]
+	tmp = line.replace('\r','')
+	tmp = tmp.replace('\n','')
+	line_type = tmp[3:7]
+	line_data = tmp[7:]
 	if line_type in map_sensitive_fields.keys():
-		anon_data = map_sensitive_fields[line_type]
 		if line_type == '8310':
 			anon_data = map_sensitive_fields[line_type] % random.randrange(sys.maxint)
 			# if you don't want random sample IDs uncomment this line:
 			#anon_data = map_sensitive_fields[line_type] % "sample ID"
+		elif line_type == '8407':
+			tmp = divmod(gender, 7)
+			gender += 1
+			anon_data = str(tmp[1])
+		else:
+			anon_data = map_sensitive_fields[line_type]
 		print "anonymisiere Feld [%s]: [%s] -> [%s]" % (line_type, line_data, anon_data)
 		len_int = len(anon_data) + 5
 		new_line = "%03d%s%s\r\n" % (len_int, line_type, anon_data)
@@ -71,6 +79,9 @@ print "done"
 
 #===============================================================
 # $Log: anonymize-ldt.py,v $
-# Revision 1.1  2004-05-13 19:44:41  ncq
+# Revision 1.2  2004-06-02 00:08:13  ncq
+# - tailor to Laborata Testwoman
+#
+# Revision 1.1  2004/05/13 19:44:41  ncq
 # - first version
 #
---
+++ gnumed/gnumed/test-area/khilbert/ldt-anonymizer/make-anon-lab_reqs.py
@@ -0,0 +1,102 @@
+# -*- coding: latin-1 -*-
+"""GnuMed LDT anonymizer.
+
+This script anonymizes German pathology result
+files in LDT format.
+
+copyright: authors
+"""
+#===============================================================
+# $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/test-area/khilbert/ldt-anonymizer/make-anon-lab_reqs.py,v $
+# $Id: make-anon-lab_reqs.py,v 1.1 2004-06-02 00:08:16 ncq Exp $
+__version__ = "$Revision: 1.1 $"
+__author__ = "Karsten Hilbert <Karsten.Hilbert@gmx.net>"
+__license__ = "GPL, details at http://www.gnu.org"
+
+import fileinput, sys, random, time
+
+from Gnumed.pycommon import gmPG, gmLoginInfo, gmLog
+from Gnumed.business import gmPathLab, gmPatient
+
+from Gnumed.pycommon.gmPyCompat import *
+
+_log = gmLog.gmDefLog
+_log.SetAllLogLevels(gmLog.lData)
+
+def usage():
+	print """use like this:"
+$> python make-anon-lab_reqs <ldt-datei>"
+
+This will generate lab requests for all sample IDs found
+in <ldt-datei>. All requests will belong to Laborata
+Testwoman.
+"""
+	sys.exit()	
+
+print "accessing patient data Laborata Testwoman"
+# set encoding
+gmPG.set_default_client_encoding('latin1')
+# setup login defs
+auth_data = gmLoginInfo.LoginInfo(
+	user = 'any-doc',
+	passwd = 'any-doc',
+	host = '',
+	port = 5432,
+	database = 'gnumed'
+)
+backend = gmPG.ConnectionPool(login = auth_data)
+
+pat_data = {
+	'lastnames': 'Testwoman',
+	'firstnames': 'Laborata',
+	'gender': 'f'
+}
+searcher = gmPatient.cPatientSearcher_SQL()
+pat_ids = searcher.get_patient_ids(search_dict = pat_data)
+
+if len(pat_ids) == 0:
+	print "cannot find Laborata Testwoman"
+	sys.exit()
+if len(pat_ids) > 1:
+	print "more than one patient for Laborata Testwoman"
+	sys.exit()
+
+patid = pat_ids[0]
+print "Laborata Testwoman has ID [%s]" % patid
+pat = gmPatient.gmCurrentPatient(aPKey=patid)
+emr = pat.get_clinical_record()
+enc_id = emr.get_active_encounter()['pk_encounter']
+print "encounter", enc_id
+epi_id = emr.get_active_episode()['id_episode']
+print "episode", epi_id
+
+infilename = sys.argv[1]
+print "generating request IDs from LDT file [%s]" % infilename
+
+for line in fileinput.input(infilename):
+	tmp = line.replace('\r','')
+	tmp = tmp.replace('\n','')
+	line_type = tmp[3:7]
+	line_data = tmp[7:]
+	if line_type == '8310':
+		_log.Log(gmLog.lInfo, "creating lab request for ID [%s]" % line_data)
+		status, data = gmPathLab.create_lab_request(
+			lab = 'your own practice',
+			req_id = line_data,
+			pat_id = patid,
+			encounter_id = enc_id,
+			episode_id = epi_id
+		)
+		if status is False:
+			print "ERROR:", data
+		elif status is None:
+			print "ERROR: check lab journal"
+
+print "done"
+backend.StopListeners()
+
+#===============================================================
+# $Log: make-anon-lab_reqs.py,v $
+# Revision 1.1  2004-06-02 00:08:16  ncq
+# - tailor to Laborata Testwoman
+#
