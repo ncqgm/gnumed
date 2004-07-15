e5b8142eca8cc054edb7fd874a25fc9904585d0e
Karsten Hilbert <karsten.hilbert@gmx.net>
2004-07-15 06:28:46 +0000

    - fixed some pathes

--- gnumed/gnumed/client/doc/make-schemadocs.sh
+++ gnumed/gnumed/client/doc/make-schemadocs.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/doc/make-schemadocs.sh,v $
-# $Revision: 1.1 $
+# $Revision: 1.2 $
 # license: GPL
 # author: Karsten.Hilbert@gmx.net
 
 # this only really works on hherb.com
-/usr/local/bin/postgresql_autodoc -d gnumed -f gnumed-schema -t html
+/usr/local/bin/postgresql_autodoc -d gnumed -f ~/gm-schemadocs/gnumed-schema -t html &> ~/schemadocs.log
 
 #============================================
 # $Log: make-schemadocs.sh,v $
-# Revision 1.1  2004-07-15 06:25:32  ncq
-# - first checkin
+# Revision 1.2  2004-07-15 06:28:46  ncq
+# - fixed some pathes
 #
-# Revision 1.1  2004/07/14 10:11:04  ncq
+# Revision 1.1  2004/07/15 06:25:32  ncq
 # - first checkin
-#
