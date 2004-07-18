a330e507e526caa735079f0937c64b81ddc9b0a2
Karsten Hilbert <karsten.hilbert@gmx.net>
2004-07-18 19:45:49 +0000

    - help MC do something about the diffs on display

--- gnumed/diff_tree.sh
+++ gnumed/diff_tree.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/diff_tree.sh,v $
-# $Revision: 1.2 $
+# $Revision: 1.3 $
 
 export CVS_RSH="ssh"
 echo "cleaning out debris"
 find ./ -name '*.pyc' -exec rm -v '{}' ';'
 echo "diffing local copy against CVS master tree"
-cvs -z9 diff | tee diff.log
+cvs -z9 diff | tee public-tree.diff
--- gnumed/update_tree.sh
+++ gnumed/update_tree.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/update_tree.sh,v $
-# $Revision: 1.6 $
+# $Revision: 1.7 $
 
 export CVS_RSH="ssh"
 echo "cleaning out debris"
 find ./ -name '*.pyc' -exec rm -v '{}' ';'
 echo "diffing against CVS just to be sure"
-cvs -z9 diff | tee diff-before-update.log
+cvs -z9 diff | tee before-update.diff
 echo "updating python client source from CVS"
 cvs -z9 update -dP | tee update.log
