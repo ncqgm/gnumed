6b78efdb0def1dc4cbc0d6394b229059536ba919
Karsten Hilbert <karsten.hilbert@gmx.net>
2004-07-24 10:27:50 +0000

    - cleanup

--- gnumed/gnumed/client/wxpython/gui/gmOffice.py
+++ gnumed/gnumed/client/wxpython/gui/gmOffice.py
@@ -8,11 +8,9 @@ Status: blank screen
 
 # make this into GNUMed plugin
 
+from Gnumed.pycommon import gmI18N
 from Gnumed.wxpython import gmPlugin
-from wxPython.wx import *
-
-if __name__ == '__main__':
-	_ = lambda x:x
+from wxPython import wx
 
 class gmOffice (gmPlugin.wxNotebookPlugin):
 
@@ -25,4 +23,4 @@ class gmOffice (gmPlugin.wxNotebookPlugin):
         return ("view", _("&Office"))
 
     def GetWidget (self, parent):
-        return wxPanel(parent, -1)
+        return wx.wxPanel(parent, -1)
