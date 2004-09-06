589697ce949dc113744b492f0efc89c94ad425df
Karsten Hilbert <karsten.hilbert@gmx.net>
2004-09-06 22:15:45 +0000

    - add v_cfg_opts_numeric/string/str_array + grants

--- gnumed/gnumed/server/sql/gmConfigViews.sql
+++ gnumed/gnumed/server/sql/gmConfigViews.sql
@@ -4,7 +4,7 @@
 -- author: Karsten Hilbert <Karsten.Hilbert@gmx.net>
 -- license: GPL
 -- $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/server/sql/gmConfigViews.sql,v $
--- $Revision: 1.1 $
+-- $Revision: 1.2 $
 -- ======================================================
 -- force terminate + exit(3) on errors if non-interactive
 \set ON_ERROR_STOP 1
@@ -18,15 +18,6 @@ create view v_cfg_options as
 select
 	cfg_t.name as option,
 	cfg_t.description as description,
---	case
---		when cfg_t.type = 'numeric'
---			then (select cfg_n.value::text from cfg_numeric cfg_n where cfg_n.id_item=cfg_i.id)
---		when cfg_t.type = 'string'
---			then (select cfg_s.value::text from cfg_string cfg_s where cfg_s.id_item=cfg_i.id)
---		when cfg_t.type = 'str_array'
---			then (select cfg_sa.value::text from cfg_str_array cfg_sa where cfg_sa.id_item=cfg_i.id)
---		else 'missing'::text
---	end as value,
 	cfg_i.owner as owner,
 	cfg_i.workplace as workplace,
 	cfg_i.cookie as cookie,
@@ -42,6 +33,84 @@ where
 ;
 
 -- ======================================================
+\unset ON_ERROR_STOP
+drop view v_cfg_opts_numeric;
+\set ON_ERROR_STOP 1
+
+create view v_cfg_opts_numeric as
+select
+	cfg_t.name as option,
+	cfg_v.value as value,
+	cfg_t.description as description,
+	cfg_i.owner as owner,
+	cfg_i.workplace as workplace,
+	cfg_i.cookie as cookie,
+	cfg_t.cfg_group as group,
+	cfg_t.id as pk_cfg_template,
+	cfg_i.id as pk_cfg_item
+from
+	cfg_template cfg_t,
+	cfg_item cfg_i,
+	cfg_numeric cfg_v
+where
+	cfg_i.id_template = cfg_t.id
+		and
+	cfg_v.id_item = cfg_i.id
+;
+
+-- ======================================================
+\unset ON_ERROR_STOP
+drop view v_cfg_opts_string;
+\set ON_ERROR_STOP 1
+
+create view v_cfg_opts_string as
+select
+	cfg_t.name as option,
+	cfg_v.value as value,
+	cfg_t.description as description,
+	cfg_i.owner as owner,
+	cfg_i.workplace as workplace,
+	cfg_i.cookie as cookie,
+	cfg_t.cfg_group as group,
+	cfg_t.id as pk_cfg_template,
+	cfg_i.id as pk_cfg_item
+from
+	cfg_template cfg_t,
+	cfg_item cfg_i,
+	cfg_string cfg_v
+where
+	cfg_i.id_template = cfg_t.id
+		and
+	cfg_v.id_item = cfg_i.id
+;
+
+-- ======================================================
+\unset ON_ERROR_STOP
+drop view v_cfg_opts_str_array;
+\set ON_ERROR_STOP 1
+
+create view v_cfg_opts_str_array as
+select
+	cfg_t.name as option,
+	cfg_v.value as value,
+	cfg_t.description as description,
+	cfg_i.owner as owner,
+	cfg_i.workplace as workplace,
+	cfg_i.cookie as cookie,
+	cfg_t.cfg_group as group,
+	cfg_t.id as pk_cfg_template,
+	cfg_i.id as pk_cfg_item
+from
+	cfg_template cfg_t,
+	cfg_item cfg_i,
+	cfg_str_array cfg_v
+where
+	cfg_i.id_template = cfg_t.id
+		and
+	cfg_v.id_item = cfg_i.id
+;
+
+-- ======================================================
 GRANT SELECT ON
 	db,
 	distributed_db,
@@ -53,6 +122,9 @@ GRANT SELECT ON
 	cfg_str_array,
 	cfg_numeric
 	, v_cfg_options
+	, v_cfg_opts_numeric
+	, v_cfg_opts_string
+	, v_cfg_opts_str_array
 TO GROUP "gm-public";
 
 GRANT select, insert, update, delete on
@@ -69,11 +141,14 @@ to group "gm-doctors";
 -- =============================================
 -- do simple schema revision tracking
 delete from gm_schema_revision where filename='$RCSfile: gmConfigViews.sql,v $';
-INSERT INTO gm_schema_revision (filename, version) VALUES('$RCSfile: gmConfigViews.sql,v $', '$Revision: 1.1 $');
+INSERT INTO gm_schema_revision (filename, version) VALUES('$RCSfile: gmConfigViews.sql,v $', '$Revision: 1.2 $');
 
 --=====================================================================
 -- $Log: gmConfigViews.sql,v $
--- Revision 1.1  2004-09-02 00:42:33  ncq
+-- Revision 1.2  2004-09-06 22:15:45  ncq
+-- - add v_cfg_opts_numeric/string/str_array + grants
+--
+-- Revision 1.1  2004/09/02 00:42:33  ncq
 -- - add v_cfg_options
 -- - move grants to volatile DDL file
 --
