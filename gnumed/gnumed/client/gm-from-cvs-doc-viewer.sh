77594d2a55cedcf2410a9a26c02c9a6245b01cff
Karsten Hilbert <karsten.hilbert@gmx.net>
2004-09-18 13:50:46 +0000

    - remove log before startup

--- gnumed/gnumed/client/gm-from-cvs-doc-viewer.sh
+++ gnumed/gnumed/client/gm-from-cvs-doc-viewer.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/Attic/gm-from-cvs-doc-viewer.sh,v $
-# $Revision: 1.1 $
+# $Revision: 1.2 $
 
 # maybe force some locale setting here
 #export LANG=fr
@@ -11,4 +11,5 @@ cd ../
 ln -vfsn client Gnumed
 cd -
 export PYTHONPATH="${PYTHONPATH}:../"
+rm -vf ./gm-slave-doc-viewer.log
 python wxpython/gnumed.py --log-file=./gm-slave-doc-viewer.log --conf-file=/home/ncq/.gnumed/gm-doc-viewer.conf --debug
