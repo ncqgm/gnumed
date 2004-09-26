c08f33fdbb62b98f05af2e51aa5d86111f6e3a34
sjtan <sjtan>
2004-09-26 07:14:27 +0000

    allergy entry. NullRootItem for non-conditional jsp access to object returned from getEarliestRootItem

---
+++ gnumed/gnumed/test-area/sjtan/test-struts-3/WEB-INF/src/java/org/gnumed/testweb1/data/AllergyEntryImpl1.java
@@ -0,0 +1,37 @@
+/*
+ * AllergyInputImpl1.java
+ *
+ * Created on September 26, 2004, 12:10 PM
+ */
+
+package org.gnumed.testweb1.data;
+
+/**
+ *
+ * @author  sjtan
+ */
+public class AllergyEntryImpl1 implements AllergyEntry {
+    private boolean definite;
+    private String substance;
+    
+    /** Creates a new instance of AllergyInputImpl1 */
+    public AllergyEntryImpl1() {
+    }
+    
+    public String getSubstance() {
+        return substance;
+    }
+    
+    public boolean isDefinite() {
+        return definite;
+    }
+    
+    public void setDefinite(boolean definite) {
+        this.definite = definite;
+    }
+    
+    public void setSubstance(String substance) {
+        this.substance = substance;
+    }
+    
+}
