48c7bd52f5d30eb944590cfbb8044329641662d7
Karsten Hilbert <karsten.hilbert@gmx.net>
2004-10-01 11:48:53 +0000

    - id -> pk

--- gnumed/gnumed/client/importers/gmLDTimporter.py
+++ gnumed/gnumed/client/importers/gmLDTimporter.py
@@ -25,8 +25,8 @@ FIXME: check status on save_payload()s
 """
 #===============================================================
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/importers/gmLDTimporter.py,v $
-# $Id: gmLDTimporter.py,v 1.17 2004-09-25 13:09:58 ncq Exp $
-__version__ = "$Revision: 1.17 $"
+# $Id: gmLDTimporter.py,v 1.18 2004-10-01 11:48:53 ncq Exp $
+__version__ = "$Revision: 1.18 $"
 __author__ = "Karsten Hilbert <Karsten.Hilbert@gmx.net>"
 __license__ = "GPL, details at http://www.gnu.org"
 
@@ -776,7 +776,7 @@ class cLDTImporter:
 			_log.Log(gmLog.lErr, 'cannot create/retrieve test type')
 			return False
 		if ttype['comment'] in [None, '']:
-			ttype['comment'] = 'created [%s] by [$RCSfile: gmLDTimporter.py,v $ $Revision: 1.17 $] from [%s]' % (time.strftime('%Y-%m-%d %H:%M'), self.ldt_filename)
+			ttype['comment'] = 'created [%s] by [$RCSfile: gmLDTimporter.py,v $ $Revision: 1.18 $] from [%s]' % (time.strftime('%Y-%m-%d %H:%M'), self.ldt_filename)
 			ttype.save_payload()
 		# try to create test result row
 		whenfield = 'lab_rxd_when'		# FIXME: make this configurable
@@ -784,7 +784,7 @@ class cLDTImporter:
 			patient_id = self.__request.get_patient()[0],
 			when_field = whenfield,
 			when = self.__request[whenfield],
-			test_type = ttype['id'],
+			test_type = ttype['pk'],
 			val_num = vnum,
 			val_alpha = valpha,
 			unit = vunit,
@@ -931,7 +931,7 @@ def run_import():
 #---------------------------------------------------------------
 def add_todo(problem, solution, context):
 	cat = 'lab'
-	by = '$RCSfile: gmLDTimporter.py,v $ $Revision: 1.17 $'
+	by = '$RCSfile: gmLDTimporter.py,v $ $Revision: 1.18 $'
 	rcvr = 'user'
 	gmPG.add_housekeeping_todo(reporter=by, receiver=rcvr, problem=problem, solution=solution, context=context, category=cat)
 #===============================================================
@@ -965,7 +965,10 @@ if __name__ == '__main__':
 
 #===============================================================
 # $Log: gmLDTimporter.py,v $
-# Revision 1.17  2004-09-25 13:09:58  ncq
+# Revision 1.18  2004-10-01 11:48:53  ncq
+# - id -> pk
+#
+# Revision 1.17  2004/09/25 13:09:58  ncq
 # - fix logged string
 #
 # Revision 1.16  2004/07/05 22:28:40  ncq
