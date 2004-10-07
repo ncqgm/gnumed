8b7175faa35a89e51726c09823fb8718f819664e
sjtan <sjtan>
2004-10-07 00:26:56 +0000

    add licensing copying when libs included

--- gnumed/gnumed/test-area/sjtan/test-struts-3/copy_libs.sh
+++ gnumed/gnumed/test-area/sjtan/test-struts-3/copy_libs.sh
@@ -1,12 +1,16 @@
 #change the 2 environment variables base_struts, base_tomcat to point to the local locations.
 base_struts=$HOME/downloads/jakarta-struts-1.2.4
 base_tomcat=$HOME/jakarta-tomcat-5.0.19
-commons_lang=$HOME/downloads/jakarta-commons/lang/commons-lang-2.0/commons-lang-2.0.jar
+#commons_lang=$HOME/downloads/jakarta-commons/lang/commons-lang-2.0/commons-lang-2.0.jar
 
 cp -u $base_struts/lib/* WEB-INF/lib
+cp -u $base_struts/LICENSE LICENSE-STRUTS
+cp -u $base_struts/NOTICE  NOTICE-STRUTS
+
 cp -u $base_tomcat/common/lib/servlet-api.jar WEB-INF/lib
 cp -u $base_struts/contrib/struts-el/lib/* WEB-INF/lib
-cp -u $commons_lang WEB-INF/lib
+cp -u $base_struts/contrib/struts-el/README.txt README-STRUTS-EL.txt
+
 
 
 
