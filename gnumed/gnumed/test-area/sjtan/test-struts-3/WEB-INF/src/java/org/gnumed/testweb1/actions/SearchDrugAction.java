283395cb181513251fbea02f6b620e3f008ca94d
sjtan <sjtan>
2004-10-10 04:31:25 +0000

    action for drug reference lookup.

---
+++ gnumed/gnumed/test-area/sjtan/test-struts-3/WEB-INF/src/java/org/gnumed/testweb1/actions/SearchDrugAction.java
@@ -0,0 +1,58 @@
+/*
+ * Created on 13-Oct-2004
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package org.gnumed.testweb1.actions;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.ActionMessages;
+import org.gnumed.testweb1.data.DataObjectFactory;
+import org.gnumed.testweb1.data.DrugRef;
+import org.gnumed.testweb1.forms.ClinicalUpdateForm;
+import org.gnumed.testweb1.global.Constants;
+import org.gnumed.testweb1.persist.scripted.gnumed1.DrugRefAccess;
+
+/**
+ * @author sjtan
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class SearchDrugAction extends Action {
+
+	Log log = LogFactory.getLog(this.getClass());
+
+	public ActionForward execute(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response) {
+
+		ActionMessages messages = new ActionMessages();
+		DataObjectFactory factory;
+		if (form instanceof ClinicalUpdateForm) {
+    		request.getSession().setAttribute( Constants.Session.CURRENT_CLINICAL_FORM, form);
+    		request.getSession().
+						setAttribute(Constants.Session.TARGET_MEDICATION_ENTRY_INDEX, 
+							request.getParameter(Constants.Request.MEDICATION_ENTRY_INDEX));
+    	}
+		
+		DrugRefAccess access  = 
+				(DrugRefAccess) servlet.getServletContext().
+						getAttribute(Constants.Servlet.DRUGREF_ACCESS);
+		try {
+					request.setAttribute(Constants.Request.DRUG_REF_CANDIDATES, access.findDrugRef((String)request.getAttribute(Constants.Request.DRUG_NAME_PREFIX)));
+					
+		} catch (Exception e) {
+			
+		}
+		return mapping.findForward("showCandidateDrugs");
+	}
+}
