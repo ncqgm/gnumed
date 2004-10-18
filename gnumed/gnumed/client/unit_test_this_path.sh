bb5bb66d8ad3fc1b63cb6a3f35b1818e5b864b1d
Karsten Hilbert <karsten.hilbert@gmx.net>
2004-10-18 09:44:58 +0000

    - what were you thinking gleefully overwriting gm-from-cvs.log ??

--- gnumed/gnumed/client/unit_test_this_path.sh
+++ gnumed/gnumed/client/unit_test_this_path.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
-#this runs the __main__ functions of each widget.e.g. run 
-#	
+# this runs the __main__ functions of each widget.e.g. run 
+#
 #	sh unit_test_this_path.sh wxpython/gmDemographics.py
-
-# start kvkd
-# FIXME: needs logic to prevent more than one kvkd from running
 
 # maybe force some locale setting here
 #export LANG=fr
@@ -14,6 +11,6 @@ cd ../
 ln -vfsn client Gnumed
 cd -
 export PYTHONPATH="${PYTHONPATH}:../"
-rm -vf gm-from-cvs.log
-python $1 --log-file=gm-from-cvs.log --debug
-#--profile=gm-from-cvs.prof
+rm -vf $0.log
+python $1 --$0.log --debug
+#--profile=$0.prof
