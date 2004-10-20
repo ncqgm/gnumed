02857353dcdfbe70f5735bcaa4463220198ffcc6
Karsten Hilbert <karsten.hilbert@gmx.net>
2004-10-20 06:54:46 +0000

    - run on Windows

---
+++ gnumed/gnumed/client/gm-from-cvs-win.bat
@@ -0,0 +1,9 @@
+# $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/Attic/gm-from-cvs-win.bat,v $
+# $Revision: 1.1 $
+
+echo must setup python files by doing bash cygwin_path.sh once.
+echo this .bat file is run from client\ directory
+set PYTHONPATH=..\;%PYTHONPATH%
+set PATH=%PATH%;c:\\Python2.3
+Python wxpython/gnumed.py --log-file=gm-from-cvs-win.log --debug
+
