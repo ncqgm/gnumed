1915fbe7a568c258be30264aa82cf820fcddac6a
sjtan <sjtan>
2004-11-20 11:03:20 +0000

    returns unsorted narratives.

---
+++ gnumed/gnumed/test-area/sjtan/test-struts-3/WEB-INF/src/java/org/gnumed/testweb1/data/EntryClinicalEncounterImpl1.java
@@ -0,0 +1,23 @@
+/*
+ * Created on Nov 20, 2004
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package org.gnumed.testweb1.data;
+
+import java.util.List;
+
+/**
+ * @author sjtan
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class EntryClinicalEncounterImpl1 extends ClinicalEncounterImpl1 {
+
+    public List getNarratives() {
+
+        return narratives;
+    }
+}
