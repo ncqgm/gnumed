8641c91d8a363f52d5c48538147ff4c49bcd88cf
Karsten Hilbert <karsten.hilbert@gmx.net>
2004-12-18 13:11:48 +0000

    - bootstrap-gm* -> redo_gm*

--- gnumed/gnumed/server/bootstrap/redo-de.sh
+++ gnumed/gnumed/server/bootstrap/redo-de.sh
@@ -11,8 +11,8 @@ dropdb -U postgres gnumed
 echo "*******************************"
 echo "bootstrapping new core database"
 rm -f redo-de-core.log
-./bootstrap-gm_db_system.py --log-file=redo-de-core.log --conf-file=bootstrap-monolithic_core.conf
+./bootstrap_gm_db_system.py --log-file=redo-de-core.log --conf-file=bootstrap-monolithic_core.conf
 echo "****************************************"
 echo "now adding database features for Germany"
 rm -f redo-de-specific.log
-./bootstrap-gm_db_system.py --log-file=redo-de-specific.log --conf-file=bootstrap-de.conf
+./bootstrap_gm_db_system.py --log-file=redo-de-specific.log --conf-file=bootstrap-de.conf
--- gnumed/gnumed/server/bootstrap/redo-public.sh
+++ gnumed/gnumed/server/bootstrap/redo-public.sh
@@ -11,16 +11,16 @@ dropdb -U postgres gnumed
 rm -rf *.log
 echo "============================"
 echo "bootstrappping core database"
-./bootstrap-gm_db_system.py --log-file=redo-public-core.log --conf-file=bootstrap-monolithic_core.conf
+./bootstrap_gm_db_system.py --log-file=redo-public-core.log --conf-file=bootstrap-monolithic_core.conf
 echo "========================="
 echo "adding data for locale DE"
-./bootstrap-gm_db_system.py --log-file=redo-public-de.log --conf-file=bootstrap-de.conf
+./bootstrap_gm_db_system.py --log-file=redo-public-de.log --conf-file=bootstrap-de.conf
 echo "========================="
 echo "adding data for locale AU"
 cd ../sql/country.specific/au/
 make-postcode-sql.sh
 cd -
-./bootstrap-gm_db_system.py --log-file=redo-public-au.log --conf-file=bootstrap-au.conf
+./bootstrap_gm_db_system.py --log-file=redo-public-au.log --conf-file=bootstrap-au.conf
 echo "================"
 echo "adding test data"
-./bootstrap-gm_db_system.py --log-file=redo-public-test_data.log --conf-file=bootstrap-test_data.conf
+./bootstrap_gm_db_system.py --log-file=redo-public-test_data.log --conf-file=bootstrap-test_data.conf
