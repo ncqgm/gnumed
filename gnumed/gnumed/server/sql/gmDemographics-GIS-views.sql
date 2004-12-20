d3729263f7f18c19876001d8c976a10d3b704c7b
Karsten Hilbert <karsten.hilbert@gmx.net>
2004-12-20 18:52:02 +0000

    - Ian reworked v_basic_address

--- gnumed/gnumed/server/sql/gmDemographics-GIS-views.sql
+++ gnumed/gnumed/server/sql/gmDemographics-GIS-views.sql
@@ -7,15 +7,14 @@
 -- droppable components of gmGIS schema
 
 -- $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/server/sql/gmDemographics-GIS-views.sql,v $
--- $Revision: 1.13 $
+-- $Revision: 1.14 $
 -- ###################################################################
 -- force terminate + exit(3) on errors if non-interactive
 \set ON_ERROR_STOP 1
 
 -- ===================================================================
 -- if you suffer from performance problems when selecting from this view,
--- implement it as a real table and create rules / triggers for insert,
--- update and delete that will update the underlying tables accordingly
+-- implement it as a real table 
 create view v_basic_address as
 select
 	adr.id as id,
@@ -25,7 +24,7 @@ select
 	urb.name as city,
 	adr.number as number,
 	str.name as street,
-	adr.addendum as street2
+	adr.addendum as addendum
 from
 	address adr,
 	state s,
@@ -38,186 +37,67 @@ where
 		and
 	urb.id_state = s.id;
 
--- added IH 8/3/02
--- insert, delete, and update rules on this table
--- problem: the street table had better contain the street.
--- solution: function to auto-create street records on demand.
-
-create view v_home_address as
-select
-	lp2a.id_identity as id,
-	s.country as country,
-	s.code as state,
-	coalesce (str.postcode, urb.postcode) as postcode,
-	urb.name as city,
-	adr.number as number,
-	str.name as street,
-	adr.addendum as street2
-from
-	address adr,
-	state s,
-	urb,
-	street str,
-	lnk_person2address lp2a
-where
-	adr.id_street = str.id
-		and
-	str.id_urb = urb.id
-		and
-	urb.id_state = s.id
-		and
-	lp2a.id_address = adr.id
-		and
-	lp2a.id_type = 1; -- home address
-
-
 -- ===================================================================
--- finds the state for a given postcode in a given country
+-- this function guarantees an ID for a given address, it will create
+-- streets and addresses if required. It insists that the urb exists.
+-- it will set street.postcode if urb.postcode is NULL.
 \unset ON_ERROR_STOP
-DROP function find_state(text, text);
-DROP function find_street(text, integer);
+DROP function create_address (text, text, text, text, text);
 \set ON_ERROR_STOP 1
-create function find_state (text, text) RETURNS text AS '
+create function create_address (text, text, text, text, text) RETURNS text AS '
 DECLARE
-	pcode ALIAS FOR $1;	-- post code
-	ccode ALIAS FOR $2;	-- country code
-	s RECORD;
-	retval text := NULL;
+	p_number alias for $1;
+	p_addendum alias for $2;
+	p_street alias for $3;
+	city alias for $4;
+	pcode alias for $5;
+	r_addr address%ROWTYPE;
+	r_urb urb%ROWTYPE;
+	r_street street%ROWTYPE;
+	street_id integer;
+	
 BEGIN
-	SELECT INTO s * FROM state WHERE
-		id = (SELECT id_state from urb where postcode like pcode||''%'' limit 1)
-			AND
-		country=ccode;
-	IF FOUND THEN
-		retval := s.code;
+	SELECT INTO r_urb * FROM urb WHERE name ilike city;
+	IF NOT FOUND THEN
+		SELECT INTO r_urb * FROM urb WHERE postcode ilike pcode;
+		IF NOT FOUND THEN
+			RAISE EXCEPTION ''No such urb %'', city;
+		END IF;
 	END IF;
-	RETURN retval;
-END;' LANGUAGE 'plpgsql';
-
--- ===================================================================
--- This function returns the id of street, BUT if the street does not
--- exist, it is created.
-create function find_street (text, integer) RETURNS integer AS '
-DECLARE
-	s_name ALIAS FOR $1;
-	s_id_urb ALIAS FOR $2;
-	s RECORD;
-BEGIN
-	SELECT INTO s * FROM street WHERE
-		name = s_name
-			AND
-		id_urb = s_id_urb;
-	IF FOUND THEN
-		RETURN s.id;
+	IF r_urb.postcode IS NULL THEN		
+		SELECT INTO r_street * FROM street WHERE
+			name = street
+				AND
+			id_urb = r_urb.id
+				AND
+			postcode = pcode;
 	ELSE
-		INSERT INTO street (id_urb, name) VALUES (s_id_urb, s_name);
-		RETURN currval (''street_id_seq'');
+		SELECT INTO r_street * FROM street WHERE
+			name = p_street
+				AND
+			id_urb = r_urb.id;
 	END IF;
-END;' LANGUAGE 'plpgsql';
-
-
--- This function returns the id of a state, BUT if the state does not
--- exist, it is created.
-\unset ON_ERROR_STOP
-DROP function find_or_create_state(text, text);
-\set ON_ERROR_STOP 1
-create function find_or_create_state(text, text) RETURNS integer AS '
-DECLARE
-        s_code ALIAS FOR $1;
-        s_country ALIAS FOR $2;
-        s RECORD;
-BEGIN
-        SELECT INTO s * FROM state
-	WHERE code = s_code
-	AND country = s_country;
-        IF FOUND THEN
-           RETURN s.id;
-        ELSE
-           INSERT INTO state (code, country) VALUES (s_code, s_country);
-           RETURN currval (''state_id_seq'');
-        END IF;
-END;' LANGUAGE 'plpgsql';
-
-
-
--- This function returns the id of urb, BUT if the urb does not
--- exist, it is created.
-\unset ON_ERROR_STOP
-DROP function find_urb(text, text, text, text);
-\set ON_ERROR_STOP 1
-create function find_urb (text, text, text, text) RETURNS integer AS '
-DECLARE
-	u_country ALIAS FOR $1;
-	u_state ALIAS FOR $2;
-	u_postcode ALIAS FOR $3;
-	u_name ALIAS FOR $4;
-	u RECORD;
-	state_code INTEGER;
-BEGIN
-	state_code = find_or_create_state(u_state, u_country);
-	SELECT INTO u *
-	FROM urb
-	WHERE
-		id_state = state_code
-			AND
-		postcode = u_postcode
-			AND
-		name = u_name;
-
 	IF FOUND THEN
-		RETURN u.id;
+		street_id := r_street.id;
 	ELSE
-		INSERT INTO urb (id_state, postcode, name)
-		VALUES (state_code, u_postcode, u_name);
-		RETURN currval (''urb_id_seq'');
+		IF r_urb.postcode IS NULL THEN
+			INSERT INTO street (id_urb, name, postcode) VALUES (r_urb.id, p_street, pcode);
+		ELSE 
+			INSERT INTO street (id_urb, name) VALUES (r_urb.id, p_street);
+		END IF;
+		street_id := currval (''street_id_seq'');
+		INSERT INTO address (number, addendum, id_street) VALUES (p_number, p_addendum, street_id);
+		RETURN currval (''address_id_seq'');
 	END IF;
-END;' language 'plpgsql';
-
--- ====================================================
-\unset ON_ERROR_STOP
-drop rule insert_address on v_basic_address;
-drop rule delete_address on v_basic_address;
-drop rule update_address on v_basic_address;
-\set ON_ERROR_STOP 1
-
-CREATE RULE insert_address AS ON INSERT TO v_basic_address DO INSTEAD
-	INSERT INTO address (id_street, number, addendum)
-    VALUES (
-		find_street(
-			NEW.street,
-			find_urb(NEW.country, NEW.state, NEW.postcode, NEW.city)
-		),
-		NEW.number,
-		NEW.street2
-	);
-
-CREATE RULE delete_address AS ON DELETE TO v_basic_address DO INSTEAD
-	DELETE FROM address
-	WHERE address.id = OLD.id
-;
-
--- updates the basic address data as identified by it's unique id
-CREATE RULE update_address AS ON UPDATE TO v_basic_address DO INSTEAD
-	UPDATE
-		address
-	SET
-		number = NEW.number,
-		addendum = NEW.street2,
-		id_street = find_street (
-			NEW.street,
-			(SELECT urb.id
-			 FROM urb, state
-			 WHERE
-				urb.name = NEW.city AND
-				urb.postcode = NEW.postcode  AND
-				urb.id_state = state.id AND
-				state.code = NEW.state AND
-				state.country = NEW.country
-			)
-		)
-	WHERE
-		address.id=OLD.id
-;
+	SELECT INTO r_addr * FROM address WHERE
+		number = p_number AND addendum = p_addendum AND id_street = street_id;
+	IF FOUND THEN
+		RETURN r_addr.id;
+	ELSE
+		INSERT INTO address (number, addendum, id_street) VALUES (p_number, p_addendum, street_id);
+		RETURN currval (''address_id_seq'');
+	END IF; 
+END;' LANGUAGE 'plpgsql';
 
 -- ===================================================================
 \unset ON_ERROR_STOP
@@ -355,9 +235,9 @@ create view v_zip2data as
 comment on view v_zip2data is
 	'aggregates nearly all known data per zip code';
 
+-- ===================================================================
 GRANT select ON
 	v_basic_address,
-	v_home_address,
 	v_zip2street,
 	v_zip2urb,
 	v_zip2data
@@ -371,11 +251,14 @@ TO GROUP "gm-doctors";
 -- ===================================================================
 -- do simple schema revision tracking
 delete from gm_schema_revision where filename='$RCSfile: gmDemographics-GIS-views.sql,v $';
-INSERT INTO gm_schema_revision (filename, version) VALUES('$RCSfile: gmDemographics-GIS-views.sql,v $', '$Revision: 1.13 $');
+INSERT INTO gm_schema_revision (filename, version) VALUES('$RCSfile: gmDemographics-GIS-views.sql,v $', '$Revision: 1.14 $');
 
 -- ===================================================================
 -- $Log: gmDemographics-GIS-views.sql,v $
--- Revision 1.13  2004-12-15 09:24:49  ncq
+-- Revision 1.14  2004-12-20 18:52:02  ncq
+-- - Ian reworked v_basic_address
+--
+-- Revision 1.13  2004/12/15 09:24:49  ncq
 -- - addr_id -> id, followup v_basic_address changes
 --
 -- Revision 1.12  2004/12/15 04:18:03  ihaywood
