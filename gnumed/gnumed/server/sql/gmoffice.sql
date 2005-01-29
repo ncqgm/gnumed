7ce8a40369b7e788b13b2e6f6e1a4a573eab7b0c
Karsten Hilbert <karsten.hilbert@gmx.net>
2005-01-29 18:45:22 +0000

    - form_target_classes -> form_job_targets - form_queue -> form_job_queue

--- gnumed/gnumed/server/sql/gmoffice.sql
+++ gnumed/gnumed/server/sql/gmoffice.sql
@@ -5,7 +5,7 @@
 -- For details regarding GPL licensing see http://gnu.org
 
 -- $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/server/sql/gmoffice.sql,v $
--- $Revision: 1.8 $ $Date: 2005-01-12 12:29:29 $ $Author: ncq $
+-- $Revision: 1.9 $ $Date: 2005-01-29 18:45:22 $ $Author: ncq $
 -- ===================================================================
 -- force terminate + exit(3) on errors if non-interactive
 \set ON_ERROR_STOP 1
@@ -13,54 +13,67 @@
 -- ===================================================================
 -- forms handling
 -- ===================================================================
--- target classes
-create table form_target_classes (
-	pk serial primary key,
-	name text unique not null
+-- queue types
+create table form_job_targets (
+	pk serial
+		primary key,
+	target text
+		unique
+		not null
 );
 
+comment on table form_job_targets is
+	'the form job targets (eg. printers, faxes, smtp servers) in
+	 whatever granularity is needed locally, can be used for load
+	 balancing/round robin servicing busy queues';
+
 -- -------------------------------------------------------------------
--- queue of pending forms
-create table form_queue (
+-- queue of forms pending for processing
+create table form_job_queue (
 	pk serial primary key,
 	fk_form_instance integer
 		not null
 		references form_instances(pk),
-	form bytea not null,
-	fk_target_class integer
+	form bytea
+		not null,
+	fk_job_target integer
 		not null
-		references form_target_classes(pk),
+		references form_job_targets(pk),
 	submitted_when timestamp with time zone
 		not null
 		default CURRENT_TIMESTAMP,
-	fk_submitted_by integer not null,
-	submitted_from text not null,
-	status text not null default 'submitted'
+	fk_submitted_by integer
+		not null,
+	submitted_from text
+		not null,
+	status text
+		not null
+		default 'submitted'
 );
 
--- FIXME: we need a trigger that allows UPDATEs only on "status"
+-- FIXME: we need a trigger that allows UPDATEs on "status" only
 -- FIXME: we also need a notify trigger on insert/update
 
-select add_x_db_fk_def('form_queue', 'fk_submitted_by', 'personalia', 'staff', 'pk');
+select add_x_db_fk_def('form_job_queue', 'fk_submitted_by', 'personalia', 'staff', 'pk');
 
-comment on table form_queue is
+comment on table form_job_queue is
 	'Queue table for rendered form instances. Note that
 	 the rows in this table will get deleted after processing.
 	 This is NOT an archive of form jobs.';
-comment on column form_queue.fk_form_instance is
+comment on column form_job_queue.fk_form_instance is
 	'points to the unrendered source instance of the form,
 	 useful for recalling submitted jobs for changing';
-comment on column form_queue.form is
+comment on column form_job_queue.form is
 	'the rendered form, IOW binary data such as a PDF file';
-comment on column form_queue.fk_target_class is
-	'points to the target class';
-comment on column form_queue.submitted_when is
+comment on column form_job_queue.fk_job_target is
+	'points to the job target';
+comment on column form_job_queue.submitted_when is
 	'when was this form job submitted';
-comment on column form_queue.fk_submitted_by is
+comment on column form_job_queue.fk_submitted_by is
 	'who of the staff submitted this form job';
-comment on column form_queue.submitted_from is
+comment on column form_job_queue.submitted_from is
 	'the workplace this form job was submitted from';
-comment on column form_queue.status is
+comment on column form_job_queue.status is
 	'status of the form job:
 	 - submitted: ready for processing
 	 - in progress: being processed
@@ -131,23 +144,27 @@ comment on column form_queue.status is
 -- permissions
 -- ===================================================================
 GRANT SELECT ON
-	form_target_classes
-	, form_queue
+	form_job_targets
+	, form_job_queue
 TO GROUP "gm-public";
 
 GRANT select, insert, delete, update ON
-	form_queue
-	, form_queue_pk_seq
+	form_job_queue
+	, form_job_queue_pk_seq
 TO GROUP "gm-doctors";
 
 -- ===================================================================
 -- do simple schema revision tracking
 delete from gm_schema_revision where filename='$RCSfile: gmoffice.sql,v $';
-INSERT INTO gm_schema_revision (filename, version) VALUES('$RCSfile: gmoffice.sql,v $', '$Revision: 1.8 $');
+INSERT INTO gm_schema_revision (filename, version) VALUES('$RCSfile: gmoffice.sql,v $', '$Revision: 1.9 $');
 
 --=====================================================================
 -- $Log: gmoffice.sql,v $
--- Revision 1.8  2005-01-12 12:29:29  ncq
+-- Revision 1.9  2005-01-29 18:45:22  ncq
+-- - form_target_classes -> form_job_targets
+-- - form_queue -> form_job_queue
+--
+-- Revision 1.8  2005/01/12 12:29:29  ncq
 -- - comment out some unused tables
 --
 -- Revision 1.7  2004/07/17 20:57:53  ncq
