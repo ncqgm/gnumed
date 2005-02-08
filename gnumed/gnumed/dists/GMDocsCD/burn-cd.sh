d40362049a93d1c08fdf12a7a88ab79d1e4e93ac
Karsten Hilbert <karsten.hilbert@gmx.net>
2005-02-08 07:04:45 +0000

    - some cleanup

--- gnumed/gnumed/dists/GMDocsCD/burn-cd.sh
+++ gnumed/gnumed/dists/GMDocsCD/burn-cd.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+BURN="/usr/local/bin/cdrecord"
+
 echo "burning ISO file $1 onto CD"
-/usr/local/bin/cdrecord -v gracetime=4 dev=/dev/hdc speed=8 -tao driveropts=burnfree -eject -overburn -data $1
+$BURN -v gracetime=4 dev=/dev/hdc speed=8 -tao driveropts=burnfree -eject -overburn -data $1
