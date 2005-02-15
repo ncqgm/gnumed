4106d9f95dc475186bc282b52da5c95c4095234b
Karsten Hilbert <karsten.hilbert@gmx.net>
2005-02-15 12:01:20 +0000

    - since there is no easy answer to cronning epydoc over wxPython   exclude that for now such that we at least get the rest of the   API epydoced regularly

--- gnumed/gnumed/client/doc/make-apidocs.sh
+++ gnumed/gnumed/client/doc/make-apidocs.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
 
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/doc/make-apidocs.sh,v $
-# $Revision: 1.3 $
+# $Revision: 1.4 $
 # license: GPL
 # author: Karsten.Hilbert@gmx.net
 
+BASE="~/gnumed/gnumed/client"
+MODULES="$BASE/business/ $BASE/connectors/ $BASE/exporters/ $BASE/importers/ $BASE/pycommon/"
+
 export DISPLAY=":0.0"
-nice -n 19 epydoc -n GnuMed -u http://www.gnumed.org --no-private -o ~/gm-apidocs ~/gnumed/gnumed/client/
+nice -n 19 epydoc -n GnuMed -u http://www.gnumed.org --no-private -o ~/gm-apidocs $MODULES
 
 #============================================
 # $Log: make-apidocs.sh,v $
-# Revision 1.3  2005-01-19 09:27:59  ncq
+# Revision 1.4  2005-02-15 12:01:20  ncq
+# - since there is no easy answer to cronning epydoc over wxPython
+#   exclude that for now such that we at least get the rest of the
+#   API epydoced regularly
+#
+# Revision 1.3  2005/01/19 09:27:59  ncq
 # - let callers deal with output, don't predefine target as file (cron mails it)
 #
 # Revision 1.2  2005/01/09 15:58:56  ncq
