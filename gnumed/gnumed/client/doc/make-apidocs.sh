ef71ce7bfc31f463ae840f2cfa1ce557d02378e9
Karsten Hilbert <karsten.hilbert@gmx.net>
2005-02-15 18:37:33 +0000

    - capture log for display in the Wiki

--- gnumed/gnumed/client/doc/make-apidocs.sh
+++ gnumed/gnumed/client/doc/make-apidocs.sh
@@ -1,19 +1,24 @@
 #!/bin/sh
 
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/doc/make-apidocs.sh,v $
-# $Revision: 1.4 $
+# $Revision: 1.5 $
 # license: GPL
 # author: Karsten.Hilbert@gmx.net
 
-BASE="~/gnumed/gnumed/client"
+BASE=~/gnumed/gnumed/client
 MODULES="$BASE/business/ $BASE/connectors/ $BASE/exporters/ $BASE/importers/ $BASE/pycommon/"
+LOG=~/gm-apidocs/epydoc-errors.txt
 
 export DISPLAY=":0.0"
-nice -n 19 epydoc -n GnuMed -u http://www.gnumed.org --no-private -o ~/gm-apidocs $MODULES
+nice -n 19 epydoc -n GnuMed -u http://www.gnumed.org --no-private -o ~/gm-apidocs $MODULES &> $LOG
+cat $LOG
 
 #============================================
 # $Log: make-apidocs.sh,v $
-# Revision 1.4  2005-02-15 12:01:20  ncq
+# Revision 1.5  2005-02-15 18:37:33  ncq
+# - capture log for display in the Wiki
+#
+# Revision 1.4  2005/02/15 12:01:20  ncq
 # - since there is no easy answer to cronning epydoc over wxPython
 #   exclude that for now such that we at least get the rest of the
 #   API epydoced regularly
