f8bd54e986541a82b30275cdb484bd6be10bc82e
Karsten Hilbert <karsten.hilbert@gmx.net>
2005-02-21 16:45:48 +0000

    - improve naming

--- gnumed/gnumed/client/doc/make-dumps.sh
+++ gnumed/gnumed/client/doc/make-dumps.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/doc/make-dumps.sh,v $
-# $Revision: 1.1 $
+# $Revision: 1.2 $
 # license: GPL
 # author: Karsten.Hilbert@gmx.net
 
-TODAY=`date -I`
-SCHEMADUMP=~/gm-schemadocs/gm-schema-dump-$TODAY.sql
-DATADUMP=~/gm-schemadocs/gm-schema-dump-$TODAY.sql
-GMDUMP=~/gm-schemadocs/gm-dump-$TODAY.tgz
+SCHEMADUMP=~/gm-schemadocs/gm-schema-dump.sql
+DATADUMP=~/gm-schemadocs/gm-data-dump.sql
+GMDUMP=~/gm-schemadocs/gm-db-dump.tgz
 
 pg_dump -f $SCHEMADUMP -F p -C -s -U gm-dbo gnumed
 pg_dump -f $DATADUMP -F p -a -D -U gm-dbo gnumed
@@ -17,7 +16,10 @@ tar -cvzf $GMDUMP $SCHEMADUMP $DATADUMP
 
 #============================================
 # $Log: make-dumps.sh,v $
-# Revision 1.1  2005-02-21 16:37:09  ncq
+# Revision 1.2  2005-02-21 16:45:48  ncq
+# - improve naming
+#
+# Revision 1.1  2005/02/21 16:37:09  ncq
 # - generate snapshot database dumps for people to try out
 #
 #
