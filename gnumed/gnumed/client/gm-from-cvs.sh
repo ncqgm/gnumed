97bbfd3197077de3b82c17806500b5b51b097066
Karsten Hilbert <karsten.hilbert@gmx.net>
2005-03-17 16:34:55 +0000

    - hint on LANG=en for unicode problems - add commented out --layout variants

--- gnumed/gnumed/client/gm-from-cvs.sh
+++ gnumed/gnumed/client/gm-from-cvs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/gm-from-cvs.sh,v $
-# $Revision: 1.3 $
+# $Revision: 1.4 $
 
 # start kvkd
 # FIXME: needs logic to prevent more than one kvkd from running
@@ -9,10 +9,15 @@
 # maybe force some locale setting here
 #export LANG=fr
 
+# if there are unicode troubles you can force to ASCII with this:
+#export LANG=en
+
 cd ../
 ln -vfsn client Gnumed
 cd -
 export PYTHONPATH="${PYTHONPATH}:../"
 rm -vf gm-from-cvs.log
 python wxpython/gnumed.py --log-file=gm-from-cvs.log --debug
-#--profile=gm-from-cvs.prof
+# --layout status_quo
+# --layout terry
+# --profile=gm-from-cvs.prof
