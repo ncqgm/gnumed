fbe61a11102ae13ab29ceb99712cc3e13bd749f6
Karsten Hilbert <karsten.hilbert@gmx.net>
2005-04-06 10:41:19 +0000

    - add a bunch of diagnoses coded ICD-10-GM

---
+++ gnumed/gnumed/server/sql/country.specific/de/gmDiagnosen.sql
@@ -0,0 +1,51 @@
+-- =============================================
+-- GNUmed: Diagnosen
+
+-- $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/server/sql/country.specific/de/gmDiagnosen.sql,v $
+-- $Revision: 1.1 $
+
+-- license: GPL
+-- author (of script file): Karsten Hilbert <Karsten.Hilbert@gmx.net>
+
+-- =============================================
+-- force terminate + exit(3) on errors if non-interactive
+\set ON_ERROR_STOP 1
+
+set client_encoding to 'LATIN1';
+-- =============================================
+select add_coded_term('Harnwegsinfekt', 'N39.0', 'ICD10-GM 2004');
+select add_coded_term('Pollinosis', 'J30.1', 'ICD-10-GM 2004');
+select add_coded_term('Basedow-Struma', 'E05.0', 'ICD-10-GM 2004');
+select add_coded_term('Strumaresektion', 'Z98.8', 'ICD-10-GM 2004');
+select add_coded_term('Follikuläres Schilddrüsenkarzinom', 'C73', 'ICD-10-GM 2004');
+select add_coded_term('Hyperlipidämie', 'E78.5', 'ICD-10-GM 2004');
+select add_coded_term('Diabetes mellitus nnb', 'E14.9', 'ICD-10-GM 2004');
+select add_coded_term('Reizblase', 'R30.0', 'ICD-10-GM 2004');
+select add_coded_term('Hyperthyreose', 'E05.9', 'ICD-10-GM 2004');
+select add_coded_term('Hypercholesterinämie', 'E78.0', 'ICD-10-GM 2004');
+select add_coded_term('Hypocalzämie', 'R79.0', 'ICD-10-GM 2004');
+select add_coded_term('Strumaresektion', 'E89.-', 'ICD-10-GM 2004');
+select add_coded_term('Lymphangitis', 'I89.1', 'ICD-10-GM 2004');
+select add_coded_term('Check up', 'Z00.-', 'ICD-10-GM 2004');
+select add_coded_term('Nikotinabusus', 'F17.2', 'ICD-10-GM 2004');
+select add_coded_term('psychovegetatives Syndrom', 'F45.1', 'ICD-10-GM 2004');
+select add_coded_term('Depressive Verstimmung', 'F32.9', 'ICD-10-GM 2004');
+select add_coded_term('Leistungsknick', 'R53', 'ICD-10-GM 2004');
+
+--select add_coded_term('', '', 'ICD-10-GM 2004');
+--select add_coded_term('', '', 'ICD-10-GM 2004');
+--select add_coded_term('', '', 'ICD-10-GM 2004');
+--select add_coded_term('', '', 'ICD-10-GM 2004');
+--select add_coded_term('', '', 'ICD-10-GM 2004');
+--select add_coded_term('', '', 'ICD-10-GM 2004');
+
+-- =============================================
+-- do simple revision tracking
+delete from gm_schema_revision where filename = '$RCSfile: gmDiagnosen.sql,v $';
+INSERT INTO gm_schema_revision (filename, version) VALUES ('$RCSfile: gmDiagnosen.sql,v $', '$Revision: 1.1 $');
+
+-- =============================================
+-- $Log: gmDiagnosen.sql,v $
+-- Revision 1.1  2005-04-06 10:41:19  ncq
+-- - add a bunch of diagnoses coded ICD-10-GM
+--
