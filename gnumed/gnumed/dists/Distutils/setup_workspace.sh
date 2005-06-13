af996c56424f28f7edc6f708fe25337fae95e9a1
shilbert <shilbert>
2005-06-13 18:52:53 +0000

    - create symlink from GNUmed-$REV --> Gnumed

--- gnumed/gnumed/dists/Distutils/setup_workspace.sh
+++ gnumed/gnumed/dists/Distutils/setup_workspace.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/dists/Distutils/Attic/setup_workspace.sh,v $
-# $Id: setup_workspace.sh,v 1.2 2005-06-13 16:32:46 ncq Exp $
+# $Id: setup_workspace.sh,v 1.3 2005-06-13 18:52:53 shilbert Exp $
 
 REV=$1
+mkdir ./GNUmed-$REV/
+ln -s ./GNUmed-$REV/ ./Gnumed
 
 echo "setting up workspace"
 cp -R ../../client/bitmaps ./GNUmed-$REV/
@@ -18,6 +20,9 @@ find ./ -name '*.pyc' -exec rm -v '{}' ';'
 
 #------------------------------------------
 # $Log: setup_workspace.sh,v $
-# Revision 1.2  2005-06-13 16:32:46  ncq
+# Revision 1.3  2005-06-13 18:52:53  shilbert
+# - create symlink from GNUmed-$REV --> Gnumed
+#
+# Revision 1.2  2005/06/13 16:32:46  ncq
 # - let user specify revision
 #
