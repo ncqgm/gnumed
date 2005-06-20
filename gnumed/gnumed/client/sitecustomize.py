622db595f7ddb3837d38fc9b3263cb0113074c06
Karsten Hilbert <karsten.hilbert@gmx.net>
2005-06-20 20:55:00 +0000

    - apparently wxPython or something messes with the encoding so   while testing the encoding works the same code fails after   wxPython startup, so don't test, use explicit flag, default False

--- gnumed/gnumed/client/sitecustomize.py
+++ gnumed/gnumed/client/sitecustomize.py
@@ -23,14 +23,15 @@ but that approach may not be fool-proof.
 """
 #==============================================================
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/sitecustomize.py,v $
-# $Id: sitecustomize.py,v 1.3 2005-06-20 20:41:30 ncq Exp $
-__version__ = "$Revision: 1.3 $"
+# $Id: sitecustomize.py,v 1.4 2005-06-20 20:55:00 ncq Exp $
+__version__ = "$Revision: 1.4 $"
 __author__  = "Karsten Hilbert <Karsten.Hilbert@gmx.net>"
 __license__ = "GPL (details at http://www.gnu.org)"
 
 import sys
 
-# some handy definitions:
+# flip this flag if you need to set the encoding explicitely
+do_set_encoding = False
 
 # - most European countries but shouldn't
 #   hurt in US-ASCII countries, either
@@ -45,17 +46,6 @@ def_encoding = 'iso8859-1'
 # - for testing
 #def_encoding = 'ascii'
 #==============================================================
-def __setup_encoding():
-	print "GNUmed startup: Setting Python string encoding to [%s]" % def_encoding
-	try:
-		sys.setdefaultencoding(def_encoding)
-	except LookupError:
-		print "GNUmed startup: Cannot set Python string encoding to invalid value [%s]" % def_encoding
-		print "GNUmed startup: Default Python string encoding is [%s]" % sys.getdefaultencoding()
-		print "GNUmed startup: GNUmed is likely to fail where non-7-bit-ASCII is involved"
-	except AttributeError:
-		print "GNUmed startup: Python string encoding must have been set already ?!?"
-#==============================================================
 if __name__ == '__main__':
 	print "------------------------------------------------"
 	print "This file is note intended to be run standalone."
@@ -65,18 +55,25 @@ if __name__ == '__main__':
 	sys.exit()
 
 print "GNUmed startup: running sitecustomize.py"
-
-try:
-	tmp = u'ß'.encode('latin-1')
-	# for testing uncomment the following line
-	#tmp = u'ß'.encode('totally-bogus-encoding')
-	print "GNUmed startup: Your Python string encoding seems to already be set to [%s]" % sys.getdefaultencoding()
-except:
-	__setup_encoding()
+if do_set_encoding:
+	print "GNUmed startup: Setting Python string encoding to [%s]" % def_encoding
+	try:
+		sys.setdefaultencoding(def_encoding)
+	except LookupError:
+		print "GNUmed startup: Cannot set Python string encoding to invalid value [%s]" % def_encoding
+		print "GNUmed startup: Default Python string encoding is [%s]" % sys.getdefaultencoding()
+		print "GNUmed startup: GNUmed is likely to fail where non-7-bit-ASCII is involved"
+	except AttributeError:
+		print "GNUmed startup: Python string encoding must have been set already ?!?"
 
 #==============================================================
 # $Log: sitecustomize.py,v $
-# Revision 1.3  2005-06-20 20:41:30  ncq
+# Revision 1.4  2005-06-20 20:55:00  ncq
+# - apparently wxPython or something messes with the encoding so
+#   while testing the encoding works the same code fails after
+#   wxPython startup, so don't test, use explicit flag, default False
+#
+# Revision 1.3  2005/06/20 20:41:30  ncq
 # - improved again, it might even work
 #
 # Revision 1.2  2005/06/20 19:42:25  ncq
