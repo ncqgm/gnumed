7cac22ead111f77f0af6d7796e692f4294df8a8f
Karsten Hilbert <karsten.hilbert@gmx.net>
2005-07-10 17:44:59 +0000

    - slightly better packages

--- gnumed/gnumed/dists/Linux/make-dists.sh
+++ gnumed/gnumed/dists/Linux/make-dists.sh
@@ -1,17 +1,23 @@
 #!/bin/sh
 
-# $Source: 
-# $Revision: 1.1 $
-# GPL
+# $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/dists/Linux/Attic/make-dists.sh,v $
+# $Id: make-dists.sh,v 1.2 2005-07-10 17:44:59 ncq Exp $
+# license: GPL
 # Karsten.Hilbert@gmx.net
 
 REV=0.1
 
 echo "=> client ..."
-tar -cvhzf GNUmed-client_$REV.tar.gz GNUmed-$REV/client check-prerequisites.py check-prerequisites.sh GnuPublicLicense.txt install.sh
+tar -cvhzf GNUmed-client.$REV.tgz GNUmed-$REV/client GNUmed-$REV/install.sh GNUmed-$REV/GnuPublicLicense.txt GNUmed-$REV/check-prerequisites.py GNUmed-$REV/check-prerequisites.sh
 
 echo "=> server ..."
-tar -cvhzf GNUmed-server_$REV.tar.gz GNUmed-$REV/server check-prerequisites.py check-prerequisites.sh GnuPublicLicense.txt
+tar -cvhzf GNUmed-server.$REV.tgz GNUmed-$REV/server GNUmed-$REV/GnuPublicLicense.txt GNUmed-$REV/check-prerequisites.py GNUmed-$REV/check-prerequisites.sh
 
 echo "=> client + server ..."
-tar -cvzf GNUmed.tgz GNUmed-client_$REV.tar.gz GNUmed-server_$REV.tar.gz
+tar -cvzf GNUmed.$REV.tgz GNUmed-client.$REV.tgz GNUmed-server.$REV.tgz
+
+#============================================
+# $Log: make-dists.sh,v $
+# Revision 1.2  2005-07-10 17:44:59  ncq
+# - slightly better packages
+#
