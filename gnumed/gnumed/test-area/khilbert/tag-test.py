cda5d0ab1a916bfb59dbd99c7a2855041b9f26b7
Karsten Hilbert <karsten.hilbert@gmx.net>
2005-07-19 16:09:59 +0000

    - just some changes

--- gnumed/gnumed/test-area/khilbert/tag-test.py
+++ gnumed/gnumed/test-area/khilbert/tag-test.py
@@ -1,11 +1,10 @@
-#===========================================================
-# $Header: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/test-area/khilbert/tag-test.py,v 1.1 2005-07-19 15:48:36 ncq Exp $
+#==========================================================================
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/test-area/khilbert/tag-test.py,v $
-# $Id: tag-test.py,v 1.1 2005-07-19 15:48:36 ncq Exp $
-#===========================================================
+# $Id: tag-test.py,v 1.2 2005-07-19 16:09:59 ncq Exp $
+#==========================================================================
 
 tag = '$Name:  $'
-rev = '$Revision: 1.1 $'
+rev = '$Revision: 1.2 $'
 
 print tag
 print rev
