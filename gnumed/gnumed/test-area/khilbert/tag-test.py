3cfa2a6fb40ade2952ba62e640d4a4fb5697efbc
Karsten Hilbert <karsten.hilbert@gmx.net>
2005-07-19 15:48:36 +0000

    - used to test tagging

---
+++ gnumed/gnumed/test-area/khilbert/tag-test.py
@@ -0,0 +1,11 @@
+#===========================================================
+# $Header: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/test-area/khilbert/tag-test.py,v 1.1 2005-07-19 15:48:36 ncq Exp $
+# $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/test-area/khilbert/tag-test.py,v $
+# $Id: tag-test.py,v 1.1 2005-07-19 15:48:36 ncq Exp $
+#===========================================================
+
+tag = '$Name:  $'
+rev = '$Revision: 1.1 $'
+
+print tag
+print rev
