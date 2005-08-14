fc4d02120f18c3331201d5fa819b986d589015ce
shilbert <shilbert>
2005-08-14 16:34:41 +0000

    - bump up to rc5 - cater for subrevisions

--- gnumed/gnumed/dists/Linux/make-dists.sh
+++ gnumed/gnumed/dists/Linux/make-dists.sh
@@ -1,23 +1,27 @@
 #!/bin/sh
 
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/dists/Linux/Attic/make-dists.sh,v $
-# $Id: make-dists.sh,v 1.2 2005-07-10 17:44:59 ncq Exp $
+# $Id: make-dists.sh,v 1.3 2005-08-14 16:34:41 shilbert Exp $
 # license: GPL
 # Karsten.Hilbert@gmx.net
 
 REV=0.1
-
+SUBREV=rc5
 echo "=> client ..."
-tar -cvhzf GNUmed-client.$REV.tgz GNUmed-$REV/client GNUmed-$REV/install.sh GNUmed-$REV/GnuPublicLicense.txt GNUmed-$REV/check-prerequisites.py GNUmed-$REV/check-prerequisites.sh
+tar -cvhzf GNUmed-client.$REV-$SUBREV.tgz GNUmed-$REV/client GNUmed-$REV/install.sh GNUmed-$REV/GnuPublicLicense.txt GNUmed-$REV/check-prerequisites.py GNUmed-$REV/check-prerequisites.sh
 
 echo "=> server ..."
-tar -cvhzf GNUmed-server.$REV.tgz GNUmed-$REV/server GNUmed-$REV/GnuPublicLicense.txt GNUmed-$REV/check-prerequisites.py GNUmed-$REV/check-prerequisites.sh
+tar -cvhzf GNUmed-server.$REV-$SUBREV.tgz GNUmed-$REV/server GNUmed-$REV/GnuPublicLicense.txt GNUmed-$REV/check-prerequisites.py GNUmed-$REV/check-prerequisites.sh
 
 echo "=> client + server ..."
-tar -cvzf GNUmed.$REV.tgz GNUmed-client.$REV.tgz GNUmed-server.$REV.tgz
+tar -cvzf GNUmed.$REV-$SUBREV.tgz GNUmed-client.$REV-$SUBREV.tgz GNUmed-server.$REV-$SUBREV.tgz
 
 #============================================
 # $Log: make-dists.sh,v $
-# Revision 1.2  2005-07-10 17:44:59  ncq
+# Revision 1.3  2005-08-14 16:34:41  shilbert
+# - bump up to rc5
+# - cater for subrevisions
+#
+# Revision 1.2  2005/07/10 17:44:59  ncq
 # - slightly better packages
 #
