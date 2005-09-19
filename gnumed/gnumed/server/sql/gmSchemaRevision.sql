e52f2ebcea0738077db8edc1edbe7c02bb481588
Karsten Hilbert <karsten.hilbert@gmx.net>
2005-09-19 16:22:12 +0000

    - remove gm_schema_revision.is_core - add gm_concat_table_structure() - remove calc_db_identity_hash() in favour of select md5(gm_concat_table_structure())

--- gnumed/gnumed/server/sql/gmSchemaRevision.sql
+++ gnumed/gnumed/server/sql/gmSchemaRevision.sql
@@ -1,7 +1,7 @@
 -- =============================================
 -- project: GNUmed
 -- $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/server/sql/gmSchemaRevision.sql,v $
--- $Id: gmSchemaRevision.sql,v 1.14 2005-07-14 21:31:42 ncq Exp $
+-- $Id: gmSchemaRevision.sql,v 1.15 2005-09-19 16:22:12 ncq Exp $
 -- license: GPL
 -- author: Karsten.Hilbert@gmx.net
 
@@ -16,7 +16,7 @@
 --  be replaced automagically with the proper data by "cvs commit")
 
 -- do simple schema revision tracking
--- select log_script_insertion('$RCSfile: gmSchemaRevision.sql,v $', '$Revision: 1.14 $', True/False);
+-- select log_script_insertion('$RCSfile: gmSchemaRevision.sql,v $', '$Revision: 1.15 $');
 
 -- =============================================
 -- force terminate + exit(3) on errors if non-interactive
@@ -28,8 +28,6 @@ create table gm_schema_revision (
 	filename text
 		not null,
 	version text
-		not null,
-	is_core boolean
 		not null,
 	imported timestamp with time zone
 		not null
@@ -46,11 +44,6 @@ comment on column gm_schema_revision.filename is
 	'the name of the script, handled most easily by CVS via "RCSfile"';
 comment on column gm_schema_revision.version is
 	'the version of the script, handled most easily by CVS via "Revision"';
-comment on column gm_schema_revision.is_core is
-	'whether this script is to be included in calculating
-	 the identity hash of this database, thereby one can
-	 flexibly ignore test data scripts and such which do
-	 not really affect the schema layout itself';
 comment on column gm_schema_revision.imported is
 	'when this script was imported, mainly for debugging';
 
@@ -69,7 +62,7 @@ comment on table gm_database_revision is
 	'this table holds the database revision against
 	 which clients can match their expectations,
 	 the algorithm to calculate the hash is found
-	 in the function below';
+	 in the function calc_db_identity_hash()';
 
 -- ---------------------------------------------
 create table gm_client_db_match (
@@ -94,60 +87,14 @@ comment on column gm_client_db_match.db_identity_hash is
 	'the identity_hash of a database revision that
 	 the client version can work with';
 
--- ---------------------------------------------
-create function calc_db_identity_hash() returns text as '
-declare
-	_row record;
-	_total text;
-	_hash text;
-begin
-	_total := '''';
-	for _row in select filename, version from gm_schema_revision where is_core order by filename, version loop
-		_total := _total || _row.filename || _row.version;
-	end loop;
-	select into _hash md5(_total);
-	return _hash;
-end;' language 'plpgsql';
-
-comment on function calc_db_identity_hash() is
-	'this is how we calculate the hash:
-	 - select relevant script rows (is_core is True)
-	 - order them by file name/revision such that
-	   real order of insertion is insignificant
-	 - concatenate name + version + name + version + ...
-	 - calculate md5 hash of concatenated string
-	   (md5 because it is built in)';
-
--- ---------------------------------------------
-create function log_script_insertion(text, text, boolean) returns text as '
-declare
-	_filename alias for $1;
-	_version alias for $2;
-	_is_core alias for $3;
-	_hash text;
-begin
-	delete from gm_schema_revision where filename = _filename;
-	insert into gm_schema_revision (filename, version, is_core) values (
-		_filename,
-		_version,
-		_is_core
-	);
-	select into _hash calc_db_identity_hash();
-	delete from gm_database_revision;
-	insert into gm_database_revision (identity_hash) values (_hash);
-	return _hash;
-end;' language 'plpgsql';
-
--- =============================================
-GRANT SELECT on
-	gm_schema_revision
-	, gm_database_revision
-	, gm_client_db_match
-TO group "gm-public";
-
 -- =============================================
 -- $Log: gmSchemaRevision.sql,v $
--- Revision 1.14  2005-07-14 21:31:42  ncq
+-- Revision 1.15  2005-09-19 16:22:12  ncq
+-- - remove gm_schema_revision.is_core
+-- - add gm_concat_table_structure()
+-- - remove calc_db_identity_hash() in favour of select md5(gm_concat_table_structure())
+--
+-- Revision 1.14  2005/07/14 21:31:42  ncq
 -- - partially use improved schema revision tracking
 --
 -- Revision 1.13  2005/07/14 21:29:40  ncq
