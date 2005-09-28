f7ce928cee7fcef3825b864b2bee6269e575aeef
Karsten Hilbert <karsten.hilbert@gmx.net>
2005-09-28 20:35:32 +0000

    - prepare for 0.2

---
+++ gnumed/gnumed/client/gm-0_2-from-cvs.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/Attic/gm-0_2-from-cvs.sh,v $
+# $Revision: 1.1 $
+
+# if there are unicode troubles you can force to ASCII with this:
+#export LANG=en
+
+cd ../
+ln -vfsn client Gnumed
+cd -
+export PYTHONPATH="${PYTHONPATH}:../"
+LOG="gm-0_2-from-cvs.log"
+rm -vf $LOG
+
+# prepare
+cp -vf gm-0_2.conf tmp-gm-0_2.conf
+python wxpython/gnumed.py --log-file=$LOG --conf-file=tmp-gm-0_2.conf --debug
+# --unicode-gettext=0
+# clean up
+rm -vf tmp-gm-0_2.conf
+rm -vf tmp-gm-0_2.conf.gmCfg.bak
