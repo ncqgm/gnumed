b373d8a1eb8147c6907424e689a44dd3c1106a19
Karsten Hilbert <karsten.hilbert@gmx.net>
2005-10-19 09:14:46 +0000

    - remove half-baked support for embedded data, now handled elsewhere - general cleanup/doc fixes

--- gnumed/gnumed/client/business/gmSOAPimporter.py
+++ gnumed/gnumed/client/business/gmSOAPimporter.py
@@ -1,24 +1,14 @@
-"""GnuMed SOAP importer
+"""GNUmed SOAP importer
 
 (specification by Karsten Hilbert <Karsten.Hilbert@gmx.net>)
 
-This script is designed for importing GnuMed SOAP input "bundle".
+This script is designed for importing GNUmed SOAP input "bundles".
 
-	- "bundle" is list of dicts. Each "bundle" is processed one by one. The dicts
-	  in the list are INDEPENDANT of each other, so every dict is then taken apart
-
-	- each bundle contain information for:
-		- a new clin_narrative row 
-		- optionally, additionally data, marked by keys "embedded" into the
-		  text of the narrative that are looked up, parsed out and appropiately
-		  imported	depending on its 'type' using the business classes.
-		- additional data that does not have a key is alerted to the
-		  user. The same is done for keys in the text that have no entry in the
-		  additional data. The most likely reason for this to happen is the user
-		  manually editing the [:...:] embedded strings in 'text' while still
-		  in the soap input widget.
-
-	- each dict has the keys: 'soap', 'types', 'text', 'struct_data', 'clin_context'
+	- "bundle" is list of dicts
+	- each "bundle" is processed dict by dict
+	- the dicts in the list are INDEPENDANT of each other
+	- each dict contains information for one new clin_narrative row
+	- each dict has the keys: 'soap', 'types', 'text', 'clin_context'
 		- 'soap':			 
 			- relates to clin_narrative.soap_cat
 		- 'types':
@@ -28,25 +18,15 @@ This script is designed for importing GnuMed SOAP input "bundle".
 			  import and the user is warned about that
 		- 'text':
 			- the narrative for clin_narrative.narrative, imported as is
-			- substrings of the form [:...:] are remembered
 		- 'clin_context':
 			- 'clin_context' is a dictionary containing clinical
 			  context information, required to properly create clinical items.
 			  Its 'episode_id' must always be supplied.
-		- 'struct_data':
-			- this is a dictionary with additional structured data
-			- the keys to this dictionary are the "..." parts of the [:...:]
-			  found in 'text' (see above)
-			- the values will be dicts themselves with the keys
-			  'type' and 'struct_data':
-				- 'type': the type of 'data' such as 'allergy', 'vaccination',
-				  set by the popup widgets inside gmSoapInput
-			- 'struct_data' is a dict of fields structured according to 'type'
 """
 #===============================================================
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/business/gmSOAPimporter.py,v $
-# $Id: gmSOAPimporter.py,v 1.9 2005-10-11 21:50:33 ncq Exp $
-__version__ = "$Revision: 1.9 $"
+# $Id: gmSOAPimporter.py,v 1.10 2005-10-19 09:14:46 ncq Exp $
+__version__ = "$Revision: 1.10 $"
 __author__ = "Carlos Moro <cfmoro1976@yahoo.es>"
 __license__ = "GPL (details at http://www.gnu.org)"
 
@@ -68,16 +48,12 @@ _cfg = gmCfg.gmDefCfgFile
 soap_bundle_SOAP_CAT_KEY = "soap"
 soap_bundle_TYPES_KEY = "types"
 soap_bundle_TEXT_KEY = "text"
-soap_bundle_STRUCT_DATA_KEY = 'struct_data'
 soap_bundle_CLIN_CTX_KEY = "clin_context"
 soap_bundle_TYPE_KEY = "type"
 soap_bundle_EPISODE_ID_KEY = "episode_id"
 soap_bundle_ENCOUNTER_ID_KEY = "encounter_id"
 soap_bundle_STAFF_ID_KEY = "staff_id"
 soap_bundle_SOAP_CATS = ['s','o','a','p']		# these are pretty much fixed
-# key pattern: any string between [: and :]. Any of chars in '[:]'
-# are forbidden in the key string
-soap_bundle_KEY_PATTERN = "\[:.[^:\[\]]*:\]"	 
 #===============================================================
 class cSOAPImporter:
 	"""
@@ -90,9 +66,6 @@ class cSOAPImporter:
 	#-----------------------------------------------------------
 	# external API
 	#-----------------------------------------------------------
-	def set_patient(self, patient=None):
-		pass
-	#-----------------------------------------------------------
 	def import_soap(self, bundle=None):
 		"""
 		Import supplied GnuMed SOAP input "bundle". For details consult current
@@ -101,29 +74,12 @@ class cSOAPImporter:
 		@param bundle: GnuMed SOAP input data (as described in module's information)
 		@type bundle: list of dicts
 		"""
-		# verify bundle
-		if bundle is None or len(bundle) == 0:
-			_log.Log(gmLog.lWarn, 'no SOAP bundle to import: [%s]' % bundle)
-			return False
-
-		# keys in the text that have no entry in the additional data
-		unmatched_keys = []
-		# additional data that does not have a key
-		unkeyed_data = []
-
 		# process each entry in soap bundle independantly
 		for soap_entry in bundle:
 			if not self.__import_narrative(soap_entry):
 				_log.Log(gmLog.lErr, 'skipping soap entry')
 				continue
-			_log.Log(gmLog.lInfo, "soap narrative imported OK")
-
-			if not self.__import_embedded_data(soap_entry):
-				_log.Log(gmLog.lErr, 'skipping soap entry')
-				continue
-			_log.Log(gmLog.lInfo, "embedded data imported OK")
 		gmDispatcher.send( gmSignals.clin_item_updated())
-
 		return True
 	#-----------------------------------------------------------
 	# internal helpers
@@ -132,8 +88,8 @@ class cSOAPImporter:
 		"""Import soap entry into GnuMed backend.
 
 		@param soap_entry: dictionary containing information related to one
-						   SOAP input
-		@type soap_entry: dictionary with keys 'soap', 'types', 'text', 'struct_data'
+						   SOAP input line
+		@type soap_entry: dictionary with keys 'soap', 'types', 'text'
 
 		FIXME: Later we may want to allow for explicitely setting a staff ID to be
 		FIXME: used for import. This would allow to import data "on behalf of" someone.
@@ -160,10 +116,7 @@ class cSOAPImporter:
 
 		# attach types
 		if soap_entry.has_key(soap_bundle_TYPES_KEY):
-			pass
-#			print "types	: %s" % soap_entry[soap_bundle_TYPES_KEY]
-#			for narr_type in soap_entry[soap_bundle_TYPES_KEY]:
-#				narr.attach_type(item_type = narr_type)
+			print "code missing to attach types to imported narrative"
 
 		return status
 	#-----------------------------------------------------------
@@ -173,7 +126,7 @@ class cSOAPImporter:
 		
 		@param soap_entry: dictionary containing information related to one
 						   SOAP input
-		@type soap_entry: dictionary with keys 'soap', 'types', 'text', 'struct_data'
+		@type soap_entry: dictionary with keys 'soap', 'types', 'text'
 		"""
 		required_keys = [
 			soap_bundle_SOAP_CAT_KEY,
@@ -201,72 +154,13 @@ class cSOAPImporter:
 			return False
 		return True
 	#-----------------------------------------------------------
-	def __print_item(self, soap_entry=None, data=None):
-		epi_id = soap_entry[soap_bundle_CLIN_CTX_KEY][soap_bundle_EPISODE_ID_KEY]
-		try:
-			enc_id = soap_entry[soap_bundle_CLIN_CTX_KEY][soap_bundle_ENCOUNTER_ID_KEY]
-		except KeyError:
-			emr = self.__pat.get_clinical_record()
-			enc_id = emr.get_active_encounter()['pk_encounter']
-
-		print "additional data"
-		print "type	 : %s" % data[soap_bundle_TYPE_KEY]
-		print "episode  : %s" % epi_id
-		print "encounter: %s" % enc_id
-		for key in data[soap_bundle_STRUCT_DATA_KEY].keys():
-			print "%s: %s" % (key, data[soap_bundle_STRUCT_DATA_KEY][key])
-	#-----------------------------------------------------------
-	# FIXME: to be replaced as written
-	_struct_data_handlers = {
-		'vaccination': __print_item,
-		'allergy': __print_item
-	}
-	#-----------------------------------------------------------
-	def __import_embedded_data(self, soap_entry):
-		# find embedded keys
-		narr = soap_entry[soap_bundle_TEXT_KEY]
-		embedded_keys = re.findall(soap_bundle_KEY_PATTERN, narr)
-		embedded_keys = map(lambda key: key.replace("[:","").replace(":]",""), embedded_keys)
-		# cross-check
-		try:
-			struct_data_list = soap_entry[soap_bundle_STRUCT_DATA_KEY]
-		except KeyError:
-			if len(embedded_keys) == 0:
-				return True
-			# FIXME: we want to alert the user here and allow her to
-			# FIXME: match dangling data with dangling keys ...
-			_log.Log(gmLog.lErr, 'dangling data keys: %s' % embedded_keys)
-			return False
-		# try importing
-		for key in struct_data_list.keys():
-			embedded_keys.remove(key)
-			struct_data = struct_data_list[key]
-			try:
-				struct_data_handler = cSOAPImporter._struct_data_handlers[struct_data[soap_bundle_TYPE_KEY]]
-			except KeyError:
-				_log.Log(gmLog.lErr, 'unknown type [%s] of additional data' % struct_data[soap_bundle_TYPE_KEY])
-				_log.Log(gmLog.lErr, '%s' % struct_data)
-				continue
-			if not struct_data_handler(self, soap_entry=soap_entry, data=struct_data):
-				_log.Log(gmLog.lErr, 'cannot import structured data')
-				continue
-		# all done ?
-		if len(embedded_keys) != 0:
-			# FIXME: we want to alert the user here and allow her to
-			# FIXME: match dangling data with dangling keys ...
-			_log.Log(gmLog.lErr, 'dangling data keys: %s' % embedded_keys)
-
-		return True
-	#-----------------------------------------------------------
-	#-----------------------------------------------------------
-	#-----------------------------------------------------------
 #	def _verify_types(self, soap_entry):
 #		"""
 #		Perform types key check of a supplied SOAP entry
 #		
 #		@param soap_entry: dictionary containing information related to one
 #						   SOAP input
-#		@type soap_entry: dictionary with keys 'soap', 'types', 'text', 'struct_data'
+#		@type soap_entry: dictionary with keys 'soap', 'types', 'text'
 #		"""
 #		
 #		# FIXME fetch from backend
@@ -329,21 +223,7 @@ if __name__ == '__main__':
 				soap_bundle_ENCOUNTER_ID_KEY: '1',
 				soap_bundle_STAFF_ID_KEY: '1'
 				},
-			 soap_bundle_STRUCT_DATA_KEY: {
-				'tetanus': {
-					soap_bundle_TYPE_KEY: 'vaccination',
-					soap_bundle_STRUCT_DATA_KEY: {
-						'vaccine':'tetanus'
-					}
-				},
-				'pneumoniae': {
-					soap_bundle_TYPE_KEY: 'vaccination',
-					soap_bundle_STRUCT_DATA_KEY: {
-						'vaccine':'pneumoniae'
-					}
-				}
 			}
-			}									 
 		]
 		importer.import_soap(bundle)
 
@@ -366,7 +246,11 @@ if __name__ == '__main__':
 	_log.Log (gmLog.lInfo, "closing SOAP importer...")
 #================================================================
 # $Log: gmSOAPimporter.py,v $
-# Revision 1.9  2005-10-11 21:50:33  ncq
+# Revision 1.10  2005-10-19 09:14:46  ncq
+# - remove half-baked support for embedded data, now handled elsewhere
+# - general cleanup/doc fixes
+#
+# Revision 1.9  2005/10/11 21:50:33  ncq
 # - create_clin_narrative() should not be aware of emr object
 #
 # Revision 1.8  2005/10/08 12:33:08  sjtan
