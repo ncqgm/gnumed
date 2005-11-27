0363ac8c600d5d3d4b984fc5747cc7a35489dad2
Karsten Hilbert <karsten.hilbert@gmx.net>
2005-11-27 19:41:38 +0000

    - prep up for slave mode testing

--- gnumed/gnumed/client/gm-slave-test-from-cvs.sh
+++ gnumed/gnumed/client/gm-slave-test-from-cvs.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
 
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/gm-slave-test-from-cvs.sh,v $
-# $Revision: 1.2 $
+# $Revision: 1.3 $
 
 # maybe force some locale setting here
 #export LANG=fr
+
+LOG="gm-slave-test.log"
+CONF="gm-slave-test.conf"
 
 # start GnuMed
 cd ../
 ln -vfsn client Gnumed
 cd -
 export PYTHONPATH="${PYTHONPATH}:../"
-python wxpython/gnumed.py --debug --log-file=./gm-slave-test.log --conf-file=./gm-slave-test.conf
+rm -vf $LOG
+cp -vf ${CONF} tmp-${CONF}
+
+python wxpython/gnumed.py --log-file=${LOG} --conf-file=tmp-${CONF} --debug
+# --unicode-gettext=0
+# clean up
+rm -vf tmp-${CONF}
+rm -vf tmp-${CONF}.gmCfg.bak
