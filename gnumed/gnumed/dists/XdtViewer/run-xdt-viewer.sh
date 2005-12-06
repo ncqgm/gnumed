c46372a88427ea1ec5e35d71aeda628d14f06ca0
shilbert <shilbert>
2005-12-06 22:45:39 +0000

    - usual cleanup to make it run again

--- gnumed/gnumed/dists/XdtViewer/run-xdt-viewer.sh
+++ gnumed/gnumed/dists/XdtViewer/run-xdt-viewer.sh
@@ -4,7 +4,7 @@
 # set your command line arguments etc. here
 
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/dists/XdtViewer/Attic/run-xdt-viewer.sh,v $
-# $Id: run-xdt-viewer.sh,v 1.1 2003-02-15 17:14:32 ncq Exp $
+# $Id: run-xdt-viewer.sh,v 1.2 2005-12-06 22:45:39 shilbert Exp $
 
 # if you want another language than the standard system one
 #LANG = "de_DE@euro"
@@ -12,7 +12,4 @@
 # if you need to set a special base directory for some reason
 #GMXDTVIEWER_DIR = ""
 
-python ./gmXdtViewer.py \
-	--text-domain=gnumed \
-	--log-file=/var/log/gnumed/xdt-viewer.log \
-	--xdt-file=$1
+python ./gmXdtViewer.py --text-domain=gnumed --xdt-file=$1
