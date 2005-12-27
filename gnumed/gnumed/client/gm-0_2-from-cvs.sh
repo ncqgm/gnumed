010ab645a8c4c3e3bb8ee6f13e8de6ec8ccf001d
Karsten Hilbert <karsten.hilbert@gmx.net>
2005-12-27 17:26:09 +0000

    - add override schema check since this is for testing

--- gnumed/gnumed/client/gm-0_2-from-cvs.sh
+++ gnumed/gnumed/client/gm-0_2-from-cvs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/Attic/gm-0_2-from-cvs.sh,v $
-# $Revision: 1.1 $
+# $Revision: 1.2 $
 
 # if there are unicode troubles you can force to ASCII with this:
 #export LANG=en
@@ -15,7 +15,7 @@ rm -vf $LOG
 
 # prepare
 cp -vf gm-0_2.conf tmp-gm-0_2.conf
-python wxpython/gnumed.py --log-file=$LOG --conf-file=tmp-gm-0_2.conf --debug
+python wxpython/gnumed.py --log-file=$LOG --conf-file=tmp-gm-0_2.conf --debug --override-schema-check
 # --unicode-gettext=0
 # clean up
 rm -vf tmp-gm-0_2.conf
