fce5ecd34308c4d86e5e42889b0007d5952b8946
Karsten Hilbert <karsten.hilbert@gmx.net>
2006-01-06 07:55:35 +0000

    - moved to proper place

--- gnumed/gnumed/client/med_protocol.sql
+++
@@ -1,5 +0,0 @@
- create table clin.med_protocol_element ( narrative text, generic text, adjuvant text, dosage_form text , ufk_drug text not null, drug_db text not null, atc_code text not null, is_cr boolean default false, dosage numeric[] not null , period interval not null, dosage_unit text null, is_paed boolean default false, paed_dose numeric default null, paed_factor text default 'mg/kg' , directions text, is_prn boolean default false) inherits (audit_fields ) ;
- create table clin.med_protocol ( pk serial primary key, name text ) inherits (audit_fields);
- alter table clin.med_protocol_element add fk_med_protocol integer references clin.med_protocol;
- 
-
