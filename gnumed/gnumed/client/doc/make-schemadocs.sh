43c2bf6e823774a911d1c7ecadb7b3a7aaa1afe1
Karsten Hilbert <karsten.hilbert@gmx.net>
2006-01-07 09:06:24 +0000

    - remove audit tables from schema ER diagram

--- gnumed/gnumed/client/doc/make-schemadocs.sh
+++ gnumed/gnumed/client/doc/make-schemadocs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/doc/make-schemadocs.sh,v $
-# $Revision: 1.10 $
+# $Revision: 1.11 $
 # license: GPL
 # author: Karsten.Hilbert@gmx.net
 
@@ -13,11 +13,16 @@ postgresql_autodoc -d $DB -f ~/gm-schemadocs/gnumed-schema -t dot
 postgresql_autodoc -d $DB -f ~/gm-schemadocs/gnumed-schema -t dia
 postgresql_autodoc -d $DB -f ~/gm-schemadocs/gnumed-schema -t zigzag.dia
 
-dot -Tpng -o ~/gm-schemadocs/gnumed-schema.png ~/gm-schemadocs/gnumed-schema.dot
+grep -v log_ ~/gm-schemadocs/gnumed-schema.dot > ~/gm-schemadocs/gnumed-schema-no_audit.dot
+
+dot -Tpng -o ~/gm-schemadocs/gnumed-schema.png ~/gm-schemadocs/gnumed-schema-no_audit.dot
 
 #============================================
 # $Log: make-schemadocs.sh,v $
-# Revision 1.10  2005-12-23 16:24:18  ncq
+# Revision 1.11  2006-01-07 09:06:24  ncq
+# - remove audit tables from schema ER diagram
+#
+# Revision 1.10  2005/12/23 16:24:18  ncq
 # - remove absolute path prefix on pg autodoc binary
 #
 # Revision 1.9  2005/12/09 20:43:25  ncq
