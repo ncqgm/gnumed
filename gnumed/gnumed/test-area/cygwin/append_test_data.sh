84727470d665f276092e27a2512830510619df18
sjtan <sjtan>
2006-01-10 21:52:49 +0000

    allow for selecting database name differently

--- gnumed/gnumed/test-area/cygwin/append_test_data.sh
+++ gnumed/gnumed/test-area/cygwin/append_test_data.sh
@@ -5,7 +5,8 @@ user=gm-dbo
 target=test_data
 rm  test_data
 touch test_data
-
+#DB=gnumed_v3
+DB=$1
 
 
 python bootstrap-parse.py $confpath/$conf_file filelist-$conf_file
@@ -14,8 +15,12 @@ for y in `cat filelist-$conf_file`;do
 	cat $path/$y >> $target
 done	
 
+echo
+echo running test data
+echo
 
-echo psql -f $target -h 127.0.0.1 -U\"$user\" gnumed
-psql -f $target -h 127.0.0.1 -U"$user" gnumed
+echo psql -f $target -h 127.0.0.1 -U \"$user\" $DB
+
+psql -f $target -h 127.0.0.1 -U "$user" $DB
 
 
