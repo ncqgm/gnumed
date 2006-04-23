3668d0142027e6e85f7ed50c9004ece7d6efb137
Karsten Hilbert <karsten.hilbert@gmx.net>
2006-04-23 15:15:20 +0000

    - add comments, all this should actually be renamed to *_role and gmManageRoleFunctions.sql - add gm_drop_user()

--- gnumed/gnumed/server/sql/gmCreateUserFunction.sql
+++ gnumed/gnumed/server/sql/gmCreateUserFunction.sql
@@ -1,7 +1,7 @@
--- GNUmed public function to create new db users
+-- GNUmed functions to manage database accounts
 
 -- $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/server/sql/gmCreateUserFunction.sql,v $
--- $Id: gmCreateUserFunction.sql,v 1.2 2006-02-02 16:20:08 ncq Exp $
+-- $Id: gmCreateUserFunction.sql,v 1.3 2006-04-23 15:15:20 ncq Exp $
 -- license: GPL
 -- author: Karsten.Hilbert@gmx.net
 -- ===================================================================
@@ -40,12 +40,60 @@ END;';
 revoke all on function gm_create_user(name, text) from public;
 grant execute on function gm_create_user(name, text) to "gm-dbo";
 
+comment on function gm_create_user(name, text) is
+	'To create users one needs to have superuser rights. We do
+	 not want to grant superuser rights to any GNUmed account,
+	 however. Therefore this function is owned by postgres and
+	 is set SECURITY DEFINER. Only gm-dbo is GRANTed EXECUTE.
+	 This way users need to know the gm-dbo (GNUmed admin) password
+	 to execute the function. Later on roles should be used to
+	 limit execution of this function.';
+
 -- ===================================================
---select log_script_insertion('$RCSfile: gmCreateUserFunction.sql,v $', '$Revision: 1.2 $');
+create or replace function gm_drop_user(name)
+	returns boolean
+	language 'plpgsql'
+	security definer
+	as '
+DECLARE
+	_username alias for $1;
+	_query text;
+BEGIN
+	perform 1 from pg_user where usename = _username;
+	if not FOUND then
+		return true;
+	end if;
+	_query := ''drop user '' || quote_ident(_username) || '';'';
+	execute _query;
+	perform 1 from pg_user where usename = _username;
+	if FOUND then
+		return false;
+	end if;
+	return true;
+END;';
+
+revoke all on function gm_drop_user(name) from public;
+grant execute on function gm_drop_user(name) to "gm-dbo";
+
+comment on function gm_drop_user(name) is
+	'To drop users one needs to have superuser rights. We do
+	 not want to grant superuser rights to any GNUmed account,
+	 however. Therefore this function is owned by postgres and
+	 is set SECURITY DEFINER. Only gm-dbo is GRANTed EXECUTE.
+	 This way users need to know the gm-dbo (GNUmed admin) password
+	 to execute the function. Later on roles should be used to
+	 limit execution of this function.';
+
+-- ===================================================
+--select log_script_insertion('$RCSfile: gmCreateUserFunction.sql,v $', '$Revision: 1.3 $');
 
 -- ===================================================
 -- $Log: gmCreateUserFunction.sql,v $
--- Revision 1.2  2006-02-02 16:20:08  ncq
+-- Revision 1.3  2006-04-23 15:15:20  ncq
+-- - add comments, all this should actually be renamed to *_role and gmManageRoleFunctions.sql
+-- - add gm_drop_user()
+--
+-- Revision 1.2  2006/02/02 16:20:08  ncq
 -- - streamline gm_create_user() and enable proper insertion
 --   into database authentication groups
 --
