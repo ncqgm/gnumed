16511d8ba317ee1369eb98d290977db2b63182f0
Karsten Hilbert <karsten.hilbert@gmx.net>
2006-05-12 22:04:22 +0000

    - add _populate_with_data() - fully implement _goto_doc_review()

--- gnumed/gnumed/client/wxpython/gmProviderInboxWidgets.py
+++ gnumed/gnumed/client/wxpython/gmProviderInboxWidgets.py
@@ -2,8 +2,8 @@
 """
 #================================================================
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/wxpython/gmProviderInboxWidgets.py,v $
-# $Id: gmProviderInboxWidgets.py,v 1.3 2006-05-12 12:21:58 ncq Exp $
-__version__ = "$Revision: 1.3 $"
+# $Id: gmProviderInboxWidgets.py,v 1.4 2006-05-12 22:04:22 ncq Exp $
+__version__ = "$Revision: 1.4 $"
 __author__ = "Karsten Hilbert <Karsten.Hilbert@gmx.net>"
 
 #import os.path, sys, re, time
@@ -14,9 +14,9 @@ try:
 except ImportError:
 	from wxPython import wx
 
-from Gnumed.pycommon import gmLog, gmI18N, gmGuiBroker
+from Gnumed.pycommon import gmLog, gmI18N, gmDispatcher, gmSignals
 from Gnumed.business import gmProviderInbox, gmPerson
-from Gnumed.wxpython import gmGuiHelpers, gmPlugin
+from Gnumed.wxpython import gmGuiHelpers
 from Gnumed.wxGladeWidgets import wxgProviderInboxPnl
 
 _log = gmLog.gmDefLog
@@ -35,10 +35,19 @@ class cProviderInboxPnl(wxgProviderInboxPnl.wxgProviderInboxPnl):
 	#--------------------------------------------------------
 	def __init__(self, *args, **kwds):
 		wxgProviderInboxPnl.wxgProviderInboxPnl.__init__(self, *args, **kwds)
-		self.__init_ui_data()
+		self._populate_with_data()
 		cProviderInboxPnl._item_handlers['clinical.review docs'] = self._goto_doc_review
 	#--------------------------------------------------------
-	def __init_ui_data(self):
+	def __reset_ui_data(self):
+		self._LCTRL_provider_inbox.DeleteAllItems()
+		self._LCTRL_provider_inbox.InsertColumn(0, '')
+		self._LCTRL_provider_inbox.InsertColumn(1, _('category'))
+		self._LCTRL_provider_inbox.InsertColumn(2, _('type'))
+		self._LCTRL_provider_inbox.InsertColumn(3, _('message'))
+	#--------------------------------------------------------
+	def _populate_with_data(self):
+		self.__reset_ui_data()
+
 		_me = gmPerson.gmCurrentProvider()
 		msg = _("""
 		Welcome %(title)s %(lname)s !
@@ -47,11 +56,6 @@ class cProviderInboxPnl(wxgProviderInboxPnl.wxgProviderInboxPnl):
 """) % {'title': _me['title'], 'lname': _me['lastnames']}
 
 		self._msg_welcome.SetLabel(msg)
-
-		self._LCTRL_provider_inbox.InsertColumn(0, '')
-		self._LCTRL_provider_inbox.InsertColumn(1, _('category'))
-		self._LCTRL_provider_inbox.InsertColumn(2, _('type'))
-		self._LCTRL_provider_inbox.InsertColumn(3, _('message'))
 
 		inbox = gmProviderInbox.cProviderInbox()
 		self.__msgs = inbox.get_messages()
@@ -87,7 +91,6 @@ Leaving message in inbox.""") % handler_key,
 		if not handle_item(pk_context = msg[6]):
 			print "handler returned False"
 			return False
-		print "seems successfully handled"
 		return True
 	#--------------------------------------------------------
 	def _lst_item_focused(self, evt):
@@ -99,16 +102,25 @@ Leaving message in inbox.""") % handler_key,
 		self._TXT_inbox_item_comment.SetValue(tmp)
 	#--------------------------------------------------------
 	def _goto_doc_review(self, pk_context=None):
-		pat = gmPerson.set_active_patient(patient=gmPerson.cIdentity(aPK_obj=pk_context))
-		gmPlugin.raise_notebook_plugin('gmShowMedDocs')
-
-		gb = gmGuiBroker.GuiBroker()
-		print gb.itemlist()
-
-		return False
+		if not gmPerson.set_active_patient(patient=gmPerson.cIdentity(aPK_obj=pk_context)):
+			gmGuiHelpers.gm_show_error (
+				_('Supposedly there are unreviewed documents'
+				'for patient [%s]. However, I cannot find'
+				'that patient in the GNUmed database.'
+				) % pk_context,
+				_('handling provider inbox item'),
+				gmLog.lErr
+			)
+			return False
+		gmDispatcher.send(gmSignals.display_widget(), name='gmShowMedDocs', sort_mode='review')
+		return True
 #============================================================
 # $Log: gmProviderInboxWidgets.py,v $
-# Revision 1.3  2006-05-12 12:21:58  ncq
+# Revision 1.4  2006-05-12 22:04:22  ncq
+# - add _populate_with_data()
+# - fully implement _goto_doc_review()
+#
+# Revision 1.3  2006/05/12 12:21:58  ncq
 # - implement double-click item handling
 # - use gmCurrentProvider
 # - show message on item focused
