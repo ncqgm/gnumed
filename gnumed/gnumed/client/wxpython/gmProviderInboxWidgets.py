1ec4f522f970076fe2f945a84a436b31b719482e
Karsten Hilbert <karsten.hilbert@gmx.net>
2006-05-12 12:21:58 +0000

    - implement double-click item handling - use gmCurrentProvider - show message on item focused

--- gnumed/gnumed/client/wxpython/gmProviderInboxWidgets.py
+++ gnumed/gnumed/client/wxpython/gmProviderInboxWidgets.py
@@ -2,8 +2,8 @@
 """
 #================================================================
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/wxpython/gmProviderInboxWidgets.py,v $
-# $Id: gmProviderInboxWidgets.py,v 1.2 2006-01-22 18:10:52 ncq Exp $
-__version__ = "$Revision: 1.2 $"
+# $Id: gmProviderInboxWidgets.py,v 1.3 2006-05-12 12:21:58 ncq Exp $
+__version__ = "$Revision: 1.3 $"
 __author__ = "Karsten Hilbert <Karsten.Hilbert@gmx.net>"
 
 #import os.path, sys, re, time
@@ -14,17 +14,13 @@ try:
 except ImportError:
 	from wxPython import wx
 
-from Gnumed.pycommon import gmLog, gmI18N, gmWhoAmI
-#, gmCfg, gmPG, gmMimeLib, gmExceptions
-from Gnumed.business import gmProviderInbox
-# gmPerson, gmMedDoc
-#from Gnumed.wxpython import gmGuiHelpers
+from Gnumed.pycommon import gmLog, gmI18N, gmGuiBroker
+from Gnumed.business import gmProviderInbox, gmPerson
+from Gnumed.wxpython import gmGuiHelpers, gmPlugin
 from Gnumed.wxGladeWidgets import wxgProviderInboxPnl
 
 _log = gmLog.gmDefLog
 _log.Log(gmLog.lInfo, __version__)
-
-_me = gmWhoAmI.cWhoAmI()
 
 _indicator = {
 	-1: '',
@@ -34,20 +30,21 @@ _indicator = {
 
 #============================================================
 class cProviderInboxPnl(wxgProviderInboxPnl.wxgProviderInboxPnl):
+
+	_item_handlers = {}
+	#--------------------------------------------------------
 	def __init__(self, *args, **kwds):
 		wxgProviderInboxPnl.wxgProviderInboxPnl.__init__(self, *args, **kwds)
 		self.__init_ui_data()
+		cProviderInboxPnl._item_handlers['clinical.review docs'] = self._goto_doc_review
 	#--------------------------------------------------------
 	def __init_ui_data(self):
-		msg = _(
-"""
-	Welcome %(title)s %(lname)s !
+		_me = gmPerson.gmCurrentProvider()
+		msg = _("""
+		Welcome %(title)s %(lname)s !
 
-Below find the currently pending items on your TODO list.
-
-Have a nice day.
-
-""") % {'title': _me.get_staff_title(), 'lname': _me.get_lastname()}
+	Below find the new messages in your Inbox.
+""") % {'title': _me['title'], 'lname': _me['lastnames']}
 
 		self._msg_welcome.SetLabel(msg)
 
@@ -56,8 +53,10 @@ Have a nice day.
 		self._LCTRL_provider_inbox.InsertColumn(2, _('type'))
 		self._LCTRL_provider_inbox.InsertColumn(3, _('message'))
 
-		inbox = gmProviderInbox.cProviderInbox(provider_id=_me.get_staff_ID())
-		msgs = inbox.get_messages()
+		inbox = gmProviderInbox.cProviderInbox()
+		self.__msgs = inbox.get_messages()
+		msgs = self.__msgs[0:]
+		msgs.reverse()
 		for msg in msgs:
 			item_idx = self._LCTRL_provider_inbox.InsertItem(info=wx.ListItem())
 			self._LCTRL_provider_inbox.SetStringItem(index = item_idx, col=0, label=_indicator[msg[0]])
@@ -65,9 +64,56 @@ Have a nice day.
 			self._LCTRL_provider_inbox.SetStringItem(index = item_idx, col=2, label=msg[2])
 			self._LCTRL_provider_inbox.SetStringItem(index = item_idx, col=3, label=msg[3])
 
+		self._LCTRL_provider_inbox.SetColumnWidth(col=3, width=wx.LIST_AUTOSIZE)
+	#--------------------------------------------------------
+	def _lst_item_activated(self, evt):
+		msg = self.__msgs[evt.m_itemIndex]
+		handler_key = '%s.%s' % (msg[1], msg[2])
+		try:
+			handle_item = cProviderInboxPnl._item_handlers[handler_key]
+		except KeyError:
+			gmGuiHelpers.gm_show_warning (
+				_(
+"""Unknown message type:
+
+ [%s]
+
+Don't know what to do with it.
+Leaving message in inbox.""") % handler_key,
+				_('handling provider inbox item'),
+				gmLog.lWarn
+			)
+			return False
+		if not handle_item(pk_context = msg[6]):
+			print "handler returned False"
+			return False
+		print "seems successfully handled"
+		return True
+	#--------------------------------------------------------
+	def _lst_item_focused(self, evt):
+		msg = self.__msgs[evt.m_itemIndex]
+		if msg[7] is None:
+			tmp = _('Message: %s') % msg[3]
+		else:
+			tmp = _('Message: %s\nData: %s') % (msg[3], msg[7])
+		self._TXT_inbox_item_comment.SetValue(tmp)
+	#--------------------------------------------------------
+	def _goto_doc_review(self, pk_context=None):
+		pat = gmPerson.set_active_patient(patient=gmPerson.cIdentity(aPK_obj=pk_context))
+		gmPlugin.raise_notebook_plugin('gmShowMedDocs')
+
+		gb = gmGuiBroker.GuiBroker()
+		print gb.itemlist()
+
+		return False
 #============================================================
 # $Log: gmProviderInboxWidgets.py,v $
-# Revision 1.2  2006-01-22 18:10:52  ncq
+# Revision 1.3  2006-05-12 12:21:58  ncq
+# - implement double-click item handling
+# - use gmCurrentProvider
+# - show message on item focused
+#
+# Revision 1.2  2006/01/22 18:10:52  ncq
 # - now really display messages from backend
 #
 # Revision 1.1  2006/01/15 14:30:56  ncq
