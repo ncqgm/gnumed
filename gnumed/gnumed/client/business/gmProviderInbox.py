18e60c3a06600493ed2e71378432e76ad4057346
Karsten Hilbert <karsten.hilbert@gmx.net>
2006-05-12 12:04:30 +0000

    - use gmCurrentProvider - load more fields from inbox for later use

--- gnumed/gnumed/client/business/gmProviderInbox.py
+++ gnumed/gnumed/client/business/gmProviderInbox.py
@@ -5,25 +5,33 @@ This should eventually end up in a class cPractice.
 """
 #============================================================
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/business/gmProviderInbox.py,v $
-# $Id: gmProviderInbox.py,v 1.1 2006-01-22 18:10:21 ncq Exp $
+# $Id: gmProviderInbox.py,v 1.2 2006-05-12 12:04:30 ncq Exp $
 __license__ = "GPL"
-__version__ = "$Revision: 1.1 $"
+__version__ = "$Revision: 1.2 $"
 __author__ = "K.Hilbert <Karsten.Hilbert@gmx.net>"
 
 from Gnumed.pycommon import gmPG
+from Gnumed.business import gmPerson
 
 #============================================================
 class cProviderInbox:
-	def __init__(self, provider_id):
-		self.__provider_id = provider_id
-
+	def __init__(self, provider_id=None):
+		if provider_id is None:
+			self.__provider_id = gmPerson.gmCurrentProvider['pk_staff']
+		else:
+			self.__provider_id = provider_id
+	#--------------------------------------------------------
 	def get_messages(self):
 		cmd = """
 select
 	importance,
 	l10n_category,
 	l10n_type,
-	comment
+	comment,
+	type,
+	category,
+	pk_context,
+	data
 from dem.v_provider_inbox vpi
 where pk_staff = %s
 order by importance desc"""
@@ -34,13 +42,17 @@ order by importance desc"""
 
 #============================================================
 if __name__ == '__main__':
-	inbox = cProviderInbox(provider_id=1)
+	inbox = cProviderInbox()
 	for msg in inbox.get_messages():
 		print msg
 
 #============================================================
 # $Log: gmProviderInbox.py,v $
-# Revision 1.1  2006-01-22 18:10:21  ncq
+# Revision 1.2  2006-05-12 12:04:30  ncq
+# - use gmCurrentProvider
+# - load more fields from inbox for later use
+#
+# Revision 1.1  2006/01/22 18:10:21  ncq
 # - class for provider inbox
 #
 #
