05d1824ef84d35632347347c56cbd568ad7d9206
Karsten Hilbert <karsten.hilbert@gmx.net>
2006-06-06 20:54:36 +0000

    - add staff member delisting dialog

--- gnumed/gnumed/client/wxpython/gmStaffWidgets.py
+++ gnumed/gnumed/client/wxpython/gmStaffWidgets.py
@@ -7,8 +7,8 @@ to anybody else.
 """
 #=========================================================================
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/wxpython/gmStaffWidgets.py,v $
-# $Id: gmStaffWidgets.py,v 1.2 2006-03-14 21:31:15 ncq Exp $
-__version__ = "$Revision: 1.2 $"
+# $Id: gmStaffWidgets.py,v 1.3 2006-06-06 20:54:36 ncq Exp $
+__version__ = "$Revision: 1.3 $"
 __author__  = "K. Hilbert <Karsten.Hilbert@gmx.net>"
 __license__ = "GPL (details at http://www.gnu.org)"
 
@@ -21,10 +21,97 @@ except ImportError:
 from Gnumed.pycommon import gmLog, gmPG
 from Gnumed.business import gmPerson
 from Gnumed.wxpython import gmGuiHelpers
-from Gnumed.wxGladeWidgets import wxgAddPatientAsStaffDlg
+from Gnumed.wxGladeWidgets import wxgAddPatientAsStaffDlg, wxgDelistStaffMemberDlg
 
 _log = gmLog.gmDefLog
 _log.Log(gmLog.lData, __version__)
+#==========================================================================
+class cDelistStaffMemberDlg(wxgDelistStaffMemberDlg.wxgDelistStaffMemberDlg):
+
+	def __init__(self, *args, **kwds):
+		wxgDelistStaffMemberDlg.wxgDelistStaffMemberDlg.__init__(self, *args, **kwds)
+		self.__init_ui_data()
+	#--------------------------------------------------------
+	# internal API
+	#--------------------------------------------------------
+	def __init_ui_data(self):
+		self._LCTRL_staff.InsertColumn(0, _('Alias'))
+		self._LCTRL_staff.InsertColumn(1, _('DB account'))
+		self._LCTRL_staff.InsertColumn(2, _('Role'))
+		self._LCTRL_staff.InsertColumn(3, _('Name'))
+		self._LCTRL_staff.InsertColumn(4, _('Comment'))
+
+		self._LCTRL_staff.DeleteAllItems()
+		staff_list = gmPerson.get_staff_list(active_only=True)
+		for staff in staff_list:
+			row_num = self._LCTRL_staff.InsertStringItem(0, label=staff['short_alias'])
+			self._LCTRL_staff.SetStringItem(index = row_num, col = 1, label = staff['db_user'])
+			self._LCTRL_staff.SetStringItem(index = row_num, col = 2, label = staff['role'])
+			self._LCTRL_staff.SetStringItem(index = row_num, col = 3, label = '%s %s, %s' % (staff['title'], staff['lastnames'], staff['firstnames']))
+			self._LCTRL_staff.SetStringItem(index = row_num, col = 4, label = staff['comment'])
+			self._LCTRL_staff.SetItemData(item = row_num, data = staff['pk_staff'])
+
+		if len(staff_list) > 0:
+			self._LCTRL_staff.SetColumnWidth(col=0, width=wx.LIST_AUTOSIZE)
+			self._LCTRL_staff.SetColumnWidth(col=1, width=wx.LIST_AUTOSIZE_USEHEADER)
+			self._LCTRL_staff.SetColumnWidth(col=2, width=wx.LIST_AUTOSIZE)
+			self._LCTRL_staff.SetColumnWidth(col=3, width=wx.LIST_AUTOSIZE)
+			self._LCTRL_staff.SetColumnWidth(col=4, width=wx.LIST_AUTOSIZE)
+
+		self._btn_delete_staff.Enable(False)
+	#--------------------------------------------------------
+	def _on_listitem_selected(self, evt):
+		self._btn_delete_staff.Enable(True)
+	#--------------------------------------------------------
+	def _on_listitem_deselected(self, evt):
+		self._btn_delete_staff.Enable(False)
+	#--------------------------------------------------------
+	def _on_delist_button_pressed(self, evt):
+		pk = self._LCTRL_staff.GetItemData(self._LCTRL_staff.GetFirstSelected())
+
+		# 1) inactivate staff entry
+		staff = gmPerson.cStaff(aPK_obj=pk)
+		staff['is_active'] = False
+		staff.save_payload()				# FIXME: error handling
+
+		# 2) disable database account login
+		# - get password for gm-dbo
+		pwd_gm_dbo = wx.GetPasswordFromUser (
+			message = _("""
+To add a new staff member to the database we
+need the password of the GNUmed database owner.
+
+Please enter the password for <gm-dbo>:"""),
+			caption = _('Delisting GNUmed staff member'),
+			parent = self
+		)
+		# - connect as gm-dbo
+		pool = gmPG.ConnectionPool()
+		conn = pool.get_connection_for_user(user='gm-dbo', password=pwd_gm_dbo, extra_verbose=True)
+		if conn is None:
+			gmGuiHelpers.gm_show_error (
+				aMessage = _('Cannot connect as the GNUmed database user <gm-dbo>.\n\nTherefore cannot disable staff member.'),
+				aTitle = _('Disabling GNUmed staff member'),
+				aLogLevel = gmLog.lErr
+			)
+			return False
+		# - run disable query
+		queries = [('select gm_disable_user(%s)', [staff['db_user']])]
+		success, data = gmPG.run_commit2 (
+			link_obj = conn,
+			queries = queries,
+			end_tx = True
+		)
+		if not success:
+			gmGuiHelpers.gm_show_error (
+				aMessage = _('Failed to disable GNUmed database user.'),
+				aTitle = _('Disabling GNUmed staff member'),
+				aLogLevel = gmLog.lErr
+			)
+			return False
+
+		self.__init_ui_data()
+		return True
 #==========================================================================
 class cAddPatientAsStaffDlg(wxgAddPatientAsStaffDlg.wxgAddPatientAsStaffDlg):
 
@@ -104,7 +191,10 @@ Please enter the password for <gm-dbo>:"""),
 		self.Close()
 #==========================================================================
 # $Log: gmStaffWidgets.py,v $
-# Revision 1.2  2006-03-14 21:31:15  ncq
+# Revision 1.3  2006-06-06 20:54:36  ncq
+# - add staff member delisting dialog
+#
+# Revision 1.2  2006/03/14 21:31:15  ncq
 # - add event handlers for buttons
 # - actually implement adding a new provider
 #
