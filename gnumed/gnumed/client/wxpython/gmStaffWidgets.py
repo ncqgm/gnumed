92024b3a0048684664a89c14f65ff83d15c29304
Karsten Hilbert <karsten.hilbert@gmx.net>
2006-06-09 14:43:02 +0000

    - improve staff member handling

--- gnumed/gnumed/client/wxpython/gmStaffWidgets.py
+++ gnumed/gnumed/client/wxpython/gmStaffWidgets.py
@@ -7,8 +7,8 @@ to anybody else.
 """
 #=========================================================================
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/wxpython/gmStaffWidgets.py,v $
-# $Id: gmStaffWidgets.py,v 1.3 2006-06-06 20:54:36 ncq Exp $
-__version__ = "$Revision: 1.3 $"
+# $Id: gmStaffWidgets.py,v 1.4 2006-06-09 14:43:02 ncq Exp $
+__version__ = "$Revision: 1.4 $"
 __author__  = "K. Hilbert <Karsten.Hilbert@gmx.net>"
 __license__ = "GPL (details at http://www.gnu.org)"
 
@@ -35,20 +35,35 @@ class cDelistStaffMemberDlg(wxgDelistStaffMemberDlg.wxgDelistStaffMemberDlg):
 	# internal API
 	#--------------------------------------------------------
 	def __init_ui_data(self):
+		lbl_active = {True: _('active'), False: _('inactive')}
+		lbl_login = {True: _('can login'), False: _('can not login')}
+
 		self._LCTRL_staff.InsertColumn(0, _('Alias'))
 		self._LCTRL_staff.InsertColumn(1, _('DB account'))
 		self._LCTRL_staff.InsertColumn(2, _('Role'))
 		self._LCTRL_staff.InsertColumn(3, _('Name'))
 		self._LCTRL_staff.InsertColumn(4, _('Comment'))
+		self._LCTRL_staff.InsertColumn(5, _('Status'))
 
 		self._LCTRL_staff.DeleteAllItems()
-		staff_list = gmPerson.get_staff_list(active_only=True)
+		staff_list = gmPerson.get_staff_list()
+		pos = len(staff_list) + 1
 		for staff in staff_list:
-			row_num = self._LCTRL_staff.InsertStringItem(0, label=staff['short_alias'])
+			row_num = self._LCTRL_staff.InsertStringItem(pos, label=staff['short_alias'])
 			self._LCTRL_staff.SetStringItem(index = row_num, col = 1, label = staff['db_user'])
 			self._LCTRL_staff.SetStringItem(index = row_num, col = 2, label = staff['role'])
 			self._LCTRL_staff.SetStringItem(index = row_num, col = 3, label = '%s %s, %s' % (staff['title'], staff['lastnames'], staff['firstnames']))
 			self._LCTRL_staff.SetStringItem(index = row_num, col = 4, label = staff['comment'])
+			self._LCTRL_staff.SetStringItem(index = row_num, col = 5, label = '%s / %s' % (lbl_active[bool(staff['is_active'])], lbl_login[bool(staff['can_login'])]))
+			# color
+			if staff['is_active'] and staff['can_login']:
+				#self._LCTRL_staff.SetItemTextColour(row_num, col=wx.NamedColour('BLUE'))
+				pass
+			elif not staff['is_active'] and not staff['can_login']:
+				self._LCTRL_staff.SetItemTextColour(row_num, col=wx.LIGHT_GREY)
+			else:
+				self._LCTRL_staff.SetItemTextColour(row_num, col=wx.NamedColour('RED'))
+			# data
 			self._LCTRL_staff.SetItemData(item = row_num, data = staff['pk_staff'])
 
 		if len(staff_list) > 0:
@@ -57,6 +72,7 @@ class cDelistStaffMemberDlg(wxgDelistStaffMemberDlg.wxgDelistStaffMemberDlg):
 			self._LCTRL_staff.SetColumnWidth(col=2, width=wx.LIST_AUTOSIZE)
 			self._LCTRL_staff.SetColumnWidth(col=3, width=wx.LIST_AUTOSIZE)
 			self._LCTRL_staff.SetColumnWidth(col=4, width=wx.LIST_AUTOSIZE)
+			self._LCTRL_staff.SetColumnWidth(col=5, width=wx.LIST_AUTOSIZE)
 
 		self._btn_delete_staff.Enable(False)
 	#--------------------------------------------------------
@@ -191,7 +207,10 @@ Please enter the password for <gm-dbo>:"""),
 		self.Close()
 #==========================================================================
 # $Log: gmStaffWidgets.py,v $
-# Revision 1.3  2006-06-06 20:54:36  ncq
+# Revision 1.4  2006-06-09 14:43:02  ncq
+# - improve staff member handling
+#
+# Revision 1.3  2006/06/06 20:54:36  ncq
 # - add staff member delisting dialog
 #
 # Revision 1.2  2006/03/14 21:31:15  ncq
