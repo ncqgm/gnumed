dcf852b28e9d18aa72b7d2bc7c31aa757c6e1e9c
Karsten Hilbert <karsten.hilbert@gmx.net>
2006-06-15 20:52:40 +0000

    - clean *.py~, too

--- gnumed/remove_pyc.sh
+++ gnumed/remove_pyc.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/remove_pyc.sh,v $
-# $Revision: 1.1 $
+# $Revision: 1.2 $
 
 echo "cleaning out debris"
 find ./ -name '*.pyc' -exec rm -v '{}' ';'
+find ./ -name '*.py~' -exec rm -v '{}' ';'
