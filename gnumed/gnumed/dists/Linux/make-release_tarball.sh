aaac469438daebb8bd8ba2d1d0d792ede7a3eb4b
Karsten Hilbert <karsten.hilbert@gmx.net>
2006-07-19 20:03:35 +0000

    - improved client packages

--- gnumed/gnumed/dists/Linux/make-release_tarball.sh
+++ gnumed/gnumed/dists/Linux/make-release_tarball.sh
@@ -2,104 +2,166 @@
 
 #====================================================
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/dists/Linux/make-release_tarball.sh,v $
-# $Id: make-release_tarball.sh,v 1.1 2006-07-19 11:31:17 ncq Exp $
+# $Id: make-release_tarball.sh,v 1.2 2006-07-19 20:03:35 ncq Exp $
 # license: GPL
 #====================================================
 REV=0.2
 
 CLIENT_FILES_REMOVE=\
-"../../client/business/gmForms.py "\
-"../../client/business/README "\
-"../../client/business/.py "\
-"../../client/business/.py "\
-"../../client/business/.py "\
-"../../client/business/.py "\
-"../../client/business/.py "\
-"../../client/business/.py "\
-"../../client/business/.py "\
-" "\
-" "\
-" "\
-" "\
-" "\
-" "\
-" "\
-" "\
-" "\
-" "\
-" "\
-" "\
-" "\
-" "\
+"./GNUmed-$REV/client/business/README "\
+"./GNUmed-$REV/client/business/gmForms.py "\
+"./GNUmed-$REV/client/business/gmOrganization.py "\
+"./GNUmed-$REV/client/business/gmXmlDocDesc.py "\
+"./GNUmed-$REV/client/pycommon/gmDrugObject.py "\
+"./GNUmed-$REV/client/pycommon/gmDrugView.py "\
+"./GNUmed-$REV/client/pycommon/gmSchemaRevisionCheck.py "\
+"./GNUmed-$REV/client/pycommon/gmSerialTools.py "\
+"./GNUmed-$REV/client/pycommon/gmTrace.py "\
+"./GNUmed-$REV/client/pycommon/gmdbf.py "\
+"./GNUmed-$REV/client/wxGladeWidgets/README.py "\
+"./GNUmed-$REV/client/wxGladeWidgets/wxgAU_AdminLoginV01.py "\
+"./GNUmed-$REV/client/wxGladeWidgets/wxgAU_DBUserSetupV01.py "\
+"./GNUmed-$REV/client/wxGladeWidgets/wxgAU_StaffMgrPanel.py "\
+"./GNUmed-$REV/client/wxGladeWidgets/wxgAU_StaffV01.py "\
+"./GNUmed-$REV/client/wxGladeWidgets/wxgRequest.py "\
+"./GNUmed-$REV/client/wxpython/gmAU_VaccV01.py "\
+"./GNUmed-$REV/client/wxpython/gmAllergyWidgets.py "\
+"./GNUmed-$REV/client/wxpython/gmCharacterValidator.py "\
+"./GNUmed-$REV/client/wxpython/gmCryptoText.py "\
+"./GNUmed-$REV/client/wxpython/gmFormPrinter.py "\
+"./GNUmed-$REV/client/wxpython/gmGP_ActiveProblems.py "\
+"./GNUmed-$REV/client/wxpython/gmGP_FamilyHistorySummary.py "\
+"./GNUmed-$REV/client/wxpython/gmGP_HabitsRiskFactors.py "\
+"./GNUmed-$REV/client/wxpython/gmGP_Inbox.py "\
+"./GNUmed-$REV/client/wxpython/gmGP_PatientPicture.py "\
+"./GNUmed-$REV/client/wxpython/gmGP_SocialHistory.py "\
+"./GNUmed-$REV/client/wxpython/gmLabWidgets.py "\
+"./GNUmed-$REV/client/wxpython/gmListCtrlMapper.py "\
+"./GNUmed-$REV/client/wxpython/gmMultiColumnList.py.py "\
+"./GNUmed-$REV/client/wxpython/gmMultiSash.py "\
+"./GNUmed-$REV/client/wxpython/gmPatientHolder.py "\
+"./GNUmed-$REV/client/wxpython/gmPlugin_Patient.py "\
+"./GNUmed-$REV/client/wxpython/gmPregWidgets.py "\
+"./GNUmed-$REV/client/wxpython/gmSelectPerson.py "\
+"./GNUmed-$REV/client/wxpython/gmShadow.py "\
+"./GNUmed-$REV/client/wxpython/gmSQLListControl.py "\
+"./GNUmed-$REV/client/wxpython/gmSQLSimpleSearch.py "\
+"./GNUmed-$REV/client/wxpython/gmAllergiesPlugin.py "\
+"./GNUmed-$REV/client/wxpython/gmAU_VaccV01Plugin.py "\
+"./GNUmed-$REV/client/wxpython/gmClinicalWindowManager.py "\
+"./GNUmed-$REV/client/wxpython/gmContacts.py "\
+"./GNUmed-$REV/client/wxpython/gmDemographicsEditor.py "\
+"./GNUmed-$REV/client/wxpython/gmDrugDisplay.py "\
+"./GNUmed-$REV/client/wxpython/gmEMRTextDumpPlugin.py "\
+"./GNUmed-$REV/client/wxpython/gmGuidelines.py "\
+"./GNUmed-$REV/client/wxpython/gmLabJournal.py "\
+"./GNUmed-$REV/client/wxpython/gmMultiSashedProgressNoteInputPlugin.py "\
+"./GNUmed-$REV/client/wxpython/gmOffice.py "\
+"./GNUmed-$REV/client/wxpython/gmPython.py "\
+"./GNUmed-$REV/client/wxpython/gmRequest.py "\
+"./GNUmed-$REV/client/wxpython/gmShowLab.py "\
+"./GNUmed-$REV/client/wxpython/gmSnellen.py "\
+"./GNUmed-$REV/client/wxpython/gmSQL.py "\
+"./GNUmed-$REV/client/wxpython/gmStikoBrowser.py "\
+"./GNUmed-$REV/client/wxpython/gmVaccinationsPlugin.py "\
+"./GNUmed-$REV/client/wxpython/gmXdtViewer.py "
+
 
 echo "cleaning up"
 rm -R ./GNUmed-$REV/*.*
+rm -vf GNUmed-client.$REV.tgz
+cd ../../
+./remove_pyc.sh
+cd -
 
 # create client package
 echo "____________"
 echo "=> client <="
 echo "============"
 
+# client
 mkdir -p ./GNUmed-$REV/client/
+cp -R ../../client/__init__.py ./GNUmed-$REV/client/
+cp -R ../../client/gm-0_2.conf ./GNUmed-$REV/client/
+cp -R ../../client/gm-0_2-from-cvs.sh ./GNUmed-$REV/client/
+cp -R ./gnumed.xpm ./GNUmed-$REV/client/
+cp -R ./gnumed ./GNUmed-$REV/client/
+cp -R ../../client/sitecustomize.py ./GNUmed-$REV/client/
+cp -R ../../../check-prerequisites.* ./GNUmed-$REV/client/
+cp -R ../../../GnuPublicLicense.txt ./GNUmed-$REV/client/
 
-cp -R ../../client/business ./GNUmed-$REV/client/
-cp -R ../../client/etc/gnumed.conf.example ./GNUmed-$REV/client/
+# bitmaps
+mkdir -p ./GNUmed-$REV/client/bitmaps/
+cp -R ../../client/bitmaps/bmi_calculator.png ./GNUmed-$REV/client/bitmaps/
+cp -R ../../client/bitmaps/any_body2.png ./GNUmed-$REV/client/bitmaps/
+cp -R ../../client/bitmaps/gnumedlogo.png ./GNUmed-$REV/client/bitmaps/
+cp -R ../../client/bitmaps/empty-face-in-bust.png ./GNUmed-$REV/client/bitmaps/
+cp -R ../../client/bitmaps/serpent.png ./GNUmed-$REV/client/bitmaps/
 
-#mkdir -p ./GNUmed-$REV/client/usr/share/gnumed/
-#mkdir -p ./GNUmed-$REV/client/usr/share/doc/gnumed/client
-#mkdir -p ./GNUmed-$REV/client/usr/lib/python/site-packages/Gnumed/exporters/
-#mkdir -p ./GNUmed-$REV/client/usr/lib/python/site-packages/Gnumed/importers/
-#mkdir -p ./GNUmed-$REV/client/etc/gnumed/
-#mkdir -p ./GNUmed-$REV/client/usr/lib/python/site-packages/Gnumed/
-#mkdir -p ./GNUmed-$REV/client/usr/share/gnumed/pixmaps/
-#mkdir -p ./GNUmed-$REV/client/usr/bin/
-#mkdir -p ./GNUmed-$REV/client/usr/share/locale/de/LC_MESSAGES/
-#mkdir -p ./GNUmed-$REV/client/usr/share/locale/de_DE/LC_MESSAGES/
-#mkdir -p ./GNUmed-$REV/client/usr/share/locale/fr/LC_MESSAGES/
-#mkdir -p ./GNUmed-$REV/client/usr/share/locale/fr_FR/LC_MESSAGES/
-#mkdir -p ./GNUmed-$REV/client/usr/share/locale/es/LC_MESSAGES/
-#mkdir -p ./GNUmed-$REV/client/usr/share/locale/es_ES/LC_MESSAGES/
+# business
+mkdir -p ./GNUmed-$REV/client/business/
+cp -R ../../client/business/*.py ./GNUmed-$REV/client/business/
 
+# doc
+mkdir -p ./GNUmed-$REV/client/doc/
+cp -R ../../client/doc/gnumed.conf.example ./GNUmed-$REV/client/doc/
+cp -R ../../client/doc/man-pages/gnumed.1 ./GNUmed-$REV/client/doc/gnumed.1
 
+# exporters
+mkdir -p ./GNUmed-$REV/client/exporters/
+cp -R ../../client/exporters/__init__.py ./GNUmed-$REV/client/exporters
+cp -R ../../client/exporters/gmPatientExporter.py ./GNUmed-$REV/client/exporters
 
+# locale
+mkdir -p ./GNUmed-$REV/client/locale/
+cp -R ../../client/locale/de.po ./GNUmed-$REV/client/locale
+cp -R ../../client/locale/es.po ./GNUmed-$REV/client/locale
+cp -R ../../client/locale/fr.po ./GNUmed-$REV/client/locale
 
-cp -R ../../server/ ./GNUmed-$REV/
-#cp -R ../../client/bitmaps ./GNUmed-$REV/client/bitmaps
-#cp -R ../../client/business ./GNUmed-$REV/client/business
-#cp -R ../../client/doc/medical_knowledge ./GNUmed-$REV/client/doc/medical_knowledge
-#cp -R ../../client/exporters/*.py ./GNUmed-$REV/client/exporters/*.py
-#cp -R ../../client/importers/*.py ./GNUmed-$REV/client/importers/*.py
-#cp -R ../../client/exporters/gmPatientExporter.conf ./GNUmed-$REV/client/exporters/gmPatientExporter.conf
-#cp -R ../../client/gm-0_1.conf ./GNUmed-$REV/client/gm-0_1.conf
-#cp -R ../../client/pycommon ./GNUmed-$REV/client/pycommon
-#cp -R ../../client/wxpython ./GNUmed-$REV/client/wxpython
-#cp -R ../../client/sitecustomize.py ./GNUmed-$REV/client/sitecustomize.py
-#cp -R ../../client/__init__.py ./GNUmed-$REV/client/__init__.py
-#cp -R ./gnumed.xpm ./GNUmed-$REV/client/gnumed.xpm
-cp -R ./gnumed ./GNUmed-$REV/client/gnumed
-
-# copy user manual from wiki
-echo "downloading Manual zip file from the web"
-rm -vf Main.TWikiGuest_Gnumed.zip
-wget -v http://salaam.homeunix.com/gm-manual/Main.TWikiGuest_Gnumed.zip
-unzip Main.TWikiGuest_Gnumed.zip -d ./GNUmed-$REV/user-manual
-cd ./GNUmed-$REV/client/user-manual/
-ln -s Release-01.html index.html
-cd -
-
-# build up2date *.po and *.mo language files
-cd ./GNUmed-$REV/client/locale/
+cd ../../client/locale/
 ./create-gnumed_mo.sh de
 ./create-gnumed_mo.sh es
 ./create-gnumed_mo.sh fr
 cd -
 
-#cp ../../client/locale/de-gnumed.mo ./GNUmed-$REV/locale/de/LC_MESSAGES/gnumed.mo
-#cp ../../client/locale/de-gnumed.mo ./GNUmed-$REV/locale/de_DE/LC_MESSAGES/gnumed.mo
-#cp ../../client/locale/fr-gnumed.mo ./GNUmed-$REV/locale/fr/LC_MESSAGES/gnumed.mo
-#cp ../../client/locale/fr-gnumed.mo ./GNUmed-$REV/locale/fr_FR/LC_MESSAGES/gnumed.mo
-#cp ../../client/locale/es-gnumed.mo ./GNUmed-$REV/locale/es/LC_MESSAGES/gnumed.mo
-#cp ../../client/locale/es-gnumed.mo ./GNUmed-$REV/locale/es_ES/LC_MESSAGES/gnumed.mo
+cp -R ../../client/locale/de-gnumed.mo ./GNUmed-$REV/client/locale
+cp -R ../../client/locale/es-gnumed.mo ./GNUmed-$REV/client/locale
+cp -R ../../client/locale/fr-gnumed.mo ./GNUmed-$REV/client/locale
+
+# pycommon
+mkdir -p ./GNUmed-$REV/client/pycommon/
+cp -R ../../client/pycommon/*.py ./GNUmed-$REV/client/pycommon/
+
+# wxGladeWidgets
+mkdir -p ./GNUmed-$REV/client/wxGladeWidgets/
+cp -R ../../client/wxGladeWidgets/*.py ./GNUmed-$REV/client/wxGladeWidgets/
+
+# wxpython
+mkdir -p ./GNUmed-$REV/client/wxpython/
+cp -R ../../client/wxpython/*.py ./GNUmed-$REV/client/wxpython/
+mkdir -p ./GNUmed-$REV/client/wxpython/gui/
+cp -R ../../client/wxpython/*.py ./GNUmed-$REV/client/wxpython/gui/
+
+
+# cleanup
+for fname in $CLIENT_FILES_REMOVE ; do
+	rm -vf $fname
+done ;
+
+
+# copy user manual from wiki
+#echo "downloading Manual zip file from the web"
+#rm -vf Main.TWikiGuest_Gnumed.zip
+#wget -v http://salaam.homeunix.com/gm-manual/Main.TWikiGuest_Gnumed.zip
+#unzip Main.TWikiGuest_Gnumed.zip -d ./GNUmed-$REV/user-manual
+#cd ./GNUmed-$REV/client/user-manual/
+#ln -s Release-01.html index.html
+#cd -
+
+# now make tarballs
+tar -cvhzf GNUmed-client.$REV.tgz ./GNUmed-$REV/client/
+cp -vf GNUmed-client.$REV.tgz gnumed-client_$REV.orig.tar.gz			# Debian source package
+
 
 #----------------------------------
 echo "____________"
@@ -131,7 +193,10 @@ find ./GNUmed-$REV/ -name 'wxg' -type d -exec rm -v -r '{}' ';'
 
 #------------------------------------------
 # $Log: make-release_tarball.sh,v $
-# Revision 1.1  2006-07-19 11:31:17  ncq
+# Revision 1.2  2006-07-19 20:03:35  ncq
+# - improved client packages
+#
+# Revision 1.1  2006/07/19 11:31:17  ncq
 # - renamed to better reflect its use
 #
 # Revision 1.1  2006/06/21 21:58:13  shilbert
