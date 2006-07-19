c0e4b2e0c60a3a911c587f9e28146d6fb28d79f1
Karsten Hilbert <karsten.hilbert@gmx.net>
2006-07-19 11:39:00 +0000

    - add here from elsewhere

---
+++ gnumed/gnumed/dists/cvs-snapshot/make-cvs_tarball.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/dists/cvs-snapshot/make-cvs_tarball.sh,v $
+# $Revision: 1.1 $
+# license: GPL
+# author: Karsten.Hilbert@gmx.net
+
+BASE=~/gm-cvs-head
+ARCHIVE=$HOME/public_html/gnumed/snapshot/gnumed-latest-snapshot.tgz
+
+cd $BASE
+tar -cvzf $ARCHIVE gnumed/
+
+#============================================
+# $Log: make-cvs_tarball.sh,v $
+# Revision 1.1  2006-07-19 11:39:00  ncq
+# - add here from elsewhere
+#
+# Revision 1.2  2005/12/27 17:15:52  ncq
+# - backup relative path
+#
+# Revision 1.1  2005/12/27 17:08:06  ncq
+# - make snapshots of the CVS tree
+#
