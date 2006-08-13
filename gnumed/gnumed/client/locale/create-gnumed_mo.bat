f4de94c22d548865802b79c2c7e6e3736b3415bb
shilbert <shilbert>
2006-08-13 11:29:20 +0000

    - ported po --> mo file builder from GNU/Linux to MS Windows

---
+++ gnumed/gnumed/client/locale/create-gnumed_mo.bat
@@ -0,0 +1,22 @@
+@echo off
+
+echo generate a gnumed.mo file from a translated $LANG.po file
+echo first arg has to be ISO language code
+
+rem $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/locale/create-gnumed_mo.bat,v $
+rem $Revision: 1.1 $
+
+SET LANGNAME=%1
+SET MOFILE=%LANGNAME%-gnumed.mo
+
+IF !==!%1 (
+echo You didn't give an ISO language code as the first argument.
+)  ELSE (
+SET POFILE=%LANGNAME%.po
+
+echo generating gnumed.mo for language %LANGNAME% ...
+msgfmt -v -o %MOFILE% %POFILE%
+
+echo You can now copy %MOFILE% into the appropriate language
+echo specific directory such as './%LANGNAME%/LC_MESSAGES/'.
+)
