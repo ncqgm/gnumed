95e37b77c264e022a55bfd727586405a257ee575
Karsten Hilbert <karsten.hilbert@gmx.net>
2006-08-28 10:16:20 +0000

    - --unicode-gettext obsoleted

--- gnumed/gnumed/client/gm-0_2-from-cvs.sh
+++ gnumed/gnumed/client/gm-0_2-from-cvs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/Attic/gm-0_2-from-cvs.sh,v $
-# $Revision: 1.3 $
+# $Revision: 1.3.2.1 $
 
 cd ../
 ln -vfsn client Gnumed
@@ -13,7 +13,6 @@ rm -vf $LOG
 # prepare
 cp -vf gm-0_2.conf tmp-gm-0_2.conf
 python wxpython/gnumed.py --log-file=$LOG --conf-file=tmp-gm-0_2.conf --debug --override-schema-check
-# --unicode-gettext=0
 # clean up
 rm -vf tmp-gm-0_2.conf
 rm -vf tmp-gm-0_2.conf.gmCfg.bak
