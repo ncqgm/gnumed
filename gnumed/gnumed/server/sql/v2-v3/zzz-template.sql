f06b6a099598e4cd672e5213aec58006144d5519
Karsten Hilbert <karsten.hilbert@gmx.net>
2006-09-16 14:02:36 +0000

    - use this as a template for change scripts

---
+++ gnumed/gnumed/server/sql/v2-v3/zzz-template.sql
@@ -0,0 +1,35 @@
+-- ==============================================================
+-- GNUmed database schema change script
+--
+-- Source database version: v2
+-- Target database version: v3
+--
+-- What it does:
+-- - ...
+--
+-- License: GPL
+-- Author: 
+-- 
+-- ==============================================================
+-- $Id: zzz-template.sql,v 1.1 2006-09-16 14:02:36 ncq Exp $
+-- $Revision: 1.1 $
+
+-- --------------------------------------------------------------
+begin;
+-- --------------------------------------------------------------
+
+
+
+
+
+-- --------------------------------------------------------------
+select public.log_script_insertion('$RCSfile: zzz-template.sql,v $', '$Revision: 1.1 $');
+
+commit;
+
+-- ==============================================================
+-- $Log: zzz-template.sql,v $
+-- Revision 1.1  2006-09-16 14:02:36  ncq
+-- - use this as a template for change scripts
+--
+--
