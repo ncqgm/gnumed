6e5d51cc28365a783b44b4387f7a6d1b102e5e14
Karsten Hilbert <karsten.hilbert@gmx.net>
2006-09-21 19:51:43 +0000

    - eventually make set_option() work

--- gnumed/gnumed/server/sql/v2-v3/cfg-set_option.sql
+++ gnumed/gnumed/server/sql/v2-v3/cfg-set_option.sql
@@ -11,8 +11,8 @@
 -- Author: Karsten Hilbert
 -- 
 -- ==============================================================
--- $Id: cfg-set_option.sql,v 1.1 2006-09-19 18:27:47 ncq Exp $
--- $Revision: 1.1 $
+-- $Id: cfg-set_option.sql,v 1.2 2006-09-21 19:51:43 ncq Exp $
+-- $Revision: 1.2 $
 
 -- --------------------------------------------------------------
 \set ON_ERROR_STOP 1
@@ -35,17 +35,17 @@ declare
 	pk_template integer;
 	pk_item integer;
 begin
-	if _owner is NULL then
-		real_owner := ''CURRENT_USER'';
-	else
-		real_owner := _owner;
-	end if;
-
 	-- check template
 	select into pk_template pk from cfg.cfg_template where name = _option and type = _val_type;
-	if not found then
+	if not FOUND then
 		insert into cfg.cfg_template (name, type) values (_option, _val_type);
 		select into pk_template currval(''cfg.cfg_template_pk_seq'');
+	end if;
+
+	if _owner is null then
+		select into real_owner CURRENT_USER;
+	else
+		real_owner := _owner;
 	end if;
 
 	-- check item
@@ -62,17 +62,19 @@ begin
 			workplace = _workplace and
 			cookie = _cookie;
 	end if;
-	if not found then
-		insert into cfg.cfg_item (
-			fk_template, workplace, cookie, owner
-		) values (
-			pk_template,
-			_workplace,
-			_cookie,
-			real_owner
-		);
-		select into pk_item currval(''cfg.cfg_item_pk_seq'');
+	if FOUND then
+		return pk_item;
 	end if;
+
+	insert into cfg.cfg_item (
+		fk_template, workplace, cookie, owner
+	) values (
+		pk_template,
+		_workplace,
+		_cookie,
+		real_owner
+	);
+	select into pk_item currval(''cfg.cfg_item_pk_seq'');
 
 	return pk_item;
 end;';
@@ -91,6 +93,7 @@ declare
 	_owner alias for $5;
 	val_type text;
 	pk_item integer;
+	rows integer;
 	cmd text;
 begin
 	-- determine data type
@@ -100,6 +103,8 @@ begin
 		val_type := ''numeric'';
 	elsif _value is of (bytea) then
 		val_type := ''data'';
+	elsif _value is of (text[]) then
+		val_type := ''str_array'';
 	else
 		raise exception ''cfg.set_option(text, any, text, text, text): invalid type of value'';
 	end if;
@@ -110,14 +115,29 @@ begin
 	-- set item value
 	cmd := ''select 1 from cfg.cfg_'' || val_type || '' where fk_item='' || pk_item || '';'';
 	execute cmd;
-	if found then
-		cmd := ''update cfg.cfg_'' || val_type || '' set value='' || quote_literal(_value) || '' where fk_item='' || pk_item || '';'';
+	get diagnostics rows = row_count;
+	found := rows <> 0;
+
+	if FOUND then
+		if val_type = ''str_array'' then
+			cmd := ''update cfg.cfg_str_array set value=''''{"'' || array_to_string(_value, ''","'') || ''"}'''' where fk_item='' || pk_item || '';'';
+		elsif val_type = ''data'' then
+			cmd := ''update cfg.cfg_data set value='''''' || encode(_value, ''escape'') || '''''' where fk_item='' || pk_item || '';'';
+		else
+			cmd := ''update cfg.cfg_'' || val_type || '' set value='' || quote_literal(_value) || '' where fk_item='' || pk_item || '';'';
+		end if;
 		execute cmd;
-	else
-		cmd := ''insert into cfg.cfg_'' || val_type || '' (fk_item, value) values ('' || pk_item || '', '' || quote_literal(_value) || '');''
-		execute cmd;
+		return True;
 	end if;
 
+	if val_type = ''str_array'' then
+		cmd := ''insert into cfg.cfg_str_array(fk_item, value) values ('' || pk_item || '', ''''{"'' || array_to_string(_value, ''","'') || ''"}'''');'';
+	elsif val_type = ''data'' then
+		cmd := ''insert into cfg.cfg_data(fk_item, value) values ('' || pk_item || '', '''''' || encode(_value, ''escape'') || '''''');'';
+	else
+		cmd := ''insert into cfg.cfg_'' || val_type || '' (fk_item, value) values ('' || pk_item || '', '' || quote_literal(_value) || '');'';
+	end if;
+	execute cmd;
 	return True;
 end;';
 
@@ -125,7 +145,7 @@ comment on function cfg.set_option(text, anyelement, text, text, text) is
 	'set option, owner = NULL means CURRENT_USER';
 
 -- --------------------------------------------------------------
-create or replace function cfg.set_option(text, text[], text, text, text)
+create or replace function cfg.set_option2(text, text[], text, text, text)
 	returns boolean
 	language 'plpgsql'
 	as '
@@ -136,40 +156,42 @@ declare
 	_cookie alias for $4;
 	_owner alias for $5;
 	real_owner text;
-	val_type text;
 	pk_template integer;
 	pk_item integer;
 	cmd text;
 begin
 	-- create template/item if need be
-	select into pk_item cfg.create_cfg_item(_option, val_type, _workplace, _cookie, _owner);
+	select into pk_item cfg.create_cfg_item(_option, ''str_array''::text, _workplace, _cookie, _owner);
 
 	-- set item
 	cmd := ''select 1 from cfg.cfg_str_array where fk_item='' || pk_item || '';'';
 	execute cmd;
 	if found then
-		cmd := ''update cfg.cfg_str_array set value='' || quote_literal(_value) || '' where fk_item='' || pk_item || '';'';
+		cmd := ''update cfg.cfg_str_array set value='' || _value || '' where fk_item='' || pk_item || '';'';
 		execute cmd;
 	else
-		cmd := ''insert into cfg.cfg_str_array(fk_item, value) values ('' || pk_item || '', '' || quote_literal(_value) || '');''
+		cmd := ''insert into cfg.cfg_str_array(fk_item, value) values ('' || pk_item || '', '' || _value || '');''
 		execute cmd;
 	end if;
 
 	return True;
 end;';
 
-comment on function cfg.set_option(text, text[], text, text, text) is
+comment on function cfg.set_option2(text, text[], text, text, text) is
 	'set option, owner = NULL means CURRENT_USER';
 
 -- --------------------------------------------------------------
-select public.log_script_insertion('$RCSfile: cfg-set_option.sql,v $', '$Revision: 1.1 $');
+select public.log_script_insertion('$RCSfile: cfg-set_option.sql,v $', '$Revision: 1.2 $');
 
 -- --------------------------------------------------------------
 commit;
 
 -- ==============================================================
 -- $Log: cfg-set_option.sql,v $
--- Revision 1.1  2006-09-19 18:27:47  ncq
+-- Revision 1.2  2006-09-21 19:51:43  ncq
+-- - eventually make set_option() work
+--
+-- Revision 1.1  2006/09/19 18:27:47  ncq
 -- - add cfg.set_option()
 -- - drop NOT NULL on cfg.cfg_item.cookie
 --
