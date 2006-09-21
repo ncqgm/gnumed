5156a44a93fa441ffa836b9fe530c1d6e8b55377
Karsten Hilbert <karsten.hilbert@gmx.net>
2006-09-21 19:50:08 +0000

    - adjust defaults, constraints and foreign keys

--- gnumed/gnumed/server/sql/v2-v3/cfg-cfg_item.sql
+++ gnumed/gnumed/server/sql/v2-v3/cfg-cfg_item.sql
@@ -11,8 +11,8 @@
 -- Author: 
 -- 
 -- ==============================================================
--- $Id: cfg-cfg_item.sql,v 1.1 2006-09-19 18:27:47 ncq Exp $
--- $Revision: 1.1 $
+-- $Id: cfg-cfg_item.sql,v 1.2 2006-09-21 19:50:08 ncq Exp $
+-- $Revision: 1.2 $
 
 -- --------------------------------------------------------------
 \set ON_ERROR_STOP 1
@@ -22,18 +22,44 @@ begin;
 
 -- --------------------------------------------------------------
 alter table cfg.cfg_item
+	alter column workplace
+		drop not null;
+
+alter table cfg.cfg_item
+	alter column workplace
+		set default null;
+
+
+alter table cfg.cfg_item
 	alter column cookie
 		drop not null;
 
+alter table cfg.cfg_item
+	alter column cookie
+		set default null;
+
+
+alter table cfg.cfg_item
+	drop constraint "$1";
+
+alter table cfg.cfg_item
+	add foreign key (fk_template)
+		references cfg.cfg_template(pk)
+		on update cascade
+		on delete cascade;
+
 -- --------------------------------------------------------------
-select public.log_script_insertion('$RCSfile: cfg-cfg_item.sql,v $', '$Revision: 1.1 $');
+select public.log_script_insertion('$RCSfile: cfg-cfg_item.sql,v $', '$Revision: 1.2 $');
 
 -- --------------------------------------------------------------
 commit;
 
 -- ==============================================================
 -- $Log: cfg-cfg_item.sql,v $
--- Revision 1.1  2006-09-19 18:27:47  ncq
+-- Revision 1.2  2006-09-21 19:50:08  ncq
+-- - adjust defaults, constraints and foreign keys
+--
+-- Revision 1.1  2006/09/19 18:27:47  ncq
 -- - add cfg.set_option()
 -- - drop NOT NULL on cfg.cfg_item.cookie
 --
