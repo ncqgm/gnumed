bf464aa3e06b1402fbaf0473cfca70f002d71660
cvs2git <admin@example.com>
2006-09-23 17:22:57 +0000

    This commit was manufactured by cvs2git to create tag 'client'.

--- gnumed/remove_pyc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/remove_pyc.sh,v $
-# $Revision: 1.3 $
-
-echo "cleaning out debris"
-find ./ -name '*.pyc' -exec rm -v '{}' ';'
-find ./ -name '*.py~' -exec rm -v '{}' ';'
-find ./ -name '*.mo' -exec rm -v '{}' ';'
