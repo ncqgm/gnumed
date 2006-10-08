815983c7ba73f702f581846c907819e25a50bf80
Karsten Hilbert <karsten.hilbert@gmx.net>
2006-10-08 09:05:15 +0000

    - normalize column name

---
+++ gnumed/gnumed/server/sql/v2-v3/static/blobs-doc_obj.sql
@@ -0,0 +1,48 @@
+-- ==============================================================
+-- GNUmed database schema change script
+--
+-- Source database version: v2
+-- Target database version: v3
+--
+-- What it does:
+-- - modify blobs.doc_obj
+--
+-- License: GPL
+-- Author: Karsten Hilbert
+-- 
+-- ==============================================================
+-- $Id: blobs-doc_obj.sql,v 1.1 2006-10-08 09:05:15 ncq Exp $
+-- $Revision: 1.1 $
+
+-- --------------------------------------------------------------
+\set ON_ERROR_STOP 1
+
+-- --------------------------------------------------------------
+begin;
+
+-- --------------------------------------------------------------
+alter table blobs.doc_obj
+	rename column doc_id to fk_doc;
+
+-- --------------------------------------------------------------
+select public.log_script_insertion('$RCSfile: blobs-doc_obj.sql,v $', '$Revision: 1.1 $');
+
+-- --------------------------------------------------------------
+commit;
+
+-- ==============================================================
+-- $Log: blobs-doc_obj.sql,v $
+-- Revision 1.1  2006-10-08 09:05:15  ncq
+-- - normalize column name
+--
+--
+-- Revision 1.3  2006/09/18 17:32:53  ncq
+-- - make more fool-proof
+--
+-- Revision 1.2  2006/09/16 21:47:37  ncq
+-- - improvements
+--
+-- Revision 1.1  2006/09/16 14:02:36  ncq
+-- - use this as a template for change scripts
+--
+--
