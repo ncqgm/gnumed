4384f1b71d1ee118c16f6e3b609dc160bca21e55
Karsten Hilbert <karsten.hilbert@gmx.net>
2006-10-08 09:15:17 +0000

    - type -> fk_type

--- gnumed/gnumed/server/sql/v2-v3/static/blobs-doc_med.sql
+++ gnumed/gnumed/server/sql/v2-v3/static/blobs-doc_med.sql
@@ -11,8 +11,8 @@
 -- Author: Karsten Hilbert
 -- 
 -- ==============================================================
--- $Id: blobs-doc_med.sql,v 1.1 2006-09-26 14:47:53 ncq Exp $
--- $Revision: 1.1 $
+-- $Id: blobs-doc_med.sql,v 1.2 2006-10-08 09:15:17 ncq Exp $
+-- $Revision: 1.2 $
 
 -- --------------------------------------------------------------
 \set ON_ERROR_STOP 1
@@ -21,7 +21,7 @@
 begin;
 
 -- --------------------------------------------------------------
-
+-- patient_id -> fk_identity
 alter table blobs.doc_med
 	drop constraint "$1";
 
@@ -34,15 +34,22 @@ alter table blobs.doc_med
 		on update cascade
 		on delete restrict;
 
+-- type -> fk_type
+alter table blobs.doc_med
+	rename column type to fk_type;
+
 -- --------------------------------------------------------------
-select public.log_script_insertion('$RCSfile: blobs-doc_med.sql,v $', '$Revision: 1.1 $');
+select public.log_script_insertion('$RCSfile: blobs-doc_med.sql,v $', '$Revision: 1.2 $');
 
 -- --------------------------------------------------------------
 commit;
 
 -- ==============================================================
 -- $Log: blobs-doc_med.sql,v $
--- Revision 1.1  2006-09-26 14:47:53  ncq
+-- Revision 1.2  2006-10-08 09:15:17  ncq
+-- - type -> fk_type
+--
+-- Revision 1.1  2006/09/26 14:47:53  ncq
 -- - those live here now
 --
 -- Revision 1.3  2006/09/18 17:32:53  ncq
