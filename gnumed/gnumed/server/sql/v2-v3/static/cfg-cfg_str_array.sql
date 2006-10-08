0cc73b744cd11e2de5ee1e565a42e379557b888c
Karsten Hilbert <karsten.hilbert@gmx.net>
2006-10-08 09:06:05 +0000

    - add workplace def

---
+++ gnumed/gnumed/server/sql/v2-v3/static/cfg-cfg_str_array.sql
@@ -0,0 +1,55 @@
+-- ==============================================================
+-- GNUmed database schema change script
+--
+-- Source database version: v2
+-- Target database version: v3
+--
+-- What it does:
+-- - ...
+--
+-- License: GPL
+-- Author: 
+-- 
+-- ==============================================================
+-- $Id: cfg-cfg_str_array.sql,v 1.1 2006-10-08 09:06:05 ncq Exp $
+-- $Revision: 1.1 $
+
+-- --------------------------------------------------------------
+\set ON_ERROR_STOP 1
+
+begin;
+
+-- --------------------------------------------------------------
+insert into cfg.cfg_str_array
+	(fk_item, value)
+values (
+	(select pk from cfg.cfg_item where workplace = 'post-Librarian Release'),
+	'{"gmProviderInboxPlugin","gmNotebookedPatientEditionPlugin","gmEMRBrowserPlugin","gmNotebookedProgressNoteInputPlugin","gmEMRJournalPlugin","gmShowMedDocs","gmScanIdxMedDocsPlugin","gmManual","gmConfigRegistry"}'
+);
+
+--comment on forgot_to_edit_comment is
+--	'';
+
+-- --------------------------------------------------------------
+select public.log_script_insertion('$RCSfile: cfg-cfg_str_array.sql,v $', '$Revision: 1.1 $');
+
+commit;
+
+-- ==============================================================
+-- $Log: cfg-cfg_str_array.sql,v $
+-- Revision 1.1  2006-10-08 09:06:05  ncq
+-- - add workplace def
+--
+-- Revision 1.4  2006/09/28 14:39:51  ncq
+-- - add comment template
+--
+-- Revision 1.3  2006/09/18 17:32:53  ncq
+-- - make more fool-proof
+--
+-- Revision 1.2  2006/09/16 21:47:37  ncq
+-- - improvements
+--
+-- Revision 1.1  2006/09/16 14:02:36  ncq
+-- - use this as a template for change scripts
+--
+--
