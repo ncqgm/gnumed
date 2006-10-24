20e3d7e8e9809cda612d3fd9ce9e143422fc5f58
Karsten Hilbert <karsten.hilbert@gmx.net>
2006-10-24 13:09:45 +0000

    - What it does duplicates the change log so axe it

--- gnumed/gnumed/server/sql/v2-v3/zzz-template.sql
+++ gnumed/gnumed/server/sql/v2-v3/zzz-template.sql
@@ -4,15 +4,12 @@
 -- Source database version: v2
 -- Target database version: v3
 --
--- What it does:
--- - ...
---
 -- License: GPL
 -- Author: 
 -- 
 -- ==============================================================
--- $Id: zzz-template.sql,v 1.4 2006-09-28 14:39:51 ncq Exp $
--- $Revision: 1.4 $
+-- $Id: zzz-template.sql,v 1.5 2006-10-24 13:09:45 ncq Exp $
+-- $Revision: 1.5 $
 
 -- --------------------------------------------------------------
 \set ON_ERROR_STOP 1
@@ -35,14 +32,17 @@ comment on forgot_to_edit_comment is
 grant select on forgot_to_edit_grants to group "gm-doctors";
 
 -- --------------------------------------------------------------
-select public.log_script_insertion('$RCSfile: zzz-template.sql,v $', '$Revision: 1.4 $');
+select public.log_script_insertion('$RCSfile: zzz-template.sql,v $', '$Revision: 1.5 $');
 
 -- --------------------------------------------------------------
 commit;
 
 -- ==============================================================
 -- $Log: zzz-template.sql,v $
--- Revision 1.4  2006-09-28 14:39:51  ncq
+-- Revision 1.5  2006-10-24 13:09:45  ncq
+-- - What it does duplicates the change log so axe it
+--
+-- Revision 1.4  2006/09/28 14:39:51  ncq
 -- - add comment template
 --
 -- Revision 1.3  2006/09/18 17:32:53  ncq
