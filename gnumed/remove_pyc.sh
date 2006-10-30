bf19b3fee7d9206b159e00405ec4e6531cd9bd95
Karsten Hilbert <karsten.hilbert@gmx.net>
2006-10-30 16:37:43 +0000

    - remove more crap

--- gnumed/remove_pyc.sh
+++ gnumed/remove_pyc.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/remove_pyc.sh,v $
-# $Revision: 1.3 $
+# $Revision: 1.4 $
 
 echo "cleaning out debris"
 find ./ -name '*.pyc' -exec rm -v '{}' ';'
 find ./ -name '*.py~' -exec rm -v '{}' ';'
+find ./ -name 'wxg*.wxg~' -exec rm -v '{}' ';'
 find ./ -name '*.mo' -exec rm -v '{}' ';'
