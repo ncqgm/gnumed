d557ed68b9e2308eec22c91281a44f7868074157
Karsten Hilbert <karsten.hilbert@gmx.net>
2006-10-30 16:49:53 +0000

    - add xdt viewer as plugin

--- gnumed/gnumed/server/sql/v2-v3/static/cfg-cfg_str_array.sql
+++ gnumed/gnumed/server/sql/v2-v3/static/cfg-cfg_str_array.sql
@@ -11,8 +11,8 @@
 -- Author: 
 -- 
 -- ==============================================================
--- $Id: cfg-cfg_str_array.sql,v 1.1 2006-10-08 09:06:05 ncq Exp $
--- $Revision: 1.1 $
+-- $Id: cfg-cfg_str_array.sql,v 1.2 2006-10-30 16:49:53 ncq Exp $
+-- $Revision: 1.2 $
 
 -- --------------------------------------------------------------
 \set ON_ERROR_STOP 1
@@ -24,20 +24,23 @@ insert into cfg.cfg_str_array
 	(fk_item, value)
 values (
 	(select pk from cfg.cfg_item where workplace = 'post-Librarian Release'),
-	'{"gmProviderInboxPlugin","gmNotebookedPatientEditionPlugin","gmEMRBrowserPlugin","gmNotebookedProgressNoteInputPlugin","gmEMRJournalPlugin","gmShowMedDocs","gmScanIdxMedDocsPlugin","gmManual","gmConfigRegistry"}'
+	'{"gmProviderInboxPlugin","gmNotebookedPatientEditionPlugin","gmEMRBrowserPlugin","gmNotebookedProgressNoteInputPlugin","gmEMRJournalPlugin","gmShowMedDocs","gmScanIdxMedDocsPlugin","gmXdtViewer","gmManual","gmConfigRegistry"}'
 );
 
 --comment on forgot_to_edit_comment is
 --	'';
 
 -- --------------------------------------------------------------
-select public.log_script_insertion('$RCSfile: cfg-cfg_str_array.sql,v $', '$Revision: 1.1 $');
+select public.log_script_insertion('$RCSfile: cfg-cfg_str_array.sql,v $', '$Revision: 1.2 $');
 
 commit;
 
 -- ==============================================================
 -- $Log: cfg-cfg_str_array.sql,v $
--- Revision 1.1  2006-10-08 09:06:05  ncq
+-- Revision 1.2  2006-10-30 16:49:53  ncq
+-- - add xdt viewer as plugin
+--
+-- Revision 1.1  2006/10/08 09:06:05  ncq
 -- - add workplace def
 --
 -- Revision 1.4  2006/09/28 14:39:51  ncq
