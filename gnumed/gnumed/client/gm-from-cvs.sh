5455a75f07b2acabd88e618c0cbc76ed6e390625
Karsten Hilbert <karsten.hilbert@gmx.net>
2006-10-31 13:42:34 +0000

    - update

--- gnumed/gnumed/client/gm-from-cvs.sh
+++ gnumed/gnumed/client/gm-from-cvs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/gm-from-cvs.sh,v $
-# $Revision: 1.6 $
+# $Revision: 1.7 $
 
 # start kvkd
 # FIXME: needs logic to prevent more than one kvkd from running
@@ -16,8 +16,17 @@ cd ../
 ln -vfsn client Gnumed
 cd -
 export PYTHONPATH="${PYTHONPATH}:../"
-rm -vf gm-from-cvs.log
-python wxpython/gnumed.py --log-file=gm-from-cvs.log --debug
-# --layout status_quo
-# --layout terry
+LOG="gm-from-cvs.log"
+rm -vf $LOG
+
+# prepare
+CONF="gm-from-cvs.conf"
+cp -vf $CONF tmp-$CONF
+
+# run
+python wxpython/gnumed.py --log-file=$LOG --conf-file=tmp-$CONF --debug --override-schema-check
 # --profile=gm-from-cvs.prof
+
+# clean up
+rm -vf tmp-$CONF
+rm -vf tmp-$CONF.gmCfg.bak
