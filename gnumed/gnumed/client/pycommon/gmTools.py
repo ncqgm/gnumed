acdb89e6370f68472e4acda8ee541a810e3a8aba
Karsten Hilbert <karsten.hilbert@gmx.net>
2006-10-31 16:03:06 +0000

    - add capitalize() and test

--- gnumed/gnumed/client/pycommon/gmTools.py
+++ gnumed/gnumed/client/pycommon/gmTools.py
@@ -1,9 +1,9 @@
 __doc__ = """GNUmed general tools."""
 
 #===========================================================================
-# $Id: gmTools.py,v 1.1 2006-09-03 08:53:19 ncq Exp $
+# $Id: gmTools.py,v 1.2 2006-10-31 16:03:06 ncq Exp $
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/pycommon/gmTools.py,v $
-__version__ = "$Revision: 1.1 $"
+__version__ = "$Revision: 1.2 $"
 __author__ = "K. Hilbert <Karsten.Hilbert@gmx.net>"
 __license__ = "GPL (details at http://www.gnu.org)"
 
@@ -22,6 +22,10 @@ def coalesce(initial=None, instead=None):
 	if initial is None:
 		return instead
 	return initial
+#---------------------------------------------------------------------------
+def capitalize(text=None):
+	"""Capitalize the first character but leave the rest alone."""
+	return text[0].capitalize() + text[1:]
 
 #===========================================================================
 if __name__ == '__main__':
@@ -33,9 +37,16 @@ if __name__ == '__main__':
 	print 'value          : %s (%s)' % (val, type(val))
 	print 'coalesce(value): %s (%s)' % (coalesce(val, 'something other than <None>'), type(coalesce(val, 'something other than <None>')))
 
+	print 'testing capitalize()'
+	for word in ['Boot', 'boot', 'booT', 'boots-Schau', 'boot camp']:
+		print word, capitalize(word)
+
 #===========================================================================
 # $Log: gmTools.py,v $
-# Revision 1.1  2006-09-03 08:53:19  ncq
+# Revision 1.2  2006-10-31 16:03:06  ncq
+# - add capitalize() and test
+#
+# Revision 1.1  2006/09/03 08:53:19  ncq
 # - first version
 #
 #
