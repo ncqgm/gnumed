6a375f0dfd3201310854b11a15ff1b7e13dcfb97
Karsten Hilbert <karsten.hilbert@gmx.net>
2006-11-05 16:05:35 +0000

    - cleanup, tabify

--- gnumed/gnumed/client/wxpython/gui/gmNotebookedProgressNoteInputPlugin.py
+++ gnumed/gnumed/client/wxpython/gui/gmNotebookedProgressNoteInputPlugin.py
@@ -7,7 +7,7 @@
 #
 # @copyright: author
 #======================================================================
-__version__ = "$Revision: 1.10 $"
+__version__ = "$Revision: 1.11 $"
 __author__ = "Carlos Moro, Karsten Hilbert"
 __license__ = 'GPL (details at http://www.gnu.org)'
 
@@ -37,89 +37,89 @@ class gmNotebookedProgressNoteInputPlugin(gmPlugin.cNotebookPlugin):
 		# need patient
 		if not self._verify_patient_avail():
 			return None
-		return 1
-		    
+		return True
 #======================================================================
 # main
 #----------------------------------------------------------------------
 if __name__ == "__main__":
 
-    import sys
-
+	import sys
 	import wx
 
-    from Gnumed.pycommon import gmCfg
-    from Gnumed.business import gmPerson
+	from Gnumed.pycommon import gmCfg
+	from Gnumed.business import gmPerson
 
-    _cfg = gmCfg.gmDefCfgFile	
-	
-    _log.Log (gmLog.lInfo, "starting Notebooked progress notes input plugin...")
+	_cfg = gmCfg.gmDefCfgFile	
+		
+	_log.Log (gmLog.lInfo, "starting Notebooked progress notes input plugin...")
 
-    if _cfg is None:
-        _log.Log(gmLog.lErr, "Cannot run without config file.")
-        sys.exit("Cannot run without config file.")
+	if _cfg is None:
+		_log.Log(gmLog.lErr, "Cannot run without config file.")
+		sys.exit("Cannot run without config file.")
 
-    try:
-        # obtain patient
-        patient = gmPerson.ask_for_patient()
-        if patient is None:
-            print "None patient. Exiting gracefully..."
-            sys.exit(0)
-        gmPerson.set_active_patient(patient=patient)
-                    
-        # display standalone multisash progress notes input
-        application = wx.wxPyWidgetTester(size=(800,600))
-        multisash_notes = gmSOAPWidgets.cNotebookedProgressNoteInputPanel(application.frame, -1)
-        
-        application.frame.Show(True)
-        application.MainLoop()
-        
-        # clean up
-        if patient is not None:
-            try:
-                patient.cleanup()
-            except:
-                print "error cleaning up patient"
-    except StandardError:
-        _log.LogException("unhandled exception caught !", sys.exc_info(), 1)
-        # but re-raise them
-        raise
+	try:
+		# obtain patient
+		patient = gmPerson.ask_for_patient()
+		if patient is None:
+			print "None patient. Exiting gracefully..."
+			sys.exit(0)
+		gmPerson.set_active_patient(patient=patient)
 
-    _log.Log (gmLog.lInfo, "closing Notebooked progress notes input plugin...")
+		# display standalone multisash progress notes input
+		application = wx.wxPyWidgetTester(size=(800,600))
+		multisash_notes = gmSOAPWidgets.cNotebookedProgressNoteInputPanel(application.frame, -1)
 
+		application.frame.Show(True)
+		application.MainLoop()
+
+		# clean up
+		if patient is not None:
+			try:
+				patient.cleanup()
+			except:
+				print "error cleaning up patient"
+	except StandardError:
+		_log.LogException("unhandled exception caught !", sys.exc_info(), 1)
+		# but re-raise them
+		raise
+
+	_log.Log (gmLog.lInfo, "closing Notebooked progress notes input plugin...")
 #======================================================================
 # $Log: gmNotebookedProgressNoteInputPlugin.py,v $
-# Revision 1.10  2006-10-31 16:06:19  ncq
+# Revision 1.11  2006-11-05 16:05:35  ncq
+# - cleanup, tabify
+#
+# Revision 1.10  2006/10/31 16:06:19  ncq
 # - no more gmPG
 #
-# Revision 1.9  2006/10/25 07:23:30  ncq
+# Revision 1.9	2006/10/25 07:23:30  ncq
 # - no gmPG no more
 #
-# Revision 1.8  2006/05/04 09:49:20  ncq
+# Revision 1.8	2006/05/04 09:49:20  ncq
 # - get_clinical_record() -> get_emr()
 # - adjust to changes in set_active_patient()
 # - need explicit set_active_patient() after ask_for_patient() if wanted
 #
-# Revision 1.7  2005/10/03 13:59:59  sjtan
+# Revision 1.7	2005/10/03 13:59:59  sjtan
 # indentation errors
 #
-# Revision 1.6  2005/09/26 18:01:52  ncq
+# Revision 1.6	2005/09/26 18:01:52  ncq
 # - use proper way to import wx26 vs wx2.4
 # - note: THIS WILL BREAK RUNNING THE CLIENT IN SOME PLACES
 # - time for fixup
 #
-# Revision 1.5  2005/09/12 15:11:15  ncq
+# Revision 1.5	2005/09/12 15:11:15  ncq
 # - tab name capitalized
 #
-# Revision 1.4  2005/06/30 10:21:01  cfmoro
+# Revision 1.4	2005/06/30 10:21:01  cfmoro
 # String corrections
 #
-# Revision 1.3  2005/06/07 10:19:18  ncq
+# Revision 1.3	2005/06/07 10:19:18  ncq
 # - string improvement
 #
-# Revision 1.2  2005/05/12 15:13:28  ncq
+# Revision 1.2	2005/05/12 15:13:28  ncq
 # - cleanup
 #
-# Revision 1.1  2005/05/08 21:45:25  ncq
+# Revision 1.1	2005/05/08 21:45:25  ncq
 # - new plugin for progress note input ...
 #
