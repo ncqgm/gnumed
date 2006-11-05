7d1c8933b188dc09c268f91aabc2b6112490bb8e
Karsten Hilbert <karsten.hilbert@gmx.net>
2006-11-05 15:54:57 +0000

    - do not remove *.mo files

--- gnumed/remove_pyc.sh
+++ gnumed/remove_pyc.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/remove_pyc.sh,v $
-# $Revision: 1.4 $
+# $Revision: 1.5 $
 
 echo "cleaning out debris"
 find ./ -name '*.pyc' -exec rm -v '{}' ';'
 find ./ -name '*.py~' -exec rm -v '{}' ';'
 find ./ -name 'wxg*.wxg~' -exec rm -v '{}' ';'
-find ./ -name '*.mo' -exec rm -v '{}' ';'
