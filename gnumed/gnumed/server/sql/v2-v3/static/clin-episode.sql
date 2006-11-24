942bdffec86a3a7209eabd157a30709594dc9020
Karsten Hilbert <karsten.hilbert@gmx.net>
2006-11-24 09:21:50 +0000

    - whitespace fix

--- gnumed/gnumed/server/sql/v2-v3/static/clin-episode.sql
+++ gnumed/gnumed/server/sql/v2-v3/static/clin-episode.sql
@@ -8,14 +8,13 @@
 -- Author: Karsten Hilbert
 -- 
 -- ==============================================================
--- $Id: clin-episode.sql,v 1.2 2006-10-28 23:39:18 ncq Exp $
--- $Revision: 1.2 $
+-- $Id: clin-episode.sql,v 1.3 2006-11-24 09:21:50 ncq Exp $
+-- $Revision: 1.3 $
 
 -- --------------------------------------------------------------
 \set ON_ERROR_STOP 1
 
 -- --------------------------------------------------------------
-
 \unset ON_ERROR_STOP
 alter table clin.episode
 	drop constraint "$2";
@@ -30,11 +29,14 @@ alter table clin.episode
 		on delete restrict;
 
 -- --------------------------------------------------------------
-select public.log_script_insertion('$RCSfile: clin-episode.sql,v $', '$Revision: 1.2 $');
+select public.log_script_insertion('$RCSfile: clin-episode.sql,v $', '$Revision: 1.3 $');
 
 -- ==============================================================
 -- $Log: clin-episode.sql,v $
--- Revision 1.2  2006-10-28 23:39:18  ncq
+-- Revision 1.3  2006-11-24 09:21:50  ncq
+-- - whitespace fix
+--
+-- Revision 1.2  2006/10/28 23:39:18  ncq
 -- - $2 -> explicit name
 --
 -- Revision 1.1  2006/10/24 13:08:26  ncq
