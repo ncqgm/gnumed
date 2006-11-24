8db03fca212ac8abc4deb93fcb2e893f62ba1f74
Karsten Hilbert <karsten.hilbert@gmx.net>
2006-11-24 09:21:36 +0000

    - fix notification trigger col name use

--- gnumed/gnumed/server/sql/v2-v3/dynamic/clin-episode.sql
+++ gnumed/gnumed/server/sql/v2-v3/dynamic/clin-episode.sql
@@ -11,8 +11,8 @@
 -- Author: Karsten Hilbert/Syan Tan
 -- 
 -- ==============================================================
--- $Id: clin-episode.sql,v 1.1 2006-09-25 10:55:01 ncq Exp $
--- $Revision: 1.1 $
+-- $Id: clin-episode.sql,v 1.2 2006-11-24 09:21:36 ncq Exp $
+-- $Revision: 1.2 $
 
 -- --------------------------------------------------------------
 \set ON_ERROR_STOP 1
@@ -33,15 +33,65 @@ comment on index clin.idx_episode_without_issue is
 	'index episodes without associated health issue';
 
 -- --------------------------------------------------------------
+\unset ON_ERROR_STOP
+drop function trf_announce_episode_mod() cascade;
+\set ON_ERROR_STOP 1
+
+create function audit.trf_announce_episode_mod()
+	returns trigger
+	language 'plpgsql'
+	as '
+declare
+	patient_pk integer;
+begin
+	-- get patient ID
+	if TG_OP = ''DELETE'' then
+		-- if no patient in episode
+		if OLD.fk_patient is null then
+			-- get it from attached health issue
+			select into patient_pk fk_patient
+				from clin.health_issue
+				where pk = OLD.fk_health_issue;
+		else
+			patient_pk := OLD.fk_patient;
+		end if;
+	else
+		-- if no patient in episode
+		if NEW.fk_patient is null then
+			-- get it from attached health issue
+			select into patient_pk fk_patient
+				from clin.health_issue
+				where pk = NEW.fk_health_issue;
+		else
+			patient_pk := NEW.fk_patient;
+		end if;
+	end if;
+	-- execute() the NOTIFY
+	execute ''notify "episode_change_db:'' || patient_pk || ''"'';
+	return NULL;
+end;
+';
+
+create trigger tr_episode_mod
+	after insert or delete or update
+	on clin.episode
+	for each row
+		execute procedure audit.trf_announce_episode_mod()
+;
+
+-- --------------------------------------------------------------
 -- don't forget appropriate grants
 --grant select on forgot_to_edit_grants to group "gm-doctors";
 
 -- --------------------------------------------------------------
-select public.log_script_insertion('$RCSfile: clin-episode.sql,v $', '$Revision: 1.1 $');
+select public.log_script_insertion('$RCSfile: clin-episode.sql,v $', '$Revision: 1.2 $');
 
 -- ==============================================================
 -- $Log: clin-episode.sql,v $
--- Revision 1.1  2006-09-25 10:55:01  ncq
+-- Revision 1.2  2006-11-24 09:21:36  ncq
+-- - fix notification trigger col name use
+--
+-- Revision 1.1  2006/09/25 10:55:01  ncq
 -- - added here
 --
 -- Revision 1.3  2006/09/18 17:32:53  ncq
