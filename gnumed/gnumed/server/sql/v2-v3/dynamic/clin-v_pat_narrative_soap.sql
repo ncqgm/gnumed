5665625b13e5915169c63b037806fd3c0bbec891
Karsten Hilbert <karsten.hilbert@gmx.net>
2006-12-11 17:05:12 +0000

    - redo view

--- gnumed/gnumed/server/sql/v2-v3/dynamic/clin-v_pat_narrative_soap.sql
+++ gnumed/gnumed/server/sql/v2-v3/dynamic/clin-v_pat_narrative_soap.sql
@@ -11,20 +11,69 @@
 -- Author: 
 -- 
 -- ==============================================================
--- $Id: clin-v_pat_narrative_soap.sql,v 1.1 2006-09-30 10:33:01 ncq Exp $
--- $Revision: 1.1 $
+-- $Id: clin-v_pat_narrative_soap.sql,v 1.2 2006-12-11 17:05:12 ncq Exp $
+-- $Revision: 1.2 $
 
 -- --------------------------------------------------------------
 \set ON_ERROR_STOP 1
 
+-- --------------------------------------------------------------
+\unset ON_ERROR_STOP
+drop view clin.v_pat_narrative_soap cascade;
+\set ON_ERROR_STOP 1
+
+
+create view clin.v_pat_narrative_soap as
+SELECT
+	vpep.pk_patient
+		AS pk_patient,
+	cn.clin_when
+		AS date, 
+	coalesce (
+		(select short_alias from dem.staff where db_user = cn.modified_by),
+		'<' || cn.modified_by || '>'
+	) as provider,
+	cn.soap_cat
+		as soap_cat,
+	cn.narrative
+		as narrative,
+	cn.pk_item
+		as pk_item,
+	cn.pk
+		AS pk_narrative,
+	vpep.pk_health_issue
+		AS pk_health_issue,
+	cn.fk_episode
+		AS pk_episode,
+	cn.fk_encounter
+		AS pk_encounter,
+	cn.xmin
+		AS xmin_clin_narrative
+FROM
+	clin.clin_narrative cn,
+	clin.v_pat_episodes vpep
+WHERE
+	vpep.pk_episode = cn.fk_episode
+;
+
+
+comment on view clin.v_pat_narrative_soap is
+	'patient SOAP-only narrative;
+	 this view aggregates all clin.clin_narrative rows
+	 and adds denormalized context';
+
+
 grant select on clin.v_pat_narrative_soap to group "gm-doctors";
 
 -- --------------------------------------------------------------
-select public.log_script_insertion('$RCSfile: clin-v_pat_narrative_soap.sql,v $', '$Revision: 1.1 $');
+select public.log_script_insertion('$RCSfile: clin-v_pat_narrative_soap.sql,v $', '$Revision: 1.2 $');
 
 -- ==============================================================
 -- $Log: clin-v_pat_narrative_soap.sql,v $
--- Revision 1.1  2006-09-30 10:33:01  ncq
+-- Revision 1.2  2006-12-11 17:05:12  ncq
+-- - redo view
+--
+-- Revision 1.1  2006/09/30 10:33:01  ncq
 -- - add missing grants
 --
 -- Revision 1.4  2006/09/28 14:39:51  ncq
