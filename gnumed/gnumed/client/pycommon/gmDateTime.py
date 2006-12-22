dfdd04cca36b39fe629c65a4eb761f5229fccbea
Karsten Hilbert <karsten.hilbert@gmx.net>
2006-12-22 16:54:28 +0000

    - add cLocalTimezone from psycopg2 until datetime supports it - better logging - enhanced test suite

--- gnumed/gnumed/client/pycommon/gmDateTime.py
+++ gnumed/gnumed/client/pycommon/gmDateTime.py
@@ -1,6 +1,6 @@
 #===========================================================================
 __doc__ = (
-"""GNUmed date time handling.
+"""GNUmed date/time handling.
 
 This modules provides access to date/time handling.
 
@@ -14,20 +14,22 @@ Note that if you want locale-aware formatting you need to call
 
 	locale.setlocale(locale.LC_ALL, '')
 
-somehwere before importing this script.
-""")
+somehwere before importing this script."""
+)
 #===========================================================================
-# $Id: gmDateTime.py,v 1.2 2006-12-21 17:44:26 ncq Exp $
+# $Id: gmDateTime.py,v 1.3 2006-12-22 16:54:28 ncq Exp $
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/pycommon/gmDateTime.py,v $
-__version__ = "$Revision: 1.2 $"
+__version__ = "$Revision: 1.3 $"
 __author__ = "K. Hilbert <Karsten.Hilbert@gmx.net>"
 __license__ = "GPL (details at http://www.gnu.org)"
 
 # stdlib
 import sys, datetime as pyDT, time, os
 
+
 # 3rd party
 import mx.DateTime as mxDT
+import psycopg2						# this will go once datetime has timezone classes
 
 # GNUmed libs
 if __name__ == '__main__':
@@ -41,6 +43,7 @@ dst_currently_in_effect = None
 current_utc_offset = None
 current_timezone_interval = None
 current_iso_timezone_string = None
+cLocalTimezone = psycopg2.tz.LocalTimezone
 
 #===========================================================================
 def init():
@@ -55,10 +58,10 @@ def init():
 	except KeyError:
 		_log.Log(gmLog.lData, '$TZ not defined')
 
-	_log.Log(gmLog.lData, 'time.daylight                : [%s]' % time.daylight)
-	_log.Log(gmLog.lData, 'time.timezone                : [%s] seconds' % time.timezone)
-	_log.Log(gmLog.lData, 'time.altzone                 : [%s] seconds' % time.altzone)
-	_log.Log(gmLog.lData, 'time.tzname                  : [%s / %s] (non-DST / DST)' % time.tzname)
+	_log.Log(gmLog.lData, 'time.daylight: [%s] (whether or not DST is locally used)' % time.daylight)
+	_log.Log(gmLog.lData, 'time.timezone: [%s] seconds' % time.timezone)
+	_log.Log(gmLog.lData, 'time.altzone : [%s] seconds' % time.altzone)
+	_log.Log(gmLog.lData, 'time.tzname  : [%s / %s] (non-DST / DST)' % time.tzname)
 	_log.Log(gmLog.lData, 'mx.DateTime.now().gmtoffset(): [%s]' % mxDT.now().gmtoffset())
 
 	global dst_currently_in_effect
@@ -99,9 +102,25 @@ if __name__ == '__main__':
 	_log.SetAllLogLevels(gmLog.lData)
 	init()
 
+	print "DST currently in effect:", dst_currently_in_effect
+	print "current UTC offset:", current_utc_offset, "seconds"
+	print "current timezone (interval):", current_timezone_interval
+	print "current timezone (ISO conformant string):", current_iso_timezone_string
+	print "local timezone class:", cLocalTimezone
+	tz = cLocalTimezone()
+	print "local timezone instance:", tz
+	print " (total) UTC offset:", tz.utcoffset(pyDT.datetime.now())
+	print " DST adjustment:", tz.dst(pyDT.datetime.now())
+	print " timezone name:", tz.tzname(pyDT.datetime.now())
+
 #===========================================================================
 # $Log: gmDateTime.py,v $
-# Revision 1.2  2006-12-21 17:44:26  ncq
+# Revision 1.3  2006-12-22 16:54:28  ncq
+# - add cLocalTimezone from psycopg2 until datetime supports it
+# - better logging
+# - enhanced test suite
+#
+# Revision 1.2  2006/12/21 17:44:26  ncq
 # - differentiate between timezone as interval and as string
 # - if timezone string is to be ISO aware it cannot contain ","
 #
