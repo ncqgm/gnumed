c3829d3c023133254fd22422af7e99c1534dd2a8
Karsten Hilbert <karsten.hilbert@gmx.net>
2007-01-18 22:05:25 +0000

    - wx2.8 needs properties !

--- gnumed/gnumed/client/wxpython/gmBMIWidgets.py
+++ gnumed/gnumed/client/wxpython/gmBMIWidgets.py
@@ -15,8 +15,8 @@ TODO:
 """
 #===========================================================================
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/wxpython/gmBMIWidgets.py,v $
-# $Id: gmBMIWidgets.py,v 1.11 2005-10-24 10:47:09 ihaywood Exp $
-__version__ = "$Revision: 1.11 $"
+# $Id: gmBMIWidgets.py,v 1.12 2007-01-18 22:05:25 ncq Exp $
+__version__ = "$Revision: 1.12 $"
 __author__  =  "Richard Terry <rterry@gnumed.net>,\
 				Michael Bonert <bonerti@mie.utoronto.ca>,\
 				Karsten Hilbert <Karsten.Hilbert@gmx.net>"
@@ -386,31 +386,31 @@ class BMICalc_Panel(wx.Panel):
 	#-----------------------------------------
 	# Moving between fields with the 'Enter' key
 	def EvtChar_height(self, event):
-                if(event.GetKeyCode()==13):		# height -> mass
+                if(event.KeyCode==13):		# height -> mass
 			self.txtmass.SetFocus()
                 else:
                          event.Skip()
 	#-----------------------------------------
 	def EvtChar_mass(self, event):
-                if(event.GetKeyCode()==13):		# mass -> slider
+                if(event.KeyCode==13):		# mass -> slider
 			self.slider.SetFocus()
                 else:
                          event.Skip()
 	#-----------------------------------------
 	def EvtChar_slider(self, event):
-                if(event.GetKeyCode()==13):		# slider -> goal
+                if(event.KeyCode==13):		# slider -> goal
 			self.txtgoal.SetFocus()
                 else:
                          event.Skip()
 	#-----------------------------------------
 	def EvtChar_goal(self, event):
-                if(event.GetKeyCode()==13):		# goal -> loss
+                if(event.KeyCode==13):		# goal -> loss
 			self.txtloss.SetFocus()
                 else:
                          event.Skip()
 	#-----------------------------------------
 	def EvtChar_loss(self, event):
-		if(event.GetKeyCode()==13):		# loss -> height
+		if(event.KeyCode==13):		# loss -> height
 			self.txtheight.SetFocus()
 		else:
 			event.Skip()
@@ -552,7 +552,10 @@ if __name__ == '__main__':
 
 #=====================================================================
 # $Log: gmBMIWidgets.py,v $
-# Revision 1.11  2005-10-24 10:47:09  ihaywood
+# Revision 1.12  2007-01-18 22:05:25  ncq
+# - wx2.8 needs properties !
+#
+# Revision 1.11  2005/10/24 10:47:09  ihaywood
 # works properly now, again using Andreas' packages, with wx 2.4
 #
 # Revision 1.10  2005/09/28 21:27:30  ncq
