716f16fd1de825e72f4b34e305a32fd842127b24
Karsten Hilbert <karsten.hilbert@gmx.net>
2007-01-23 13:52:30 +0000

    - no need for this table anymore

---
+++ gnumed/gnumed/server/sql/v3-v4/static/public-gm_services.sql
@@ -0,0 +1,30 @@
+-- ==============================================================
+-- GNUmed database schema change script
+--
+-- Source database version: v2
+-- Target database version: v3
+--
+-- License: GPL
+-- Author: Karsten Hilbert
+-- 
+-- ==============================================================
+-- $Id: public-gm_services.sql,v 1.1.2.1 2007-01-23 13:52:30 ncq Exp $
+-- $Revision: 1.1.2.1 $
+
+-- --------------------------------------------------------------
+\set ON_ERROR_STOP 1
+
+-- --------------------------------------------------------------
+drop table public.gm_services cascade;
+
+-- --------------------------------------------------------------
+select public.log_script_insertion('$RCSfile: public-gm_services.sql,v $', '$Revision: 1.1.2.1 $');
+
+-- ==============================================================
+-- $Log: public-gm_services.sql,v $
+-- Revision 1.1.2.1  2007-01-23 13:52:30  ncq
+-- - no need for this table anymore
+--
+-- Revision 1.5  2006/10/24 13:09:45  ncq
+-- - What it does duplicates the change log so axe it
+--
