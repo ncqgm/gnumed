71c43d64207b928a83e565d9effe2e600f735153
Karsten Hilbert <karsten.hilbert@gmx.net>
2007-01-24 15:11:29 +0000

    - proper base dir

--- gnumed/gnumed/dists/cvs-snapshot/make-cvs_tarball.sh
+++ gnumed/gnumed/dists/cvs-snapshot/make-cvs_tarball.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/dists/cvs-snapshot/make-cvs_tarball.sh,v $
-# $Revision: 1.1 $
+# $Revision: 1.2 $
 # license: GPL
 # author: Karsten.Hilbert@gmx.net
 
-BASE=~/gm-cvs-head
+BASE=~/gm-cvs-branches/HEAD
 ARCHIVE=$HOME/public_html/gnumed/snapshot/gnumed-latest-snapshot.tgz
 
 cd $BASE
@@ -13,7 +13,10 @@ tar -cvzf $ARCHIVE gnumed/
 
 #============================================
 # $Log: make-cvs_tarball.sh,v $
-# Revision 1.1  2006-07-19 11:39:00  ncq
+# Revision 1.2  2007-01-24 15:11:29  ncq
+# - proper base dir
+#
+# Revision 1.1  2006/07/19 11:39:00  ncq
 # - add here from elsewhere
 #
 # Revision 1.2  2005/12/27 17:15:52  ncq
