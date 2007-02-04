931824cc7eb8f60c0f96d1b1485b811ddf7f1db9
Karsten Hilbert <karsten.hilbert@gmx.net>
2007-02-04 15:31:40 +0000

    - add check for END after START

---
+++ gnumed/gnumed/server/sql/v4-v5/dynamic/clin-encounter.sql
@@ -0,0 +1,60 @@
+-- ==============================================================
+-- GNUmed database schema change script
+--
+-- Source database version: v2
+-- Target database version: v3
+--
+-- License: GPL
+-- Author: 
+-- 
+-- ==============================================================
+-- $Id: clin-encounter.sql,v 1.1 2007-02-04 15:31:40 ncq Exp $
+-- $Revision: 1.1 $
+
+-- --------------------------------------------------------------
+\set ON_ERROR_STOP 1
+
+-- --------------------------------------------------------------
+-- remember to handle dependant objects possibly dropped by CASCADE
+--\unset ON_ERROR_STOP
+--drop forgot_to_edit_drops;
+--\set ON_ERROR_STOP 1
+
+alter table clin.encounter
+	add constraint encounter_must_start_before_it_ends
+		check (started <= last_affirmed);
+
+--comment on forgot_to_edit_comment is
+--	'';
+
+-- --------------------------------------------------------------
+-- don't forget appropriate grants
+--grant select on forgot_to_edit_grants to group "gm-doctors";
+
+-- --------------------------------------------------------------
+select public.log_script_insertion('$RCSfile: clin-encounter.sql,v $', '$Revision: 1.1 $');
+
+-- ==============================================================
+-- $Log: clin-encounter.sql,v $
+-- Revision 1.1  2007-02-04 15:31:40  ncq
+-- - add check for END after START
+--
+-- Revision 1.6  2007/01/27 21:16:08  ncq
+-- - the begin/commit does not fit into our change script model
+--
+-- Revision 1.5  2006/10/24 13:09:45  ncq
+-- - What it does duplicates the change log so axe it
+--
+-- Revision 1.4  2006/09/28 14:39:51  ncq
+-- - add comment template
+--
+-- Revision 1.3  2006/09/18 17:32:53  ncq
+-- - make more fool-proof
+--
+-- Revision 1.2  2006/09/16 21:47:37  ncq
+-- - improvements
+--
+-- Revision 1.1  2006/09/16 14:02:36  ncq
+-- - use this as a template for change scripts
+--
+--
