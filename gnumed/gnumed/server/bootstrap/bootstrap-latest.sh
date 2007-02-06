8f59040ea54ab18de310c2b9280a9262ce0e7873
Karsten Hilbert <karsten.hilbert@gmx.net>
2007-02-06 11:22:03 +0000

    - convenience shell skript to bootstrap latest database

---
+++ gnumed/gnumed/server/bootstrap/bootstrap-latest.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+cd ../../
+ln -vfsn client Gnumed
+cd -
+export PYTHONPATH="../../:${PYTHONPATH}"
+
+VER="5"
+GM_CORE_DB="gnumed_v${VER}"
+
+echo "==========================================================="
+echo "Bootstrapping latest GNUmed database."
+echo ""
+echo "This will set up a GNUmed database of version v${VER}"
+echo "with the name \"${GM_CORE_DB}\"."
+echo "It contains all the currently working parts including"
+echo "localizations for countries you don't live in. This does"
+echo "not disturb the operation of the GNUmed client in your"
+echo "country in any way."
+echo "==========================================================="
+echo "1) Dropping old databases if there are any."
+dropdb -U gm-dbo -i gnumed_v2
+dropdb -U gm-dbo -i gnumed_v3
+dropdb -U gm-dbo -i gnumed_v4
+dropdb -U gm-dbo -i gnumed_v5
+rm -rf ${LOG}
+
+echo "=========================="
+echo "2) bootstrapping databases"
+
+LOG="bootstrap-latest-v2.log"
+CONF="redo-v2.conf"
+export GM_CORE_DB="gnumed_v2"
+./bootstrap_gm_db_system.py --log-file=${LOG} --conf-file=${CONF}
+unset GM_CORE_DB
+
+LOG="bootstrap-latest-v3.log"
+CONF="update_db-v2_v3.conf"
+./bootstrap_gm_db_system.py --log-file=${LOG} --conf-file=${CONF}
+dropdb -U gm-dbo -i gnumed_v2
+
+LOG="bootstrap-latest-v4.log"
+CONF="update_db-v3_v4.conf"
+./bootstrap_gm_db_system.py --log-file=${LOG} --conf-file=${CONF}
+dropdb -U gm-dbo -i gnumed_v3
+
+LOG="bootstrap-latest-v5.log"
+CONF="update_db-v4_v5.conf"
+./bootstrap_gm_db_system.py --log-file=${LOG} --conf-file=${CONF}
+dropdb -U gm-dbo -i gnumed_v4
