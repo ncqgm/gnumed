6256c81f5f749d7d57583226af0ce9eb4e31c594
sjtan <sjtan>
2007-03-01 10:05:58 +0000

    help message.

--- gnumed/gnumed/client/importers/au/md2/a/dbf_2_pg/view_table.py
+++ gnumed/gnumed/client/importers/au/md2/a/dbf_2_pg/view_table.py
@@ -6,6 +6,17 @@ if __name__ == "__main__":
 	import sys
 	print sys.argv
 
+	if len(sys.argv) == 1:
+		print"""
+USAGE:
+	python view_table.py <path to dbf file>  <start record number> <next n records>
+
+	e.g. python view_table.py /data/dbf_schema1/customers.dbf  1  100
+
+	will view records from 1 to 100 in customers.dbf
+
+"""
+		sys.exit(-1)
 	p = Parser(sys.argv[1])
 	start = None
 	if len(sys.argv) > 2:
