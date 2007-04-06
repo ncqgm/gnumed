1e37930f2ad5a975d79759b2ce3ed5d706085c54
Karsten Hilbert <karsten.hilbert@gmx.net>
2007-04-06 23:19:31 +0000

    - include data mining panel

--- gnumed/gnumed/server/sql/v5-v6/static/cfg-cfg_str_array.sql
+++ gnumed/gnumed/server/sql/v5-v6/static/cfg-cfg_str_array.sql
@@ -8,8 +8,8 @@
 -- Author: Karsten Hilbert
 -- 
 -- ==============================================================
--- $Id: cfg-cfg_str_array.sql,v 1.1 2007-04-02 14:16:44 ncq Exp $
--- $Revision: 1.1 $
+-- $Id: cfg-cfg_str_array.sql,v 1.2 2007-04-06 23:19:31 ncq Exp $
+-- $Revision: 1.2 $
 
 -- --------------------------------------------------------------
 \set ON_ERROR_STOP 1
@@ -19,15 +19,18 @@ insert into cfg.cfg_str_array
 	(fk_item, value)
 values (
 	(select pk from cfg.cfg_item where workplace = 'GNUmed Standard'),
-	'{"gmProviderInboxPlugin","gmNotebookedPatientEditionPlugin","gmEMRBrowserPlugin","gmNotebookedProgressNoteInputPlugin","gmEMRJournalPlugin","gmShowMedDocs","gmScanIdxMedDocsPlugin","gmXdtViewer","gmManual","gmConfigRegistry"}'
+	'{"gmProviderInboxPlugin","gmNotebookedPatientEditionPlugin","gmEMRBrowserPlugin","gmNotebookedProgressNoteInputPlugin","gmEMRJournalPlugin","gmShowMedDocs","gmScanIdxMedDocsPlugin","gmDataMiningPlugin","gmXdtViewer","gmManual","gmConfigRegistry"}'
 );
 
 -- --------------------------------------------------------------
-select public.log_script_insertion('$RCSfile: cfg-cfg_str_array.sql,v $', '$Revision: 1.1 $');
+select public.log_script_insertion('$RCSfile: cfg-cfg_str_array.sql,v $', '$Revision: 1.2 $');
 
 -- ==============================================================
 -- $Log: cfg-cfg_str_array.sql,v $
--- Revision 1.1  2007-04-02 14:16:44  ncq
+-- Revision 1.2  2007-04-06 23:19:31  ncq
+-- - include data mining panel
+--
+-- Revision 1.1  2007/04/02 14:16:44  ncq
 -- - added
 --
 --
