456acfb4b0163a6fe9c37dcd3d2e0ff92a7078bb
Karsten Hilbert <karsten.hilbert@gmx.net>
2007-04-17 10:13:44 +0000

    - VER seems to be a reserverd word in batch files

--- gnumed/gnumed/server/bootstrap/upgrade-db.bat
+++ gnumed/gnumed/server/bootstrap/upgrade-db.bat
@@ -3,20 +3,19 @@ SET PYTHONPATH="%PYTHONPATH%;../../"
 if %1=="" echo "usage: update-db x x+1"
 
 set PREV_VER=%1
-set VER=%2
-set LOG=update_db-v%PREV_VER%_v%VER%.log
-set CONF=update_db-v%PREV_VER%_v%VER%.conf
-set GM_CORE_DB=gnumed_v%VER%
+set NEXT_VER=%2
+set LOG=update_db-v%PREV_VER%_v%NEXT_VER%.log
+set CONF=update_db-v%PREV_VER%_v%NEXT_VER%.conf
+set GM_CORE_DB=gnumed_v%NEXT_VER%
 
 echo ===========================================================
 echo Bootstrapping GNUmed database.
 echo ...
 echo This will non-destructively transform a GNUmed database
-echo of version v%PREV_VER% into a version v%VER% database.
+echo of version v%PREV_VER% into a version v%NEXT_VER% database.
 echo ...
 echo The name of the new database will be %GM_CORE_DB% .
 echo ===========================================================
-echo Dropping target database if there is any.
 
 del %LOG%
 echo =======================
