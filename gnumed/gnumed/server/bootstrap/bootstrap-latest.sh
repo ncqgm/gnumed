b5bff84c9bc3d11924f4dae900de4f24f1384154
Karsten Hilbert <karsten.hilbert@gmx.net>
2007-04-20 08:30:29 +0000

    - enable setting port - drop databases as postgres - hint to run as root

--- gnumed/gnumed/server/bootstrap/bootstrap-latest.sh
+++ gnumed/gnumed/server/bootstrap/bootstrap-latest.sh
@@ -1,25 +1,34 @@
 #!/bin/sh
 
+# should be run as root
+
 cd ../../
 ln -vsn client Gnumed
 cd -
 export PYTHONPATH="../../:${PYTHONPATH}"
 
-VER="5"
-GM_CORE_DB="gnumed_v${VER}"
+VER="6"
+DROPDB_PORT=""
+
+# uncomment and set if your PostgreSQL server is running
+# on a port different from the default port 5432, such as
+# 5433 for PostgreSQL 8.1 running on Debian alongside
+# a 7.4 server
+#export GM_DB_PORT="5433"
+#DROPDB_PORT="-p ${GM_DB_PORT}"
 
 echo "==========================================================="
 echo "Bootstrapping latest GNUmed database."
 echo ""
 echo "This will set up a GNUmed database of version v${VER}"
-echo "with the name \"${GM_CORE_DB}\"."
+echo "with the name \"gnumed_v${VER}\"."
 echo "It contains all the currently working parts including"
 echo "localizations for countries you don't live in. This does"
 echo "not disturb the operation of the GNUmed client in your"
 echo "country in any way."
 echo "==========================================================="
 echo "1) Dropping old baseline gnumed_v2 database if there is any."
-dropdb -U gm-dbo -i gnumed_v2
+sudo -u postgres dropdb ${DROPDB_PORT} -i gnumed_v2
 rm -rf ${LOG}
 
 echo "=========================="
@@ -34,19 +43,19 @@ unset GM_CORE_DB
 LOG="bootstrap-latest-v3.log"
 CONF="update_db-v2_v3.conf"
 ./bootstrap_gm_db_system.py --log-file=${LOG} --conf-file=${CONF}
-dropdb -U gm-dbo gnumed_v2
+sudo -u postgres dropdb ${DROPDB_PORT} gnumed_v2
 
 LOG="bootstrap-latest-v4.log"
 CONF="update_db-v3_v4.conf"
 ./bootstrap_gm_db_system.py --log-file=${LOG} --conf-file=${CONF}
-dropdb -U gm-dbo gnumed_v3
+sudo -u postgres dropdb ${DROPDB_PORT} gnumed_v3
 
 LOG="bootstrap-latest-v5.log"
 CONF="update_db-v4_v5.conf"
 ./bootstrap_gm_db_system.py --log-file=${LOG} --conf-file=${CONF}
-dropdb -U gm-dbo gnumed_v4
+sudo -u postgres dropdb ${DROPDB_PORT} gnumed_v4
 
 LOG="bootstrap-latest-v6.log"
 CONF="update_db-v5_v6.conf"
 ./bootstrap_gm_db_system.py --log-file=${LOG} --conf-file=${CONF}
-dropdb -U gm-dbo gnumed_v5
+sudo -u postgres dropdb ${DROPDB_PORT} gnumed_v5
