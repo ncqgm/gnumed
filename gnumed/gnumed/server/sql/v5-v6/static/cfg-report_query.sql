6ac4e69ba180847159f40d4ec4ab5fffc3cdf149
Karsten Hilbert <karsten.hilbert@gmx.net>
2007-04-21 19:44:22 +0000

    - label must be unique, cmd doesn't have to be

--- gnumed/gnumed/server/sql/v5-v6/static/cfg-report_query.sql
+++ gnumed/gnumed/server/sql/v5-v6/static/cfg-report_query.sql
@@ -8,8 +8,8 @@
 -- Author: Karsten Hilbert
 -- 
 -- ==============================================================
--- $Id: cfg-report_query.sql,v 1.2 2007-04-07 22:29:12 ncq Exp $
--- $Revision: 1.2 $
+-- $Id: cfg-report_query.sql,v 1.3 2007-04-21 19:44:22 ncq Exp $
+-- $Revision: 1.3 $
 
 -- --------------------------------------------------------------
 \set ON_ERROR_STOP 1
@@ -21,16 +21,18 @@ create table cfg.report_query (
 		unique
 		not null,
 	cmd text
-		unique
 		not null
 ) inherits (audit.audit_fields);
 
 -- --------------------------------------------------------------
-select public.log_script_insertion('$RCSfile: cfg-report_query.sql,v $', '$Revision: 1.2 $');
+select public.log_script_insertion('$RCSfile: cfg-report_query.sql,v $', '$Revision: 1.3 $');
 
 -- ==============================================================
 -- $Log: cfg-report_query.sql,v $
--- Revision 1.2  2007-04-07 22:29:12  ncq
+-- Revision 1.3  2007-04-21 19:44:22  ncq
+-- - label must be unique, cmd doesn't have to be
+--
+-- Revision 1.2  2007/04/07 22:29:12  ncq
 -- - only this is static
 --
 -- Revision 1.1  2007/04/06 23:10:54  ncq
