8e844cc427e080636b624dfcc80ea85809089884
Karsten Hilbert <karsten.hilbert@gmx.net>
2007-06-05 15:00:31 +0000

    - support max bandwidth utilization

--- gnumed/gnumed/server/gm-move_backups_offsite.sh
+++ gnumed/gnumed/server/gm-move_backups_offsite.sh
@@ -2,7 +2,7 @@
 
 #==============================================================
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/server/gm-move_backups_offsite.sh,v $
-# $Id: gm-move_backups_offsite.sh,v 1.2 2007-02-19 10:35:14 ncq Exp $
+# $Id: gm-move_backups_offsite.sh,v 1.3 2007-06-05 15:00:31 ncq Exp $
 #
 # author: Karsten Hilbert
 # license: GPL v2
@@ -38,10 +38,19 @@ BACKUP_DIR="need to set this"
 # this is where you want the backup files to end up on $TARGET_HOST
 TARGET_DIR="need to set this"
 
+# the maximum bandwith, in KBytes/second, to utilize
+MAX_BANDWIDTH=""
+
 #==============================================================
 # There really should not be any need to
 # change anything below this line.
 #==============================================================
+
+if test -z ${MAX_BANDWITH} ; then
+	BW_LIMIT=""
+else
+	BW_LIMIT="--bwlimit=${MAX_BANDWIDTH}"
+fi
 
 BACKUP_FILE_GLOB="*.bz2"
 
@@ -50,7 +59,7 @@ LOG="${BACKUP_DIR}/backup.log"
 TS=`date +%Y-%m-%d-%H-%M-%S`
 
 if ping -c 3 -i 2 $TARGET_HOST > /dev/null; then
-	if rsync -qac ${BACKUP_DIR}/${BACKUP_FILE_GLOB} ${TARGET_HOST}:${TARGET_DIR}; then
+	if rsync -qac ${BW_LIMIT} ${BACKUP_DIR}/${BACKUP_FILE_GLOB} ${TARGET_HOST}:${TARGET_DIR}; then
 		echo "$HOST: $TS: rsynced backups to ${TARGET_HOST}: success" >> $LOG
 	else
 		echo "$HOST: $TS: rsyncing backups to ${TARGET_HOST}: failure" >> $LOG
@@ -61,7 +70,10 @@ fi;
 
 #==============================================================
 # $Log: gm-move_backups_offsite.sh,v $
-# Revision 1.2  2007-02-19 10:35:14  ncq
+# Revision 1.3  2007-06-05 15:00:31  ncq
+# - support max bandwidth utilization
+#
+# Revision 1.2  2007/02/19 10:35:14  ncq
 # - add some (ana)crontab lines and a few lines of documentation
 #
 # Revision 1.1  2007/02/16 15:33:37  ncq
