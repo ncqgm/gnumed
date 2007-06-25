707e6ea88e35e1e14fa09fe965044fde840541d1
Karsten Hilbert <karsten.hilbert@gmx.net>
2007-06-25 09:24:55 +0000

    - comment out log_script_insertion() fixup, only needed v5 -> v6 - allow skipping backups by command line arg 3 = "no-backup"

--- gnumed/gnumed/server/bootstrap/upgrade-db.sh
+++ gnumed/gnumed/server/bootstrap/upgrade-db.sh
@@ -19,8 +19,10 @@ ln -vsn client Gnumed
 cd -
 export PYTHONPATH="../../:${PYTHONPATH}"
 
+
 PREV_VER="$1"
 NEXT_VER="$2"
+SKIP_BACKUP="$3"
 LOG="update_db-v${PREV_VER}_v${NEXT_VER}.log"
 CONF="update_db-v${PREV_VER}_v${NEXT_VER}.conf"
 BAK_FILE="backup-upgrade-v${PREV_VER}-to-v${NEXT_VER}-"`hostname`".sql.bz2"
@@ -43,6 +45,7 @@ if test ! -f $CONF ; then
 	echo "   x+1 - database version to upgrade to (sequentially only)"
 	exit
 fi ;
+
 
 # if you need to adjust the database name to something
 # other than what the config file has you can use the
@@ -75,14 +78,28 @@ echo ""
 echo "The name of the new database will be \"gnumed_v${NEXT_VER}\"."
 echo "==========================================================="
 echo ""
+
+
 echo "1) creating backup of existing database ..."
 echo "   Note that this may take a substantial amount of time and disk space!"
 echo "   You may need to type in the password for gm-dbo."
-pg_dump -C -U gm-dbo -d gnumed_v${PREV_VER} ${PORT_DEF} | bzip2 -z9 > ${BAK_FILE}
+if test "$SKIP_BACKUP" != "no-backup" ; then
+	pg_dump -C -U gm-dbo -d gnumed_v${PREV_VER} ${PORT_DEF} | bzip2 -z9 > ${BAK_FILE}
+else
+	echo ""
+	echo "   !!! SKIPPED backup !!!"
+	echo ""
+	echo "   This will prevent you from restoring your database from"
+	echo "   a fresh backup should things go horribly wrong."
+	echo ""
+	echo "   Be sure you really know what you are doing !"
+fi ;
 echo ""
+
+
 echo "2) upgrading to new database ..."
 rm -rf ${LOG}
 # fixup for schema hash function - cannot be done inside bootstrapper
-echo "==> fixup for database hashing (will probably ask for gm-dbo password) ..."
-psql -U gm-dbo -d gnumed_v${PREV_VER} ${PORT_DEF} -f ../sql/gmConcatTableStructureFutureStub.sql
+#echo "==> fixup for database hashing (will probably ask for gm-dbo password) ..."
+#psql -U gm-dbo -d gnumed_v${PREV_VER} ${PORT_DEF} -f ../sql/gmConcatTableStructureFutureStub.sql
 ./bootstrap_gm_db_system.py --log-file=${LOG} --conf-file=${CONF}
