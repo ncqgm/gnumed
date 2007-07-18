8d84f1414f717c392e289ef7e64843abfae1d148
Karsten Hilbert <karsten.hilbert@gmx.net>
2007-07-18 14:28:23 +0000

    - we are now really getting into forms handling

---
+++ gnumed/gnumed/server/sql/v6-v7/static/ref-form_tables.sql
@@ -0,0 +1,68 @@
+-- ==============================================================
+-- GNUmed database schema change script
+--
+-- Source database version: v6
+-- Target database version: v7
+--
+-- License: GPL
+-- Author: karsten.hilbert@gmx.net
+-- 
+-- ==============================================================
+-- $Id: ref-form_tables.sql,v 1.1 2007-07-18 14:28:23 ncq Exp $
+-- $Revision: 1.1 $
+
+-- --------------------------------------------------------------
+\set ON_ERROR_STOP 1
+
+-- --------------------------------------------------------------
+create table ref.form_types (
+	name text
+		unique
+		not null,
+	pk serial primary key
+);
+
+insert into ref.form_types select * from public.form_types;
+
+-- --------------------------------------------------------------
+create table ref.form_defs (
+	pk serial primary key,
+	fk_type integer
+		references form_types(pk)
+		on update cascade
+		on delete restrict,
+	name_short text
+		not null,
+	name_long text
+		not null,
+	revision text
+		not null,
+	template bytea,
+	engine text
+		default 'T'
+		not null
+		check (engine in ('T', 'L', 'H', 'O')),
+	in_use boolean not null
+		default true,
+	unique (name_long, name_short),
+	unique (name_long, revision)
+) inherits (audit.audit_fields);
+
+insert into ref.form_defs (fk_type, name_short, name_long, revision, engine, in_use)
+	select fk_type, name_short, name_long, revision, engine, in_use from public.form_defs
+;
+
+drop table public.form_defs cascade;
+drop table public.form_types cascade;
+
+delete from audit.audited_tables where schema = 'public' and table_name = 'form_defs';
+
+-- --------------------------------------------------------------
+select gm.log_script_insertion('$RCSfile: ref-form_tables.sql,v $', '$Revision: 1.1 $');
+
+-- ==============================================================
+-- $Log: ref-form_tables.sql,v $
+-- Revision 1.1  2007-07-18 14:28:23  ncq
+-- - we are now really getting into forms handling
+--
+--
