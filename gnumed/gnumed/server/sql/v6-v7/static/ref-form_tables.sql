3fce2b433e9b91ddccebd45040c4a631e82e0d54
Karsten Hilbert <karsten.hilbert@gmx.net>
2007-08-20 14:38:42 +0000

    - public.form_defs -> ref.paperwork_templates, move full data - rename cols

--- gnumed/gnumed/server/sql/v6-v7/static/ref-form_tables.sql
+++ gnumed/gnumed/server/sql/v6-v7/static/ref-form_tables.sql
@@ -8,8 +8,8 @@
 -- Author: karsten.hilbert@gmx.net
 -- 
 -- ==============================================================
--- $Id: ref-form_tables.sql,v 1.4 2007-08-13 22:07:50 ncq Exp $
--- $Revision: 1.4 $
+-- $Id: ref-form_tables.sql,v 1.5 2007-08-20 14:38:42 ncq Exp $
+-- $Revision: 1.5 $
 
 -- --------------------------------------------------------------
 \set ON_ERROR_STOP 1
@@ -27,14 +27,14 @@ insert into ref.form_types select * from public.form_types;
 select setval(pg_get_serial_sequence('ref.form_types', 'pk'), (select max(pk) from ref.form_types) + 1);
 
 -- --------------------------------------------------------------
-create table ref.form_defs (
+create table ref.paperwork_templates (
 	pk serial primary key,
-	fk_type integer
+	fk_template_type integer
 		not null
 		references ref.form_types(pk)
 		on update cascade
 		on delete restrict,
-	document_type text
+	instance_type text
 		default null,
 	name_short text
 		not null,
@@ -51,15 +51,27 @@ create table ref.form_defs (
 		default true,
 	filename text
 		default null,
-	template bytea
+	data_md5 text
 		not null,
-	unique (name_long, name_short),
-	unique (name_long, revision)
+	data bytea
+		not null,
+	-- a certain revision of any given template can only exist once
+	unique (revision, name_long),
+	-- a certain form can only ever have one short alias, regardless of revision
+	unique (name_long, name_short)
 ) inherits (audit.audit_fields);
 
-insert into ref.form_defs (fk_type, name_short, name_long, revision, engine, in_use, template)
-	select fk_type, name_short, name_long, revision, engine, in_use, ''::bytea from public.form_defs
-;
+insert into ref.paperwork_templates (fk_template_type, name_short, name_long, revision, engine, in_use, data, data_md5)
+select
+	fk_type,
+	name_short,
+	name_long,
+	revision,
+	engine,
+	in_use,
+	decode(replace(template, '\\', '\\\\'), 'escape'),
+	md5(decode(replace(template, '\\', '\\\\'), 'escape'))
+from public.form_defs;
 
 drop table public.form_defs cascade;
 drop table audit.log_form_defs cascade;
@@ -68,11 +80,15 @@ drop table public.form_types cascade;
 delete from audit.audited_tables where schema = 'public' and table_name = 'form_defs';
 
 -- --------------------------------------------------------------
-select gm.log_script_insertion('$RCSfile: ref-form_tables.sql,v $', '$Revision: 1.4 $');
+select gm.log_script_insertion('$RCSfile: ref-form_tables.sql,v $', '$Revision: 1.5 $');
 
 -- ==============================================================
 -- $Log: ref-form_tables.sql,v $
--- Revision 1.4  2007-08-13 22:07:50  ncq
+-- Revision 1.5  2007-08-20 14:38:42  ncq
+-- - public.form_defs -> ref.paperwork_templates, move full data
+-- - rename cols
+--
+-- Revision 1.4  2007/08/13 22:07:50  ncq
 -- - add ref.form_defs.filename
 --
 -- Revision 1.3  2007/08/12 00:19:23  ncq
