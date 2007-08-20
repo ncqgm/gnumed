7d97ba9546aee0607c650d41535f6443e6982f00
Karsten Hilbert <karsten.hilbert@gmx.net>
2007-08-20 14:33:14 +0000

    - improved wording - add commented out vacuuming facility

--- gnumed/gnumed/server/bootstrap/upgrade-db.sh
+++ gnumed/gnumed/server/bootstrap/upgrade-db.sh
@@ -68,7 +68,7 @@ else
 	PORT_DEF=""
 fi ;
 
-
+echo ""
 echo "==========================================================="
 echo "Upgrading GNUmed database."
 echo ""
@@ -78,26 +78,26 @@ echo "Existing data is transferred and transformed as necessary."
 echo ""
 echo "The name of the new database will be \"gnumed_v${NEXT_VER}\"."
 echo "==========================================================="
+
+
 echo ""
-
-
 echo "1) creating backup of existing database ..."
-echo "   Note that this may take a substantial amount of time and disk space!"
-echo "   You may need to type in the password for gm-dbo."
 if test "$SKIP_BACKUP" != "no-backup" ; then
+	echo "   Note that this may take a substantial amount of time and disk space!"
+	echo "   You may need to type in the password for gm-dbo."
 	pg_dump -C -U gm-dbo -d gnumed_v${PREV_VER} ${PORT_DEF} | bzip2 -z9 > ${BAK_FILE}
 else
 	echo ""
 	echo "   !!! SKIPPED backup !!!"
 	echo ""
-	echo "   This will prevent you from restoring your database from"
-	echo "   a fresh backup should things go horribly wrong."
+	echo "   This may prevent you from being able to restore your"
+	echo "   database from a up-to-date backup should you need to."
 	echo ""
 	echo "   Be sure you really know what you are doing !"
 fi ;
+
+
 echo ""
-
-
 echo "2) upgrading to new database ..."
 rm -rf ${LOG}
 # fixup for schema hash function
@@ -106,3 +106,10 @@ rm -rf ${LOG}
 #echo "==> fixup for database hashing (will probably ask for gm-dbo password) ..."
 #psql -U gm-dbo -d gnumed_v${PREV_VER} ${PORT_DEF} -f ../sql/gmConcatTableStructureFutureStub.sql
 ./bootstrap_gm_db_system.py --log-file=${LOG} --conf-file=${CONF}
+
+
+#echo ""
+#echo "3) preparing new database for efficient use ..."
+#echo "   If the database is large this may take quite a while!"
+#echo "   You may need to type in the password for gm-dbo."
+#vacuumdb --full --analyze ${PORT_DEF} -U gm-dbo -d gnumed_v${NEXT_VER}
