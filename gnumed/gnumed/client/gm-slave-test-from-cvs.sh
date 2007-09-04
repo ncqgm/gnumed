ab11b9fa7784f2d0bf7a7216972126e033d7d428
Karsten Hilbert <karsten.hilbert@gmx.net>
2007-09-04 23:26:51 +0000

    - use unified config file

--- gnumed/gnumed/client/gm-slave-test-from-cvs.sh
+++ gnumed/gnumed/client/gm-slave-test-from-cvs.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/gm-slave-test-from-cvs.sh,v $
-# $Revision: 1.4 $
+# $Revision: 1.5 $
 
 # maybe force some locale setting here
 #export LANG=fr
 
 LOG="gm-slave-test.log"
-CONF="gm-slave-test.conf"
+CONF="gm-from-cvs.conf"
 
 # start GnuMed
 cd ../
@@ -17,7 +17,7 @@ export PYTHONPATH="${PYTHONPATH}:../"
 rm -vf $LOG
 cp -vf ${CONF} tmp-${CONF}
 
-python wxpython/gnumed.py --log-file=${LOG} --conf-file=tmp-${CONF} --debug
+python wxpython/gnumed.py --log-file=${LOG} --conf-file=tmp-${CONF} --debug --slave
 # clean up
 rm -vf tmp-${CONF}
 rm -vf tmp-${CONF}.gmCfg.bak
