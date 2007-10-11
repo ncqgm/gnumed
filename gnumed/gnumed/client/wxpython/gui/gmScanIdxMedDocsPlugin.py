513d0eff7d3b88a7d08a43eca67234498e087c34
Karsten Hilbert <karsten.hilbert@gmx.net>
2007-10-11 16:42:28 +0000

    - cleanp cruft

--- gnumed/gnumed/client/wxpython/gui/gmScanIdxMedDocsPlugin.py
+++ gnumed/gnumed/client/wxpython/gui/gmScanIdxMedDocsPlugin.py
@@ -2,20 +2,12 @@
 # GNUmed scan and index plugin
 #=====================================================
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/wxpython/gui/gmScanIdxMedDocsPlugin.py,v $
-__version__ = "$Revision: 1.4 $"
+__version__ = "$Revision: 1.4.12.1 $"
 __author__ = "Sebastian Hilbert <Sebastian.Hilbert@gmx.net>\
               Karsten Hilbert <Karsten.Hilbert@gmx.net>"
 __license__ = "GPL"
 
-try:
-    import wxversion
-    import wx
-except ImportError:
-    from wxPython import wx
-
 from Gnumed.wxpython import gmPlugin, gmMedDocWidgets
-from Gnumed.pycommon import gmLog
-_log = gmLog.gmDefLog
 
 #====================================
 class gmScanIdxMedDocsPlugin(gmPlugin.cNotebookPlugin):
@@ -40,7 +32,10 @@ class gmScanIdxMedDocsPlugin(gmPlugin.cNotebookPlugin):
 		return 1
 #======================================================================
 # $Log: gmScanIdxMedDocsPlugin.py,v $
-# Revision 1.4  2005-11-27 12:46:42  ncq
+# Revision 1.4.12.1  2007-10-11 16:42:28  ncq
+# - cleanp cruft
+#
+# Revision 1.4  2005/11/27 12:46:42  ncq
 # - cleanup
 #
 #
