758ea44b90a6a4734de37b783e5cc78a1abc21a6
Karsten Hilbert <karsten.hilbert@gmx.net>
2007-10-18 18:05:01 +0000

    - rearrange, clean up and make runnable

--- gnumed/gnumed/client/wxpython/gui/gmSnellen.py
+++ gnumed/gnumed/client/wxpython/gui/gmSnellen.py
@@ -6,15 +6,20 @@ FIXME: store screen size
 """
 #============================================================================
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/wxpython/gui/Attic/gmSnellen.py,v $
-# $Id: gmSnellen.py,v 1.20 2007-10-12 07:28:25 ncq Exp $
-__version__ = "$Revision: 1.20 $"
+# $Id: gmSnellen.py,v 1.21 2007-10-18 18:05:01 ncq Exp $
+__version__ = "$Revision: 1.21 $"
 __author__ = "Ian Haywood, Karsten Hilbert <Karsten.Hilbert@gmx.net>"
 __license__ = "GPL (details at http://www.gnu.org)"
 
-import math, random
+import math, random, sys
+
 
 import wx
 
+
+if __name__ == '__main__':
+	sys.path.insert(0, '../../../')
+	_ = lambda x:x
 from Gnumed.pycommon import gmLog, gmI18N
 #from Gnumed.wxpython import gmPlugin
 
@@ -36,217 +41,146 @@ class cSnellenChart(wx.Frame):
 			int ((Y * self.blockY) + self.startY)
 		)
 
-	def rect (self, x1, y1, x2, y2):
-		"""
-		Draw a rectangle
-		"""
-		x1, y1 = self.convert (x1, y1)
-		x2, y2 = self.convert (x2, y2)
-		width = x2 - x1
-		if width < 0:
-			width = -width
-			x = x2
-		else:
-			x = x1
-		height = y2 - y1
-		if height < 0:
-			height = -height
-			y = y2
-		else:
-			y = y1
-		self.dc.DrawRectangle (x, y, width, height)
-
-	def straight (self, x1, y1, x2, y2, width = 1):
-		"""
-		Draws straight descending letter-stroke, (x1, y1) is top-left,
-		(x2, y2) is bottom left point
-		"""
-		
-		list = [self.convert (x1, y1), self.convert (x1+width, y1),
-		self.convert (x2+width, y2), self.convert (x2, y2)]
-		self.dc.DrawPolygon (list)
-
-	def reverse (self):
-		"""
-		Swap fore- and background pens
-		"""
-		background = self.dc.GetBackground ()
-		foreground = self.dc.GetBrush ()
-		self.dc.SetBrush (background)
-		self.dc.SetBackground (foreground)
-		
-
-	def arc (self, x, y, arm, start, end):
-		"""
-		Draws an arc-stroke, 1 unit wide, subtending (x, y), the outer
-		distance is arm, between start and end angle
-		"""
-		topx, topy = self.convert (x-arm, y-arm)
-		botx, boty = self.convert (x+arm, y+arm)
-		width = botx - topx
-		if width < 0:
-			width = -width
-			t = botx
-			botx = topx
-			topx = t
-		height = boty - topy
-		self.dc.DrawEllipticArc(topx, topy, width, height, start, end)
-		# now do wedge as background, to give arc pen-stroke
-		arm -= 1
-		self.reverse ()
-		topx, topy = self.convert (x-arm, y-arm)
-		botx, boty = self.convert (x+arm, y+arm)
-		width = botx - topx
-		if width < 0:
-			width = -width
-			t = botx
-			botx = topx
-			topx = t
-		height = boty- topy
-		self.dc.DrawEllipticArc(topx, topy, width, height, start, end)
-		self.reverse ()
-
 	def O (self):
 		"""
 		Draws the letter O
 		"""
-		self.arc (2.5, 2.5, 2.5, 0, 360)
+		self._draw_arc (2.5, 2.5, 2.5, 0, 360)
 
 	def Q (self):
-		self.O ()
-		self.straight (2.6, 3, 4, 5)
+		self.O()
+		self._draw_line (2.6, 3, 4, 5)
 
 	def C (self):
 		if self.mirror:
-			self.arc (2.5, 2.5, 2.5, 140, -140)
+			self._draw_arc (2.5, 2.5, 2.5, 140, -140)
 		else:
-			self.arc (2.5, 2.5, 2.5, 40, 320)
+			self._draw_arc (2.5, 2.5, 2.5, 40, 320)
 
 	def G (self):
 		if self.mirror:
-			self.arc (2.5, 2.5, 2.5, 140, -150)
+			self._draw_arc (2.5, 2.5, 2.5, 140, -150)
 		else:
-			self.arc (2.5, 2.5, 2.5, 40, 330)
-		self.rect (2.5, 2.7, 5, 3.7)
-		self.rect (4, 2.7, 5, 5)
+			self._draw_arc (2.5, 2.5, 2.5, 40, 330)
+		self._draw_rect (2.5, 2.7, 5, 3.7)
+		self._draw_rect (4, 2.7, 5, 5)
 
 	def W (self):
-		self.straight (0, 0, 1, 5)
-		self.straight (2, 0, 1, 5)
-		self.straight (2, 0, 3, 5)
-		self.straight (4, 0, 3, 5)
+		self._draw_line (0, 0, 1, 5)
+		self._draw_line (2, 0, 1, 5)
+		self._draw_line (2, 0, 3, 5)
+		self._draw_line (4, 0, 3, 5)
 
 	def V (self):
-		self.straight (0, 0, 2, 5)
-		self.straight (4, 0, 2, 5)
+		self._draw_line (0, 0, 2, 5)
+		self._draw_line (4, 0, 2, 5)
 
 	def T (self):
-		self.rect (0, 0, 5, 1)
-		self.rect (2, 1, 3, 5)
+		self._draw_rect (0, 0, 5, 1)
+		self._draw_rect (2, 1, 3, 5)
 
 	def I (self):
-		self.rect (2, 0, 3, 5)
+		self._draw_rect (2, 0, 3, 5)
 
 	def A (self):
-		self.straight (2, 0, 0, 5)
-		self.straight (2, 0, 4, 5)
-		self.rect (1.4, 2.5, 3.6, 3.5)
+		self._draw_line (2, 0, 0, 5)
+		self._draw_line (2, 0, 4, 5)
+		self._draw_rect (1.4, 2.5, 3.6, 3.5)
 
 	def F (self):
-		self.rect (0, 0, 1, 5)
-		self.rect (1, 0, 5, 1)
-		self.rect (1, 2, 5, 3)
+		self._draw_rect (0, 0, 1, 5)
+		self._draw_rect (1, 0, 5, 1)
+		self._draw_rect (1, 2, 5, 3)
 
 	def E (self):
-		self.rect (0, 0, 1, 5)
-		self.rect (0, 0, 5, 1)
-		self.rect (0, 2, 5, 3)
-		self.rect (0, 4, 5, 5)
+		self._draw_rect (0, 0, 1, 5)
+		self._draw_rect (0, 0, 5, 1)
+		self._draw_rect (0, 2, 5, 3)
+		self._draw_rect (0, 4, 5, 5)
 
 	def BackE (self):
-		self.rect (4, 0, 5, 5)
-		self.rect (0, 0, 5, 1)
-		self.rect (0, 2, 5, 3)
-		self.rect (0, 4, 5, 5)
+		self._draw_rect (4, 0, 5, 5)
+		self._draw_rect (0, 0, 5, 1)
+		self._draw_rect (0, 2, 5, 3)
+		self._draw_rect (0, 4, 5, 5)
 
 	def UpE (self):
-		self.rect (0, 4, 5, 5)
-		self.rect (0, 0, 1, 5)
-		self.rect (2, 0, 3, 5)
-		self.rect (4, 0, 5, 5)
+		self._draw_rect (0, 4, 5, 5)
+		self._draw_rect (0, 0, 1, 5)
+		self._draw_rect (2, 0, 3, 5)
+		self._draw_rect (4, 0, 5, 5)
 
 	def DownE (self):
-		self.rect (0, 0, 5, 1)
-		self.rect (0, 0, 1, 5)
-		self.rect (2, 0, 3, 5)
-		self.rect (4, 0, 5, 5)
+		self._draw_rect (0, 0, 5, 1)
+		self._draw_rect (0, 0, 1, 5)
+		self._draw_rect (2, 0, 3, 5)
+		self._draw_rect (4, 0, 5, 5)
 
 	def H (self):
-		self.rect (0, 0, 1, 5)
-		self.rect (4, 0, 5, 5)
-		self.rect (1, 2, 4, 3)
+		self._draw_rect (0, 0, 1, 5)
+		self._draw_rect (4, 0, 5, 5)
+		self._draw_rect (1, 2, 4, 3)
 
 	def K (self):
-		self.rect (0, 0, 1, 5)
-		self.straight (3.5, 0, 0.5, 2.5, width = 1.5)
-		self.straight (0.5, 2.5, 3.5, 5, width = 1.5)
+		self._draw_rect (0, 0, 1, 5)
+		self._draw_line (3.5, 0, 0.5, 2.5, width = 1.5)
+		self._draw_line (0.5, 2.5, 3.5, 5, width = 1.5)
 
 	def L (self):
-		self.rect (0, 0, 1, 5)
-		self.rect (1, 4, 5, 5)
+		self._draw_rect (0, 0, 1, 5)
+		self._draw_rect (1, 4, 5, 5)
 
 	def Z (self):
-		self.rect (0, 0, 5, 1)
-		self.rect (0, 4, 5, 5)
-		self.straight (3.5, 1, 0, 4, width = 1.5)
+		self._draw_rect (0, 0, 5, 1)
+		self._draw_rect (0, 4, 5, 5)
+		self._draw_line (3.5, 1, 0, 4, width = 1.5)
 
 	def X (self):
-		self.straight (4, 0, 0, 5)
-		self.straight (0, 0, 4, 5)
+		self._draw_line (4, 0, 0, 5)
+		self._draw_line (0, 0, 4, 5)
 
 	def NM (self):
+		"""Sidebars common to N and M
 		"""
-		Sidebars common to N and M
-		"""
-		self.rect (0, 0, 1, 5)
-		self.rect (4, 0, 5, 5)
+		self._draw_rect (0, 0, 1, 5)
+		self._draw_rect (4, 0, 5, 5)
 
 	def N (self):
 		self.NM ()
-		self.straight (0, 0, 4, 5)
+		self._draw_line (0, 0, 4, 5)
 
 	def BackN (self):
 		self.NM ()
-		self.straight (4, 0, 0, 5)
+		self._draw_line (4, 0, 0, 5)
 
 	def M (self):
 		self.NM ()
-		self.straight (0, 0, 2, 5)
-		self.straight (4, 0, 2, 5)
+		self._draw_line (0, 0, 2, 5)
+		self._draw_line (4, 0, 2, 5)
 
 	def gamma (self):
-		self.rect (0, 0, 5, 1)
-		self.rect (0, 0, 1, 5)
+		self._draw_rect (0, 0, 5, 1)
+		self._draw_rect (0, 0, 1, 5)
 
 	def delta (self):
-		self.straight (2, 0, 0, 5)
-		self.straight (2, 0, 4, 5)
-		self.rect (0.5, 4, 4.5, 5)
+		self._draw_line (2, 0, 0, 5)
+		self._draw_line (2, 0, 4, 5)
+		self._draw_rect (0.5, 4, 4.5, 5)
 
 	def pi (self):
-		self.rect (0, 0, 5, 1)
-		self.rect (0, 0, 1, 5)
-		self.rect (4, 0, 5, 5)
+		self._draw_rect (0, 0, 5, 1)
+		self._draw_rect (0, 0, 1, 5)
+		self._draw_rect (4, 0, 5, 5)
 
 	def cross (self):
-		self.rect (2, 0, 3, 5)
-		self.rect (0, 2, 5, 3)
+		self._draw_rect (2, 0, 3, 5)
+		self._draw_rect (0, 2, 5, 3)
 
 	def box (self):
-		self.rect (0, 0, 5, 1)
-		self.rect (0, 4, 5, 5)
-		self.rect (0, 1, 1, 4)
-		self.rect (4, 1, 5, 4)
+		self._draw_rect (0, 0, 5, 1)
+		self._draw_rect (0, 4, 5, 5)
+		self._draw_rect (0, 1, 1, 4)
+		self._draw_rect (4, 1, 5, 4)
 
 	def star (self):
 		"""
@@ -283,9 +217,13 @@ class cSnellenChart(wx.Frame):
 
 	symbol = [O, cross, star, box]
 
-	alphabets = {_("Latin"):latin, _("Greek"):greek,
-				 _("Cyrillic"):cyrillic, _("Four Es"):fourE,
-				 _("Symbol"):symbol}
+	alphabets = {
+		_("Latin"): latin,
+		_("Greek"): greek,
+		_("Cyrillic"): cyrillic,
+		_("Four Es"): fourE,
+		_("Symbol"): symbol
+	}
 
 	def __init__(self, width, height, alpha = symbol, mirr = 0, parent = None):
 		"""
@@ -293,101 +231,65 @@ class cSnellenChart(wx.Frame):
 		CRT in cm.
 		"""
 		wx.Frame.__init__ (self, parent, -1, _("Snellen Chart"))
-		wx.EVT_CLOSE (self, self.OnClose)
+
 		# width/Y is screen size (X/Y in cm)
-		# distance is distance in metres between CRT and the "average" patient
-		self.width = width
-		self.height = height
-		self.distances = [3, 5, 6, 7.5, 9, 12, 15, 18, 24, 30, 48, 60]
+		#wx.SystemSettings.GetMetric(wx.SYS_SCREEN_X)
+#		screensizes = {_("14 inch"):(28, 21), _("16 inch"):(30, 23)}
+		self.screen_width_cm = width
+		self.screen_height_cm = height
+
+		self.screen_width_pixel = 0
+		self.screen_height_pixel = 0
+
+		self.standard_patient_chart_distances = [3, 5, 6, 7.5, 9, 12, 15, 18, 24, 30, 48, 60] # in metres
 		self.mirror = mirr
 		self.alphabet = alpha
+
+		wx.EVT_CLOSE (self, self.OnClose)
 		wx.EVT_KEY_DOWN (self, self.OnKeyUp)
 		wx.EVT_LEFT_UP (self, self.OnLeftDown)
 		wx.EVT_RIGHT_UP (self, self.OnRightDown)
 		wx.EVT_LEFT_DCLICK (self, self.OnDClick)
 		wx.EVT_PAINT (self, self.OnPaint)
-		wx.EVT_WINDOW_CREATE (self, self.OnCreate)
-		screensizes = {_("14 inch"):(28, 21), _("16 inch"):(30, 23)}
-		self.screen_x = 0
-		self.screen_y = 0
+#		wx.EVT_WINDOW_CREATE (self, self.OnCreate)
 
-	def OnCreate (self, event):
-		self.ShowFullScreen (1)
-		
-	def set_distance (self, n):
-		"""
-		Sets standard viewing distance, against which patient is
-		compared. n is an index to the list self.distances
-		"""
-		self.distance = n
-		# Snellen characters are the smallest readable characters by
-		# an average person at the stated distance. They are defined
-		# exactly as being in a box which subtends 5' of an arc on the
-		# patient's eye, each stroke subtending 1' of arc
-		one_minute = (math.pi/180)/60
-		blocksize = self.distances[n]*100*math.atan(one_minute) # in cm
-		# convert to pixels
-		self.blockX = int (blocksize/self.width*self.screen_x)
-		self.blockY = int (blocksize/self.height*self.screen_y)
-		# how many characters can we fit now?
-		chars = int (self.screen_x / (self.blockX*5)) - 1
-		if chars < 1:
-			chars = 1
-		if chars > 7:
-			chars = 7
-		if chars < len (self.alphabet):
-			self.choices = []
-			while len (self.choices) < chars:
-				c = random.choice (self.alphabet)
-				if not c in self.choices:
-					self.choices.append (c)
-		else:
-			self.choices = [random.choice (self.alphabet) for i in
-							range (1, chars)]
-		self.spacing = int ((self.screen_x -
-		(chars*self.blockX*5))/(chars+1))
-		if self.spacing < 0:
-			self.spacing = 0
-		self.startY = int ((self.screen_y-(self.blockY*5))/2)
+		self.ShowFullScreen(1)
+	#---------------------------------
+	# event handlers
+	#---------------------------------
+#	def OnCreate(self, event):
+#		self.ShowFullScreen(1)
+	#---------------------------------
+	def OnPaint (self, event):
+		self.dc = wx.PaintDC(self)
+		if self.screen_width_pixel == 0:
+			self.screen_width_pixel, self.screen_height_pixel = self.GetClientSizeTuple()
+			self.set_distance(2)
+#			gmLog.gmDefLog.Log (gmLog.lInfo, 'I think the screen size is %d x %d' % (self.screen_width_pixel, self.screen_height_pixel))
 
-	def draw (self):
-		"""
-		displays characters in the centre of the screen from the
-		selected alphabet
-		"""
+#		self.setup_DC()
+		self.dc.SetFont(wx.Font (36, wx.ROMAN, wx.NORMAL, wx.NORMAL))
+		self.dc.SetBrush(wx.BLACK_BRUSH)
+		self.dc.SetBackground(wx.WHITE_BRUSH)
+		self.dc.SetPen(wx.TRANSPARENT_PEN)
 
+#		self.draw ()
 		# clear the screen
-		self.reverse ()
-		self.dc.DrawRectangle (0,0, self.screen_x, self.screen_y)
-		self.reverse ()
+		self._swap_fg_bg_col()
+		self.dc.DrawRectangle(0, 0, self.screen_width_pixel, self.screen_height_pixel)
+		self._swap_fg_bg_col ()
 		# draw size
-		self.dc.DrawText (str(self.distances[self.distance]), 20, 20)
+		self.dc.DrawText (str(self.standard_patient_chart_distances[self.distance]), 20, 20)
 		self.startX = self.spacing
 		for i in self.choices:
 			i (self)
 			self.startX += self.blockX*5
 			self.startX += self.spacing
 
-
-	def setup_DC (self):
-		self.dc.SetFont (wx.Font (36, wx.ROMAN, wx.NORMAL, wx.NORMAL))
-		self.dc.SetBrush (wx.BLACK_BRUSH)
-		self.dc.SetBackground (wx.WHITE_BRUSH)
-		self.dc.SetPen (wx.TRANSPARENT_PEN)
-
-
-	def OnPaint (self, event):
-		self.dc = wx.PaintDC (self)
-		if self.screen_x == 0:
-			self.screen_x, self.screen_y = self.GetClientSizeTuple ()
-			self.set_distance (2)
-			gmLog.gmDefLog.Log (gmLog.lInfo, 'I think the screen size is %d x %d' % (self.screen_x, self.screen_y))
-		self.setup_DC ()
-		self.draw ()
-		self.dc = None 
+		self.dc = None
 
 	def OnKeyUp (self, key):
-		if key.GetKeyCode() == wx.K_UP and self.distance < len (self.distances)-1:
+		if key.GetKeyCode() == wx.K_UP and self.distance < len (self.standard_patient_chart_distances)-1:
 			self.set_distance (self.distance+1)
 		if key.GetKeyCode() == wx.K_DOWN and self.distance > 0:
 			self.set_distance (self.distance-1)
@@ -400,7 +302,7 @@ class cSnellenChart(wx.Frame):
 		self.Refresh ()
 
 	def OnRightDown (self, key):
-		if self.distance < len(self.distances)-1:
+		if self.distance < len(self.standard_patient_chart_distances)-1:
 			self.set_distance (self.distance+1)
 		self.Refresh()
 
@@ -413,6 +315,140 @@ class cSnellenChart(wx.Frame):
 	def DestroyWhenApp (self):
 		import sys
 		sys.exit (0)
+
+	#---------------------------------
+	# internal API
+	#---------------------------------
+	def _swap_fg_bg_col(self):
+		"""Swap fore- and background pens."""
+		background = self.dc.GetBackground()
+		foreground = self.dc.GetBrush()
+		self.dc.SetBrush(background)
+		self.dc.SetBackground(foreground)
+	#---------------------------------
+	def _draw_rect(self, x1, y1, x2, y2):
+		"""Draw a rectangle."""
+		x1, y1 = self.convert (x1, y1)
+		x2, y2 = self.convert (x2, y2)
+
+		width = x2 - x1
+		if width < 0:
+			width = -width
+			x = x2
+		else:
+			x = x1
+
+		height = y2 - y1
+		if height < 0:
+			height = -height
+			y = y2
+		else:
+			y = y1
+
+		self.dc.DrawRectangle(x, y, width, height)
+	#---------------------------------
+	def _draw_arc (self, x, y, arm, start, end):
+		"""
+		Draws an arc-stroke, 1 unit wide, subtending (x, y), the outer
+		distance is arm, between start and end angle
+		"""
+		topx, topy = self.convert (x-arm, y-arm)
+		botx, boty = self.convert (x+arm, y+arm)
+		width = botx - topx
+		if width < 0:
+			width = -width
+			t = botx
+			botx = topx
+			topx = t
+		height = boty - topy
+		self.dc.DrawEllipticArc(topx, topy, width, height, start, end)
+		# now do wedge as background, to give arc pen-stroke
+		arm -= 1
+		self._swap_fg_bg_col()
+		topx, topy = self.convert (x-arm, y-arm)
+		botx, boty = self.convert (x+arm, y+arm)
+		width = botx - topx
+		if width < 0:
+			width = -width
+			t = botx
+			botx = topx
+			topx = t
+		height = boty- topy
+		self.dc.DrawEllipticArc(topx, topy, width, height, start, end)
+		self._swap_fg_bg_col()
+	#---------------------------------
+	def _draw_line(self, x1, y1, x2, y2, width = 1):
+		"""Draws straight descending letter-stroke.
+
+		(x1, y1) is top-left,
+		(x2, y2) is bottom left point
+		"""
+		coords = [
+			self.convert (x1, y1),
+			self.convert (x1+width, y1),
+			self.convert (x2+width, y2),
+			self.convert (x2, y2)
+		]
+		self.dc.DrawPolygon(coords)
+	#---------------------------------
+	#---------------------------------
+	def set_distance (self, n):
+		"""
+		Sets standard viewing distance, against which patient is
+		compared. n is an index to the list self.standard_patient_chart_distances
+		"""
+		self.distance = n
+		# Snellen characters are the smallest readable characters by
+		# an average person at the stated distance. They are defined
+		# exactly as being in a box which subtends 5' of an arc on the
+		# patient's eye, each stroke subtending 1' of arc
+		one_minute = (math.pi / 180) / 60
+		blocksize = (self.standard_patient_chart_distances[n] * 100) * math.atan(one_minute) # in cm
+		# convert to pixels
+		self.blockX = int (blocksize / self.screen_width_cm * self.screen_width_pixel)
+		self.blockY = int (blocksize / self.screen_height_cm * self.screen_height_pixel)
+		# how many characters can we fit now?
+		chars = int (self.screen_width_pixel / (self.blockX*5)) - 1
+		if chars < 1:
+			chars = 1
+		if chars > 7:
+			chars = 7
+		if chars < len (self.alphabet):
+			self.choices = []
+			while len (self.choices) < chars:
+				c = random.choice (self.alphabet)
+				if not c in self.choices:
+					self.choices.append (c)
+		else:
+			self.choices = [ random.choice(self.alphabet) for i in range(1, chars) ]
+		self.spacing = int ((self.screen_width_pixel -
+		(chars*self.blockX*5))/(chars+1))
+		if self.spacing < 0:
+			self.spacing = 0
+		self.startY = int ((self.screen_height_pixel-(self.blockY*5))/2)
+
+#	def draw (self):
+#		"""
+#		displays characters in the centre of the screen from the
+#		selected alphabet
+#		"""
+#		# clear the screen
+#		self._swap_fg_bg_col()
+#		self.dc.DrawRectangle (0,0, self.screen_width_pixel, self.screen_height_pixel)
+#		self._swap_fg_bg_col ()
+#		# draw size
+#		self.dc.DrawText (str(self.standard_patient_chart_distances[self.distance]), 20, 20)
+#		self.startX = self.spacing
+#		for i in self.choices:
+#			i (self)
+#			self.startX += self.blockX*5
+#			self.startX += self.spacing
+
+#	def setup_DC (self):
+#		self.dc.SetFont (wx.Font (36, wx.ROMAN, wx.NORMAL, wx.NORMAL))
+#		self.dc.SetBrush (wx.BLACK_BRUSH)
+#		self.dc.SetBackground (wx.WHITE_BRUSH)
+#		self.dc.SetPen (wx.TRANSPARENT_PEN)
 
 #============================================================================
 class cSnellenCfgDlg (wx.Dialog):
@@ -428,6 +464,10 @@ class cSnellenCfgDlg (wx.Dialog):
 			wx.DefaultPosition,
 			wx.Size(350, 200)
 		)
+
+#		print wx.DisplaySize()
+#		print wx.DisplaySizeMM()
+
 		vbox = wx.BoxSizer (wx.VERTICAL)
 		hbox1 = wx.BoxSizer (wx.HORIZONTAL)
 		hbox1.Add (wx.StaticText(self, -1, _("Screen Height (cm): ")), 0, wx.ALL, 15)
@@ -441,7 +481,7 @@ class cSnellenCfgDlg (wx.Dialog):
 		vbox.Add (hbox2, 1, wx.EXPAND)
 		hbox3 = wx.BoxSizer (wx.HORIZONTAL)
 		hbox3.Add (wx.StaticText(self, -1, _("Alphabet: ")), 0, wx.ALL, 15)
-		self.alpha_ctrl = wx.Choice (self, -1, choices=cSnellenChart.alphabets.keys ())
+		self.alpha_ctrl = wx.Choice (self, -1, choices = cSnellenChart.alphabets.keys ())
 		hbox3.Add (self.alpha_ctrl, 1, wx.TOP, 15)
 		vbox.Add (hbox3, 1, wx.EXPAND)
 		self.mirror_ctrl = wx.CheckBox (self, -1, label = _("Mirror"))
@@ -513,6 +553,10 @@ double-click ends""")), 0, wx.ALL, 15)
 # main
 #----------------------------------------------------------------------------
 if __name__ == '__main__':
+
+	gmI18N.activate_locale()
+	gmI18N.install_domain('gnumed')
+
 	class TestApp (wx.App):
 		def OnInit (self):
 			cfg = cSnellenCfgDlg()
@@ -537,7 +581,10 @@ if __name__ == '__main__':
 	main()
 #============================================================================
 # $Log: gmSnellen.py,v $
-# Revision 1.20  2007-10-12 07:28:25  ncq
+# Revision 1.21  2007-10-18 18:05:01  ncq
+# - rearrange, clean up and make runnable
+#
+# Revision 1.20  2007/10/12 07:28:25  ncq
 # - lots of import related cleanup
 #
 # Revision 1.19  2007/01/20 22:53:32  ncq
