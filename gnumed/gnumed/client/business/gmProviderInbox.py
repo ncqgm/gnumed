8e670057f579449e04e7083ce6dbc02266df695c
Karsten Hilbert <karsten.hilbert@gmx.net>
2007-10-30 12:47:53 +0000

    - fix test suite - make messages a property on inbox

--- gnumed/gnumed/client/business/gmProviderInbox.py
+++ gnumed/gnumed/client/business/gmProviderInbox.py
@@ -5,11 +5,17 @@ This should eventually end up in a class cPractice.
 """
 #============================================================
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/business/gmProviderInbox.py,v $
-# $Id: gmProviderInbox.py,v 1.9 2007-03-01 13:51:13 ncq Exp $
+# $Id: gmProviderInbox.py,v 1.10 2007-10-30 12:47:53 ncq Exp $
 __license__ = "GPL"
-__version__ = "$Revision: 1.9 $"
+__version__ = "$Revision: 1.10 $"
 __author__ = "K.Hilbert <Karsten.Hilbert@gmx.net>"
 
+
+import sys
+
+
+if __name__ == '__main__':
+	sys.path.insert(0, '../../')
 from Gnumed.pycommon import gmPG2
 
 #============================================================
@@ -21,7 +27,7 @@ class cProviderInbox:
 		else:
 			self.__provider_id = provider_id
 	#--------------------------------------------------------
-	def get_messages(self):
+	def _get_messages(self):
 		cmd = u"""
 select
 	importance,
@@ -51,19 +57,37 @@ order by importance desc"""
 				None
 			]]
 		return rows
+
+	def _set_messages(self, messages):
+		return
+
+	messages = property(_get_messages, _set_messages)
 	#--------------------------------------------------------
 	def delete_message(self, pk=None):
 		gmPG2.run_rw_queries(queries = [{'cmd': u"delete from dem.provider_inbox where pk=%s", 'args': [pk]}])
 		return True
 #============================================================
 if __name__ == '__main__':
-	inbox = cProviderInbox()
-	for msg in inbox.get_messages():
-		print msg
+
+	from Gnumed.pycommon import gmI18N
+	from Gnumed.business import gmPerson
+
+	gmI18N.activate_locale()
+	gmI18N.install_domain()
+
+	if (len(sys.argv) > 1) and (sys.argv[1] == 'test'):
+		gmPerson.gmCurrentProvider(provider = gmPerson.cStaff())
+		inbox = cProviderInbox()
+		for msg in inbox.messages:
+			print msg
 
 #============================================================
 # $Log: gmProviderInbox.py,v $
-# Revision 1.9  2007-03-01 13:51:13  ncq
+# Revision 1.10  2007-10-30 12:47:53  ncq
+# - fix test suite
+# - make messages a property on inbox
+#
+# Revision 1.9  2007/03/01 13:51:13  ncq
 # - remove call to _log
 #
 # Revision 1.8  2006/10/08 15:10:01  ncq
