92a623bd3b6baa274a30315d55d2ad8f94bbf9eb
Karsten Hilbert <karsten.hilbert@gmx.net>
2007-12-20 13:57:01 +0000

    - not needed anymore

--- gnumed/gnumed/dists/GMDocsCD/burn-cd.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-BURN="/usr/local/bin/cdrecord"
-
-echo "burning ISO file $1 onto CD"
-$BURN -v gracetime=4 dev=/dev/hdc speed=8 -tao driveropts=burnfree -eject -overburn -data $1
--- gnumed/gnumed/dists/GMDocsCD/get-latest-snapshot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "getting a fresh gnumed cvs snapshot"
-httrack -%i -w http://salaam.homeunix.com/~ncq/gnumed/snapshot/gnumed-latest-snapshot.tgz \
- -O "/home/basti/Firma/gnumed-devel-cd" -%P -N0 -s0 -p7 -S -a -K0 -%k -A25000 \
- -F "Mozilla/4.5 (compatible; HTTrack 3.0x; Windows 98)" -%F '' \
- -%s -P "proxy.uni-leipzig.de:3128" -x -%x -%u -%U www-data \
-
