ced5d5460c875ba8d2f5da9f368ef2c57bd32b95
Karsten Hilbert <karsten.hilbert@gmx.net>
2007-12-26 14:35:29 +0000

    - no more old cli handling

--- gnumed/gnumed/client/pycommon/tools/show_img.py
+++ gnumed/gnumed/client/pycommon/tools/show_img.py
@@ -2,22 +2,17 @@
 ###############################################################
 # This is a tool to show images from files in client/artwork/
 #
-# @Date: $Date: 2005-09-27 20:27:41 $
+# @Date: $Date: 2007-12-26 14:35:29 $
 ###############################################################
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/pycommon/tools/show_img.py,v $
-__version__ = "$Revision: 1.2 $"
+__version__ = "$Revision: 1.3 $"
 __license__ = "GPL (details at http://www.gnu.org)"
 __author__ = "Sebastian Hilbert"
 
 import cPickle, zlib, string 
 
-try:
-	import wxversion
-	import wx
-except ImportError:
-	from wxPython import wx
+import wx
 
-from Gnumed.pycommon import gmCLI
 
 class indexPnl(wxPanel):
 	def __init__(self, parent):
@@ -32,7 +27,6 @@ class indexPnl(wxPanel):
 			parent = self,
 			style = wxTAB_TRAVERSAL
 		)
-#		file = gmCLI.arg['--image']
 		image = gmCLI.arg['--image']
 #		image = string.split(file,'/')[-1][:-3]
 		exec "from Gnumed.artwork import %s as image" % image
@@ -60,7 +54,10 @@ if __name__ == '__main__':
 
 #=====================================================================
 # $Log: show_img.py,v $
-# Revision 1.2  2005-09-27 20:27:41  ncq
+# Revision 1.3  2007-12-26 14:35:29  ncq
+# - no more old cli handling
+#
+# Revision 1.2  2005/09/27 20:27:41  ncq
 # - wx2.6 fix
 #
 # Revision 1.1  2004/06/21 18:47:10  ncq
