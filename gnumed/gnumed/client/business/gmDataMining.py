cbe97d5f0731214651cd9600cd730dcbf1214c71
Karsten Hilbert <karsten.hilbert@gmx.net>
2008-01-05 23:24:29 +0000

    - fixup test framework

--- gnumed/gnumed/client/business/gmDataMining.py
+++ gnumed/gnumed/client/business/gmDataMining.py
@@ -1,9 +1,9 @@
 """GNUmed data mining middleware."""
 #============================================================
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/business/gmDataMining.py,v $
-# $Id: gmDataMining.py,v 1.2 2007-04-08 21:16:16 ncq Exp $
+# $Id: gmDataMining.py,v 1.3 2008-01-05 23:24:29 ncq Exp $
 __license__ = "GPL"
-__version__ = "$Revision: 1.2 $"
+__version__ = "$Revision: 1.3 $"
 __author__ = "K.Hilbert <Karsten.Hilbert@gmx.net>"
 
 
@@ -46,19 +46,23 @@ def delete_report_definition(name=None):
 #============================================================
 if __name__ == '__main__':
 
-	test_report = u'test suite report'
-	test_query = u'select 1 as test_suite_report_result'
+	if len(sys.argv) > 1 and sys.argv[1] == 'test':
+		test_report = u'test suite report'
+		test_query = u'select 1 as test_suite_report_result'
 
-	print "delete (should work):", delete_report_definition(name = test_report)
-	print "check (should return False):", report_exists(name = test_report)
-	print "save (should work):", save_report_definition(name = test_report, query = test_query)
-	print "save (should fail):", save_report_definition(name = test_report, query = test_query, overwrite = False)
-	print "save (should work):", save_report_definition(name = test_report, query = test_query, overwrite = True)
-	print "delete (should work):", delete_report_definition(name = test_report)
-	print "check (should return False):", report_exists(name = test_report)
+		print "delete (should work):", delete_report_definition(name = test_report)
+		print "check (should return False):", report_exists(name = test_report)
+		print "save (should work):", save_report_definition(name = test_report, query = test_query)
+		print "save (should fail):", save_report_definition(name = test_report, query = test_query, overwrite = False)
+		print "save (should work):", save_report_definition(name = test_report, query = test_query, overwrite = True)
+		print "delete (should work):", delete_report_definition(name = test_report)
+		print "check (should return False):", report_exists(name = test_report)
 #============================================================
 # $Log: gmDataMining.py,v $
-# Revision 1.2  2007-04-08 21:16:16  ncq
+# Revision 1.3  2008-01-05 23:24:29  ncq
+# - fixup test framework
+#
+# Revision 1.2  2007/04/08 21:16:16  ncq
 # - fix and add test suite
 # - delete_report_definition()
 #
