99307bfe415aac1277627573a76f3c959ce62217
Karsten Hilbert <karsten.hilbert@gmx.net>
2008-01-11 16:17:09 +0000

    - fix delete_document()

--- gnumed/gnumed/server/sql/v7-v8/dynamic/blobs-delete_document.sql
+++ gnumed/gnumed/server/sql/v7-v8/dynamic/blobs-delete_document.sql
@@ -8,8 +8,8 @@
 -- Author: karsten.hilbert@gmx.net
 -- 
 -- ==============================================================
--- $Id: blobs-delete_document.sql,v 1.4 2007-11-12 23:22:22 ncq Exp $
--- $Revision: 1.4 $
+-- $Id: blobs-delete_document.sql,v 1.5 2008-01-11 16:17:09 ncq Exp $
+-- $Revision: 1.5 $
 
 -- --------------------------------------------------------------
 \set ON_ERROR_STOP 1
@@ -38,14 +38,16 @@ BEGIN
 	select * into _doc_row from blobs.doc_med where pk = _pk_doc;
 
 	_del_note := _(''Deletion of document'') || E'':\n''
-		|| '' '' || to_char(_doc_row.date, ''YYYY-MM-DD HH24:MI'') || '' "''
-		|| (select _(dt.name) from blobs.doc_type dt where pk = _doc_row.fk_type) || ''" (''
-		|| _doc_row.ext_ref || E'')\n''
-		|| '' '' || coalesce(_doc_row.comment, '''');
+		|| '' ''
+			|| to_char(_doc_row.date, ''YYYY-MM-DD HH24:MI'')
+			|| '' "'' || (select _(dt.name) from blobs.doc_type dt where pk = _doc_row.fk_type) || ''"''
+			|| coalesce('' ('' || _doc_row.ext_ref || '')'', '''')
+		|| coalesce(E''\n '' || _doc_row.comment, '''')
+	;
 
 	FOR _obj_row IN select * from blobs.doc_obj where fk_doc = _pk_doc order by seq_idx LOOP
 		_del_note := _del_note || E''\n''
-			|| '' #'' || _obj_row.seq_idx || '': "'' || coalesce(_obj_row.comment, '''') || E''"\n''
+			|| '' #'' || coalesce(_obj_row.seq_idx, ''-1'') || '': "'' || coalesce(_obj_row.comment, '''') || E''"\n''
 			|| '' '' || coalesce(_obj_row.filename, '''') || E''\n'';
 	end LOOP;
 
@@ -61,7 +63,6 @@ BEGIN
 	delete from blobs.doc_med where pk = _pk_doc;
 
 	return True;
-
 END;';
 
 
@@ -72,11 +73,14 @@ select i18n.upd_tx('de_DE', 'Deletion of document', 'Dokument gelöscht');
 revoke delete on blobs.doc_med from "gm-doctors";
 
 -- --------------------------------------------------------------
-select gm.log_script_insertion('$RCSfile: blobs-delete_document.sql,v $', '$Revision: 1.4 $');
+select gm.log_script_insertion('$RCSfile: blobs-delete_document.sql,v $', '$Revision: 1.5 $');
 
 -- ==============================================================
 -- $Log: blobs-delete_document.sql,v $
--- Revision 1.4  2007-11-12 23:22:22  ncq
+-- Revision 1.5  2008-01-11 16:17:09  ncq
+-- - fix delete_document()
+--
+-- Revision 1.4  2007/11/12 23:22:22  ncq
 -- - remove exception handling
 --
 -- Revision 1.3  2007/11/12 23:11:24  ncq
