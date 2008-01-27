a4a5e18f787b65fa3bb9d92e6e77035a053da390
Karsten Hilbert <karsten.hilbert@gmx.net>
2008-01-27 21:24:58 +0000

    - improved naming of alias

--- gnumed/gnumed/server/sql/v8-v9/dynamic/v9-clin-coded_item_narrative.sql
+++ gnumed/gnumed/server/sql/v8-v9/dynamic/v9-clin-coded_item_narrative.sql
@@ -5,8 +5,8 @@
 -- Author: Karsten Hilbert
 -- 
 -- ==============================================================
--- $Id: v9-clin-coded_item_narrative.sql,v 1.1 2007-12-30 21:06:54 ncq Exp $
--- $Revision: 1.1 $
+-- $Id: v9-clin-coded_item_narrative.sql,v 1.2 2008-01-27 21:24:58 ncq Exp $
+-- $Revision: 1.2 $
 
 -- --------------------------------------------------------------
 \set ON_ERROR_STOP 1
@@ -23,9 +23,9 @@ select
 	vn4s.pk_patient as pk_identity,
 	vn4s.soap_cat as soap_cat,
 	vn4s.narrative as narrative,
-	cn.code as code,
-	cn.xfk_coding_system as coding_system,
-	cn.pk as pk_coded_narrative,
+	cp.code as code,
+	cp.xfk_coding_system as coding_system,
+	cp.pk as pk_coded_phrase,
 	vn4s.pk_encounter,
 	vn4s.pk_episode,
 	vn4s.pk_health_issue,
@@ -34,8 +34,8 @@ select
 from
 	clin.v_narrative4search vn4s
 		inner join
-	clin.coded_narrative cn
-		on vn4s.narrative = cn.term
+	clin.coded_phrase cp
+		on vn4s.narrative = cp.term
 ;
 
 grant select on clin.v_coded_item_narrative to group "gm-doctors";
@@ -45,11 +45,14 @@ comment on view clin.v_coded_item_narrative is
 	 a corresponding code is known to the system.';
 
 -- --------------------------------------------------------------
-select gm.log_script_insertion('$RCSfile: v9-clin-coded_item_narrative.sql,v $', '$Revision: 1.1 $');
+select gm.log_script_insertion('$RCSfile: v9-clin-coded_item_narrative.sql,v $', '$Revision: 1.2 $');
 
 -- ==============================================================
 -- $Log: v9-clin-coded_item_narrative.sql,v $
--- Revision 1.1  2007-12-30 21:06:54  ncq
+-- Revision 1.2  2008-01-27 21:24:58  ncq
+-- - improved naming of alias
+--
+-- Revision 1.1  2007/12/30 21:06:54  ncq
 -- - new
 --
 -- Revision 1.1  2007/12/30 15:51:49  ncq
