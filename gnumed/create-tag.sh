84002d3903298631f1aabe9b364787b6c895d412
Karsten Hilbert <karsten.hilbert@gmx.net>
2008-01-30 13:28:20 +0000

    - properly use cvs

--- gnumed/create-tag.sh
+++ gnumed/create-tag.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #------------------------------------------------------------------
-# $Id: create-tag.sh,v 1.2 2008-01-03 16:28:17 ncq Exp $
+# $Id: create-tag.sh,v 1.3 2008-01-30 13:28:20 ncq Exp $
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/create-tag.sh,v $
-# $Revision: 1.2 $
+# $Revision: 1.3 $
 # License: GPL
 #------------------------------------------------------------------
 
@@ -28,6 +28,7 @@ if ! [[ "${TAG}" =~ "rel-[0-9]-[0-9]-[0-9]((-rc[0-9])|(-[0-9]))?\$" ]] ; then
 	exit
 fi
 
+echo ""
 echo "Are you absolutely positively sure you want"
 echo "to tag the CVS tree as \"${TAG}\" ?"
 echo "Note that you must have checked in all changes."
@@ -44,11 +45,14 @@ fi
 read -p "Hit [ENTER] to start tagging ..."
 echo ""
 echo "Tagging CVS tree as \"${TAG}\" ..."
-cvs -v tag -c ${TAG}
+cvs tag -c ${TAG}
 
 #------------------------------------------------------------------
 # $Log: create-tag.sh,v $
-# Revision 1.2  2008-01-03 16:28:17  ncq
+# Revision 1.3  2008-01-30 13:28:20  ncq
+# - properly use cvs
+#
+# Revision 1.2  2008/01/03 16:28:17  ncq
 # - stricter TAG checking
 #
 # Revision 1.1  2008/01/03 15:53:39  ncq
