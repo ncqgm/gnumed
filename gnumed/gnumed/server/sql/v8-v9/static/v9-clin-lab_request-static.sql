e3b12798264a03bdc31c364ba896d1e9b39517b1
Karsten Hilbert <karsten.hilbert@gmx.net>
2008-03-06 23:21:38 +0000

    - audit table doesn't exist yet

--- gnumed/gnumed/server/sql/v8-v9/static/v9-clin-lab_request-static.sql
+++ gnumed/gnumed/server/sql/v8-v9/static/v9-clin-lab_request-static.sql
@@ -5,8 +5,8 @@
 -- Author: Karsten Hilbert
 -- 
 -- ==============================================================
--- $Id: v9-clin-lab_request-static.sql,v 1.1 2008-03-05 22:36:11 ncq Exp $
--- $Revision: 1.1 $
+-- $Id: v9-clin-lab_request-static.sql,v 1.2 2008-03-06 23:21:38 ncq Exp $
+-- $Revision: 1.2 $
 
 -- --------------------------------------------------------------
 \set ON_ERROR_STOP 1
@@ -15,22 +15,18 @@
 alter table clin.lab_request
 	add column diagnostic_service_section text;
 
-alter table audit.log_lab_request
-	add column diagnostic_service_section text;
-
-
 alter table clin.lab_request
 	add column ordered_service text;
 
-alter table audit.log_lab_request
-	add column ordered_service text;
-
 -- --------------------------------------------------------------
-select gm.log_script_insertion('$RCSfile: v9-clin-lab_request-static.sql,v $', '$Revision: 1.1 $');
+select gm.log_script_insertion('$RCSfile: v9-clin-lab_request-static.sql,v $', '$Revision: 1.2 $');
 
 -- ==============================================================
 -- $Log: v9-clin-lab_request-static.sql,v $
--- Revision 1.1  2008-03-05 22:36:11  ncq
+-- Revision 1.2  2008-03-06 23:21:38  ncq
+-- - audit table doesn't exist yet
+--
+-- Revision 1.1  2008/03/05 22:36:11  ncq
 -- - new
 --
 -- Revision 1.1  2008/03/02 11:25:01  ncq
