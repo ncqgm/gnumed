8eae53bfb2776c593f0985bb4779aae5986d3dc8
Karsten Hilbert <karsten.hilbert@gmx.net>
2008-03-06 23:21:13 +0000

    - proper sequence name

--- gnumed/gnumed/server/sql/v8-v9/dynamic/v9-ref-coding_system_root-dynamic.sql
+++ gnumed/gnumed/server/sql/v8-v9/dynamic/v9-ref-coding_system_root-dynamic.sql
@@ -5,8 +5,8 @@
 -- Author: Karsten Hilbert
 -- 
 -- ==============================================================
--- $Id: v9-ref-coding_system_root-dynamic.sql,v 1.1 2008-03-05 22:35:14 ncq Exp $
--- $Revision: 1.1 $
+-- $Id: v9-ref-coding_system_root-dynamic.sql,v 1.2 2008-03-06 23:21:13 ncq Exp $
+-- $Revision: 1.2 $
 
 -- --------------------------------------------------------------
 \set ON_ERROR_STOP 1
@@ -14,7 +14,7 @@
 -- --------------------------------------------------------------
 grant select, insert, update, delete on
 	ref.coding_system_root,
-	ref.coding_system_root_pk_seq
+	ref.coding_system_root_pk_coding_system_seq
 to group "gm-doctors";
 
 
@@ -27,11 +27,14 @@ comment on column ref.coding_system_root.comment is
  child tables will use this in different ways';
 
 -- --------------------------------------------------------------
-select gm.log_script_insertion('$RCSfile: v9-ref-coding_system_root-dynamic.sql,v $', '$Revision: 1.1 $');
+select gm.log_script_insertion('$RCSfile: v9-ref-coding_system_root-dynamic.sql,v $', '$Revision: 1.2 $');
 
 -- ==============================================================
 -- $Log: v9-ref-coding_system_root-dynamic.sql,v $
--- Revision 1.1  2008-03-05 22:35:14  ncq
+-- Revision 1.2  2008-03-06 23:21:13  ncq
+-- - proper sequence name
+--
+-- Revision 1.1  2008/03/05 22:35:14  ncq
 -- - added
 --
 --
