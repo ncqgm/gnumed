8dfaaf6809f858a376a6f595bd11a1cbb858c753
Karsten Hilbert <karsten.hilbert@gmx.net>
2008-03-29 16:03:40 +0000

    - add unreviewed results

---
+++ gnumed/gnumed/server/sql/v8-v9/dynamic/v9-dem-v_provider_inbox.sql
@@ -0,0 +1,132 @@
+-- ==============================================================
+-- GNUmed database schema change script
+--
+-- License: GPL
+-- Author: karsten.hilbert@gmx.net
+-- 
+-- ==============================================================
+-- $Id: v9-dem-v_provider_inbox.sql,v 1.1 2008-03-29 16:03:40 ncq Exp $
+-- $Revision: 1.1 $
+
+-- --------------------------------------------------------------
+\set ON_ERROR_STOP 1
+
+-- --------------------------------------------------------------
+\unset ON_ERROR_STOP
+drop view dem.v_provider_inbox cascade;
+\set ON_ERROR_STOP 1
+
+-- ---------------------------------------------
+create view dem.v_provider_inbox as
+select
+	(select short_alias from dem.staff where dem.staff.pk = pi.fk_staff) as provider,
+	pi.importance,
+	vit.category,
+	vit.l10n_category,
+	vit.type,
+	vit.l10n_type,
+	pi.comment,
+	pi.ufk_context as pk_context,
+	pi.data as data,
+	pi.pk as pk_provider_inbox,
+	pi.fk_staff as pk_staff,
+	vit.pk_category,
+	pi.fk_inbox_item_type as pk_type
+from
+	dem.provider_inbox pi,
+	dem.v_inbox_item_type vit
+where
+	pi.fk_inbox_item_type = vit.pk_type
+
+union
+
+select
+	(select short_alias from dem.staff where dem.staff.pk = vo4dnd.pk_intended_reviewer)
+		as provider,
+	0	as importance,
+	'clinical'
+		as category,
+	_('clinical')
+		as l10n_category,
+	'review docs'
+		as type,
+	_('review docs')
+		as l10n_type,
+	(select _('unreviewed documents for patient') || ' [' || vbp.lastnames || ', ' || vbp.firstnames || ']'
+	 from dem.v_basic_person vbp
+	 where vbp.pk_identity=vo4dnd.pk_patient)
+	 	as comment,
+	vo4dnd.pk_patient
+		as pk_context,
+	NULL
+		as data,
+	NULL
+		as pk_provider_inbox,
+	vo4dnd.pk_intended_reviewer
+		as pk_staff,
+	(select pk_category from dem.v_inbox_item_type where type='review docs')
+		as pk_category,
+	(select pk_type from dem.v_inbox_item_type where type='review docs')
+		as pk_type
+from
+	blobs.v_obj4doc_no_data vo4dnd
+where
+	reviewed is False
+
+union
+
+select
+	(select short_alias from dem.staff where dem.staff.pk = vtr.pk_intended_reviewer)
+		as provider,
+	0	as importance,
+	'clinical'
+		as category,
+	_('clinical')
+		as l10n_category,
+	'review results'
+		as type,
+	_('review results')
+		as l10n_type,
+	(select _('unreviewed results for patient') || ' [' || vbp.lastnames || ', ' || vbp.firstnames || ']'
+	 from dem.v_basic_person vbp
+	 where vbp.pk_identity = vtr.pk_patient
+	)	as comment,
+	vtr.pk_patient
+		as pk_context,
+	NULL
+		as data,
+	NULL
+		as pk_provider_inbox,
+	vtr.pk_intended_reviewer
+		as pk_staff,
+	(select pk_category from dem.v_inbox_item_type where type='review results')
+		as pk_category,
+	(select pk_type from dem.v_inbox_item_type where type='review results')
+		as pk_type
+from
+	clin.v_test_results vtr
+where
+	reviewed is False
+
+;
+
+comment on view dem.v_provider_inbox is
+'Denormalized messages for the providers.
+Using UNION makes sure we get the right level of uniqueness.';
+
+
+select i18n.i18n('review results');
+select i18n.i18n('unreviewed results for patient');
+
+-- --------------------------------------------------------------
+grant select on dem.v_provider_inbox to group "gm-doctors";
+
+-- --------------------------------------------------------------
+select gm.log_script_insertion('$RCSfile: v9-dem-v_provider_inbox.sql,v $', '$Revision: 1.1 $');
+
+-- ==============================================================
+-- $Log: v9-dem-v_provider_inbox.sql,v $
+-- Revision 1.1  2008-03-29 16:03:40  ncq
+-- - add unreviewed results
+--
+--
