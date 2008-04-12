443588bd6f56d3db3dced735d9f58dd2830256e0
Karsten Hilbert <karsten.hilbert@gmx.net>
2008-04-12 22:39:22 +0000

    - support "quiet"

--- gnumed/gnumed/server/bootstrap/upgrade-db.sh
+++ gnumed/gnumed/server/bootstrap/upgrade-db.sh
@@ -4,7 +4,7 @@
 # Upgrade GNUmed database from version to version.
 #
 # usage:
-#  upgrade-db.sh vX vX+1 <secret>
+#  upgrade-db.sh vX vX+1 <secrets>
 #
 # limitations:
 #  Only works from version to version sequentially.
@@ -14,7 +14,11 @@
 #
 # "secret" command line options:
 #  no-backup
+#  - don't create a database backup (you got faith)
 #  no-compression
+#  - create a backup but don't compress it (you got plenty of disk but run low on cycles)
+#  quiet
+#  - display failures only, don't chatter
 #
 # ========================================================
 
@@ -28,7 +32,7 @@ PREV_VER="$1"
 NEXT_VER="$2"
 SKIP_BACKUP="$3"
 BZIP_BACKUP="$3"
-QUIET="$4"
+QUIET="$3"
 LOG_BASE="."
 LOG="${LOG_BASE}/update_db-v${PREV_VER}_v${NEXT_VER}.log"
 CONF="update_db-v${PREV_VER}_v${NEXT_VER}.conf"
@@ -74,58 +78,70 @@ else
 	PORT_DEF=""
 fi ;
 
-echo ""
-echo "==========================================================="
-echo "Upgrading GNUmed database."
-echo ""
-echo "This will *non-destructively* create a new GNUmed database"
-echo "of version v${NEXT_VER} from an existing v${PREV_VER} database."
-echo "Existing data is transferred and transformed as necessary."
-echo ""
-echo "The name of the new database will be \"gnumed_v${NEXT_VER}\"."
-echo "==========================================================="
+
+if test "$QUIET" != "quiet"; then
+	QUIET="$4"
+fi ;
+function echo_msg () {
+	if test "$QUIET" == "quiet"; then
+		return
+	fi ;
+	echo $1 ;
+}
 
 
-echo ""
-echo "1) creating backup of existing database ..."
+echo_msg ""
+echo_msg "==========================================================="
+echo_msg "Upgrading GNUmed database."
+echo_msg ""
+echo_msg "This will *non-destructively* create a new GNUmed database"
+echo_msg "of version v${NEXT_VER} from an existing v${PREV_VER} database."
+echo_msg "Existing data is transferred and transformed as necessary."
+echo_msg ""
+echo_msg "The name of the new database will be \"gnumed_v${NEXT_VER}\"."
+echo_msg "==========================================================="
+
+
+echo_msg ""
+echo_msg "1) creating backup of existing database ..."
 if test "$SKIP_BACKUP" != "no-backup" ; then
-	echo "   Note that this may take a substantial amount of time and disk space!"
-	echo "   You may need to type in the password for gm-dbo."
+	echo_msg "   Note that this may take a substantial amount of time and disk space!"
+	echo_msg "   You may need to type in the password for gm-dbo."
 	if test "$BZIP_BACKUP" != "no-compression" ; then
 		pg_dump -C -U gm-dbo -d gnumed_v${PREV_VER} ${PORT_DEF} | bzip2 -z9 > ${BAK_FILE}.bz2
 	else
 		pg_dump -C -U gm-dbo -d gnumed_v${PREV_VER} ${PORT_DEF} -f ${BAK_FILE}
 	fi ;
 else
-	echo ""
+	echo_msg ""
 	echo "   !!! SKIPPED backup !!!"
-	echo ""
-	echo "   This may prevent you from being able to restore your"
-	echo "   database from a up-to-date backup should you need to."
-	echo ""
-	echo "   Be sure you really know what you are doing !"
+	echo_msg ""
+	echo_msg "   This may prevent you from being able to restore your"
+	echo_msg "   database from a up-to-date backup should you need to."
+	echo_msg ""
+	echo_msg "   Be sure you really know what you are doing !"
 fi ;
 
 
-echo ""
-echo "2) upgrading to new database ..."
+echo_msg ""
+echo_msg "2) upgrading to new database ..."
 rm -rf ${LOG}
 # fixup for schema hash function
 # - cannot be done inside bootstrapper
 # - only needed for converting anything below v6 with a v6 bootstrapper
-#echo "==> fixup for database hashing (will probably ask for gm-dbo password) ..."
+#echo_msg "==> fixup for database hashing (will probably ask for gm-dbo password) ..."
 #psql -U gm-dbo -d gnumed_v${PREV_VER} ${PORT_DEF} -f ../sql/gmConcatTableStructureFutureStub.sql
-./bootstrap_gm_db_system.py --log-file=${LOG} --conf-file=${CONF} ${QUIET}
+./bootstrap_gm_db_system.py --log-file=${LOG} --conf-file=${CONF} --${QUIET}
 if test "$?" != "0" ; then
 	echo "Upgrading \"gnumed_v${PREV_VER}\" to \"gnumed_v${NEXT_VER}\" did not finish successfully."
 	exit 1
 fi
 
 
-#echo ""
-#echo "3) preparing new database for efficient use ..."
-#echo "   If the database is large this may take quite a while!"
-#echo "   You may need to type in the password for gm-dbo."
+#echo_msg ""
+#echo_msg "3) preparing new database for efficient use ..."
+#echo_msg "   If the database is large this may take quite a while!"
+#echo_msg "   You may need to type in the password for gm-dbo."
 #vacuumdb --full --analyze ${PORT_DEF} -U gm-dbo -d gnumed_v${NEXT_VER}
 
 exit 0
