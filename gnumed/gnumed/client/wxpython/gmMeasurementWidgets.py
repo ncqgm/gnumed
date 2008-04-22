0022603dd11c94d6b9869e1d9fb215a49ec4e9f4
Karsten Hilbert <karsten.hilbert@gmx.net>
2008-04-22 21:18:49 +0000

    - implement signing - improved tooltip - properly clear grid when active patient changes

--- gnumed/gnumed/client/wxpython/gmMeasurementWidgets.py
+++ gnumed/gnumed/client/wxpython/gmMeasurementWidgets.py
@@ -2,8 +2,8 @@
 """
 #================================================================
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/wxpython/gmMeasurementWidgets.py,v $
-# $Id: gmMeasurementWidgets.py,v 1.9 2008-04-16 20:39:39 ncq Exp $
-__version__ = "$Revision: 1.9 $"
+# $Id: gmMeasurementWidgets.py,v 1.10 2008-04-22 21:18:49 ncq Exp $
+__version__ = "$Revision: 1.10 $"
 __author__ = "Karsten Hilbert <Karsten.Hilbert@gmx.net>"
 __license__ = "GPL"
 
@@ -70,12 +70,46 @@ class cMeasurementsGrid(wx.grid.Grid):
 
 		dlg = cMeasurementsReviewDlg (
 			self,
-#			wx.GetTopLevelParent(self),
 			-1,
 			tests = tests,
 			test_count = test_count
 		)
-		dlg.ShowModal()
+		decision = dlg.ShowModal()
+
+		if decision == wx.ID_APPLY:
+			wx.BeginBusyCursor()
+
+			if dlg._RBTN_confirm_abnormal.GetValue():
+				abnormal = None
+			elif dlg._RBTN_results_normal.GetValue():
+				abnormal = False
+			else:
+				abnormal = True
+
+			if dlg._RBTN_confirm_relevance.GetValue():
+				relevant = None
+			elif dlg._RBTN_results_not_relevant.GetValue():
+				relevant = False
+			else:
+				relevant = True
+
+			if tests is None:
+				tests = [ self.__cell_data[c[1]][c[0]] for c in selected_cells if c[1] != 0 ]
+
+			comment = None
+			if len(tests) == 1:
+				comment = dlg._TCTRL_comment.GetValue()
+
+			for test in tests:
+				test.set_review (
+					technically_abnormal = abnormal,
+					clinically_relevant = relevant,
+					comment = comment,
+					make_me_responsible = dlg._CHBOX_responsible.IsChecked()
+				)
+
+			wx.EndBusyCursor()
+
 		dlg.Destroy()
 	#------------------------------------------------------------
 	def get_selected_cells(self):
@@ -89,11 +123,11 @@ class cMeasurementsGrid(wx.grid.Grid):
 		selected_cells = []
 
 		if (len(sel_block_top_left) > 1) and (len(sel_block_bottom_right) > 0):
-			rl = sel_block_bottom_right[0] - sel_block_top_left[0]
-			rr = sel_block_bottom_right[0]
-			ct = sel_block_bottom_right[1] - sel_block_top_left[1]
-			cb = sel_block_bottom_right[1]
-			selected_cells.extend([ (r, c) for r in range(rl, rr+1) for c in range(ct, cb+1) ])
+			row_left = sel_block_bottom_right[0] - sel_block_top_left[0]
+			row_right = sel_block_bottom_right[0]
+			col_top = sel_block_bottom_right[1] - sel_block_top_left[1]
+			col_bottom = sel_block_bottom_right[1]
+			selected_cells.extend([ (r, c) for r in range(row_left, row_right+1) for c in range(col_top, col_bottom+1) ])
 
 		if len(sel_rows) > 0:
 			selected_cells.extend([ (r, c) for r in sel_rows for c in range(self.GetNumberCols()) ])
@@ -120,14 +154,21 @@ class cMeasurementsGrid(wx.grid.Grid):
 		tests = [ u'%s (%s)' % (test[1], test[0]) for test in emr.get_test_types_for_results() ]
 		if len(tests) == 0:
 			return
-		test_details, td_idx = emr.get_test_types_details()
+#		test_details, td_idx = emr.get_test_types_details()
 		dates = [ date[0].strftime(self.__date_format) for date in emr.get_dates_for_results() ]
-		results, idx = emr.get_measurements_by_date()
+		results = emr.get_test_results_by_date()
 
 		self.BeginBatch()
 
-		# row labels in column 0 (test names)
 		self.AppendRows(numRows = len(tests))
+		self.AppendCols(numCols = len(dates) + 1)
+
+		# column labels:
+		self.SetColLabelValue(0, _("Test"))
+		for date_idx in range(len(dates)):
+			self.SetColLabelValue(date_idx + 1, dates[date_idx])
+
+		# row "labels" (== column 0 cell values)
 		for row_idx in range(len(tests)):
 			self.SetCellValue(row_idx, 0, tests[row_idx])
 			self.SetCellBackgroundColour(row_idx, 0, self.GetLabelBackgroundColour())
@@ -137,15 +178,10 @@ class cMeasurementsGrid(wx.grid.Grid):
 #			self.__cell_tooltips[0] = {}
 #			self.__cell_tooltips[0][row_idx] = _('test type tooltip row %s') % row_idx
 
-		# column labels (test dates)
-		self.AppendCols(numCols = len(dates))
-		for date_idx in range(len(dates)):
-			self.SetColLabelValue(date_idx + 1, dates[date_idx])
-
 		# cell values (test results)
 		for result in results:
-			row = tests.index(u'%s (%s)' % (result[idx['unified_code']], result[idx['unified_name']]))
-			col = dates.index(result[idx['clin_when']].strftime(self.__date_format)) + 1
+			row = tests.index(u'%s (%s)' % (result['unified_code'], result['unified_name']))
+			col = dates.index(result['clin_when'].strftime(self.__date_format)) + 1
 
 			try:
 				self.__cell_data[col]
@@ -203,17 +239,40 @@ class cMeasurementsGrid(wx.grid.Grid):
 			# no - display ... and truncate to 7 chars
 			if (len(result['unified_val']) > 8) or (has_result_comment):
 				val2display = u'%7.7s\u2026%6.6s%1.1s' % (
-					result[idx['unified_val']][:7],
+					result['unified_val'][:7],
 					abnormality_indicator,
 					gmTools.bool2subst(missing_review, u'\u270D', u'')
 				)
 			# yes - display fully up to 8 chars
 			else:
 				val2display = u'%8.8s%6.6s%1.1s' % (
-					result[idx['unified_val']][:8],
+					result['unified_val'][:8],
 					abnormality_indicator,
 					gmTools.bool2subst(missing_review, u'\u270D', u'')
 				)
+
+			has_normal_min_or_max = (result['val_normal_min'] is not None) or (result['val_normal_min'] is not None)
+			if has_normal_min_or_max:
+				normal_min_max = u'%s - %s' % (
+					gmTools.coalesce(result['val_normal_min'], u'?'),
+					gmTools.coalesce(result['val_normal_min'], u'?')
+				)
+			else:
+				normal_min_max = u''
+
+			has_clinical_min_or_max = (result['val_target_min'] is not None) or (result['val_target_min'] is not None)
+			if has_clinical_min_or_max:
+				clinical_min_max = u'%s - %s' % (
+					gmTools.coalesce(result['val_target_min'], u'?'),
+					gmTools.coalesce(result['val_target_min'], u'?')
+				)
+			else:
+				clinical_min_max = u''
+
+			if result['reviewed']:
+				review_status = result['last_reviewed'].strftime('%c')
+			else:
+				review_status = _('not yet')
 
 			self.SetCellValue(row, col, val2display)
 			self.SetCellAlignment(row, col, horiz = wx.ALIGN_RIGHT, vert = wx.ALIGN_CENTRE)
@@ -236,9 +295,9 @@ class cMeasurementsGrid(wx.grid.Grid):
 				' Date: %(clin_when)s\n'
 				' Type: "%(name)s" (%(code)s)\n'
 				' Result: %(val)s%(unit)s%(ind)s\n'
-				' Standard normal range: %(norm_min)s - %(norm_max)s%(norm_range)s  \n'
+				' Standard normal range: %(norm_min_max)s%(norm_range)s  \n'
 				' Reference group: %(ref_group)s\n'
-				' Clinical target range: %(clin_min)s - %(clin_max)s%(clin_range)s  \n'
+				' Clinical target range: %(clin_min_max)s%(clin_range)s  \n'
 				' Doc: %(comment_doc)s\n'
 				' Lab: %(comment_lab)s\n'	# note_test_org
 				' Episode: %(epi)s\n'
@@ -260,40 +319,54 @@ class cMeasurementsGrid(wx.grid.Grid):
 				'\n'
 				'Last modified %(mod_when)s by %(mod_by)s.'
 			) % ({
-				'clin_when': result[idx['clin_when']].strftime('%c'),
-				'code': result[idx['code_tt']],
-				'name': result[idx['name_tt']],
-				'val': result[idx['unified_val']],
-				'unit': gmTools.coalesce(result[idx['val_unit']], u'', u' %s'),
-				'ind': gmTools.coalesce(result[idx['abnormality_indicator']], u'', u' (%s)'),
-				'norm_min': gmTools.coalesce(result[idx['val_normal_min']], u'?'),
-				'norm_max': gmTools.coalesce(result[idx['val_normal_max']], u'?'),
-				'norm_range': gmTools.coalesce(result[idx['val_normal_range']], u'', u' / %s'),
-				'ref_group': gmTools.coalesce(result[idx['norm_ref_group']], u''),
-				'clin_min': gmTools.coalesce(result[idx['val_target_min']], u'?'),
-				'clin_max': gmTools.coalesce(result[idx['val_target_max']], u'?'),
-				'clin_range': gmTools.coalesce(result[idx['val_target_range']], u'', u' / %s'),
-				'comment_doc': u'\n Doc: '.join(gmTools.coalesce(result[idx['comment']], u'').split('\n')),
-				'comment_lab': u'\n Lab: '.join(gmTools.coalesce(result[idx['comment']], u'').split('\n')),
-				'epi': result[idx['episode']],
-				'issue': gmTools.coalesce(result[idx['health_issue']], u''),
-				'material': gmTools.coalesce(result[idx['material']], u''),
-				'mat_detail': gmTools.coalesce(result[idx['material_detail']], u''),
+				'clin_when': result['clin_when'].strftime('%c'),
+				'code': result['code_tt'],
+				'name': result['name_tt'],
+				'val': result['unified_val'],
+				'unit': gmTools.coalesce(result['val_unit'], u'', u' %s'),
+				'ind': gmTools.coalesce(result['abnormality_indicator'], u'', u' (%s)'),
+				'norm_min_max': normal_min_max,
+				'norm_range': gmTools.coalesce (
+					result['val_normal_range'],
+					u'',
+					gmTools.bool2subst (
+						has_normal_min_or_max,
+						u' / %s',
+						u'%s'
+					)
+				),
+				'ref_group': gmTools.coalesce(result['norm_ref_group'], u''),
+				'clin_min_max': clinical_min_max,
+				'clin_range': gmTools.coalesce (
+					result['val_target_range'],
+					u'',
+					gmTools.bool2subst (
+						has_clinical_min_or_max,
+						u' / %s',
+						u'%s'
+					)
+				),
+				'comment_doc': u'\n Doc: '.join(gmTools.coalesce(result['comment'], u'').split('\n')),
+				'comment_lab': u'\n Lab: '.join(gmTools.coalesce(result['comment'], u'').split('\n')),
+				'epi': result['episode'],
+				'issue': gmTools.coalesce(result['health_issue'], u''),
+				'material': gmTools.coalesce(result['material'], u''),
+				'mat_detail': gmTools.coalesce(result['material_detail'], u''),
 
-				'reviewed': gmTools.bool2str(result[idx['reviewed']], result[idx['last_reviewed']], _('not yet')),
-				'reviewer': gmTools.bool2subst(result[idx['review_by_you']], _('you'), gmTools.coalesce(result[idx['last_reviewer']], u'')),
-				'abnormal': gmTools.coalesce(result[idx['is_technically_abnormal']], u''),
-				'relevant': gmTools.coalesce(result[idx['is_clinically_relevant']], u''),
-				'rev_comment': gmTools.coalesce(result[idx['review_comment']], u''),
-				'responsible_reviewer': gmTools.bool2subst(result[idx['you_are_responsible']], _('you'), result[idx['responsible_reviewer']]),
+				'reviewed': review_status,
+				'reviewer': gmTools.bool2subst(result['review_by_you'], _('you'), gmTools.coalesce(result['last_reviewer'], u'')),
+				'abnormal': gmTools.bool2subst(result['is_technically_abnormal'], _('yes'), _('no'), u''),
+				'relevant': gmTools.bool2subst(result['is_clinically_relevant'], _('yes'), _('no'), u''),
+				'rev_comment': gmTools.coalesce(result['review_comment'], u''),
+				'responsible_reviewer': gmTools.bool2subst(result['you_are_responsible'], _('you'), result['responsible_reviewer']),
 
-				'comment_type': u'\n Type comment:'.join(gmTools.coalesce(result[idx['comment_tt']], u'').split('\n')),
-				'name_unified': result[idx['name_unified']],
-				'code_unified': result[idx['code_unified']],
-				'comment_type_unified': u'\n Group comment: '.join(gmTools.coalesce(result[idx['comment_unified']], u'').split('\n')),
+				'comment_type': u'\n Type comment:'.join(gmTools.coalesce(result['comment_tt'], u'').split('\n')),
+				'name_unified': result['name_unified'],
+				'code_unified': result['code_unified'],
+				'comment_type_unified': u'\n Group comment: '.join(gmTools.coalesce(result['comment_unified'], u'').split('\n')),
 
-				'mod_when': result[idx['modified_when']].strftime('%c'),
-				'mod_by': result[idx['modified_by']],
+				'mod_when': result['modified_when'].strftime('%c'),
+				'mod_by': result['modified_by'],
 
 				'sig_hand': u'\u270D'
 			})
@@ -304,7 +377,9 @@ class cMeasurementsGrid(wx.grid.Grid):
 	#------------------------------------------------------------
 	def empty_grid(self):
 		self.BeginBatch()
-		self.DeleteCols(pos = 2, numCols = (self.GetNumberCols() - 2))
+		self.ClearGrid()
+		self.DeleteRows(pos = 0, numRows = self.GetNumberRows())
+		self.DeleteCols(pos = 0, numCols = self.GetNumberCols())
 		self.EndBatch()
 	#------------------------------------------------------------
 	# internal helpers
@@ -313,10 +388,8 @@ class cMeasurementsGrid(wx.grid.Grid):
 		self.CreateGrid(0, 1)
 		self.EnableEditing(0)
 		self.EnableDragGridSize(0)
-		self.SetColLabelValue(0, _("Test"))
 		self.SetRowLabelSize(20)
 		self.SetRowLabelAlignment(horiz = wx.ALIGN_LEFT, vert = wx.ALIGN_CENTRE)
-		# set background/font/... on test type pseudo-column
 	#------------------------------------------------------------
 	# event handling
 	#------------------------------------------------------------
@@ -369,6 +442,8 @@ class cMeasurementsReviewDlg(wxgMeasurementsReviewDlg.wxgMeasurementsReviewDlg):
 			tests = kwargs['tests']
 			del kwargs['tests']
 			test_count = len(tests)
+			try: del kwargs['test_count']
+			except KeyError: pass
 		except KeyError:
 			tests = None
 			test_count = kwargs['test_count']
@@ -400,7 +475,10 @@ class cMeasurementsReviewDlg(wxgMeasurementsReviewDlg.wxgMeasurementsReviewDlg):
 	# event handling
 	#--------------------------------------------------------
 	def _on_signoff_button_pressed(self, evt):
-		print "signing off"
+		if self.IsModal():
+			self.EndModal(wx.ID_APPLY)
+		else:
+			self.Close()
 #================================================================
 class cMeasurementsPnl(wxgMeasurementsPnl.wxgMeasurementsPnl, gmRegetMixin.cRegetOnPaintMixin):
 
@@ -413,10 +491,16 @@ class cMeasurementsPnl(wxgMeasurementsPnl.wxgMeasurementsPnl, gmRegetMixin.cRege
 	# event handling
 	#--------------------------------------------------------
 	def __register_interests(self):
-		gmDispatcher.connect(signal = u'pre_patient_selection', receiver = self._schedule_data_reget)
+		gmDispatcher.connect(signal = u'pre_patient_selection', receiver = self._on_pre_patient_selection)
 		gmDispatcher.connect(signal = u'post_patient_selection', receiver = self._schedule_data_reget)
 		gmDispatcher.connect(signal = u'test_result_mod_db', receiver = self._schedule_data_reget)
-		# FIXME: listen for review changes, too
+		gmDispatcher.connect(signal = u'reviewed_test_results_mod_db', receiver = self._schedule_data_reget)
+	#--------------------------------------------------------
+	def _on_pre_patient_selection(self, evt):
+		wx.CallAfter(self.__on_pre_patient_selection)
+	#--------------------------------------------------------
+	def __on_pre_patient_selection(self):
+		self.data_grid.patient = None
 	#--------------------------------------------------------
 	def _on_review_button_pressed(self, evt):
 		self.data_grid.sign_current_selection()
@@ -456,7 +540,12 @@ if __name__ == '__main__':
 
 #================================================================
 # $Log: gmMeasurementWidgets.py,v $
-# Revision 1.9  2008-04-16 20:39:39  ncq
+# Revision 1.10  2008-04-22 21:18:49  ncq
+# - implement signing
+# - improved tooltip
+# - properly clear grid when active patient changes
+#
+# Revision 1.9  2008/04/16 20:39:39  ncq
 # - working versions of the wxGlade code and use it, too
 # - show client version in login dialog
 #
