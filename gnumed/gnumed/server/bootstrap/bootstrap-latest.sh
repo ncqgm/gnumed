abf7410e15c30b9f084abbdee6fb0ac9df3e0909
Karsten Hilbert <karsten.hilbert@gmx.net>
2008-07-12 16:10:08 +0000

    - use ./upgrade-db.sh rather than script everything ourselves

--- gnumed/gnumed/server/bootstrap/bootstrap-latest.sh
+++ gnumed/gnumed/server/bootstrap/bootstrap-latest.sh
@@ -49,6 +49,8 @@ echo_msg "It contains all the currently working parts including"
 echo_msg "localizations for countries you don't live in. This does"
 echo_msg "not disturb the operation of the GNUmed client in your"
 echo_msg "country in any way."
+
+
 echo_msg "==========================================================="
 echo_msg "1) Dropping old baseline gnumed_v2 database if there is any."
 echo_msg "   (you may need to provide the password for ${USER})"
@@ -60,7 +62,7 @@ echo_msg "2) bootstrapping databases"
 
 
 # baseline v2
-LOG="${GM_LOG_BASE}/bootstrap-latest-v2.log"
+LOG="${GM_LOG_BASE}/bootstrap-v2.log"
 rm -rf ${LOG}
 CONF="redo-v2.conf"
 export GM_CORE_DB="gnumed_v2"
@@ -73,10 +75,7 @@ unset GM_CORE_DB
 
 
 # v2 -> v3
-LOG="${GM_LOG_BASE}/bootstrap-latest-v3.log"
-rm -rf ${LOG}
-CONF="update_db-v2_v3.conf"
-./bootstrap_gm_db_system.py --log-file=${LOG} --conf-file=${CONF} --${QUIET}
+./upgrade-db.sh 2 3 no-backup
 if test "$?" != "0" ; then
 	echo "Bootstrapping \"gnumed_v3\" did not finish successfully. Aborting."
 	exit 1
@@ -87,10 +86,7 @@ sudo -u postgres dropdb ${PORT_DEF} gnumed_v2
 
 
 # v3 -> v4
-LOG="${GM_LOG_BASE}/bootstrap-latest-v4.log"
-rm -rf ${LOG}
-CONF="update_db-v3_v4.conf"
-./bootstrap_gm_db_system.py --log-file=${LOG} --conf-file=${CONF} --${QUIET}
+./upgrade-db.sh 3 4 no-backup
 if test "$?" != "0" ; then
 	echo "Bootstrapping \"gnumed_v4\" did not finish successfully. Aborting."
 	exit 1
@@ -101,10 +97,7 @@ sudo -u postgres dropdb ${PORT_DEF} gnumed_v3
 
 
 # v4 -> v5
-LOG="${GM_LOG_BASE}/bootstrap-latest-v5.log"
-rm -rf ${LOG}
-CONF="update_db-v4_v5.conf"
-./bootstrap_gm_db_system.py --log-file=${LOG} --conf-file=${CONF} --${QUIET}
+./upgrade-db.sh 4 5 no-backup
 if test "$?" != "0" ; then
 	echo "Bootstrapping \"gnumed_v5\" did not finish successfully. Aborting."
 	exit 1
@@ -115,10 +108,7 @@ sudo -u postgres dropdb ${PORT_DEF} gnumed_v4
 
 
 # v5 -> v6
-LOG="${GM_LOG_BASE}/bootstrap-latest-v6.log"
-rm -rf ${LOG}
-CONF="update_db-v5_v6.conf"
-./bootstrap_gm_db_system.py --log-file=${LOG} --conf-file=${CONF} --${QUIET}
+./upgrade-db.sh 5 6 no-backup
 if test "$?" != "0" ; then
 	echo "Bootstrapping \"gnumed_v6\" did not finish successfully. Aborting."
 	exit 1
@@ -129,10 +119,7 @@ sudo -u postgres dropdb ${PORT_DEF} gnumed_v5
 
 
 # v6 -> v7
-LOG="${GM_LOG_BASE}/bootstrap-latest-v7.log"
-rm -rf ${LOG}
-CONF="update_db-v6_v7.conf"
-./bootstrap_gm_db_system.py --log-file=${LOG} --conf-file=${CONF} --${QUIET}
+./upgrade-db.sh 6 7 no-backup
 if test "$?" != "0" ; then
 	echo "Bootstrapping \"gnumed_v7\" did not finish successfully. Aborting."
 	exit 1
@@ -143,10 +130,7 @@ sudo -u postgres dropdb ${PORT_DEF} gnumed_v6
 
 
 # v7 -> v8
-LOG="${GM_LOG_BASE}/bootstrap-latest-v8.log"
-rm -rf ${LOG}
-CONF="update_db-v7_v8.conf"
-./bootstrap_gm_db_system.py --log-file=${LOG} --conf-file=${CONF} --${QUIET}
+./upgrade-db.sh 7 8 no-backup
 if test "$?" != "0" ; then
 	echo "Bootstrapping \"gnumed_v8\" did not finish successfully. Aborting."
 	exit 1
@@ -157,10 +141,7 @@ sudo -u postgres dropdb ${PORT_DEF} gnumed_v7
 
 
 # v8 -> v9
-LOG="${GM_LOG_BASE}/bootstrap-latest-v9.log"
-rm -rf ${LOG}
-CONF="update_db-v8_v9.conf"
-./bootstrap_gm_db_system.py --log-file=${LOG} --conf-file=${CONF} --${QUIET}
+./upgrade-db.sh 8 9 no-backup
 if test "$?" != "0" ; then
 	echo "Bootstrapping \"gnumed_v9\" did not finish successfully. Aborting."
 	exit 1
