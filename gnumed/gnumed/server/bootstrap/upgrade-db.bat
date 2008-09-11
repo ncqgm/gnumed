55c3120f5de470f957b69b0504d5a77dca127041
shilbert <shilbert>
2008-09-11 19:31:53 +0000

    - port of the new options from Linux version to make it work for v9 database

--- gnumed/gnumed/server/bootstrap/upgrade-db.bat
+++ gnumed/gnumed/server/bootstrap/upgrade-db.bat
@@ -1,23 +1,67 @@
 @echo off
-SET PYTHONPATH="%PYTHONPATH%;../../"
+
+REM ========================================================
+REM Upgrade GNUmed database from version to version.
+REM
+REM usage:
+REM  upgrade-db.sh vX vX+1 <secrets>
+REM
+REM limitations:
+REM  Only works from version to version sequentially.
+REM
+REM prerequisites:
+REM  update_db-vX_vX+1.conf must exist
+REM
+REM "secret" command line options:
+REM  no-backup
+REM  - don't create a database backup (you got faith)
+REM  no-compression
+REM  - create a backup but don't compress it (you got plenty of disk but REM run low on cycles)
+REM  quiet
+REM  - display failures only, don't chatter
+REM
+REM ========================================================
+
+
+REM SET PYTHONPATH="%PYTHONPATH%;../../"
+
+
+SET PREV_VER=%1
+SET NEXT_VER=%2
+SET SKIP_BACKUP=%3
+SET BZIP_BACKUP=%3
+SET QUIET=%3
+SET LOG_BASE=.
+SET LOG=%LOG_BASE%\update_db-v%PREV_VER%_v%NEXT_VER%.log
+SET CONF=update_db-v%PREV_VER%_v%NEXT_VER%.conf
+SET BAK_FILE=backup-upgrade-v%PREV_VER%-to-v%NEXT_VER%.sql
+
+
 if %1=="" echo "usage: update-db x x+1"
 
-set PREV_VER=%1
-set NEXT_VER=%2
-set LOG=update_db-v%PREV_VER%_v%NEXT_VER%.log
-set CONF=update_db-v%PREV_VER%_v%NEXT_VER%.conf
-set GM_CORE_DB=gnumed_v%NEXT_VER%
+REM if you need to adjust the database name to something
+REM other than what the config file has you can use the
+REM following environment variable:
+REM SET GM_CORE_DB=gnumed_v%NEXT_VER%
+
+REM if you need to adjust the port you want to use to
+REM connect to PostgreSQL you can use the environment
+REM variable below (this may be necessary if your PostgreSQL
+REM server is running on a port different from the default 5432)
+REM SET GM_DB_PORT=5433
+
+
 
 echo ===========================================================
-echo Bootstrapping GNUmed database.
+echo Upgrading GNUmed database.
 echo ...
 echo This will non-destructively transform a GNUmed database
-echo of version v%PREV_VER% into a version v%NEXT_VER% database.
+echo of version v%NEXT_VER% from an existing version v%PREV_VER% database.
+echo Existing data is transferred and transformed as necessary.
 echo ...
-echo The name of the new database will be %GM_CORE_DB% .
+echo The name of the new database will be gnumed_v%NEXT_VER% .
 echo ===========================================================
 
-del %LOG%
-echo =======================
-echo bootstrapping database
-python bootstrap_gm_db_system.py --log-file=%LOG% --conf-file=%CONF%
+
+echo 2) upgrading to new database ...
+python bootstrap_gm_db_system.py --log-file=%LOG% --conf-file=%CONF% %QUIET
