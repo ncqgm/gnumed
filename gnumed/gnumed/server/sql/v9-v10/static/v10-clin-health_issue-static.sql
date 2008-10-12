55088d22ecd941678b5934023ab65cd697a04e99
Karsten Hilbert <karsten.hilbert@gmx.net>
2008-10-12 15:00:03 +0000

    - drop trigger to ensure patient consistency

--- gnumed/gnumed/server/sql/v9-v10/static/v10-clin-health_issue-static.sql
+++ gnumed/gnumed/server/sql/v9-v10/static/v10-clin-health_issue-static.sql
@@ -5,24 +5,29 @@
 -- Author: Karsten Hilbert
 -- 
 -- ==============================================================
--- $Id: v10-clin-health_issue-static.sql,v 1.1 2008-09-02 15:42:37 ncq Exp $
--- $Revision: 1.1 $
+-- $Id: v10-clin-health_issue-static.sql,v 1.2 2008-10-12 15:00:03 ncq Exp $
+-- $Revision: 1.2 $
 
 -- --------------------------------------------------------------
 \set ON_ERROR_STOP 1
 
 -- --------------------------------------------------------------
--- drop function clin.trf_ensure_issue_encounter_patient_consistency() cascade;
+drop function clin.trf_ensure_issue_encounter_patient_consistency() cascade;
+
+
 alter table clin.health_issue drop column fk_patient cascade;
 
 
 alter table audit.log_health_issue drop column fk_patient;
 -- --------------------------------------------------------------
-select gm.log_script_insertion('$RCSfile: v10-clin-health_issue-static.sql,v $', '$Revision: 1.1 $');
+select gm.log_script_insertion('$RCSfile: v10-clin-health_issue-static.sql,v $', '$Revision: 1.2 $');
 
 -- ==============================================================
 -- $Log: v10-clin-health_issue-static.sql,v $
--- Revision 1.1  2008-09-02 15:42:37  ncq
+-- Revision 1.2  2008-10-12 15:00:03  ncq
+-- - drop trigger to ensure patient consistency
+--
+-- Revision 1.1  2008/09/02 15:42:37  ncq
 -- - new
 --
 --
