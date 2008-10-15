1dd918ba8e1840525c6742535c5affd09c4ade1f
Karsten Hilbert <karsten.hilbert@gmx.net>
2008-10-15 15:39:55 +0000

    - bump version

--- gnumed/gnumed/dists/Linux/net_install-gnumed_client.sh
+++ gnumed/gnumed/dists/Linux/net_install-gnumed_client.sh
@@ -2,10 +2,10 @@
 
 # ===========================================================
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/dists/Linux/Attic/net_install-gnumed_client.sh,v $
-# $Id: net_install-gnumed_client.sh,v 1.4.2.2 2008-09-09 18:39:54 ncq Exp $
+# $Id: net_install-gnumed_client.sh,v 1.4.2.3 2008-10-15 15:39:55 ncq Exp $
 # ===========================================================
 
-VER_LATEST="0.3.2"
+VER_LATEST="0.3.3"
 INSTALL_BASE=~/".gnumed/client-installation"
 
 # ===========================================================
@@ -192,7 +192,10 @@ mc -e gm-from-cvs.conf
 
 # ============================================
 # $Log: net_install-gnumed_client.sh,v $
-# Revision 1.4.2.2  2008-09-09 18:39:54  ncq
+# Revision 1.4.2.3  2008-10-15 15:39:55  ncq
+# - bump version
+#
+# Revision 1.4.2.2  2008/09/09 18:39:54  ncq
 # - bump version
 #
 # Revision 1.4.2.1  2008/08/31 14:26:59  ncq
