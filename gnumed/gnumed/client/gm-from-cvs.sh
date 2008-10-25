5b197e8d21755524c07d17e175897c05552c7127
Karsten Hilbert <karsten.hilbert@gmx.net>
2008-10-25 20:37:03 +0000

    - turn on psycopg debugging if available

--- gnumed/gnumed/client/gm-from-cvs.sh
+++ gnumed/gnumed/client/gm-from-cvs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/gm-from-cvs.sh,v $
-# $Revision: 1.13 $
+# $Revision: 1.14 $
 
 # maybe force some locale setting here
 #export LANGUAGE=fr
@@ -25,6 +25,7 @@ fi
 
 
 # run
+PSYCOPG_DEBUG="on"		# should actually be done within gnumed.py based on --debug
 LOG="gm-from-cvs.log"
 python wxpython/gnumed.py --log-file=$LOG --conf-file=gm-from-cvs.conf --override-schema-check --debug --local-import
 # --profile=gm-from-cvs.prof
