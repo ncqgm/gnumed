e9247b4676f32f0bee24d2afbd28e77d67919fc8
Karsten Hilbert <karsten.hilbert@gmx.net>
2008-10-26 01:24:21 +0000

    - adjust for "user" -> db_user

--- gnumed/gnumed/server/sql/v9-v10/dynamic/v10-i18n-dynamic.sql
+++ gnumed/gnumed/server/sql/v9-v10/dynamic/v10-i18n-dynamic.sql
@@ -5,14 +5,18 @@
 -- Author: karsten.hilbert@gmx.net
 -- 
 -- ==============================================================
--- $Id: v10-i18n-dynamic.sql,v 1.2 2008-10-25 20:50:32 ncq Exp $
--- $Revision: 1.2 $
+-- $Id: v10-i18n-dynamic.sql,v 1.3 2008-10-26 01:24:21 ncq Exp $
+-- $Revision: 1.3 $
 
 -- --------------------------------------------------------------
 --set default_transaction_read_only to off;
 \set ON_ERROR_STOP 1
 
--- =============================================
+-- --------------------------------------------------------------
+comment on column i18n.curr_lang.db_user is
+'The database account this language setting applies to.';
+
+-- --------------------------------------------------------------
 \unset ON_ERROR_STOP
 drop function i18n.set_curr_lang(text, name) cascade;
 \set ON_ERROR_STOP 1
@@ -24,7 +28,7 @@ create or replace function i18n.set_curr_lang(text, name)
 	as E'
 DECLARE
 	_lang ALIAS FOR $1;
-	_user ALIAS FOR $2;
+	_db_user ALIAS FOR $2;
 	lang_has_tx boolean;
 BEGIN
 	select into lang_has_tx exists(select pk from i18n.translations where lang = _lang);
@@ -34,8 +38,8 @@ BEGIN
 		return False;
 	end if;
 
-	delete from i18n.curr_lang where quote_ident(user) = _user;
-	insert into i18n.curr_lang ("user", lang) values (_user, _lang);
+	delete from i18n.curr_lang where db_user = _db_user;
+	insert into i18n.curr_lang (db_user, lang) values (_db_user, _lang);
 	return true;
 END;
 ';
@@ -45,6 +49,7 @@ comment on function i18n.set_curr_lang(text, name) is
 	 the second argument if translations are available';
 
 
+-- --------------------------------------------------------------
 \unset ON_ERROR_STOP
 drop function i18n.set_curr_lang(text) cascade;
 \set ON_ERROR_STOP 1
@@ -67,6 +72,19 @@ comment on function i18n.set_curr_lang(text) is
 	 - for "current user"
 	 - only if translations for this language are available';
 
+-- =============================================
+create or replace function i18n.get_curr_lang(text)
+	returns text
+	language sql
+	as 'select lang from i18n.curr_lang where db_user = $1'
+;
+
+
+create or replace function i18n.get_curr_lang()
+	returns text
+	language sql
+	as 'select i18n.get_curr_lang(CURRENT_USER)'
+;
 
 -- =============================================
 create or replace function i18n._(text, text)
@@ -94,7 +112,7 @@ comment on function i18n._(text, text) is
 	 (2nd argument) for the current user or the input,
 	 created in public schema for easy access';
 
-
+-- =============================================
 create or replace function i18n._(text)
 	returns text
 	language sql
@@ -106,27 +124,15 @@ comment on function i18n._(text) is
 	 or the input,
 	 created in public schema for easy access';
 
-
--- =============================================
-create or replace function i18n.get_curr_lang(text)
-	returns text
-	language sql
-	as 'select lang from i18n.curr_lang where "user" = $1'
-;
-
-
-create or replace function i18n.get_curr_lang()
-	returns text
-	language sql
-	as 'select i18n.get_curr_lang(quote_literal(CURRENT_USER))'
-;
-
 -- --------------------------------------------------------------
-select gm.log_script_insertion('$RCSfile: v10-i18n-dynamic.sql,v $', '$Revision: 1.2 $');
+select gm.log_script_insertion('$RCSfile: v10-i18n-dynamic.sql,v $', '$Revision: 1.3 $');
 
 -- ==============================================================
 -- $Log: v10-i18n-dynamic.sql,v $
--- Revision 1.2  2008-10-25 20:50:32  ncq
+-- Revision 1.3  2008-10-26 01:24:21  ncq
+-- - adjust for "user" -> db_user
+--
+-- Revision 1.2  2008/10/25 20:50:32  ncq
 -- - forward port "user" fixes for v9 -> v10, too, since someone might upgrade an unpatched v9
 --
 -- Revision 1.1  2008/10/12 14:58:07  ncq
