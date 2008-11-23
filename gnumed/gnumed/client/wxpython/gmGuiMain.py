f77eebde7b6c8709fd9bf7f8fa3328dc0f7c8607
Karsten Hilbert <karsten.hilbert@gmx.net>
2008-11-23 17:23:51 +0000

    - bump version

--- gnumed/gnumed/client/wxpython/gmGuiMain.py
+++ gnumed/gnumed/client/wxpython/gmGuiMain.py
@@ -15,8 +15,8 @@ copyright: authors
 """
 #==============================================================================
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/wxpython/gmGuiMain.py,v $
-# $Id: gmGuiMain.py,v 1.426.2.9 2008-11-21 15:24:18 ncq Exp $
-__version__ = "$Revision: 1.426.2.9 $"
+# $Id: gmGuiMain.py,v 1.426.2.10 2008-11-23 17:23:51 ncq Exp $
+__version__ = "$Revision: 1.426.2.10 $"
 __author__  = "H. Herb <hherb@gnumed.net>,\
 			   K. Hilbert <Karsten.Hilbert@gmx.net>,\
 			   I. Haywood <i.haywood@ugrad.unimelb.edu.au>"
@@ -70,7 +70,7 @@ _scripting_listener = None
 
 expected_db_ver = u'v9'
 
-current_client_ver = u'0.3.6'
+current_client_ver = u'0.3.7'
 current_client_branch = '0.3'
 
 _log = logging.getLogger('gm.main')
@@ -2694,7 +2694,10 @@ if __name__ == '__main__':
 
 #==============================================================================
 # $Log: gmGuiMain.py,v $
-# Revision 1.426.2.9  2008-11-21 15:24:18  ncq
+# Revision 1.426.2.10  2008-11-23 17:23:51  ncq
+# - bump version
+#
+# Revision 1.426.2.9  2008/11/21 15:24:18  ncq
 # - ignoring mismatching sys locale is stored in user prefs which includes
 #   --conf so need to check for it there, too
 #
