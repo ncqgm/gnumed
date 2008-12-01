0bc93d39b787d63a140dbfef4ef745649acf8085
Karsten Hilbert <karsten.hilbert@gmx.net>
2008-12-01 21:48:46 +0000

    - no more fk_patient so no more consistency checking

--- gnumed/gnumed/server/sql/v9-v10/dynamic/v10-clin-episode-dynamic.sql
+++ gnumed/gnumed/server/sql/v9-v10/dynamic/v10-clin-episode-dynamic.sql
@@ -5,53 +5,26 @@
 -- Author: Karsten Hilbert
 -- 
 -- ==============================================================
--- $Id: v10-clin-episode-dynamic.sql,v 1.1 2008-11-24 11:05:32 ncq Exp $
--- $Revision: 1.1 $
+-- $Id: v10-clin-episode-dynamic.sql,v 1.2 2008-12-01 21:48:46 ncq Exp $
+-- $Revision: 1.2 $
 
 -- --------------------------------------------------------------
 \set ON_ERROR_STOP 1
 
 -- --------------------------------------------------------------
+\unset ON_ERROR_STOP
 drop function clin.trf_ensure_episode_issue_patient_consistency() cascade;
-
-
-create function clin.trf_ensure_episode_issue_patient_consistency()
-	returns trigger
-	language 'plpgsql'
-	as '
-declare
-	issue_patient integer;
-	msg text;
-begin
-	-- insert or update, both have NEW.*
-
-	if NEW.fk_health_issue is NULL then
-		return NEW;
-	end if;
-
-	select into issue_patient fk_patient from clin.encounter where pk = (
-		select fk_encounter from clin.health_issue where pk = NEW.fk_health_issue
-	);
-	if issue_patient != NEW.fk_patient then
-		msg := ''clin.trf_ensure_episode_issue_patient_consistency(): clin.episode must have the same <fk_patient> as the clin.health_issue it is attached to'';
-		raise exception ''%'', msg;
-	end if;
-
-	return NEW;
-end;';
-
-
-create trigger tr_ensure_episode_issue_patient_consistency
-	before insert or update on clin.episode
-	for each row execute procedure clin.trf_ensure_episode_issue_patient_consistency()
-;
+\set ON_ERROR_STOP 1
 
 -- --------------------------------------------------------------
-select gm.log_script_insertion('$RCSfile: v10-clin-episode-dynamic.sql,v $', '$Revision: 1.1 $');
+select gm.log_script_insertion('$RCSfile: v10-clin-episode-dynamic.sql,v $', '$Revision: 1.2 $');
 
 -- ==============================================================
 -- $Log: v10-clin-episode-dynamic.sql,v $
--- Revision 1.1  2008-11-24 11:05:32  ncq
+-- Revision 1.2  2008-12-01 21:48:46  ncq
+-- - no more fk_patient so no more consistency checking
+--
+-- Revision 1.1  2008/11/24 11:05:32  ncq
 -- - fix patient consistency trigger
 --
 --
