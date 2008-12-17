793b5904f8f8ddd9d1f79630ce82970cbcdc8fc1
Karsten Hilbert <karsten.hilbert@gmx.net>
2008-12-17 21:51:35 +0000

    - add --hipaa - pass on arguments

--- gnumed/gnumed/client/gm-from-cvs.sh
+++ gnumed/gnumed/client/gm-from-cvs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/gm-from-cvs.sh,v $
-# $Revision: 1.14 $
+# $Revision: 1.15 $
 
 # maybe force some locale setting here
 #export LANGUAGE=fr
@@ -27,5 +27,6 @@ fi
 # run
 PSYCOPG_DEBUG="on"		# should actually be done within gnumed.py based on --debug
 LOG="gm-from-cvs.log"
-python wxpython/gnumed.py --log-file=$LOG --conf-file=gm-from-cvs.conf --override-schema-check --debug --local-import
+python wxpython/gnumed.py --log-file=$LOG --conf-file=gm-from-cvs.conf --override-schema-check --debug --local-import $@
+# --hipaa
 # --profile=gm-from-cvs.prof
