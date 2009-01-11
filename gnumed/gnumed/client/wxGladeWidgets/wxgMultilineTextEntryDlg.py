9111694bb259ae43e84bbf124d14be3a2db0999a
Karsten Hilbert <karsten.hilbert@gmx.net>
2009-01-11 19:16:42 +0000

    - more action buttons

--- gnumed/gnumed/client/wxGladeWidgets/wxgMultilineTextEntryDlg.py
+++ gnumed/gnumed/client/wxGladeWidgets/wxgMultilineTextEntryDlg.py
@@ -17,19 +17,27 @@ class wxgMultilineTextEntryDlg(wx.Dialog):
         self._LBL_msg = wx.StaticText(self, -1, "")
         self._TCTRL_text = wx.TextCtrl(self, -1, "", style=wx.TE_MULTILINE|wx.HSCROLL)
         self._BTN_save = wx.Button(self, wx.ID_SAVE, "")
+        self._BTN_delete = wx.Button(self, wx.ID_DELETE, "")
+        self._BTN_clear = wx.Button(self, wx.ID_CLEAR, "")
+        self._BTN_restore = wx.Button(self, wx.ID_REVERT_TO_SAVED, "")
         self._BTN_cancel = wx.Button(self, wx.ID_CANCEL, "")
 
         self.__set_properties()
         self.__do_layout()
 
         self.Bind(wx.EVT_BUTTON, self._on_save_button_pressed, self._BTN_save)
+        self.Bind(wx.EVT_BUTTON, self._on_delete_button_pressed, self._BTN_delete)
+        self.Bind(wx.EVT_BUTTON, self._on_clear_button_pressed, self._BTN_clear)
+        self.Bind(wx.EVT_BUTTON, self._on_restore_button_pressed, self._BTN_restore)
         # end wxGlade
 
     def __set_properties(self):
         # begin wxGlade: wxgMultilineTextEntryDlg.__set_properties
         self.SetTitle(_("Generic multi line text entry dialog"))
-        self.SetSize((400, 300))
+        self.SetSize((600, 350))
         self._BTN_save.Enable(False)
+        self._BTN_delete.Enable(False)
+        self._BTN_restore.Enable(False)
         # end wxGlade
 
     def __do_layout(self):
@@ -38,10 +46,13 @@ class wxgMultilineTextEntryDlg(wx.Dialog):
         __szr_buttons = wx.BoxSizer(wx.HORIZONTAL)
         __szr_main.Add(self._LBL_msg, 0, wx.ALL|wx.EXPAND, 5)
         __szr_main.Add(self._TCTRL_text, 1, wx.ALL|wx.EXPAND, 5)
+        __szr_buttons.Add(self._BTN_save, 0, wx.RIGHT|wx.EXPAND, 5)
+        __szr_buttons.Add(self._BTN_delete, 0, wx.EXPAND, 3)
         __szr_buttons.Add((20, 20), 1, wx.EXPAND, 0)
-        __szr_buttons.Add(self._BTN_save, 0, wx.RIGHT|wx.EXPAND, 3)
-        __szr_buttons.Add(self._BTN_cancel, 0, wx.LEFT|wx.EXPAND, 3)
-        __szr_buttons.Add((20, 20), 1, wx.EXPAND, 0)
+        __szr_buttons.Add(self._BTN_clear, 0, wx.RIGHT|wx.EXPAND, 5)
+        __szr_buttons.Add(self._BTN_restore, 0, wx.EXPAND, 3)
+        __szr_buttons.Add((20, 20), 3, wx.EXPAND, 0)
+        __szr_buttons.Add(self._BTN_cancel, 0, wx.EXPAND, 3)
         __szr_main.Add(__szr_buttons, 0, wx.ALL|wx.EXPAND, 4)
         self.SetSizer(__szr_main)
         self.Layout()
@@ -50,6 +61,18 @@ class wxgMultilineTextEntryDlg(wx.Dialog):
 
     def _on_save_button_pressed(self, event): # wxGlade: wxgMultilineTextEntryDlg.<event_handler>
         print "Event handler `_on_save_button_pressed' not implemented!"
+        event.Skip()
+
+    def _on_delete_button_pressed(self, event): # wxGlade: wxgMultilineTextEntryDlg.<event_handler>
+        print "Event handler `_on_delete_button_pressed' not implemented"
+        event.Skip()
+
+    def _on_clear_button_pressed(self, event): # wxGlade: wxgMultilineTextEntryDlg.<event_handler>
+        print "Event handler `_on_clear_button_pressed' not implemented"
+        event.Skip()
+
+    def _on_restore_button_pressed(self, event): # wxGlade: wxgMultilineTextEntryDlg.<event_handler>
+        print "Event handler `_on_restore_button_pressed' not implemented"
         event.Skip()
 
 # end of class wxgMultilineTextEntryDlg
