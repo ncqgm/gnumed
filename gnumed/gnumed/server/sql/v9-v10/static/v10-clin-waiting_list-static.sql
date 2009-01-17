f24673a3091d743f16f135343cb5fba75eb1d361
Karsten Hilbert <karsten.hilbert@gmx.net>
2009-01-17 23:16:52 +0000

    - cleanup

--- gnumed/gnumed/server/sql/v9-v10/static/v10-clin-waiting_list-static.sql
+++ gnumed/gnumed/server/sql/v9-v10/static/v10-clin-waiting_list-static.sql
@@ -5,8 +5,8 @@
 -- Author: Karsten Hilbert
 -- 
 -- ==============================================================
--- $Id: v10-clin-waiting_list-static.sql,v 1.1 2009-01-16 13:32:19 ncq Exp $
--- $Revision: 1.1 $
+-- $Id: v10-clin-waiting_list-static.sql,v 1.2 2009-01-17 23:16:52 ncq Exp $
+-- $Revision: 1.2 $
 
 -- --------------------------------------------------------------
 \set ON_ERROR_STOP 1
@@ -16,17 +16,19 @@ alter table clin.waiting_list
 	add column area
 		text;
 
-
 alter table audit.log_waiting_list
 	add column area
 		text;
 
 -- --------------------------------------------------------------
-select gm.log_script_insertion('$RCSfile: v10-clin-waiting_list-static.sql,v $', '$Revision: 1.1 $');
+select gm.log_script_insertion('$RCSfile: v10-clin-waiting_list-static.sql,v $', '$Revision: 1.2 $');
 
 -- ==============================================================
 -- $Log: v10-clin-waiting_list-static.sql,v $
--- Revision 1.1  2009-01-16 13:32:19  ncq
+-- Revision 1.2  2009-01-17 23:16:52  ncq
+-- - cleanup
+--
+-- Revision 1.1  2009/01/16 13:32:19  ncq
 -- - new
 --
 --
