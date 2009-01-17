35442a8e6ab1c026ba9f1d30b6ef09a8295530b0
Karsten Hilbert <karsten.hilbert@gmx.net>
2009-01-17 23:16:35 +0000

    - add proper signals - improve view

--- gnumed/gnumed/server/sql/v9-v10/dynamic/v10-clin-waiting_list-dynamic.sql
+++ gnumed/gnumed/server/sql/v9-v10/dynamic/v10-clin-waiting_list-dynamic.sql
@@ -5,16 +5,27 @@
 -- Author: Karsten Hilbert
 -- 
 -- ==============================================================
--- $Id: v10-clin-waiting_list-dynamic.sql,v 1.1 2009-01-16 13:30:40 ncq Exp $
--- $Revision: 1.1 $
+-- $Id: v10-clin-waiting_list-dynamic.sql,v 1.2 2009-01-17 23:16:35 ncq Exp $
+-- $Revision: 1.2 $
 
 -- --------------------------------------------------------------
 \set ON_ERROR_STOP 1
+--set default_transaction_read_only to off;
 
 -- --------------------------------------------------------------
+\unset ON_ERROR_STOP
+alter table clin.waiting_list drop constraint non_empty_area;
+\set ON_ERROR_STOP 1
+
 alter table clin.waiting_list
 	add constraint non_empty_area
 		check (trim(area) <> '');
+
+comment on column clin.waiting_list.area is
+	'an arbitrary value by which filtering waiting patients into zones becomes possible';
+
+select gm.add_table_for_notifies('clin', 'waiting_list');
+select gm.add_table_for_notifies('clin', 'waiting_list', 'waiting_list_generic');
 
 
 \unset ON_ERROR_STOP
@@ -25,6 +36,8 @@ create view clin.v_waiting_list as
 select
 	wl.list_position
 		as list_position,
+	wl.area
+		as waiting_zone,
 	wl.urgency
 		as urgency,
 	i.title
@@ -45,34 +58,39 @@ select
 		as registered,
 	(select now() - wl.registered)
 		as waiting_time,
-	vmre.started
-		as start_most_recent_encounter,
-	vmre.reason_for_encounter
-		as most_recent_rfe,
+	(select to_char(age(now(), wl.registered), 'HH24:MI'))
+		as waiting_time_formatted,
+--	(select started from clin.v_most_recent_encounters where wl.fk_patient = vmre.pk_patient)
+--		as start_most_recent_encounter,
+--	(select reason_for_encounter from clin.v_most_recent_encounters where wl.fk_patient = vmre.pk_patient)
+--		as most_recent_rfe,
 	wl.comment
 		as comment,
-	(select started from clin.encounter ce
-	 where vmre.pk_encounter = ce.pk
-	 order by last_affirmed desc limit 1 offset 1
-	)	as start_previous_encounter,
+--	(select started from clin.encounter ce
+--	 where vmre.pk_encounter = ce.pk
+--	 order by last_affirmed desc limit 1 offset 1
+--	)	as start_previous_encounter,
 	i.pk
 		as pk_identity,
 	n.id
 		as pk_name,
 	i.pupic
-		as pupic
+		as pupic,
+	wl.pk
+		as pk_waiting_list
 from
 	clin.waiting_list wl,
 	dem.identity i,
-	dem.names n,
-	clin.v_most_recent_encounters vmre
+	dem.names n
 where
 	wl.fk_patient = i.pk and
 	wl.fk_patient = n.id_identity and
-	wl.fk_patient = vmre.pk_patient and
 	i.deceased is NULL and
 	n.active is true
 ;
+
+
+grant select on clin.v_waiting_list to group "gm-public";
 
 
 insert into clin.waiting_list (
@@ -88,11 +106,15 @@ insert into clin.waiting_list (
 );
 
 -- --------------------------------------------------------------
-select gm.log_script_insertion('$RCSfile: v10-clin-waiting_list-dynamic.sql,v $', '$Revision: 1.1 $');
+select gm.log_script_insertion('$RCSfile: v10-clin-waiting_list-dynamic.sql,v $', '$Revision: 1.2 $');
 
 -- ==============================================================
 -- $Log: v10-clin-waiting_list-dynamic.sql,v $
--- Revision 1.1  2009-01-16 13:30:40  ncq
+-- Revision 1.2  2009-01-17 23:16:35  ncq
+-- - add proper signals
+-- - improve view
+--
+-- Revision 1.1  2009/01/16 13:30:40  ncq
 -- - new
 --
 --
