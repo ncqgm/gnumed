5b709955d21b3060bdc297bf02f1a886d24ace9a
Karsten Hilbert <karsten.hilbert@gmx.net>
2009-01-17 23:15:23 +0000

    - add waiting list to default workplace

--- gnumed/gnumed/server/sql/v9-v10/dynamic/v10-cfg-cfg_str_array.sql
+++ gnumed/gnumed/server/sql/v9-v10/dynamic/v10-cfg-cfg_str_array.sql
@@ -5,8 +5,8 @@
 -- Author: Karsten Hilbert
 -- 
 -- ==============================================================
--- $Id: v10-cfg-cfg_str_array.sql,v 1.1 2008-11-11 21:15:26 ncq Exp $
--- $Revision: 1.1 $
+-- $Id: v10-cfg-cfg_str_array.sql,v 1.2 2009-01-17 23:15:23 ncq Exp $
+-- $Revision: 1.2 $
 
 -- --------------------------------------------------------------
 \set ON_ERROR_STOP 1
@@ -14,7 +14,7 @@ set default_transaction_read_only to off;
 
 -- --------------------------------------------------------------
 update cfg.cfg_str_array set
-	value = '{"gmProviderInboxPlugin","gmNotebookedPatientEditionPlugin","gmEMRBrowserPlugin","gmNotebookedProgressNoteInputPlugin","gmSoapPlugin","gmEMRJournalPlugin","gmShowMedDocs","gmScanIdxMedDocsPlugin","gmMeasurementsGridPlugin","gmKOrganizerPlugin","gmDataMiningPlugin","gmXdtViewer","gmManual"}'
+	value = '{"gmProviderInboxPlugin","gmWaitingListPlugin","gmNotebookedPatientEditionPlugin","gmEMRBrowserPlugin","gmNotebookedProgressNoteInputPlugin","gmSoapPlugin","gmEMRJournalPlugin","gmShowMedDocs","gmScanIdxMedDocsPlugin","gmMeasurementsGridPlugin","gmKOrganizerPlugin","gmDataMiningPlugin","gmXdtViewer","gmManual"}'
 where
 	fk_item in (
 		select pk_cfg_item
@@ -25,11 +25,14 @@ where
 		)
 ;
 -- --------------------------------------------------------------
-select gm.log_script_insertion('$RCSfile: v10-cfg-cfg_str_array.sql,v $', '$Revision: 1.1 $');
+select gm.log_script_insertion('$RCSfile: v10-cfg-cfg_str_array.sql,v $', '$Revision: 1.2 $');
 
 -- ==============================================================
 -- $Log: v10-cfg-cfg_str_array.sql,v $
--- Revision 1.1  2008-11-11 21:15:26  ncq
+-- Revision 1.2  2009-01-17 23:15:23  ncq
+-- - add waiting list to default workplace
+--
+-- Revision 1.1  2008/11/11 21:15:26  ncq
 -- - add new soap plugin
 --
 --
