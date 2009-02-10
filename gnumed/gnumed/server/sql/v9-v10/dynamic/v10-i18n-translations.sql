b0acaf119335e008b0b0e15808c008937e660f31
Karsten Hilbert <karsten.hilbert@gmx.net>
2009-02-10 18:43:37 +0000

    - add some translation

---
+++ gnumed/gnumed/server/sql/v9-v10/dynamic/v10-i18n-translations.sql
@@ -0,0 +1,18 @@
+set default_transaction_read_only to off
+
+\unset ON_ERROR_STOP
+
+select i18n.upd_tx('de_DE', 'Central African Republic', 'Zentralafrikanische Republik');
+
+select i18n.upd_tx('de_DE', 'japanese B encephalitis', 'Japanische Enzephalitis B');
+select i18n.upd_tx('de_DE', 'varicella', 'Varizellen');
+select i18n.upd_tx('de_DE', 'yellow fever', 'Gelbfieber');
+select i18n.upd_tx('de_DE', 'female only', 'nur Frauen');
+
+select i18n.upd_tx('de_DE', 'Allergy state', 'Allergiestatus');
+select i18n.upd_tx('de_DE', 'no known allergies', 'keine bekannten Allergie');
+select i18n.upd_tx('de_DE', 'does have allergies', 'Allergien vorhanden');
+select i18n.upd_tx('de_DE', 'unknown, unasked', 'unbekannt, nicht erfragt');
+select i18n.upd_tx('de_DE', 'last confirmed', 'zuletzt überprüft');
+
+\set ON_ERROR_STOP 1
