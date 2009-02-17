4f40f8b60e38864072f5957c040601e1b6481fb8
Karsten Hilbert <karsten.hilbert@gmx.net>
2009-02-17 10:49:28 +0000

    - add Wells and GCS

---
+++ gnumed/gnumed/server/sql/v9-v10/dynamic/v10-clin-keyword_expansion-data.sql
@@ -0,0 +1,101 @@
+-- ==============================================================
+-- GNUmed database schema change script
+--
+-- License: GPL
+-- Author: Karsten Hilbert
+-- 
+-- ==============================================================
+-- $Id: v10-clin-keyword_expansion-data.sql,v 1.1 2009-02-17 10:49:28 ncq Exp $
+-- $Revision: 1.1 $
+
+-- --------------------------------------------------------------
+\set ON_ERROR_STOP 1
+--set default_transaction_read_only to off;
+
+-- --------------------------------------------------------------
+delete from clin.keyword_expansion where keyword = 'score-wells';
+
+insert into clin.keyword_expansion (
+	fk_staff,
+	keyword,
+	expansion
+) values (
+	null,
+	'score-wells',
+'Wells-Score: Wahrscheinlichkeit akute tiefe Beinvenenthrombose
+--------------------------------------------------------------
++1: maligne Erkrankung vorhanden oder Therapie in den letzten 6 Monaten;
++1: Lähmung/kürzliche Immobilisation der Beine;
++1: Bettruhe > 3d oder große OP < 12 Wo;
++1: Schmerz/Verhärtung entlang der tiefen Venen;
++1: Schwellung des gesamten Beins;
++1: US-Schwellung > 3cm (10cm unterhalb des Knie):
+    li: cm, re: cm;
++1: eindrückbares Ödem des betroffenen Beines;
++1: dilatierte oberflächliche Venen (nicht Varizen);
+-2: andere Diagnose (...) wahrscheinlicher;
+
+Summe:
+ < 1  niedrige Wahrscheinlichkeit
+ 1-2  mittlere Wahrscheinlichkeit
+ > 3  hohe Wahrscheinlichkeit
+--------------------------------------------------------------
+D-Dimer-Schnelltest:
+D-Dimer quantitativ:
+
+Vd. US-Thrombose links/rechts
+'
+);
+
+-- --------------------------------------------------------------
+delete from clin.keyword_expansion where keyword = 'score-gcs';
+
+insert into clin.keyword_expansion (
+	fk_staff,
+	keyword,
+	expansion
+) values (
+	null,
+	'score-gcs',
+'Glasgow Coma Scale
+------------------
+- ab 3 Jahre
+- bei normalerweise orientierten Personen
+
+   Augen öffnen
+   ------------
+4: spontan
+3: auf Aufforderung
+2: auf Schmerzreiz
+1: keine Reaktion
+
+   Verbale Kommunikation
+   ---------------------
+5: konversationsfähig, orientiert
+4: konversationsfähig, desorientiert
+3: unzusammenhängende Worte
+2: unverständliche Laute
+1: keine verbale Reaktion
+
+   Motorische Reaktion
+   -------------------
+6: befolgt Aufforderungen
+5: gezielte Schmerzabwehr
+4: ungezielte Schmerzabwehr
+3: auf Schmerzreiz abnormale Beugeabwehr
+2: auf Schmerzreiz Strecksynergismen
+1: keine Reaktion auf Schmerzreiz
+
+14 - 15 leicht, 9 - 13 mittel, 3 - 8 schwer
+'
+);
+
+-- --------------------------------------------------------------
+select gm.log_script_insertion('$RCSfile: v10-clin-keyword_expansion-data.sql,v $', '$Revision: 1.1 $');
+
+-- ==============================================================
+-- $Log: v10-clin-keyword_expansion-data.sql,v $
+-- Revision 1.1  2009-02-17 10:49:28  ncq
+-- - add Wells and GCS
+--
+--
