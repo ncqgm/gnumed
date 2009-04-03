518890d42481fb4ddabb8b3d63f2e240f5369d08
Karsten Hilbert <karsten.hilbert@gmx.net>
2009-04-03 09:58:12 +0000

    - cleanup

--- gnumed/gnumed/server/sql/v10-v11/static/v11-bill-bill_item.sql
+++ gnumed/gnumed/server/sql/v10-v11/static/v11-bill-bill_item.sql
@@ -5,8 +5,8 @@
 -- Author: Karsten Hilbert
 -- 
 -- ==============================================================
--- $Id: v11-bill-bill_item.sql,v 1.2 2009-03-16 15:13:38 ncq Exp $
--- $Revision: 1.2 $
+-- $Id: v11-bill-bill_item.sql,v 1.3 2009-04-03 09:58:12 ncq Exp $
+-- $Revision: 1.3 $
 
 -- --------------------------------------------------------------
 \set ON_ERROR_STOP 1
@@ -16,12 +16,12 @@ create table bill.bill_item (
 	pk serial primary key,
 	fk_provider integer
 		references dem.staff(pk)
-		on update cascade
-		on delete restrict,
+			on update cascade
+			on delete restrict,
 	fk_encounter integer
 		references clin.encounter(pk)
-		on update cascade
-		on delete restrict,
+			on update cascade
+			on delete restrict,
 	date_to_bill date,
 	code text,
 	system text,
@@ -34,11 +34,14 @@ create table bill.bill_item (
 ) inherits (audit.audit_fields);
 
 -- --------------------------------------------------------------
-select gm.log_script_insertion('$RCSfile: v11-bill-bill_item.sql,v $', '$Revision: 1.2 $');
+select gm.log_script_insertion('$RCSfile: v11-bill-bill_item.sql,v $', '$Revision: 1.3 $');
 
 -- ==============================================================
 -- $Log: v11-bill-bill_item.sql,v $
--- Revision 1.2  2009-03-16 15:13:38  ncq
+-- Revision 1.3  2009-04-03 09:58:12  ncq
+-- - cleanup
+--
+-- Revision 1.2  2009/03/16 15:13:38  ncq
 -- - columns adjusted
 --
 -- Revision 1.1  2009/03/10 14:29:05  ncq
