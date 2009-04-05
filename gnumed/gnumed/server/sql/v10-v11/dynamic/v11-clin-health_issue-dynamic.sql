56ff604d83fa4ad1ac48864e49d53f2f26319eb7
Karsten Hilbert <karsten.hilbert@gmx.net>
2009-04-05 17:48:20 +0000

    - new

---
+++ gnumed/gnumed/server/sql/v10-v11/dynamic/v11-clin-health_issue-dynamic.sql
@@ -0,0 +1,40 @@
+-- ==============================================================
+-- GNUmed database schema change script
+--
+-- License: GPL
+-- Author: Karsten Hilbert
+-- 
+-- ==============================================================
+-- $Id: v11-clin-health_issue-dynamic.sql,v 1.1 2009-04-05 17:48:20 ncq Exp $
+-- $Revision: 1.1 $
+
+-- --------------------------------------------------------------
+\set ON_ERROR_STOP 1
+
+-- --------------------------------------------------------------
+comment on column clin.health_issue.grouping is
+'This can be used to entirely arbitrarily group health
+ issues felt to belong to each other.';
+
+
+alter table clin.health_issue
+	alter column grouping
+		set default null
+;
+
+
+alter table clin.health_issue
+	add constraint sane_grouping check (
+		gm.is_null_or_non_empty_string(grouping)
+	)
+;
+
+-- --------------------------------------------------------------
+select gm.log_script_insertion('$RCSfile: v11-clin-health_issue-dynamic.sql,v $', '$Revision: 1.1 $');
+
+-- ==============================================================
+-- $Log: v11-clin-health_issue-dynamic.sql,v $
+-- Revision 1.1  2009-04-05 17:48:20  ncq
+-- - new
+--
+--
