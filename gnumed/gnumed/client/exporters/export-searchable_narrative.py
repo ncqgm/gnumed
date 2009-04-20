175df76a66bb7f8bca2ba756e5e51052d5b3d7ea
Karsten Hilbert <karsten.hilbert@gmx.net>
2009-04-20 11:38:41 +0000

    - adjust to current code

--- gnumed/gnumed/client/exporters/export-searchable_narrative.py
+++ gnumed/gnumed/client/exporters/export-searchable_narrative.py
@@ -1,17 +1,19 @@
 # GNUmed vaccination metadata exporter
 #============================================================
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/exporters/export-searchable_narrative.py,v $
-# $Id: export-searchable_narrative.py,v 1.1 2006-09-20 12:31:08 ncq Exp $
-__version__ = "$Revision: 1.1 $"
+# $Id: export-searchable_narrative.py,v 1.2 2009-04-20 11:38:41 ncq Exp $
+__version__ = "$Revision: 1.2 $"
 __author__ = "Karsten Hilbert"
 __license__ = 'GPL'
 
 import sys, codecs
 
-import gmPG2
+sys.path.insert(0, '../../')
+
+from Gnumed.pycommon import gmPG2
 #============================================================
 
-cmd = """
+cmd = u"""
 select
 	soap_cat,
 	narrative,
@@ -28,20 +30,23 @@ order by
 	, src_table
 """
 
-conn = gmPG2.get_connection(encoding='iso-8859-15')
+conn = gmPG2.get_connection(encoding='utf8')
 
 rows, idx = gmPG2.run_ro_queries(link_obj=conn, queries=[{'cmd': cmd, 'args': {'pat': sys.argv[1]}}])
 
-f = codecs.open('emr-%s-narrative-dump.txt' % sys.argv[1], 'wb', encoding='iso-8859-15', errors='strict')
+f = codecs.open('emr-%s-narrative-dump.txt' % sys.argv[1], 'wb', encoding='utf8', errors='strict')
 
 for row in rows:
-	f.write('%s: %s (%s)\n'.encode('iso-8859-15') % (row['soap_cat'], row['narrative'], row['src_table']))
+	f.write('%s: %s (%s)\n'.encode('utf8') % (row['soap_cat'], row['narrative'], row['src_table']))
 
 f.close()
 
 #============================================================
 # $Log: export-searchable_narrative.py,v $
-# Revision 1.1  2006-09-20 12:31:08  ncq
+# Revision 1.2  2009-04-20 11:38:41  ncq
+# - adjust to current code
+#
+# Revision 1.1  2006/09/20 12:31:08  ncq
 # - export search narrative into file
 #
 #
