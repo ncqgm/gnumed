7cac07c76000b8e820626a004415431a8953bd25
Karsten Hilbert <karsten.hilbert@gmx.net>
2009-05-12 12:10:19 +0000

    - cleanup

--- gnumed/gnumed/server/sql/v9-v10/dynamic/v10-cfg-cfg_str_array.sql
+++ gnumed/gnumed/server/sql/v9-v10/dynamic/v10-cfg-cfg_str_array.sql
@@ -5,12 +5,12 @@
 -- Author: Karsten Hilbert
 -- 
 -- ==============================================================
--- $Id: v10-cfg-cfg_str_array.sql,v 1.2 2009-01-17 23:15:23 ncq Exp $
--- $Revision: 1.2 $
+-- $Id: v10-cfg-cfg_str_array.sql,v 1.3 2009-05-12 12:10:19 ncq Exp $
+-- $Revision: 1.3 $
 
 -- --------------------------------------------------------------
 \set ON_ERROR_STOP 1
-set default_transaction_read_only to off;
+--set default_transaction_read_only to off;
 
 -- --------------------------------------------------------------
 update cfg.cfg_str_array set
@@ -25,11 +25,14 @@ where
 		)
 ;
 -- --------------------------------------------------------------
-select gm.log_script_insertion('$RCSfile: v10-cfg-cfg_str_array.sql,v $', '$Revision: 1.2 $');
+select gm.log_script_insertion('$RCSfile: v10-cfg-cfg_str_array.sql,v $', '$Revision: 1.3 $');
 
 -- ==============================================================
 -- $Log: v10-cfg-cfg_str_array.sql,v $
--- Revision 1.2  2009-01-17 23:15:23  ncq
+-- Revision 1.3  2009-05-12 12:10:19  ncq
+-- - cleanup
+--
+-- Revision 1.2  2009/01/17 23:15:23  ncq
 -- - add waiting list to default workplace
 --
 -- Revision 1.1  2008/11/11 21:15:26  ncq
