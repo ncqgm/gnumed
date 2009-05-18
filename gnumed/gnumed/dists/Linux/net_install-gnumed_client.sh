fa73c66e906d798a0f9e4e3c1718ad3e07e3c0df
Karsten Hilbert <karsten.hilbert@gmx.net>
2009-05-18 10:23:40 +0000

    - bump version

--- gnumed/gnumed/dists/Linux/net_install-gnumed_client.sh
+++ gnumed/gnumed/dists/Linux/net_install-gnumed_client.sh
@@ -7,10 +7,10 @@
 # you will need to know the root password.
 #
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/dists/Linux/Attic/net_install-gnumed_client.sh,v $
-# $Id: net_install-gnumed_client.sh,v 1.10.2.4 2009-05-01 16:03:06 ncq Exp $
+# $Id: net_install-gnumed_client.sh,v 1.10.2.5 2009-05-18 10:21:41 ncq Exp $
 # ===========================================================
 
-VER_LATEST="0.4.4"
+VER_LATEST="0.4.5"
 
 # ===========================================================
 VER_CLI="$1"
@@ -224,7 +224,10 @@ mc -e gm-from-cvs.conf
 
 # ============================================
 # $Log: net_install-gnumed_client.sh,v $
-# Revision 1.10.2.4  2009-05-01 16:03:06  ncq
+# Revision 1.10.2.5  2009-05-18 10:21:41  ncq
+# - bump version
+#
+# Revision 1.10.2.4  2009/05/01 16:03:06  ncq
 # - bump version
 #
 # Revision 1.10.2.3  2009/04/20 12:11:00  ncq
