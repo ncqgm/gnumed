31b2e74c67f8441d4c03f49a0c6f4b055c9ecb76
Karsten Hilbert <karsten.hilbert@gmx.net>
2009-05-24 16:32:59 +0000

    - rename "meta" test type

--- gnumed/gnumed/server/sql/v10-v11/dynamic/v11-clin-v_meta_test_types-dynamic.sql
+++ gnumed/gnumed/server/sql/v10-v11/dynamic/v11-clin-v_meta_test_types-dynamic.sql
@@ -5,8 +5,8 @@
 -- Author: Karsten Hilbert
 -- 
 -- ==============================================================
--- $Id: v11-clin-v_meta_test_types-dynamic.sql,v 1.1 2009-05-22 10:57:50 ncq Exp $
--- $Revision: 1.1 $
+-- $Id: v11-clin-v_meta_test_types-dynamic.sql,v 1.2 2009-05-24 16:32:59 ncq Exp $
+-- $Revision: 1.2 $
 
 -- --------------------------------------------------------------
 \set ON_ERROR_STOP 1
@@ -15,38 +15,43 @@
 -- --------------------------------------------------------------
 \unset ON_ERROR_STOP
 drop view clin.v_test_type_unified cascade;
+drop view clin.v_meta_test_types cascade;
 \set ON_ERROR_STOP 1
 
 create view clin.v_meta_test_types as
 select
-	ttm.pk as pk_test_type_unified,
-	ltt2ut.fk_test_type as pk_test_type,
+	ttm.pk as pk_meta_test_type,
+	ltt2mt.fk_test_type as pk_test_type,
 	ttm.abbrev as abbrev_meta,
 	ttm.name as name_meta,
 	ttm.loinc as loinc_meta,
 	ttm.comment as comment_meta,
-	ltt2ut.pk as pk_lnk_ttype2unified_type
+	ltt2mt.pk as pk_lnk_ttype2meta_type,
+	ttm.xmin as xmin_meta_test_type
 from
-	clin.test_type_unified ttm,
-	clin.lnk_ttype2unified_type ltt2ut
+	clin.meta_test_type ttm,
+	clin.lnk_ttype2unified_type ltt2mt
 where
-	ltt2ut.fk_test_type_unified = ttm.pk
+	ltt2mt.fk_test_type_unified = ttm.pk
 ;
 
 
 comment on view clin.v_meta_test_types is
-'denormalized view of test_type_unified + link table to test_type,
- shows all test types for which a meta type exists';
+'denormalized view of meta_test_type + link table to test_type,
+ shows all meta test types to which a test type is linked';
 
 
 grant select on clin.v_meta_test_types to group "gm-doctors";
 
 -- --------------------------------------------------------------
-select gm.log_script_insertion('$RCSfile: v11-clin-v_meta_test_types-dynamic.sql,v $', '$Revision: 1.1 $');
+select gm.log_script_insertion('$RCSfile: v11-clin-v_meta_test_types-dynamic.sql,v $', '$Revision: 1.2 $');
 
 -- ==============================================================
 -- $Log: v11-clin-v_meta_test_types-dynamic.sql,v $
--- Revision 1.1  2009-05-22 10:57:50  ncq
+-- Revision 1.2  2009-05-24 16:32:59  ncq
+-- - rename "meta" test type
+--
+-- Revision 1.1  2009/05/22 10:57:50  ncq
 -- - new
 --
 --
