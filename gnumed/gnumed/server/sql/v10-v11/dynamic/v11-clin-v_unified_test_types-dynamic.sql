2c2f9c5578021a46aa5637ea7a5083873d3a9afa
Karsten Hilbert <karsten.hilbert@gmx.net>
2009-05-24 16:34:30 +0000

    - add fake flag

--- gnumed/gnumed/server/sql/v10-v11/dynamic/v11-clin-v_unified_test_types-dynamic.sql
+++ gnumed/gnumed/server/sql/v10-v11/dynamic/v11-clin-v_unified_test_types-dynamic.sql
@@ -5,8 +5,8 @@
 -- Author: Karsten Hilbert
 -- 
 -- ==============================================================
--- $Id: v11-clin-v_unified_test_types-dynamic.sql,v 1.1 2009-05-22 10:57:50 ncq Exp $
--- $Revision: 1.1 $
+-- $Id: v11-clin-v_unified_test_types-dynamic.sql,v 1.2 2009-05-24 16:34:30 ncq Exp $
+-- $Revision: 1.2 $
 
 -- --------------------------------------------------------------
 \set ON_ERROR_STOP 1
@@ -39,27 +39,31 @@ select
 	vmtt.comment_meta,
 	-- admin links
 	ctt.fk_test_org as pk_test_org,
-	vmtt.pk_test_type_unified,
-	vmtt.pk_lnk_ttype2unified_type
+	vmtt.pk_meta_test_type,
+	vmtt.pk_lnk_ttype2meta_type,
+	(vmtt.pk_meta_test_type is null) as is_fake_unified_type
 from
 	clin.test_type ctt left outer join clin.v_meta_test_types vmtt on (ctt.pk = vmtt.pk_test_type)
 ;
 
 
 comment on view clin.v_unified_test_types is
-	'provides a view of test types aggregated under their
-	 corresponding unified name if any, if not linked to a
-	 unified test type name the original name is used';
+'provides a "unified" view of test types aggregated under
+ their corresponding unified name if any; if not linked
+ to a meta test type the original name is used';
 
 
 grant select on clin.v_unified_test_types to group "gm-doctors";
 
 -- --------------------------------------------------------------
-select gm.log_script_insertion('$RCSfile: v11-clin-v_unified_test_types-dynamic.sql,v $', '$Revision: 1.1 $');
+select gm.log_script_insertion('$RCSfile: v11-clin-v_unified_test_types-dynamic.sql,v $', '$Revision: 1.2 $');
 
 -- ==============================================================
 -- $Log: v11-clin-v_unified_test_types-dynamic.sql,v $
--- Revision 1.1  2009-05-22 10:57:50  ncq
+-- Revision 1.2  2009-05-24 16:34:30  ncq
+-- - add fake flag
+--
+-- Revision 1.1  2009/05/22 10:57:50  ncq
 -- - new
 --
 --
