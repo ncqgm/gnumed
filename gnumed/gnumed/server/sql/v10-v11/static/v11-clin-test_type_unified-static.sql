2cad555236eaa37e2540431f67ae2b98aa85c9c2
Karsten Hilbert <karsten.hilbert@gmx.net>
2009-05-24 16:35:55 +0000

    - rename table to "meta"

--- gnumed/gnumed/server/sql/v10-v11/static/v11-clin-test_type_unified-static.sql
+++ gnumed/gnumed/server/sql/v10-v11/static/v11-clin-test_type_unified-static.sql
@@ -5,26 +5,33 @@
 -- Author: Karsten Hilbert
 -- 
 -- ==============================================================
--- $Id: v11-clin-test_type_unified-static.sql,v 1.1 2009-05-22 10:59:01 ncq Exp $
--- $Revision: 1.1 $
+-- $Id: v11-clin-test_type_unified-static.sql,v 1.2 2009-05-24 16:35:55 ncq Exp $
+-- $Revision: 1.2 $
 
 -- --------------------------------------------------------------
 \set ON_ERROR_STOP 1
 
 -- --------------------------------------------------------------
 alter table clin.test_type_unified
+	rename to meta_test_type;
+
+
+alter table clin.meta_test_type
 	rename column code to abbrev;
 
 
-alter table clin.test_type_unified
+alter table clin.meta_test_type
 	rename column coding_system to loinc;
 
 -- --------------------------------------------------------------
-select gm.log_script_insertion('$RCSfile: v11-clin-test_type_unified-static.sql,v $', '$Revision: 1.1 $');
+select gm.log_script_insertion('$RCSfile: v11-clin-test_type_unified-static.sql,v $', '$Revision: 1.2 $');
 
 -- ==============================================================
 -- $Log: v11-clin-test_type_unified-static.sql,v $
--- Revision 1.1  2009-05-22 10:59:01  ncq
+-- Revision 1.2  2009-05-24 16:35:55  ncq
+-- - rename table to "meta"
+--
+-- Revision 1.1  2009/05/22 10:59:01  ncq
 -- - new
 --
 --
