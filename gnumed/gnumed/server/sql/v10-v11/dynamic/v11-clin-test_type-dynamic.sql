a3942615e69b15759fe4854cf15c5e74a659cd19
Karsten Hilbert <karsten.hilbert@gmx.net>
2009-05-24 16:31:35 +0000

    - new v_test_types

--- gnumed/gnumed/server/sql/v10-v11/dynamic/v11-clin-test_type-dynamic.sql
+++ gnumed/gnumed/server/sql/v10-v11/dynamic/v11-clin-test_type-dynamic.sql
@@ -5,8 +5,8 @@
 -- Author: Karsten Hilbert
 -- 
 -- ==============================================================
--- $Id: v11-clin-test_type-dynamic.sql,v 1.1 2009-05-22 10:57:49 ncq Exp $
--- $Revision: 1.1 $
+-- $Id: v11-clin-test_type-dynamic.sql,v 1.2 2009-05-24 16:31:35 ncq Exp $
+-- $Revision: 1.2 $
 
 -- --------------------------------------------------------------
 \set ON_ERROR_STOP 1
@@ -27,11 +27,58 @@ alter table clin.test_type
 		drop not null;
 
 -- --------------------------------------------------------------
-select gm.log_script_insertion('$RCSfile: v11-clin-test_type-dynamic.sql,v $', '$Revision: 1.1 $');
+\unset ON_ERROR_STOP
+drop view clin.v_test_types cascade;
+\set ON_ERROR_STOP 1
+
+create view clin.v_test_types as
+select
+	ctt.pk
+		 as pk_test_type,
+	ctt.abbrev,
+	ctt.name,
+	ctt.loinc,
+	ctt.code,
+	ctt.coding_system,
+	ctt.conversion_unit,
+	ctt.comment
+		as comment_type,
+	cto.internal_name
+		as internal_name_org,
+	cto.comment
+		as comment_org,
+	-- admin links
+	ctt.fk_test_org
+		 as pk_test_org,
+	cto.fk_org
+		as pk_org,
+	cto.fk_adm_contact
+		as pk_adm_contact_org,
+	cto.fk_med_contact
+		as pk_med_contact_org,
+	ctt.xmin
+		as xmin_test_type
+from
+	clin.test_type ctt
+		left join clin.test_org cto on (cto.pk = ctt.fk_test_org)
+;
+
+
+comment on view clin.v_test_types is
+'denormalizes test types with test orgs';
+
+
+grant select on clin.v_test_types to group "gm-doctors";
+
+-- --------------------------------------------------------------
+select gm.log_script_insertion('$RCSfile: v11-clin-test_type-dynamic.sql,v $', '$Revision: 1.2 $');
 
 -- ==============================================================
 -- $Log: v11-clin-test_type-dynamic.sql,v $
--- Revision 1.1  2009-05-22 10:57:49  ncq
+-- Revision 1.2  2009-05-24 16:31:35  ncq
+-- - new v_test_types
+--
+-- Revision 1.1  2009/05/22 10:57:49  ncq
 -- - new
 --
 --
