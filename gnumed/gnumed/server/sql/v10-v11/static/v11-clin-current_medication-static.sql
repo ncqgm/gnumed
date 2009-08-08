0b4d8cb90c842cab00cb6328d9a06e0448b751b3
Karsten Hilbert <karsten.hilbert@gmx.net>
2009-08-08 10:45:19 +0000

    - fix drop which was missing a table keyword

--- gnumed/gnumed/server/sql/v10-v11/static/v11-clin-current_medication-static.sql
+++ gnumed/gnumed/server/sql/v10-v11/static/v11-clin-current_medication-static.sql
@@ -5,8 +5,8 @@
 -- Author: karsten.hilbert@gmx.net
 --
 -- ==============================================================
--- $Id: v11-clin-current_medication-static.sql,v 1.4 2009-06-04 16:37:57 ncq Exp $
--- $Revision: 1.4 $
+-- $Id: v11-clin-current_medication-static.sql,v 1.5 2009-08-08 10:45:19 ncq Exp $
+-- $Revision: 1.5 $
 
 -- --------------------------------------------------------------
 \set ON_ERROR_STOP 1
@@ -14,7 +14,7 @@
 
 -- --------------------------------------------------------------
 \unset ON_ERROR_STOP
-drop audit.log_clin_medication;
+drop table audit.log_clin_medication;
 \set ON_ERROR_STOP 1
 
 -- --------------------------------------------------------------
@@ -68,11 +68,14 @@ create table clin.lnk_substance2episode (
 ) inherits (audit.audit_fields);
 
 -- --------------------------------------------------------------
-select gm.log_script_insertion('$RCSfile: v11-clin-current_medication-static.sql,v $', '$Revision: 1.4 $');
+select gm.log_script_insertion('$RCSfile: v11-clin-current_medication-static.sql,v $', '$Revision: 1.5 $');
 
 -- ==============================================================
 -- $Log: v11-clin-current_medication-static.sql,v $
--- Revision 1.4  2009-06-04 16:37:57  ncq
+-- Revision 1.5  2009-08-08 10:45:19  ncq
+-- - fix drop which was missing a table keyword
+--
+-- Revision 1.4  2009/06/04 16:37:57  ncq
 -- - intake-is-approved-of
 --
 -- Revision 1.3  2009/05/12 12:09:41  ncq
