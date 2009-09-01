dd33db0e6030285a2b553441e03d51a103beeee9
Karsten Hilbert <karsten.hilbert@gmx.net>
2009-09-01 22:12:47 +0000

    - new

---
+++ gnumed/gnumed/server/sql/test-data/test_data-Kirk-diagnostic_certainty-dynamic.sql
@@ -0,0 +1,53 @@
+-- =============================================
+-- Project GNUmed
+
+-- James T. Kirk test data: second CRP result for a particular day
+
+-- author: Karsten Hilbert <Karsten.Hilbert@gmx.net>
+-- license: GPL
+--
+-- $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/server/sql/test-data/test_data-Kirk-diagnostic_certainty-dynamic.sql,v $
+-- $Revision: 1.1 $
+-- =============================================
+
+-- force terminate + exit(3) on errors if non-interactive
+\set ON_ERROR_STOP 1
+
+--set default_transaction_read_only to off;
+
+--begin;
+-- =============================================
+update clin.health_issue
+set
+	diagnostic_certainty_classification = 'D'
+where
+	description = 'post appendectomy/peritonitis'
+		and
+	fk_encounter in (select pk from clin.encounter where fk_patient = 12)
+;
+
+
+update clin.health_issue
+set
+	diagnostic_certainty_classification = 'C'
+where
+	description = '9/2000 extraterrestrial infection'
+		and
+	fk_encounter in (select pk from clin.encounter where fk_patient = 12)
+;
+
+-- =============================================
+-- do simple schema revision tracking
+select gm.log_script_insertion('$RCSfile: test_data-Kirk-diagnostic_certainty-dynamic.sql,v $', '$Revision: 1.1 $');
+
+-- comment out the "rollback" if you want to
+-- really store the above patient data
+--rollback;
+--commit;
+
+-- =============================================
+-- $Log: test_data-Kirk-diagnostic_certainty-dynamic.sql,v $
+-- Revision 1.1  2009-09-01 22:11:26  ncq
+-- - new
+--
+--
---
+++ gnumed/gnumed/server/sql/v11-v12/dynamic/v12-clin-v_pat_items.sql
@@ -0,0 +1,60 @@
+-- ==============================================================
+-- GNUmed database schema change script
+--
+-- License: GPL
+-- Author: Karsten Hilbert
+-- 
+-- ==============================================================
+-- $Id: v12-clin-v_pat_items.sql,v 1.1 2009-09-01 22:12:47 ncq Exp $
+-- $Revision: 1.1 $
+
+-- --------------------------------------------------------------
+\set ON_ERROR_STOP 1
+
+-- --------------------------------------------------------------
+\unset ON_ERROR_STOP
+drop view clin.v_pat_items cascade;
+\set ON_ERROR_STOP 1
+
+
+create view clin.v_pat_items as
+select
+	cri.modified_when as modified_when,
+	cri.modified_by as modified_by,
+	cri.clin_when as clin_when,
+	case cri.row_version
+		when 0 then false
+		else true
+	end as is_modified,
+	cenc.fk_patient as pk_patient,
+	cri.pk_item as pk_item,
+	cri.fk_encounter as pk_encounter,
+	cri.fk_episode as pk_episode,
+	cepi.fk_health_issue as pk_health_issue,
+	cri.soap_cat as soap_cat,
+	cri.narrative as narrative,
+	pgc.relname as src_table
+from
+	clin.clin_root_item cri,
+	clin.encounter cenc,
+	clin.episode cepi,
+	pg_class pgc
+where
+	cri.fk_encounter = cenc.pk
+		and
+	cri.fk_episode = cepi.pk
+		and
+	cri.tableoid = pgc.oid
+;
+
+
+grant select on clin.v_pat_items TO GROUP "gm-doctors";
+-- --------------------------------------------------------------
+select gm.log_script_insertion('$RCSfile: v12-clin-v_pat_items.sql,v $', '$Revision: 1.1 $');
+
+-- ==============================================================
+-- $Log: v12-clin-v_pat_items.sql,v $
+-- Revision 1.1  2009-09-01 22:12:47  ncq
+-- - new
+--
+--
