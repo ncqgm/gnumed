eaf2e67e4d80c1e237bed0cdc783f99c70838015
Karsten Hilbert <karsten.hilbert@gmx.net>
2009-11-13 21:10:57 +0000

    - try to improve single location check - improved trigger name and fix NEW reference - add missing grants - journal view

--- gnumed/gnumed/server/sql/v11-v12/dynamic/v12-clin-procedure-dynamic.sql
+++ gnumed/gnumed/server/sql/v11-v12/dynamic/v12-clin-procedure-dynamic.sql
@@ -5,8 +5,8 @@
 -- Author: Karsten Hilbert
 -- 
 -- ==============================================================
--- $Id: v12-clin-procedure-dynamic.sql,v 1.4 2009-09-23 14:47:15 ncq Exp $
--- $Revision: 1.4 $
+-- $Id: v12-clin-procedure-dynamic.sql,v 1.5 2009-11-13 21:10:57 ncq Exp $
+-- $Revision: 1.5 $
 
 -- --------------------------------------------------------------
 --set default_transaction_read_only to off;
@@ -75,11 +75,11 @@ alter table clin.procedure drop constraint single_location_definition cascade;
 
 alter table clin.procedure
 	add constraint single_location_definition
-		check (
-			((fk_hospital_stay is null) and (clin_where is not null))
-				or
-			((fk_hospital_stay is not null) and (clin_where is null))
-		)
+		check ((
+				(fk_hospital_stay is null) and (clin_where is not null)
+			) or (
+				(fk_hospital_stay is not null) and (clin_where is null)
+		))
 ;
 
 
@@ -97,10 +97,10 @@ alter table clin.procedure
 
 -- x-check fk_episode vs fk_hospital_stay
 \unset ON_ERROR_STOP
-drop function clin.trf_sanity_check_episode() cascade;
+drop function clin.trf_sanity_check_procedure_episode() cascade;
 \set ON_ERROR_STOP 1
 
-create function clin.trf_sanity_check_episode()
+create function clin.trf_sanity_check_procedure_episode()
 	returns trigger
 	language 'plpgsql'
 	as '
@@ -119,20 +119,24 @@ BEGIN
 		return NEW;
 	end if;
 
-	raise exception ''[clin.procedure]: INSERT/UPDATE failed: fk_episode (%) does not match fk_episode (%) behind fk_hospital_stay (%)'', NEW.fk_episode, _hospital_stay_episode_pk, fk_hospital_stay;
+	raise exception ''[clin.procedure]: INSERT/UPDATE failed: fk_episode (%) does not match fk_episode (%) behind fk_hospital_stay (%)'', NEW.fk_episode, _hospital_stay_episode_pk, NEW.fk_hospital_stay;
 	return NEW;
 END;';
 
 
-create trigger tr_sanity_check_episode
+create trigger tr_sanity_check_procedure_episode
 	before insert or update on clin.procedure
-		for each row execute procedure clin.trf_sanity_check_episode()
+		for each row execute procedure clin.trf_sanity_check_procedure_episode()
 ;
 
 
 grant select, insert, update, delete on
 	clin.procedure
 TO GROUP "gm-doctors";
+
+grant usage, select, update on
+	clin.procedure_pk_seq
+to group "gm-doctors";
 
 -- --------------------------------------------------------------
 \unset ON_ERROR_STOP
@@ -191,15 +195,63 @@ grant select on
 TO GROUP "gm-doctors";
 
 -- --------------------------------------------------------------
+\unset ON_ERROR_STOP
+drop view clin.v_pat_procedures_journal cascade;
+\set ON_ERROR_STOP 1
 
--- emr journal
+
+create view clin.v_pat_procedures_journal as
+select
+	(select fk_patient from clin.encounter where pk = cpr.fk_encounter)
+		as pk_patient,
+	cpr.modified_when
+		as modified_when,
+	cpr.clin_when
+		as clin_when,
+	coalesce (
+		(select short_alias from dem.staff where db_user = cpr.modified_by),
+		'<' || cpr.modified_by || '>'
+	)
+		as modified_by,
+	cpr.soap_cat
+		as soap_cat,
+	_('Procedure') || ' "' || narrative || '" ('
+		|| coalesce (
+			(select chs.narrative from clin.hospital_stay chs where cpr.fk_hospital_stay = chs.pk),
+			cpr.clin_where
+		) || ')'
+		as narrative,
+	cpr.fk_encounter
+		as pk_encounter,
+	cpr.pk
+		as pk_episode,
+	(select fk_health_issue from clin.episode where pk = cpr.fk_episode)
+		as pk_health_issue,
+	cpr.pk
+		as src_pk,
+	'clin.procedure'::text
+		as src_table,
+	cpr.row_version
+from
+	clin.procedure cpr
+;
+
+
+grant select on clin.v_pat_procedures_journal TO GROUP "gm-doctors";
+
 
 -- --------------------------------------------------------------
-select gm.log_script_insertion('$RCSfile: v12-clin-procedure-dynamic.sql,v $', '$Revision: 1.4 $');
+select gm.log_script_insertion('$RCSfile: v12-clin-procedure-dynamic.sql,v $', '$Revision: 1.5 $');
 
 -- ==============================================================
 -- $Log: v12-clin-procedure-dynamic.sql,v $
--- Revision 1.4  2009-09-23 14:47:15  ncq
+-- Revision 1.5  2009-11-13 21:10:57  ncq
+-- - try to improve single location check
+-- - improved trigger name and fix NEW reference
+-- - add missing grants
+-- - journal view
+--
+-- Revision 1.4  2009/09/23 14:47:15  ncq
 -- - sane-procedure constraint
 --
 -- Revision 1.3  2009/09/17 22:01:58  ncq
