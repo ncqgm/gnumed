9ad130fd3b3b11c6f9da64041e709218564cb132
Karsten Hilbert <karsten.hilbert@gmx.net>
2009-11-29 16:06:51 +0000

    - turn into psql func, and fix wrongly returning trim()ed result in passing

--- gnumed/gnumed/server/sql/v11-v12/dynamic/v12-gm-nullify_empty_string.sql
+++ gnumed/gnumed/server/sql/v11-v12/dynamic/v12-gm-nullify_empty_string.sql
@@ -5,8 +5,8 @@
 -- Author: karsten.hilbert@gmx.net
 --
 -- ==============================================================
--- $Id: v12-gm-nullify_empty_string.sql,v 1.1 2009-08-28 12:48:12 ncq Exp $
--- $Revision: 1.1 $
+-- $Id: v12-gm-nullify_empty_string.sql,v 1.2 2009-11-29 16:06:51 ncq Exp $
+-- $Revision: 1.2 $
 
 -- --------------------------------------------------------------
 \set ON_ERROR_STOP 1
@@ -17,42 +17,34 @@ set check_function_bodies to 1;
 -- --------------------------------------------------------------
 create or replace function gm.nullify_empty_string(text)
 	returns text
-	language sql
-	as 'select nullif(trim($1), '''');';
+	language 'plpgsql'
+	as '
+DECLARE
+	_input alias for $1;
+BEGIN
+	if _input is null then
+		return null;
+	end if;
+
+	if trim(_input) = '''' then
+		return null;
+	end if;
+
+	return _input;
+END;';
 
 
 grant execute on function gm.nullify_empty_string(text) to group "gm-public";
 
 -- --------------------------------------------------------------
---create or replace function gm.trf_nullify_empty_string()
---	returns trigger
---	language 'plpgsql'
---	as '
---DECLARE
---	_col_name name;
---BEGIN
---	_col_name := TG_ARGV[0];
---
---	if NEW._col_name is null then
---		return NEW;
---	end if;
---
---	if trim(NEW._col_name) = '''' then
---		NEW._col_name := null;
---	end if;
---
---	return NEW;
---END;';
---
---
---grant execute on function gm.trf_nullify_empty_string() to group "gm-public";
-
--- --------------------------------------------------------------
-select gm.log_script_insertion('$RCSfile: v12-gm-nullify_empty_string.sql,v $', '$Revision: 1.1 $');
+select gm.log_script_insertion('$RCSfile: v12-gm-nullify_empty_string.sql,v $', '$Revision: 1.2 $');
 
 -- ==============================================================
 -- $Log: v12-gm-nullify_empty_string.sql,v $
--- Revision 1.1  2009-08-28 12:48:12  ncq
+-- Revision 1.2  2009-11-29 16:06:51  ncq
+-- - turn into psql func, and fix wrongly returning trim()ed result in passing
+--
+-- Revision 1.1  2009/08/28 12:48:12  ncq
 -- - add gm.nullify_empty_string()
 --
 --
