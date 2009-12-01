de4f986456a62e9f916f16c363f7f12baad6a01a
Karsten Hilbert <karsten.hilbert@gmx.net>
2009-12-01 21:46:20 +0000

    - remove .mo files, too

--- gnumed/remove_pyc.sh
+++ gnumed/remove_pyc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/remove_pyc.sh,v $
-# $Revision: 1.9 $
+# $Revision: 1.10 $
 
 echo "cleaning out debris"
 find ./ -name '*.pyc' -exec rm -v '{}' ';'
@@ -10,5 +10,6 @@ find ./ -name 'wxg*.wxg~' -exec rm -v '{}' ';'
 find ./ -name '*.log' -exec rm -v '{}' ';'
 find ./ -name '*.tgz' -exec rm -v '{}' ';'
 find ./ -name '*.bz2' -exec rm -v '{}' ';'
+find ./ -name '*-gnumed.mo' -exec rm -v '{}' ';'
 rm -v before-update.diff
 rm -v public-tree.diff
