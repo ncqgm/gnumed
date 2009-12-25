e28725559f087a31f7535a25f9f3c76af20c4522
Karsten Hilbert <karsten.hilbert@gmx.net>
2009-12-25 21:42:52 +0000

    - gm_print* -> gm-print* - no more .sh - proper argument order for shell script

--- gnumed/gnumed/client/pycommon/gmPrinting.py
+++ gnumed/gnumed/client/pycommon/gmPrinting.py
@@ -1,8 +1,8 @@
 """GNUmed printing."""
 # =======================================================================
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/pycommon/gmPrinting.py,v $
-# $Id: gmPrinting.py,v 1.1 2009-12-25 19:04:50 ncq Exp $
-__version__ = "$Revision: 1.1 $"
+# $Id: gmPrinting.py,v 1.2 2009-12-25 21:42:52 ncq Exp $
+__version__ = "$Revision: 1.2 $"
 __author__  = "K.Hilbert <Karsten.Hilbert@gmx.net>"
 __license__ = 'GPL (details at http://www.gnu.org)'
 
@@ -26,16 +26,18 @@ _log.info(__version__)
 # =======================================================================
 def print_file_by_shellscript(filename=None, jobtype=None):
 
-	# 1) find gm_print_doc.sh
-	found, external_cmd = gmShellAPI.detect_external_binary(u'gm_print_doc.sh')
+	_log.debug('printing "%s": [%s]', jobtype, filename)
+
+	# 1) find gm-print_doc
+	found, external_cmd = gmShellAPI.detect_external_binary(u'gm-print_doc')
 	if not found:
-		found, external_cmd = gmShellAPI.detect_external_binary(u'gm_print_doc.bat')
+		found, external_cmd = gmShellAPI.detect_external_binary(u'gm-print_doc.bat')
 	if not found:
-		_log.error('neither of gm_print_doc.sh or gm_print_doc.bat found')
+		_log.error('neither of gm-print_doc or gm-print_doc.bat found')
 		return False
 
 	# 2) call it
-	cmd = u'%s %s %s' % (external_cmd, filename, jobtype)
+	cmd = u'%s %s %s' % (external_cmd, jobtype, filename)
 	success = gmShellAPI.run_command_in_shell (
 		command = cmd,
 		blocking = True
@@ -59,7 +61,12 @@ if __name__ == '__main__':
 
 # =======================================================================
 # $Log: gmPrinting.py,v $
-# Revision 1.1  2009-12-25 19:04:50  ncq
+# Revision 1.2  2009-12-25 21:42:52  ncq
+# - gm_print* -> gm-print*
+# - no more .sh
+# - proper argument order for shell script
+#
+# Revision 1.1  2009/12/25 19:04:50  ncq
 # - new code
 #
 #
