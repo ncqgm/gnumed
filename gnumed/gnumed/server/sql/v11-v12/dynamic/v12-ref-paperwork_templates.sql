6ecf915f296fe89d094acbde978e317e15dd687e
Karsten Hilbert <karsten.hilbert@gmx.net>
2009-12-30 18:48:36 +0000

    - remove the confusing previous LaTeX templates

--- gnumed/gnumed/server/sql/v11-v12/dynamic/v12-ref-paperwork_templates.sql
+++ gnumed/gnumed/server/sql/v11-v12/dynamic/v12-ref-paperwork_templates.sql
@@ -5,8 +5,8 @@
 -- Author: karsten.hilbert@gmx.net
 -- 
 -- ==============================================================
--- $Id: v12-ref-paperwork_templates.sql,v 1.1 2009-12-22 11:52:46 ncq Exp $
--- $Revision: 1.1 $
+-- $Id: v12-ref-paperwork_templates.sql,v 1.2 2009-12-30 18:48:36 ncq Exp $
+-- $Revision: 1.2 $
 
 -- --------------------------------------------------------------
 \set ON_ERROR_STOP 1
@@ -40,11 +40,34 @@ insert into ref.paperwork_templates (
 );
 
 -- --------------------------------------------------------------
-select gm.log_script_insertion('$RCSfile: v12-ref-paperwork_templates.sql,v $', '$Revision: 1.1 $');
+delete from public.form_fields where
+	fk_form = (select pk from ref.paperwork_templates where name_long = 'Standard specialist referral letter for AU');
+
+delete from ref.paperwork_templates where name_long = 'Standard specialist referral letter for AU';
+
+
+
+delete from public.form_fields where
+	fk_form = (select pk from ref.paperwork_templates where name_long = 'Prescription using the standard form of the Pharmaceutical Benefits Scheme');
+
+delete from ref.paperwork_templates where name_long = 'Prescription using the standard form of the Pharmaceutical Benefits Scheme';
+
+
+
+delete from public.form_fields where
+	fk_form = (select pk from ref.paperwork_templates where name_long = 'A proof-of-concept basic request form');
+
+delete from ref.paperwork_templates where name_long = 'A proof-of-concept basic request form';
+
+-- --------------------------------------------------------------
+select gm.log_script_insertion('$RCSfile: v12-ref-paperwork_templates.sql,v $', '$Revision: 1.2 $');
 
 -- ==============================================================
 -- $Log: v12-ref-paperwork_templates.sql,v $
--- Revision 1.1  2009-12-22 11:52:46  ncq
+-- Revision 1.2  2009-12-30 18:48:36  ncq
+-- - remove the confusing previous LaTeX templates
+--
+-- Revision 1.1  2009/12/22 11:52:46  ncq
 -- - medication list template skeleton
 --
 --
