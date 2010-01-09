766c9494c5d83c8018bab05e9648a3de4040d12b
Karsten Hilbert <karsten.hilbert@gmx.net>
2010-01-09 19:54:13 +0000

    - fix it, eventually :-)

--- gnumed/create-tag.sh
+++ gnumed/create-tag.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
 #------------------------------------------------------------------
-# $Id: create-tag.sh,v 1.4 2009-08-24 20:11:27 ncq Exp $
+# $Id: create-tag.sh,v 1.5 2010-01-09 19:54:13 ncq Exp $
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/create-tag.sh,v $
-# $Revision: 1.4 $
+# $Revision: 1.5 $
 # License: GPL
 #------------------------------------------------------------------
 
 TAG=$1
+REGEX="rel-[0-9]-[0-9](-(rc){0,1}[0-9]){0,1}$"
 
-if ! [[ "${TAG}" =~ "rel-[0-9]-[0-9]((-rc[0-9])|(-[0-9]))?\$" ]] ; then
+if ! [[ "${TAG}" =~ ${REGEX} ]] ; then
 	echo ""
 	echo "usage: $0 <tag>"
 	echo ""
+	echo "   should match: ${REGEX}"
+	echo "   you entered : ${TAG}"
+	echo ""
 	echo "   tag syntax convention:"
 	echo ""
-	echo "      rel-X-Y-Z:"
+	echo "      rel-X-Y:"
 	echo "         - root of branch rel-x-y-z-patches for release x.y.z"
 	echo "         - apply to TRUNK"
-	echo "      rel-X-Y-Z-rcN:"
+	echo "      rel-X-Y-rcN:"
 	echo "         - Release Candidate N for release x.y.z.0"
 	echo "         - apply to branch rel-x-y-z-patches"
 	echo "      rel-X-Y-Z-N:"
@@ -29,9 +33,12 @@ if ! [[ "${TAG}" =~ "rel-[0-9]-[0-9]((-rc[0-9])|(-[0-9]))?\$" ]] ; then
 fi
 
 echo ""
-echo "Are you absolutely positively sure you want"
-echo "to tag the CVS tree as \"${TAG}\" ?"
-echo "Note that you must have checked in all changes."
+echo "Are you absolutely positively sure you"
+echo "want to tag the CVS tree as"
+echo ""
+echo "   \"${TAG}\" ?"
+echo ""
+echo "(you must have checked in all changes)"
 echo ""
 read -e -p "Tag CVS tree ? [yes/no]: "
 
@@ -42,14 +49,18 @@ if test "${REPLY}" != "yes" ; then
 	exit 0
 fi
 
-read -p "Hit [ENTER] to start tagging ..."
+echo ""
+read -p "Hit [ENTER] to *actually* start tagging ..."
 echo ""
 echo "Tagging CVS tree as \"${TAG}\" ..."
 cvs tag -c ${TAG}
 
 #------------------------------------------------------------------
 # $Log: create-tag.sh,v $
-# Revision 1.4  2009-08-24 20:11:27  ncq
+# Revision 1.5  2010-01-09 19:54:13  ncq
+# - fix it, eventually :-)
+#
+# Revision 1.4  2009/08/24 20:11:27  ncq
 # - bump db version
 # - fix tag creation
 # - provider inbox:
