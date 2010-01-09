3197e0400d9264ef67bb531c8f512e2b204f8c07
Karsten Hilbert <karsten.hilbert@gmx.net>
2010-01-09 19:34:54 +0000

    - bump version

--- gnumed/gnumed/client/wxpython/gnumed.py
+++ gnumed/gnumed/client/wxpython/gnumed.py
@@ -48,8 +48,8 @@ care of all the pre- and post-GUI runtime environment setup.
 """
 #==========================================================
 # $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/gnumed/client/wxpython/gnumed.py,v $
-# $Id: gnumed.py,v 1.165 2010-01-01 21:23:15 ncq Exp $
-__version__ = "$Revision: 1.165 $"
+# $Id: gnumed.py,v 1.166 2010-01-09 19:34:54 ncq Exp $
+__version__ = "$Revision: 1.166 $"
 __author__  = "H. Herb <hherb@gnumed.net>, K. Hilbert <Karsten.Hilbert@gmx.net>, I. Haywood <i.haywood@ugrad.unimelb.edu.au>"
 __license__ = "GPL (details at http://www.gnu.org)"
 
@@ -78,7 +78,7 @@ against. Please run GNUmed as a non-root user.
 	sys.exit(1)
 
 #----------------------------------------------------------
-current_client_version = u'0.6.rc3'
+current_client_version = u'0.6.rc4'
 #current_client_version = u'CVS HEAD'
 current_client_branch = u'0.6'
 #current_client_branch = u'CVS HEAD'
@@ -508,7 +508,10 @@ shutdown_logging()
 
 #==========================================================
 # $Log: gnumed.py,v $
-# Revision 1.165  2010-01-01 21:23:15  ncq
+# Revision 1.166  2010-01-09 19:34:54  ncq
+# - bump version
+#
+# Revision 1.165  2010/01/01 21:23:15  ncq
 # - improve checking of import link
 # - better logging
 #
