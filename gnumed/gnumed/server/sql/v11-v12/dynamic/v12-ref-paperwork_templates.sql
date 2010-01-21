df1eefe29a02ad9ba98458946fab4b24c9923a9f
Karsten Hilbert <karsten.hilbert@gmx.net>
2010-01-21 08:49:11 +0000

    - add referral letter template

--- gnumed/gnumed/server/sql/v11-v12/dynamic/v12-ref-paperwork_templates.sql
+++ gnumed/gnumed/server/sql/v11-v12/dynamic/v12-ref-paperwork_templates.sql
@@ -5,8 +5,8 @@
 -- Author: karsten.hilbert@gmx.net
 -- 
 -- ==============================================================
--- $Id: v12-ref-paperwork_templates.sql,v 1.2 2009-12-30 18:48:36 ncq Exp $
--- $Revision: 1.2 $
+-- $Id: v12-ref-paperwork_templates.sql,v 1.3 2010-01-21 08:49:11 ncq Exp $
+-- $Revision: 1.3 $
 
 -- --------------------------------------------------------------
 \set ON_ERROR_STOP 1
@@ -40,6 +40,36 @@ insert into ref.paperwork_templates (
 );
 
 -- --------------------------------------------------------------
+-- example referral template
+\unset ON_ERROR_STOP
+insert into ref.form_types (name) values (i18n.i18n('referral letter'));
+\set ON_ERROR_STOP 1
+
+select i18n.upd_tx('de_DE', 'referral letter', 'Überweisungsbrief');
+
+delete from ref.paperwork_templates where name_long = 'Referral letter (GNUmed default) [Dr.Rogerio Luz]';
+
+insert into ref.paperwork_templates (
+	fk_template_type,
+	name_short,
+	name_long,
+	external_version,
+	engine,
+	filename,
+	data
+) values (
+	(select pk from ref.form_types where name = 'referral letter'),
+	'Referral letter (GNUmed)',
+	'Referral letter (GNUmed default) [Dr.Rogerio Luz]',
+	'1.0',
+	'L',
+	'referral-letter.tex',
+	'real template missing'::bytea
+);
+
+-- --------------------------------------------------------------
+-- some cleanup as not to confuse users
+
 delete from public.form_fields where
 	fk_form = (select pk from ref.paperwork_templates where name_long = 'Standard specialist referral letter for AU');
 
@@ -60,11 +90,14 @@ delete from public.form_fields where
 delete from ref.paperwork_templates where name_long = 'A proof-of-concept basic request form';
 
 -- --------------------------------------------------------------
-select gm.log_script_insertion('$RCSfile: v12-ref-paperwork_templates.sql,v $', '$Revision: 1.2 $');
+select gm.log_script_insertion('$RCSfile: v12-ref-paperwork_templates.sql,v $', '$Revision: 1.3 $');
 
 -- ==============================================================
 -- $Log: v12-ref-paperwork_templates.sql,v $
--- Revision 1.2  2009-12-30 18:48:36  ncq
+-- Revision 1.3  2010-01-21 08:49:11  ncq
+-- - add referral letter template
+--
+-- Revision 1.2  2009/12/30 18:48:36  ncq
 -- - remove the confusing previous LaTeX templates
 --
 -- Revision 1.1  2009/12/22 11:52:46  ncq
