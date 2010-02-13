8b26773da9a45f83d31ffd89a23f1c57125d426b
Karsten Hilbert <karsten.hilbert@gmx.net>
2010-02-13 20:38:07 +0100

    Gitify tree handling scripts.

--- gnumed/update_tree.sh
+++ gnumed/update_tree.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# $Source: /home/ncq/Projekte/cvs2git/vcs-mirror/gnumed/update_tree.sh,v $
-# $Revision: 1.9 $
-
 ./remove_pyc.sh
 
-export CVS_RSH="ssh"
-echo "diffing against CVS just to be sure"
-cvs -z9 diff | tee before-update.diff
-echo "updating python client source from CVS"
-cvs -z9 update -dP | tee update.log
+echo "updating python client source from VCS"
+git pull
+
+#export CVS_RSH="ssh"
+#echo "diffing against CVS just to be sure"
+#cvs -z9 diff | tee before-update.diff
+#echo "updating python client source from VCS"
+#cvs -z9 update -dP | tee update.log
+
