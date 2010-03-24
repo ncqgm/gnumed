6bfe9c8eb40132b9d214d150e6b6e1ff4750489f
Karsten Hilbert <karsten.hilbert@gmx.net>
2010-03-24 15:11:49 +0100

    Cleanup by Basti.

--- gnumed/gnumed/server/bootstrap/bootstrap-latest.bat
+++ gnumed/gnumed/server/bootstrap/bootstrap-latest.bat
@@ -29,8 +29,8 @@ echo ===========================================================
 
 
 
-echo 1) Dropping old baseline gnumed_v2 database if there is any.
-dropdb -U gm-dbo -i gnumed_v2
+rem echo 1) Dropping old baseline gnumed_v2 database if there is any.
+rem dropdb -U gm-dbo -i gnumed_v2
 del %LOG%
 
 echo ==========================
@@ -48,49 +48,54 @@ set GM_CORE_DB=
 REM v2 -> v3
 COLOR 0A
 call upgrade-db.bat 2 3
-dropdb -U gm-dbo gnumed_v2
 
 REM v3 -> v4
 COLOR 0F
 call upgrade-db.bat 3 4
-dropdb -U gm-dbo gnumed_v3
 
 REM v4 -> v5
 COLOR 07
 call upgrade-db.bat 4 5
-dropdb -U gm-dbo gnumed_v4
 
 REM v5 -> v6
 COLOR F9
 call upgrade-db.bat 5 6
-dropdb -U gm-dbo gnumed_v5
 
 REM v6 -> v7
 COLOR 0E
 call upgrade-db.bat 6 7
-dropdb -U gm-dbo gnumed_v6
 
 REM v7 -> v8
 COLOR 0A
 call upgrade-db.bat 7 8
-dropdb -U gm-dbo gnumed_v7
 
 REM v8 -> v9
 COLOR 0F
 call upgrade-db.bat 8 9
-dropdb -U gm-dbo gnumed_v8
 
 REM v9 -> v10
 COLOR 07
 call upgrade-db.bat 9 10
-dropdb -U gm-dbo gnumed_v9
 
 REM v10 -> v11
 COLOR F9
 call upgrade-db.bat 10 11
-dropdb -U gm-dbo gnumed_v10
 
 REM v11 -> v12
 COLOR 0E
 call upgrade-db.bat 11 12
+
+REM v12 -> v13
+COLOR 0A
+call upgrade-db.bat 12 13
+
+dropdb -U gm-dbo gnumed_v2
+dropdb -U gm-dbo gnumed_v3
+dropdb -U gm-dbo gnumed_v4
+dropdb -U gm-dbo gnumed_v5
+dropdb -U gm-dbo gnumed_v6
+dropdb -U gm-dbo gnumed_v7
+dropdb -U gm-dbo gnumed_v8
+dropdb -U gm-dbo gnumed_v9
+dropdb -U gm-dbo gnumed_v10
 dropdb -U gm-dbo gnumed_v11
