739e0a06d49d52ea8a6271ed1be78275b7319201
Karsten Hilbert <karsten.hilbert@gmx.net>
2010-04-17 23:38:00 +0200

    Consolidate vaccines and vaccination indications.
    	- create ref.branded_drug entries from previous vaccines
    	- create per-indication fake generic vaccine
    	- link indication to per-indication fake generic vaccine
    	- adjust plausibility check on vaccines
    	- add ATCs to vaccines
    	- add relevant ATCs to indications
    	- add a few more indications
    	- add vaccines view

--- gnumed/gnumed/server/sql/v13-v14/dynamic/v14-clin-vaccine-dynamic.sql
+++ gnumed/gnumed/server/sql/v13-v14/dynamic/v14-clin-vaccine-dynamic.sql
@@ -66,7 +66,85 @@ alter table clin.vaccine
 		);
 
 -- --------------------------------------------------------------
--- improve ATC codes
+-- generic mono-valent vaccines
+create or replace function gm.create_generic_monovalent_vaccines()
+	returns boolean
+	language plpgsql
+	as '
+DECLARE
+	_row record;
+	_generic_name text;
+	_pk_brand integer;
+	_pk_vaccine integer;
+BEGIN
+	for _row in select * from clin.vacc_indication loop
+
+		_generic_name := _row.description || '' (generic vaccine)'';
+		raise notice ''re-creating [%]'', _generic_name;
+
+		-- retrieve or create ref.branded_drug entry for indication
+		select pk into _pk_brand from ref.branded_drug
+		where
+			is_fake is true
+				and
+			description = _generic_name;
+
+		if FOUND is false then
+			insert into ref.branded_drug (
+				description,
+				preparation,
+				is_fake,
+				atc_code
+			) values (
+				_generic_name,
+				''vaccine'',		-- this is rather arbitrary
+				True,
+				coalesce(_row.atcs_mono_indication[1], ''J07'')
+			)
+			returning pk
+			into _pk_brand;
+		end if;
+
+		-- retrieve or create clin.vaccine entry for generic brand
+		select pk into _pk_vaccine from clin.vaccine
+		where fk_brand = _pk_brand;
+
+		if FOUND is false then
+			insert into clin.vaccine (
+				id_route,
+				is_live,
+				fk_brand
+			) values (
+				(select id from clin.vacc_route where abbreviation = ''i.m.''),
+				false,
+				_pk_brand
+			)
+			returning pk
+			into _pk_vaccine;
+		end if;
+
+		-- link indication to vaccine
+		delete from clin.lnk_vaccine2inds
+		where
+			fk_vaccine = _pk_vaccine;
+
+		insert into clin.lnk_vaccine2inds (
+			fk_vaccine,
+			fk_indication
+		) values (
+			_pk_vaccine,
+			_row.id
+		);
+
+	end loop;
+
+	return true;
+END;';
+
+select gm.create_generic_monovalent_vaccines();
+
+-- --------------------------------------------------------------
+-- improve ATC codes of pre-existing vaccines
 update ref.branded_drug set
 	atc_code = 'J07AM01'
 where
@@ -258,7 +336,57 @@ where
 	description = 'Tetasorbat (SFCMS) (Tetanus)';
 
 -- --------------------------------------------------------------
---select i18n.upd_tx('de_DE', 'visual progress note', 'Bildnotiz');
+\unset ON_ERROR_STOP
+drop view clin.v_vaccines cascade;
+\set ON_ERROR_STOP 1
+
+create view clin.v_vaccines as
+
+	select
+		clv.pk
+			as pk_vaccine,
+
+		rbd.description
+			as vaccine,
+		rbd.preparation
+			as preparation,
+		rbd.atc_code
+			as atc_code,
+		rbd.is_fake
+			as is_fake_vaccine,
+
+		cvr.abbreviation
+			as route_abbreviation,
+		cvr.description
+			as route_description,
+
+		clv.is_live,
+		clv.min_age,
+		clv.max_age,
+		clv.comment,
+
+		rbd.external_code,
+		rbd.external_code_type,
+
+		clv.id_route
+			as pk_route,
+		clv.fk_brand
+			as pk_brand,
+
+		rbd.fk_data_source
+			as pk_data_source
+
+	from
+		clin.vaccine clv
+			join ref.branded_drug rbd on (clv.fk_brand = rbd.pk)
+				join clin.vacc_route cvr on (clv.id_route = cvr.id)
+
+;
+
+comment on view clin.v_vaccines is
+	'A list of vaccines.';
+
+grant select on clin.v_vaccines to group "gm-public";
 
 -- --------------------------------------------------------------
 select gm.log_script_insertion('$RCSfile: v14-clin-vaccine-dynamic.sql,v $', '$Revision: 1.3 $');
