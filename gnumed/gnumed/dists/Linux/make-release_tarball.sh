28149e10397e597b87a19afab217e905dbb6e6b2
Karsten Hilbert <karsten.hilbert@gmx.net>
2010-04-20 12:42:13 +0200

    Bump to 0.7.1/v13.1.

--- gnumed/gnumed/dists/Linux/make-release_tarball.sh
+++ gnumed/gnumed/dists/Linux/make-release_tarball.sh
@@ -3,10 +3,10 @@
 #====================================================
 # license: GPL
 #====================================================
-CLIENTREV="0.7.0"
+CLIENTREV="0.7.1"
 CLIENTARCH="gnumed-client.$CLIENTREV.tgz"
 
-SRVREV="13.0"
+SRVREV="13.1"
 SRVARCH="gnumed-server.$SRVREV.tgz"
 
 FILES_REMOVE=\
