d695f41d23044a6c7f074abf17732067950bb635
Sebastian Hilbert <sebastian.hilbert@gmx.net>
2010-07-17 08:11:54 +0200

    - updated pyjamas example

--- gnumed/gnumed/client/ProxiedWeb/jsonrpc/JSONRPCExample.py
+++ gnumed/gnumed/client/ProxiedWeb/jsonrpc/JSONRPCExample.py
@@ -8,26 +8,30 @@ from pyjamas.ui.HTML import HTML
 from pyjamas.ui.VerticalPanel import VerticalPanel
 from pyjamas.ui.HorizontalPanel import HorizontalPanel
 from pyjamas.ui.ListBox import ListBox
+from pyjamas.ui.Grid import Grid
 from pyjamas.JSONService import JSONProxy
+
+import jsonobjproxy # just to get it in for pyjs
 
 class JSONRPCExample:
     def onModuleLoad(self):
         self.TEXT_WAITING = "Waiting for response..."
         self.TEXT_ERROR = "Server Error"
         self.METHOD_ECHO = "Echo"
-        self.METHOD_REVERSE = "get schema"
-        self.METHOD_UPPERCASE = "get doc types"
+        self.METHOD_DOCTYPES = "get doc types"
+        self.METHOD_UPPERCASE = "get schema"
         self.METHOD_LOWERCASE = "doSomething"
-        self.METHOD_NONEXISTANT = "Non existant"
-        self.methods = [self.METHOD_ECHO, self.METHOD_REVERSE, 
+        self.METHOD_GETDOCS = "get documents"
+        self.methods = [self.METHOD_ECHO, self.METHOD_DOCTYPES, 
                         self.METHOD_UPPERCASE, self.METHOD_LOWERCASE, 
-                        self.METHOD_NONEXISTANT]
+                        self.METHOD_GETDOCS]
 
         self.remote_py = EchoServicePython()
 
         self.status=Label()
         self.username = TextBox(Text="any-doc")
         self.password = TextBox()
+        self.dockey = TextBox(Text="12")
         
         self.method_list = ListBox()
         self.method_list.setName("hello")
@@ -61,6 +65,8 @@ class JSONRPCExample:
         panel.add(HTML(info))
         panel.add(self.username)
         panel.add(self.password)
+        panel.add(HTML("Document Key:"))
+        panel.add(self.dockey)
         panel.add(method_panel)
         panel.add(buttons)
         panel.add(self.status)
@@ -79,15 +85,30 @@ class JSONRPCExample:
         else:
             if method == self.METHOD_ECHO:
                 id = self.remote_py.echo("Hello", self)
-            elif method == self.METHOD_REVERSE:
+            elif method == self.METHOD_DOCTYPES:
                 id = self.remote_py.get_doc_types(self)
             elif method == self.METHOD_UPPERCASE:
                 id = self.remote_py.get_schema_version(self)
             elif method == self.METHOD_LOWERCASE:
                 id = self.remote_py.doSomething(self)
+            elif method == self.METHOD_GETDOCS:
+                key = int(self.dockey.getText()) # TODO: check it!
+                id = self.remote_py.get_documents(key, self)
 
     def onRemoteResponse(self, response, request_info):
-        self.status.setText(str(response))
+        if request_info.method == 'get_documents':
+            grid = Grid()
+            grid.resize(len(response)+1, 8)
+            grid.setHTML(0, 0, "Comment")
+            grid.setHTML(0, 1, "Episode")
+            grid.setHTML(0, 2, "When")
+            for (row, item) in enumerate(response):
+                grid.setHTML(row+1, 0, item.comment)
+                grid.setHTML(row+1, 1, item.episode)
+                grid.setHTML(row+1, 2, str(item.clin_when))
+            RootPanel().add(grid)
+        else:
+            self.status.setText(str(response))
 
     def onRemoteError(self, code, errobj, request_info):
         # onRemoteError gets the HTTP error code or 0 and
@@ -113,6 +134,7 @@ class EchoServicePython(JSONProxy):
                 ["login",
                 "echo", "get_doc_types",
                 "get_schema_version",
+                "get_documents",
                 "doSomething"])
 
 if __name__ == '__main__':
