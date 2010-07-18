8bec65d841e880dda41138f0a68adea348768c0e
Karsten Hilbert <karsten.hilbert@gmx.net>
2010-07-18 22:58:32 +0200

    Fix faulty quoting on INITCOND of array_agg.

--- gnumed/gnumed/server/sql/v13-v14/dynamic/v14-array_agg-dynamic.sql
+++ gnumed/gnumed/server/sql/v13-v14/dynamic/v14-array_agg-dynamic.sql
@@ -17,7 +17,7 @@
 CREATE AGGREGATE array_agg(anyelement) (
 	SFUNC = array_append,
 	STYPE = anyarray,
-	INITCOND = ’{}’
+	INITCOND = '{}'
 );
 
 -- --------------------------------------------------------------
