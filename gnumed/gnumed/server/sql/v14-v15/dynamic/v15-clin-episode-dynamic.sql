93fb7dcb94c00de1b9c903b7ca9205622540fe2d
Karsten Hilbert <karsten.hilbert@gmx.net>
2010-09-11 15:29:40 +0200

    Add status field to issue and episode.
    	- clin.health_issue.status
    	- clin.episode.status
    	- propagate into views

---
+++ gnumed/gnumed/server/sql/v14-v15/dynamic/v15-clin-episode-dynamic.sql
@@ -0,0 +1,22 @@
+-- ==============================================================
+-- GNUmed database schema change script
+--
+-- License: GPL
+-- Author: karsten.hilbert@gmx.net
+-- 
+-- ==============================================================
+\set ON_ERROR_STOP 1
+
+-- --------------------------------------------------------------
+comment on column clin.episode.status is
+'Used for tracking the status of this episode.';
+
+
+alter table clin.health_issue
+	add constraint episode_sane_status check (
+		gm.is_null_or_blank_string(status)
+	)
+;
+
+-- --------------------------------------------------------------
+select gm.log_script_insertion('v15-clin-episode-dynamic.sql', 'Revision: 1.1');
---
+++ gnumed/gnumed/server/sql/v14-v15/dynamic/v15-clin-health_issue-dynamic.sql
@@ -0,0 +1,22 @@
+-- ==============================================================
+-- GNUmed database schema change script
+--
+-- License: GPL
+-- Author: karsten.hilbert@gmx.net
+-- 
+-- ==============================================================
+\set ON_ERROR_STOP 1
+
+-- --------------------------------------------------------------
+comment on column clin.health_issue.status is
+'Used for tracking the status of this health issue.';
+
+
+alter table clin.health_issue
+	add constraint health_issue_sane_status check (
+		gm.is_null_or_blank_string(status)
+	)
+;
+
+-- --------------------------------------------------------------
+select gm.log_script_insertion('v15-clin-health_issue-dynamic.sql', 'Revision: 1.1');
