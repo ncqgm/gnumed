49fbe7d151ea06f650f971ae2742da4d1f865ced
Karsten Hilbert <karsten.hilbert@gmx.net>
2011-01-01 18:12:10 +0100

    Implement picking date from calendar upon <F4>.

--- gnumed/gnumed/client/wxpython/gmDateTimeInput.py
+++ gnumed/gnumed/client/wxpython/gmDateTimeInput.py
@@ -12,10 +12,14 @@ __version__ = "$Revision: 1.66 $"
 __author__  = "K. Hilbert <Karsten.Hilbert@gmx.net>"
 __licence__ = "GPL (details at http://www.gnu.org)"
 
+# standard libary
 import re, string, sys, time, datetime as pyDT, logging
 
 
-import mx.DateTime as mxDT, wx
+# 3rd party
+import mx.DateTime as mxDT
+import wx
+import wx.calendar
 
 
 # GNUmed specific
@@ -25,6 +29,55 @@ from Gnumed.pycommon import gmMatchProvider, gmDateTime
 from Gnumed.wxpython import gmPhraseWheel, gmGuiHelpers
 
 _log = logging.getLogger('gm.ui')
+
+#============================================================
+class cCalendarDatePickerDlg(wx.Dialog):
+
+	def __init__(self, parent):
+
+		wx.Dialog.__init__(self, parent, title = _('Pick a date ...'))
+		panel = wx.Panel(self, -1)
+
+		sizer = wx.BoxSizer(wx.VERTICAL)
+		panel.SetSizer(sizer)
+
+		cal = wx.calendar.CalendarCtrl(panel)
+
+		if sys.platform != 'win32':
+			# gtk truncates the year - this fixes it
+			w, h = cal.Size
+			cal.Size = (w+25, h)
+			cal.MinSize = cal.Size
+
+		sizer.Add(cal, 0)
+
+		button_sizer = wx.BoxSizer(wx.HORIZONTAL)
+		button_sizer.Add((0, 0), 1)
+		btn_ok = wx.Button(panel, wx.ID_OK)
+		btn_ok.SetDefault()
+		button_sizer.Add(btn_ok, 0, wx.ALL, 2)
+		button_sizer.Add((0, 0), 1)
+		btn_can = wx.Button(panel, wx.ID_CANCEL)
+		button_sizer.Add(btn_can, 0, wx.ALL, 2)
+		button_sizer.Add((0, 0), 1)
+		sizer.Add(button_sizer, 1, wx.EXPAND | wx.ALL, 10)
+		sizer.Fit(panel)
+		self.ClientSize = panel.Size
+
+		cal.Bind(wx.EVT_KEY_DOWN, self.__on_key_down)
+		cal.SetFocus()
+		self.cal = cal
+	#-----------------------------------------------------------
+	def __on_key_down(self, evt):
+		code = evt.KeyCode
+		if code == wx.WXK_TAB:
+			self.cal.Navigate()
+		elif code in (wx.WXK_RETURN, wx.WXK_NUMPAD_ENTER):
+			self.EndModal(wx.ID_OK)
+		elif code == wx.WXK_ESCAPE:
+			self.EndModal(wx.ID_CANCEL)
+		else:
+			evt.Skip()
 
 #============================================================
 class cDateMatchProvider(gmMatchProvider.cMatchProvider):
@@ -66,8 +119,23 @@ class cDateMatchProvider(gmMatchProvider.cMatchProvider):
 	#--------------------------------------------------------
 	def getAllMatches(self):
 		"""Return all items."""
-		# FIXME: popup calendar to pick from
-		return (False, [])
+
+		matches = (False, [])
+		return matches
+
+		dlg = cCalendarDatePickerDlg(None)
+		# FIXME: show below parent
+		dlg.CentreOnScreen()
+
+		if dlg.ShowModal() == wx.ID_OK:
+			date = dlg.cal.Date
+			if date is not None:
+				if date.IsValid():
+					date = gmDateTime.wxDate2py_dt(wxDate = date)
+					matches = (True, [{'data': date, 'label': date.strftime('%Y-%m-%d')}])
+		dlg.Destroy()
+
+		return matches
 #============================================================
 class cDateInputCtrl2(gmPhraseWheel.cPhraseWheel):
 
@@ -94,6 +162,26 @@ class cDateInputCtrl2(gmPhraseWheel.cPhraseWheel):
 
 		return None
 	#--------------------------------------------------------
+	def __pick_from_calendar(self):
+		dlg = cCalendarDatePickerDlg(self)
+		# FIXME: show below parent
+		dlg.CentreOnScreen()
+		decision = dlg.ShowModal()
+		date = dlg.cal.Date
+		dlg.Destroy()
+
+		if decision != wx.ID_OK:
+			return
+
+		if date is None:
+			return
+
+		if not date.IsValid():
+			return
+
+		date = gmDateTime.wxDate2py_dt(wxDate = date)
+		self.SetText(value = date.strftime('%Y-%m-%d'), data = date, suppress_smarts = True)
+	#--------------------------------------------------------
 	# phrasewheel internal API
 	#--------------------------------------------------------
 	def _on_lose_focus(self, event):
@@ -108,8 +196,19 @@ class cDateInputCtrl2(gmPhraseWheel.cPhraseWheel):
 	def _picklist_selection2display_string(self):
 		data = self._picklist.GetSelectedItemData()
 		if data is not None:
-			return data.format_accurately()
+			return data.strftime('%Y-%m-%d')
 		return self._picklist.get_selected_item_label()
+	#--------------------------------------------------------
+	def _on_key_down(self, event):
+
+		if event.AltDown() is False:
+			keycode = event.GetKeyCode()
+			if keycode == wx.WXK_F4:
+				self.__pick_from_calendar()
+				return
+
+		if self.GetValue().strip() != u'':
+			super(self.__class__, self)._on_key_down(event)
 	#--------------------------------------------------------
 	# external API
 	#--------------------------------------------------------
