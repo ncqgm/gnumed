a7e75ce1026c9bc9d2d4372333b02576d1595ec3
Karsten Hilbert <karsten.hilbert@gmx.net>
2011-02-26 00:34:32 +0100

    Add Gulich Score on GABHS in sore throat.

--- gnumed/gnumed/server/sql/v14-v15/dynamic/v15-clin-keyword_expansion-data.sql
+++ gnumed/gnumed/server/sql/v14-v15/dynamic/v15-clin-keyword_expansion-data.sql
@@ -39,6 +39,54 @@ account vital signs and symptoms !
 ');
 
 -- --------------------------------------------------------------
+delete from clin.keyword_expansion where keyword = 'score-Gulich-StrepA_Tonsillitis';
+
+insert into clin.keyword_expansion (
+	fk_staff,
+	keyword,
+	expansion
+) values (
+	null,
+	'score-Gulich-StrepA_Tonsillitis',
+'Gulich-Score: likelihood of GABHS in sore throat
+------------------------------------------------
+Gulich M, Triebel T, Zeitler H-P; Eur J Gen Pract 2002;8:58-62.
+
+    Throat mucosa
+0 - (lymphoid) granulations
+1 - reddish
+2 - deeply red
+
+    Uvula
+0 - normal
+1 - infected/slightly reddish
+2 - deeply red
+
+    Soft palate
+0 - normal
+1 - infected/slightly reddish
+2 - deeply red
+
+    Tonsils
+0 - normal/removed
+1 - reddish/swollen
+2 - exsudate
+-------------------------------
+
+Sum < 4: 91% GABHS negative
+Sum > 5: 81% GAHBS positive
+
+Sum 4-5 -> near-patient CRP
+
+	CRP < 35mg/L: 89% GABHS negative
+	CRP > 35mg/L: 89% GABHS positive
+
+Most accurate if
+- onset: 1 day to 1 week ago
+- age: 16 to 76 years
+');
+
+-- --------------------------------------------------------------
 select gm.log_script_insertion('v15-clin-keyword_expansion-data.sql', '1.3');
 
 -- ==============================================================
