fd1b1529b33401ef46f0a827dfdad987a81621ca
Karsten Hilbert <karsten.hilbert@gmx.net>
2011-03-16 17:20:52 +0100

    Prepare for 0.9.rc1.

--- gnumed/gnumed/dists/Linux/make-release_tarball.sh
+++ gnumed/gnumed/dists/Linux/make-release_tarball.sh
@@ -3,12 +3,10 @@
 #====================================================
 # license: GPL
 #====================================================
-#CLIENTREV="0.7.rc1"
-CLIENTREV="Git"
+CLIENTREV="0.9.rc1"
 CLIENTARCH="gnumed-client.$CLIENTREV.tgz"
 
-#SRVREV="13.rc1"
-SRVREV="Git"
+SRVREV="15.rc1"
 SRVARCH="gnumed-server.$SRVREV.tgz"
 
 FILES_REMOVE=\
