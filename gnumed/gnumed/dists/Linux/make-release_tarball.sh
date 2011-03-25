6ae3a035ab9dba868544dabaa66b6f1d4bb95b5c
Karsten Hilbert <karsten.hilbert@gmx.net>
2011-03-25 10:19:06 +0100

    Bump version to 0.9.rc5.

--- gnumed/gnumed/dists/Linux/make-release_tarball.sh
+++ gnumed/gnumed/dists/Linux/make-release_tarball.sh
@@ -3,10 +3,10 @@
 #====================================================
 # license: GPL
 #====================================================
-CLIENTREV="0.9.rc4"
+CLIENTREV="0.9.rc5"
 CLIENTARCH="gnumed-client.$CLIENTREV.tgz"
 
-SRVREV="15.rc4"
+SRVREV="15.rc5"
 SRVARCH="gnumed-server.$SRVREV.tgz"
 
 FILES_REMOVE=\
