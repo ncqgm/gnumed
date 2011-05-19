51c501fa413eb3aa5cd2ea5800e222966c8176ba
Karsten Hilbert <karsten.hilbert@gmx.net>
2011-05-19 16:50:33 +0200

    Prepare 0.9.5 release.

--- gnumed/gnumed/dists/Linux/make-release_tarball.sh
+++ gnumed/gnumed/dists/Linux/make-release_tarball.sh
@@ -3,10 +3,10 @@
 #====================================================
 # license: GPL
 #====================================================
-CLIENTREV="0.9.4"
+CLIENTREV="0.9.5"
 CLIENTARCH="gnumed-client.$CLIENTREV.tgz"
 
-SRVREV="15.4"
+SRVREV="15.5"
 SRVARCH="gnumed-server.$SRVREV.tgz"
 
 FILES_REMOVE=\
--- gnumed/gnumed/server/sql/v14-v15/dynamic/v15-release_notes-dynamic.sql
+++ gnumed/gnumed/server/sql/v14-v15/dynamic/v15-release_notes-dynamic.sql
@@ -18,23 +18,23 @@ INSERT INTO dem.message_inbox (
 ) VALUES (
 	(select pk from dem.staff where db_user = 'any-doc'),
 	(select pk_type from dem.v_inbox_item_type where type = 'memo' and category = 'administrative'),
-	'Release Notes for GNUmed 0.9.4 (database v15.4)',
-	'GNUmed 0.9.4 Release Notes:
+	'Release Notes for GNUmed 0.9.5 (database v15.5)',
+	'GNUmed 0.9.5 Release Notes:
 
-	0.9.4
+	Client 0.9.5
 
-FIX: gm-remove_person.sh did not properly delete persons [thanks J.Busser]
-FIX: enable running arriba w/o an active patient
-FIX: cEpisode.get_narrative() ignored <soap_cats> argument
-FIX: wrapper around Python"s deficient strftime() [thanks J.Jaarsveld]
-FIX: exception on displaying date deceased if not null [thanks J.Busser]
-FIX: exception on activating non-existant patient from inbox message [thanks Oliver]
+FIX: exception on trying to use Wine version of GL/MMI under Windows [thanks S.Hilbert]
+FIX: adjust default Windows path to FreeDiams executable [thanks S.Hilbert]
+FIX: exception on trying to put patient on same drug twice [thanks J.Busser]
+FIX: Windows can"t check hook script for -rw------- with os.stat() [thanks LuisCapriles]
+FIX: exception on DOB input because .strptime() returns TZ naive dates [thanks J.Busser]
+FIX: exceptions on entering large integers into DOB field
 
-IMPROVED: make creating/updating tags a restricted procedure [thanks Rogerio]
-IMPROVED: slightly relax external app exit code check on Windows [thanks vbanait]
-IMPROVED: placeholder "soap_for_encounters" now sorts by SOAP cat rank, then by date [thanks vbanait]
-IMRPOVED: better remove SOAP-less encounters from consultation report output [thanks vbanait]
-IMPROVED: do not try to sign results if none selected
+IMPROVED: logging of hook script permissions [thanks L.Capriles]
+
+	Database 15.5
+
+FIX: faulty quoting in database backup scripts using "su -c" [thanks Marc]
 ');
 
 -- --------------------------------------------------------------
