40e5f0bd8897a090dcb2a31173274ec07ef9f121
Karsten Hilbert <karsten.hilbert@gmx.net>
2011-07-05 12:00:24 +0200

    Drop obsolete clin.lnk_identity2primary_doc.

--- gnumed/gnumed/server/sql/v15-v16/static/v16-dem-lnk_identity2primary_doc.sql
+++ gnumed/gnumed/server/sql/v15-v16/static/v16-dem-lnk_identity2primary_doc.sql
@@ -1,13 +1,14 @@
 -- ==============================================================
 -- GNUmed database schema change script
 --
--- License: GPL
+-- License: GPL v2 or later
 -- Author: karsten.hilbert@gmx.net
 -- 
 -- ==============================================================
 \set ON_ERROR_STOP 1
 
 -- --------------------------------------------------------------
+drop table dem.lnk_identity2primary_doc cascade;
 
 -- --------------------------------------------------------------
-select gm.log_script_insertion('v16-clin-fhx_item-static.sql', 'v16.0');
+select gm.log_script_insertion('v16-dem-lnk_identity2primary_doc.sql', 'v16');
