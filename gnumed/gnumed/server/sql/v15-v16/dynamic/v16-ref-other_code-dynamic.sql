386502ac55a0c03da1f59e554bc95b6ea54c0ea2
Karsten Hilbert <karsten.hilbert@gmx.net>
2011-07-12 12:05:30 +0200

    Add table for "other" coding systems not specifically defined yet.

---
+++ gnumed/gnumed/server/sql/v15-v16/dynamic/v16-ref-other_code-dynamic.sql
@@ -0,0 +1,26 @@
+-- ==============================================================
+-- GNUmed database schema change script
+--
+-- License: GPL v2 or later
+-- Author: karsten.hilbert@gmx.net
+-- 
+-- ==============================================================
+\set ON_ERROR_STOP 1
+
+-- --------------------------------------------------------------
+comment on table ref.other_code is
+	'Holds codes from "other" coding systems for which no specific tables exist just yet.';
+
+
+-- UPDATE
+create trigger tr_upd_ref_code_tbl_check_backlink
+	before update on ref.other_code
+		for each row execute procedure ref.trf_upd_ref_code_tbl_check_backlink();
+
+-- DELETE
+create trigger tr_del_ref_code_tbl_check_backlink
+	before update on ref.other_code
+		for each row execute procedure ref.trf_del_ref_code_tbl_check_backlink();
+
+-- --------------------------------------------------------------
+select gm.log_script_insertion('v16-ref-other_code-dynamic.sql', '16.0');
