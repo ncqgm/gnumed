4b46c6578e6c5605d2745649864233e9049e631d
Karsten Hilbert <karsten.hilbert@gmx.net>
2011-09-22 15:09:33 +0200

    Bump version numbers.

--- gnumed/gnumed/dists/Linux/make-release_tarball.sh
+++ gnumed/gnumed/dists/Linux/make-release_tarball.sh
@@ -3,10 +3,10 @@
 #====================================================
 # license: GPL v2 or later
 #====================================================
-CLIENTREV="1.0.rc3"
+CLIENTREV="1.0.rc4"
 CLIENTARCH="gnumed-client.$CLIENTREV.tgz"
 
-SRVREV="16.rc3"
+SRVREV="16.rc4"
 SRVARCH="gnumed-server.$SRVREV.tgz"
 
 FILES_REMOVE=\
