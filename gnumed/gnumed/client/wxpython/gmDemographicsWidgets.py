1bdb277b332d108336c3014f17c0d7dd346d0c46
Karsten Hilbert <karsten.hilbert@gmx.net>
2011-12-01 13:49:50 +0100

    Protect against deletion of active name
    	- noticed by J.Busser

--- gnumed/gnumed/client/wxpython/gmDemographicsWidgets.py
+++ gnumed/gnumed/client/wxpython/gmDemographicsWidgets.py
@@ -1169,6 +1169,10 @@ class cPersonNamesManagerPnl(gmListWidgets.cGenericListManagerPnl):
 			gmDispatcher.send(signal = u'statustext', msg = _('Cannot delete the only name of a person.'), beep = True)
 			return False
 
+		if name['active_name']:
+			gmDispatcher.send(signal = u'statustext', msg = _('Cannot delete the active name of a person.'), beep = True)
+			return False
+
 		go_ahead = gmGuiHelpers.gm_show_question (
 			_(	'It is often advisable to keep old names around and\n'
 				'just create a new "currently active" name.\n'
