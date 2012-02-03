40bca4342d6435515b4588a28cd6ac08c98c6f72
Karsten Hilbert <karsten.hilbert@gmx.net>
2012-02-03 13:26:24 +0100

    Bump version to 1.1.11

--- gnumed/gnumed/dists/Linux/make-release_tarball.sh
+++ gnumed/gnumed/dists/Linux/make-release_tarball.sh
@@ -3,10 +3,10 @@
 #====================================================
 # license: GPL v2 or later
 #====================================================
-CLIENTREV="1.1.10"
+CLIENTREV="1.1.11"
 CLIENTARCH="gnumed-client.$CLIENTREV.tgz"
 
-SRVREV="16.10"
+SRVREV="16.11"
 SRVARCH="gnumed-server.$SRVREV.tgz"
 
 FILES_REMOVE=\
