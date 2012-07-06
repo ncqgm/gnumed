d1ed01d38ed5b1faa7f4c82ab3a6f5bd71a2cd72
Karsten Hilbert <karsten.hilbert@gmx.net>
2012-07-06 16:59:20 +0200

    Wrap the timeline as an EMR plugin

---
+++ gnumed/gnumed/client/wxpython/gmEMRTimelineWidgets.py
@@ -0,0 +1,85 @@
+"""GNUmed patient EMR timeline browser.
+
+Uses the excellent TheTimlineProject.
+"""
+#================================================================
+__author__ = "cfmoro1976@yahoo.es, sjtan@swiftdsl.com.au, Karsten.Hilbert@gmx.net"
+__license__ = "GPL"
+
+# std lib
+import sys
+import logging
+#os.path, codecs
+
+
+# 3rd party
+import wx
+
+
+# GNUmed libs
+if __name__ == '__main__':
+	sys.path.insert(0, '../../')
+from timelinelib.wxgui.component import TimelineComponent
+
+from Gnumed.exporters import timeline
+from Gnumed.business import gmPerson
+from Gnumed.wxpython import gmRegetMixin
+
+
+_log = logging.getLogger('gm.ui')
+
+#============================================================
+class cEMRTimelinePnl(TimelineComponent):
+
+	def __init__(self, *args, **kwargs):
+#		TimelineComponent.__init__(self, *args, **kwargs)
+#	def __init__(self, parent):
+		TimelineComponent.__init__(self, args[0])
+
+#============================================================
+from Gnumed.wxGladeWidgets import wxgEMRTimelinePluginPnl
+
+class cEMRTimelinePluginPnl(wxgEMRTimelinePluginPnl.wxgEMRTimelinePluginPnl, gmRegetMixin.cRegetOnPaintMixin):
+	"""Panel holding a number of widgets. Used as notebook page."""
+	def __init__(self, *args, **kwargs):
+		wxgEMRTimelinePluginPnl.wxgEMRTimelinePluginPnl.__init__(self, *args, **kwargs)
+		gmRegetMixin.cRegetOnPaintMixin.__init__(self)
+#		self.__init_ui()
+#		self.__register_interests()
+	#--------------------------------------------------------
+	# event handling
+	#--------------------------------------------------------
+#	def __register_interests(self):
+#		gmDispatcher.connect(signal = u'pre_patient_selection', receiver = self._on_pre_patient_selection)
+#		gmDispatcher.connect(signal = u'post_patient_selection', receiver = self._schedule_data_reget)
+	#--------------------------------------------------------
+#	def _on_pre_patient_selection(self):
+#		wx.CallAfter(self.__on_pre_patient_selection)
+	#--------------------------------------------------------
+#	def __on_pre_patient_selection(self):
+#		pass
+	#--------------------------------------------------------
+	def _on_refresh_button_pressed(self, event):
+		self._populate_with_data()
+	#--------------------------------------------------------
+	def repopulate_ui(self):
+		self._populate_with_data()
+	#--------------------------------------------------------
+	# internal API
+	#--------------------------------------------------------
+#	def __init_ui(self):
+#		pass
+	#--------------------------------------------------------
+	# reget mixin API
+	#--------------------------------------------------------
+	# remember to call
+	#	self._schedule_data_reget()
+	# whenever you learn of data changes from database listener
+	# threads, dispatcher signals etc.
+	def _populate_with_data(self):
+		pat = gmPerson.gmCurrentPatient()
+		if not pat.connected:
+			return True
+		self._PNL_timeline.open_timeline(timeline.create_timeline_file(patient = pat))
+		return True
+#============================================================
