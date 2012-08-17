f5f3ab21573c0abc8bfeca8fae8474e4cecdce80
Karsten Hilbert <karsten.hilbert@gmx.net>
2012-08-17 10:19:31 +0200

    Prepare 1.2.3 release

--- gnumed/gnumed/server/sql/v16-v17/dynamic/v17-release_notes-dynamic.sql
+++ gnumed/gnumed/server/sql/v16-v17/dynamic/v17-release_notes-dynamic.sql
@@ -17,26 +17,15 @@ INSERT INTO dem.message_inbox (
 ) VALUES (
 	(select pk from dem.staff where db_user = 'any-doc'),
 	(select pk_type from dem.v_inbox_item_type where type = 'memo' and category = 'administrative'),
-	'Release Notes for GNUmed 1.2.2 (database v17.2)',
-	'GNUmed 1.2.2 Release Notes:
+	'Release Notes for GNUmed 1.2.3 (database v17.3)',
+	'GNUmed 1.2.3 Release Notes:
 
-	1.2.2
+	1.2.3
 
-IMPROVED: more frequent waiting list updates [thanks J.Busser]
-IMRPOVED: release mail word-smithing
-IMPROVED: disambiguate "problem IS health issue" in SOAP editor [thanks J.Busser]
-IMPROVED: better listing of bills w/o items [thanks J.Busser]
-
-FIX: tooltip of bills w/o items [thanks J.Busser]
-FIX: failure to save .tex bill files
-FIX: faulty encounter matcher SQL [thanks J.Busser]
-
-	17.2
-
-FIX: incorrect auditing setup of ref.billable [thanks J.Busser]
-
-IMPROVED: prevent bills w/o items [thanks J.Busser]
-IMPROVED: warn on upgrade if target DB exists [thanks V.Banait]
+FIX: failure to parse plugin directory with frozen app [thanks S.Hilbert]
+FIX: failure to add >1 branded intake in sequence [thanks V.Banait]
+FIX: exception on saving hospital stay w/o admission date
+FIX: failure to check for message expiry > due [thanks S.Griesfeller]
 ');
 
 -- --------------------------------------------------------------
