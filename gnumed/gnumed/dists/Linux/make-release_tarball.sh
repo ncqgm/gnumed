425bc8e67eff1d772fcdb15b8bc60f565ef9fabd
Karsten Hilbert <karsten.hilbert@gmx.net>
2012-09-15 10:38:22 +0200

    Stamp 1.1.19 release

--- gnumed/gnumed/dists/Linux/make-release_tarball.sh
+++ gnumed/gnumed/dists/Linux/make-release_tarball.sh
@@ -3,10 +3,10 @@
 #====================================================
 # license: GPL v2 or later
 #====================================================
-CLIENTREV="1.1.18"
+CLIENTREV="1.1.19"
 CLIENTARCH="gnumed-client.$CLIENTREV.tgz"
 
-SRVREV="16.18"
+SRVREV="16.19"
 SRVARCH="gnumed-server.$SRVREV.tgz"
 
 FILES_REMOVE=\
--- gnumed/gnumed/server/sql/v15-v16/dynamic/v16-release_notes-dynamic.sql
+++ gnumed/gnumed/server/sql/v15-v16/dynamic/v16-release_notes-dynamic.sql
@@ -17,12 +17,12 @@ INSERT INTO dem.message_inbox (
 ) VALUES (
 	(select pk from dem.staff where db_user = 'any-doc'),
 	(select pk_type from dem.v_inbox_item_type where type = 'memo' and category = 'administrative'),
-	'Release Notes for GNUmed 1.1.18 (database v16.18)',
-	'GNUmed 1.1.18 Release Notes:
+	'Release Notes for GNUmed 1.1.19 (database v16.19)',
+	'GNUmed 1.1.19 Release Notes:
 
-	1.1.18
+	1.1.19
 
-FIX: exception on adding region [thanks U.Koch Kronberg]
+FIX: faulty handling of inbox message context PK arrays
 ');
 
 -- --------------------------------------------------------------
