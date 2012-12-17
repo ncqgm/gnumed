f04455465dccb4eeb1f064bc89c4d76bfb85f0fc
Karsten Hilbert <karsten.hilbert@gmx.net>
2012-12-17 20:35:28 +0100

    Add scores on bacterial conjunctivitis/Polymyalgia rheumatica
    	Both scores are in German.

---
+++ gnumed/gnumed/server/sql/v17-v18/dynamic/v18-ref-Bird_PMR_1979.sql
@@ -0,0 +1,41 @@
+-- ==============================================================
+-- GNUmed database schema change script
+--
+-- License: GPL v2 or later
+-- Author: karsten.hilbert@gmx.net
+--
+-- ==============================================================
+\set ON_ERROR_STOP 1
+
+--set default_transaction_read_only to off;
+-- --------------------------------------------------------------
+delete from ref.keyword_expansion where keyword = 'score-Bird-Polymyalgia_rheumatica';
+
+insert into ref.keyword_expansion (
+	fk_staff,
+	keyword,
+	textual_data
+) values (
+	null,
+	'score-Bird-Polymyalgia_rheumatica',
+'Bird-Score für Polymyalgia rheumatica
+-------------------------------------
+Bird A, et al. An evaluation of criteria for polymyalgia rheumatica. Ann Rheum Dis 1979 Oct;38(5): 434-9.
+
+$<1/0>$ bilaterale Schulterschmerzen und/oder –steife
+$<1/0>$ Oberarm-Druckschmerz bds
+$<1/0>$ Morgensteifigkeit >1h
+$<1/0>$ Depression und/oder Gewichtsverlust
+$<1/0>$ initiale BSG >40 (nach 1h)
+$<1/0>$ Alter >65
+$<1/0>$ akuter Beginn (in 2 Wochen von Erstsymptomen bis Vollbild)
+
+Summe: $<Summe>$ (>2: PMR wahrscheinlich, Sens ~92%, Spez ~80%)
+
+Anmerkung: bisher keine international akzeptierten
+Diagnosekriterien (laut Deutscher Gesellschaft für
+Rheumatologie, 2012)'
+);
+
+-- --------------------------------------------------------------
+select gm.log_script_insertion('v18-ref-Bird_PMR_1979.sql', '18.0');
---
+++ gnumed/gnumed/server/sql/v17-v18/dynamic/v18-ref-bacterial_conjunctivitis_score.sql
@@ -0,0 +1,45 @@
+-- ==============================================================
+-- GNUmed database schema change script
+--
+-- License: GPL v2 or later
+-- Author: karsten.hilbert@gmx.net
+--
+-- ==============================================================
+\set ON_ERROR_STOP 1
+
+--set default_transaction_read_only to off;
+-- --------------------------------------------------------------
+delete from ref.keyword_expansion where keyword = 'score-bakterielle_Konjunktivitis';
+
+insert into ref.keyword_expansion (
+	fk_staff,
+	keyword,
+	textual_data
+) values (
+	null,
+	'score-bakterielle_Konjunktivitis',
+'Score zur bakteriellen Konjunktivitis (2004)
+--------------------------------------------
+Rietveld R. et al: Predicting bacterial cause in infectious conjunctivitis. BMJ 2004; 329: 206-210.
+
+$<5/0>$ ZWEI verklebte Augen morgens
+$<2/0>$ NUR EIN verklebtes Auge morgens
+$<ja: -1 / nein: 0>$ juckendes Auge
+$<ja: -2 / nein: 0>$ Konjunktivitis in der Anamnese
+
+Summe: $<Summe>$ (>= 2: 67% korrekt positiv und 73% korrekt negativ)
+
+Ausschlußkriterien:
+
+	< 18 Jahre alt
+	> 7 Tage Symptome
+	Antibiose in den letzten 2 Wochen
+	Kontaktlinsen
+	Augentrauma/-OP
+	akuter Visusverlust
+
+Therapie: Bibrocathol (Antiseptikum), sonst Kanamycin (Arznei-Telegramm 1995)'
+);
+
+-- --------------------------------------------------------------
+select gm.log_script_insertion('v18-ref-bacterial_conjunctivitis_score.sql', '18.0');
