525d574184adaf7ef813954c9552939c20266760
Karsten Hilbert <karsten.hilbert@gmx.net>
2013-03-11 22:35:34 +0100

    Set version numbers to 1.3.0/18.0

--- gnumed/gnumed/client/gnumed.py
+++ gnumed/gnumed/client/gnumed.py
@@ -86,7 +86,7 @@ against. Please run GNUmed as a non-root user.
 	sys.exit(1)
 
 #----------------------------------------------------------
-current_client_version = u'1.3.rc6'
+current_client_version = u'1.3.0'
 current_client_branch = u'1.3'
 
 _log = None
