a9de7dadc30aa50379b5f4105f2407dee78c4705
Karsten Hilbert <karsten.hilbert@gmx.net>
2013-04-17 15:12:25 +0200

    Prepare 1.3.3 release

--- gnumed/gnumed/server/sql/v17-v18/dynamic/v18-release_notes-dynamic.sql
+++ gnumed/gnumed/server/sql/v17-v18/dynamic/v18-release_notes-dynamic.sql
@@ -17,36 +17,22 @@ INSERT INTO dem.message_inbox (
 ) VALUES (
 	(select pk from dem.staff where db_user = 'any-doc'),
 	(select pk_type from dem.v_inbox_item_type where type = 'memo' and category = 'administrative'),
-	'Release Notes for GNUmed 1.3.2 (database v18.2)',
-	'GNUmed 1.3.2 Release Notes:
+	'Release Notes for GNUmed 1.3.3 (database v18.3)',
+	'GNUmed 1.3.3 Release Notes:
 
-	1.3.2
+	1.3.3
 
-FIX: failure to fully escape % in Xe(La)TeX engine [thanks V.Banait]
-FIX: formatting error in current substance intake list
-FIX: collision of placeholder replacement and text expansion filling
-FIX: exception on substance-discontinued in the future [thanks S.Hilbert]
-FIX: exception on months of different length in timeline [thanks S.Hilbert]
-FIX: faulty use of test results formatting
-FIX: intake EA forgot non-brand preparation [thanks S.Hilbert]
-FIX: re-saving of durations when editing substance intakes [thanks S.Hilbert]
-FIX: unable to save same-day hospital admission/discharge
+FIX: failure to use active patient on adding waiting list entry [thanks S.Hilbert]
+FIX: failure to print in some Windows installations [thanks V.Banait]
 
-IMPROVED: record deletion of inbox messages in EMR
-IMPROVED: create recalls from vaccinations manager
-IMPROVED: clean up LaTeX/Xe(La)TeX handling
-IMPROVED: keep a copy of printed current medication lists [thanks Liz]
-IMPROVED: add MELD score [thanks S.Hilbert]
-IMPROVED: include duration with current_meds_table [thanks V.Banait]
+IMPROVED: primitive HL7 formatting
+IMPROVED: added recalls document template
+IMRPOVED: check files for readability before printing
+IMPROVED: sorta workaround infamous TreeCtrl.GetPyData() PyAssertion
 
+	18.3
 
-	18.2
-
-FIX: fake .due_/.expiry_date must be TIMESTAMP WITH TIME ZONE (not DATE)
-FIX: faultily quoted DB translations/translation exporter
-FIX: adjust dynamic text expansions to new HINT detection format
-
-IMPROVED: French DB translations
+FIX: faulty bill.trf_prevent_empty_bills [thanks S.Urbanek]
 ');
 
 -- --------------------------------------------------------------
