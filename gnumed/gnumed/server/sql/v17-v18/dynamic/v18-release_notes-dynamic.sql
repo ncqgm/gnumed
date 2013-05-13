7db44e2c9ab0c331fbc408c35572426d32e7cc5a
Karsten Hilbert <karsten.hilbert@gmx.net>
2013-05-13 12:22:16 +0200

    Prepare 1.3.4 release

--- gnumed/gnumed/server/sql/v17-v18/dynamic/v18-release_notes-dynamic.sql
+++ gnumed/gnumed/server/sql/v17-v18/dynamic/v18-release_notes-dynamic.sql
@@ -17,22 +17,27 @@ INSERT INTO dem.message_inbox (
 ) VALUES (
 	(select pk from dem.staff where db_user = 'any-doc'),
 	(select pk_type from dem.v_inbox_item_type where type = 'memo' and category = 'administrative'),
-	'Release Notes for GNUmed 1.3.3 (database v18.3)',
-	'GNUmed 1.3.3 Release Notes:
+	'Release Notes for GNUmed 1.3.4 (database v18.4)',
+	'GNUmed 1.3.4 Release Notes:
 
-	1.3.3
+	1.3.4
 
-FIX: failure to use active patient on adding waiting list entry [thanks S.Hilbert]
-FIX: failure to print in some Windows installations [thanks V.Banait]
+FIX: exception on DDE access to IE when printing
+FIX: exception on timeline display problems
+FIX: exception on logging cIdentity.export_tray [thanks J.Busser]
+FIX: billing EA ignored encounter selection [thanks J.Busser]
 
-IMPROVED: primitive HL7 formatting
-IMPROVED: added recalls document template
-IMRPOVED: check files for readability before printing
-IMPROVED: sorta workaround infamous TreeCtrl.GetPyData() PyAssertion
+IMPROVED: prepare for static placeholder depreciation
+IMPROVED: verbosely format duration in $<current_meds_list>$ [thanks V.Banait]
+IMPROVED: "Cave:" field not red when known to not have allergies
+IMPROVED: several reports on medications taken by patients
+IMPROVED: display of dynamic text expansion fill-in
+IMPROVED: duplicates error message in drug brands EA [thanks V.Banait]
+IMPROVED: also invoke text macros with <CTRL-ALT-T>
 
-	18.3
+	18.4
 
-FIX: faulty bill.trf_prevent_empty_bills [thanks S.Urbanek]
+FIX: missing constraint clin.encounter.started <= clin.encounter.last_affirmed
 ');
 
 -- --------------------------------------------------------------
