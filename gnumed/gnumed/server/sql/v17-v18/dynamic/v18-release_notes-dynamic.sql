1bafa3a887f7fb6a05508e823f22c76b71767ea6
Karsten Hilbert <karsten.hilbert@gmx.net>
2013-06-10 12:38:24 +0200

    Prepare 1.3.5 release

--- gnumed/gnumed/server/sql/v17-v18/dynamic/v18-release_notes-dynamic.sql
+++ gnumed/gnumed/server/sql/v17-v18/dynamic/v18-release_notes-dynamic.sql
@@ -17,28 +17,19 @@ INSERT INTO dem.message_inbox (
 ) VALUES (
 	(select pk from dem.staff where db_user = 'any-doc'),
 	(select pk_type from dem.v_inbox_item_type where type = 'memo' and category = 'administrative'),
-	'Release Notes for GNUmed 1.3.4 (database v18.4)',
-	'GNUmed 1.3.4 Release Notes:
+	'Release Notes for GNUmed 1.3.5 (database v18.5)',
+	'GNUmed 1.3.5 Release Notes:
 
-	1.3.4
+	1.3.5
 
-FIX: exception on DDE access to IE when printing
-FIX: exception on timeline display problems
-FIX: exception on logging cIdentity.export_tray [thanks J.Busser]
-FIX: billing EA ignored encounter selection [thanks J.Busser]
+FIX: exception on merging patients with identical external IDs [thanks Jim]
 
-IMPROVED: prepare for static placeholder depreciation
-IMPROVED: verbosely format duration in $<current_meds_list>$ [thanks V.Banait]
-IMPROVED: "Cave:" field not red when known to not have allergies
-IMPROVED: several reports on medications taken by patients
-IMPROVED: display of dynamic text expansion fill-in
-IMPROVED: duplicates error message in drug brands EA [thanks V.Banait]
-IMPROVED: also invoke text macros with <CTRL-ALT-T>
+IMPROVED: further clarify license of Timeline icons
 
-	18.4
+	18.5
 
-FIX: missing constraint clin.encounter.started <= clin.encounter.last_affirmed
+no changes
 ');
 
 -- --------------------------------------------------------------
-select gm.log_script_insertion('v18-release_notes-dynamic.sql', '18.2');
+select gm.log_script_insertion('v18-release_notes-dynamic.sql', '18.5');
