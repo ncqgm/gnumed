4dac14a44295bfaa99460fd6d061aae89d7797df
Karsten Hilbert <karsten.hilbert@gmx.net>
2013-06-12 15:26:44 +0200

    Forward port drop of clin_root_item_soap_cat check

---
+++ gnumed/gnumed/server/sql/v17-v18/fixups/v18-clin-soapU_check-fixup.sql
@@ -0,0 +1,17 @@
+-- ==============================================================
+-- GNUmed database schema change script
+--
+-- License: GPL v2 or later
+-- Author: karsten.hilbert@gmx.net
+--
+-- ==============================================================
+\set ON_ERROR_STOP 1
+
+-- --------------------------------------------------------------
+-- clin.clin_root_item
+\unset ON_ERROR_STOP
+alter table clin.clin_root_item	drop constraint clin_root_item_soap_cat;
+\set ON_ERROR_STOP 1
+
+-- --------------------------------------------------------------
+select gm.log_script_insertion('v18-clin-soapU_check-fixup.sql', '17.10');
