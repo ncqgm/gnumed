4d6e21a41dc09a25a8db7c10de4b7481a300717f
Karsten Hilbert <karsten.hilbert@gmx.net>
2013-08-21 22:36:03 +0200

    Improved placeholder documentation

--- gnumed/gnumed/client/wxpython/gmMacro.py
+++ gnumed/gnumed/client/wxpython/gmMacro.py
@@ -91,6 +91,9 @@ _injectable_placeholders = {
 known_variant_placeholders = [
 	# generic:
 	u'free_text',							# show a dialog for entering some free text
+											# <data>:
+											#	format:	tex (only, currently)
+											#	message: shown in input dialog, must not contain "//" or "::"
 	u'text_snippet',						# a text snippet, taken from the keyword expansion mechanism
 											# args: <snippet name>//<template>
 	u'data_snippet',						# a binary snippet, taken from the keyword expansion mechanism
