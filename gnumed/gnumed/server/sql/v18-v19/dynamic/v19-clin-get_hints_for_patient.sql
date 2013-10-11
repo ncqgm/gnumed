61a92c437146c35a7352bcc7555130cac9b1d680
Karsten Hilbert <karsten.hilbert@gmx.net>
2013-10-11 22:26:51 +0200

    PG-9.1-ify clin.get_hints_for_patient()

--- gnumed/gnumed/server/sql/v18-v19/dynamic/v19-clin-get_hints_for_patient.sql
+++ gnumed/gnumed/server/sql/v18-v19/dynamic/v19-clin-get_hints_for_patient.sql
@@ -20,11 +20,11 @@ DECLARE
 	_r ref.auto_hint%rowtype;
 	_query text;
 	_applies boolean;
-	_exc_state text;
-	_exc_msg text;
-	_exc_detail text;
-	_exc_hint text;
-	_exc_context text;
+--	_exc_state text;
+--	_exc_msg text;
+--	_exc_detail text;
+--	_exc_hint text;
+--	_exc_context text;
 BEGIN
 	FOR _r IN SELECT * FROM ref.auto_hint WHERE is_active LOOP
 		_query := replace(_r.query, ''ID_ACTIVE_PATIENT'', _pk_identity::text);
@@ -39,17 +39,21 @@ BEGIN
 			--WHEN insufficient_privilege THEN RAISE WARNING ''auto hint query failed: %'', _query;
 			WHEN others THEN
 				RAISE WARNING ''auto hint query failed: %'', _query;
-				GET STACKED DIAGNOSTICS
-					_exc_state = RETURNED_SQLSTATE,
-					_exc_msg = MESSAGE_TEXT,
-					_exc_detail = PG_EXCEPTION_DETAIL,
-					_exc_hint = PG_EXCEPTION_HINT,
-					_exc_context = PG_EXCEPTION_CONTEXT;
-				RAISE WARNING ''SQL STATE: %'', _exc_state;
-				RAISE WARNING ''MESSAGE: %'', _exc_msg;
-				RAISE WARNING ''DETAIL: %'', _exc_detail;
-				RAISE WARNING ''HINT: %'', _exc_hint;
-				RAISE WARNING ''CONTEXT: %'', _exc_context;
+				-- only available starting with PG 9.2:
+				--GET STACKED DIAGNOSTICS
+				--	_exc_state = RETURNED_SQLSTATE,
+				--	_exc_msg = MESSAGE_TEXT,
+				--	_exc_detail = PG_EXCEPTION_DETAIL,
+				--	_exc_hint = PG_EXCEPTION_HINT,
+				--	_exc_context = PG_EXCEPTION_CONTEXT;
+				--RAISE WARNING ''SQL STATE: %'', _exc_state;
+				--RAISE WARNING ''MESSAGE: %'', _exc_msg;
+				--RAISE WARNING ''DETAIL: %'', _exc_detail;
+				--RAISE WARNING ''HINT: %'', _exc_hint;
+				--RAISE WARNING ''CONTEXT: %'', _exc_context;
+				-- workaround for 9.1:
+				RAISE WARNING ''SQL STATE: %'', SQLSTATE;
+				RAISE WARNING ''MESSAGE: %'', SQLERRM;
 				_r.title := ''ERROR checking for ['' || _r.title || ''] !'';
 				_r.hint := _query;
 				RETURN NEXT _r;
