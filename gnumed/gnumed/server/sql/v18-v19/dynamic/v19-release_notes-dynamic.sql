d76d417b077d6dd90ee11be980133d74f9bc616f
Karsten Hilbert <karsten.hilbert@gmx.net>
2014-01-29 15:02:12 +0100

    Prepare 1.4.6 release

--- gnumed/gnumed/server/sql/v18-v19/dynamic/v19-release_notes-dynamic.sql
+++ gnumed/gnumed/server/sql/v18-v19/dynamic/v19-release_notes-dynamic.sql
@@ -17,21 +17,22 @@ INSERT INTO dem.message_inbox (
 ) VALUES (
 	(select pk from dem.staff where db_user = 'any-doc'),
 	(select pk_type from dem.v_inbox_item_type where type = 'memo' and category = 'administrative'),
-	'Release Notes for GNUmed 1.4.5 (database v19.5)',
-	'GNUmed 1.4.5 Release Notes:
+	'Release Notes for GNUmed 1.4.6 (database v19.6)',
+	'GNUmed 1.4.6 Release Notes:
 
-	1.4.5
+	1.4.6
 
-	FIX: permissions of ${TMP}/gnumed/ on multiuser systems [thanks Kalle]
+FIX: missing qualification of make_pg_exception_fields_unicode() [thanks Khalil]
+FIX: missing method in TimeLine code
+FIX: bug in new-person EA validation code
+FIX: creating dummy identities used faulty gender
+FIX: faulty apparent_age formatting (age between 1-2 month ignored)
+FIX: attempt to use "chart review" encounter type even if it does not exist [thanks Jim]
 
-	IMPROVED: lab results grid tooltips
-	IMRPOVED: manage test types from lab results grid
-	IMRPOVED: manage aggregates from test types listing
-	IMPROVED: temporary directory
+IMPROVED: DOB/age display in top panel
+IMPROVED: deal with display of missing gender
 
-	19.5 -- Requires PostgreSQL 9.1 !
-
-	FIX: gm-adjust_db_settings.sh to work from non-CWD [thanks Marc]
+	19.6 -- Requires PostgreSQL 9.1 !
 ');
 
 -- --------------------------------------------------------------
