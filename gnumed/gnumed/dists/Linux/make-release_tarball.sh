eb0650aa15987db8cd07d7e2673baba01a7ef6b0
Karsten Hilbert <karsten.hilbert@gmx.net>
2015-01-08 10:47:43 +0100

    Prep 1.5.0 release
    	Set appropriate version numbers

--- gnumed/gnumed/dists/Linux/make-release_tarball.sh
+++ gnumed/gnumed/dists/Linux/make-release_tarball.sh
@@ -3,10 +3,10 @@
 #====================================================
 # license: GPL v2 or later
 #====================================================
-CLIENTREV="1.5.rc4"
+CLIENTREV="1.5.0"
 CLIENTARCH="gnumed-client.$CLIENTREV.tgz"
 
-SRVREV="20.rc4"
+SRVREV="20.0"
 SRVARCH="gnumed-server.$SRVREV.tgz"
 
 LANG_LIST="ar bg ca cs da de el en_AU en_CA es fr id it ka nb nl pl pt pt_BR ro ru ru_RU sq sr sv tr uk zh_TW"
