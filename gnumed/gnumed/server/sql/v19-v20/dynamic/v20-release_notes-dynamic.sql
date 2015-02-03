0bc8234af52f5ab8b27540120b47338985981b92
Karsten Hilbert <karsten.hilbert@gmx.net>
2015-02-03 20:37:25 +0100

    Prepare 1.5.3/20.3 release

--- gnumed/gnumed/server/sql/v19-v20/dynamic/v20-release_notes-dynamic.sql
+++ gnumed/gnumed/server/sql/v19-v20/dynamic/v20-release_notes-dynamic.sql
@@ -17,13 +17,32 @@ INSERT INTO dem.message_inbox (
 ) VALUES (
 	(select pk from dem.staff where db_user = 'any-doc'),
 	(select pk_type from dem.v_inbox_item_type where type = 'memo' and category = 'administrative'),
-	'Release Notes for GNUmed 1.5.2 (database v20.2)',
-	'GNUmed 1.5.2 Release Notes:
+	'Release Notes for GNUmed 1.5.3 (database v20.3)',
+	'GNUmed 1.5.3 Release Notes:
 
-	Database 20.2
+	1.5.3
 
-FIX: setting of FK to clin.test_org on dangling test types
+FIX: HL7 import [thanks Jim]
+FIX: VCF export with address/phone
+FIX: LinuxMedNews XML export with address/phone
+FIX: wx.HIDE_READONLY is no more
+FIX: wxp2.8 does not yet have GetToolTipString()
+FIX: creating dynamic hints [thanks Jim]
+FIX: editing-from-encounter-list w/o encounter selected [thanks Jim]
+FIX: properly LaTeX-escape "\n" into "\\" [thanks Jim]
+
+IMPROVED: do not require simplejson in gmKVK
+IMPROVED: show ext IDs/comm channels in EMR Journal Export
+IMPROVED: provider inbox labels [thanks Jim]
+IMPROVED: most recent test result display in patient overview [thanks Jim]
+IMPROVED: select-day shows results in by-day test results panel [thanks Jim]
+IMPROVED: $<gen_adr_*>$ can now cache several instances [thanks Jim]
+IMPROVED: $<receiver_*>$ can now cache several instances [thanks Jim]
+
+	20.3
+
+FIX: setting FK to clin.test_org on dangling test types, really, this time
 ');
 
 -- --------------------------------------------------------------
-select gm.log_script_insertion('v20-release_notes-dynamic.sql', '20.0');
+select gm.log_script_insertion('v20-release_notes-dynamic.sql', '20.3');
