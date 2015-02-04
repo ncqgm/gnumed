8ff90e0f492574aa8c5d19a01d627b4a12443cf3
Karsten Hilbert <karsten.hilbert@gmx.net>
2015-02-04 23:39:12 +0100

    Improve wxPython Assert() debugging

--- gnumed/gnumed/client/wxpython/gmGuiMain.py
+++ gnumed/gnumed/client/wxpython/gmGuiMain.py
@@ -3151,6 +3151,11 @@ class gmApp(wx.App):
 
 	def OnInit(self):
 
+		if _cfg.get(option = 'debug'):
+			self.SetAssertMode(wx.PYAPP_ASSERT_EXCEPTION | wx.PYAPP_ASSERT_LOG)
+		else:
+			self.SetAssertMode(wx.PYAPP_ASSERT_SUPPRESS)
+
 		self.__starting_up = True
 
 		gmExceptionHandlingWidgets.install_wx_exception_handler()
