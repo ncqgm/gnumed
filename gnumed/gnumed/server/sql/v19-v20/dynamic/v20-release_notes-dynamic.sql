f83f81b7e7b2205c0c89cf1d588f80c89d385e77
Karsten Hilbert <karsten.hilbert@gmx.net>
2015-03-02 10:16:57 +0100

    Prepare 1.5.4 release

--- gnumed/gnumed/server/sql/v19-v20/dynamic/v20-release_notes-dynamic.sql
+++ gnumed/gnumed/server/sql/v19-v20/dynamic/v20-release_notes-dynamic.sql
@@ -17,32 +17,26 @@ INSERT INTO dem.message_inbox (
 ) VALUES (
 	(select pk from dem.staff where db_user = 'any-doc'),
 	(select pk_type from dem.v_inbox_item_type where type = 'memo' and category = 'administrative'),
-	'Release Notes for GNUmed 1.5.3 (database v20.3)',
-	'GNUmed 1.5.3 Release Notes:
+	'Release Notes for GNUmed 1.5.4 (database v20.4)',
+	'GNUmed 1.5.4 Release Notes:
 
-	1.5.3
+	1.5.4
 
-FIX: HL7 import [thanks Jim]
-FIX: VCF export with address/phone
-FIX: LinuxMedNews XML export with address/phone
-FIX: wx.HIDE_READONLY is no more
-FIX: wxp2.8 does not yet have GetToolTipString()
-FIX: creating dynamic hints [thanks Jim]
-FIX: editing-from-encounter-list w/o encounter selected [thanks Jim]
-FIX: properly LaTeX-escape "\n" into "\\" [thanks Jim]
+FIX: LaTeX output of $soap_for_encounter$ [thanks Jim]
+FIX: streamline fairly-recent encounter activation
+FIX: patient instantiation in episode management tooltips
+FIX: street PRW exception on streets w/o postcode
+FIX: exception on moving SOAP between encounters (1.4.15)
+FIX: exception in staff list if there is staff with deleted DB account (1.4.16)
 
-IMPROVED: do not require simplejson in gmKVK
-IMPROVED: show ext IDs/comm channels in EMR Journal Export
-IMPROVED: provider inbox labels [thanks Jim]
-IMPROVED: most recent test result display in patient overview [thanks Jim]
-IMPROVED: select-day shows results in by-day test results panel [thanks Jim]
-IMPROVED: $<gen_adr_*>$ can now cache several instances [thanks Jim]
-IMPROVED: $<receiver_*>$ can now cache several instances [thanks Jim]
+IMPROVED: default consultation report
+IMPROVED: logging of unexpected encounter changes
+IMPROVED: run pdflatex with -recorder so package "currfile" can be used
 
-	20.3
+	20.4
 
-FIX: setting FK to clin.test_org on dangling test types, really, this time
+no changes
 ');
 
 -- --------------------------------------------------------------
-select gm.log_script_insertion('v20-release_notes-dynamic.sql', '20.3');
+select gm.log_script_insertion('v20-release_notes-dynamic.sql', '20.4');
