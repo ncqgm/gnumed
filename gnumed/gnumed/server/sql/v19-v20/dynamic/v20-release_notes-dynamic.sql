ecb61dc9ae8ac639b02d21e293c048b47470cfe8
Karsten Hilbert <karsten.hilbert@gmx.net>
2015-04-24 11:24:11 +0200

    Prepare 1.5.5. release

--- gnumed/gnumed/server/sql/v19-v20/dynamic/v20-release_notes-dynamic.sql
+++ gnumed/gnumed/server/sql/v19-v20/dynamic/v20-release_notes-dynamic.sql
@@ -17,26 +17,19 @@ INSERT INTO dem.message_inbox (
 ) VALUES (
 	(select pk from dem.staff where db_user = 'any-doc'),
 	(select pk_type from dem.v_inbox_item_type where type = 'memo' and category = 'administrative'),
-	'Release Notes for GNUmed 1.5.4 (database v20.4)',
-	'GNUmed 1.5.4 Release Notes:
+	'Release Notes for GNUmed 1.5.5 (database v20.5)',
+	'GNUmed 1.5.5 Release Notes:
 
-	1.5.4
+	1.5.5
 
-FIX: LaTeX output of $soap_for_encounter$ [thanks Jim]
-FIX: streamline fairly-recent encounter activation
-FIX: patient instantiation in episode management tooltips
-FIX: street PRW exception on streets w/o postcode
-FIX: exception on moving SOAP between encounters (1.4.15)
-FIX: exception in staff list if there is staff with deleted DB account (1.4.16)
+FIX: exception in emr.get_clin_narrative (encounters is None) [thanks Marc]
 
-IMPROVED: default consultation report
-IMPROVED: logging of unexpected encounter changes
-IMPROVED: run pdflatex with -recorder so package "currfile" can be used
+NEW: startup notice to ensure users are running Python 2.7
 
-	20.4
+	20.5
 
 no changes
 ');
 
 -- --------------------------------------------------------------
-select gm.log_script_insertion('v20-release_notes-dynamic.sql', '20.4');
+select gm.log_script_insertion('v20-release_notes-dynamic.sql', '20.5');
