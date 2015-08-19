769d0282f770d8232cb4b21507070704cc6abb4b
Karsten Hilbert <karsten.hilbert@gmx.net>
2015-08-19 16:53:15 +0200

    Improve gm-from-vcs.bat
    	- add --local-import
    	- add comments
    	- use xcopy rather than mklink to pre-create .../Gnumed

--- gnumed/gnumed/client/gm-from-vcs.bat
+++ gnumed/gnumed/client/gm-from-vcs.bat
@@ -1,4 +1,15 @@
-mklink /J ..\Gnumed ..\client
-set PYTHONPATH=..\;%PYTHONPATH%
-Python gnumed.py --log-file=gm-from-vcs.log --conf-file=gm-from-vcs.conf --debug
+REM # GNUmed tarball startup batch file
+
+REM # normally we would like to use a link but Python
+REM # on Windows seems to have problems importing
+REM # modules from directory links
+REM mklink /J ..\Gnumed ..\client
+
+REM # hence we use xcopy: http://commandwindows.com/xcopy.htm
+REM # note that no (old) link should pre-exist lest we overwrite ourselves
+xcopy ..\client ..\Gnumed /E /I /F /H /O /Y
+
+set PYTHONPATH=..;%PYTHONPATH%
+
+Python gnumed.py --log-file=gm-from-vcs.log --conf-file=gm-from-vcs.conf --local-import --debug
 
