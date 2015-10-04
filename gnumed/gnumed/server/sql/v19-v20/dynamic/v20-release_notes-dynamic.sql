f32ab1e54a1e7807925923309f7291816d951007
Karsten Hilbert <karsten.hilbert@gmx.net>
2015-10-04 15:39:08 +0200

    Prepare 1.5.7 release

--- gnumed/gnumed/server/sql/v19-v20/dynamic/v20-release_notes-dynamic.sql
+++ gnumed/gnumed/server/sql/v19-v20/dynamic/v20-release_notes-dynamic.sql
@@ -17,19 +17,28 @@ INSERT INTO dem.message_inbox (
 ) VALUES (
 	(select pk from dem.staff where db_user = 'any-doc'),
 	(select pk_type from dem.v_inbox_item_type where type = 'memo' and category = 'administrative'),
-	'Release Notes for GNUmed 1.5.6 (database v20.6)',
-	'GNUmed 1.5.6 Release Notes:
+	'Release Notes for GNUmed 1.5.7 (database v20.7)',
+	'GNUmed 1.5.7 Release Notes:
 
-	1.5.6
+	1.5.7
 
-FIX: exception on removing temporary config file [thanks Vaibhav]
-FIX: exception on importing duplicate file into export area
-FIX: exception on merging patients under wxPython 3 [thanks max]
+FIX: one more nonissue-problem tooltip exception in SOAP editor [thanks Marc]
+FIX: encounter change exception on patient change w/ multiple clients [thanks Marc]
+FIX: patient overview tooltip exception on patient change [thanks Marc]
+FIX: mysterious non-problem with missing "Gnumed." in import [thanks Basti]
+FIX: symlink creation on Windows
 
-	20.6
+IMPROVED: logging of payload changes in case of conflict
+IMPROVED: early startup logging
+IMPROVED: show low file location during startup
+IMPROVED: windows startup batch file
+IMPROVED: redirect wxPython log to python logging
+IMPROVED: set wxPython AssertMode appropriately
+
+	20.7
 
 no changes
 ');
 
 -- --------------------------------------------------------------
-select gm.log_script_insertion('v20-release_notes-dynamic.sql', '20.6');
+select gm.log_script_insertion('v20-release_notes-dynamic.sql', '20.7');
