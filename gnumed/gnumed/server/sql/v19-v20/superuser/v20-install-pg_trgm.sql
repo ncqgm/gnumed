853447348aaaa8c5239f0aba27b0e3ee76ca3de3
Karsten Hilbert <karsten.hilbert@gmx.net>
2016-01-11 13:48:11 +0100

    Fix pg_trgm install
    	Relocate from pg_catalog. to pgtrgm.

---
+++ gnumed/gnumed/server/sql/v19-v20/superuser/v20-install-pg_trgm.sql
@@ -0,0 +1,20 @@
+-- ==============================================================
+-- GNUmed database schema change script
+--
+-- License: GPL v2 or later
+-- Author: karsten.hilbert@gmx.net
+--
+-- ==============================================================
+\set ON_ERROR_STOP 1
+--set default_transaction_read_only to off;
+
+-- --------------------------------------------------------------
+drop schema if exists pgtrgm cascade;
+create schema pgtrgm;
+grant usage on schema pgtrgm to "gm-dbo";
+
+drop extension if exists pg_trgm cascade;
+create extension pg_trgm with schema pgtrgm;
+
+-- --------------------------------------------------------------
+select gm.log_script_insertion('v19-install-pg_trgm.sql', '19.12');
