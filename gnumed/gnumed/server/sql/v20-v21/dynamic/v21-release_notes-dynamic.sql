5c766ef9faf375b5e0798c8ac6f5acd1b71c0fa2
Karsten Hilbert <karsten.hilbert@gmx.net>
2016-04-21 15:48:53 +0200

    Prepare 1.6.4 release

--- gnumed/gnumed/server/sql/v20-v21/dynamic/v21-release_notes-dynamic.sql
+++ gnumed/gnumed/server/sql/v20-v21/dynamic/v21-release_notes-dynamic.sql
@@ -17,27 +17,20 @@ INSERT INTO dem.message_inbox (
 ) VALUES (
 	(select pk from dem.staff where db_user = 'any-doc'),
 	(select pk_type from dem.v_inbox_item_type where type = 'memo' and category = 'administrative'),
-	'Release Notes for GNUmed 1.6.3 (database v21.3)',
-	'GNUmed 1.6.3 Release Notes:
+	'Release Notes for GNUmed 1.6.4 (database v21.4)',
+	'GNUmed 1.6.4 Release Notes:
 
-	1.6.3
+	1.6.4
 
-FIX: exception on creating invoice from bill [thanks Marc]
-FIX: faulty assumption on what %()s keys must exist in translations
-FIX: exception in expando SOAP editor when lines need wrapping [thanks Marc]
-FIX: exception on saving progress note under new episode [thanks Marc]
-FIX: exception on deleting list items [thanks Marc]
-FIX: exception on building list context menu [thanks Marc]
-FIX: exception in measurements widget on client idling w/o patient [thanks Marc]
+FIX: EMR journal exporter on Windows [thanks Marc]
 
-IMPROVED: add httplib2 to check-prerequisites.py [thanks Marc]
-IMPROVED: single-line formatting of addresses
-IMPROVED: enhance list context menu to selected rows
+IMPROVED: by-org sort mode in document tree
+IMPROVED: file describer script
+IMPROVED: STIKO tetanus auto hint
+IMPROVED: ES translation [thanks Uwe]
 
-	21.3
-
-FIX: GRANTs on demographics views [thanks Marc]
+NEW: gm-import_incoming script for external use
 ');
 
 -- --------------------------------------------------------------
-select gm.log_script_insertion('v21-release_notes-dynamic.sql', '21.3');
+select gm.log_script_insertion('v21-release_notes-dynamic.sql', '21.4');
