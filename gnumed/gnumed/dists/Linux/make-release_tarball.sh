d161da2e47d28d6037fca7f96b1f20dc24a122c9
Karsten Hilbert <karsten.hilbert@gmx.net>
2016-08-01 23:02:14 +0200

    Prepare 1.6.8/21.8 release

--- gnumed/gnumed/dists/Linux/make-release_tarball.sh
+++ gnumed/gnumed/dists/Linux/make-release_tarball.sh
@@ -3,10 +3,10 @@
 #====================================================
 # license: GPL v2 or later
 #====================================================
-CLIENTREV="1.6.7"
+CLIENTREV="1.6.8"
 CLIENTARCH="gnumed-client.$CLIENTREV.tgz"
 
-SRVREV="21.7"
+SRVREV="21.8"
 SRVARCH="gnumed-server.$SRVREV.tgz"
 
 LANG_LIST="ar bg ca cs da de el en_AU en_CA es fr id it ka nb nl pl pt pt_BR ro ru ru_RU sq sr sv tr uk zh_TW"
--- gnumed/gnumed/server/sql/v20-v21/dynamic/v21-release_notes-dynamic.sql
+++ gnumed/gnumed/server/sql/v20-v21/dynamic/v21-release_notes-dynamic.sql
@@ -17,29 +17,36 @@ INSERT INTO dem.message_inbox (
 ) VALUES (
 	(select pk from dem.staff where db_user = 'any-doc'),
 	(select pk_type from dem.v_inbox_item_type where type = 'memo' and category = 'administrative'),
-	'Release Notes for GNUmed 1.6.7 (database v21.7)',
-	'GNUmed 1.6.7 Release Notes:
+	'Release Notes for GNUmed 1.6.8 (database v21.8)',
+	'GNUmed 1.6.8 Release Notes:
 
-	1.6.7
+	1.6.8
 
-FIX: constrain hospital stay PRW to current patient
-FIX: smoking status detection in dynamic hints
-FIX: GKV checkup auto hint
-FIX: tetanus shot auto hint
-FIX: substance intake discontinuation reason field behaviour
-FIX: exception in clinical calculator with pre-birth test results
+FIX: remove dynamic hint lacking evidence of clinical relevance
+FIX: off-by-one calculation of substance intake end date
+FIX: faulty use of $<gender_mapper>$ in Begleitbrief template
+FIX: fix EMR access deadlock in encounter display widget [thanks Marc]
+FIX: exception on non-ASCII VCF data
+FIX: exception displaying birthday/age of patient w/ estimated DOB
+FIX: list sorting by column header click
 
-IMPROVED: file viewer detection on Windows [thanks John]
-IMPROVED: DICOM studies/series display
-IMPROVED: ZIP-with-DICOMDIR support
-IMPROVED: browse index.html after saving/burning from export area
-IMPROVED: substance abuse management workflow
-IMPROVED: check for tools im gm-describe_file
-IMPROVED: substance intake start/end formatting
+IMPROVED: document tree orgs sort mode tooltips
+IMPROVED: file description shell script
+IMPROVED: less in-your-face default list tooltip
+IMPROVED: update AMTS Medikationsplan to 2.3
+IMPROVED: log file placement
+IMPROVED: form template EA information
+IMPROVED: logging of EMR access locking
+IMPROVED: EMR journal: show applicable dynamic hints
+IMRPOVED: ES translation [thanks Uwe]
+IMPROVED: show comm channels of org units in receiver selection
+IMPROVED: show doc sources as receiver selection candidates
+IMPROVED: letter receiver selection widget layout
+IMPROVED: logging of patient change encounter editing
 
-NEW: a few hints from the German Choosing Wisely initiative
-NEW: CD/DVD sleeve LaTeX template
+NEW: placeholder $ph_cfg::encoding::$
+NEW: blanko AMTS Medikationsplan ~2.3
 ');
 
 -- --------------------------------------------------------------
-select gm.log_script_insertion('v21-release_notes-dynamic.sql', '21.6');
+select gm.log_script_insertion('v21-release_notes-dynamic.sql', '21.8');
