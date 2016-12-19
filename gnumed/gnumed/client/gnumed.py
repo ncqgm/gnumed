91297b9f7c3ad101df65fe64dc51a1a58252cfd7
Karsten Hilbert <karsten.hilbert@gmx.net>
2016-12-19 14:19:37 +0100

    Prepare 1.6.11 release

--- gnumed/gnumed/client/gnumed.py
+++ gnumed/gnumed/client/gnumed.py
@@ -91,7 +91,7 @@ against. Please run GNUmed as a non-root user.
 	sys.exit(1)
 
 #----------------------------------------------------------
-current_client_version = u'1.6.10'
+current_client_version = u'1.6.11'
 current_client_branch = u'1.6'
 
 _log = None
--- gnumed/gnumed/server/sql/v20-v21/dynamic/v21-release_notes-dynamic.sql
+++ gnumed/gnumed/server/sql/v20-v21/dynamic/v21-release_notes-dynamic.sql
@@ -17,25 +17,32 @@ INSERT INTO dem.message_inbox (
 ) VALUES (
 	(select pk from dem.staff where db_user = 'any-doc'),
 	(select pk_type from dem.v_inbox_item_type where type = 'memo' and category = 'administrative'),
-	'Release Notes for GNUmed 1.6.10 (database v21.10)',
-	'GNUmed 1.6.10 Release Notes:
+	'Release Notes for GNUmed 1.6.11 (database v21.11)',
+	'GNUmed 1.6.11 Release Notes:
 
-	1.6.10
+	1.6.11
 
-FIX: more faults with dynamic hint detection
-FIX: exception on verifying substance intake EA
-FIX: failure to download studies from early Orthanc versions
-FIX: failure to create BMP when no allergy check date available
+IMPROVED: edit area refresh on first setting data
+IMPROVED: DB link error logging
+IMPROVED: suppressed hints display in patient overview
+IMPROVED: sorting of Hx items in patient overview
+IMPROVED: use of pdfinfo in gm-describe_file
 
-IMPROVED: LaTeX formatting of current medications
+FIX: stall of gm-create_datamatrix in swap storm
+FIX: BMP creation without substance intakes
+FIX: missing quotes in BMP datafile [thanks Moritz]
+FIX: failure to sometimes store progress notes [thanks Marc]
+FIX: exception on double-clicking document tree label node
+FIX: exception on switching to drug database frontend [thanks a sk_SK]
+FIX: exception on saving hospital stay [thanks a sk_SK]
+FIX: exception on checking for upgrade [thanks Philipp]
 
-NEW: placeholders $<bill_adr_*>$ for accessing the address of a bill
-NEW: --wxp=2|3 command line option
+	21.11
 
-	21.10
+IMPROVED: backup scripts error checking
 
-FIX: clin.get_hints_for_patient()
+FIX: serialization failures due to table mod announcement triggers
 ');
 
 -- --------------------------------------------------------------
-select gm.log_script_insertion('v21-release_notes-dynamic.sql', '21.10');
+select gm.log_script_insertion('v21-release_notes-dynamic.sql', '21.11');
