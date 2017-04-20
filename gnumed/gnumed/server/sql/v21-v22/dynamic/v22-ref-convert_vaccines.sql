53e2834455ae4a01217691daad56594f380b21ce
Karsten Hilbert <karsten.hilbert@gmx.net>
2017-04-20 21:02:47 +0200

    Fix conversion of old generic vaccinations
    	Vaccinations are to be relinked, not the generic vaccine updated.

--- gnumed/gnumed/server/sql/v21-v22/dynamic/v22-ref-convert_vaccines.sql
+++ gnumed/gnumed/server/sql/v21-v22/dynamic/v22-ref-convert_vaccines.sql
@@ -27,47 +27,55 @@ create function staging.v22_convert_vaccines()
 	language 'plpgsql'
 	as '
 DECLARE
-	_old_vacc_prod_row record;
-	_old_vacc_ind integer;
-	_new_vacc_dose integer;
+	_row_old_vacc_prod record;
+	_pk_old_vacc_prod integer;
+	_pk_old_vacc_indication integer;
+	_pk_old_vaccine integer;
+	_old_is_live boolean;
+
+	_pk_new_vacc_prod integer;
+	_pk_new_vaccine integer;
+	_new_is_live boolean;
+	_pk_new_vacc_dose integer;
 	_new_vacc_doses integer[];
-	_new_vacc_prod integer;
+
 BEGIN
 
 	-- loop over old-style generic vaccines
-	FOR _old_vacc_prod_row IN
+	FOR _row_old_vacc_prod IN
 		SELECT * FROM ref.v_drug_products
 		WHERE
 			product ILIKE ''% - generic vaccine''
 	LOOP
+		_pk_old_vacc_prod := _row_old_vacc_prod.pk_drug_product;
 		-- get list of linked indications
-		RAISE NOTICE ''converting old-style generic vaccine [%]'', _old_vacc_prod_row;
+		RAISE NOTICE ''converting old-style generic vaccine [%]'', _row_old_vacc_prod;
 		_new_vacc_doses := ARRAY[]::integer[];
 		-- loop over indications of this vaccine
-		FOR _old_vacc_ind IN
+		FOR _pk_old_vacc_indication IN
 			SELECT fk_indication FROM ref.lnk_vaccine2inds WHERE
 				fk_vaccine = (
-					SELECT pk FROM ref.vaccine WHERE fk_drug_product = _old_vacc_prod_row.pk_drug_product
+					SELECT pk FROM ref.vaccine WHERE fk_drug_product = _pk_old_vacc_prod
 				)
 		LOOP
 			-- translate indication to substance dose
-			SELECT fk_dose INTO STRICT _new_vacc_dose
+			SELECT fk_dose INTO STRICT _pk_new_vacc_dose
 			FROM staging.lnk_vacc_ind2subst_dose
 			WHERE
-				fk_indication = _old_vacc_ind
+				fk_indication = _pk_old_vacc_indication
 					AND
 				is_live = (
 					SELECT is_live FROM ref.vaccine
-					WHERE fk_drug_product = _old_vacc_prod_row.pk_drug_product
+					WHERE fk_drug_product = _pk_old_vacc_prod
 				)
 			;
-			RAISE NOTICE ''mapping ind [%] to dose [%]'', _old_vacc_ind, _new_vacc_dose;
-			_new_vacc_doses := _new_vacc_doses ||_new_vacc_dose;
+			RAISE NOTICE ''mapping ind [%] to dose [%]'', _pk_old_vacc_indication, _pk_new_vacc_dose;
+			_new_vacc_doses := _new_vacc_doses ||_pk_new_vacc_dose;
 		END LOOP;
 		RAISE NOTICE ''found doses: %'', _new_vacc_doses;
 
 		-- find new vaccine drug product with same list of substance doses
-		SELECT fk_drug_product INTO STRICT _new_vacc_prod FROM (
+		SELECT fk_drug_product INTO STRICT _pk_new_vacc_prod FROM (
 			SELECT fk_drug_product, doses, preparation, is_fake_product, product FROM (
 				select fk_drug_product, array_agg(fk_dose) AS doses from ref.lnk_dose2drug group by fk_drug_product
 			) as agged_products
@@ -87,23 +95,43 @@ BEGIN
 			doses = _new_vacc_doses
 		;
 
-		-- update ref.vaccine to point to the new fk_drug_product
-		RAISE NOTICE ''updating vaccine with fk_drug_product=% to point to fk_drug_product=%'', _old_vacc_prod_row.pk_drug_product, _new_vacc_prod;
-		UPDATE ref.vaccine SET
-			fk_drug_product = _new_vacc_prod
+		SELECT pk, is_live INTO STRICT _pk_old_vaccine, _old_is_live
+		FROM ref.vaccine WHERE fk_drug_product = _pk_old_vacc_prod;
+
+		SELECT pk, is_live INTO STRICT _pk_new_vaccine, _new_is_live
+		FROM ref.vaccine WHERE fk_drug_product = _pk_new_vacc_prod;
+
+		RAISE NOTICE ''updating clin.vaccination with fk_vaccine=% to point to fk_vaccine=%'', _pk_old_vaccine, _pk_new_vaccine;
+
+		IF _old_is_live IS DISTINCT FROM _new_is_live THEN
+			RAISE EXCEPTION ''<live> status of old and new vaccine are different'';
+			RETURN FALSE;
+		END IF;
+
+		UPDATE clin.vaccination SET
+			fk_vaccine = _pk_new_vaccine
 		WHERE
-			fk_drug_product = _old_vacc_prod_row.pk_drug_product
+			fk_vaccine = _pk_old_vaccine
 		;
 
+--		-- update ref.vaccine to point to the new fk_drug_product
+--		RAISE NOTICE ''updating vaccine with fk_drug_product=% to point to fk_drug_product=%'', _pk_old_vacc_prod, _pk_new_vacc_prod;
+--		UPDATE ref.vaccine SET
+--			fk_drug_product = _pk_new_vacc_prod
+--		WHERE
+--			fk_drug_product = _pk_old_vacc_prod
+--		;
+
 		-- delete old vaccine product
-		DELETE FROM ref.drug_product
-		WHERE pk = _old_vacc_prod_row.pk_drug_product;
+		RAISE NOTICE ''deleting old-style generic vaccine product %'', _pk_old_vacc_prod;
+		DELETE FROM ref.drug_product WHERE pk = _pk_old_vacc_prod;
+		DELETE FROM ref.vaccine WHERE pk = _pk_old_vaccine;
 
 	END LOOP;
 
 
 	-- loop over real, user-entered or previously bootstrapped vaccines
-	FOR _old_vacc_prod_row IN
+	FOR _row_old_vacc_prod IN
 		SELECT * FROM ref.v_drug_products r_vdp WHERE
 			(
 				(array_length(r_vdp.components, 1) = 0)
@@ -113,45 +141,46 @@ BEGIN
 				SELECT 1 FROM ref.vaccine r_v WHERE r_v.fk_drug_product = r_vdp.pk_drug_product
 			)
 	LOOP
+		_pk_old_vacc_prod := _row_old_vacc_prod.pk_drug_product;
 		-- get list of linked indications
-		RAISE NOTICE ''converting old-style real vaccine [%]'', _old_vacc_prod_row;
+		RAISE NOTICE ''converting old-style real vaccine [%]'', _row_old_vacc_prod;
 		-- loop over indications of this vaccine
-		FOR _old_vacc_ind IN
+		FOR _pk_old_vacc_indication IN
 			SELECT fk_indication FROM ref.lnk_vaccine2inds WHERE
 				fk_vaccine = (
-					SELECT pk FROM ref.vaccine WHERE fk_drug_product = _old_vacc_prod_row.pk_drug_product
+					SELECT pk FROM ref.vaccine WHERE fk_drug_product = _pk_old_vacc_prod
 				)
 		LOOP
 			-- translate indication to substance dose
-			SELECT fk_dose INTO STRICT _new_vacc_dose
+			SELECT fk_dose INTO STRICT _pk_new_vacc_dose
 			FROM staging.lnk_vacc_ind2subst_dose
 			WHERE
-				fk_indication = _old_vacc_ind
+				fk_indication = _pk_old_vacc_indication
 					AND
 				is_live = (
 					SELECT is_live FROM ref.vaccine
-					WHERE fk_drug_product = _old_vacc_prod_row.pk_drug_product
+					WHERE fk_drug_product = _pk_old_vacc_prod
 				)
 			;
-			RAISE NOTICE ''mapping ind [%] to dose [%]'', _old_vacc_ind, _new_vacc_dose;
+			RAISE NOTICE ''mapping ind [%] to dose [%]'', _pk_old_vacc_indication, _pk_new_vacc_dose;
 			-- link dose to old-style vaccine product as component
 			INSERT INTO ref.lnk_dose2drug (fk_drug_product, fk_dose) (
 				SELECT
-					_old_vacc_prod_row.pk_drug_product,
-					_new_vacc_dose
+					_pk_old_vacc_prod,
+					_pk_new_vacc_dose
 				-- better safe than sorry ...
 				WHERE NOT EXISTS (
 					SELECT 1 FROM ref.lnk_dose2drug WHERE
-						fk_drug_product = _old_vacc_prod_row.pk_drug_product
+						fk_drug_product = _pk_old_vacc_prod
 							AND
-						fk_dose = _new_vacc_dose
+						fk_dose = _pk_new_vacc_dose
 				)
 			);
 		END LOOP;
 		-- drop all indication links for this vaccine
 		DELETE FROM ref.lnk_vaccine2inds WHERE
 			fk_vaccine = (
-				SELECT pk FROM ref.vaccine WHERE fk_drug_product = _old_vacc_prod_row.pk_drug_product
+				SELECT pk FROM ref.vaccine WHERE fk_drug_product = _pk_old_vacc_prod
 			)
 		;
 	END LOOP;
