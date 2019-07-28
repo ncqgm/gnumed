052933156844e76211eac5d80d6c157f4f509641
Karsten Hilbert <karsten.hilbert@gmx.net>
2019-07-28 17:33:35 +0200

    Update release notes in inbox to 1.7.6

--- gnumed/gnumed/server/sql/v21-v22/fixups/v22-release_notes-fixup.sql
+++ gnumed/gnumed/server/sql/v21-v22/fixups/v22-release_notes-fixup.sql
@@ -17,41 +17,25 @@ INSERT INTO dem.message_inbox (
 ) VALUES (
 	(select pk from dem.staff where db_user = 'any-doc'),
 	(select pk_type from dem.v_inbox_item_type where type = 'memo' and category = 'administrative'),
-	'Release Notes for GNUmed 1.7.5 (database v22.5)',
-	'GNUmed 1.7.5 Release Notes:
+	'Release Notes for GNUmed 1.7.6 (database v22.6)',
+	'GNUmed 1.7.6 Release Notes:
 
-	1.7.5
+	1.7.6
 
-FIX: exception on following vaccs reminder from provider inbox
+FIX: application metadata files
+FIX: searching across all EMRs
+FIX: constrain document PRW to current patient
+FIX: lab/table: exception on double-clicking empty cell in row w/o meta test type
+FIX: lab/result EA: failure to show test type on edit
 
-	1.7.4
+NEW: stub out $praxis_scan2pay$
 
-NEW: placeholders now nest with $2<>2$ $3<>3$ rather than $<<>>$ $<<<>>>$
+	22.6
 
-NEW: placeholder $<patient_mcf>$
-NEW: placeholder $<praxis_mcf>$
-NEW: placehodler $<qrcode>$
-NEW: placeholder $<if_debugging>$
-NEW: LaTeX letter template example
-NEW: Begleitbrief mit Diagnosen (LaTeX)
+FIX: properly include fixups in v21-v22 upgrade
 
-FIX: map None to '' in address parts placeholder
-FIX: export area export-to-media
-FIX: $<vaccination_history::%(l10n_indications)s::>$ field
-FIX: vaccine creation
-FIX: error in closing expired episodes
-FIX: date formatting in document tree
-
-IMPROVED: AppStream and desktop metadata
-IMPROVED: add "preset" option to $<free_text>$ placeholder
-IMPROVED: include MCF in export area metadata
-IMPROVED: Begleitbrief template
-
-	22.4
-
-FIX: LaTeX-Template for Begleitbrief
-FIX: 2nd/3rd level placeholders in LaTeX templates
+IMPROVED: database backup script
 ');
 
 -- --------------------------------------------------------------
-select gm.log_script_insertion('v22-release_notes-fixup.sql', '22.4');
+select gm.log_script_insertion('v22-release_notes-fixup.sql', '22.6');
