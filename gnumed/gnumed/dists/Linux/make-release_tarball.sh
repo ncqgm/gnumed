01911175194102eccc38ad0d9fb5fd04d1a9d1b4
Karsten Hilbert <karsten.hilbert@gmx.net>
2019-11-10 23:07:40 +0100

    Adjust server tarball v22 name to show py3 dependency

--- gnumed/gnumed/dists/Linux/make-release_tarball.sh
+++ gnumed/gnumed/dists/Linux/make-release_tarball.sh
@@ -6,7 +6,7 @@
 CLIENTREV="1.8.0rc1"
 CLIENTARCH="gnumed-client.$CLIENTREV.tgz"
 
-SRVREV="22.8"
+SRVREV="22.8.py3"
 SRVARCH="gnumed-server.$SRVREV.tgz"
 
 LANG_LIST="ar bg ca cs da de el en_AU en_CA es fr id it ka nb nl pl pt pt_BR ro ru ru_RU sq sr sv tr uk zh_TW"
