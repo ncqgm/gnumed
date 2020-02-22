e68c440f9121f873d0fef126115f40c7b6156343
Karsten Hilbert <karsten.hilbert@gmx.net>
2020-02-22 15:27:39 +0100

    Prepare 1.8.0rc3 release

--- gnumed/gnumed/dists/Linux/make-release_tarball.sh
+++ gnumed/gnumed/dists/Linux/make-release_tarball.sh
@@ -3,10 +3,10 @@
 #====================================================
 # license: GPL v2 or later
 #====================================================
-CLIENTREV="1.8.0rc2"
+CLIENTREV="1.8.0rc3"
 CLIENTARCH="gnumed-client.$CLIENTREV.tgz"
 
-SRVREV="22.9.py3.rc2"
+SRVREV="22.10.py3"
 SRVARCH="gnumed-server.$SRVREV.tgz"
 
 LANG_LIST="ar bg ca cs da de el en_AU en_CA es fr id it ka nb nl pl pt pt_BR ro ru ru_RU sq sr sv tr uk zh_TW"
--- gnumed/gnumed/server/sql/v21-v22/fixups/v22-release_notes-fixup.sql
+++ gnumed/gnumed/server/sql/v21-v22/fixups/v22-release_notes-fixup.sql
@@ -17,15 +17,14 @@ INSERT INTO dem.message_inbox (
 ) VALUES (
 	(select pk from dem.staff where db_user = 'any-doc'),
 	(select pk_type from dem.v_inbox_item_type where type = 'memo' and category = 'administrative'),
-	'Release Notes for GNUmed 1.8.0rc1 (database v22.8)',
-	'GNUmed 1.8.0rc1 Release Notes:
+	'Release Notes for GNUmed 1.8.0rc3 (database v22.10.py3)',
+	'GNUmed 1.8.0rc3 Release Notes:
 
 	1.8.0
 
 NEW: port to wxPython 4 (wxPhoenix)
 NEW: port to Python 3
 NEW: port bootstrapper to Python 3
-
 NEW: EMR tree: toggle episode status from context menu
 NEW: EMR tree: show/edit clinical items from below encounters
 NEW: ReST formatting in $free_text::::$ placeholder
@@ -41,6 +40,9 @@ NEW: status bar history/visual bell
 NEW: dicomize images/PDF into DICOM study
 NEW: [Abort] client from exception dialog
 NEW: edit clinical item from EMR list journal
+NEW: dist: add PortableApp XML skeleton
+NEW: placeholder: $most_recent_test_results$
+NEW: tool: check_mimetypes_in_archive
 
 IMPROVED: symbolic link creation on Windows
 IMPROVED: Orthanc connection handling
@@ -66,21 +68,19 @@ FIX: [Save] functionality of Export Area
 FIX: document tree sorting / document insertion
 FIX: inability to delete inbox message w/o receiver
 FIX: "lastname, firstname" based patient search under Python 3
+FIX: billing: invoice ID generation [thanks Marc]
+FIX: export area: saving document part entries
+FIX: lab: grid display row tooltips
+FIX: lists: context menu CSV export
+FIX: EMR/tree: selection of pseudo issue node
+FIX: documents/new: error handling of unreadable parts
+FIX: PG access: rewrite connection pool
+FIX: y2038 exception in DST detection
 
+	22.10
 
-	22.8
-
-FIX: i18n.set_curr/force_curr_lang()
-
-	22.7
-
-IMPROVED: lab results plotting scripts for gnuplot
-IMPROVED: bills tables grants for invoice ID generation
-
-NEW: multi-results plotting script for gnuplot
-
-FIX: clin.v_candidate_diagnoses: missing coalesce()
+IMPROVED: database fixup script
 ');
 
 -- --------------------------------------------------------------
-select gm.log_script_insertion('v22-release_notes-fixup.sql', '22.8');
+select gm.log_script_insertion('v22-release_notes-fixup.sql', '22.10.py3');
