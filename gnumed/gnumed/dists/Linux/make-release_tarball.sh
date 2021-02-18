ec2d4679889debb875aa3814006537635232668f
Karsten Hilbert <karsten.hilbert@gmx.net>
2021-02-18 12:07:43 +0100

    Add support for timestamp-versioned releases
    (cherry picked from commit 3e0c5a433479a47018acc224c7cea500f2dd442a)

--- gnumed/gnumed/dists/Linux/make-release_tarball.sh
+++ gnumed/gnumed/dists/Linux/make-release_tarball.sh
@@ -3,10 +3,13 @@
 #====================================================
 # license: GPL v2 or later
 #====================================================
-CLIENTREV="1.9.0rc1"
+TS=""
+#TS=".$(date +%Y%m%d-%H%M%S)"
+CLIENTREV="1.9.0rc1${TS}"
 CLIENTARCH="gnumed-client.$CLIENTREV.tgz"
 
-SRVREV="23.0rc1"
+SRVREV="22.15"
+SRVREV="23.0rc1${TS}"
 SRVARCH="gnumed-server.$SRVREV.tgz"
 
 LANG_LIST="ar bg ca cs da de el en_AU en_CA es fr id it ka nb nl pl pt pt_BR ro ru ru_RU sq sr sv tr uk zh_TW"
