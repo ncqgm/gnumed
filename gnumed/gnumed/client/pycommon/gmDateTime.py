58b5d0cc684946367e2a99c79f8248ad007607ff
Karsten Hilbert <karsten.hilbert@gmx.net>
2021-03-22 12:44:37 +0100

    Fix interval formatting
    	GNUmed someplaces wrongly assumed a Gregorian year to
    	usually last 364 rather than 365 days. Brown paper bag.
    	Also properly take into account leap days. They must be
    	_subtracted_ from days left over after divmod()ing out
    	years, as they are attributable to passed leap years and
    	do not count towards weeks/days over and above the passed
    	years in total.
    
    (cherry picked from commit 692138aaeabe8e86503431888dae930b75d07eae)

--- gnumed/gnumed/client/pycommon/gmDateTime.py
+++ gnumed/gnumed/client/pycommon/gmDateTime.py
@@ -559,10 +559,11 @@ def format_interval_medically(interval=None):
 	This isn't mathematically correct but close enough for display.
 	"""
 	# more than 1 year ?
-	if interval.days > 363:
-		years, days = divmod(interval.days, 364)
+	if interval.days > 364:
+		years, days = divmod(interval.days, avg_days_per_gregorian_year)
 		leap_days, tmp = divmod(years, 4)
-		months, day = divmod((days + leap_days), 30.33)
+		days_left_without_leap_days = days - leap_days
+		months, day = divmod((days_left_without_leap_days), 30.33)
 		if int(months) == 0:
 			return "%s%s" % (int(years), _('interval_format_tag::years::y')[-1:])
 		return "%s%s %s%s" % (int(years), _('interval_format_tag::years::y')[-1:], int(months), _('interval_format_tag::months::m')[-1:])
@@ -2146,10 +2147,9 @@ if __name__ == '__main__':
 			pyDT.timedelta(days = 366),
 			pyDT.timedelta(days = 367),
 			pyDT.timedelta(days = 400),
-			pyDT.timedelta(weeks = 52 * 30),
-			pyDT.timedelta(weeks = 52 * 79, days = 33)
+			pyDT.timedelta(weeks = 53 * 30),
+			pyDT.timedelta(weeks = 53 * 79, days = 33)
 		]
-
 		idx = 1
 		for intv in intervals:
 			print ('%s) %s -> %s' % (idx, intv, format_interval_medically(intv)))
@@ -2341,8 +2341,8 @@ if __name__ == '__main__':
 	#test_cFuzzyTimeStamp()
 	#test_get_pydt()
 	#test_str2interval()
-	test_format_interval()
-	#test_format_interval_medically()
+	#test_format_interval()
+	test_format_interval_medically()
 	#test_str2pydt()
 	#test_pydt_strftime()
 	#test_calculate_apparent_age()
