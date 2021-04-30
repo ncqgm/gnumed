ncqgm/gnumed

commit 49992d3da5397437a8302de2a84aa00feff8efcb

Improve prevention of password leaks via log file
	GNUmed now filters log file emissions at the
	io.TextIOWrapper.write() level well below the logging
	machinery thereby allowing for obfuscation of sensitive
	strings. Such strings (as in passwords) are marked for
	replacement from client code as soon as entered by the
	user. That way, there's nearly no chance of inadvertantly
	spilling credentials into the log file, not even when
	generically logging a stack trace during exception
	handling.

	A known-plaintext attack is possible by fuzzing arbitrary
	strings into the log until one becomes obfuscated which
	will then correspond to a sensitive string. This attack
	will work even more easily with trivial passwords that
	are likely to happen in log output anyway, such as when
	the password equals the user name. To make automated log
	file analysis slightly more difficult the actual
	replacement for a sensitive string is randomly selected
	whenever it happens to pass through the obfuscator.

gnumed/gnumed/client/pycommon/gmLog2.py
@@ -51,6 +51,9 @@ import io
 import codecs
 import locale
 import datetime as pydt
+import random
+import time
+import calendar
 
 
 _logfile_name = None
@@ -242,6 +245,31 @@ def set_string_encoding(encoding=None):
 #===============================================================
 # internal API
 #===============================================================
+__words2hide = []
+
+def add_word2hide(word):
+	if word not in __words2hide:
+		__words2hide.append(word)
+
+#---------------------------------------------------------------
+__original_logger_write_func = None
+
+def __safe_logger_write_func(s):
+	for word in __words2hide:
+		# random is seeded from system time at import,
+		# jump ahead, scrambled by whatever is "now"
+		random.jumpahead(calendar.timegm(time.gmtime()))
+		# from that generate a replacement string valid for
+		# *this* round of replacements of *this* word,
+		# this approach won't mitigate guessing trivial passwords
+		# from replacements of known data (a known-plaintext attack)
+		# but will make automated searching for replaced strings
+		# in the log more difficult
+		bummer = hex(random.randint(0, sys.maxint)).lstrip(u'0x')
+		s = s.replace(word, bummer)
+	__original_logger_write_func(s)
+
+#---------------------------------------------------------------
 def __setup_logging():
 
 	set_string_encoding()
@@ -260,6 +288,10 @@ def __setup_logging():
 
 	_logfile = io.open(_logfile_name, mode = 'wt', encoding = 'utf8', errors = 'replace')
 
+	global __original_logger_write_func
+	__original_logger_write_func = _logfile.write
+	_logfile.write = __safe_logger_write_func
+
 	logging.basicConfig (
 		format = fmt,
 		datefmt = '%Y-%m-%d %H:%M:%S',
@@ -267,12 +299,15 @@ def __setup_logging():
 		stream = _logfile
 	)
 
+	logging.captureWarnings(True)
+
 	logger = logging.getLogger('gm.logging')
 	logger.critical(u'-------- start of logging ------------------------------')
 	logger.info(u'log file is <%s>', _logfile_name)
 	logger.info(u'log level is [%s]', logging.getLevelName(logger.getEffectiveLevel()))
 	logger.info(u'log file encoding is <utf8>')
 	logger.info(u'initial python.str -> python.unicode encoding is <%s>', _string_encoding)
+	logger.debug(u'log file .write() patched from original %s to patched %s', __original_logger_write_func, __safe_logger_write_func)
 
 #---------------------------------------------------------------
 def __get_logfile_name():
@@ -319,10 +354,19 @@ __setup_logging()
 
 if __name__ == '__main__':
 
+	if len(sys.argv) < 2:
+		sys.exit()
+
+	if sys.argv[1] != u'test':
+		sys.exit()
+
 	#-----------------------------------------------------------
 	def test():
 		logger = logging.getLogger('gmLog2.test')
 		logger.error("I expected to see %s::test()" % __file__)
+		add_word2hide(u'super secret passphrase')
+		logger.debug('credentials: super secret passphrase')
+
 		try:
 			int(None)
 		except:
@@ -330,6 +374,4 @@ if __name__ == '__main__':
 			log_stack_trace()
 		flush()
 	#-----------------------------------------------------------
-	if len(sys.argv) > 1 and sys.argv[1] == u'test':
-		test()
-#===============================================================
+	test()

gnumed/gnumed/client/pycommon/gmPG2.py
@@ -437,6 +437,7 @@ def __request_login_params_tui():
 		login.user = prompted_input(prompt = "user name", default = '')
 		tmp = 'password for "%s" (not shown): ' % login.user
 		login.password = getpass.getpass(tmp)
+		gmLog2.add_word2hide(login.password)
 		login.port = prompted_input(prompt = "port", default = 5432)
 	except KeyboardInterrupt:
 		_log.warning("user cancelled text mode login dialog")
@@ -468,6 +469,8 @@ def __request_login_params_gui_wx():
 	#if user cancelled or something else went wrong, raise an exception
 	if login is None:
 		raise gmExceptions.ConnectionError(_("Can't connect to database without login information!"))
+
+	gmLog2.add_word2hide(login.password)
 
 	return login
 

gnumed/gnumed/client/wxpython/gmAuthWidgets.py
@@ -29,6 +29,7 @@ from Gnumed.pycommon import gmBackendListener
 from Gnumed.pycommon import gmTools
 from Gnumed.pycommon import gmCfg2
 from Gnumed.pycommon import gmI18N
+from Gnumed.pycommon import gmLog2
 
 from Gnumed.business import gmPraxis
 
@@ -134,6 +135,8 @@ def connect_to_database(max_attempts=3, expected_version=None, require_version=T
 		if login is None:
 			_log.info("user cancelled login dialog")
 			break
+
+		gmLog2.add_word2hide(login.password)
 
 		# try getting a connection to verify the DSN works
 		dsn = gmPG2.make_psycopg2_dsn (
@@ -287,6 +290,7 @@ def connect_to_database(max_attempts=3, expected_version=None, require_version=T
 	dlg.Destroy()
 
 	return connected
+
 #================================================================
 def get_dbowner_connection(procedure=None, dbo_password=None, dbo_account=u'gm-dbo'):
 	if procedure is None:
@@ -309,6 +313,8 @@ Please enter the current password for <%s>:""") % (
 		)
 		if dbo_password == '':
 			return None
+
+	gmLog2.add_word2hide(dbo_password)
 
 	# 2) connect as gm-dbo
 	login = gmPG2.get_default_login()
@@ -336,6 +342,7 @@ Please enter the current password for <%s>:""") % (
 		return None
 
 	return conn
+
 #================================================================
 def change_gmdbowner_password():
 
@@ -364,6 +371,8 @@ def change_gmdbowner_password():
 	if dbo_pwd_new_1.strip() == u'':
 		return False
 
+	gmLog2.add_word2hide(dbo_pwd_new_1)
+
 	dbo_pwd_new_2 = wx.GetPasswordFromUser (
 		message = _(u"""Enter the NEW password for the GNUmed database owner, again.
 
@@ -377,6 +386,49 @@ def change_gmdbowner_password():
 	if dbo_pwd_new_1 != dbo_pwd_new_2:
 		return False
 
+	# pwd2 == pwd1 at this point so no need to hide (again)
+
+	"""	On Mon, Mar 13, 2017 at 12:19:22PM -0400, Tom Lane wrote:
+		> Date: Mon, 13 Mar 2017 12:19:22 -0400
+		> From: Tom Lane <tgl@sss.pgh.pa.us>
+		> To: Adrian Klaver <adrian.klaver@aklaver.com>
+		> cc: Schmid Andreas <Andreas.Schmid@bd.so.ch>,
+		>  "'pgsql-general@postgresql.org'" <pgsql-general@postgresql.org>
+		> Subject: Re: [GENERAL] createuser: How to specify a database to connect to
+		> 
+		> Adrian Klaver <adrian.klaver@aklaver.com> writes:
+		> > On 03/13/2017 08:52 AM, Tom Lane wrote:
+		> >> If by "history" you're worried about the server-side statement log, this
+		> >> is merest fantasy: the createuser program is not magic, it just constructs
+		> >> and sends a CREATE USER command for you.  You'd actually be more secure
+		> >> using psql, where (if you're superuser) you could shut off log_statement
+		> >> for your session first.
+		> 
+		> > There is a difference though:
+		> 
+		> > psql> CREATE USER:
+		> 
+		> > postgres-2017-03-13 09:03:27.147 PDT-0LOG:  statement: create user 
+		> > dummy_user with login password '1234';
+		> 
+		> Well, what you're supposed to do is
+		> 
+		> postgres=# create user dummy_user;
+		> postgres=# \password dummy_user
+		> Enter new password: 
+		> Enter it again: 
+		> postgres=# 
+		> 
+		> which will result in sending something like
+		> 
+		> ALTER USER dummy_user PASSWORD 'md5c5e9567bc40082671d02c654260e0e09'
+		> 
+		> You can additionally protect that by wrapping it into one transaction
+		> (if you have a setup where the momentary existence of the role without a
+		> password would be problematic) and/or shutting off logging beforehand.
+	"""
+
+	# this REALLY should be prefixed with md5 and the md5sum sent rather than the pwd
 	cmd = u"""ALTER ROLE "%s" ENCRYPTED PASSWORD '%s';""" % (
 		dbo_account,
 		dbo_pwd_new_2
@@ -384,9 +436,11 @@ def change_gmdbowner_password():
 	gmPG2.run_rw_queries(link_obj = dbo_conn, queries = [{'cmd': cmd}], end_tx = True)
 
 	return True
+
 #================================================================
 class cBackendProfile:
 	pass
+
 #================================================================
 class cLoginDialog(wx.Dialog):
 	"""cLoginDialog - window holding cLoginPanel"""
@@ -398,6 +452,7 @@ class cLoginDialog(wx.Dialog):
 		self.Centre()
 
 		self.SetIcon(gmTools.get_icon(wx = wx))
+
 #================================================================
 class cLoginPanel(wx.Panel):
 	"""GUI panel class that interactively gets Postgres login parameters.

commit 62966a99a80fa6390bb3db1753ddc0e69d577faf

Fix overzealous string obfuscation in logger
	Commit 49992d3da5397437a8302de2a84aa00feff8efcb
	introduced sanitizing log strings but failed to not
	obfuscate empty strings which leads to all kinds of
	funny log entries.

	Backport fix from master.

gnumed/gnumed/client/pycommon/gmLog2.py
@@ -255,6 +255,10 @@ def set_string_encoding(encoding=None):
 __words2hide = []
 
 def add_word2hide(word):
+	if word is None:
+		return
+	if word.strip() == u'':
+		return
 	if word not in __words2hide:
 		__words2hide.append(word)
 