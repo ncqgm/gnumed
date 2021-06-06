05b83f2d3190e32ef833047732ef66c7f73d8a80
Karsten Hilbert <karsten.hilbert@gmx.net>
2021-06-06 23:28:59 +0200

    Improve previous result display in measurements EA

--- gnumed/gnumed/client/wxpython/gmMeasurementWidgets.py
+++ gnumed/gnumed/client/wxpython/gmMeasurementWidgets.py
@@ -3396,13 +3396,13 @@ class cMeasurementEditAreaPnl(wxgMeasurementEditAreaPnl.wxgMeasurementEditAreaPn
 			return
 
 		most_recent = most_recent_results[0]
-		self._TCTRL_previous_value.SetValue(_('%s ago: %s%s%s - %s%s') % (
+		self._TCTRL_previous_value.SetValue(_('%s ago: %s%s%s (%s%s)') % (
 			gmDateTime.format_interval_medically(gmDateTime.pydt_now_here() - most_recent['clin_when']),
 			most_recent['unified_val'],
 			gmTools.coalesce(most_recent['val_unit'], ''),
-			gmTools.coalesce(most_recent['abnormality_indicator'], '', ' (%s)'),
+			gmTools.coalesce(most_recent['abnormality_indicator'], '', ' "%s"'),
 			most_recent['abbrev_tt'],
-			gmTools.coalesce(most_recent.formatted_range, '', ' [%s]')
+			gmTools.coalesce(most_recent.formatted_range, '', ': %s')
 		))
 		self._TCTRL_previous_value.SetToolTip(most_recent.format (
 			with_review = True,
