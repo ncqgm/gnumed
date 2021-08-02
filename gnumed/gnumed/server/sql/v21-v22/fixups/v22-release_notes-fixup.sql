225ab19efbb5ce3d0931a58fb626dcd28818eeae
Karsten Hilbert <karsten.hilbert@gmx.net>
2021-08-02 13:16:25 +0200

    Prepare 1.8.6 Maintenance Release

--- gnumed/gnumed/server/sql/v21-v22/fixups/v22-release_notes-fixup.sql
+++ gnumed/gnumed/server/sql/v21-v22/fixups/v22-release_notes-fixup.sql
@@ -17,22 +17,38 @@ INSERT INTO dem.message_inbox (
 ) VALUES (
 	(select pk from dem.staff where db_user = 'any-doc'),
 	(select pk_type from dem.v_inbox_item_type where type = 'memo' and category = 'administrative'),
-	'Release Notes for GNUmed 1.8.5 (database v22.15)',
-	'GNUmed 1.8.5 Release Notes:
+	'Release Notes for GNUmed 1.8.6 (database v22.16)',
+	'GNUmed 1.8.6 Release Notes:
 
-	1.8.5
+	1.8.6
 
-FIX: image conversion w/o target extension [thanks ...]
-FIX: SVG icon [thanks freddii]
-FIX: lab: tab "most-recent": [x]ing "show missing" throws exception
-FIX: log: exception on inaccessible attributes
+FIX: unlocking encounters (missing import)
+FIX: row locking code
+FIX: metadata: screenshots URL in appdata.xml
+FIX: GUI: screenshot function [thanks The-o]
+FIX: medical interval formatting [thanks brulefa]
+FIX: date/time input: time parsing
+FIX: date/time input: function key to weekday mapping
+FIX: encounters: do not crash if current encounter is deleted
+FIX: paperwork: fix emr_journal placeholder LaTeX escaping [thanks Marc]
 
-NEW: add CoVid-2019 vaccines
+IMPROVED: desktop entry file [thanks freddii]
+IMPROVED: patient switch: encounter checking
+IMPROVED: pat overview: problems display
+IMPROVED: DB: connection loss detection
+IMPROVED: measurement EA: previous result formatting
+IMPROVED: DB: login problem logging
+IMPROVED: top pane: show age of results in tooltip
+IMPROVED: paperwork: LaTeX formatting of text
+IMPROVED: EMR: extend search to select demographics [thanks brulefa]
+IMPROVED: startup: bash code [thanks shellcheck]
 
-	22.15
+	22.16
 
-FIX: message inbox view
+IMPROVED: bootstrapper: check disk space
+
+FIX: check encounter lock in clin.remove_old_empty_encounters()
 ');
 
 -- --------------------------------------------------------------
-select gm.log_script_insertion('v22-release_notes-fixup.sql', '22.15@1.8.5');
+select gm.log_script_insertion('v22-release_notes-fixup.sql', '22.16@1.8.6');
