b9152168116659547ec71004e48c7acd60402bd0
Karsten Hilbert <karsten.hilbert@gmx.net>
2022-05-01 18:14:26 +0200

    Drop ancient config table

---
+++ gnumed/gnumed/server/sql/v22-v23/dynamic/v23-cfg-drop_old.sql
@@ -0,0 +1,16 @@
+-- ==============================================================
+-- GNUmed database schema change script
+--
+-- License: GPL v2 or later
+-- Author: Karsten Hilbert
+-- 
+-- ==============================================================
+\set ON_ERROR_STOP 1
+set default_transaction_read_only to off;
+set check_function_bodies to on;
+
+-- --------------------------------------------------------------
+drop table if exists cfg.config cascade;
+
+-- --------------------------------------------------------------
+select gm.log_script_insertion('v23-cfg-drop_old.sql', 'v23');
