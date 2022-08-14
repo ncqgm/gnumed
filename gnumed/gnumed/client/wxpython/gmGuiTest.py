afd09a201ef9838d76b2467aa06e8e85b93b47bd
Karsten Hilbert <karsten.hilbert@gmx.net>
2022-08-14 23:50:03 +0200

    Improve GUI testing framework

--- gnumed/gnumed/client/wxpython/gmGuiTest.py
+++ gnumed/gnumed/client/wxpython/gmGuiTest.py
@@ -28,7 +28,7 @@ from Gnumed.business import gmPersonSearch
 _log = logging.getLogger('gm.guitest')
 
 #==============================================================================
-def test_widget(widget, patient=None):
+def test_widget(widget_class, patient=None, size=None):
 	gmPG2.request_login_params(setup_pool = True, force_tui = True)
 	gmPraxis.activate_first_praxis_branch()
 	if patient is None:
@@ -38,13 +38,56 @@ def test_widget(widget, patient=None):
 
 	gmPerson.set_active_patient(patient = patient)
 	app = wx.App()
-	frame = wx.Frame(None, id = -1, title = _('GNUmed widget test: %s') % widget, size = (800, 600))
+	if size is None:
+		size = (800, 600)
+	frame = wx.Frame(None, id = -1, title = _('GNUmed widget test: %s') % widget_class, size = size)
 	frame.CentreOnScreen(wx.BOTH)
 	app.SetTopWindow(frame)
-	widget(frame)
+	widget = widget_class(frame)
+	#widget.matcher.print_queries = True
 	frame.Show(True)
-	app.MainLoop()
+	try:
+		app.MainLoop()
+	except Exception:
+		_log.log_stack_trace(message = 'test failure')
 
+#==============================================================================
+def setup_widget_test_env(patient=None):
+	"""Setup widget test environment.
+
+	- connect to DB
+	- setup praxis branch
+	- setup active patient, asking for patient if not given
+	- setup wx app
+	- setup wx frame as topwindow
+
+	Does *not* run app.MainLoop(). You will have to do that
+	yourself like so:
+
+	main_frame = setup_widget_test_env(...)
+	my_widget = cWidgetClass(main_frame, ...)
+	my_widget.whatever(...)
+	wx.GetApp().MainLoop()
+
+	Returns:
+		main frame, which can be used to instantiate widgets
+	"""
+	gmPG2.request_login_params(setup_pool = True, force_tui = True)
+	gmPraxis.activate_first_praxis_branch()
+	if patient is None:
+		patient = gmPersonSearch.ask_for_patient()
+	if patient is None:
+		sys.exit()
+
+	gmPerson.set_active_patient(patient = patient)
+	app = wx.App()
+	frame = wx.Frame(None, id = -1, title = _('GNUmed widget test'), size = (800, 600))
+	frame.CentreOnScreen(wx.BOTH)
+	app.SetTopWindow(frame)
+	frame.Show(True)
+	return frame
+
+#==============================================================================
 #==============================================================================
 if __name__ == '__main__':
 
