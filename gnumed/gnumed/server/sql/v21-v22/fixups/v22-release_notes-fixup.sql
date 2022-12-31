aee857fdf8bcf4614a3d4b00448a029c38c68766
Karsten Hilbert <karsten.hilbert@gmx.net>
2022-12-31 15:15:10 +0100

    Update release notes for 1.8.8

--- gnumed/gnumed/server/sql/v21-v22/fixups/v22-release_notes-fixup.sql
+++ gnumed/gnumed/server/sql/v21-v22/fixups/v22-release_notes-fixup.sql
@@ -17,19 +17,26 @@ INSERT INTO dem.message_inbox (
 ) VALUES (
 	(select pk from dem.staff where db_user = 'any-doc'),
 	(select pk_type from dem.v_inbox_item_type where type = 'memo' and category = 'administrative'),
-	'Release Notes for GNUmed 1.8.7 (database v22.18)',
-	'GNUmed 1.8.7 Release Notes:
+	'Release Notes for GNUmed 1.8.8 (database v22.18)',
+	'GNUmed 1.8.8 Release Notes:
 
-	1.8.7
+	1.8.8
 
-FIX: export area: dumping encrypted/PDFed image to disk
-FIX: top panel: heart rate display
-FIX: paperwork: recalls list LaTeX template
+IMPROVED: PACS: better image/image buttons placement
+
+FIX: py3.10+ *requires* ints for rescaling images [thanks henrique]
+FIX: patient tags: do not crash when rescaling image fails [thanks henrique]
+FIX: fix a number of errors found by pyflakes3
+FIX: lists: no more wx.LIST_HITTEST_ONITEMRIGHT in wxPython 4.2 [thanks jonas]
+FIX: date/time input: exception on entering "0"
 
 	22.18
 
 IMPROVED: bootstrapper: schema "public" permissions and ownership as per PG 15
+IMPROVED: backup: avoid unnecessary recompression
+
+FIX: bootstrapper: schema hash function in v19+ databases
 ');
 
 -- --------------------------------------------------------------
-select gm.log_script_insertion('v22-release_notes-fixup.sql', '22.18@1.8.7');
+select gm.log_script_insertion('v22-release_notes-fixup.sql', '22.18@1.8.8');
