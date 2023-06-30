15c7d303ba570795c9475954b9342bd655932f6d
Karsten Hilbert <karsten.hilbert@gmx.net>
2023-06-30 15:53:20 +0200

    Update release notes

--- gnumed/gnumed/server/sql/v21-v22/fixups/v22-release_notes-fixup.sql
+++ gnumed/gnumed/server/sql/v21-v22/fixups/v22-release_notes-fixup.sql
@@ -17,17 +17,17 @@ INSERT INTO dem.message_inbox (
 ) VALUES (
 	(select pk from dem.staff where db_user = 'any-doc'),
 	(select pk_type from dem.v_inbox_item_type where type = 'memo' and category = 'administrative'),
-	'Release Notes for GNUmed 1.8.12 (database v22.22)',
-	'GNUmed 1.8.12 Release Notes:
+	'Release Notes for GNUmed 1.8.13 (database v22.23)',
+	'GNUmed 1.8.13 Release Notes:
 
-	1.8.12
+	1.8.13
 
-FIX: waiting list: exception on removing entry [thanks Marc]
+FIX: billing: exception on saving bill w/o address [thanks M.Schmid]
 
-	22.22
+	22.23
 
-No changes over 22.21./22.20/22.19.
+No changes over v22.19 database.
 ');
 
 -- --------------------------------------------------------------
-select gm.log_script_insertion('v22-release_notes-fixup.sql', '22.22@1.8.12');
+select gm.log_script_insertion('v22-release_notes-fixup.sql', '22.23@1.8.13');
