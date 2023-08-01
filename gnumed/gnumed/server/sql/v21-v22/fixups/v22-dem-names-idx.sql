692a5cea30d58c1c831b1efd1e1300be27030ba5
Karsten Hilbert <karsten.hilbert@gmx.net>
2023-08-01 16:06:28 +0200

    Add indices to dem.names
    (cherry picked from commit b7faf243c7397e48a1110089aecafa61a3c51375)

--- gnumed/gnumed/server/sql/v21-v22/fixups/v22-dem-names-idx.sql
+++ gnumed/gnumed/server/sql/v21-v22/fixups/v22-dem-names-idx.sql
@@ -15,21 +15,34 @@ create index idx_dem_names_id_identity on dem.names(id_identity);
 
 -- lastnames
 drop index if exists dem.idx_dem_names_last cascade;
-create index idx_dem_names_last on dem.names(lastnames);
+drop index if exists dem.idx_dem_names_lastnames cascade;
+create index idx_dem_names_lastnames on dem.names(lastnames);
 
 drop index if exists dem.idx_dem_names_last_lower cascade;
-create index idx_dem_names_last_lower on dem.names (lower(lastnames));
+drop index if exists dem.idx_dem_names_lastnames_lower cascade;
+create index idx_dem_names_lastnames_lower on dem.names (lower(lastnames));
 
 drop index if exists dem.idx_dem_names_last_trgm cascade;
-create index idx_dem_names_last_trgm on dem.names using gin (lastnames pgtrgm.gin_trgm_ops);
+drop index if exists dem.idx_dem_names_lastnames_trgm cascade;
+create index idx_dem_names_lastnames_trgm on dem.names using gin (lastnames pgtrgm.gin_trgm_ops);
 
 
 -- firstnames
+drop index if exists dem.idx_names_firstnames cascade;
+drop index if exists dem.idx_dem_names_firstnames cascade;
+create index idx_dem_names_firstnames on dem.names (firstnames);
+
 drop index if exists dem.idx_dem_names_firstnames_lower cascade;
 create index idx_dem_names_firstnames_lower on dem.names (lower(firstnames));
 
 drop index if exists dem.idx_dem_names_first_trgm cascade;
-create index idx_dem_names_first_trgm on dem.names using gin (firstnames pgtrgm.gin_trgm_ops);
+drop index if exists dem.idx_dem_names_firstnames_trgm cascade;
+create index idx_dem_names_firstnames_trgm on dem.names using gin (firstnames pgtrgm.gin_trgm_ops);
+
+
+-- active names
+drop index if exists dem.idx_dem_names_active_names cascade;
+create index idx_dem_names_active_names on dem.names (id_identity, active) WHERE active IS TRUE;
 
 -- --------------------------------------------------------------
 select gm.log_script_insertion('v22-dem-names-idx.sql', '22.25');
