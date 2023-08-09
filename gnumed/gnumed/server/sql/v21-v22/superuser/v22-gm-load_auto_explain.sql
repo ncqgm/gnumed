2697b024ca5ec3c2fa667fea270ab891ead9be46
Karsten Hilbert <karsten.hilbert@gmx.net>
2023-08-09 19:57:59 +0200

    Improve auto_explain use
    	- decrease minimal duration to 1,5s
    	- enable logging of nested statements (inside functions, that is)

--- gnumed/gnumed/server/sql/v21-v22/superuser/v22-gm-load_auto_explain.sql
+++ gnumed/gnumed/server/sql/v21-v22/superuser/v22-gm-load_auto_explain.sql
@@ -19,21 +19,22 @@ create or replace function gm.load_auto_explain(IN _min_duration integer)
 	security definer
 	as '
 BEGIN
-	IF _min_duration < 2000 THEN
-		_min_duration := 2000;
+	IF _min_duration < 1500 THEN
+		_min_duration := 1500;
 	END IF;
 	LOAD ''auto_explain'';
 	PERFORM set_config(''auto_explain.log_min_duration''::TEXT, _min_duration::TEXT, false);
 	PERFORM set_config(''auto_explain.log_analyze''::TEXT, ''ON''::TEXT, false);
 	PERFORM set_config(''auto_explain.log_verbose''::TEXT, ''ON''::TEXT, false);
 	PERFORM set_config(''auto_explain.log_timing''::TEXT, ''ON''::TEXT, false);
+	PERFORM set_config(''auto_explain.log_nested_statements''::TEXT, ''ON''::TEXT, false);
 	RETURN TRUE;
 END;';
 
 COMMENT ON FUNCTION gm.load_auto_explain(IN _min_duration integer) IS
 'Load and configure auto_explain.
 .
-Integer argument = auto_explain.log_min_duration in milliseconds with a lower bound of 2000.';
+Integer argument = auto_explain.log_min_duration in milliseconds with a lower bound of 1500.';
 
 -- --------------------------------------------------------------
-select gm.log_script_insertion('v22-gm-load_auto_explain.sql', '22.25');
+select gm.log_script_insertion('v22-gm-load_auto_explain.sql', '22.26');
