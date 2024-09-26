eef110d25567e1052dd9d8a2a99a15214accd592
Karsten Hilbert <karsten.hilbert@gmx.net>
2024-09-26 17:45:29 +0200

    Bump version of server packages

--- gnumed/gnumed/dists/Linux/make-release_tarball.sh
+++ gnumed/gnumed/dists/Linux/make-release_tarball.sh
@@ -8,7 +8,7 @@ TS=""
 CLIENTREV="1.8.19${TS}"
 CLIENTARCH="gnumed-client.$CLIENTREV.tgz"
 
-SRVREV="22.28${TS}"
+SRVREV="22.29${TS}"
 SRVARCH="gnumed-server.$SRVREV.tgz"
 
 LANG_LIST="ar bg ca cs da de el en_AU en_CA es fr id it ka nb nl pl pt pt_BR ro ru ru_RU sq sr sv tr uk zh_TW"
