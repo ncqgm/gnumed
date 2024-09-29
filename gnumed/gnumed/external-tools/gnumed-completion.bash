689c20bc07172f94022ac96c8f1b2eb2aa9dc099
Karsten Hilbert <karsten.hilbert@gmx.net>
2024-09-29 15:49:11 +0200

    Improve bash completion script

--- gnumed/gnumed/external-tools/gnumed-completion.bash
+++ gnumed/gnumed/external-tools/gnumed-completion.bash
@@ -1,9 +1,11 @@
 # bash completions for GNUmed
 
+
 _gnumed_completion()
 {
 	COMPREPLY=()
-	OPTS="--quiet --debug --slave --override-schema-check --skip-update-check --local-import --help --version -V -h -? --tool= --text-domain= --log-file= --conf-file= --lang-gettext="
+	OPTS="--quiet --debug --slave --override-schema-check --skip-update-check --local-import --help --version -V -h -? --hipaa --tool= --text-domain= --log-file= --conf-file= --lang-gettext="
+	TOOLS="check_enc_epi_xref export_pat_emr_structure check_mimetypes_in_archive read_all_rows_of_table fingerprint_db generate_man_page get_object_passphrases update_collations
 	CURR="${COMP_WORDS[COMP_CWORD]}"
 	PREV="${COMP_WORDS[COMP_CWORD-1]}"
 	PREVPREV="${COMP_WORDS[COMP_CWORD-2]}"
@@ -12,7 +14,6 @@ _gnumed_completion()
 		"=")
 			case "${PREV}" in
 				"--tool")
-					TOOLS="check_enc_epi_xref export_pat_emr_structure check_mimetypes_in_archive read_all_rows_of_table fingerprint_db generate_man_page"
 					COMPREPLY=($(compgen -W "${TOOLS}"))
 					return 0
 				;;
@@ -24,7 +25,6 @@ _gnumed_completion()
 		"=")
 			case "${PREVPREV}" in
 				"--tool")
-					TOOLS="check_enc_epi_xref export_pat_emr_structure check_mimetypes_in_archive read_all_rows_of_table fingerprint_db generate_man_page"
 					COMPREPLY=($(compgen -W "${TOOLS}" -- ${CURR}))
 					return 0
 				;;
