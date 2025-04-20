4f66bed034c51bddd34cd6e23b9d8a712730865a
Karsten Hilbert <karsten.hilbert@gmx.net>
2025-04-20 16:24:01 +0200

    Update release notes for 1.8.21/v22.31

--- gnumed/gnumed/server/sql/v21-v22/fixups/v22-release_notes-fixup.sql
+++ gnumed/gnumed/server/sql/v21-v22/fixups/v22-release_notes-fixup.sql
@@ -17,13 +17,21 @@ INSERT INTO dem.message_inbox (
 ) VALUES (
 	(select pk from dem.staff where db_user = 'any-doc'),
 	(select pk_type from dem.v_inbox_item_type where type = 'memo' and category = 'administrative'),
-	'Release Notes for GNUmed 1.8.20 (database v22.30)',
-	'GNUmed 1.8.20 Release Notes:
+	'Release Notes for GNUmed 1.8.21 (database v22.31)',
+	'GNUmed 1.8.21 Release Notes:
+
+	1.8.21
+
+FIX: startup: crash on fingerprinting v15+ servers [thanks gm-dbo]
 
 	1.8.20
 
 FIX: startup: crash on fingerprinting episodes in DB if gm-staff [thanks Maria]
 FIX: patient search: gm-staff shall not ensure patient-ness [thanks Maria]
+
+	22.31
+
+FIX: crash on fingerprinting v15+ servers [thanks gm-dbo]
 
 	22.30
 
@@ -34,4 +42,4 @@ FIX: permissions on org/unit tables/views
 ');
 
 -- --------------------------------------------------------------
-select gm.log_script_insertion('v22-release_notes-fixup.sql', '22.30@1.8.20');
+select gm.log_script_insertion('v22-release_notes-fixup.sql', '22.31@1.8.21');
