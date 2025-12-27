d4dac63ab880c84ef01adb151f1d1f278cb49066
Karsten Hilbert <karsten.hilbert@gmx.net>
2025-12-27 20:46:15 +0100

    Add plpgsql function to check for needed password update
    	This function checks whether a given user needs to renew
    	its password in order to switch it from md5 to
    	scram-sha-256 encryption.
    
    (cherry picked from commit a926f68d6ef6259e10bf6c25ff94e8ca6cd9abd4)

---
+++ gnumed/gnumed/server/sql/v21-v22/superuser/v22-gm-user_needs_md5_2_scramsha256_pwd_switch.sql
@@ -0,0 +1,36 @@
+-- ==============================================================
+-- GNUmed database schema change script
+--
+-- License: GPL v2 or later
+-- Author: karsten.hilbert@gmx.net
+--
+-- ==============================================================
+\set ON_ERROR_STOP 1
+--set default_transaction_read_only to off;
+
+set check_function_bodies to on;
+
+-- --------------------------------------------------------------
+drop function if exists gm.user_needs_md5_2_scramsha256_pwd_switch(IN _user TEXT) cascade;
+
+create or replace function gm.user_needs_md5_2_scramsha256_pwd_switch(IN _user TEXT)
+	returns boolean
+	language 'plpgsql'
+	security definer
+	as '
+BEGIN
+	PERFORM 1 FROM pg_authid WHERE
+		rolname = _user
+			AND
+		rolpassword LIKE ''md5%'';
+	IF NOT FOUND THEN
+		RETURN FALSE;
+	END IF;
+	RETURN TRUE;
+END;';
+
+COMMENT ON FUNCTION gm.user_needs_md5_2_scramsha256_pwd_switch(IN _user TEXT) IS
+'Check if a given user needs to renew the password for making the encryption method switch.';
+
+-- --------------------------------------------------------------
+select gm.log_script_insertion('v22-gm-user_needs_md5_2_scramsha256_pwd_switch.sql', '22.33');
