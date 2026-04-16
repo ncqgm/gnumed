49dd965c3c9cbf9f01ea9248b3217f8b81669830
Karsten Hilbert <karsten.hilbert@gmx.net>
2026-04-16 19:34:05 +0200

    Prepare 1.8.24/22.34 release

--- gnumed/gnumed/server/sql/v21-v22/fixups/v22-release_notes-fixup.sql
+++ gnumed/gnumed/server/sql/v21-v22/fixups/v22-release_notes-fixup.sql
@@ -17,20 +17,23 @@ INSERT INTO dem.message_inbox (
 ) VALUES (
 	(select pk from dem.staff where db_user = 'any-doc'),
 	(select pk_type from dem.v_inbox_item_type where type = 'memo' and category = 'administrative'),
-	'Release Notes for GNUmed 1.8.23 (database v22.33)',
-	'GNUmed 1.8.23 Release Notes:
+	'Release Notes for GNUmed 1.8.24 (database v22.34)',
+	'GNUmed 1.8.24 Release Notes:
 
-	1.8.23
+	1.8.24
 
-FIX: hyphenated module names failing in newer Python versions [thanks María]
+FIX: more robust UI update when quickly scrolling document tree
+FIX: more robust handling of DICOM instance download issues
+FIX: more robust Orthanc DICOM instance parsing
 
-IMPROVED: UI: cfg: notebook tabs position [thanks María]
-IMPROVED: DB: warn on non-SCRAM passwords
+	22.34
 
-	22.33
-
-FIX: boostrapping: gm-dbo cannot GRANT ...  WITH ADMIN to itself
+FIX: bootstrapping: chicken-egg-problem with gm-dbo vs groups creation
+FIX: bootstrapping: use pg_roles rather than pg_user b/c of collation dependance
+FIX: gm.transfer_users(): switch from pg_user to pg_roles
+FIX: bootstrapping: properly set ADMIN on groups when running several upgrades in a row
+FIX: bootstrapping: using CREATE ROLE for users requires explicit LOGIN
 ');
 
 -- --------------------------------------------------------------
-select gm.log_script_insertion('v22-release_notes-fixup.sql', '22.33@1.8.23');
+select gm.log_script_insertion('v22-release_notes-fixup.sql', '22.34@1.8.24');
